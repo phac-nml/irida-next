5280db9351b17df4a01e763695bbf6cc35913b90
Deep Sidhu <deepsidhu85@gmail.com>
2023-02-08 14:48:34 -0600

    Added guard to watch for file changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root "dashboard#index"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "dashboard#index"
 end
