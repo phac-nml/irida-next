0b2c32dd759d38b3e1e119c6fe5fef700c58d5df
Eric Enns <eric.enns@phac-aspc.gc.ca>
2023-02-08 10:16:33 -0600

    feat: add in devise with generated user model and database authentication

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# entity class for User
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
