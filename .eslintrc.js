19279feab38c57281af1d091db890ea94db48456
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-02-10 10:48:12 -0600

    chore: Do not need to add react import

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   ],
   parserOptions: {
     project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
     ecmaFeatures: {
       jsx: true,
     },
@@ -31,5 +32,8 @@ module.exports = {
     "@typescript-eslint/no-unsafe-call": "warn",
     "@typescript-eslint/no-unsafe-member-access": "warn",
     "@typescript-eslint/no-unsafe-return": "warn",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
   },
+  ignorePatterns: ["config/webpack/**/*"],
 };
