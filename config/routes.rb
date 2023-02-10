bd8b1e960020c20e1f189fcda60ae4a3a5dcc071
Deep Sidhu <deepsidhu85@gmail.com>
2023-02-10 11:33:50 -0600

    Removed route not needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
