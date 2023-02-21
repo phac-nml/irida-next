a900579b33c2967f7910cbc809010922772ee0c3
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-02-21 12:56:29 -0600

    chore: Redirect after logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@
 # Main application controller
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
