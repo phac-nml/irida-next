56043caab5cbd4634239acb253414d09950a3b2e
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-02-21 15:33:19 -0600

    chore: Working edit password and profile

--- app/controllers/profiles/passwords_controller.rb
+++ app/controllers/profiles/passwords_controller.rb
@@ -1,7 +1,40 @@
 class Profiles::PasswordsController < ApplicationController
-  def edit
-  end
+  layout 'profiles'
+
+  before_action :set_user
+
+  def edit; end
 
   def update
+    # Devise::Models::DatabaseAuthenticatable#update_with_password
+    # Update record attributes when :current_password matches, otherwise returns error on :current_password.
+    # It also automatically rejects :password and :password_confirmation if they are blank.
+    if @user.update_with_password(update_password_params)
+
+      # Sign in the user bypassing validation in case his password changed
+      sign_in @user, bypass: true
+
+      redirect_to profile_path, notice: 'Profile was successfully updated'
+    else
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(@user, partial: 'form',
+                                                           locals: { user: @user })
+        end
+
+        format.html { render :show }
+      end
+
+    end
+  end
+
+  private
+
+  def update_password_params
+    params.require(:user).permit(:password, :password_confirmation, :current_password)
+  end
+
+  def set_user
+    @user = current_user
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,43 @@
+# frozen_string_literal: true
+
+# Controller for the user profile page
 class ProfilesController < ApplicationController
+  layout 'profiles'
+
+  before_action :set_user
+
   def show; end
 
-  def update; end
+  def update
+    # Devise::Models::DatabaseAuthenticatable#update_with_password
+    # Update record attributes when :current_password matches, otherwise returns error on :current_password.
+    # It also automatically rejects :password and :password_confirmation if they are blank.
+    if @user.update(update_params)
+
+      # Sign in the user bypassing validation in case his password changed
+      sign_in @user, bypass: true
+
+      redirect_to profile_path, notice: 'Profile was successfully updated'
+    else
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(@user, partial: 'profile',
+                                                           locals: { user: @user })
+        end
+
+        format.html { render :show }
+      end
+
+    end
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+  end
+
+  def set_user
+    @user = current_user
+  end
 end
