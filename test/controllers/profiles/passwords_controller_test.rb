f7d0e3c20d5e8947602641fc4c5f0a7b83a7549d
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-02-22 16:13:18 -0600

    chore: Updating tests

--- test/controllers/profiles/passwords_controller_test.rb
+++ test/controllers/profiles/passwords_controller_test.rb
@@ -1,13 +1,24 @@
-require "test_helper"
+# frozen_string_literal: true
 
-class Profiles::PasswordsControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
-    get profiles_passwords_edit_url
-    assert_response :success
-  end
+require 'test_helper'
 
-  test "should get update" do
-    get profiles_passwords_update_url
-    assert_response :success
+module Profiles
+  class PasswordsControllerTest < ActionDispatch::IntegrationTest
+    include Devise::Test::IntegrationHelpers
+
+    test 'should get edit' do
+      sign_in users(:john_doe)
+
+      get edit_profile_password_url
+      assert_response :success
+    end
+
+    test 'should update user password' do
+      sign_in users(:john_doe)
+
+      patch profile_password_url,
+            params: { user: { password: 'password', password_confirmation: 'password', current_password: 'password1' } }
+      assert_response :success
+    end
   end
 end
