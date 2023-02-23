a7d6365e4c8015b17c30e3985d7876a897f8bb6b
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-02-23 16:08:06 -0600

    chore: Removed empty helpers

--- app/helpers/profiles/accounts_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Purpose: Helper methods for the profile controller
-module Profiles
-  # Purpose: Helper methods for the account controller
-  module AccountsHelper
-  end
-end
