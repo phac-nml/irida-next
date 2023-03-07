f07a2058d947d69101a3187bfbae43a51ab3226e
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-07 14:05:23 -0600

    chore: Updated comments for new_groupd_controller.js

--- app/javascript/controllers/groups_new_controller.js
+++ app/javascript/controllers/groups_new_controller.js
@@ -4,10 +4,18 @@ import slugify from "@sindresorhus/slugify";
 export default class extends Controller {
   static targets = ["name", "path"];
 
+  /**
+   * When the controller is initialized, set the placeholder for the
+   * path input to be the slugified version of the name.
+   */
   initialize() {
     this.pathTarget.placeholder = slugify(this.nameTarget.placeholder);
   }
 
+  /**
+   * Called when the name input changes, and updated the path
+   * to be the slugified version of the name.
+   */
   nameChanged() {
     this.pathTarget.value = slugify(this.nameTarget.value);
   }
