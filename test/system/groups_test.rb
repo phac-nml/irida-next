602b3502368cf27e027302d008f9eb1358ed5829
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-08 10:59:29 -0600

    test: Test editing a group

--- test/system/groups_test.rb
+++ test/system/groups_test.rb
@@ -13,9 +13,13 @@ class GroupsTest < ApplicationSystemTestCase
     click_button I18n.t(:'general.navbar.new_dropdown.aria_label')
     click_link I18n.t(:'general.navbar.new_dropdown.group')
 
-    within %(div[data-controller="groups-new"]) do
+    within %(div[data-controller="groups-new"][data-controller-connected="true"]) do
       fill_in I18n.t(:'activerecord.attributes.group.name'), with: 'New group'
-      fill_in 'Path', with: 'new-group' # <-- working but should happen dynamically
+
+      assert_selector %(input[data-groups-new-target="path"]) do |input|
+        assert_equal 'new-group', input['value']
+      end
+
       fill_in 'Description', with: 'New group description'
       click_on I18n.t(:'groups.create.submit')
     end
@@ -29,14 +33,33 @@ class GroupsTest < ApplicationSystemTestCase
 
     click_link I18n.t(:'groups.show.create_subgroup_button')
 
-    within %(div[data-controller="groups-new"]) do
+    within %(div[data-controller="groups-new"][data-controller-connected="true"]) do
       fill_in I18n.t(:'activerecord.attributes.group.name'), with: 'New sub-group'
-      fill_in 'Path', with: 'new-sub-group' # <-- working but should happen dynamically
       fill_in 'Description', with: 'New sub-group description'
       click_on I18n.t(:'groups.new_subgroup.submit')
     end
 
     assert_text I18n.t(:'groups.create.success')
     assert_selector 'h1', text: 'New sub-group'
+  end
+
+  test 'can edit a group' do
+    visit group_url(groups(:group_one))
+
+    click_link I18n.t(:'groups.sidebar.settings')
+
+    within %(div[data-controller="groups-new"][data-controller-connected="true"]) do
+      fill_in I18n.t(:'activerecord.attributes.group.name'), with: 'Edited group'
+
+      assert_selector %(input[data-groups-new-target="path"]) do |input|
+        assert_equal 'edited-group', input['value']
+      end
+
+      fill_in 'Description', with: 'Edited group description'
+      click_on I18n.t(:'groups.edit.details.submit')
+    end
+
+    assert_text I18n.t(:'groups.update.success')
+    assert_selector 'h1', text: 'Edited group'
   end
 end
