4fa2abbc006a91a18842ce983b7f41dee3daaa5a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-08 07:31:35 -0600

    chore: Clean up comment to pass linting

--- test/test_helpers/better_rails_system_tests.rb
+++ test/test_helpers/better_rails_system_tests.rb
@@ -32,8 +32,9 @@ Capybara.default_normalize_ws = true
 # It could be useful to be able to configure this path from the outside (e.g., on CI).
 Capybara.save_path = ENV.fetch('CAPYBARA_ARTIFACTS', './tmp/capybara')
 
-# The Capybara.using_session allows you to manipulate a different browser session, and thus, multiple independent sessions within a single test scenario. That’s especially useful for testing real-time features, e.g., something with WebSocket.
-# This patch tracks the name of the last session used. We’re going to use this information to support taking failure screenshots in multi-session tests.
+# The Capybara.using_session allows you to manipulate a different browser session, and thus, multiple independent
+# sessions within a single test scenario. That’s especially useful for testing real-time features, e.g., something
+# with WebSocket.
 Capybara.singleton_class.prepend(Module.new do
   attr_accessor :last_used_session
 
