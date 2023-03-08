aaf7afac4124e7ddfc269a562a11f8ab2e26e583
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-08 07:22:19 -0600

    test: Added test for creating a sub-group

--- test/system/groups_test.rb
+++ test/system/groups_test.rb
@@ -8,7 +8,6 @@ class GroupsTest < ApplicationSystemTestCase
   end
 
   test 'can create a group' do
-    Capybara.current_driver = :selenium
     visit groups_url
 
     click_button I18n.t(:'general.navbar.new_dropdown.aria_label')
@@ -23,5 +22,21 @@ class GroupsTest < ApplicationSystemTestCase
 
     assert_text I18n.t(:'groups.create.success')
     assert_selector 'h1', text: 'New group'
+  end
+
+  test 'can create a sub-group' do
+    visit group_url(groups(:group_one))
+
+    click_link I18n.t(:'groups.show.create_subgroup_button')
+
+    within %(div[data-controller="groups-new"]) do
+      fill_in I18n.t(:'activerecord.attributes.group.name'), with: 'New sub-group'
+      fill_in 'Path', with: 'new-sub-group' # <-- working but should happen dynamically
+      fill_in 'Description', with: 'New sub-group description'
+      click_on I18n.t(:'groups.new_subgroup.submit')
+    end
+
+    assert_text I18n.t(:'groups.create.success')
+    assert_selector 'h1', text: 'New sub-group'
   end
 end
