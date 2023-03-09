d9dba6179335fc40f632628324fc4eb504687066
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-08 19:06:02 -0600

    chore: Toggle dark mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,17 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   before_action :authenticate_user!
+  before_action :set_theme
 
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
+  end
+
+  def set_theme
+    if params[:theme].present?
+      theme = params[:theme].to_sym
+      cookies[:theme] = theme
+      redirect_to(request.referer || root_path)
+    end
   end
 end
