dbf9ab5634e893089fd443f589415a80bb1fed3b
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-03-10 11:01:25 -0600

    adding some users to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(
+  [
+    { email: 'aaron.petkau@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
+    { email: 'eric.enns@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
+    { email: 'jeffrey.thiessen@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
+    { email: 'josh.adam@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
+    { email: 'katherine.thiessen@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
+    { email: 'khiem.bui@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
+    { email: 'sukhdeep.sidhu@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' }
+  ]
+)
