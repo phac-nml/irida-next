b8f59a2a4de7330e4ba1cd42176e48136a3e3568
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-03-10 13:41:58 -0600

    adding some groups and subgroups to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,29 @@
 
 if Rails.env.development?
 
+  # Seed Users
+  admin = User.create!({ email: 'admin@cscscience.ca', password: '123456', password_confirmation: '123456' })
+
   User.create!(
     [
-      { email: 'aaron.petkau@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
-      { email: 'eric.enns@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
-      { email: 'jeffrey.thiessen@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
-      { email: 'josh.adam@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
-      { email: 'katherine.thiessen@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
-      { email: 'khiem.bui@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' },
-      { email: 'sukhdeep.sidhu@phac-aspc.gc.ca', password: '123456', password_confirmation: '123456' }
+      { email: 'aaron.petkau@cscscience.ca', password: '123456', password_confirmation: '123456' },
+      { email: 'eric.enns@cscscience.ca', password: '123456', password_confirmation: '123456' },
+      { email: 'jeffrey.thiessen@cscscience.ca', password: '123456', password_confirmation: '123456' },
+      { email: 'josh.adam@cscscience.ca', password: '123456', password_confirmation: '123456' },
+      { email: 'katherine.thiessen@cscscience.ca', password: '123456', password_confirmation: '123456' },
+      { email: 'khiem.bui@cscscience.ca', password: '123456', password_confirmation: '123456' },
+      { email: 'sukhdeep.sidhu@cscscience.ca', password: '123456', password_confirmation: '123456' }
     ]
   )
 
+  # Seed Groups
+  1.upto(3) do |i|
+    parent_group = Group.create!(name: "Group #{i}", path: "group-#{i}",
+                                 description: "This is a description for group #{i}.", owner: admin)
+    # Seed Subgroups
+    1.upto(3) do |j|
+      Group.create!(name: "Subgroup #{j}", path: "subgroup-#{j}",
+                    description: "This is a description for subgroup #{j}.", owner: admin, parent: parent_group)
+    end
+  end
 end
