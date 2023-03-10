c10725e27a4907a268e26d76c071e0c5498dfdf5
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-10 06:30:25 -0600

    chore: Use rails in the bin

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: ["http://localhost:3000/"],
-      startServerCommand: "rails server -e production",
+      startServerCommand: "bin/rails server -e production",
     },
     upload: {
       target: "temporary-public-storage",
