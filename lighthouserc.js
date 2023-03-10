3b2f11adb45c17cad3007b2601633311d798b0fe
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-10 06:32:39 -0600

    chore: Try not running in production mode

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: ["http://localhost:3000/"],
-      startServerCommand: "bin/rails server -e production",
+      startServerCommand: "bin/rails server",
     },
     upload: {
       target: "temporary-public-storage",
