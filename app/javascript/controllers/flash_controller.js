6d9493cd209a6eac6b280fb97f47bf29a592cd69
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-15 10:47:28 -0500

    Added JSDoc

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,5 +1,8 @@
 import { Controller } from "@hotwired/stimulus";
 
+/**
+ * This controller is responsible for dismissing flash messages after a timeout
+ */
 export default class extends Controller {
   connect() {
     setTimeout(() => {
