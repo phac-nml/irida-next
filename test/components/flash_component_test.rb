d864bdf8ef0910a5438275996c8dcaac3f2539f9
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-15 15:57:34 -0500

    chore: Added generated files

---
+++ test/components/flash_component_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class FlashComponentTest < ViewComponent::TestCase
+  def test_success_message
+    message = 'Successful Message!'
+    render_inline(FlashComponent.new(type: :success)) { message }
+    assert_text message
+    assert_selector '.bg-green-100.text-green-700'
+  end
+
+  def test_error_message
+    message = 'Error Message!'
+    render_inline(FlashComponent.new(type: :error)) { message }
+    assert_text message
+    assert_selector '.bg-red-100.text-red-700'
+  end
+
+  def test_warning_message
+    message = 'Warning Message!'
+    render_inline(FlashComponent.new(type: :warning)) { message }
+    assert_text message
+    assert_selector '.bg-yellow-100.text-yellow-700'
+  end
+
+  def test_info_message
+    message = 'Info Message!'
+    render_inline(FlashComponent.new(type: :info)) { message }
+    assert_text message
+    assert_selector '.bg-blue-100.text-blue-700'
+  end
+end
