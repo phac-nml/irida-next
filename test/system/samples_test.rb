03cb70fa9d345b33b8e01631073d04ee50db2b61
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-03-16 14:03:28 -0500

    fixing rubocop failures for tests

--- test/system/samples_test.rb
+++ test/system/samples_test.rb
@@ -1,45 +1,47 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class SamplesTest < ApplicationSystemTestCase
   setup do
     @sample = samples(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit samples_url
-    assert_selector "h1", text: "Samples"
+    assert_selector 'h1', text: 'Samples'
   end
 
-  test "should create sample" do
+  test 'should create sample' do
     visit samples_url
-    click_on "New sample"
+    click_on 'New sample'
 
-    fill_in "Description", with: @sample.description
-    fill_in "Name", with: @sample.name
-    fill_in "Project", with: @sample.project_id
-    click_on "Create Sample"
+    fill_in 'Description', with: @sample.description
+    fill_in 'Name', with: @sample.name
+    fill_in 'Project', with: @sample.project_id
+    click_on 'Create Sample'
 
-    assert_text "Sample was successfully created"
-    click_on "Back"
+    assert_text 'Sample was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Sample" do
+  test 'should update Sample' do
     visit sample_url(@sample)
-    click_on "Edit this sample", match: :first
+    click_on 'Edit this sample', match: :first
 
-    fill_in "Description", with: @sample.description
-    fill_in "Name", with: @sample.name
-    fill_in "Project", with: @sample.project_id
-    click_on "Update Sample"
+    fill_in 'Description', with: @sample.description
+    fill_in 'Name', with: @sample.name
+    fill_in 'Project', with: @sample.project_id
+    click_on 'Update Sample'
 
-    assert_text "Sample was successfully updated"
-    click_on "Back"
+    assert_text 'Sample was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Sample" do
+  test 'should destroy Sample' do
     visit sample_url(@sample)
-    click_on "Destroy this sample", match: :first
+    click_on 'Destroy this sample', match: :first
 
-    assert_text "Sample was successfully destroyed"
+    assert_text 'Sample was successfully destroyed'
   end
 end
