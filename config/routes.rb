f2c015075c381e031bb1d8d04a62ed3f34293825
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-03-17 09:12:54 -0500

    moving samples routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :samples
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -12,6 +11,7 @@ Rails.application.routes.draw do
   scope path: '-' do
     resources :groups, only: %i[index new create]
     resources :projects, only: %i[new create]
+    resources :samples
 
     draw :profile
   end
