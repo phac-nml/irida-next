65cf05a3d6b3b98e17914fc9e36e79b8d0dd672a
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-03-22 11:17:57 -0500

    adding internationization to tests

--- test/system/projects/samples_test.rb
+++ test/system/projects/samples_test.rb
@@ -13,40 +13,40 @@ module Projects
 
     test 'visiting the index' do
       visit namespace_project_samples_url(namespace_id: @namespace.path, project_id: @project.path)
-      assert_selector 'h1', text: 'Samples'
+      assert_selector 'h1', text: I18n.t('projects.samples.index.title')
     end
 
     test 'should create sample' do
       visit namespace_project_samples_url(namespace_id: @namespace.path, project_id: @project.path)
-      click_on 'New sample'
+      click_on I18n.t('projects.samples.index.new_button')
 
       fill_in 'Description', with: @sample.description
       fill_in 'Name', with: 'New Name'
-      click_on 'Create sample'
+      click_on I18n.t('projects.samples.new.submit_button')
 
-      assert_text 'Sample was successfully created'
+      assert_text I18n.t('projects.samples.create.success')
       click_on 'Back'
     end
 
     test 'should update Sample' do
       visit namespace_project_sample_url(namespace_id: @namespace.path, project_id: @project.path, id: @sample.id)
-      click_on 'Edit this sample', match: :first
+      click_on I18n.t('projects.samples.show.edit_button'), match: :first
 
       fill_in 'Description', with: @sample.description
       fill_in 'Name', with: 'New Sample Name'
       click_on 'Update sample'
 
-      assert_text 'Sample was successfully updated'
-      click_on 'Back'
+      assert_text I18n.t('projects.samples.update.success')
+      click_on I18n.t('projects.samples.show.back_button')
     end
 
     test 'should destroy Sample' do
       visit namespace_project_sample_url(namespace_id: @namespace.path, project_id: @project.path, id: @sample.id)
       accept_confirm do
-        click_link 'Remove', match: :first
+        click_link I18n.t('projects.samples.index.remove_button'), match: :first
       end
 
-      assert_text 'Sample Sample 1 was removed'
+      assert_text I18n.t('projects.samples.destroy.success', sample_name: @sample.name)
     end
   end
 end
