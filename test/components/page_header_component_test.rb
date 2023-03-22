02661558889499b39d237cf16901e173b6e22b48
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-22 17:47:33 -0500

    test: Fixed test name

--- test/components/page_header_component_test.rb
+++ test/components/page_header_component_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class PageHeaderComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
+  def test_renders_header
     title = 'THIS IS THE TITLE'
     subtitle = 'THIS IS A SUBTITLE'
     render_inline(PageHeaderComponent.new(title:, subtitle:))
