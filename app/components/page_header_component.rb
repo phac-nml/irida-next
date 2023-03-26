6f8238f8ab1a30f7b14eaf2fdcc99c514b290ac1
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-26 14:38:06 -0500

    chore: Styled breadcrumbs, added to group layout

--- app/components/page_header_component.rb
+++ app/components/page_header_component.rb
@@ -2,7 +2,6 @@
 
 # Component for displaying a page header with title, subtitle and buttons
 class PageHeaderComponent < ViewComponent::Base
-  renders_one :breadcrumbs
   renders_one :icon
   renders_one :buttons
 
