d1d9b032673bf547d6d15ed0428db75df79fd1dd
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-27 13:07:30 -0500

    chore: Cleaned up breadcrumb_component.rb

--- app/components/breadcrumb_component.rb
+++ app/components/breadcrumb_component.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Create breadcrumbs for current route
 class BreadcrumbComponent < ViewComponent::Base
   def initialize(route:, request:)
     @links = build_crumbs(route, request)
@@ -8,21 +9,31 @@ class BreadcrumbComponent < ViewComponent::Base
   def build_crumbs(route, request)
     crumbs = []
     route.path.split('/').each_with_index do |_part, index|
-      crumbs << {
-        name: route.name.split(' / ')[index],
-        path: route.path.split('/')[0..index].join('/')
-      }
+      crumbs << crumb_for_route(route, index)
     end
     if "/#{crumbs.last[:path]}" != request.path
       begin
-        crumbs << {
-          name: t(:"views.#{request.params[:controller]}.#{request.params[:action]}", raise: true),
-          path: request.path.sub('/', '')
-        }
+        crumbs << crumb_for_request(request)
       rescue I18n::MissingTranslationData
         # Don't do shit
       end
     end
     crumbs
+  end
+
+  private
+
+  def crumb_for_route(route, index)
+    {
+      name: route.name.split(' / ')[index],
+      path: route.path.split('/')[0..index].join('/')
+    }
+  end
+
+  def crumb_for_request(request)
+    {
+      name: t(:"views.#{request.params[:controller]}.#{request.params[:action]}", raise: true),
+      path: request.path.sub('/', '')
+    }
   end
 end
