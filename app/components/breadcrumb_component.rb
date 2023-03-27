82faed9990d6ad0f57918d932528fa6ab83ee8ef
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-27 11:35:51 -0500

    chore: Added styling for last item

--- app/components/breadcrumb_component.rb
+++ app/components/breadcrumb_component.rb
@@ -1,17 +1,27 @@
 # frozen_string_literal: true
 
 class BreadcrumbComponent < ViewComponent::Base
-  def initialize(route:)
-    @links = build_crumbs(route)
+  def initialize(route:, request:)
+    @links = build_crumbs(route, request)
   end
 
-  def build_crumbs(route)
+  def build_crumbs(route, request)
     crumbs = []
     route.path.split('/').each_with_index do |_part, index|
       crumbs << {
         name: route.name.split(' / ')[index],
         path: route.path.split('/')[0..index].join('/')
       }
+    end
+    if "/#{crumbs.last[:path]}" != request.path
+      begin
+        crumbs << {
+          name: t(:"views.#{request.params[:controller]}.#{request.params[:action]}", raise: true),
+          path: request.path.sub('/', '')
+        }
+      rescue I18n::MissingTranslationData
+        # Don't do shit
+      end
     end
     crumbs
   end
