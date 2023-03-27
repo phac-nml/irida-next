bb607fa6442071f35b6f7b7074aa2ec4effd23fe
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-27 13:48:30 -0500

    test: Added breadcrumb testing

--- test/components/breadcrumb_component_test.rb
+++ test/components/breadcrumb_component_test.rb
@@ -1,12 +1,33 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class BreadcrumbComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(BreadcrumbComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_single_path
+    # Mock route
+    mock_route = routes(:group_one_route)
+
+    # Mock request
+    mock_request = ActionDispatch::TestRequest.create(Rack::MockRequest.env_for("/#{mock_route.path}"))
+
+    render_inline(BreadcrumbComponent.new(route: mock_route, request: mock_request))
+    assert_text 'Group 1'
+    assert_no_text 'Settings'
+    assert_selector 'a', count: 1
+    assert_none_of_selectors 'svg'
+  end
+
+  def test_compound_path
+    # Mock route
+    mock_route = routes(:group_one_route)
+
+    # Mock request
+    mock_request = ActionDispatch::TestRequest.create(Rack::MockRequest.env_for("/#{mock_route.path}/-/edit"))
+
+    render_inline(BreadcrumbComponent.new(route: mock_route, request: mock_request))
+    assert_text 'Group 1'
+    assert_text 'Settings'
+    assert_selector 'a', count: 2
+    assert_selector 'svg', count: 1
   end
 end
