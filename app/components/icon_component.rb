1951e7dddcaf2d6f157b1bc31a8487a744acdd2a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-27 10:44:49 -0500

    chore: Working on icon testing

---
+++ app/components/icon_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Icon Component for icons
+class IconComponent < Component
+  def initialize(name:, **system_arguments)
+    @source = heroicons_source(name)
+    @system_arguments = system_arguments
+    @system_arguments[:classes] = class_names(
+      @system_arguments[:classes]
+    )
+  end
+end
--- test/components/icon_component_test.rb
+++ test/components/icon_component_test.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class IconComponentTest < ViewComponent::TestCase
   def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(IconComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+    assert_equal(
+      %(<svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="w-5 h-5" focusable="false">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
+    </svg>),
+      render_inline(IconComponent.new(name: 'home')).to_html
+    )
   end
 end
