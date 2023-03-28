70ba665c92931f9fe5a2076b849c0cf49f46daeb
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-28 10:37:33 -0500

    chore: More navigation cleanup

--- app/components/icon_component.rb
+++ app/components/icon_component.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 # Icon Component for icons
-class IconComponent < BaseComponent
-  def initialize(name:, **system_arguments)
-    @source = heroicons_source(name)
-    @system_arguments = system_arguments
-    @system_arguments[:classes] = class_names(
-      @system_arguments[:classes]
-    )
+class IconComponent < Component
+  def initialize(name:, classes: nil)
+    @source = heroicons_source(name, classes)
   end
 end
--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -18,12 +18,12 @@ module ViewHelper
     classes.compact.uniq.join(' ')
   end
 
-  def heroicons_source(icon_name)
+  def heroicons_source(icon_name, classes)
     path = Rails.root.join('app', 'assets', 'icons', 'heroicons', "#{icon_name}.svg")
     file = File.read(path)
     doc = Nokogiri::HTML::DocumentFragment.parse(file)
     svg = doc.at_css 'svg'
-    svg[:class] = 'w-5 h-5'
+    svg[:class] = classes
     svg[:focusable] = false
     svg[:'aria-hidden'] = true
     doc.to_html.html_safe # rubocop:disable Rails/OutputSafety
