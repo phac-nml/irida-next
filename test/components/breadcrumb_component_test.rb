4db8255956a1072db8013798f61849154b26b9d0
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-29 14:51:18 -0500

    chore: Updated testing for all paths
    100% Coverage!

--- test/components/breadcrumb_component_test.rb
+++ test/components/breadcrumb_component_test.rb
@@ -53,4 +53,52 @@ class BreadcrumbComponentTest < ViewComponent::TestCase
     assert_selector 'a', count: 2
     assert_selector 'svg', count: 1
   end
+
+  def test_context_crumbs_not_an_array
+    # Mock route
+    mock_route = routes(:group_one_route)
+
+    # Mock context breadcrumb
+    context_crumbs = { name: I18n.t('groups.edit.title', raise: true), path: groups(:group_one).path }
+
+    assert_raises ArgumentError do
+      render_inline(BreadcrumbComponent.new(route: mock_route, context_crumbs:))
+    end
+  end
+
+  def test_context_crumbs_without_hash
+    # Mock route
+    mock_route = routes(:group_one_route)
+
+    # Mock context breadcrumb
+    context_crumbs = ['FOOBAR']
+
+    assert_raises ArgumentError do
+      render_inline(BreadcrumbComponent.new(route: mock_route, context_crumbs:))
+    end
+  end
+
+  def test_context_crumbs_without_name
+    # Mock route
+    mock_route = routes(:group_one_route)
+
+    # Mock context breadcrumb
+    context_crumbs = [{ path: groups(:group_one).path }]
+
+    assert_raises ArgumentError do
+      render_inline(BreadcrumbComponent.new(route: mock_route, context_crumbs:))
+    end
+  end
+
+  def test_context_crumbs_without_path
+    # Mock route
+    mock_route = routes(:group_one_route)
+
+    # Mock context breadcrumb
+    context_crumbs = [{ name: I18n.t('groups.edit.title', raise: true) }]
+
+    assert_raises ArgumentError do
+      render_inline(BreadcrumbComponent.new(route: mock_route, context_crumbs:))
+    end
+  end
 end
