63a77335a32e38ba1311caf7057db2b8bca9d8fc
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-29 22:11:54 -0500

    test: For full navigation

--- test/components/navigation_component_test.rb
+++ test/components/navigation_component_test.rb
@@ -3,10 +3,26 @@
 require 'test_helper'
 
 class NavigationComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(NavigationComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_should_render_navigation
+    render_inline Navigation::NavigationComponent.new do |navigation|
+      navigation.with_header(label: 'Home', url: '/', icon: 'home')
+      navigation.with_section do |section|
+        section.with_item(label: 'Dashboard', url: '/dashboard', icon: 'home')
+        section.with_item(label: 'Settings', url: '/settings', icon: 'cog_6_tooth')
+      end
+    end
+
+    assert_selector 'aside' do
+      assert_text 'Home'
+      assert_selector 'ul' do
+        assert_selector 'li', count: 2
+        assert_selector 'a[href="/dashboard"]'
+        assert_selector 'a[href="/settings"]'
+      end
+    end
+
+    assert_selector 'button[data-action="click->layout#toggle"]' do
+      assert_selector 'svg.w-6.h-6'
+    end
   end
 end
