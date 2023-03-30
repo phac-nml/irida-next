e88f4556316d88404b8ce05bd7ee8875fdf50e02
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-29 22:17:18 -0500

    test: All components tested to 100% coverage

--- app/components/navigation/navigation_component.rb
+++ app/components/navigation/navigation_component.rb
@@ -18,9 +18,5 @@ module Navigation
     def initialize(**system_arguments)
       @system_arguments = system_arguments
     end
-
-    def renders?
-      sections.any? || items.any?
-    end
   end
 end
