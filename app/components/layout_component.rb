65ce64d754d24b375775e7f656f5284bd74192b7
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-03-30 15:41:02 -0500

    chore: Updated comments

--- app/components/layout_component.rb
+++ app/components/layout_component.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# This component is used to render the layout of the application.
 class LayoutComponent < Component
   renders_one :sidebar, Navigation::NavigationComponent
   renders_one :body
