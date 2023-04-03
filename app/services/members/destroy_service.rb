219ab788d10e2d2602fbddb46c7afc6f4b48aadb
Deep Sidhu <deepsidhu85@gmail.com>
2023-04-03 13:07:08 -0500

    Updated members destroy service to break up execute method to fix rubocop branch assignment length error

--- app/services/members/destroy_service.rb
+++ app/services/members/destroy_service.rb
@@ -12,15 +12,10 @@ module Members
     end
 
     def execute
-      if current_user == member.user
-        member.errors.add(:base,
-                          I18n.t('services.members.destroy.cannot_remove_self',
-                                 namespace_type: namespace.type.downcase))
-      elsif namespace.owners.include?(current_user)
-        member.destroy
+      if (current_user == member.user) || namespace.owners.exclude?(current_user)
+        member.errors.add(:base, error_message)
       else
-        member.errors.add(:base,
-                          I18n.t('services.members.destroy.no_permission', namespace_type: namespace.type.downcase))
+        member.destroy
       end
     end
 
@@ -29,6 +24,15 @@ module Members
         namespace.group_members
       else
         namespace.project_members
+      end
+    end
+
+    def error_message
+      if current_user == member.user
+        I18n.t('services.members.destroy.cannot_remove_self',
+               namespace_type: namespace.type.downcase)
+      else
+        I18n.t('services.members.destroy.no_permission', namespace_type: namespace.type.downcase)
       end
     end
   end
