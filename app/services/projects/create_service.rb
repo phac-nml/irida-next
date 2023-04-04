08b37072ffd9e4a85c9ba873297972318f2ba57a
Deep Sidhu <deepsidhu85@gmail.com>
2023-04-04 14:06:38 -0500

    Added basic permissions to check if a user is allowed to create a project in a namespace

--- app/services/projects/create_service.rb
+++ app/services/projects/create_service.rb
@@ -3,6 +3,7 @@
 module Projects
   # Service used to Create Projects
   class CreateService < BaseService
+    CreateError = Class.new(StandardError)
     attr_accessor :namespace_params
 
     def initialize(user = nil, params = {})
@@ -12,17 +13,38 @@ module Projects
 
     def execute
       @project = Project.new(params.merge(creator: current_user))
+      namespace = Namespace.find_by(id: namespace_params[:parent_id])
+      raise CreateError, I18n.t('services.projects.create.namespace_required') if namespace.nil?
 
-      @project.build_namespace(namespace_params.merge(owner: current_user))
+      unless Project.allowed_to_create_new_project_in_namespace?(namespace)
+        raise CreateError,
+              I18n.t('services.projects.create.no_permission',
+                     namespace_type: namespace.type.downcase)
+      end
+      create_associations(@project)
+      @project
+    rescue Projects::CreateService::CreateError => e
+      @project.errors.add(:base, e.message)
+      false
+    end
 
-      @project.save
+    def allowed_to_create_new_project_in_namespace?(namespace)
+      if namespace.group_namespace?
+        namespace.owners.include?(current_user)
+      elsif namespace.user_namespace?
+        namespace.owner == current_user
+      else
+        false
+      end
+    end
 
+    def create_associations(project)
+      project.build_namespace(namespace_params.merge(owner: current_user))
+      project.save
       Members::CreateService.new(current_user, @project.namespace, {
                                    user: current_user,
                                    access_level: Member::AccessLevel::OWNER
                                  }).execute
-
-      @project
     end
   end
 end
--- test/services/projects/create_service_test.rb
+++ test/services/projects/create_service_test.rb
@@ -7,9 +7,10 @@ module Projects
     def setup
       @user = users(:john_doe)
       @parent_namespace = namespaces_user_namespaces(:john_doe_namespace)
+      @parent_group_namespace = groups(:group_one)
     end
 
-    test 'create project with valid params' do
+    test 'create project with valid params under user namespace' do
       valid_params = { namespace_attributes: { name: 'proj1', path: 'proj1', parent_id: @parent_namespace.id } }
 
       assert_difference -> { Project.count } => 1, -> { Members::ProjectMember.count } => 1 do
@@ -22,6 +23,30 @@ module Projects
 
       assert_no_difference ['Project.count', 'Members::ProjectMember.count'] do
         Projects::CreateService.new(@user, invalid_params).execute
+      end
+    end
+
+    test 'create project with valid params but incorrect permissions under user namespace' do
+      valid_params = { namespace_attributes: { name: 'proj1', path: 'proj1', parent_id: @parent_namespace.id } }
+      user = users(:steve_doe)
+      assert_no_difference ['Project.count', 'Members::ProjectMember.count'] do
+        Projects::CreateService.new(user, valid_params).execute
+      end
+    end
+
+    test 'create project with valid params under group namespace' do
+      valid_params = { namespace_attributes: { name: 'proj1', path: 'proj1', parent_id: @parent_group_namespace.id } }
+
+      assert_difference -> { Project.count } => 1, -> { Members::ProjectMember.count } => 1 do
+        Projects::CreateService.new(@user, valid_params).execute
+      end
+    end
+
+    test 'create project with valid params but incorrect permissions under group namespace' do
+      valid_params = { namespace_attributes: { name: 'proj1', path: 'proj1', parent_id: @parent_group_namespace.id } }
+      user = users(:steve_doe)
+      assert_no_difference ['Project.count', 'Members::ProjectMember.count'] do
+        Projects::CreateService.new(user, valid_params).execute
       end
     end
   end
