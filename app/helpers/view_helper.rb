f1114d9f79a3cf835b3e6404d61abebd60c63294
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-04-05 09:58:25 -0500

    chore: Fixed viral helper

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -2,6 +2,16 @@
 
 # ViewHelper for user interface components
 module ViewHelper
+  VIRAL_HELPERS = {
+    icon: 'Viral::IconComponent'
+  }.freeze
+
+  VIRAL_HELPERS.each do |name, component|
+    define_method "viral_#{name}" do |*args, **kwargs, &block|
+      render component.constantize.new(*args, **kwargs), &block
+    end
+  end
+
   # Get the svg file for a heroicon by name, and add the applicable classes
   def heroicons_source(icon_name, classes)
     path = Rails.root.join('app', 'assets', 'icons', 'heroicons', "#{icon_name}.svg")
