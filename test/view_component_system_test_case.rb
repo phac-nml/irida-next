eb4214bea2055a000bf6b07c330ae9ff7c76a61e
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-04-06 11:52:33 -0500

    chore: Added view_component_system_test_case

---
+++ test/view_component_system_test_case.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'test_helpers/capybara_setup'
+require 'test_helpers/cuprite_helpers'
+require 'test_helpers/cuprite_setup'
+
+class ViewComponentSystemTestCase < ViewComponent::SystemTestCase
+  driven_by :irida_next_cuprite
+
+  include CupriteHelpers
+end
