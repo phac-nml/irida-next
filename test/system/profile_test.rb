e22ddaf6cdb704e1f7b7255b156596ce470ba5fb
Eric Enns <492127+ericenns@users.noreply.github.com>
2023-04-06 15:26:51 -0500

    chore: add in system for revoking a personal access token

--- test/system/profile_test.rb
+++ test/system/profile_test.rb
@@ -4,8 +4,9 @@ require 'application_system_test_case'
 
 class ProfileTest < ApplicationSystemTestCase
   def setup
-    login_as users(:john_doe)
-    @active_token_count = users(:john_doe).personal_access_tokens.active.count
+    @user = users(:john_doe)
+    login_as @user
+    @active_token_count = @user.personal_access_tokens.active.count
   end
 
   test 'is accessible' do
@@ -19,7 +20,7 @@ class ProfileTest < ApplicationSystemTestCase
 
     within %(form[action="/-/profile"]) do
       assert_selector %(input[id="user_email"]) do |input|
-        assert_equal users(:john_doe).email, input['value']
+        assert_equal @user.email, input['value']
       end
 
       fill_in 'Email', with: 'fred_doe@gmail.com'
@@ -67,7 +68,7 @@ class ProfileTest < ApplicationSystemTestCase
     click_link I18n.t(:'profiles.sidebar.access_tokens')
 
     within %(form[action="/-/profile/personal_access_tokens"]) do
-      fill_in 'Token name', with: 'token'
+      fill_in 'Token name', with: 'my new token'
       check 'api', allow_label_click: true
       click_button I18n.t(:'profiles.personal_access_tokens.create.submit')
     end
@@ -75,7 +76,29 @@ class ProfileTest < ApplicationSystemTestCase
     assert_text I18n.t(:'profiles.personal_access_tokens.access_token_section.label')
     assert_text I18n.t(:'profiles.personal_access_tokens.access_token_section.description')
 
+    assert_text 'my new token'
     assert_text I18n.t(:'profiles.personal_access_tokens.index.active_personal_access_tokens',
                        count: @active_token_count + 1)
+  end
+
+  test 'can revoke personal access tokens' do
+    visit profile_path
+    click_link I18n.t(:'profiles.sidebar.access_tokens')
+
+    token_to_revoke = @user.personal_access_tokens.active.first
+
+    assert_text I18n.t(:'profiles.personal_access_tokens.index.active_personal_access_tokens',
+                       count: @active_token_count)
+    assert_text token_to_revoke.name
+
+    within %(tr[id=personal_access_token_#{token_to_revoke.id}]) do
+      accept_alert do
+        click_button I18n.t(:'profiles.personal_access_tokens.personal_access_token.revoke_button')
+      end
+    end
+
+    assert_no_text token_to_revoke.name
+    assert_text I18n.t(:'profiles.personal_access_tokens.index.active_personal_access_tokens',
+                       count: @active_token_count - 1)
   end
 end
