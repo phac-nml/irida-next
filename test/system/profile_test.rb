4c5bad859bf8240d846aacc66521edcbf3ec6b43
Eric Enns <492127+ericenns@users.noreply.github.com>
2023-04-06 15:17:31 -0500

    chore: add in basic system testing for profiles personal access tokens section

--- test/system/profile_test.rb
+++ test/system/profile_test.rb
@@ -5,6 +5,7 @@ require 'application_system_test_case'
 class ProfileTest < ApplicationSystemTestCase
   def setup
     login_as users(:john_doe)
+    @active_token_count = users(:john_doe).personal_access_tokens.active.count
   end
 
   test 'is accessible' do
@@ -51,5 +52,30 @@ class ProfileTest < ApplicationSystemTestCase
     end
 
     assert_text 'Register now'
+  end
+
+  test 'can view personal access tokens' do
+    visit profile_path
+    click_link I18n.t(:'profiles.sidebar.access_tokens')
+
+    assert_text I18n.t(:'profiles.personal_access_tokens.index.active_personal_access_tokens',
+                       count: @active_token_count)
+  end
+
+  test 'can create personal access tokens' do
+    visit profile_path
+    click_link I18n.t(:'profiles.sidebar.access_tokens')
+
+    within %(form[action="/-/profile/personal_access_tokens"]) do
+      fill_in 'Token name', with: 'token'
+      check 'api', allow_label_click: true
+      click_button I18n.t(:'profiles.personal_access_tokens.create.submit')
+    end
+
+    assert_text I18n.t(:'profiles.personal_access_tokens.access_token_section.label')
+    assert_text I18n.t(:'profiles.personal_access_tokens.access_token_section.description')
+
+    assert_text I18n.t(:'profiles.personal_access_tokens.index.active_personal_access_tokens',
+                       count: @active_token_count + 1)
   end
 end
