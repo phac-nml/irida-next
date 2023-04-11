3148e254f559a035ac5e63e9a149be6effe04ead
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-04-11 09:55:31 -0500

    test: Fixed testing on flash preview

--- test/components/viral/system/flash_component_test.rb
+++ test/components/viral/system/flash_component_test.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-require 'view_component_system_test_case'
+require 'application_system_test_case'
 
 module System
-  class FlashComponentTest < ViewComponentSystemTestCase
-    def success_message_with_javascript_closes
+  class FlashComponentTest < ApplicationSystemTestCase
+    test 'flash message with javascript closes' do
       visit('/rails/view_components/flash_component/success')
       message = 'Successful Message!'
 
       assert_text message
-      # assert_selector '.bg-green-700'
-      # find('[data-action="flash#dismiss"]').click
-      # assert_no_text message
+      assert_selector '.bg-green-700'
+      find('[data-action="flash#dismiss"]').click
+      assert_no_text message
     end
   end
 end
