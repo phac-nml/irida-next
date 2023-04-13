e99b803ccbce93f8f4e329ece831ce455a9c8450
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-04-13 08:12:42 -0500

    chore: Removed code that was updated by method

--- app/components/viral/dropdown_component.rb
+++ app/components/viral/dropdown_component.rb
@@ -38,7 +38,7 @@ module Viral
 
     def system_arguments
       @system_arguments.tap do |opts|
-        opts[:tag] ||= :button
+        opts[:tag] = :button
         opts[:data] = { 'viral--dropdown-target': 'trigger' }
         opts[:classes] ||= 'py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700' # rubocop:disable Layout/LineLength
         opts[:classes] = class_names(
