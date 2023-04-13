2b1f41c96540db8314f3b877046258fee7142140
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-04-13 13:26:44 -0500

    chore: Added preciew with item icons

--- test/components/previews/dropdown_component_preview.rb
+++ test/components/previews/dropdown_component_preview.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DropdownComponentPreview < ViewComponent::Preview
-  def default
+  def with_label_and_caret
     render Viral::DropdownComponent.new(label: 'Items', caret: true) do |dropdown|
       dropdown.item(label: 'Item 1', url: '#')
       dropdown.item(label: 'Item 2', url: '#')
@@ -12,6 +12,13 @@ class DropdownComponentPreview < ViewComponent::Preview
     render Viral::DropdownComponent.new(icon: 'bars_3') do |dropdown|
       dropdown.item(label: 'Item 1', url: '#')
       dropdown.item(label: 'Item 2', url: '#')
+    end
+  end
+
+  def with_item_icon
+    render Viral::DropdownComponent.new(label: 'Items', caret: true) do |dropdown|
+      dropdown.item(label: 'Item 1', url: '#', icon_name: 'check')
+      dropdown.item(label: 'Item 2', url: '#', icon_name: 'inbox_stack')
     end
   end
 end
