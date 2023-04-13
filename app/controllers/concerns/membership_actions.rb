a11c9c768378fa5e50e557466b32a1b73e43342f
Deep Sidhu <deepsidhu85@gmail.com>
2023-04-12 22:29:04 -0500

    Updated access_levels to get the member with the highest role in the hierarchy, updated last_namespace_owner_member function

--- app/controllers/concerns/membership_actions.rb
+++ app/controllers/concerns/membership_actions.rb
@@ -50,7 +50,7 @@ module MembershipActions
   private
 
   def access_levels
-    member = Member.find_by(user: current_user, namespace: @namespace.self_and_ancestors)
+    member = Member.where(user: current_user, namespace: @namespace.self_and_ancestors).order(:access_level).last
     @access_levels = Member.access_levels(member)
   end
 
--- app/models/members/group_member.rb
+++ app/models/members/group_member.rb
@@ -12,9 +12,10 @@ module Members
 
     # Method to ensure we don't leave a group or project without an owner
     def last_namespace_owner_member
-      return if destroyed_by_association || Members::GroupMember.where(namespace: namespace.self_and_ancestors,
-                                                                       access_level: Member::AccessLevel::OWNER)
-                                                                .order(:access_level).many?
+      return if destroyed_by_association
+      return if access_level != Member::AccessLevel::OWNER
+      return if Members::GroupMember.where(namespace: namespace.self_and_ancestors,
+                                           access_level: Member::AccessLevel::OWNER).order(:access_level).many?
 
       errors.add(:base,
                  I18n.t('activerecord.errors.models.member.destroy.last_member',
