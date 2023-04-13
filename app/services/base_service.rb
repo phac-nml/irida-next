ad5ed64c124bff1057149e57df15e25ce542e80d
Deep Sidhu <deepsidhu85@gmail.com>
2023-04-13 09:43:04 -0500

    Updated queries to use .exist?

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -11,33 +11,28 @@ class BaseService
 
   def allowed_to_modify_projects_in_namespace?(namespace)
     return true if namespace_owner_is_user?(namespace)
-    return true if namespace_owners_include_user?(namespace)
 
-    false
+    namespace_owners_include_user?(namespace)
   end
 
   def allowed_to_modify_members_in_namespace?(namespace)
     return true if namespace.project_namespace? && allowed_to_modify_projects_in_namespace?(namespace)
-    return true if namespace.group_namespace? && allowed_to_modify_group?(namespace)
 
-    false
+    namespace.group_namespace? && allowed_to_modify_group?(namespace)
   end
 
   def allowed_to_modify_group?(group)
     return true if group.parent.nil? && group.owner == current_user
-    return true if Members::GroupMember.where(namespace: group.self_and_ancestors, user: current_user,
-                                              access_level: Member::AccessLevel::OWNER).order(:access_level).last
 
-    false
+    Members::GroupMember.exists?(namespace: group.self_and_ancestors, user: current_user,
+                                 access_level: Member::AccessLevel::OWNER)
   end
 
   def namespace_owners_include_user?(namespace)
-    return true if Member.where(
+    Member.exists?(
       namespace: namespace.self_and_ancestors, user: current_user,
       access_level: Member::AccessLevel::OWNER
-    ).order(:access_level).last
-
-    false
+    )
   end
 
   def namespace_owner_is_user?(namespace)
@@ -50,9 +45,7 @@ class BaseService
   def user_has_namespace_maintainer_access?
     return unless namespace.group_namespace?
 
-    return true if Member.where(user: current_user, namespace: namespace.self_and_ancestors,
-                                access_level: Member::AccessLevel::MAINTAINER).last
-
-    false
+    Member.exists?(user: current_user, namespace: namespace.self_and_ancestors,
+                   access_level: Member::AccessLevel::MAINTAINER)
   end
 end
