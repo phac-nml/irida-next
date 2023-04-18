58c6ce5a515fe691aa6f7dd58574f62365f51942
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-04-18 10:42:51 -0500

    creating group samples controller and index view

---
+++ app/controllers/groups/samples_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Groups
+  # Controller actions for Samples
+  class SamplesController < ApplicationController
+    def index; end
+  end
+end
---
+++ test/controllers/groups/samples_controller_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Groups
+  class SamplesControllerTest < ActionDispatch::IntegrationTest
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
