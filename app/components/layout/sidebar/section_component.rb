68b5de79264cf88e6da8c54d750c64405583bd90
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-04-18 14:36:56 -0500

    chore: Cleaned up remaining attr_reader

--- app/components/layout/sidebar/section_component.rb
+++ app/components/layout/sidebar/section_component.rb
@@ -4,6 +4,8 @@ module Layout
   module Sidebar
     # Sidebar section component to group items
     class SectionComponent < Component
+      attr_reader :title
+
       renders_many :items, ItemComponent
 
       def initialize(title: nil)
