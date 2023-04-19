cf48b99aa618973c008b83182256d85d5bee6c8a
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-04-19 10:46:32 -0500

    starting to list the sample names

--- app/controllers/groups/samples_controller.rb
+++ app/controllers/groups/samples_controller.rb
@@ -3,6 +3,29 @@
 module Groups
   # Controller actions for Samples
   class SamplesController < ApplicationController
-    def index; end
+    layout 'groups'
+    before_action :context_crumbs, only: %i[index]
+
+    def index
+      @samples = Sample.where(project: group.descendants.where(type: 'Project'))
+    end
+
+    private
+
+    def group
+      return unless params[:group_id]
+
+      @group = Group.find_by_full_path(params[:group_id]) # rubocop:disable Rails/DynamicFindBy
+    end
+
+    def context_crumbs
+      case action_name
+      when 'index'
+        @context_crumbs = [{
+          name: I18n.t('groups.samples.index.title'),
+          path: group_samples_path
+        }]
+      end
+    end
   end
 end
