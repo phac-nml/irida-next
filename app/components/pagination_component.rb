f0c32a5617fa06811f19103a2044576b8d700391
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-04-20 12:03:23 -0500

    chore: Created pagination component

---
+++ app/components/pagination_component.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class PaginationComponent < ViewComponent::Base
+  attr_reader :prev_url, :next_url, :info
+
+  def initialize(info:, prev_url: nil, next_url: nil)
+    @info = info
+    @prev_url = prev_url
+    @next_url = next_url
+  end
+end
