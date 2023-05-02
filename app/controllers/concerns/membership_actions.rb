33609f7eaad5c89e5a2b1011afa9dc8b4ec04168
Deep Sidhu <deepsidhu85@gmail.com>
2023-05-02 12:26:26 -0500

    Fixed bug which prevented project owner under their own user_namespace from adding a project member due to a nil error

--- app/controllers/concerns/membership_actions.rb
+++ app/controllers/concerns/membership_actions.rb
@@ -50,8 +50,15 @@ module MembershipActions
   private
 
   def access_levels
-    member = Member.where(user: current_user, namespace: @namespace.self_and_ancestors).order(:access_level).last
-    @access_levels = Member.access_levels(member)
+    if @namespace.parent&.user_namespace? && @namespace.parent.owner == current_user
+      @access_levels = Member::AccessLevel.access_level_options_owner
+    else
+      member = Member.where(user: current_user,
+                            namespace: @namespace.self_and_ancestors)
+                     .or(Member.where(user: current_user,
+                                      namespace: @namespace.parent&.self_and_ancestors)).order(:access_level).last
+      @access_levels = Member.access_levels(member)
+    end
   end
 
   def available_users
