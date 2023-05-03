c3a50d438a72bb060db3db8addb10ef797656518
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2023-05-03 17:40:12 -0500

    enable/disable providers via figaro yml file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,15 +2,20 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   # disable dev provider in production somehow???
-  provider :developer #unless Rails.env.production?
+  # is there a better production variable? some tutorials say Rails.env.production
+  provider :developer if ENV.fetch('OMNIAUTH_MODES').include? 'developer'
 
-  provider :saml,
-           idp_cert_fingerprint: ENV.fetch('IDP_CERT_FINGERPRINT', nil),
-           idp_sso_service_url: ENV.fetch('IDP_SSO_SERVICE_URL', nil)
+  if ENV.fetch('OMNIAUTH_MODES').include? 'saml'
+    provider :saml,
+             idp_cert_fingerprint: ENV.fetch('IDP_CERT_FINGERPRINT', nil),
+             idp_sso_service_url: ENV.fetch('IDP_SSO_SERVICE_URL', nil)
+  end
 
-  provider :azure_activedirectory_v2,
-           client_id: ENV.fetch('AZURE_CLIENT_ID', nil),
-           client_secret: ENV.fetch('AZURE_CLIENT_SECRET', nil)
+  if ENV.fetch('OMNIAUTH_MODES').include? 'azure_activedirectory_v2'
+    provider :azure_activedirectory_v2,
+             client_id: ENV.fetch('AZURE_CLIENT_ID', nil),
+             client_secret: ENV.fetch('AZURE_CLIENT_SECRET', nil)
+  end
 
   #  # I can't remember what this was for, from some guide??
   # configure do |config|
