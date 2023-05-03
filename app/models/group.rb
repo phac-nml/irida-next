dc35e9f4af3ad127634314b94f066943b86c7d2b
Eric Enns <492127+ericenns@users.noreply.github.com>
2023-05-03 13:20:52 -0500

    chore: updated associations for namespaces to destroy dependant associations

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,11 @@
 class Group < Namespace
   has_many :group_members, foreign_key: :namespace_id, inverse_of: :namespace,
                            class_name: 'Member', dependent: :destroy
-
+  has_many :project_namespaces,
+           lambda {
+             where(type: Namespaces::ProjectNamespace.sti_name)
+           },
+           class_name: 'Namespace', foreign_key: :parent_id, inverse_of: :parent, dependent: :destroy
   has_many :users, through: :group_members
 
   def self.sti_name
--- app/models/namespaces/user_namespace.rb
+++ app/models/namespaces/user_namespace.rb
@@ -3,6 +3,11 @@
 module Namespaces
   # Namespace for Users
   class UserNamespace < Namespace
+    has_many :project_namespaces,
+             lambda {
+               where(type: Namespaces::ProjectNamespace.sti_name)
+             }, class_name: 'Namespace', foreign_key: :parent_id, inverse_of: :parent, dependent: :destroy
+
     def self.sti_name
       'User'
     end
