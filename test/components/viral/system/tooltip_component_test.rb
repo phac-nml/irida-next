c5272c686032062c71b51357177e8292815ea493
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-05-03 11:23:44 -0500

    chore: Simplified selector for hover

--- test/components/viral/system/tooltip_component_test.rb
+++ test/components/viral/system/tooltip_component_test.rb
@@ -7,7 +7,7 @@ module System
     test 'tooltip appears on hover' do
       visit('/rails/view_components/tooltip_component/default')
       assert_selector '[data-viral--tooltip-component-target="target"]', visible: false
-      find('[data-viral--tooltip-component-target="trigger"]').hover
+      find('a', text: 'Hover me').hover
       assert_text I18n.t('auth.scopes.api')
       assert_selector '[data-viral--tooltip-component-target="target"]', visible: true
     end
