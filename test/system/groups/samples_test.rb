6faf8d2cb905378ccea51ff3b5207f7c67e36e0c
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-05-04 15:46:44 -0500

    testing pagination

--- test/system/groups/samples_test.rb
+++ test/system/groups/samples_test.rb
@@ -7,15 +7,24 @@ module Groups
     def setup
       login_as users(:john_doe)
       @group = groups(:group_one)
-      @namespaces = Namespaces::ProjectNamespace.where(parent_id: @group.self_and_descendant_ids)
-      @samples_count = samples.select { |sample| @namespaces.include?(sample.project.namespace) }.count
     end
 
     test 'visiting the index' do
       visit group_samples_url(@group)
 
       assert_selector 'h1', text: I18n.t(:'groups.samples.index.title')
-      assert_selector 'tbody > tr', count: @samples_count
+      assert_selector 'tbody > tr', count: 20
+      assert_text samples(:sample3).name
+      assert_selector 'a', text: I18n.t(:'components.pagination.next')
+      assert_no_selector 'a', text: I18n.t(:'components.pagination.previous')
+
+      click_on I18n.t(:'components.pagination.next')
+      assert_selector 'tbody > tr', count: 2
+      click_on I18n.t(:'components.pagination.previous')
+      assert_selector 'tbody > tr', count: 20
+
+      # click_link samples(:sample3).name
+      # assert_selector 'h1', text: samples(:sample3).name
     end
   end
 end
