c0617871993c029caa52387d92f930dfab990211
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-05-08 15:12:07 -0500

    test: Finished tests for alert_component

--- test/components/viral/alert_component_test.rb
+++ test/components/viral/alert_component_test.rb
@@ -2,10 +2,18 @@
 
 require 'test_helper'
 
-class Viral::AlertComponentTest < ViewComponent::TestCase
-  test 'notice alert' do
-    render_inline(Viral::AlertComponent.new(message: 'This is a notice alert', type: 'notice'))
-    assert_text 'This is a notice alert'
-    assert_selector 'div[data-controller="viral--alert-component"]', count: 1
+module Viral
+  class AlertComponentTest < ViewComponent::TestCase
+    test 'notice alert' do
+      render_inline(Viral::AlertComponent.new(message: 'This is a notice alert', type: 'notice'))
+      assert_text 'This is a notice alert'
+      assert_selector 'div.text-blue-800.border-blue-300.bg-blue-50', count: 1
+    end
+
+    test 'alert alert' do
+      render_inline(Viral::AlertComponent.new(message: 'This is an alert alert', type: 'alert'))
+      assert_text 'This is an alert alert'
+      assert_selector 'div.text-red-800.border-red-300.bg-red-50', count: 1
+    end
   end
 end
