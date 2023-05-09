ffcf0d8b53ab32741e88eb6b0821a08001127d27
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2023-05-09 16:12:33 -0500

    use alias to simplify code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,53 +3,25 @@
 module Users
   # Handles callbacks from Omniauth providers
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    # TODO: We should do something different than skipping token authenticity.
-    # OpenID (SAML) need a ca cert
     # See https://github.com/omniauth/omniauth/wiki/FAQ#rails-session-is-clobbered-after-callback-on-openid-providers
-    # Skip should only be for developer
-    # skip_before_action :verify_authenticity_token, only: :developer
-    # See https://github.com/omniauth/omniauth/wiki/FAQ#rails-session-is-clobbered-after-callback-on-developer-strategy
-    skip_before_action :verify_authenticity_token#, only: :developer
+    protect_from_forgery with: :exception, except: %i[saml developer]
 
-    def developer
-      Rails.logger.debug('Omniauth callback developer')
+    def all
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: 'DEVELOPER') if is_navigational_format?
+        set_flash_message(:notice, :success, kind: action_name)
       else
-        # Removing extra as it can overflow some session stores
-        session['devise.omniauth_data'] = request.env['omniauth.auth'].except(:extra)
-        redirect_to new_user_registration_url
+        # Removing extra and credentials as it can overflow some session stores
+        session['devise.omniauth_data'] = request.env['omniauth.auth'].except(:extra, :credentials)
+        # TODO: somehow alert failed sign in?
+        redirect_to root_path
       end
     end
 
-    def saml
-      Rails.logger.debug('Omniauth callback saml')
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: 'SAML') if is_navigational_format?
-      else
-        # Removing extra as it can overflow some session stores
-        session['devise.omniauth_data'] = request.env['omniauth.auth'].except(:extra)
-        redirect_to new_user_registration_url
-      end
-    end
-
-    def azure_activedirectory_v2
-      Rails.logger.debug('omniauth callback azure_activedirectory_v2')
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-
-      if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: 'Azure') if is_navigational_format?
-      else
-        session['devise.omniauth_data'] = request.env['omniauth.auth'].except(:extra)
-        redirect_to new_user_registration_url
-      end
-    end
+    alias developer all
+    alias saml all
+    alias azure_activedirectory_v2 all
   end
 end
