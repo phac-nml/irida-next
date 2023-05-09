13a78bb76f7fe16265ec67d3ecd8e40bb72e5fcd
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-05-09 14:43:25 -0500

    chore: Working on modal component for deleting project

--- app/components/viral/pop_up_component.rb
+++ app/components/viral/pop_up_component.rb
@@ -2,7 +2,11 @@
 
 module Viral
   class PopUpComponent < Component
-    attr_reader :button_text
+    attr_reader :title, :button_text
+
+    renders_one :header
+    renders_one :body
+    renders_one :footer
 
     def initialize(button_text:, **system_arguments)
       @button_text = button_text
