74b8c077c379100de576b6f6e29e00005d7970ce
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-05-10 13:39:26 -0500

    using slots

--- app/components/modal_component.rb
+++ app/components/modal_component.rb
@@ -2,11 +2,12 @@
 
 # A component for displaying a modal.
 class ModalComponent < Component
-  attr_reader :button_text, :title, :body
+  attr_reader :button_text, :title
 
-  def initialize(button_text:, title:, body:)
+  renders_one :body
+
+  def initialize(button_text:, title:)
     @button_text = button_text
     @title = title
-    @body = body
   end
 end
--- test/components/previews/modal_component_preview.rb
+++ test/components/previews/modal_component_preview.rb
@@ -2,6 +2,10 @@
 
 class ModalComponentPreview < ViewComponent::Preview
   def default
-    render ModalComponent.new(button_text: 'Open Model', title: 'This is a modal title', body: 'This is a modal body')
+    render ModalComponent.new(button_text: 'Open Model', title: 'This is a modal title') do |modal|
+      modal.with_body do
+        'This is a modal body'
+      end
+    end
   end
 end
