a07c99aa37778d2a185ce81c44eaf9485451aa63
Eric Enns <492127+ericenns@users.noreply.github.com>
2023-05-12 15:12:29 -0500

    Feature: Sessionless authentication (#94)
    * feat: add in sessionless authentication for api controllers
    
    * chore: fix seed of tokens and update token auth to update the last_used_at field
    
    * test: add in separate tests for SessionlessAuthentication controller concern
    
    * chore: removed unnecessary setup from SessionAuthenticationConcern test
    
    * chore: added in test for malformed HTTP Basic auth header
    
    * chore: fix issue when HTTP Basic auth string is malformed
    
    * chore: fix rubocop error
    
    * chore: fix typo
    
    * chore: fix issue with hauthorization header having newline character

---
+++ app/controllers/concerns/sessionless_authentication.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+# Authentication methods for sessionless access (e.g. Graphql)
+module SessionlessAuthentication
+  def authenticate_sessionless_user!
+    access_token = token_from_basic_authorization(request)
+    username = username_from_basic_authorization(request)
+
+    user = User.find_by(email: username)
+    return unless user
+
+    pat = user.personal_access_tokens.find_by_token(access_token) # rubocop:disable Rails/DynamicFindBy
+    return unless pat&.active?
+
+    pat.touch(:last_used_at) # rubocop:disable Rails/SkipsModelValidations
+
+    sessionless_sign_in(user)
+  end
+
+  def sessionless_sign_in(user)
+    sign_in(user, store: false)
+  end
+
+  private
+
+  def token_from_basic_authorization(request)
+    pattern = /^Basic /i
+    header = request.authorization&.strip
+    token_from_basic_header(header, pattern) if header&.match(pattern)
+  end
+
+  def username_from_basic_authorization(request)
+    pattern = /^Basic /i
+    header = request.authorization&.strip
+    username_from_basic_header(header, pattern) if header&.match(pattern)
+  end
+
+  def token_from_basic_header(header, pattern)
+    encoded_header = header.gsub(pattern, '')
+    decode_basic_credentials_token(encoded_header) if base64?(encoded_header)
+  end
+
+  def decode_basic_credentials_token(encoded_header)
+    Base64.decode64(encoded_header).split(/:/, 2).last
+  end
+
+  def username_from_basic_header(header, pattern)
+    encoded_header = header.gsub(pattern, '')
+    decode_basic_credentials_username(encoded_header) if base64?(encoded_header)
+  end
+
+  def decode_basic_credentials_username(encoded_header)
+    Base64.decode64(encoded_header).split(/:/, 2).first
+  end
+
+  def base64?(value)
+    Base64.strict_encode64(Base64.decode64(value)) == value
+  end
+end
---
+++ test/controllers/concerns/sessionless_authentication_concern_test.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SessionlessAuthController < ApplicationController
+  include SessionlessAuthentication
+
+  skip_before_action :authenticate_user!
+
+  protect_from_forgery with: :null_session, only: :execute
+
+  before_action { authenticate_sessionless_user! }
+
+  def fake_action
+    render json: { current_user: { email: current_user&.email } }
+  end
+end
+
+class SessionlessAuthenticationConcernTest < ActionDispatch::IntegrationTest
+  setup do
+    Rails.application.routes.draw do
+      post 'fake_action' => 'sessionless_auth#fake_action'
+    end
+  end
+
+  teardown do
+    Rails.application.reload_routes!
+  end
+
+  test 'request without HTTP Basic Authorization header does not set current_user' do
+    post fake_action_path
+    assert_response :success
+    response_hash = response.parsed_body
+
+    assert response_hash.key?('current_user')
+    assert response_hash['current_user'].key?('email')
+    assert_nil response_hash['current_user']['email']
+  end
+
+  test 'request with valid HTTP Basic Authorization header sets current_user' do
+    basic_auth = Base64.encode64("#{users(:john_doe).email}:JQ2w5maQc4zgvC8GGMEp")
+    authorization_header = "Basic #{basic_auth}"
+
+    post fake_action_path, headers: { Authorization: authorization_header }
+    assert_response :success
+    response_hash = response.parsed_body
+
+    assert response_hash.key?('current_user')
+    assert response_hash['current_user'].key?('email')
+    assert_equal users(:john_doe).email, response_hash['current_user']['email']
+  end
+
+  test 'request with invalid HTTP Basic Authorization header does not set current_user' do
+    basic_auth = Base64.encode64("#{users(:jane_doe).email}:JQ2w5maQc4zgvC8GGMEp")
+    authorization_header = "Basic #{basic_auth}"
+
+    post fake_action_path, headers: { Authorization: authorization_header }
+    assert_response :success
+    response_hash = response.parsed_body
+
+    assert response_hash.key?('current_user')
+    assert response_hash['current_user'].key?('email')
+    assert_not_equal users(:john_doe).email, response_hash['current_user']['email']
+  end
+
+  test 'request with malformed HTTP Basic Authorization header does not set current_user' do
+    basic_auth = Base64.encode64('MALFORMED')
+    authorization_header = "Basic #{basic_auth}"
+
+    post fake_action_path, headers: { Authorization: authorization_header }
+    assert_response :success
+    response_hash = response.parsed_body
+
+    assert response_hash.key?('current_user')
+    assert response_hash['current_user'].key?('email')
+    assert_nil response_hash['current_user']['email']
+  end
+end
