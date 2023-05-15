976c51259b2660e4a9c42e6048e2c0029a262a09
Deep Sidhu <deepsidhu85@gmail.com>
2023-05-15 15:55:12 -0500

    Cleaned up namespace policies, updated test

--- app/policies/namespaces/project_namespace_policy.rb
+++ app/policies/namespaces/project_namespace_policy.rb
@@ -5,24 +5,11 @@ module Namespaces
   class ProjectNamespacePolicy < NamespacePolicy
     alias_rule :new?, :create?, :update?, to: :allowed_to_modify_project_namespace?
     alias_rule :index?, to: :allowed_to_view_project_namespace?
-    alias_rule :destroy?, to: :allowed_to_destroy?
 
     def allowed_to_modify_project_namespace?
       return true if record.parent.user_namespace? && record.parent.owner == user
 
       can_modify?(record)
-    end
-
-    def allowed_to_view_project_namespace?
-      return true if record.parent.user_namespace? && record.parent.owner == user
-
-      can_view?(record)
-    end
-
-    def allowed_to_destroy?
-      return true if record.parent.user_namespace? && record.parent.owner == user
-
-      can_destroy?(record)
     end
   end
 end
--- test/policies/namespaces/project_namespace_policy_test.rb
+++ test/policies/namespaces/project_namespace_policy_test.rb
@@ -14,22 +14,10 @@ module Namespaces
       assert @policy.allowed_to_modify_project_namespace?
     end
 
-    test '#allowed_to_view_project_namespace?' do
-      assert @policy.allowed_to_view_project_namespace?
-    end
-
-    test '#allowed_to_destroy?' do
-      assert @policy.allowed_to_destroy?
-    end
-
     test 'aliases' do
       assert_equal :allowed_to_modify_project_namespace?, @policy.resolve_rule(:new?)
       assert_equal :allowed_to_modify_project_namespace?, @policy.resolve_rule(:create?)
       assert_equal :allowed_to_modify_project_namespace?, @policy.resolve_rule(:update?)
-
-      assert_equal :allowed_to_view_project_namespace?, @policy.resolve_rule(:index?)
-
-      assert_equal :allowed_to_destroy?, @policy.resolve_rule(:destroy?)
     end
   end
 end
