bcb2cae4f799b11011ea5426fbc9684fa04c5a97
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-05-17 12:23:48 -0500

    replacing update_all

--- app/services/samples/transfer_service.rb
+++ app/services/samples/transfer_service.rb
@@ -6,12 +6,10 @@ module Samples
     def execute(sample_transfer)
       @sample_transfer = sample_transfer
 
-      # @sample_transfer.sample_ids.each do |sample_id|
-      #   sample = Sample.find_by(id: sample_id)
-      #   sample.update(project_id: @sample_transfer.project_id)
-      # end
-
-      Sample.where(id: @sample_transfer.sample_ids).update_all(project_id: @sample_transfer.project_id)
+      @sample_transfer.sample_ids.each do |sample_id|
+        sample = Sample.find_by(id: sample_id)
+        sample.update(project_id: @sample_transfer.project_id)
+      end
 
       true
     end
