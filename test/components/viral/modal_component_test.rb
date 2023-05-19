853adf4406a8eb206a79b9f843a0492431a8454b
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-05-19 13:48:05 -0500

    adding modal testing

--- test/components/viral/modal_component_test.rb
+++ test/components/viral/modal_component_test.rb
@@ -4,11 +4,23 @@ require 'test_helper'
 
 module Viral
   class ModalComponentTest < ViewComponent::TestCase
-    def test_component_renders_something_useful
-      # assert_equal(
-      #   %(<span>Hello, components!</span>),
-      #   render_inline(ModalComponent.new(message: "Hello, components!")).css("span").to_html
-      # )
+    test 'with title' do
+      title = 'Modal Title'
+      render_inline(Viral::ModalComponent.new(title:))
+      assert_text title
+    end
+
+    test 'with title and body' do
+      title = 'Modal Title'
+      body = 'Modal Body'
+      render_inline(Viral::ModalComponent.new(title:)) do |modal|
+        modal.with_body do
+          body
+        end
+      end
+      assert_selector '#Viral-Modal-Body' do
+        assert_text body
+      end
     end
   end
 end
