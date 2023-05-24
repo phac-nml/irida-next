8c9b83bf72a3527e4bbb5c2b451f3501f0485d39
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-05-24 14:24:17 -0500

    adding size options

--- app/components/viral/modal_component.rb
+++ app/components/viral/modal_component.rb
@@ -3,12 +3,21 @@
 module Viral
   # A component for displaying a modal.
   class ModalComponent < Component
-    attr_reader :title
+    attr_reader :title, :size
 
     renders_one :body
 
-    def initialize(title:)
+    SIZE_DEFAULT = :default
+    SIZE_MAPPINGS = {
+      small: 'max-w-md',
+      default: 'max-w-lg',
+      large: 'max-w-4xl',
+      extra_large: 'max-w-7xl'
+    }.freeze
+
+    def initialize(title:, size: SIZE_DEFAULT)
       @title = title
+      @size = SIZE_MAPPINGS[size]
     end
   end
 end
