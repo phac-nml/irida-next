4a7f53ea377d7686d5b151a019915fc02b356b09
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-05-26 13:48:34 -0500

    refactor: Update viral flash previews

---
+++ test/components/previews/viral_flash_component_preview.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class ViralFlashComponentPreview < ViewComponent::Preview
+  def success_flash; end
+
+  def error_flash; end
+
+  def warning_flash; end
+
+  def info_flash; end
+end
