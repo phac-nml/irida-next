f06464db5ad61ae2b6c6771ef0f2fdab6f81050e
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-05-26 13:28:45 -0500

    refactor: Updated clipboard previews

---
+++ test/components/previews/viral_card_component_preview.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ViralCardComponentPreview < ViewComponent::Preview
+  def default; end
+
+  def with_header_actions; end
+
+  def simple_header; end
+
+  def with_multiple_sections; end
+
+  def titled_sections; end
+
+  def section_with_action; end
+end
