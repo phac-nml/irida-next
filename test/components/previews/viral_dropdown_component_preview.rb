4f094fba2175ade56169d85eed12ec9aab9d1117
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-05-26 13:38:54 -0500

    refactor: Updated dropdown component previews

---
+++ test/components/previews/viral_dropdown_component_preview.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class ViralDropdownComponentPreview < ViewComponent::Preview
+  def default; end
+
+  def with_caret; end
+
+  def with_icon; end
+
+  def with_item_icon; end
+end
