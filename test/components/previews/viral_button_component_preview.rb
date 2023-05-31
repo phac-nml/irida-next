e83b2e2347e555e22ae1a5d367c42c593213b414
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-05-31 10:22:47 -0500

    chore: Added more previews

--- test/components/previews/viral_button_component_preview.rb
+++ test/components/previews/viral_button_component_preview.rb
@@ -5,33 +5,11 @@ class ViralButtonComponentPreview < ViewComponent::Preview
 
   def full_width; end
 
-  # def primary
-  #   render Viral::ButtonComponent.new(type: :primary) do
-  #     'Primary Button'
-  #   end
-  # end
-  #
-  # def destructive
-  #   render Viral::ButtonComponent.new(type: :destructive) do
-  #     'Destructive Button'
-  #   end
-  # end
-  #
-  # def small
-  #   render Viral::ButtonComponent.new(size: :small) do
-  #     'Small Button'
-  #   end
-  # end
-  #
-  # def large
-  #   render Viral::ButtonComponent.new(size: :large) do
-  #     'Large Button'
-  #   end
-  # end
-  #
-  # def full_width
-  #   render Viral::ButtonComponent.new(full_width: true) do
-  #     'Full Width Button'
-  #   end
-  # end
+  def primary; end
+
+  def destructive; end
+
+  def small; end
+
+  def large; end
 end
