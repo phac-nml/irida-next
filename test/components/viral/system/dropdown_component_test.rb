67172741a3f9184287572c6cd0197f27e6d57729
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-05-31 11:10:16 -0500

    chore: Updated tests with new dropdown item names

--- test/components/viral/system/dropdown_component_test.rb
+++ test/components/viral/system/dropdown_component_test.rb
@@ -7,24 +7,28 @@ module System
     test 'dropdown component with label' do
       visit('/rails/view_components/viral_dropdown_component/with_caret')
       within('.Viral-Preview > [data-controller-connected="true"]') do
-        assert_no_text 'Pizza'
-        assert_no_text 'Bacon'
+        assert_no_text 'Aspergillus awamori'
+        assert_no_text 'Bacillus cereus'
+        assert_no_text 'Pseudomonas aeruginosa'
         assert_selector '.Viral-Dropdown--button'
-        find('.Viral-Dropdown--button').click
-        assert_text 'Pizza'
-        assert_text 'Bacon'
+        click_on 'Organism'
+        assert_text 'Aspergillus awamori'
+        assert_text 'Bacillus cereus'
+        assert_text 'Pseudomonas aeruginosa'
       end
     end
 
     test 'dropdown component with icon' do
       visit('/rails/view_components/viral_dropdown_component/with_icon')
       within('.Viral-Preview > [data-controller-connected="true"]') do
-        assert_no_text 'Pizza'
-        assert_no_text 'Bacon'
+        assert_no_text 'Aspergillus awamori'
+        assert_no_text 'Bacillus cereus'
+        assert_no_text 'Pseudomonas aeruginosa'
         assert_selector '.Viral-Dropdown--icon'
-        find('.Viral-Dropdown--icon').click
-        assert_text 'Pizza'
-        assert_text 'Bacon'
+        click_on 'Organism'
+        assert_text 'Aspergillus awamori'
+        assert_text 'Bacillus cereus'
+        assert_text 'Pseudomonas aeruginosa'
       end
     end
   end
