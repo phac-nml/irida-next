afc8941a45ceceb08e685c7975d2387fc0273e20
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-06-01 07:43:12 -0500

    chore: Fixed issue with primary button hover colour

--- app/components/viral/button_component.rb
+++ app/components/viral/button_component.rb
@@ -7,7 +7,7 @@ module Viral
     TYPE_DEFAULT = :default
     TYPE_MAPPINGS = {
       TYPE_DEFAULT => 'text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700',
-      :primary => 'text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg dark:bg-blue-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800',
+      :primary => 'text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg dark:bg-primary-600 dark:hover:bg-primary-800 focus:outline-none dark:focus:ring-primary-800',
       :destructive => 'focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'
     }.freeze
 
