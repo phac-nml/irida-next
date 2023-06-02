af1d512c02bf91108efa37d487e26e699d876105
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-06-02 11:15:54 -0500

    chore: Cleaned up close button and made it optional

--- app/components/viral/modal_component.rb
+++ app/components/viral/modal_component.rb
@@ -2,8 +2,8 @@
 
 module Viral
   # A component for displaying a modal.
-  class ModalComponent < Component
-    attr_reader :title, :size
+  class ModalComponent < Viral::Component
+    attr_reader :closable, :close_button_arguments, :title, :size
 
     renders_one :body
 
@@ -15,9 +15,18 @@ module Viral
       extra_large: 'max-w-7xl'
     }.freeze
 
-    def initialize(title:, size: SIZE_DEFAULT)
+    def initialize(title:, closable: true, size: SIZE_DEFAULT)
       @title = title
+      @closable = closable
       @size = SIZE_MAPPINGS[size]
+
+      @close_button_arguments = {
+        tag: 'button',
+        type: 'button',
+        data: { action: 'click->viral--modal-component#close' },
+        classes: 'text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white',
+        aria: { label: t('components.modal.close') }
+      }
     end
   end
 end
