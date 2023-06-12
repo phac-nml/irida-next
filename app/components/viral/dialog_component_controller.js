b30f1126ad4736a15ca772d1d9f739b6a46641cf
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-06-12 05:12:57 -0500

    chore: Removed logging statement

--- app/components/viral/dialog_component_controller.js
+++ app/components/viral/dialog_component_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
   static values = { open: Boolean };
   connect() {
     this.dialog = document.getElementById("dialog");
-    console.log(this.openValue);
     if (this.openValue) this.open();
     this.element.setAttribute("data-controller-connected", "true");
   }
