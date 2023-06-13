b568d494c1422b6a64a6945ed57f7ee15bbc6677
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-06-13 14:30:31 -0500

    Upgrade: Updated modal to have action buttons and JavaScript open (#116)
    * chore: Cleaned up close button and made it optional
    
    * chore: Cleaned up modal backdrop css
    
    * chore: Cleaned up sizes
    
    * chore: Workout on adding a modal footer with buttons
    
    * chore: Added primary action button
    
    * chore: Working on secondary buttons
    
    * chore: Updated to use button component
    
    * chore: Working on modal trigger
    
    * chore: Ability to set if modal should start open or closed
    
    * fix: Fixed previews to be open by default
    
    * chore: Working on getting modal wrapper working
    
    * chore: Open modal with a button
    
    * chore: Updated previews
    
    * chore: Renamed to match rest of naming convention
    
    * chore: Modal style cleanup
    
    * chore: Remove unused test file
    
    * chore: Updating accessibility
    
    * chore: Native dialog with different sizes
    
    * chore: Cleaned up testing
    
    * chore: Updated testing
    
    * chore: Fixed dark backdrop
    
    * chore: Added multiple sections
    
    * chore: Renamed to dialog
    
    Cleaned up old files
    
    * chore: Renamed testing file
    
    * chore: Removed logging statement
    
    * test: Updated test to include `span[data-controller-connected="true"]`
    
    This ensures that the javascript has triggered before the test runs
    
    * chore: Fixed test that might be flaky
    
    * chore: Renamed file and added test for closing dialog
    
    * chore: Demonstration of a confirmation modal

---
+++ test/components/previews/viral_dialog_component_preview.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ViralDialogComponentPreview < ViewComponent::Preview
+  def confirmation; end
+
+  def default; end
+
+  def small; end
+
+  def large; end
+
+  def extra_large; end
+
+  def with_action_buttons; end
+
+  def with_trigger; end
+
+  def with_multiple_sections; end
+end
