0e13134db87db7b41a73785bf8ef59c08d8a13f1
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-06-16 09:30:55 -0500

    adding a test for with_a_storage_key

--- test/components/viral/system/selection_test.rb
+++ test/components/viral/system/selection_test.rb
@@ -6,10 +6,27 @@ module System
   class SelectionTest < ApplicationSystemTestCase
     test 'default' do
       visit('rails/view_components/selection/default')
-      check 'item0'
-      uncheck 'item1'
-      check 'item2'
+      within('.Viral-Preview > [data-controller-connected="true"]') do
+        check 'item0'
+        uncheck 'item1'
+        check 'item2'
+      end
       visit('rails/view_components/selection/default')
+      within('.Viral-Preview > [data-controller-connected="true"]') do
+        assert find_field('item0').checked?
+        assert_not find_field('item1').checked?
+        assert find_field('item2').checked?
+      end
+    end
+
+    test 'with a storage key' do
+      visit('rails/view_components/selection/with_a_storage_key')
+      within('.Viral-Preview > [data-controller-connected="true"]') do
+        check 'item0'
+        uncheck 'item1'
+        check 'item2'
+      end
+      visit('rails/view_components/selection/with_a_storage_key')
       within('.Viral-Preview > [data-controller-connected="true"]') do
         assert find_field('item0').checked?
         assert_not find_field('item1').checked?
@@ -19,9 +36,11 @@ module System
 
     test 'with a table' do
       visit('rails/view_components/selection/within_a_table')
-      uncheck 'row0'
-      check 'row1'
-      uncheck 'row2'
+      within('.Viral-Preview > [data-controller-connected="true"]') do
+        uncheck 'row0'
+        check 'row1'
+        uncheck 'row2'
+      end
       visit('rails/view_components/selection/within_a_table')
       within('.Viral-Preview > [data-controller-connected="true"]') do
         assert_not find_field('row0').checked?
