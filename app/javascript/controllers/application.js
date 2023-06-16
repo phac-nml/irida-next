745eabfca6f434523a4e57e7f6b31d520f47b738
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-06-16 08:50:25 -0500

    chore: Removed confirm code

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,23 +2,4 @@ import { Application } from "@hotwired/stimulus";
 
 const application = Application.start();
 
-// Configure Stimulus development experience
-// application.debug = false
-window.Stimulus = application;
-
-Turbo.setConfirmMethod((message, element) => {
-  let dialog = document.getElementById("turbo-confirm");
-  dialog.showModal();
-
-  return new Promise((resolve, reject) => {
-    dialog.addEventListener(
-      "close",
-      () => {
-        resolve(dialog.returnValue == "confirm");
-      },
-      { once: true }
-    );
-  });
-});
-
 export { application };
