2c4a7fa28af0aa9e3ed725ead2aafc1a59b03247
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-06-20 11:23:00 -0500

    chore: Updated more tests to use new confirm dialog

--- test/system/projects/members_test.rb
+++ test/system/projects/members_test.rb
@@ -64,9 +64,10 @@ module Projects
       visit namespace_project_members_url(@namespace, @project)
 
       first('.member-settings-ellipsis').click
+      click_link I18n.t(:'projects.members.index.remove')
 
-      accept_confirm do
-        click_link I18n.t(:'projects.members.index.remove')
+      within('#turbo-confirm[open]') do
+        click_button 'Confirm'
       end
 
       assert_no_text I18n.t(:'projects.members.destroy.success')
