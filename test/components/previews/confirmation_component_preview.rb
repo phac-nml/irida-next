1c1b727e61545f6622ac9b13cdd9adfe8d829a23
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-06-21 05:41:24 -0500

    chore: Updated LookBook description comments

--- test/components/previews/confirmation_component_preview.rb
+++ test/components/previews/confirmation_component_preview.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
 class ConfirmationComponentPreview < ViewComponent::Preview
+  # Default Confirmation Dialog
+  # ---------------------------
+  # Text provided through the `data-turbo-confirm` attribute will be used as the confirmation message
   def default; end
 
+  # Custom Confirmation Dialog Content
+  # ----------------------------------
+  # Added the `data-turbo-content` attribute to the dom **id** for custom content
+  # This can include HTML
   def custom_content; end
 
-  # Strongly consider if a confirmation or a dialog is the correct choice for your form
+  # Custom Confirmation Dialog Form
+  # -------------------------------
+  # The entire contents of the modal can be overridden with the `data-turbo-form` attribute
+  # This accepts the **id** for the form to be rendered
+  # Buttons must be provided in the form
+  #   <button value='cancel'>Cancel</button>
+  #   <button value='confirm'>Confirm</button>
   def with_custom_form; end
 end
