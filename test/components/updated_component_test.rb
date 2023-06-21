d42d23046ffbb03432c93ec1c311a0bb70532371
Deep Sidhu <deepsidhu85@gmail.com>
2023-06-21 11:23:38 -0500

    Updated tests

--- test/components/updated_component_test.rb
+++ test/components/updated_component_test.rb
@@ -5,16 +5,32 @@ require 'test_helper'
 class UpdatedComponentTest < ViewComponent::TestCase
   include ActionView::Helpers::DateHelper
 
-  test 'renders updated at time ago (less than a minute)' do
-    render_inline(UpdatedComponent.new(description: 'Updated',
-                                       updated_at: distance_of_time_in_words(Time.zone.now, Time.zone.now + 1)))
+  test 'renders custom description updated at time ago (less than a minute)' do
+    render_inline(UpdatedComponent.new(description: 'Record updated',
+                                       updated_at: Time.zone.now + 1))
+
+    assert_text 'Record updated less than a minute ago'
+  end
+
+  test 'renders custom description updated at time ago (greater than a minute)' do
+    render_inline(UpdatedComponent.new(description: 'Record updated',
+                                       updated_at: Time.zone.now + 1000))
+
+    assert_text 'Record updated 17 minutes ago'
+  end
+
+  test 'renders default updated at time ago (less than a minute)' do
+    render_inline(UpdatedComponent.new(
+                    updated_at: Time.zone.now + 1
+                  ))
 
     assert_text 'Updated less than a minute ago'
   end
 
-  test 'renders updated at time ago (greater than a minute)' do
-    render_inline(UpdatedComponent.new(description: 'Updated',
-                                       updated_at: distance_of_time_in_words(Time.zone.now, Time.zone.now + 1000)))
+  test 'renders default updated at time ago (greater than a minute)' do
+    render_inline(UpdatedComponent.new(
+                    updated_at: Time.zone.now + 1000
+                  ))
 
     assert_text 'Updated 17 minutes ago'
   end
