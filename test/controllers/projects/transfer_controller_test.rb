a60c1e43294e64502f6de624090cf6c18b8ded2a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-06-26 15:46:43 -0500

    test: UI and controller tests

---
+++ test/controllers/projects/transfer_controller_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Projects
+  class TransferControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      sign_in users(:john_doe)
+      @project = projects(:project1)
+      @namespace = namespaces_user_namespaces(:john_doe_namespace)
+      @old_namespace = groups(:group_one)
+    end
+
+    test 'should create a new transfer' do
+      post namespace_project_transfer_index_path(@old_namespace, @project),
+           params: { new_namespace_id: @namespace.id }, as: :turbo_stream
+
+      assert_response :redirect
+    end
+  end
+end
