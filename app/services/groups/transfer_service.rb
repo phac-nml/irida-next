ab71daf65db8dfc69a22e177d4199f5da095c1d3
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-06-28 13:32:00 -0500

    starting to add transfer service tests

--- app/services/groups/transfer_service.rb
+++ app/services/groups/transfer_service.rb
@@ -6,26 +6,36 @@ module Groups
     TransferError = Class.new(StandardError)
 
     def execute(new_namespace)
-      @new_namespace = new_namespace
-      raise TransferError, I18n.t('services.groups.transfer.namespace_empty') if @new_namespace.blank?
-
-      if @new_namespace.id == @group.parent_id
-        raise TransferError,
-              I18n.t('services.groups.transfer.group_in_namespace')
-      end
+      validate(new_namespace)
 
       # Authorize if user can transfer group
       authorize! @group, to: :transfer?
 
       # Authorize if user can transfer group into namespace
-      authorize! @new_namespace, to: :transfer_into_namespace?
+      authorize! new_namespace, to: :transfer_into_namespace?
 
-      group.update(parent_id: @new_namespace.id)
+      group.update(parent_id: new_namespace.id)
 
       true
     rescue Groups::TransferService::TransferError => e
       group.errors.add(:new_namespace, e.message)
       false
+    end
+
+    private
+
+    def validate(new_namespace)
+      raise TransferError, I18n.t('services.groups.transfer.namespace_empty') if new_namespace.blank?
+
+      if new_namespace.id == @group.id
+        raise TransferError,
+              I18n.t('services.groups.transfer.same_group_and_namespace')
+      end
+
+      return unless new_namespace.id == @group.parent_id
+
+      raise TransferError,
+            I18n.t('services.groups.transfer.group_in_namespace')
     end
   end
 end
