81a970e27ff0689c0308c5dacd677d422a74d8c1
Deep Sidhu <deepsidhu85@gmail.com>
2023-06-28 09:59:06 -0500

    Updated indexes on user's table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_190220) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_28_145212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
@@ -115,8 +115,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_190220) do
     t.jsonb "log_data"
     t.datetime "deleted_at"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, where: "(deleted_at IS NULL)"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, where: "(deleted_at IS NULL)"
   end
 
   add_foreign_key "members", "namespaces"
