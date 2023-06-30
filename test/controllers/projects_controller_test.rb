eb2846e63d8db9038a1e1dbabf6e905558936819
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-06-30 07:32:06 -0500

    chore: Updated projects transfer testing
    Needed to move the tests into the projects controller test

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -225,4 +225,36 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest # rubocop:disable
 
     assert_response :unauthorized
   end
+
+  test 'should transfer project' do
+    project = projects(:project1)
+    namespace = namespaces_user_namespaces(:john_doe_namespace)
+    old_namespace = groups(:group_one)
+
+    post namespace_project_transfer_path(old_namespace, project),
+         params: { new_namespace_id: namespace.id }, as: :turbo_stream
+
+    assert_response :redirect
+  end
+
+  test 'should not create a new transfer with wrong parameters' do
+    sign_in users(:david_doe)
+    project = projects(:project1)
+    old_namespace = groups(:group_one)
+    post namespace_project_transfer_path(old_namespace, project),
+         params: { new_namespace_id: 'asdfasd' }, as: :turbo_stream
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'should not transfer a project to unowned namespace' do
+    sign_in users(:david_doe)
+    project = projects(:project1)
+    old_namespace = groups(:group_one)
+    post namespace_project_transfer_path(old_namespace, project),
+         params: { new_namespace_id: groups(:david_doe_group_four).id },
+         as: :turbo_stream
+
+    assert_response :unauthorized
+  end
 end
