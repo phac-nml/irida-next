8ada87dfaf0eef89308e5eb4658279761de6762f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-06-30 14:42:04 -0500

    Update: Updated UI for transfering a project namespace (#99)
    * chore: Updated UI for transfering project
    
    * chore: Removed checking slug since no longer possibble
    
    * chore: Added missinig partial
    
    * test: Updated testing for name, description and namespace
    
    * chore: Fixed merge conflicts
    
    * chore: Working on project transfer
    
    * chore: Fixed namespace errors
    
    * chore: Cleaned up random errors
    
    * chore: Working turbo
    
    * chore: Updated error messaging
    
    * chore: Working on styling errors
    
    * chore: Working on select error
    
    * chore: Cleaned up turbo response
    
    * chore: Formatting
    
    * chore: Created new controller for project transfer
    
    * Revert "chore: Created new controller for project transfer"
    
    This reverts commit 74e107ade5ced8ee62205bea9e1613bd308b8980.
    
    * chore: Working on moving to new transfer controller
    
    * chore: Working with redirect and turbo_streams
    
    * chore: FIxed issue with error not didsplaying properly
    
    * chore: Fixed redirect after successful transfer
    
    * chore: Cleaning up variables
    
    * chore: Cleaning up transfer_controller
    
    * chore: Working on modal for transfer
    
    * chore: Working transfer dialog
    
    * chore: Updated internationalization
    
    * chore: Fixed projects_test formatting
    
    From a bad merge conflict
    
    * chore: Removed confirm code
    
    * chore: Working on dialog
    
    * chore: Working on getting the form  to work for submission
    
    * chore: Updated to use new confirmation dialog
    
    * test: UI and controller tests
    
    * chore: Fixed semantic naming of button state
    
    * chore: Fixed issue with disclosure button
    
    * chore: Removed old file
    
    * chore: Removed transferable scope
    
    * chore: Cleaned up form
    
    * chore: Fixed layout headings of general settings
    
    * chore: Fixed confirmation not working
    
    * chore: Removed old tests and added to new ones
    
    * chore: Removed old test for changing path
    
    * chore: Fixed disclosure on button component
    
    * chore: Prevent content from floating in the center
    
    * chore: Removed unused code from transfer_controller
    
    * chore: Updated post to include as: :turbo_stream
    
    * chore: Remove en.yml from tailwind config
    
    * chore: Combined transfer_controller into projects_controller
    
    * chore: Add back bad params test
    
    * chore: Updated projects transfer testing
    
    Needed to move the tests into the projects controller test
    
    * chore: Renamed files and moved down to the projects directory
    
    * chore: Updated internationalization
    
    * chore: Updated internationalization

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,8 +2,4 @@ import { Application } from "@hotwired/stimulus";
 
 const application = Application.start();
 
-// Configure Stimulus development experience
-// application.debug = false
-window.Stimulus = application;
-
 export { application };
