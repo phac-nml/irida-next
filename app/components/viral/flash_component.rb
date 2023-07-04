c862b983aa4e7d15c8ffe2df4ba67aa9e9bab44b
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-07-04 12:32:34 -0500

    chore: Added hover state pause to timeout

--- app/components/viral/flash_component.rb
+++ app/components/viral/flash_component.rb
@@ -11,34 +11,6 @@ module Viral
       @type = type
       @data = data
       @timeout = timeout
-      @icon = icon_for_flash
-      @classes = classes_for_flash
-    end
-
-    def classes_for_flash
-      case type.to_s
-      when 'error'
-        'bg-red-600 dark:bg-red-800'
-      when 'success'
-        'bg-green-700 dark:bg-green-900'
-      when 'warning'
-        'bg-yellow-300 dark:bg-yellow-800'
-      else
-        'bg-blue-600 dark:bg-blue-800'
-      end
-    end
-
-    def icon_for_flash
-      case type
-      when 'error'
-        'exclamation_circle'
-      when 'success'
-        'check'
-      when 'warning'
-        'exclamation_triangle'
-      else
-        'information_circle'
-      end
     end
   end
 end
--- app/components/viral/flash_controller.js
+++ app/components/viral/flash_controller.js
@@ -8,13 +8,31 @@ export default class extends Controller {
 
   connect() {
     if (this.timeoutValue > 0) {
-      setTimeout(() => {
+      this.run();
+      this.element.addEventListener("mouseenter", this.pause.bind(this));
+      this.element.addEventListener("mouseleave", this.run.bind(this));
+    }
+  }
+
+  run() {
+    this.startedTime = new Date().getTime();
+    if (this.timeoutValue > 0) {
+      this.timeout = setTimeout(() => {
         this.dismiss();
       }, this.timeoutValue);
     }
   }
 
+  pause() {
+    if (this.timeoutValue > 0) {
+      clearTimeout(this.timeout);
+      this.timeoutValue -= new Date().getTime() - this.startedTime;
+    }
+  }
+
   dismiss() {
+    this.element.removeEventListener("mouseenter", this.pause);
+    this.element.removeEventListener("mouseleave", this.run);
     this.element.remove();
   }
 }
