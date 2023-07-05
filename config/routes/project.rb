1722466dfaecd464893d143eed2b7379cbc7e819
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-07-05 11:36:55 -0500

    adding sample transfer controller

--- config/routes/project.rb
+++ config/routes/project.rb
@@ -17,7 +17,13 @@ constraints(::Constraints::ProjectUrlConstrainer.new) do
       get :edit
       post :transfer
       resources :members, only: %i[create destroy index new update]
-      resources :samples
+      resources :samples do
+        scope module: :samples do
+          collection do
+            resources :transfer, only: %i[create new]
+          end
+        end
+      end
     end
   end
 end
---
+++ test/controllers/projects/samples/transfer_controller_test.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Projects
+  module Samples
+    class TransferControllerTest < ActionDispatch::IntegrationTest
+      setup do
+        sign_in users(:john_doe)
+        @sample1 = samples(:sample1)
+        @sample2 = samples(:sample2)
+        @project1 = projects(:project1)
+        @project2 = projects(:project2)
+        @namespace = groups(:group_one)
+      end
+
+      test 'should get new if owner' do
+        get new_namespace_project_transfer_path(@namespace, @project1)
+        assert_response :success
+      end
+
+      test 'should create sample transfer for a member that is an owner' do
+        post namespace_project_transfer_index_path(@namespace, @project1),
+             params: {
+               new_project_id: @project2.id,
+               sample_ids: [@sample1.id, @sample2.id]
+             }
+
+        assert_redirected_to namespace_project_samples_path
+      end
+
+      test 'should not create sample transfer for a member that is a maintainer' do
+        user = users(:joan_doe)
+        login_as user
+
+        post namespace_project_transfer_index_path(@namespace, @project1),
+             params: {
+               new_project_id: @project2.id,
+               sample_ids: [@sample1.id, @sample2.id]
+             }
+        assert_response :unauthorized
+      end
+
+      test 'should not create sample transfer for a member that is a guest' do
+        user = users(:ryan_doe)
+        login_as user
+
+        post namespace_project_transfer_index_path(@namespace, @project1),
+             params: {
+               new_project_id: @project2.id,
+               sample_ids: [@sample1.id, @sample2.id]
+             }
+        assert_response :unauthorized
+      end
+
+      test 'should not create sample transfer within the same project' do
+        post namespace_project_transfer_index_path(@namespace, @project1),
+             params: {
+               new_project_id: @project1.id,
+               sample_ids: [@sample1.id, @sample2.id]
+             }
+
+        assert_response :unprocessable_entity
+      end
+    end
+  end
+end
