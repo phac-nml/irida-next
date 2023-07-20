d09c8938e1c85d8e0958d47b369a0bf6a4a5b130
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2023-07-20 15:00:58 -0500

    Add new test for user fields

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -19,6 +19,29 @@ class ProfilesControllerTest < ActionDispatch::IntegrationTest
     assert_response :redirect
   end
 
+  test 'should update user fields' do
+    u = users(:john_doe)
+    sign_in u
+
+    assert_equal("john.doe@localhost", u.email)
+    assert_nil(u.first_name)
+    assert_nil(u.last_name)
+    assert_nil(u.phone_number)
+
+    patch profile_url, params: { user: {
+      email: 'john.doe@gmail.com',
+      first_name: 'john',
+      last_name: 'doe',
+      phone_number: '1234'
+      } }
+
+    assert_response :redirect
+    assert_equal("john.doe@gmail.com", u.email)
+    assert_equal("john", u.first_name)
+    assert_equal("doe", u.last_name)
+    assert_equal("1234", u.phone_number)
+  end
+
   test 'should not update a users email with a blank email' do
     sign_in users(:john_doe)
 
