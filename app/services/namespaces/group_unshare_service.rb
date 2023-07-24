bd78e386efcec1d2df301032189d10440b0a37ff
Deep Sidhu <deepsidhu85@gmail.com>
2023-07-24 15:57:51 -0500

    Added missing end

--- app/services/namespaces/group_unshare_service.rb
+++ app/services/namespaces/group_unshare_service.rb
@@ -3,7 +3,6 @@
 module Namespaces
   # Service used to Destroy NamespaceGroupLinks
   class GroupUnShareService < BaseService
-
     attr_accessor :group_id, :namespace, :max_group_access_role
 
     def initialize(user, group_id, namespace)
@@ -13,7 +12,6 @@ module Namespaces
     end
 
     def execute
-
       # authorize! namespace, to: :unshare_namespace_with_group?
 
       namespace_group_link = NamespaceGroupLink.find_by(group_id:, namespace:)
@@ -21,5 +19,6 @@ module Namespaces
       namespace_group_link.destroy
 
       namespace_group_link
+    end
   end
 end
