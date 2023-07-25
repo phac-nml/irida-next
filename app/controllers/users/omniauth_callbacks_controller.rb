bcb1811866c275ec5ecf3451aa92b0327cb58217
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2023-07-24 20:53:40 -0500

    Refactoring omniauth callback methods

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,6 @@ module Users
 
     def all
       @user = User.from_omniauth(request.env['omniauth.auth'])
-      @user.update_fields_from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
