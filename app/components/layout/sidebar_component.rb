4f858067da6ec012746a7822fa639867f7d7e6ca
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-08-02 06:02:54 -0500

    chore: Use custom icon on sidebar dropdown of sections

--- app/components/layout/sidebar_component.rb
+++ app/components/layout/sidebar_component.rb
@@ -3,14 +3,15 @@
 module Layout
   # Sidebar component to for navigation
   class SidebarComponent < Component
-    attr_reader :label
+    attr_reader :label, :icon_name
 
     renders_one :header, Sidebar::HeaderComponent
     renders_many :sections, Sidebar::SectionComponent
     renders_many :items, Sidebar::ItemComponent
 
-    def initialize(label:, **system_arguments)
+    def initialize(label:, icon_name:, **system_arguments)
       @label = label
+      @icon_name = icon_name
       @system_arguments = system_arguments
     end
   end
