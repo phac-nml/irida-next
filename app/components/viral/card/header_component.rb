6a2a30166d8dadceb7e9d20dba7ae80d1a99317e
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-08-08 10:57:31 -0500

    chore: Fixed card header stream updates

--- app/components/viral/card/header_component.rb
+++ app/components/viral/card/header_component.rb
@@ -4,12 +4,13 @@ module Viral
   module Card
     # Header component for the card
     class HeaderComponent < Component
-      attr_reader :subtitle, :title
+      attr_reader :subtitle, :title, :title_id
 
       renders_many :actions
 
-      def initialize(title: '', subtitle: nil, **system_arguments)
+      def initialize(title_id: '', title: '', subtitle: nil, **system_arguments)
         @title = title
+        @title_id = title_id
         @subtitle = subtitle
         @system_arguments = system_arguments
         @system_arguments[:classes] = class_names(
--- app/components/viral/card_component.rb
+++ app/components/viral/card_component.rb
@@ -3,13 +3,14 @@
 module Viral
   # Card component for rendering sections of pages.
   class CardComponent < Viral::Component
-    attr_reader :sectioned, :subtitle, :title
+    attr_reader :sectioned, :subtitle, :title, :title_id
 
     renders_one :header, Viral::Card::HeaderComponent
     renders_many :sections, Viral::Card::SectionComponent
 
-    def initialize(title: nil, subtitle: nil, sectioned: true, **system_arguments)
+    def initialize(title: nil, title_id: nil, subtitle: nil, sectioned: true, **system_arguments)
       @title = title
+      @title_id = title_id
       @subtitle = subtitle
       @sectioned = sectioned
       @system_arguments = system_arguments
