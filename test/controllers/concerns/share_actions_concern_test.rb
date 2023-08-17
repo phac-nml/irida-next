e5898d2e84d5d0049bcd8bb9a12647d8546b209f
Deep Sidhu <deepsidhu85@gmail.com>
2023-08-17 18:06:20 -0500

    Added more testing

--- test/controllers/concerns/share_actions_concern_test.rb
+++ test/controllers/concerns/share_actions_concern_test.rb
@@ -12,12 +12,25 @@ class TestClassController < ApplicationController
   def destroy
     redirect_to group_links_path
   end
+
+  def update
+    redirect_to group_links_path
+  end
+
+  def index; end
+
+  private
+
+  def access_levels
+    member_user = Member.find_by(user: current_user, namespace: @namespace)
+    @access_levels = Member.access_levels(member_user)
+  end
 end
 
 class ShareActionsConcernTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  test 'calling share on the controller with group_links_path not implemented' do
+  test 'calling create on the controller with group_links_path not implemented' do
     sign_in users(:john_doe)
 
     @controller = TestClassController.new
@@ -26,12 +39,45 @@ class ShareActionsConcernTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'calling unshare on the controller with group_links_path not implemented' do
+  test 'calling destroy on the controller with group_links_path not implemented' do
     sign_in users(:john_doe)
 
     @controller = TestClassController.new
     assert_raises(NotImplementedError) do
       post @controller.destroy
     end
+  end
+
+  test 'calling update on the controller with group_links_path not implemented' do
+    sign_in users(:john_doe)
+
+    @controller = TestClassController.new
+    assert_raises(NotImplementedError) do
+      post @controller.update
+    end
+  end
+
+  test 'calling index should not result in an error' do
+    sign_in users(:john_doe)
+
+    @controller = TestClassController.new
+    get @controller.index
+
+    assert @namespace.nil?
+    assert @namespace_group_link.nil?
+    assert_response :success
+  end
+
+  test 'calling group_link_namespace should result in an error' do
+    sign_in users(:john_doe)
+
+    @controller = TestClassController.new
+    assert_raises(NotImplementedError) do
+      @controller.send(:group_link_namespace)
+    end
+
+    assert @namespace.nil?
+    assert @namespace_group_link.nil?
+    assert @member.nil?
   end
 end
