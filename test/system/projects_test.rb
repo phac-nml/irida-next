b165cf0a2883bf4734bb74232259a897d6079574
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-08-17 09:34:26 -0500

    adding a quick test to check that error is displayed

--- test/system/projects_test.rb
+++ test/system/projects_test.rb
@@ -78,4 +78,18 @@ class ProjectsTest < ApplicationSystemTestCase
     assert_text I18n.t('projects.update.success', project_name: project.name)
     assert_current_path '/group-1/project-1-edited'
   end
+
+  test 'show error when editing a project path to an existing namespace' do
+    project = projects(:project1)
+    visit project_edit_path(project)
+
+    within all('form[action="/group-1/project-1"]')[1] do
+      fill_in 'project_namespace_attributes_path', with: 'project-2'
+
+      click_on I18n.t(:'projects.edit.advanced.path.submit')
+    end
+
+    assert_text 'Path has already been taken'
+    assert_current_path '/group-1/project-1/-/edit'
+  end
 end
