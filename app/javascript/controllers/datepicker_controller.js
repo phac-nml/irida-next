12da7a908fcae400a5a583f80ebbf06a5499acfa
Deep Sidhu <deepsidhu85@gmail.com>
2023-08-30 11:24:28 -0500

    Updated datepicker_controller to allow autosubmitting when updating a date for the invited groups

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -5,12 +5,31 @@ import { Datepicker } from "flowbite-datepicker";
 export default class extends Controller {
   static targets = ["datePicker"];
 
-  initialize() {
+  connect() {
+    if (this.datePickerTarget.dataset.datepickerAutosubmit) {
+      this.datePickerTarget.addEventListener("changeDate", (e) =>
+        this.handleDateSelected(e)
+      );
+    }
+
     new Datepicker(this.datePickerTarget, {
       container: "#dialog",
       format: "yyyy-mm-dd",
       orientation: "bottom left",
       autohide: true,
     });
+  }
+
+  disconnect() {
+    if (this.datePickerTarget.dataset.datepickerAutosubmit) {
+      this.datePickerTarget.removeEventListener("changeDate", (e) =>
+        this.handleDateSelected(e)
+      );
+    }
+  }
+
+  handleDateSelected(e) {
+    this.datePickerTarget.setAttribute("value", e.target.value);
+    this.datePickerTarget.form.requestSubmit();
   }
 }
