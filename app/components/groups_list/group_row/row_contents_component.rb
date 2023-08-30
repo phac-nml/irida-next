34470c0226f4e253ace60ef49d3888e6d730d64e
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-08-30 09:11:16 -0500

    chore: Updated top-level documentation comment

--- app/components/groups_list/group_row/row_contents_component.rb
+++ app/components/groups_list/group_row/row_contents_component.rb
@@ -2,6 +2,7 @@
 
 module GroupsList
   module GroupRow
+    # Component for the contents of a group tree row
     class RowContentsComponent < Viral::Component
       def initialize(group:, path: nil, path_args: {}, collapsed: false)
         @group = group
