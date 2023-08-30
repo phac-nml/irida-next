bf4dd4c47099105c895c4316a5f9a8e70cf783a3
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-08-30 09:31:59 -0500

    chore: Updated JSDoc

--- app/javascript/controllers/groups/row_controller.js
+++ app/javascript/controllers/groups/row_controller.js
@@ -3,6 +3,10 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["toggle"];
 
+  /**
+   * Toggle the group row to expose children content
+   * @param event
+   */
   toggle(event) {
     if (event.target.tagName !== "A") {
       this.toggleTarget.click();
