53a75a9d257095ff50076f5524f0896a4bbb7e25
Deep Sidhu <deepsidhu85@gmail.com>
2023-08-31 13:24:34 -0500

    Fixed route for project in fixtures, added project group links tests, updated group to group links expiration test to set the expiry date to 7 days past the current date

--- test/system/projects/group_links_test.rb
+++ test/system/projects/group_links_test.rb
@@ -2,98 +2,99 @@
 
 require 'application_system_test_case'
 
-module Groups
+module Projects
   class GroupLinksTest < ApplicationSystemTestCase
     header_row_count = 1
 
     def setup
       @user = users(:john_doe)
       login_as @user
-      @namespace = groups(:group_one)
+      @namespace = namespaces_project_namespaces(:project25_namespace)
       @group_links_count = namespace_group_links.select { |group_link| group_link.namespace == @namespace }.count
     end
 
     test 'can see a list of group links' do
-      visit group_members_url(@namespace, tab: 'invited_groups')
+      visit namespace_project_members_url(@namespace.parent, @namespace.project, tab: 'invited_groups')
 
-      assert_selector 'h1', text: I18n.t(:'groups.members.index.title')
+      assert_selector 'h1', text: I18n.t(:'projects.members.index.title')
       assert_selector 'tr', count: @group_links_count + header_row_count
     end
 
     test 'cannot access group links' do
       login_as users(:david_doe)
 
-      visit group_members_url(@namespace, tab: 'invited_groups')
+      visit namespace_project_members_url(@namespace.parent, @namespace.project, tab: 'invited_groups')
 
-      assert_text I18n.t(:'action_policy.policy.group.member_listing?', name: @namespace.name)
+      assert_text I18n.t(:'action_policy.policy.namespaces/project_namespace.member_listing?', name: @namespace.name)
     end
 
-    test 'can create a group to group link' do
-      visit group_members_url(@namespace, tab: 'invited_groups')
+    test 'can create a project to group link' do
+      visit namespace_project_members_url(@namespace.parent, @namespace.project, tab: 'invited_groups')
       assert_selector 'tr', count: @group_links_count + header_row_count
 
-      assert_selector 'a', text: I18n.t(:'groups.members.index.invite_group'), count: 1
+      assert_selector 'a', text: I18n.t(:'projects.members.index.invite_group'), count: 1
 
-      click_link I18n.t(:'groups.members.index.invite_group')
+      click_link I18n.t(:'projects.members.index.invite_group')
 
       within('span[data-controller-connected="true"] dialog') do
-        assert_selector 'h1', text: I18n.t(:'groups.group_links.new.title')
+        assert_selector 'h1', text: I18n.t(:'projects.group_links.new.title')
         assert_selector 'p', text: I18n.t(
-          :'groups.group_links.new.sharing_namespace_with_group',
+          :'projects.group_links.new.sharing_namespace_with_group',
           name: @namespace.human_name
         )
         find('#namespace_group_link_group_id').find(:xpath, 'option[2]').select_option
         find('#namespace_group_link_group_access_level').find(:xpath, 'option[3]').select_option
 
-        click_button I18n.t(:'groups.group_links.new.button.submit')
+        click_button I18n.t(:'projects.group_links.new.button.submit')
       end
 
       assert_selector 'tr', count: (@group_links_count + 1) + header_row_count
     end
 
-    test 'can remove a group to group link' do
-      namespace_group_link = namespace_group_links(:namespace_group_link5)
+    test 'can remove a project to group link' do
+      namespace_group_link = namespace_group_links(:namespace_group_link3)
 
-      visit group_members_url(@namespace, tab: 'invited_groups')
+      visit namespace_project_members_url(@namespace.parent, @namespace.project, tab: 'invited_groups')
       assert_selector 'tr', count: @group_links_count + header_row_count
 
       table_row = find(:table_row, { 'Group' => namespace_group_link.group.name })
 
       within table_row do
-        click_link I18n.t(:'groups.group_links.index.unlink')
+        click_link I18n.t(:'projects.group_links.index.unlink')
       end
 
       within('#turbo-confirm[open]') do
         click_button 'Confirm'
       end
 
-      assert_text I18n.t(:'groups.group_links.destroy.success', namespace_name: namespace_group_link.namespace.name,
-                                                                group_name: namespace_group_link.group.name)
+      assert_text I18n.t(:'projects.group_links.destroy.success', namespace_name: namespace_group_link.namespace.name,
+                                                                  group_name: namespace_group_link.group.name)
 
       assert_selector 'tr', count: (@group_links_count - 1) + header_row_count
     end
 
-    test 'cannot add a group to group link' do
+    test 'cannot add a project to group link' do
       login_as users(:ryan_doe)
-      visit group_members_url(@namespace, tab: 'invited_groups')
+      visit namespace_project_members_url(@namespace.parent, @namespace.project, tab: 'invited_groups')
 
-      assert_selector 'a', text: I18n.t(:'groups.members.index.invite_group'), count: 0
+      assert_selector 'a', text: I18n.t(:'projects.members.index.invite_group'), count: 0
     end
 
     test 'can update namespace group links group access level to another access level' do
-      namespace_group_link = namespace_group_links(:namespace_group_link5)
+      namespace_group_link = namespace_group_links(:namespace_group_link3)
 
       Timecop.travel(Time.zone.now + 5) do
-        visit group_members_url(@namespace, tab: 'invited_groups')
+        visit namespace_project_members_url(@namespace.parent, @namespace.project, tab: 'invited_groups')
         assert_selector 'tr', count: @group_links_count + header_row_count
 
         find("#invited-group-#{namespace_group_link.group.id}-access-level-select").find(:xpath,
                                                                                          'option[2]').select_option
 
         within %(turbo-frame[id="invited-group-alert"]) do
-          assert_text I18n.t(:'groups.group_links.update.success', namespace_name: namespace_group_link.namespace.name,
-                                                                   group_name: namespace_group_link.group.name,
-                                                                   param_name: 'group access level')
+          assert_text I18n.t(:'projects.group_links.update.success',
+                             namespace_name: namespace_group_link.namespace.name,
+                             group_name: namespace_group_link.group.name,
+                             param_name: 'group access level')
         end
 
         namespace_group_link_row = find(:table_row, { 'Group' => namespace_group_link.group.name })
@@ -106,20 +107,21 @@ module Groups
     end
 
     test 'can update namespace group links expiration' do
-      namespace_group_link = namespace_group_links(:namespace_group_link5)
+      namespace_group_link = namespace_group_links(:namespace_group_link3)
       expiry_date = (Time.zone.today + 7).strftime('%Y-%m-%d')
 
       Timecop.travel(Time.zone.now + 5) do
-        visit group_members_url(@namespace, tab: 'invited_groups')
+        visit namespace_project_members_url(@namespace.parent, @namespace.project, tab: 'invited_groups')
         assert_selector 'tr', count: @group_links_count + header_row_count
 
         find("#invited-group-#{namespace_group_link.group.id}-expiration").click.set(expiry_date)
                                                                           .native.send_keys(:return)
 
         within %(turbo-frame[id="invited-group-alert"]) do
-          assert_text I18n.t(:'groups.group_links.update.success', namespace_name: namespace_group_link.namespace.name,
-                                                                   group_name: namespace_group_link.group.name,
-                                                                   param_name: 'expiration')
+          assert_text I18n.t(:'projects.group_links.update.success',
+                             namespace_name: namespace_group_link.namespace.name,
+                             group_name: namespace_group_link.group.name,
+                             param_name: 'expiration')
         end
 
         namespace_group_link_row = find(:table_row, { 'Group' => namespace_group_link.group.name })
