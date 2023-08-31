009ade8af7e38e80154a1d7cc9a1f6072fbe867e
Eric Enns <492127+ericenns@users.noreply.github.com>
2023-08-31 12:02:18 -0500

    chore: add in Attachment model test

--- test/models/attachment_test.rb
+++ test/models/attachment_test.rb
@@ -3,7 +3,24 @@
 require 'test_helper'
 
 class AttachmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @attachment1 = attachments(:attachment1)
+    @sample = samples(:sample1)
+  end
+
+  test 'valid attachment' do
+    assert @attachment1.valid?
+  end
+
+  test '#destroy does not destroy the ActiveStorage::Attachment' do
+    assert_no_difference('ActiveStorage::Attachment.count') do
+      @attachment1.destroy
+    end
+  end
+
+  test '#destroy is a soft deletion and sets deleted_at' do
+    assert_nil @attachment1.deleted_at
+    @attachment1.destroy
+    assert_not_nil @attachment1.deleted_at
+  end
 end
