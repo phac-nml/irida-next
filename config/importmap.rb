ecde47c42d2458c7d72ef7b9145e7421d17e2c33
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-05 05:43:57 -0500

    chore: Move component js to javascript/controllers folder to eliminate console erros

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,6 @@ pin 'application', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
-pin_all_from 'app/components', under: 'components'
 pin 'flowbite', to: 'https://ga.jspm.io/npm:flowbite@1.6.5/lib/esm/index.js'
 pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.7/dist/esm/index.js'
 pin 'flowbite-datepicker', to: 'https://ga.jspm.io/npm:flowbite-datepicker@1.2.2/js/main.js'
