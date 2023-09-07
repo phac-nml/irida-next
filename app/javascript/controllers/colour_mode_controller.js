b403ede83a6b39b7037a48fdab915e7a14503262
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-07 11:30:22 -0500

    chore: Update classes in the javascript controller

--- app/javascript/controllers/colour_mode_controller.js
+++ app/javascript/controllers/colour_mode_controller.js
@@ -25,7 +25,9 @@ export default class extends Controller {
 
     if (isDarkMode) {
       document.documentElement.classList.add("dark");
+      document.documentElement.classList.remove("light");
     } else {
+      document.documentElement.classList.add("light");
       document.documentElement.classList.remove("dark");
     }
   }
