c25ab6a7f6e37bc170930357d1ab17b4b2c411d2
Eric Enns <492127+ericenns@users.noreply.github.com>
2023-09-07 08:27:21 -0500

    chore: add in env variable fetching to simplify config for produciont

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = ENV.fetch('RAILS_STORAGE_SERVICE', 'local').to_sym
+
+  config.action_mailer.default_url_options = { host: ENV.fetch('RAILS_HOST'), port: ENV.fetch('RAILS_PROTOCOL') }
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
