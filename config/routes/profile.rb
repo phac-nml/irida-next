7ee55ae0df9c92fc5f1248c96a6495b141720e4e
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-07 11:36:12 -0500

    Dark mode move to profile preferences (#157)
    * chore: Initial setup
    
    * chore: Working on updating in seperate tab
    
    * chore: Fixed issue preventing cross tab updating
    
    * chore: Removed logging statement
    
    * chore: Cleaned up internationalization
    
    * chore: Added watcher for when the window mode changes
    
    * chore: Make sure to remove the opposite colour theme class
    
    * chore: Update classes in the javascript controller

--- config/routes/profile.rb
+++ config/routes/profile.rb
@@ -4,6 +4,7 @@ resource :profile, only: %i[show update] do
   scope module: :profiles do
     resource :password, only: %i[edit update]
     resource :account, only: %i[show destroy]
+    resource :preferences, only: %i[show]
     resources :personal_access_tokens, only: %i[index create] do
       member do
         delete :revoke
