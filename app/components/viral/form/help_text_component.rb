5b66b77dab4dea26ac53f09bd6cda54b8b1cba35
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-08 10:09:01 -0500

    Fix field errors (#168)
    * chore: Testing
    
    * chore: Fixed file upload error
    
    * chore: Fixed rubocop error
    
    * chore: Updated method name to prevent errors

---
+++ app/components/viral/form/help_text_component.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Viral
+  module Form
+    # Component to render form help text can be either default, success or error
+    class HelpTextComponent < ViewComponent::Base
+      attr_reader :icon
+
+      STATE_MAPPINGS = {
+        default: 'mt-2 text-sm text-slate-500 dark:text-slate-400',
+        success: 'mt-2 text-sm text-green-600 dark:text-green-500',
+        error: 'mt-2 text-sm text-red-600 dark:text-red-500'
+      }.freeze
+
+      ICON_MAPPINGS = {
+        default: 'information_circle_solid',
+        success: 'check_circle_solid',
+        error: 'x_circle'
+      }.freeze
+
+      def initialize(state: :default, **system_arguments)
+        @state = state
+        @arguments = system_arguments
+        @icon = ICON_MAPPINGS.key?(@state) ? ICON_MAPPINGS[@state] : ICON_MAPPINGS[:default]
+      end
+
+      def system_arguments
+        @arguments.tap do |args|
+          args[:tag] = 'p'
+          args[:classes] = class_names(
+            'flex items-center justify-start',
+            STATE_MAPPINGS.key?(@state) ? STATE_MAPPINGS[@state] : STATE_MAPPINGS[:default]
+          )
+        end
+      end
+    end
+  end
+end
