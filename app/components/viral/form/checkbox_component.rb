707d48a2100b630109f37618183e5ac0e19a028e
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-12 09:43:48 -0500

    chore: Checkbox cleanup

--- app/components/viral/form/checkbox_component.rb
+++ app/components/viral/form/checkbox_component.rb
@@ -3,9 +3,9 @@
 module Viral
   module Form
     class CheckboxComponent < Viral::Component
-      attr_reader :label, :default, :help_text
+      attr_reader :label, :name, :default, :help_text
 
-      def initialize(label:, default: false, help_text: nil, hidden: false, **arguments)
+      def initialize(name:, label:, default: false, help_text: nil, hidden: false, **arguments)
         @label = label
         @default = default
         @help_text = help_text
@@ -16,7 +16,7 @@ module Viral
       def system_arguments
         @arguments.tap do |args|
           args[:tag] = 'div'
-          args[:classes] = class_names('mb-4', args[:classes], @hidden ? 'hidden' : '')
+          args[:classes] = class_names('mb-4', args[:classes], @hidden ? 'hidden' : nil)
           args['aria-hidden'] = @hidden
         end
       end
---
+++ test/components/nextflow_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class NextflowComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(NextflowComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
