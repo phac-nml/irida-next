ed47d762ff244f77f221a00a9b031d7970683f0a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-12 14:35:53 -0500

    chore: Clean up class definition as they were too long

--- app/components/viral/form/text_input_component.rb
+++ app/components/viral/form/text_input_component.rb
@@ -28,7 +28,9 @@ module Viral
           args[:value] = @default
           args[:required] = @required
           args[:classes] = class_names(
-            'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500',
+            'focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400',
+            'dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
             args[:classes]
           )
         end
