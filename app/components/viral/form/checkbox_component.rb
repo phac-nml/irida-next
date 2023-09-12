e6deeec2afff373e40080a21644989cc7676fdac
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-12 11:52:31 -0500

    chore: Cleaned up linting errors and fixed name argument

--- app/components/viral/form/checkbox_component.rb
+++ app/components/viral/form/checkbox_component.rb
@@ -2,10 +2,13 @@
 
 module Viral
   module Form
+    # Checkbox component
     class CheckboxComponent < Viral::Component
       attr_reader :label, :name, :default, :help_text
 
+      # rubocop:disable Metrics/ParameterLists
       def initialize(name:, label:, default: false, help_text: nil, hidden: false, **arguments)
+        @name = name
         @label = label
         @default = default
         @help_text = help_text
