b1eff6f8363c19645b302ba8ae414479685ccf36
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-13 11:10:22 -0500

    chore: Moved nextflow test file to tests fixtures

--- app/components/nextflow_component.rb
+++ app/components/nextflow_component.rb
@@ -5,8 +5,7 @@ class NextflowComponent < Component
   attr_reader :schema, :url
 
   def initialize(schema_file:, url:)
-    path = Rails.root.join('tmp', schema_file)
-    @schema = JSON.parse(File.read(path))
+    @schema = JSON.parse(File.read(schema_file))
     @url = url
   end
 end
