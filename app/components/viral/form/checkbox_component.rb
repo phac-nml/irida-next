f16577584d793b2bfcb8dd174a65ba90ccb55558
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-14 11:11:31 -0500

    chore: Updated testing for checkbox component

--- app/components/viral/form/checkbox_component.rb
+++ app/components/viral/form/checkbox_component.rb
@@ -4,12 +4,13 @@ module Viral
   module Form
     # Checkbox component
     class CheckboxComponent < Viral::Component
-      attr_reader :label, :name, :default, :help_text
+      attr_reader :label, :name, :value, :checked, :help_text
 
-      def initialize(name:, label:, default: false, help_text: nil, hidden: false, **arguments) # rubocop:disable Metrics/ParameterLists
+      def initialize(name:, value:, label:, checked: false, help_text: nil, hidden: false, **arguments) # rubocop:disable Metrics/ParameterLists
         @name = name
+        @value = value
         @label = label
-        @default = default
+        @checked = checked
         @help_text = help_text
         @hidden = hidden
         @arguments = arguments
