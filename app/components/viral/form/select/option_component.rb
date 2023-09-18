aefd4134be593126801bc2e73cd6acb83d6b3fbe
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-18 14:41:16 -0500

    chore: Select appopriate option

--- app/components/viral/form/select/option_component.rb
+++ app/components/viral/form/select/option_component.rb
@@ -3,15 +3,23 @@
 module Viral
   module Form
     module Select
+      # Options for a select component
       class OptionComponent < ViewComponent::Base
         with_collection_parameter :option
 
         erb_template <<~ERB
-          <option value="<%= @option[:value] %>"><%= @option[:label] %></option>
+          <% if @selected_value == @option[:value] %>
+            <option value="<%= @option[:value] %>" selected>
+          <% else %>
+            <option value="<%= @option[:value] %>">
+          <% end %>
+            <%= @option[:label] %>
+          </option>
         ERB
 
-        def initialize(option:)
+        def initialize(option:, selected_value: nil)
           @option = option
+          @selected_value = selected_value
         end
       end
     end
--- app/components/viral/form/select_component.rb
+++ app/components/viral/form/select_component.rb
@@ -3,14 +3,14 @@
 module Viral
   module Form
     class SelectComponent < Viral::Component
-      attr_reader :label, :name, :multiple, :options, :default
+      attr_reader :label, :name, :multiple, :options, :selected_value
 
       # rubocop:disable Metrics/ParameterLists
-      def initialize(label:, name:, options: [], multiple: false, default: nil, **_options)
+      def initialize(label:, name:, options: [], multiple: false, selected_value: nil, **_options)
         @label = label
         @name = name
         @multiple = multiple
-        @default = default
+        @selected_value = selected_value
         @options = options
       end
     end
