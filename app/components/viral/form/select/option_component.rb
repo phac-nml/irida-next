7d995ea6460f5beae2c5fd1b56f2a2d1ee62ca69
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-19 08:06:08 -0500

    chore: Use base component
    This allows for better addition of attributes as opposed to if statements on the dom

--- app/components/viral/form/select/option_component.rb
+++ app/components/viral/form/select/option_component.rb
@@ -4,22 +4,28 @@ module Viral
   module Form
     module Select
       # Options for a select component
-      class OptionComponent < ViewComponent::Base
+      class OptionComponent < Viral::Component
         with_collection_parameter :option
 
         erb_template <<~ERB
-          <% if @selected_value == @option[:value] %>
-            <option value="<%= @option[:value] %>" selected>
-          <% else %>
-            <option value="<%= @option[:value] %>">
-          <% end %>
+          <%= render Viral::BaseComponent.new(**system_arguments) do %>
             <%= @option[:label] %>
-          </option>
+          <% end %>
         ERB
 
-        def initialize(option:, selected_value: nil)
+        def initialize(option:, selected_value: nil, **args)
           @option = option
           @selected_value = selected_value
+          @args = args
+        end
+
+        def system_arguments
+          @args.tap do |opts|
+            opts[:tag] = 'option'
+            opts[:value] = @option[:value]
+            opts[:selected] = 'selected' if @selected_value == @option[:value]
+            opts[:disabled] if @option[:disabled]
+          end
         end
       end
     end
