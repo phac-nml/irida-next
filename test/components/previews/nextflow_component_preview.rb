dd1fd990c7926f861273dc3cfbe3b66ae6dfbe4a
Eric Enns <492127+ericenns@users.noreply.github.com>
2023-09-21 10:34:02 -0500

    chore: add in param to nextflow component preview so that a developer can select the schema file to render

--- test/components/previews/nextflow_component_preview.rb
+++ test/components/previews/nextflow_component_preview.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
 class NextflowComponentPreview < ViewComponent::Preview
-  def default; end
+  # @param schema_file select :schema_file_options
+  def default(schema_file: 'nextflow_schema.json')
+    render_with_template(locals: {
+                           schema_file:
+                         })
+  end
+
+  private
+
+  def schema_file_options
+    Rails.root.join('test/fixtures/files/nextflow').entries.select do |f|
+      File.file?(File.join('test/fixtures/files/nextflow', f)) && f.to_s.starts_with?('nextflow_schema')
+    end
+  end
 end
