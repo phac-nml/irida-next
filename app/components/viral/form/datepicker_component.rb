9864ab2a57ff0ad0e68d81ae8a97eb65dc3b906f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-09-21 11:08:37 -0500

    chore: Code cleanup

--- app/components/viral/form/datepicker_component.rb
+++ app/components/viral/form/datepicker_component.rb
@@ -1,5 +1,36 @@
 # frozen_string_literal: true
 
-class Viral::Form::DatepickerComponent < ViewComponent::Base
+module Viral
+  module Form
+    # Form control for datepicker
+    class DatepickerComponent < Viral::Component
+      attr_reader :name, :label
 
+      def initialize(id:, name:, label: nil, value: nil, **options)
+        @id = id
+        @name = name
+        @label = label
+        @value = value
+        @options = options
+      end
+
+      def system_arguments
+        @options.tap do |opts|
+          opts[:tag] = 'input'
+          opts[:type] = 'text'
+          opts[:name] = @name
+          opts[:id] = @id
+          opts[:value] = @value
+          opts[:classes] = class_names(
+            'border border-slate-300 text-slate-900 sm:text-sm rounded-md focus:ring-primary-700',
+            'focus:border-primary-700 block w-full pl-10 p-2.5 dark:bg-slate-800 dark:border-slate-600',
+            'dark:placeholder-slate-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+          )
+          opts[:data] = {
+            datepicker_target: 'datePicker'
+          }
+        end
+      end
+    end
+  end
 end
---
+++ test/components/previews/viral_form_datepicker_component_preview.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class ViralFormDatepickerComponentPreview < ViewComponent::Preview
+  def default; end
+end
