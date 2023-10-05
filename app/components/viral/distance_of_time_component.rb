088f2355b1fba4454e39a1588e3d4aa05200bb3d
Chris Huynh <chris.huynh333@gmail.com>
2023-10-05 12:26:54 -0500

    Combine viral_tooltip into viral_distance
    Calling viral_distance in html.erb now includes the viral_tooltip

--- app/components/viral/distance_of_time_component.rb
+++ app/components/viral/distance_of_time_component.rb
@@ -3,10 +3,11 @@
 module Viral
   # Card component for rendering sections of pages.
   class DistanceOfTimeComponent < Viral::Component
-    attr_reader :time_difference
+    attr_reader :time_difference, :original_time
 
     def initialize(current_time:, original_time:)
       @time_difference = distance_of_time_in_words(current_time, original_time)
+      @original_time = original_time
     end
   end
 end
