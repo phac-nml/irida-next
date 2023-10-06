d08f2957ccc5fee26dca667c20948cdc88bea133
Chris Huynh <chris.huynh333@gmail.com>
2023-10-06 14:05:03 -0500

    Fix tests for time_ago viral component

---
+++ test/components/viral/system/time_ago_component_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+module System
+  class TimeAgoComponentTest < ApplicationSystemTestCase
+    test 'default' do
+      visit('/rails/view_components/viral_time_ago_component/default')
+      assert_text '15 days ago'
+    end
+
+    test 'current time input' do
+      visit('/rails/view_components/viral_time_ago_component/current_time_input')
+      assert_text '5 days ago'
+    end
+
+    test 'tooltip' do
+      visit('/rails/view_components/viral_time_ago_component/default')
+      within('.Viral-Preview > [data-controller-connected="true"]') do
+        assert_selector '[data-viral--tooltip-target="target"]', visible: false
+        find('span.text-sm', text: '15 days ago').hover
+        assert_text (DateTime.now - 15.days).strftime('%B %d, %Y %H:%M')
+        assert_selector '[data-viral--tooltip-target="target"]', visible: true
+      end
+    end
+  end
+end
