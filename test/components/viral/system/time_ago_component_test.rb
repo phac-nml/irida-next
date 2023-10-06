ebaa3e31e69d1e06fdfd0d9fcc6b28975e70ae59
Chris Huynh <chris.huynh333@gmail.com>
2023-10-06 14:53:47 -0500

    Fix tooltip test for default and add current time tooltip test

--- test/components/viral/system/time_ago_component_test.rb
+++ test/components/viral/system/time_ago_component_test.rb
@@ -14,11 +14,23 @@ module System
       assert_text '5 days ago'
     end
 
-    test 'tooltip' do
+    test 'default tooltip' do
       visit('/rails/view_components/viral_time_ago_component/default')
+      assert_text '15 days ago'
       within('.Viral-Preview > [data-controller-connected="true"]') do
         assert_selector '[data-viral--tooltip-target="target"]', visible: false
         find('span.text-sm', text: '15 days ago').hover
+        assert_text (DateTime.now - 15.days).strftime('%B %d, %Y %H:%M')
+        assert_selector '[data-viral--tooltip-target="target"]', visible: true
+      end
+    end
+
+    test 'current time input tooltip' do
+      visit('/rails/view_components/viral_time_ago_component/current_time_input')
+      assert_text '5 days ago'
+      within('.Viral-Preview > [data-controller-connected="true"]') do
+        assert_selector '[data-viral--tooltip-target="target"]', visible: false
+        find('span.text-sm', text: '5 days ago').hover
         assert_text (DateTime.now - 15.days).strftime('%B %d, %Y %H:%M')
         assert_selector '[data-viral--tooltip-target="target"]', visible: true
       end
