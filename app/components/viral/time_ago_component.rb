ebf6676022ee1938aa3ba12f06ef58ba32d369ae
Chris Huynh <chris.huynh333@gmail.com>
2023-10-06 08:55:32 -0500

    Add default current_time to time_ago component

--- app/components/viral/time_ago_component.rb
+++ app/components/viral/time_ago_component.rb
@@ -5,8 +5,8 @@ module Viral
   class TimeAgoComponent < Viral::Component
     attr_reader :time_ago, :original_time
 
-    def initialize(original_time:)
-      @time_ago = distance_of_time_in_words(Time.zone.now, original_time)
+    def initialize(original_time:, current_time: Time.zone.now)
+      @time_ago = distance_of_time_in_words(current_time, original_time)
       @original_time = original_time
     end
   end
