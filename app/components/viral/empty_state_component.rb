3b8261baeca7ffba23589b13945e78abc8c5c9b1
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-10-10 10:20:20 -0500

    chore: Made emtpy state view component

---
+++ app/components/viral/empty_state_component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Viral
+  class EmptyStateComponent < Viral::Component
+    attr_reader :title, :description, :icon_name
+
+    def initialize(title:, description:, icon_name:)
+      @title = title
+      @description = description
+      @icon_name = icon_name
+    end
+  end
+end
