73aa8bbc45605e36f3fa91ad646a721407d0ab79
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-10-10 21:00:06 -0500

    chore: Simplified and added samplesheet param to LookBook

--- test/components/previews/nextflow_samplesheet_component_preview.rb
+++ test/components/previews/nextflow_samplesheet_component_preview.rb
@@ -2,23 +2,23 @@
 
 require 'csv'
 class NextflowSamplesheetComponentPreview < ViewComponent::Preview
+  # @param samplesheet_file select :samplesheet_file_options
+
   def default(samplesheet_file: 'samplesheet.csv')
-    headers, *remainder = CSV.read(
+    headers, *samples = CSV.read(
       Rails.root.join('test', 'fixtures', 'files', 'nextflow', samplesheet_file)
     )
-
-    samples = remainder.map do |row|
-      {
-        headers[0] => row[0],
-        headers[1] => row[1],
-        headers[2] => row[2],
-        headers[3] => row[3]
-      }
-    end
 
     render_with_template(locals: {
                            headers:,
                            samples:
                          })
+  end
+
+  def samplesheet_file_options
+    Rails.root.join('test/fixtures/files/nextflow').entries.select do |f|
+      File.file?(File.join('test/fixtures/files/nextflow',
+                           f)) && f.to_s.starts_with?('samplesheet') && f.to_s.ends_with?('.csv')
+    end
   end
 end
