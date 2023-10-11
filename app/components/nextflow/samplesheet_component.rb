36d008db53a6ff5c919476737a45a81bfa254626
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-10-10 21:02:05 -0500

    chore: Updated file comment

--- app/components/nextflow/samplesheet_component.rb
+++ app/components/nextflow/samplesheet_component.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Nextflow
+  # Render the contents of a Nextflow samplesheet to a table
   class SamplesheetComponent < Component
     def initialize(headers:, samples:)
       @headers = headers
