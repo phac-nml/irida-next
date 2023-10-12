888ec63089226723d438ec1d0ada1e22cac1676a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-10-12 13:57:30 -0500

    chore: Added file selection

--- test/components/previews/nextflow_samplesheet_component_preview.rb
+++ test/components/previews/nextflow_samplesheet_component_preview.rb
@@ -4,7 +4,7 @@ require 'csv'
 class NextflowSamplesheetComponentPreview < ViewComponent::Preview
   # @param schema_file select :schema_file_options
   def default(schema_file: 'samplesheet_schema.json')
-    sample1 = Sample.find_by(id: 961)
+    sample1 = Sample.find_by(id: 941)
     sample2 = Sample.new(name: 'sample_2', description: 'Sample 2 description')
 
     render_with_template(locals: {
