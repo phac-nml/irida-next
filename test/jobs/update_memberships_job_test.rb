4fd6d9d26eb76183e1818c9e9d71bab0ac9dfbff
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-10-24 09:39:37 -0500

    adding some edge case tests

--- test/jobs/update_memberships_job_test.rb
+++ test/jobs/update_memberships_job_test.rb
@@ -170,4 +170,45 @@ class UpdateMembershipsJobTest < ActiveJob::TestCase
     assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.reload.access_level
     assert_equal Member::AccessLevel::MAINTAINER, @second_subgroup_member.reload.access_level
   end
+
+  test 'empty memberships' do
+    assert_equal Member::AccessLevel::GUEST, @group_member.access_level
+    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.access_level
+    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.access_level
+
+    UpdateMembershipsJob.perform_now([])
+
+    assert_equal Member::AccessLevel::GUEST, @group_member.reload.access_level
+    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.reload.access_level
+    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.reload.access_level
+  end
+
+  test 'memberships do not exist' do
+    assert_equal Member::AccessLevel::GUEST, @group_member.access_level
+    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.access_level
+    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.access_level
+
+    UpdateMembershipsJob.perform_now([0])
+
+    assert_equal Member::AccessLevel::GUEST, @group_member.reload.access_level
+    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.reload.access_level
+    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.reload.access_level
+  end
+
+  test 'maximum nested memberships' do
+    (Namespace::MAX_ANCESTORS - 1).times do |n|
+      assert_equal Member::AccessLevel::GUEST,
+                   groups("subgroup#{n + 1}").group_members.where(user_id: @group_member.user_id).first.access_level
+    end
+
+    @group_member.access_level = Member::AccessLevel::MAINTAINER
+    @group_member.save
+
+    UpdateMembershipsJob.perform_now([@group_member.id])
+
+    (Namespace::MAX_ANCESTORS - 1).times do |n|
+      assert_equal Member::AccessLevel::MAINTAINER,
+                   groups("subgroup#{n + 1}").group_members.where(user_id: @group_member.user_id).first.access_level
+    end
+  end
 end
