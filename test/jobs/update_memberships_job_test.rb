557cc43493887464d6cab1595ba0aecb5cbaae31
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-10-24 11:15:58 -0500

    skipping callback when memberships saved during testing

--- test/jobs/update_memberships_job_test.rb
+++ test/jobs/update_memberships_job_test.rb
@@ -4,48 +4,14 @@ require 'test_helper'
 
 class UpdateMembershipsJobTest < ActiveJob::TestCase
   def setup
+    Member.skip_callback(:update, :after, :update_descendant_memberships)
+
     @group_member = members(:group_one_member_ryan_doe)
     @first_subgroup_member = members(:subgroup1_member_ryan_doe)
     @second_subgroup_member = members(:subgroup2_member_ryan_doe)
   end
 
-  test 'equal access levels when parent group membership passed' do
-    assert_equal Member::AccessLevel::GUEST, @group_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.access_level
-
-    UpdateMembershipsJob.perform_now([@group_member.id])
-
-    assert_equal Member::AccessLevel::GUEST, @group_member.reload.access_level
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.reload.access_level
-    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.reload.access_level
-  end
-
-  test 'equal access levels when subgroup membership passed' do
-    assert_equal Member::AccessLevel::GUEST, @group_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.access_level
-
-    UpdateMembershipsJob.perform_now([@first_subgroup_member.id])
-
-    assert_equal Member::AccessLevel::GUEST, @group_member.reload.access_level
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.reload.access_level
-    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.reload.access_level
-  end
-
-  test 'equal access levels when nested subgroup membership passed' do
-    assert_equal Member::AccessLevel::GUEST, @group_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.access_level
-
-    UpdateMembershipsJob.perform_now([@second_subgroup_member.id])
-
-    assert_equal Member::AccessLevel::GUEST, @group_member.reload.access_level
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.reload.access_level
-    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.reload.access_level
-  end
-
-  test 'parent group access level higher when parent group membership passed' do
+  test 'parent group access level higher' do
     @group_member.access_level = Member::AccessLevel::MAINTAINER
     @group_member.save
 
@@ -59,49 +25,7 @@ class UpdateMembershipsJobTest < ActiveJob::TestCase
     assert_equal Member::AccessLevel::MAINTAINER, @second_subgroup_member.reload.access_level
   end
 
-  test 'parent group access level higher when subgroup group membership passed' do
-    @group_member.access_level = Member::AccessLevel::MAINTAINER
-    @group_member.save
-
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.access_level
-
-    UpdateMembershipsJob.perform_now([@first_subgroup_member.id])
-
-    assert_equal Member::AccessLevel::MAINTAINER, @group_member.reload.access_level
-    assert_equal Member::AccessLevel::MAINTAINER, @first_subgroup_member.reload.access_level
-    assert_equal Member::AccessLevel::MAINTAINER, @second_subgroup_member.reload.access_level
-  end
-
-  test 'parent group access level higher when nested subgroup group membership passed' do
-    @group_member.access_level = Member::AccessLevel::MAINTAINER
-    @group_member.save
-
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.access_level
-
-    UpdateMembershipsJob.perform_now([@second_subgroup_member.id])
-
-    assert_equal Member::AccessLevel::MAINTAINER, @group_member.reload.access_level
-    assert_equal Member::AccessLevel::MAINTAINER, @first_subgroup_member.reload.access_level
-    assert_equal Member::AccessLevel::MAINTAINER, @second_subgroup_member.reload.access_level
-  end
-
-  test 'subgroup access level higher when parent group membership passed' do
-    @first_subgroup_member.access_level = Member::AccessLevel::MAINTAINER
-    @first_subgroup_member.save
-
-    assert_equal Member::AccessLevel::GUEST, @group_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.access_level
-
-    UpdateMembershipsJob.perform_now([@group_member.id])
-
-    assert_equal Member::AccessLevel::GUEST, @group_member.reload.access_level
-    assert_equal Member::AccessLevel::MAINTAINER, @first_subgroup_member.reload.access_level
-    assert_equal Member::AccessLevel::MAINTAINER, @second_subgroup_member.reload.access_level
-  end
-
-  test 'subgroup access level higher when subgroup group membership passed' do
+  test 'subgroup access level higher' do
     @first_subgroup_member.access_level = Member::AccessLevel::MAINTAINER
     @first_subgroup_member.save
 
@@ -115,49 +39,7 @@ class UpdateMembershipsJobTest < ActiveJob::TestCase
     assert_equal Member::AccessLevel::MAINTAINER, @second_subgroup_member.reload.access_level
   end
 
-  test 'subgroup access level higher when nested subgroup group membership passed' do
-    @first_subgroup_member.access_level = Member::AccessLevel::MAINTAINER
-    @first_subgroup_member.save
-
-    assert_equal Member::AccessLevel::GUEST, @group_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @second_subgroup_member.access_level
-
-    UpdateMembershipsJob.perform_now([@second_subgroup_member.id])
-
-    assert_equal Member::AccessLevel::GUEST, @group_member.reload.access_level
-    assert_equal Member::AccessLevel::MAINTAINER, @first_subgroup_member.reload.access_level
-    assert_equal Member::AccessLevel::MAINTAINER, @second_subgroup_member.reload.access_level
-  end
-
-  test 'nested subgroup access level higher when parent group membership passed' do
-    @second_subgroup_member.access_level = Member::AccessLevel::MAINTAINER
-    @second_subgroup_member.save
-
-    assert_equal Member::AccessLevel::GUEST, @group_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.access_level
-
-    UpdateMembershipsJob.perform_now([@group_member.id])
-
-    assert_equal Member::AccessLevel::GUEST, @group_member.reload.access_level
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.reload.access_level
-    assert_equal Member::AccessLevel::MAINTAINER, @second_subgroup_member.reload.access_level
-  end
-
-  test 'nested subgroup access level higher when subgroup group membership passed' do
-    @second_subgroup_member.access_level = Member::AccessLevel::MAINTAINER
-    @second_subgroup_member.save
-
-    assert_equal Member::AccessLevel::GUEST, @group_member.access_level
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.access_level
-
-    UpdateMembershipsJob.perform_now([@first_subgroup_member.id])
-
-    assert_equal Member::AccessLevel::GUEST, @group_member.reload.access_level
-    assert_equal Member::AccessLevel::GUEST, @first_subgroup_member.reload.access_level
-    assert_equal Member::AccessLevel::MAINTAINER, @second_subgroup_member.reload.access_level
-  end
-
-  test 'nested subgroup access level higher when nested subgroup group membership passed' do
+  test 'nested subgroup access level higher' do
     @second_subgroup_member.access_level = Member::AccessLevel::MAINTAINER
     @second_subgroup_member.save
 
@@ -196,13 +78,13 @@ class UpdateMembershipsJobTest < ActiveJob::TestCase
   end
 
   test 'maximum nested memberships' do
+    @group_member.access_level = Member::AccessLevel::MAINTAINER
+    @group_member.save
+
     (Namespace::MAX_ANCESTORS - 1).times do |n|
       assert_equal Member::AccessLevel::GUEST,
                    groups("subgroup#{n + 1}").group_members.where(user_id: @group_member.user_id).first.access_level
     end
-
-    @group_member.access_level = Member::AccessLevel::MAINTAINER
-    @group_member.save
 
     UpdateMembershipsJob.perform_now([@group_member.id])
 
