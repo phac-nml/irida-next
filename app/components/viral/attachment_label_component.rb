4f9fbc4c1f178c4e6fa1e5f394e88f54b37535c2
Chris Huynh <chris.huynh333@gmail.com>
2023-10-25 10:33:29 -0500

    Change attachment label to flowbite pill and switch pill from div to span

--- app/components/viral/attachment_label_component.rb
+++ app/components/viral/attachment_label_component.rb
@@ -7,18 +7,18 @@ module Viral
 
     COLORS = {
       format: {
-        fasta: 'bg-blue-100 text-blue-800 text-base font-medium
-        mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300 text-center',
-        fastq: 'bg-green-100 text-green-800 text-base font-medium
-        mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 text-center',
-        unknown: 'bg-gray-100 text-gray-800 text-base font-medium
-        mr-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300 text-center'
+        fasta: 'bg-blue-100 text-blue-800 text-xs font-medium
+          mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300',
+        fastq: 'bg-green-100 text-green-800 text-xs font-medium
+          mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300',
+        unknown: 'bg-gray-100 text-gray-800 text-xs font-medium
+          mr-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300'
       },
       type: {
-        assembly: 'bg-pink-100 text-pink-800 text-base font-medium
-        mr-2 px-2.5 py-0.5 rounded-full dark:bg-pink-900 dark:text-pink-300 text-center',
-        illumina_pe: 'bg-purple-100 text-purple-800 text-base font-medium
-        mr-2 px-2.5 py-0.5 rounded-full dark:bg-purple-900 dark:text-purple-300 text-center'
+        assembly: 'bg-pink-100 text-pink-800 text-xs font-medium
+          mr-2 px-2.5 py-0.5 rounded-full dark:bg-pink-900 dark:text-pink-300',
+        illumina_pe: 'bg-purple-100 text-purple-800 text-xs font-medium
+          mr-2 px-2.5 py-0.5 rounded-full dark:bg-purple-900 dark:text-purple-300'
       }
     }.freeze
 
