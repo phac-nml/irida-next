b265302acf47f8d228735e52ddb83bb1b54bfded
Chris Huynh <chris.huynh333@gmail.com>
2023-10-30 12:06:19 -0500

    Add fixes for PR review
    Includes moving generic pill classes into sys_arguments
    
    Moving color choosing logic for attachments to front end

--- app/components/viral/pill_component.rb
+++ app/components/viral/pill_component.rb
@@ -6,49 +6,30 @@ module Viral
     attr_reader :text
 
     COLORS = {
-      blue: 'bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300',  # rubocop:disable Layout/LineLength
-      gray: 'bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300',  # rubocop:disable Layout/LineLength
-      red: 'bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300', # rubocop:disable Layout/LineLength
-      green: 'bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300', # rubocop:disable Layout/LineLength
-      yellow: 'bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300', # rubocop:disable Layout/LineLength
-      indigo: 'bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-indigo-900 dark:text-indigo-300', # rubocop:disable Layout/LineLength
-      purple: 'bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-purple-900 dark:text-purple-300', # rubocop:disable Layout/LineLength
-      pink: 'bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-pink-900 dark:text-pink-300' # rubocop:disable Layout/LineLength
+      blue: 'bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-300',
+      gray: 'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300',
+      red: 'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300',
+      green: 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300',
+      yellow: 'bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300',
+      indigo: 'bg-indigo-100 text-indigo-800 dark:bg-indigo-900 dark:text-indigo-300',
+      purple: 'bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-300',
+      pink: 'bg-pink-100 text-pink-800 dark:bg-pink-900 dark:text-pink-300'
     }.freeze
 
     def initialize(
       text: nil,
-      color: COLORS[:gray],
+      color: nil,
       **system_arguments
     )
       @text = text
-      @color = if %w[format type].include?(color)
-                 get_color(text) if text
-               else
-                 color.to_sym
-               end
+      @color = color.to_sym if color
       @system_arguments = system_arguments
       @system_arguments[:classes] = class_names(
         @system_arguments[:classes],
-        COLORS[@color]
+        COLORS[@color],
+        # Tailwind classes pertaining to all pill component colors
+        'text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full'
       )
-    end
-
-    private
-
-    def get_color(text)
-      case text
-      when 'fasta'
-        :blue
-      when 'fastq'
-        :green
-      when 'assembly'
-        :pink
-      when 'illumina_pe'
-        :purple
-      else
-        :gray
-      end
     end
   end
 end
