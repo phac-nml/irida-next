9fde28e1bab0cebc9ae7142848f7aa2f747db687
Chris Huynh <chris.huynh333@gmail.com>
2023-10-31 11:38:33 -0500

    Separate pill color helper methods

--- app/helpers/pill_color_helper.rb
+++ app/helpers/pill_color_helper.rb
@@ -13,7 +13,12 @@ module PillColorHelper
       illumina_pe: 'purple'
     }
   }.freeze
-  def find_pill_color_for_attachment(attachment, label_type)
-    attachment.metadata.key?(label_type) ? ATTACHMENT_COLORS[label_type.to_sym][attachment.metadata[label_type].to_sym] : nil
+
+  def find_pill_color_for_format(attachment)
+    attachment.metadata.key?('format') ? ATTACHMENT_COLORS[:format][attachment.metadata['format'].to_sym] : nil
+  end
+
+  def find_pill_color_for_type(attachment)
+    attachment.metadata.key?('type') ? ATTACHMENT_COLORS[:type][attachment.metadata['type'].to_sym] : nil
   end
 end
