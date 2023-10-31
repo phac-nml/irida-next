3fbee4acb627b1d20eef951c3bd70722aaca2af4
Chris Huynh <chris.huynh333@gmail.com>
2023-10-31 10:55:07 -0500

    Refactor viral_pill test further

--- test/components/viral/pill_component_test.rb
+++ test/components/viral/pill_component_test.rb
@@ -7,10 +7,10 @@ module Viral
     test 'default' do
       render_preview(:default)
       assert_selector 'span', count: 3
-      assert_selector '.bg-purple-100.text-purple-800', text: 'This has much more text and is purple'
-      assert_selector '.bg-green-100.text-green-800', text: 'This is green'
-      assert_selector '.bg-blue-100.text-blue-800', text: 'This is blue'
-      assert_selector '.text-xs.font-medium.mr-2.rounded-full', count: 3
+      assert_selector '.bg-purple-100.text-purple-800.text-xs.font-medium.mr-2.rounded-full',
+                      text: 'This has much more text and is purple'
+      assert_selector '.bg-green-100.text-green-800.text-xs.font-medium.mr-2.rounded-full', text: 'This is green'
+      assert_selector '.bg-blue-100.text-blue-800.text-xs.font-medium.mr-2.rounded-full', text: 'This is blue'
     end
 
     test 'with_classes' do
