e5a5c5f97b1a4d8ee48f93f812c012c654151638
Deep Sidhu <deepsidhu85@gmail.com>
2023-11-02 14:43:03 -0500

    Updated translation for updated component to use scope for distance_in_words

--- test/components/updated_component_test.rb
+++ test/components/updated_component_test.rb
@@ -5,20 +5,6 @@ require 'test_helper'
 class UpdatedComponentTest < ViewComponent::TestCase
   include ActionView::Helpers::DateHelper
 
-  test 'renders custom description updated at time ago (less than a minute)' do
-    render_inline(UpdatedComponent.new(description: 'Record updated',
-                                       updated_at: Time.zone.now + 1))
-
-    assert_text 'Record updated less than a minute ago'
-  end
-
-  test 'renders custom description updated at time ago (greater than a minute)' do
-    render_inline(UpdatedComponent.new(description: 'Record updated',
-                                       updated_at: Time.zone.now + 1000))
-
-    assert_text 'Record updated 17 minutes ago'
-  end
-
   test 'renders default updated at time ago (less than a minute)' do
     render_inline(UpdatedComponent.new(
                     updated_at: Time.zone.now + 1
