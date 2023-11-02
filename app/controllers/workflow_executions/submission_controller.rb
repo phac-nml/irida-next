70565c1fad870ed939f5a81460bff3796a277f74
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-11-02 11:43:03 -0500

    chore: Added tests for pipeline selection

--- app/controllers/workflow_executions/submission_controller.rb
+++ app/controllers/workflow_executions/submission_controller.rb
@@ -3,6 +3,13 @@
 module WorkflowExecutions
   # Workflow submission controller
   class SubmissionController < ApplicationController
-    def pipelines; end
+    def pipeline_selection
+      respond_to do |format|
+        format.html { redirect_to dashboard_projects_path }
+        format.turbo_stream do
+          render 'workflow_executions/submissions/pipeline_selection'
+        end
+      end
+    end
   end
 end
--- test/controllers/workflow_executions/submission_controller_test.rb
+++ test/controllers/workflow_executions/submission_controller_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 module WorkflowExecutions
@@ -5,9 +7,17 @@ module WorkflowExecutions
     include Devise::Test::IntegrationHelpers
 
     test 'should redirect to dashboard projects on html request' do
-      sign_in(:john_doe)
-      get workflow_executions_selection_path
+      sign_in users(:john_doe)
+
+      get pipeline_selection_workflow_executions_submission_index_path(format: :html)
       assert_redirected_to dashboard_projects_path
+    end
+
+    test 'should render pipeline selection on turbo stream request' do
+      sign_in users(:john_doe)
+
+      get pipeline_selection_workflow_executions_submission_index_path(format: :turbo_stream)
+      assert_response :ok
     end
   end
 end
