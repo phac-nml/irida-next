75579b90a89af3d035e465b13cb99846848a597d
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2023-11-06 18:55:34 -0600

    remove unneeded exceptions

--- lib/integrations/api_exceptions.rb
+++ lib/integrations/api_exceptions.rb
@@ -6,9 +6,7 @@ module Integrations
     BadRequestError = Class.new(APIExceptionError)
     UnauthorizedError = Class.new(APIExceptionError)
     ForbiddenError = Class.new(APIExceptionError)
-    # ApiRequestsQuotaReachedError = Class.new(APIExceptionError)
     NotFoundError = Class.new(APIExceptionError)
-    # UnprocessableEntityError = Class.new(APIExceptionError)
     ApiError = Class.new(APIExceptionError)
   end
 end
