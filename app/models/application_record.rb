3125ec8bfee0f036928dbce557bd45656fe3d3a6
Deep Sidhu <deepsidhu85@gmail.com>
2023-11-07 14:52:41 -0600

    Begin setting up public activity

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,9 @@
 
 # Base Entity Class
 class ApplicationRecord < ActiveRecord::Base
+  include PublicActivity::Model
+  tracked owner: proc { |controller, _model| controller.current_user }
+
   primary_abstract_class
   self.implicit_order_column = 'created_at'
 end
