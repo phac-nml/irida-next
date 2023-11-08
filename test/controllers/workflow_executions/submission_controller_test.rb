d60a7b343e8e494af74035772922ade25abff5ae
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-11-08 11:06:36 -0600

    test: Updated test for submissions

--- test/controllers/workflow_executions/submission_controller_test.rb
+++ test/controllers/workflow_executions/submission_controller_test.rb
@@ -6,17 +6,10 @@ module WorkflowExecutions
   class SubmissionControllerTest < ActionDispatch::IntegrationTest
     include Devise::Test::IntegrationHelpers
 
-    test 'should redirect to dashboard projects on html request' do
-      sign_in users(:john_doe)
-
-      get pipeline_selection_workflow_executions_submission_index_path(format: :html)
-      assert_redirected_to dashboard_projects_path
-    end
-
     test 'should render pipeline selection on turbo stream request' do
       sign_in users(:john_doe)
 
-      get pipeline_selection_workflow_executions_submission_index_path(format: :turbo_stream)
+      get pipeline_selection_workflow_executions_submissions_path(format: :turbo_stream)
       assert_response :ok
     end
   end
