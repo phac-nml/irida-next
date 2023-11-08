06a78a914f95e2cf223be2eda103b91f2b6bb42a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-11-08 10:14:12 -0600

    chore: Only accept turbo_stream requests in this controller

--- app/controllers/workflow_executions/submission_controller.rb
+++ app/controllers/workflow_executions/submission_controller.rb
@@ -3,9 +3,9 @@
 module WorkflowExecutions
   # Workflow submission controller
   class SubmissionController < ApplicationController
+    respond_to :turbo_stream
     def pipeline_selection
       respond_to do |format|
-        format.html { redirect_to dashboard_projects_path }
         format.turbo_stream do
           render 'workflow_executions/submissions/pipeline_selection'
         end
