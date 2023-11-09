acfe4089459c507ab35852994006eb7cc182e44d
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-11-09 07:37:36 -0600

    test: First submission system test

---
+++ test/system/workflow_executions/submissions_test.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class SubmissionsTest < ApplicationSystemTestCase
+  setup do
+    @user = users(:john_doe)
+    login_as @user
+    @sample1 = samples(:sample1)
+    @sample2 = samples(:sample2)
+    @project = projects(:project1)
+    @namespace = groups(:group_one)
+  end
+
+  test 'should display a pipeline selection modal' do
+    visit namespace_project_samples_url(namespace_id: @namespace.path, project_id: @project.path)
+    click_on I18n.t(:'projects.samples.index.workflows.button_sr')
+    assert_selector 'dialog' do
+      assert_selector '.dialog--header', text: I18n.t(:'workflow_executions.submission.pipeline_selection.title')
+      assert_selector 'button', text: 'Super Awesome Workflow'
+    end
+  end
+end
