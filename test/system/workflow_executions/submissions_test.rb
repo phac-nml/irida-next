565e424c45ced0ac45f32dfe44b2e814091755c3
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-11-20 09:41:02 -0600

    chore: Needed to select a couple samples

--- test/system/workflow_executions/submissions_test.rb
+++ test/system/workflow_executions/submissions_test.rb
@@ -14,6 +14,10 @@ class SubmissionsTest < ApplicationSystemTestCase
 
   test 'should display a pipeline selection modal' do
     visit namespace_project_samples_url(namespace_id: @namespace.path, project_id: @project.path)
+
+    find("input[type='checkbox'][value='#{@sample1.id}']").click
+    find("input[type='checkbox'][value='#{@sample2.id}']").click
+
     click_on I18n.t(:'projects.samples.index.workflows.button_sr')
     assert_selector 'dialog' do
       assert_selector '.dialog--header', text: I18n.t(:'workflow_executions.submission.pipeline_selection.title')
