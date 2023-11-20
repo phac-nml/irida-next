16a28a5c48dc44eb77c1319768d9b63e86c941a1
Chris Huynh <chris.huynh333@gmail.com>
2023-11-20 13:38:36 -0600

    Add ransackable attributes and associations to top level model

--- app/models/project.rb
+++ app/models/project.rb
@@ -29,11 +29,11 @@ class Project < ApplicationRecord
   end
 
   def self.ransackable_attributes(_auth_object = nil)
-    %w[id created_at updated_at]
+    authorizable_ransackable_attributes
   end
 
   def self.ransackable_associations(_auth_object = nil)
-    %w[namespace]
+    authorizable_ransackable_associations & ['namespace']
   end
 
   private
