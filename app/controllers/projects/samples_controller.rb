3117f2f1b7166f2db82efa094c3c81cc653f17d7
Chris Huynh <chris.huynh333@gmail.com>
2023-11-21 10:39:58 -0600

    Refactor samples controller to use load_samples fxn

--- app/controllers/projects/samples_controller.rb
+++ app/controllers/projects/samples_controller.rb
@@ -9,14 +9,14 @@ module Projects
     def index
       authorize! @project, to: :sample_listing?
 
-      @q = Sample.ransack(params[:q])
+      @q = load_samples(@project.id).ransack(params[:q])
       set_default_sort
       respond_to do |format|
         format.html do
-          @has_samples = Sample.exists?(project_id: @project.id)
+          @has_samples = load_samples(@project.id).exists?
         end
         format.turbo_stream do
-          @pagy, @samples = pagy(@q.result.where(id: Sample.where(project_id: @project.id)))
+          @pagy, @samples = pagy(@q.result)
         end
       end
     end
@@ -98,6 +98,10 @@ module Projects
 
     def set_default_sort
       @q.sorts = 'updated_at desc' if @q.sorts.empty?
+    end
+
+    def load_samples(project_id)
+      Sample.where(project_id:)
     end
   end
 end
