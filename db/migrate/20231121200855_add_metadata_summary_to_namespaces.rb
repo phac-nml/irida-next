597f52b2e8e43202a5a8d70b6cd872fac8b98998
Deep Sidhu <deepsidhu85@gmail.com>
2023-11-21 16:39:04 -0600

    Removed duplicated frozen string literal

--- db/migrate/20231121200855_add_metadata_summary_to_namespaces.rb
+++ db/migrate/20231121200855_add_metadata_summary_to_namespaces.rb
@@ -3,7 +3,6 @@
 # migration to add metadata summary to namespaces table
 class AddMetadataSummaryToNamespaces < ActiveRecord::Migration[7.1]
   def change
-    # frozen_string_literal: true
     add_column :namespaces, :metadata_summary, :jsonb, default: {}
   end
 end
