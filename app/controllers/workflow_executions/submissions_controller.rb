084cb38acd8b8dfb63db19dd620016cc013fbd00
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-11-22 12:08:45 -0600

    chore: Display samplesheet

--- app/controllers/workflow_executions/submissions_controller.rb
+++ app/controllers/workflow_executions/submissions_controller.rb
@@ -13,13 +13,39 @@ module WorkflowExecutions
       end
     end
 
+    def show
+      @samples = samples
+      @schema = workflow_schema
+      @workflow = workflow
+      respond_to do |format|
+        format.turbo_stream do
+          render status: :ok
+        end
+      end
+    end
+
     private
+
+    def workflow
+      workflow = Struct.new(:name, :id, :description, :version)
+      workflow.new('Super Awesome Workflow', 1, 'This is a super awesome workflow', '1.0.0')
+    end
 
     def workflows
       workflow = Struct.new(:name, :id, :description, :version)
       awesome_flow = workflow.new('Super Awesome Workflow', 1, 'This is a super awesome workflow', '1.0.0')
       slow_flow = workflow.new('Incredibly Slow Workflow', 2, 'This is a super slow workflow', '0.0.1')
       [awesome_flow, slow_flow]
+    end
+
+    def workflow_schema
+      # Need to get a schema file path from the workflow
+      JSON.parse(Rails.root.join('test/fixtures/files/nextflow/samplesheet_schema.json').read)
+    end
+
+    def samples
+      sample_ids = params[:sample_ids]
+      @samples = Sample.where(id: sample_ids)
     end
   end
 end
