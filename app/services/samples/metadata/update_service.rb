ec88fb4c15ca376046c22378f32c4bc6c756fc75
Chris Huynh <chris.huynh333@gmail.com>
2023-11-23 08:30:29 -0600

    Start execute method

--- app/services/samples/metadata/update_service.rb
+++ app/services/samples/metadata/update_service.rb
@@ -4,20 +4,29 @@ module Samples
   module Metadata
     # Service used to Update Samples::Metadata
     class UpdateService < BaseService
-      attr_accessor :sample, :metadata, :key
+      SampleMetadataUpdateError = Class.new(StandardError)
+      attr_accessor :sample, :metadata, :metadata_key
 
-      def initialize(sample, user = nil, params = {}, metadata = {}, key = nil) # rubocop:disable Metrics/ParameterLists
+      def initialize(sample, user = nil, params = {}, metadata = nil, metadata_key = nil) # rubocop:disable Metrics/ParameterLists
         super(user, params.except(:sample, :id))
         @sample = sample
         @metadata = metadata
-        @key = key
+        @metadata_key = metadata_key
       end
 
-      # def execute
-      #   authorize! sample.project, to: :update_sample?
+      def execute
+        authorize! sample.project, to: :update_sample?
 
-      #   sample.update(params)
-      # end
+        @sample.metadata.merge(@metadata) if @metadata
+
+        if @metadata_key
+          raise SampleMetadataUpdateError, I18n.t('key_does_not_exist') unless @sample.metadata.key?(@metadata_key)
+
+          @sample.metadata.delete(@metadata_key)
+        end
+      rescue Samples::Metadata::UpdateService::SampleMetadataUpdateError
+        @sample.errors.add(:base, e.message)
+      end
     end
   end
 end
