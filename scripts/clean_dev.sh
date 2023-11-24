fab8474a50d4ac36fa3525deafa96c6caa11e79b
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2023-11-24 16:01:53 -0600

    add limit to clean_dev.sh

--- scripts/clean_dev.sh
+++ scripts/clean_dev.sh
@@ -2,5 +2,7 @@
 rm -r storage/*
 # grab any new gems
 bundle
-# delete, create and seed database
-bin/rails info db:drop db:create db:migrate db:seed
+# delete, create and migrate database
+bin/rails info db:drop db:create db:migrate
+# seed db, attachment max set to 50 for quick seed time
+SEED_MAXIMUM_TOTAL_SAMPLE_ATTACHMENTS=50 bin/rails info db:seed
