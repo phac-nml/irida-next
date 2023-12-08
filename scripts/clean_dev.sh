6209f487d3adfdc367b90a769499d845eceee764
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2023-12-08 13:00:32 -0600

    added jobs db + db tier rework

--- scripts/clean_dev.sh
+++ scripts/clean_dev.sh
@@ -2,7 +2,8 @@
 rm -r storage/*
 # grab any new gems
 bundle
-# delete, create and migrate database
+# delete, create and migrate databases
+# includes primary: and jobs: databases
 bin/rails info db:drop db:create db:migrate
 # seed db, attachment max set to 50 for quick seed time
 SEED_MAXIMUM_TOTAL_SAMPLE_ATTACHMENTS=50 bin/rails info db:seed
