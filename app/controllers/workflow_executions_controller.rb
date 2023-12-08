9833cac6a2407ad937e8686b0285e2001d8f7ed2
Eric Enns <492127+ericenns@users.noreply.github.com>
2023-12-08 09:18:23 -0600

    feat: added in create endpoint to workflow executions controller which can be used to create a new workflow execution

--- app/controllers/workflow_executions_controller.rb
+++ app/controllers/workflow_executions_controller.rb
@@ -2,5 +2,43 @@
 
 # Workflow executions controller
 class WorkflowExecutionsController < ApplicationController
-  def index; end
+  def create
+    @workflow_execution = WorkflowExecution.new(workflow_execution_params)
+    @workflow_execution.submitter = current_user
+
+    if @workflow_execution.save
+      render turbo_stream: [], status: :ok
+    else
+      render turbo_stream: [], status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def workflow_execution_params
+    params.require(:workflow_execution).permit(workflow_execution_params_attributes)
+  end
+
+  def workflow_execution_params_attributes
+    [
+      :workflow_type,
+      :workflow_type_version,
+      :workflow_engine,
+      :workflow_engine_version,
+      :workflow_url,
+      { tags: [],
+        metadata: {},
+        workflow_params: {},
+        workflow_engine_parameters: {},
+        samples_workflow_executions_attributes: samples_workflow_execution_params_attributes }
+    ]
+  end
+
+  def samples_workflow_execution_params_attributes
+    [
+      :id,
+      :sample_id,
+      { samplesheet_params: {} }
+    ]
+  end
 end
---
+++ test/controllers/workflow_executions_controller_test.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class WorfklowExecutionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users(:john_doe)
+    @sample1 = samples(:sample1)
+    @attachment1 = attachments(:attachment1)
+  end
+
+  test 'should create workflow execution with valid params' do
+    assert_difference -> { WorkflowExecution.count } => 1,
+                      -> { SamplesWorkflowExecution.count } => 1 do
+      post workflow_executions_path(format: :turbo_stream),
+           params: {
+             workflow_execution: {
+               metadata: { workflow_name: 'irida-next-example', workflow_version: '1.0dev' },
+               workflow_params: { '-r': 'dev' },
+               workflow_type: 'DSL2',
+               workflow_type_version: '22.10.7',
+               tags: [],
+               workflow_engine: 'nextflow',
+               workflow_engine_version: '',
+               workflow_engine_parameters: { engine: 'nextflow', execute_loc: 'azure' },
+               workflow_url: 'https://github.com/phac-nml/iridanextexample',
+               samples_workflow_executions_attributes: [
+                 {
+                   sample_id: @sample1.id,
+                   samplesheet_params: {
+                     sample: "Sample_#{@sample1.id}",
+                     fastq_1: @attachment1.to_global_id,
+                     fastq_2: ''
+                   }
+                 }
+               ]
+             }
+           }
+
+      assert_response :success
+    end
+
+    created_workflow_execution = WorkflowExecution.last
+
+    assert_equal users(:john_doe), created_workflow_execution.submitter
+
+    assert_equal 1, created_workflow_execution.samples_workflow_executions.count
+    assert_equal @sample1, created_workflow_execution.samples_workflow_executions.first.sample
+  end
+end
