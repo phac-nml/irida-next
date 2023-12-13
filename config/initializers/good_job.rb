1dd152b7018bbd26d0a059154b2676f2bda091cf
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2023-12-13 12:19:42 -0600

    remove redundant table assignment

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,8 +1,3 @@
 # frozen_string_literal: true
 
-GoodJob.configure_active_record do
-  connects_to database: :jobs
-  self.table_name_prefix = 'jobs_'
-end
-
 GoodJob.active_record_parent_class = 'JobsRecord'
