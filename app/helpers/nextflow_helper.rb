855687227486e366488563a8d7ae26816026f51d
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-12-14 13:54:04 -0600

    chore: Fixed label name for form elements

--- app/helpers/nextflow_helper.rb
+++ app/helpers/nextflow_helper.rb
@@ -5,15 +5,14 @@ module NextflowHelper
   SCHEMA_PATH = 'test/fixtures/files/nextflow/'
 
   def form_input(container, name, property, required)
-    formatted_name = name.length > 1 ? "--#{name}" : "-#{name}"
-    return checkbox_input(container, formatted_name, property) if property['type'] == 'boolean'
+    return checkbox_input(container, name, property) if property['type'] == 'boolean'
 
     if property['enum'].present?
-      return viral_select(container:, name: formatted_name, options: property['enum'], hidden: property['hidden'],
+      return viral_select(container:, name:, options: property['enum'], hidden: property['hidden'],
                           selected_value: property['default'], help_text: property['help_text'])
     end
 
-    viral_text_input(container:, name: formatted_name, required:, pattern: property['pattern'])
+    viral_text_input(container:, name:, required:, pattern: property['pattern'])
   end
 
   def checkbox_input(_fields, name, property)
@@ -28,19 +27,12 @@ module NextflowHelper
     )
   end
 
-  def file_input(_fields, name, property, required)
-    viral_file_input(
-      label: property['description'],
-      name: "metadata[#{name}]",
-      pattern: property['pattern'],
-      help_text: property['help_text'],
-      hidden: property['hidden'],
-      required:
-    )
-  end
-
   def samplesheet_schema(given_path)
     path = File.basename(given_path)
     JSON.parse(Rails.root.join(SCHEMA_PATH, path).read)
+  end
+
+  def format_name_as_arg(name)
+    name.length > 1 ? "--#{name}" : "-#{name}"
   end
 end
