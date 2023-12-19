9341f64eb136cec9d3b01404a73bf0de74135558
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-12-19 10:49:37 -0600

    chore: Working on workflow executions listing tests

---
+++ test/system/workflow_executions_test.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class WorkflowExecutionsTest < ApplicationSystemTestCase
+  setup do
+    @user = users(:john_doe)
+    login_as @user
+  end
+
+  test 'should display a list of workflow executions' do
+    visit workflow_executions_path
+
+    assert_selector 'h1', text: I18n.t(:'workflow_executions.index.title')
+  end
+end
