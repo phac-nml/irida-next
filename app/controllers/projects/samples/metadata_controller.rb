870adc1bc3a228295de0b2a44c91d3e0836974fe
Chris Huynh <82407232+ChrisHuynh333@users.noreply.github.com>
2023-12-19 22:18:40 -0600

    Sample metadata controller (#330)
    * Start sample metadata controller
    
    * Add test to metadata_controller
    
    * Remove test code
    
    * Add additional tests for sample_metadata_controller
    
    * Cleanup
    
    * delete more testing code
    
    * Move validate metadata away from own fxn

---
+++ app/controllers/projects/samples/metadata_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Projects
+  module Samples
+    # Controller actions for Project Samples Attachments
+    class MetadataController < Projects::Samples::ApplicationController
+      def update # rubocop:disable Metrics/AbcSize
+        authorize! @project, to: :update_sample?
+        respond_to do |format|
+          metadata_fields = ::Samples::Metadata::UpdateService.new(@project, @sample, current_user,
+                                                                   metadata_params).execute
+          if !metadata_fields.nil? && metadata_fields[:updated].count.positive?
+            flash[:success] =
+              t('.success', metadata_fields: metadata_fields[:updated].join(', '), sample_name: @sample.name)
+          end
+          flash[:error] = @sample.errors.full_messages.first if @sample.errors.any?
+          format.turbo_stream { redirect_to(project_path(@project)) }
+        end
+      end
+
+      private
+
+      def metadata_params
+        params.require(:metadata).permit(:analysis_id, metadata: {})
+      end
+    end
+  end
+end
