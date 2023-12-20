f949bfe1fd61e525d6af8eef5240f1c6bbf06787
Deep Sidhu <deepsidhu85@gmail.com>
2023-12-19 23:23:02 -0600

    Added not assert_html5_inputs_valid check after clicking the concatenate button with the basename not matching the pattern

--- test/system/projects/samples_test.rb
+++ test/system/projects/samples_test.rb
@@ -560,6 +560,7 @@ module Projects
         fill_in I18n.t('projects.samples.attachments.concatenations.modal.basename'), with: 'concatenated file'
         check 'Delete originals'
         click_on I18n.t('projects.samples.attachments.concatenations.modal.submit_button')
+        !assert_html5_inputs_valid # rubocop:disable Lint/Void
         fill_in I18n.t('projects.samples.attachments.concatenations.modal.basename'), with: 'concatenated_file'
         click_on I18n.t('projects.samples.attachments.concatenations.modal.submit_button')
         assert_html5_inputs_valid
