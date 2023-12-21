48af86ae30b3a0a4640bf9c11b95e6d3a6c46b7b
Chris Huynh <chris.huynh333@gmail.com>
2023-12-21 16:21:35 -0600

    Fixes to hidden field component

--- app/components/ransack/hidden_sort_field_component.rb
+++ app/components/ransack/hidden_sort_field_component.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module Ransack
-  # Component to render the hidden search field within the ransack search form
-  class HiddenFieldComponent < Component
+  # Component to render the hidden sort field within the ransack search form
+  class HiddenSortFieldComponent < Component
     attr_reader :ransack_obj
 
     def initialize(ransack_obj)
