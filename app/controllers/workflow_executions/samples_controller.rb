63b83cdebff47f4d99134ef4017011fb878fd221
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-12-21 10:52:26 -0600

    chore: Show sample names and the files that were executed

--- app/controllers/workflow_executions/samples_controller.rb
+++ app/controllers/workflow_executions/samples_controller.rb
@@ -5,10 +5,32 @@ module WorkflowExecutions
   class SamplesController < ApplicationController
     layout 'workflow_executions'
     before_action :current_page
-    before_action :workflow
+    before_action :workflow, only: %i[index]
 
     def index
-      @samples = SamplesWorkflowExecution.where(workflow_execution: @workflow)
+      samples = {}
+
+      SamplesWorkflowExecution.where(workflow_execution: @workflow).each do |record|
+        samples[record.sample.id] = record.sample
+      end
+
+      @samples = []
+      @workflow.samples_workflow_executions.each do |params|
+        s = samples[params.sample_id] # actual sample
+
+        files = []
+        params.samplesheet_params.each do |_key, value|
+          matches = value.match(%r{/(\d+)$})
+          next unless matches
+
+          id = matches[1]
+          s.attachments.each do |file|
+            files << file.file if file.file.id == id.to_i
+          end
+        end
+
+        @samples << { sample: s, files: }
+      end
     end
 
     private
