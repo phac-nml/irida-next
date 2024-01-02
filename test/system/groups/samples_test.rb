ce6e8ade139c633295327f0d800083bf221b81ba
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-01-02 14:20:37 -0600

    ading testing for filtering and sorting

--- test/system/groups/samples_test.rb
+++ test/system/groups/samples_test.rb
@@ -7,6 +7,9 @@ module Groups
     def setup
       login_as users(:john_doe)
       @group = groups(:group_one)
+      @sample1 = samples(:sample1)
+      @sample2 = samples(:sample2)
+      @sample9 = samples(:sample9)
     end
 
     test 'visiting the index' do
@@ -71,6 +74,87 @@ module Groups
       visit group_samples_url(@group)
 
       assert_text I18n.t(:'action_policy.policy.group.sample_listing?', name: @group.name)
+    end
+
+    test 'can search the list of samples by name' do
+      visit group_samples_url(@group)
+
+      assert_selector 'table tbody#group-samples-table-body tr', count: 20
+      assert_text @sample1.name
+      assert_text @sample2.name
+
+      fill_in I18n.t(:'groups.samples.index.search.placeholder'), with: 'Sample 1'
+
+      assert_selector 'table tbody#group-samples-table-body tr', count: 13
+      assert_text @sample1.name
+      assert_no_text @sample2.name
+    end
+
+    test 'can sort the list of samples' do
+      visit group_samples_url(@group)
+
+      assert_selector 'table tbody#group-samples-table-body tr', count: 20
+      within first('table tbody#group-samples-table-body tr td') do
+        assert_text @sample1.name
+      end
+
+      click_on I18n.t(:'groups.samples.index.sorting.updated_at_desc')
+      click_on I18n.t(:'groups.samples.index.sorting.name_desc')
+
+      assert_text I18n.t(:'groups.samples.index.sorting.name_desc')
+      assert_selector 'table tbody#group-samples-table-body tr', count: 20
+      within first('table tbody#group-samples-table-body tr:nth-child(3) td') do
+        assert_text @sample9.name
+      end
+    end
+
+    test 'can filter and then sort the list of samples' do
+      visit group_samples_url(@group)
+
+      assert_selector 'table tbody#group-samples-table-body tr', count: 20
+      within first('table tbody#group-samples-table-body tr td') do
+        assert_text @sample1.name
+      end
+
+      fill_in I18n.t(:'groups.samples.index.search.placeholder'), with: 'Sample 1'
+
+      assert_selector 'table tbody#group-samples-table-body tr', count: 13
+      assert_text @sample1.name
+      assert_no_text @sample2.name
+
+      click_on I18n.t(:'groups.samples.index.sorting.updated_at_desc')
+      click_on I18n.t(:'groups.samples.index.sorting.name_desc')
+
+      assert_text I18n.t(:'groups.samples.index.sorting.name_desc')
+      assert_selector 'table tbody#group-samples-table-body tr', count: 13
+      within first('table tbody#group-samples-table-body tr:nth-child(13) td') do
+        assert_text @sample1.name
+      end
+    end
+
+    test 'can sort and then filter the list of samples' do
+      visit group_samples_url(@group)
+
+      assert_selector 'table tbody#group-samples-table-body tr', count: 20
+      within first('table tbody#group-samples-table-body tr td') do
+        assert_text @sample1.name
+      end
+
+      click_on I18n.t(:'groups.samples.index.sorting.updated_at_desc')
+      click_on I18n.t(:'groups.samples.index.sorting.name_desc')
+
+      assert_text I18n.t(:'groups.samples.index.sorting.name_desc')
+      assert_selector 'table tbody#group-samples-table-body tr', count: 20
+      within first('table tbody#group-samples-table-body tr:nth-child(3) td') do
+        assert_text @sample9.name
+      end
+
+      fill_in I18n.t(:'groups.samples.index.search.placeholder'), with: 'Sample 1'
+
+      assert_selector 'table tbody#group-samples-table-body tr', count: 13
+      assert_text @sample1.name
+      assert_no_text @sample2.name
+      assert_no_text @sample9.name
     end
   end
 end
