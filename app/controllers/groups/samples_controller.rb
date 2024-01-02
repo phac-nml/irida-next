7840db1d55f04b51026d514a14a878fc3f310986
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-01-02 09:25:28 -0600

    adding sorting

--- app/controllers/groups/samples_controller.rb
+++ app/controllers/groups/samples_controller.rb
@@ -11,6 +11,8 @@ module Groups
 
       @q = authorized_samples.ransack(params[:q])
 
+      set_default_sort
+
       respond_to do |format|
         format.html do
           @has_samples = @q.result.count.positive?
@@ -44,6 +46,10 @@ module Groups
 
     def current_page
       @current_page = 'samples'
+    end
+
+    def set_default_sort
+      @q.sorts = 'updated_at desc' if @q.sorts.empty?
     end
   end
 end
