f9bde5fbd5705e3c69c0a6a9a4d34eb47b5128ef
Deep Sidhu <deepsidhu85@gmail.com>
2024-01-11 12:08:43 -0600

    Fixed formatting

--- db/migrate/20240111163859_update_member_access_levels.rb
+++ db/migrate/20240111163859_update_member_access_levels.rb
@@ -3,7 +3,8 @@
 # migration to update the access levels for members as a new role `Uploader` was added
 class UpdateMemberAccessLevels < ActiveRecord::Migration[7.1]
   def change
-    members = Member.where(access_level: [Member::AccessLevel::ANALYST - 10, Member::AccessLevel::MAINTAINER - 10,
+    members = Member.where(access_level: [Member::AccessLevel::ANALYST - 10,
+                                          Member::AccessLevel::MAINTAINER - 10,
                                           Member::AccessLevel::OWNER - 10])
 
     members.each do |member|
