c82fbe59be078e451156c5a996a23c8319aba1b2
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-01-11 18:39:53 -0600

    cleanup, comments, fixes

--- app/graphql/types/workflow_execution_type.rb
+++ app/graphql/types/workflow_execution_type.rb
@@ -2,25 +2,25 @@
 
 module Types
   # WorkflowExecutions Type
-  class WorkflowExecutionType < Types::BaseObject # rubocop:disable convention:GraphQL/ExtractType
+  class WorkflowExecutionType < Types::BaseObject
     implements GraphQL::Types::Relay::Node
     description 'A workflow execution'
 
     # TODO: should fields like this have extra types or is an array of strings good enough?
-    field :metadata, [String], null: true, description: 'todo'
-    field :run_id, String, null: true, description: 'todo'
-    field :state, String, null: true, description: 'todo'
-    field :submitter, UserType, null: false, description: 'todo'
-    field :submitter_id, String, null: true, description: 'todo'
-    field :tags, [String], null: true, description: 'todo'
+    field :metadata, [String], null: true, description: 'WorkflowExecution metadata'
+    field :run_id, String, null: true, description: 'WorkflowExecution run id'
+    field :state, String, null: true, description: 'WorkflowExecution state'
+    field :submitter, UserType, null: false, description: 'WorkflowExecution submitter (User)'
+    field :submitter_id, String, null: true, description: 'WorkflowExecution submitter_id'
+    field :tags, [String], null: true, description: 'WorkflowExecution tags'
 
-    field :workflow_engine, String, null: true, description: 'todo'
-    field :workflow_engine_parameters, [String], null: true, description: 'todo'
-    field :workflow_engine_version, String, null: true, description: 'todo'
-    field :workflow_params, [String], null: true, description: 'todo'
-    field :workflow_type, String, null: true, description: 'todo'
-    field :workflow_type_version, String, null: true, description: 'todo'
-    field :workflow_url, String, null: true, description: 'todo'
+    field :workflow_engine, String, null: true, description: 'WorkflowExecution workflow engine'
+    field :workflow_engine_parameters, [String], null: true, description: 'WorkflowExecution workflow engine parameters'
+    field :workflow_engine_version, String, null: true, description: 'WorkflowExecution workflow engine version' # rubocop:disable GraphQL/ExtractType
+    field :workflow_params, [String], null: true, description: 'WorkflowExecution params'
+    field :workflow_type, String, null: true, description: 'WorkflowExecution type'
+    field :workflow_type_version, String, null: true, description: 'WorkflowExecution type version'
+    field :workflow_url, String, null: true, description: 'WorkflowExecution url' # rubocop:disable GraphQL/ExtractType
 
     field :samples,
           SampleType.connection_type,
@@ -30,13 +30,12 @@ module Types
           resolver: Resolvers::SamplesWorkflowExecutionsResolver
 
     def self.authorized?(object, context)
-      super # TODO: add additional authorization?
-      # super &&
-      #   allowed_to?(
-      #     :read?,
-      #     object,
-      #     context: { user: context[:current_user] }
-      #   )
+      super &&
+        allowed_to?(
+          :read?,
+          object.submitter, # TODO: Should we display only for submitter? Should a WorkflowExecution policy be made?
+          context: { user: context[:current_user] }
+        )
     end
   end
 end
