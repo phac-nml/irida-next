c0f9ab5113197f5286bfac2e71e7d3902cde5eaf
Chris Huynh <chris.huynh333@gmail.com>
2024-01-15 13:34:09 -0600

    Add attachments upload, concate, delete files buttons

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -24,7 +24,7 @@ class Sample < ApplicationRecord
   end
 
   def self.ransackable_attributes(_auth_object = nil)
-    %w[id name created_at updated_at metadata]
+    %w[id name created_at updated_at]
   end
 
   def self.ransackable_associations(_auth_object = nil)
