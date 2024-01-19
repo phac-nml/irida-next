35291ee828f5f0e6b45b15d66d28548cf6e0b4ce
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-01-19 07:25:23 -0600

    chore: added in test to retrieve metadata for keys not present

--- test/graphql/samples_query_test.rb
+++ test/graphql/samples_query_test.rb
@@ -91,4 +91,20 @@ class SamplesQueryTest < ActiveSupport::TestCase
     assert data['metadata'].key?('metadatafield1')
     assert_not data['metadata'].key?('metadatafield2')
   end
+
+  test 'sample and metadata fields query with non-existing keys should return empty metadata hash' do
+    result = IridaSchema.execute(SAMPLE_AND_LIMITED_METADATA_QUERY,
+                                 context: { current_user: @user },
+                                 variables: { sample_id: @sample32.to_global_id.to_s, keys: ['nometadatafield'] })
+
+    assert_nil result['errors'], 'should work and have no errors.'
+
+    data = result['data']['sample']
+
+    assert_not_empty data, 'sample type should work'
+    assert_not_equal @sample32.metadata, data['metadata']
+    assert_not data['metadata'].key?('metadatafield1')
+    assert_not data['metadata'].key?('metadatafield2')
+    assert_empty data['metadata']
+  end
 end
