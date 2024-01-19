7ff737e9f274afa854a2093b02697820cb03bff0
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-01-19 15:45:28 -0600

    fixing rebasing mistake

--- test/services/samples/metadata/file_import_service_test.rb
+++ test/services/samples/metadata/file_import_service_test.rb
@@ -11,10 +11,7 @@ module Samples
         @project = projects(:project1)
         @sample1 = samples(:sample1)
         @sample2 = samples(:sample2)
-
-        @csv = File.open('test/fixtures/files/metadata/valid.csv', 'r')
-        @sample1 = samples(:sample1)
-        @sample2 = samples(:sample2)
+        @csv = File.new('test/fixtures/files/metadata/valid.csv', 'r')
       end
 
       test 'import sample metadata with permission' do
@@ -165,46 +162,6 @@ module Samples
         csv = File.new('test/fixtures/files/metadata/mixed_project_samples.csv', 'r')
         params = { file: csv, sample_id_column: 'sample_name' }
         response = Samples::Metadata::FileImportService.new(@project, @john_doe, params).execute
-        assert_equal({ @sample1.name => { added: %w[metadatafield1 metadatafield2 metadatafield3],
-                                          updated: [], deleted: [], not_updated: [] } }, response)
-        assert_equal("Sample 'Project 2 Sample 1' is not found within this project",
-                     @project.errors.full_messages_for(:sample).first)
-        assert_equal({ 'metadatafield1' => '10', 'metadatafield2' => '20', 'metadatafield3' => '30' },
-                     @sample1.reload.metadata)
-        assert_equal({}, @sample2.reload.metadata)
-      end
-
-      test 'import sample metadata with empty values set to true' do
-        sample32 = samples(:sample32)
-        project29 = projects(:project29)
-        csv = File.new('test/fixtures/files/metadata/contains_empty_values.csv', 'r')
-        params = { file: csv, sample_id_column: 'sample_name', ignore_empty_values: true }
-        response = Samples::Metadata::FileImportService.new(project29, @john_doe,
-                                                            params).execute
-        assert_equal({ sample32.name => { added: ['metadatafield3'], updated: ['metadatafield2'],
-                                          deleted: [], not_updated: [] } }, response)
-        assert_equal({ 'metadatafield1' => 'value1', 'metadatafield2' => '20', 'metadatafield3' => '30' },
-                     sample32.reload.metadata)
-      end
-
-      test 'import sample metadata with empty values set to false' do
-        sample32 = samples(:sample32)
-        project29 = projects(:project29)
-        csv = File.new('test/fixtures/files/metadata/contains_empty_values.csv', 'r')
-        params = { file: csv, sample_id_column: 'sample_name', ignore_empty_values: false }
-        response = Samples::Metadata::FileImportService.new(project29, @john_doe,
-                                                            params).execute
-        assert_equal({ sample32.name => { added: ['metadatafield3'], updated: ['metadatafield2'],
-                                          deleted: ['metadatafield1'], not_updated: [] } }, response)
-        assert_equal({ 'metadatafield2' => '20', 'metadatafield3' => '30' }, sample32.reload.metadata)
-      end
-
-      test 'import sample metadata with a sample that does not belong to project' do
-        csv = File.new('test/fixtures/files/metadata/mixed_project_samples.csv', 'r')
-        params = { file: csv, sample_id_column: 'sample_name' }
-        response = Samples::Metadata::FileImportService.new(@project, @john_doe,
-                                                            params).execute
-
         assert_equal({ @sample1.name => { added: %w[metadatafield1 metadatafield2 metadatafield3],
                                           updated: [], deleted: [], not_updated: [] } }, response)
         assert_equal("Sample 'Project 2 Sample 1' is not found within this project",
