2d5c740b84e01974c2c5e838ba83775445389491
Deep Sidhu <deepsidhu85@gmail.com>
2024-01-19 09:22:39 -0600

    Updated error message when a project is not selected to transfer samples to and the user clicks submit. Added disabled attribute to submit button in transfer samples modal.

--- app/controllers/projects/samples/transfers_controller.rb
+++ app/controllers/projects/samples/transfers_controller.rb
@@ -32,7 +32,7 @@ module Projects
                  locals: { sample_ids: transferred_samples_ids, type: :alert,
                            message: t('.error'), errors: @errors }
         else
-          @errors = @project.errors.full_messages_for(:base)
+          @errors = @project.errors.full_messages_for(:base).first
           render status: :unprocessable_entity,
                  locals: { sample_ids: [], type: :alert,
                            message: @errors, errors: [] }
