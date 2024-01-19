bdcbaeea699cf2be417832b63f28f120267f4184
Katherine Thiessen <ksierks@users.noreply.github.com>
2024-01-19 14:32:48 -0600

    Metadata file import service (#373)
    * building a skeleton for the new Samples::Metadata::FileImportService
    
    * adding tests to check that user can modify project
    
    * adding params and file extension validation
    
    * using roo as spreadsheet parser
    
    * finishing file validation
    
    * starting to call sample metadata update service
    
    * cleaning leading & trailing whitespaces
    
    * returing all the responses back from the sample metadata update service
    
    * applying the ignore_empty_values variable ad described in the issue
    
    * testing the ignore_empty_values variable
    
    * adding some basic error handling when a sample cannot be found
    
    * testing responses
    
    * fixing linting error
    
    * adding project sample not found error
    
    * fixing broken tests from last merge
    
    * changing some variables
    
    * creating sample errors
    
    * using different test fixtures
    
    * code review changes
    
    * adding assertions
    
    * updating translations
    
    * renaming a variable again

---
+++ app/services/samples/metadata/file_import_service.rb
@@ -0,0 +1,120 @@
+# frozen_string_literal: true
+
+require 'roo'
+
+module Samples
+  module Metadata
+    # Service used to import sample metadata via a file
+    class FileImportService < BaseService
+      SampleMetadataFileImportError = Class.new(StandardError)
+
+      def initialize(project, user = nil, params = {})
+        super(user, params)
+        @project = project
+        @file = params[:file]
+        @sample_id_column = params[:sample_id_column]
+        @ignore_empty_values = params[:ignore_empty_values]
+        @spreadsheet = nil
+        @headers = nil
+      end
+
+      def execute
+        authorize! @project, to: :update_sample?
+
+        validate_sample_id_column
+
+        validate_file
+
+        perform_file_import
+      rescue Samples::Metadata::FileImportService::SampleMetadataFileImportError => e
+        @project.errors.add(:base, e.message)
+        {}
+      end
+
+      private
+
+      def validate_sample_id_column
+        return unless @sample_id_column.nil?
+
+        raise SampleMetadataFileImportError,
+              I18n.t('services.samples.metadata.import_file.empty_sample_id_column')
+      end
+
+      def validate_file_extension
+        file_extension = File.extname(@file).downcase
+
+        return if %w[.csv .xls .xlsx].include?(file_extension)
+
+        raise SampleMetadataFileImportError,
+              I18n.t('services.samples.metadata.import_file.invalid_file_extension')
+      end
+
+      def validate_file_headers
+        duplicate_headers = @headers.find_all { |header| @headers.count(header) > 1 }.uniq
+        unless duplicate_headers.empty?
+          raise SampleMetadataFileImportError,
+                I18n.t('services.samples.metadata.import_file.duplicate_column_names')
+        end
+
+        unless @headers.include?(@sample_id_column)
+          raise SampleMetadataFileImportError,
+                I18n.t('services.samples.metadata.import_file.missing_sample_id_column')
+        end
+
+        return if @headers.count { |header| header != @sample_id_column } > 1
+
+        raise SampleMetadataFileImportError,
+              I18n.t('services.samples.metadata.import_file.missing_metadata_column')
+      end
+
+      def validate_file_rows
+        first_row = @spreadsheet.row(2)
+        return unless first_row.compact.empty?
+
+        raise SampleMetadataFileImportError,
+              I18n.t('services.samples.metadata.import_file.missing_metadata_row')
+      end
+
+      def validate_file
+        if @file.nil?
+          raise SampleMetadataFileImportError,
+                I18n.t('services.samples.metadata.import_file.empty_file')
+        end
+
+        validate_file_extension
+
+        @spreadsheet = Roo::Spreadsheet.open(@file)
+        @headers = @spreadsheet.row(1).collect(&:strip)
+
+        validate_file_headers
+
+        validate_file_rows
+      end
+
+      def perform_file_import # rubocop:disable Metrics/MethodLength
+        response = {}
+        parse_settings = @headers.zip(@headers).to_h
+
+        @spreadsheet.each_with_index(parse_settings) do |metadata, index|
+          next unless index.positive?
+
+          sample_id = metadata[@sample_id_column]
+          sample = Sample.find_by(name: sample_id, project_id: @project.id)
+
+          metadata.delete(@sample_id_column)
+          metadata.compact! if @ignore_empty_values
+          begin
+            response[sample_id] =
+              UpdateService.new(@project, sample, current_user, { 'metadata' => metadata }).execute
+          rescue StandardError
+            @project.errors.add(:sample,
+                                I18n.t('services.samples.metadata.import_file.sample_not_found',
+                                       sample_name: sample_id))
+            next
+          end
+        end
+        response
+      end
+    end
+  end
+end
---
+++ test/services/samples/metadata/file_import_service_test.rb
@@ -0,0 +1,175 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Samples
+  module Metadata
+    class FileImportServiceTest < ActiveSupport::TestCase
+      def setup
+        @john_doe = users(:john_doe)
+        @jane_doe = users(:jane_doe)
+        @project = projects(:project1)
+        @sample1 = samples(:sample1)
+        @sample2 = samples(:sample2)
+        @csv = File.new('test/fixtures/files/metadata/valid.csv', 'r')
+      end
+
+      test 'import sample metadata with permission' do
+        assert_authorized_to(:update_sample?, @project,
+                             with: ProjectPolicy,
+                             context: { user: @john_doe }) do
+          params = { file: @csv, sample_id_column: 'sample_name' }
+          Samples::Metadata::FileImportService.new(@project, @john_doe, params).execute
+        end
+      end
+
+      test 'import sample metadata without permission' do
+        assert_raises(ActionPolicy::Unauthorized) do
+          params = { file: @csv, sample_id_column: 'sample_name' }
+          Samples::Metadata::FileImportService.new(@project, @jane_doe, params).execute
+        end
+      end
+
+      test 'import sample metadata with empty params' do
+        assert_empty Samples::Metadata::FileImportService.new(@project, @john_doe, {}).execute
+        assert_equal(@project.errors.full_messages_for(:base).first,
+                     I18n.t('services.samples.metadata.import_file.empty_sample_id_column'))
+      end
+
+      test 'import sample metadata with no file' do
+        params = { sample_id_column: 'sample_name' }
+        assert_empty Samples::Metadata::FileImportService.new(@project, @john_doe, params).execute
+        assert_equal(@project.errors.full_messages_for(:base).first,
+                     I18n.t('services.samples.metadata.import_file.empty_file'))
+      end
+
+      test 'import sample metadata via csv file' do
+        assert_equal({}, @sample1.metadata)
+        assert_equal({}, @sample2.metadata)
+        params = { file: @csv, sample_id_column: 'sample_name' }
+        response = Samples::Metadata::FileImportService.new(@project, @john_doe,
+                                                            params).execute
+        assert_equal({ @sample1.name => { added: %w[metadatafield1 metadatafield2 metadatafield3],
+                                          updated: [], deleted: [], not_updated: [] },
+                       @sample2.name => { added: %w[metadatafield1 metadatafield2 metadatafield3],
+                                          updated: [], deleted: [], not_updated: [] } }, response)
+        assert_equal({ 'metadatafield1' => '10', 'metadatafield2' => '20', 'metadatafield3' => '30' },
+                     @sample1.reload.metadata)
+        assert_equal({ 'metadatafield1' => '15', 'metadatafield2' => '25', 'metadatafield3' => '35' },
+                     @sample2.reload.metadata)
+      end
+
+      test 'import sample metadata via xls file' do
+        assert_equal({}, @sample1.metadata)
+        assert_equal({}, @sample2.metadata)
+        xls = File.new('test/fixtures/files/metadata/valid.xls', 'r')
+        params = { file: xls, sample_id_column: 'sample_name' }
+        response = Samples::Metadata::FileImportService.new(@project, @john_doe, params).execute
+        assert_equal({ @sample1.name => { added: %w[metadatafield1 metadatafield2 metadatafield3],
+                                          updated: [], deleted: [], not_updated: [] },
+                       @sample2.name => { added: %w[metadatafield1 metadatafield2 metadatafield3],
+                                          updated: [], deleted: [], not_updated: [] } }, response)
+        assert_equal({ 'metadatafield1' => 10, 'metadatafield2' => 20, 'metadatafield3' => 30 },
+                     @sample1.reload.metadata)
+        assert_equal({ 'metadatafield1' => 15, 'metadatafield2' => 25, 'metadatafield3' => 35 },
+                     @sample2.reload.metadata)
+      end
+
+      test 'import sample metadata via xlsx file' do
+        assert_equal({}, @sample1.metadata)
+        assert_equal({}, @sample2.metadata)
+        xlsx = File.new('test/fixtures/files/metadata/valid.xlsx', 'r')
+        params = { file: xlsx, sample_id_column: 'sample_name' }
+        response = Samples::Metadata::FileImportService.new(@project, @john_doe, params).execute
+        assert_equal({ @sample1.name => { added: %w[metadatafield1 metadatafield2 metadatafield3],
+                                          updated: [], deleted: [], not_updated: [] },
+                       @sample2.name => { added: %w[metadatafield1 metadatafield2 metadatafield3],
+                                          updated: [], deleted: [], not_updated: [] } }, response)
+        assert_equal({ 'metadatafield1' => 10, 'metadatafield2' => 20, 'metadatafield3' => 30 },
+                     @sample1.reload.metadata)
+        assert_equal({ 'metadatafield1' => 15, 'metadatafield2' => 25, 'metadatafield3' => 35 },
+                     @sample2.reload.metadata)
+      end
+
+      test 'import sample metadata via other file' do
+        other = File.new('test/fixtures/files/metadata/invalid.txt', 'r')
+        params = { file: other, sample_id_column: 'sample_name' }
+        assert_empty Samples::Metadata::FileImportService.new(@project, @john_doe, params).execute
+        assert_equal(@project.errors.full_messages_for(:base).first,
+                     I18n.t('services.samples.metadata.import_file.invalid_file_extension'))
+      end
+
+      test 'import sample metadata with no sample_id_column' do
+        csv = File.new('test/fixtures/files/metadata/missing_sample_id_column.csv', 'r')
+        params = { file: csv, sample_id_column: 'sample_name' }
+        assert_empty Samples::Metadata::FileImportService.new(@project, @john_doe, params).execute
+        assert_equal(@project.errors.full_messages_for(:base).first,
+                     I18n.t('services.samples.metadata.import_file.missing_sample_id_column'))
+      end
+
+      test 'import sample metadata with duplicate column names' do
+        csv = File.new('test/fixtures/files/metadata/duplicate_headers.csv', 'r')
+        params = { file: csv, sample_id_column: 'sample_name' }
+        assert_empty Samples::Metadata::FileImportService.new(@project, @john_doe, params).execute
+        assert_equal(@project.errors.full_messages_for(:base).first,
+                     I18n.t('services.samples.metadata.import_file.duplicate_column_names'))
+      end
+
+      test 'import sample metadata with no metadata columns' do
+        csv = File.new('test/fixtures/files/metadata/missing_metadata_columns.csv', 'r')
+        params = { file: csv, sample_id_column: 'sample_name' }
+        assert_empty Samples::Metadata::FileImportService.new(@project, @john_doe, params).execute
+        assert_equal(@project.errors.full_messages_for(:base).first,
+                     I18n.t('services.samples.metadata.import_file.missing_metadata_column'))
+      end
+
+      test 'import sample metadata with no metadata rows' do
+        csv = File.new('test/fixtures/files/metadata/missing_metadata_rows.csv', 'r')
+        params = { file: csv, sample_id_column: 'sample_name' }
+        assert_empty Samples::Metadata::FileImportService.new(@project, @john_doe, params).execute
+        assert_equal(@project.errors.full_messages_for(:base).first,
+                     I18n.t('services.samples.metadata.import_file.missing_metadata_row'))
+      end
+
+      test 'import sample metadata with empty values set to true' do
+        sample32 = samples(:sample32)
+        project29 = projects(:project29)
+        assert_equal({ 'metadatafield1' => 'value1', 'metadatafield2' => 'value2' }, sample32.metadata)
+        csv = File.new('test/fixtures/files/metadata/contains_empty_values.csv', 'r')
+        params = { file: csv, sample_id_column: 'sample_name', ignore_empty_values: true }
+        response = Samples::Metadata::FileImportService.new(project29, @john_doe, params).execute
+        assert_equal({ sample32.name => { added: ['metadatafield3'], updated: ['metadatafield2'],
+                                          deleted: [], not_updated: [] } }, response)
+        assert_equal({ 'metadatafield1' => 'value1', 'metadatafield2' => '20', 'metadatafield3' => '30' },
+                     sample32.reload.metadata)
+      end
+
+      test 'import sample metadata with empty values set to false' do
+        sample32 = samples(:sample32)
+        project29 = projects(:project29)
+        assert_equal({ 'metadatafield1' => 'value1', 'metadatafield2' => 'value2' }, sample32.metadata)
+        csv = File.new('test/fixtures/files/metadata/contains_empty_values.csv', 'r')
+        params = { file: csv, sample_id_column: 'sample_name', ignore_empty_values: false }
+        response = Samples::Metadata::FileImportService.new(project29, @john_doe, params).execute
+        assert_equal({ sample32.name => { added: ['metadatafield3'], updated: ['metadatafield2'],
+                                          deleted: ['metadatafield1'], not_updated: [] } }, response)
+        assert_equal({ 'metadatafield2' => '20', 'metadatafield3' => '30' }, sample32.reload.metadata)
+      end
+
+      test 'import sample metadata with a sample that does not belong to project' do
+        assert_equal({}, @sample1.metadata)
+        assert_equal({}, @sample2.metadata)
+        csv = File.new('test/fixtures/files/metadata/mixed_project_samples.csv', 'r')
+        params = { file: csv, sample_id_column: 'sample_name' }
+        response = Samples::Metadata::FileImportService.new(@project, @john_doe, params).execute
+        assert_equal({ @sample1.name => { added: %w[metadatafield1 metadatafield2 metadatafield3],
+                                          updated: [], deleted: [], not_updated: [] } }, response)
+        assert_equal("Sample 'Project 2 Sample 1' is not found within this project",
+                     @project.errors.full_messages_for(:sample).first)
+        assert_equal({ 'metadatafield1' => '10', 'metadatafield2' => '20', 'metadatafield3' => '30' },
+                     @sample1.reload.metadata)
+        assert_equal({}, @sample2.reload.metadata)
+      end
+    end
+  end
+end
