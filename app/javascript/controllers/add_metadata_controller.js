39dd5a5625a4c8e0e6f326babaf7198909612ae7
Chris Huynh <chris.huynh333@gmail.com>
2024-01-25 11:11:41 -0600

    Change add metadata model fields to use floating labels

--- app/javascript/controllers/add_metadata_controller.js
+++ app/javascript/controllers/add_metadata_controller.js
@@ -6,13 +6,34 @@ export default class extends Controller {
         const inputFields = document.getElementsByClassName('inputField')
         const lastInputField = inputFields[inputFields.length - 1]
         const nextFieldId = parseInt(lastInputField.id.split('-')[2]) + 1
-
         const newField =
-            `<div id="input-field-${nextFieldId}" class="flex space-x-2 mb-2 inputField">
-                <input type="text" id="sample_key_${nextFieldId}">
-                <input type="text" id="sample_value_${nextFieldId}">
-            </div>`
+            `<div id="field_label_${nextFieldId}" class="text-slate-700 dark:text-slate-400 text-sm">
+    Field ${nextFieldId + 1}:
+    </div>
+
+    <div id='input-field-${nextFieldId}' class="flex space-x-2 mb-2 inputField">
+      <div class="relative z-0 w-full group">
+      <input type="text" name="key_${nextFieldId}" id="key_${nextFieldId}" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary-500 focus:outline-none focus:ring-0 focus:border-primary-500 peer" placeholder=" " required />
+      <label for="key_${nextFieldId}" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-primary-500 peer-focus:dark:text-primary-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Key</label>
+
+    </div>
+    <div class="relative z-0 w-full group">
+      <input type="text" name="value_${nextFieldId}" id="value_${nextFieldId}" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary-500 focus:outline-none focus:ring-0 focus:border-primary-500 peer" placeholder=" " required />
+      <label for="value_${nextFieldId}" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-primary-500 peer-focus:dark:text-primary-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Value</label>
+    </div>
+    <button type="button" data-action="add-metadata#removeField" class="ml-auto bg-white text-slate-400 hover:text-slate-900 rounded-lg focus:ring-2 focus:ring-slate-300 p-1.5 hover:bg-slate-100 inline-flex items-center justify-center h-8 w-8 dark:text-slate-500 dark:hover:text-white dark:bg-slate-800 dark:hover:bg-slate-700" data-dismiss-target="#toast-success" aria-label="Close">
+      <span class="h-5 w-5 Viral-Icon">
+    <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="Viral-Icon__Svg icon-x_mark" focusable="false">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+</svg>
+
+</span>
+    </button>`
         lastInputField.insertAdjacentHTML("afterend", newField)
+    }
+
+    removeField() {
+        console.log('hello remove')
     }
 
     buildMetadata() {
