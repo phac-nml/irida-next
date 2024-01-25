48245511696c177b0eeb84245c6d4e6ac747adba
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-01-25 16:22:57 -0600

    graphql/attach_files_to_samples

---
+++ app/graphql/mutations/attach_files_to_sample.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Mutations
+  # Mutation that attaches files to sample
+  class AttachFilesToSample < BaseMutation
+    description 'Attaches files to sample.'
+    argument :files, [String], required: true, description: 'A list of files to attach to the sample'
+    argument :sample_id, ID,
+             required: true,
+             description: 'The Node ID of the sample to be updated.'
+
+    field :errors, [String], null: false, description: 'A list of errors that prevented the mutation.'
+    field :sample, Types::SampleType, null: false, description: 'The updated sample.'
+    field :status, GraphQL::Types::JSON, null: false, description: 'The status of the mutation.'
+
+    def resolve(sample_id:, files:)
+      sample = IridaSchema.object_from_id(sample_id, { expected_type: Sample })
+      files_attached = Attachments::CreateService.new(current_user, sample, { 'files' => files }).execute
+      {
+        sample:,
+        status: files_attached,
+        errors: sample.errors.full_messages
+      }
+    end
+
+    def ready?(**_args)
+      authorize!(to: :mutate?, with: GraphqlPolicy, context: { user: context[:current_user], token: context[:token] })
+    end
+  end
+end
