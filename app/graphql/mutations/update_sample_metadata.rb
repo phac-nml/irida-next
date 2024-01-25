1331386e2d114d1c829a0705bfdcb8a7c85d968e
Katherine Thiessen <ksierks@users.noreply.github.com>
2024-01-25 09:08:40 -0600

    Graphql update sample metadata mutation (#388)
    * starting to create and add the new update sample metadata mutation
    
    * fixing node id and starting to write tests
    
    * updating graphql schema
    
    * adding an additional test to check no permission

---
+++ app/graphql/mutations/update_sample_metadata.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Mutations
+  # Mutation that updates sample metadata
+  class UpdateSampleMetadata < BaseMutation
+    description 'Update metadata for a sample.'
+    argument :metadata, GraphQL::Types::JSON, required: true, description: 'The metadata to update the sample with.'
+    argument :sample_id, ID,
+             required: true,
+             description: 'The Node ID of the sample to be updated.'
+
+    field :errors, [String], null: false, description: 'A list of errors that prevented the mutation.'
+    field :sample, Types::SampleType, null: false, description: 'The updated sample.'
+    field :status, GraphQL::Types::JSON, null: false, description: 'The status of the mutation.'
+
+    def resolve(sample_id:, metadata:)
+      sample = IridaSchema.object_from_id(sample_id, { expected_type: Sample })
+      metadata_changes = Samples::Metadata::UpdateService.new(sample.project, sample, current_user,
+                                                              { 'metadata' => metadata }).execute
+      {
+        sample:,
+        status: metadata_changes,
+        errors: sample.errors.full_messages
+      }
+    end
+
+    def ready?(**_args)
+      authorize!(to: :mutate?, with: GraphqlPolicy, context: { user: context[:current_user], token: context[:token] })
+    end
+  end
+end
---
+++ test/graphql/update_sample_metadata_test.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UpdateSampleMetadataMutationTest < ActiveSupport::TestCase
+  UPDATE_SAMPLE_METADATA_MUTATION = <<~GRAPHQL
+    mutation($sampleId: ID!, $metadata: JSON!) {
+      updateSampleMetadata(input: { sampleId: $sampleId, metadata: $metadata }) {
+        sample {
+          id,
+          name,
+          description,
+          metadata
+        },
+        status,
+        errors
+      }
+    }
+  GRAPHQL
+
+  def setup
+    @user = users(:john_doe)
+    @api_scope_token = personal_access_tokens(:john_doe_valid_pat)
+    @read_api_scope_token = personal_access_tokens(:john_doe_valid_read_pat)
+    @sample = samples(:sample1)
+  end
+
+  test 'updateSampleMetadata mutation should work with valid params and api scope token' do
+    result = IridaSchema.execute(UPDATE_SAMPLE_METADATA_MUTATION,
+                                 context: { current_user: @user, token: @api_scope_token },
+                                 variables: { sampleId: @sample.to_global_id.to_s,
+                                              metadata: { key1: 'value1' } })
+
+    assert_nil result['errors'], 'should work and have no errors.'
+
+    data = result['data']['updateSampleMetadata']
+
+    assert_not_empty data, 'updateSampleMetadata should be populated when no authorization errors'
+    assert_empty data['errors']
+    assert_not_empty data['status']
+    assert_not_empty data['status'][:added]
+    assert_equal 'key1', data['status'][:added].first
+    assert_not_empty data['sample']
+    assert_not_empty data['sample']['metadata']
+    assert_not_empty data['sample']['metadata']['key1']
+    assert_equal 'value1', data['sample']['metadata']['key1']
+  end
+
+  test 'updateSampleMetadata mutation should error with empty metadata params and api scope token' do
+    result = IridaSchema.execute(UPDATE_SAMPLE_METADATA_MUTATION,
+                                 context: { current_user: @user, token: @api_scope_token },
+                                 variables: { sampleId: @sample.to_global_id.to_s,
+                                              metadata: {} })
+
+    assert_nil result['errors'], 'should work and have no errors.'
+
+    data = result['data']['updateSampleMetadata']
+
+    assert_not_empty data, 'updateSampleMetadata should be populated when no authorization errors'
+    assert_empty data['status'][:added], 'status added changes should be empty as the sample was not updated.'
+    assert_empty data['status'][:updated], 'status updated changes should be empty as the sample was not updated.'
+    assert_empty data['status'][:deleted], 'status deleted changes should be empty as the sample was not updated.'
+    assert_empty data['status'][:not_updated],
+                 'status not_updated changes should be empty as the sample was not updated.'
+    assert_not_empty data['errors']
+    assert_equal I18n.t('services.samples.metadata.empty_metadata', sample_name: @sample.name), data['errors'][0]
+  end
+
+  test 'updateSampleMetadata mutation should not work with valid params and read api scope token' do
+    result = IridaSchema.execute(UPDATE_SAMPLE_METADATA_MUTATION,
+                                 context: { current_user: @user, token: @read_api_scope_token },
+                                 variables: { sampleId: @sample.to_global_id.to_s,
+                                              metadata: { key1: 'value1' } })
+
+    assert_not_nil result['errors'], 'shouldn\'t work and have errors.'
+
+    error_message = result['errors'][0]['message']
+
+    assert_equal 'You are not authorized to perform this action', error_message
+  end
+
+  test 'updateSampleMetadata mutation should not work with valid params and no permission' do
+    user = users(:jane_doe)
+    api_scope_token = personal_access_tokens(:jane_doe_valid_pat)
+
+    result = IridaSchema.execute(UPDATE_SAMPLE_METADATA_MUTATION,
+                                 context: { current_user: user, token: api_scope_token },
+                                 variables: { sampleId: @sample.to_global_id.to_s,
+                                              metadata: { key1: 'value1' } })
+
+    assert_not_nil result['errors'], 'shouldn\'t work and have errors.'
+
+    error_message = result['errors'][0]['message']
+
+    assert_equal I18n.t(:'action_policy.policy.project.update_sample?', name: @sample.project.name), error_message
+  end
+end
