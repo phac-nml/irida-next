61310488b10bce7efb99629d436810e6dba6f967
Deep Sidhu <deepsidhu85@gmail.com>
2024-01-29 14:54:12 -0600

    Updated history concern and history views to take into account records that are deleted and then restored

--- app/models/concerns/history.rb
+++ app/models/concerns/history.rb
@@ -5,11 +5,15 @@ module History
   extend ActiveSupport::Concern
 
   def log_data_without_changes
+    version = version.to_i
     log_data = []
+
     reload_log_data.data['h'].each do |change_log|
       responsible = responsible_user_for_version(change_log)
       log_data << { version: change_log['v'], user: responsible,
-                    updated_at: DateTime.parse(change_log['c']['updated_at']).strftime('%a %b %e %Y %H:%M') }
+                    updated_at: DateTime.parse(change_log['c']['updated_at']).strftime('%a %b %e %Y %H:%M'),
+                    restored: record_restored?(change_log, version),
+                    deleted: record_deleted?(change_log) }
     end
     log_data
   end
@@ -32,9 +36,11 @@ module History
     current_version = add_puid_to_current_version(current_version) if version == 1
 
     { version:, user: responsible,
-      changes_from_prev_version: current_version['c'].except('id', 'created_at', 'updated_at', 'deleted_at'),
-      previous_version: initial_version['c'] }
+      changes_from_prev_version: format_changes(current_version['c'].except('id', 'created_at', 'updated_at')),
+      previous_version: format_changes(initial_version['c']) }
   end
+
+  private
 
   def responsible_user_for_version(current_version)
     # Change was made outside the web request (ie. rails console)
@@ -49,5 +55,30 @@ module History
   def add_puid_to_current_version(current_version)
     current_version['c'].merge!(puid: project.puid) if project && !current_version['c'].key?('puid')
     current_version
+  end
+
+  # Format keys in changes to format required
+  # Currently converts string to date time and formats to YYYY-MM-DD H:M
+  # for created_at, deleted_at, and updated_at
+  def format_changes(changes)
+    datetime_format = I18n.t('time.formats.default')
+
+    if changes.key?('deleted_at') && !changes['deleted_at'].nil?
+      changes['deleted_at'] =
+        DateTime.parse(changes['deleted_at']).strftime(datetime_format)
+    end
+    changes
+  end
+
+  def record_deleted?(change_log)
+    return false unless change_log['c'].key?('deleted_at')
+
+    change_log['c'].key?('deleted_at') && change_log['c']['deleted_at'].present?
+  end
+
+  def record_restored?(change_log, version)
+    return false if version == 1
+
+    change_log['c'].key?('deleted_at') && change_log['c']['deleted_at'].blank?
   end
 end
