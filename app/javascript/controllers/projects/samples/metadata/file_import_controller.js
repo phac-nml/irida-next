76b7bff7afd6137ed774d3a5bd14426363846298
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-02-01 10:40:25 -0600

    update gray to slate

--- app/javascript/controllers/projects/samples/metadata/file_import_controller.js
+++ app/javascript/controllers/projects/samples/metadata/file_import_controller.js
@@ -5,10 +5,10 @@ export default class extends Controller {
   static targets = ["selectInput", "submitButton"];
 
   #disabled_classes = [
-    "bg-gray-50",
+    "bg-slate-50",
     "border",
-    "border-gray-300",
-    "text-gray-900",
+    "border-slate-300",
+    "text-slate-900",
     "text-sm",
     "rounded-lg",
     "focus:ring-blue-500",
@@ -16,9 +16,9 @@ export default class extends Controller {
     "block",
     "w-full",
     "p-2.5",
-    "dark:bg-gray-700",
-    "dark:border-gray-600",
-    "dark:placeholder-gray-400",
+    "dark:bg-slate-700",
+    "dark:border-slate-600",
+    "dark:placeholder-slate-400",
     "dark:text-white",
     "dark:focus:ring-blue-500",
     "dark:focus:border-blue-500",
