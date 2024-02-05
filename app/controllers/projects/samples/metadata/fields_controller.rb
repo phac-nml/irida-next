5d176da5cf882f147c8d4c19e7274dcc05f215ac
Chris Huynh <chris.huynh333@gmail.com>
2024-02-05 10:25:34 -0600

    Change render_params to respective create/update_render_params

--- app/controllers/projects/samples/metadata/fields_controller.rb
+++ app/controllers/projects/samples/metadata/fields_controller.rb
@@ -7,6 +7,8 @@ module Projects
       class FieldsController < Projects::Samples::ApplicationController
         respond_to :turbo_stream
 
+        # Param received as:
+        # params: {sample: {create_fields: {key1: value1, key2: value2, ...}}}
         def create
           authorize! @project, to: :update_sample?
           create_metadata_fields =
@@ -16,9 +18,9 @@ module Projects
           if @sample.errors.any?
             render status: :unprocessable_entity, locals: { type: 'error', message: @sample.errors.full_messages.first }
           else
-            render_params = get_create_status_and_messages(create_metadata_fields[:added_keys],
-                                                           create_metadata_fields[:existing_keys])
-            render status: render_params[:status], locals: { messages: render_params[:messages] }
+            create_render_params = get_create_status_and_messages(create_metadata_fields[:added_keys],
+                                                                  create_metadata_fields[:existing_keys])
+            render status: create_render_params[:status], locals: { messages: render_params[:messages] }
           end
         end
 
@@ -34,9 +36,9 @@ module Projects
                    locals: { key: edit_field_params['update_field']['key'].keys[0],
                              value: edit_field_params['update_field']['value'].keys[0] }
           else
-            render_params = get_update_status_and_message(updated_metadata_field)
-            render status: render_params[:status], locals: { type: render_params[:message][:type],
-                                                             message: render_params[:message][:message] }
+            update_render_params = get_update_status_and_message(updated_metadata_field)
+            render status: update_render_params[:status], locals: { type: update_render_params[:message][:type],
+                                                                    message: update_render_params[:message][:message] }
           end
         end
 
@@ -51,35 +53,35 @@ module Projects
         end
 
         def get_create_status_and_messages(added_keys, existing_keys)
-          params = { status: '', messages: [] }
+          create_render_params = { status: '', messages: [] }
           success_msg = { type: 'success', message: t('.success', keys: added_keys.join(', ')) }
           error_msg = { type: 'error', message: t('.keys_exist', keys: existing_keys.join(', ')) }
 
           if added_keys.count.positive? && existing_keys.count.positive?
-            params[:status] = :multi_status
-            params[:messages] = [success_msg, error_msg]
+            create_render_params[:status] = :multi_status
+            create_render_params[:messages] = [success_msg, error_msg]
           elsif existing_keys.count.positive?
-            params[:status] = :unprocessable_entity
-            params[:messages] = [error_msg]
+            create_render_params[:status] = :unprocessable_entity
+            create_render_params[:messages] = [error_msg]
           else
-            params[:status] = :ok
-            params[:messages] = [success_msg]
+            create_render_params[:status] = :ok
+            create_render_params[:messages] = [success_msg]
           end
-          params
+          create_render_params
         end
 
         def get_update_status_and_message(updated_metadata_field)
-          render_params = {}
+          update_render_params = {}
           modified_metadata = updated_metadata_field[:added] + updated_metadata_field[:updated] +
                               updated_metadata_field[:deleted]
           if modified_metadata.count.positive?
-            render_params[:status] = :ok
-            render_params[:message] = { type: 'success', message: t('.success') }
+            update_render_params[:status] = :ok
+            update_render_params[:message] = { type: 'success', message: t('.success') }
           else
-            render_params[:status] = :unprocessable_entity
-            render_params[:message] = { type: 'error', message: @sample.errors.full_messages.first }
+            update_render_params[:status] = :unprocessable_entity
+            update_render_params[:message] = { type: 'error', message: @sample.errors.full_messages.first }
           end
-          render_params
+          update_render_params
         end
       end
     end
