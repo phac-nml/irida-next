ec8b446a0a001a0bc394c81cc845b520bbebd881
Chris Huynh <chris.huynh333@gmail.com>
2024-02-05 11:45:28 -0600

    Fix controller params for flash messages, fix destroy.turbo update

--- app/controllers/projects/samples/metadata/deletions_controller.rb
+++ app/controllers/projects/samples/metadata/deletions_controller.rb
@@ -17,24 +17,37 @@ module Projects
         end
 
         def destroy
-          puts 'DESTROY!'
-          puts params
-
           metadata_fields_update = ::Samples::Metadata::UpdateService.new(@project, @sample, current_user,
                                                                           deletion_params).execute
-          if @sample.errors.any?
-            puts 'hi'
-          else
-            render status: :ok, locals: { type: 'success',
-                                          message: t('.success',
-                                                     deleted_keys: metadata_fields_update[:deleted].join(', ')) }
-          end
+
+          destroy_render_params = get_destroy_status_and_messages(metadata_fields_update[:deleted])
+
+          render status: destroy_render_params[:status], locals: { messages: destroy_render_params[:messages] }
         end
 
         private
 
         def deletion_params
           params.require(:sample).permit(metadata: {})
+        end
+
+        def get_destroy_status_and_messages(deleted_keys)
+          destroy_render_params = { status: '', messages: [] }
+          success_msg = { type: 'success', message: t('.success',
+                                                      deleted_keys: deleted_keys.join(', ')) }
+          error_msg = { type: 'error', message: @sample.errors.full_messages.first }
+
+          if @sample.errors.any? && deleted_keys.count.positive?
+            destroy_render_params[:status] = :multi_status
+            destroy_render_params[:messages] = [success_msg, error_msg]
+          elsif @sample.errors.any?
+            destroy_render_params[:status] = :unprocessable_entity
+            destroy_render_params[:messages] = [error_msg]
+          else
+            destroy_render_params[:status] = :ok
+            destroy_render_params[:messages] = [success_msg]
+          end
+          destroy_render_params
         end
       end
     end
