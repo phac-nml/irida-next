724379fd367da52a5cfda27754d1ce39cf385210
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-02-08 11:46:26 -0600

    test: Added UI tests for cancelling a workflow

--- test/system/workflow_executions_test.rb
+++ test/system/workflow_executions_test.rb
@@ -15,4 +15,24 @@ class WorkflowExecutionsTest < ApplicationSystemTestCase
 
     assert_selector 'table#workflow_executions tbody tr', count: 5
   end
+
+  test 'should be able to cancel a workflow' do
+    visit workflow_executions_path
+
+    assert_selector 'h1', text: I18n.t(:'workflow_executions.index.title')
+
+    tr = find('td', text: 'prepared').ancestor('tr')
+
+    within tr do
+      assert_selector 'td:nth-child(4)', text: 'prepared'
+      assert_selector 'input[type="submit"][value="Cancel"]', count: 1
+      find('input[type="submit"][value="Cancel"]').click
+    end
+
+    assert_text 'Confirmation required'
+    click_button 'Confirm'
+
+    assert_selector 'tbody tr td:nth-child(4)', text: 'canceling'
+    assert_no_selector 'tbody tr input[type="submit"][value="Cancel"]'
+  end
 end
