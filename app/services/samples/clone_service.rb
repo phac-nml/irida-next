a79ae582db5c5d1b8753fcb4ef6e89927e69c183
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-02-13 13:11:37 -0600

    refactoring

--- app/services/samples/clone_service.rb
+++ app/services/samples/clone_service.rb
@@ -31,27 +31,34 @@ module Samples
       raise CloneError, I18n.t('services.samples.clone.same_project')
     end
 
-    def clone_samples(new_project, sample_ids) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    def clone_samples(new_project, sample_ids)
       cloned_sample_ids = {}
       sample_ids.each do |sample_id|
-        sample = Sample.find_by(id: sample_id, project_id: @project.id)
-        clone = Sample.new(name: sample.name, description: sample.description, project_id: new_project.id)
-        metadata_changes = Samples::Metadata::UpdateService.new(new_project, clone, @current_user,
-                                                                { 'metadata' => sample.metadata }).execute
-        not_updated_metadata_changes = metadata_changes[:not_updated]
-        unless not_updated_metadata_changes.empty?
-          @project.errors.add(:sample,
-                              I18n.t('services.samples.metadata.import_file.sample_metadata_fields_not_updated',
-                                     sample_id:, metadata_fields: not_updated_metadata_changes.join(', ')))
-          next
-        end
-        clone_attachments(sample, clone) if clone.valid?
-        clone.save!
-        cloned_sample_ids[sample.id] = clone.id
+        cloned_sample_ids[sample_id] = clone_sample(new_project, sample_id)
       rescue ActiveRecord::RecordInvalid
         @project.errors.add(:sample, I18n.t('services.samples.clone.sample_exists', sample_id:))
       end
       cloned_sample_ids
+    end
+
+    def clone_sample(new_project, sample_id)
+      sample = Sample.find_by(id: sample_id, project_id: @project.id)
+      clone = Sample.new(name: sample.name, description: sample.description, project_id: new_project.id)
+      clone_metadata(new_project, sample, clone) if clone.valid?
+      clone_attachments(sample, clone) if clone.valid?
+      clone.save! if @project.errors.empty?
+      clone.id
+    end
+
+    def clone_metadata(new_project, sample, clone)
+      metadata_changes = Samples::Metadata::UpdateService.new(new_project, clone, @current_user,
+                                                              { 'metadata' => sample.metadata }).execute
+      not_updated_metadata_changes = metadata_changes[:not_updated]
+      return if not_updated_metadata_changes.empty?
+
+      @project.errors.add(:sample,
+                          I18n.t('services.samples.metadata.import_file.sample_metadata_fields_not_updated',
+                                 sample_id:, metadata_fields: not_updated_metadata_changes.join(', ')))
     end
 
     def clone_attachments(sample, clone)
