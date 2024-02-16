a99fe6c0004f18a72f81f4757148fe04986225fb
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-02-16 11:38:50 -0600

    handle setup/teardown of test case

--- test/helpers/view_helper_test.rb
+++ test/helpers/view_helper_test.rb
@@ -5,6 +5,18 @@ require 'test_helper'
 class ViewHelperTest < ActionView::TestCase
   include ViewHelper
 
+  setup do
+    %w[developer saml azure_activedirectory_v2].each do |provider|
+      Rails.configuration.auth_config["#{provider}_icon"] = '../test/fixtures/files/tyrell.svg'
+    end
+  end
+
+  teardown do
+    %w[developer saml azure_activedirectory_v2].each do |provider|
+      Rails.configuration.auth_config["#{provider}_icon"] = nil
+    end
+  end
+
   test 'should add assigned classes' do
     source = viral_icon_source('bars_3')
     assert source.include? 'focusable="false"'
@@ -13,7 +25,6 @@ class ViewHelperTest < ActionView::TestCase
 
   test 'should load override icons' do
     %w[developer saml azure_activedirectory_v2].each do |provider|
-      Rails.configuration.auth_config["#{provider}_icon"] = '../test/fixtures/files/tyrell.svg'
       source = viral_icon_source("#{provider}_icon")
       assert source.include? %(class="Viral-Icon__Svg icon-#{provider}_icon")
       assert source.include? 'viewbox="0 0 1140 1012"'
