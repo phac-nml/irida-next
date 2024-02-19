8189eb5fc7b2622ada5aa29c9bfdf306dd19a5e7
Deep Sidhu <deepsidhu85@gmail.com>
2024-02-19 11:44:48 -0600

    Added tests and fixed markup violations in history version component

--- test/components/history_component_test.rb
+++ test/components/history_component_test.rb
@@ -3,7 +3,7 @@
 require 'view_component_test_case'
 
 class HistoryComponentTest < ViewComponentTestCase
-  test 'listing of versions' do
+  test 'listing of project change log versions' do
     log_data = []
     version_hash = { 'h' =>
     [{ 'c' => { 'id' => 254, 'name' => 'Project 1', 'path' => 'project-1', 'type' => 'Project', 'owner_id' => 1,
@@ -42,5 +42,67 @@ class HistoryComponentTest < ViewComponentTestCase
 
     assert_selector 'h2', text: 'Version 1'
     assert_selector 'a[href="/user_at_email.com/project-1/-/history/new?version=1"]', count: 1
+  end
+
+  test 'listing of sample change log versions' do
+    log_data = []
+    version_hash = { 'h' =>
+    [{ 'c' =>
+       { 'id' => 1421,
+         'name' => 'Sample 1',
+         'puid' => 'INXT_SAM_AYCQCHLKFD',
+         'metadata' => '{}',
+         'deleted_at' => nil,
+         'project_id' => 254,
+         'description' => '' },
+       'm' => { '_r' => 1 },
+       'v' => 1,
+       'ts' => 1_708_114_666_172 },
+     { 'c' => { 'metadata' => '{"field1": "value1", "field2": "value2"}' }, 'm' => { '_r' => 1 }, 'v' => 2,
+       'ts' => 1_708_114_680_185 },
+     { 'c' => { 'metadata' => '{"field1": "newvalue1", "field2": "newvalue2"}' }, 'm' => { '_r' => 1 }, 'v' => 3,
+       'ts' => 1_708_114_724_089 },
+     { 'c' => { 'metadata' => '{"field1": "newvalue2", "field2": "newvalue3", "newfield": "newfieldvalue1"}' },
+       'm' => { '_r' => 1 },
+       'v' => 4,
+       'ts' => 1_708_114_832_929 },
+     { 'c' => { 'metadata' => '{"field1": "newvalue2", "field2": "newvalue3"}' }, 'm' => { '_r' => 1 }, 'v' => 5,
+       'ts' => 1_708_114_947_526 },
+     { 'c' => { 'metadata' => '{}' }, 'm' => { '_r' => 1 }, 'v' => 6, 'ts' => 1_708_121_682_922 }],
+                     'v' => 6 }
+
+    version_hash['h'].each do |change_log|
+      responsible = 'user@email.com'
+
+      log_data << { version: change_log['v'], user: responsible,
+                    updated_at: DateTime.parse(Time.zone.at(0, change_log['ts'], :millisecond).to_s)
+                                        .strftime('%a %b%e %Y %H:%M') }
+
+      render_inline HistoryComponent.new(data: log_data, type: 'Sample',
+                                         url: '/user_at_email.com/project-1/-/samples/1421/view_history_version')
+    end
+
+    assert_selector 'li', count: 6
+    assert_selector 'h2', count: 6
+    assert_selector 'p', text: 'Sample created by user@email.com', count: 1
+    assert_selector 'p', text: 'Sample modified by user@email.com', count: 5
+
+    assert_selector 'h2', text: 'Version 6'
+    assert_selector 'a[href="/user_at_email.com/project-1/-/samples/1421/view_history_version?version=6"]', count: 1
+
+    assert_selector 'h2', text: 'Version 5'
+    assert_selector 'a[href="/user_at_email.com/project-1/-/samples/1421/view_history_version?version=5"]', count: 1
+
+    assert_selector 'h2', text: 'Version 4'
+    assert_selector 'a[href="/user_at_email.com/project-1/-/samples/1421/view_history_version?version=4"]', count: 1
+
+    assert_selector 'h2', text: 'Version 3'
+    assert_selector 'a[href="/user_at_email.com/project-1/-/samples/1421/view_history_version?version=3"]', count: 1
+
+    assert_selector 'h2', text: 'Version 2'
+    assert_selector 'a[href="/user_at_email.com/project-1/-/samples/1421/view_history_version?version=2"]', count: 1
+
+    assert_selector 'h2', text: 'Version 1'
+    assert_selector 'a[href="/user_at_email.com/project-1/-/samples/1421/view_history_version?version=1"]', count: 1
   end
 end
