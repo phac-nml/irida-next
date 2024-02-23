dd33cd4ef2dae2af9761d9fafcc6be430c7c1b3c
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-02-23 16:35:43 -0600

    asking a question within a comment

--- test/jobs/samples_cleanup_job_test.rb
+++ test/jobs/samples_cleanup_job_test.rb
@@ -36,7 +36,11 @@ class SamplesCleanupJobTest < ActiveJob::TestCase
                       -> { Sample.only_deleted.count } => -1,
                       -> { Attachment.only_deleted.count } => -2,
                       -> { ActiveStorage::Attachment.count } => -2,
-                      -> { SamplesWorkflowExecution.only_deleted.count } => 0 do
+                      -> { SamplesWorkflowExecution.only_deleted.count } => 0,
+                      -> { WorkflowExecution.only_deleted.count } => 0 do
+      # Question: I noticed `dependent: :destroy`` is specified for sample attachments,
+      # but not samples_workflow_executions and workflow_executions. Is this intentional?
+      # If so, should I leave the test assertions that they don't change?
       SamplesCleanupJob.perform_now
     end
 
@@ -54,7 +58,8 @@ class SamplesCleanupJobTest < ActiveJob::TestCase
                       -> { Sample.only_deleted.count } => -1,
                       -> { Attachment.only_deleted.count } => -2,
                       -> { ActiveStorage::Attachment.count } => -2,
-                      -> { SamplesWorkflowExecution.only_deleted.count } => 0 do
+                      -> { SamplesWorkflowExecution.only_deleted.count } => 0,
+                      -> { WorkflowExecution.only_deleted.count } => 0 do
       SamplesCleanupJob.perform_now(days_old: 4)
     end
 
