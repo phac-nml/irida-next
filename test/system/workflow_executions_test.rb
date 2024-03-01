3aefbce6a01dacb2069c5e02c45d8e4155478cea
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-03-01 14:40:56 -0600

    trying to add ui tests for deleting workflow execution

--- test/system/workflow_executions_test.rb
+++ test/system/workflow_executions_test.rb
@@ -17,14 +17,16 @@ class WorkflowExecutionsTest < ApplicationSystemTestCase
   end
 
   test 'should be able to cancel a workflow' do
+    workflow_execution = workflow_executions(:irida_next_example_prepared)
+
     visit workflow_executions_path
 
     assert_selector 'h1', text: I18n.t(:'workflow_executions.index.title')
 
-    tr = find('td', text: 'prepared').ancestor('tr')
+    tr = find('td', text: workflow_execution.run_id).ancestor('tr')
 
     within tr do
-      assert_selector 'td:nth-child(4)', text: 'prepared'
+      assert_selector 'td:nth-child(4)', text: workflow_execution.state
       assert_selector 'input[type="submit"][value="Cancel"]', count: 1
       find('input[type="submit"][value="Cancel"]').click
     end
@@ -34,5 +36,77 @@ class WorkflowExecutionsTest < ApplicationSystemTestCase
 
     assert_selector 'tbody tr td:nth-child(4)', text: 'canceling'
     assert_no_selector 'tbody tr input[type="submit"][value="Cancel"]'
+  end
+
+  test 'should not delete a prepared workflow' do
+    workflow_execution = workflow_executions(:irida_next_example_prepared)
+
+    visit workflow_executions_path
+
+    assert_selector 'h1', text: I18n.t(:'workflow_executions.index.title')
+
+    tr = find('td', text: workflow_execution.run_id).ancestor('tr')
+
+    within tr do
+      assert_selector 'td:nth-child(4)', text: workflow_execution.state
+      assert_no_selector 'input[type="submit"][value="Delete"]'
+    end
+  end
+
+  test 'should delete a completed workflow' do
+    workflow_execution = workflow_executions(:irida_next_example_completed)
+
+    visit workflow_executions_path
+
+    assert_selector 'h1', text: I18n.t(:'workflow_executions.index.title')
+
+    tr = find('td', text: workflow_execution.run_id).ancestor('tr')
+
+    within tr do
+      assert_selector 'td:nth-child(4)', text: workflow_execution.state
+      assert_selector 'input[type="submit"][value="Delete"]', count: 1
+      find('input[type="submit"][value="Delete"]').click
+    end
+
+    assert_text 'Confirmation required'
+    click_button 'Confirm'
+
+    assert_no_text workflow_execution.run_id
+  end
+
+  test 'should delete an errored workflow' do
+    workflow_execution = workflow_executions(:irida_next_example_error)
+
+    visit workflow_executions_path
+
+    assert_selector 'h1', text: I18n.t(:'workflow_executions.index.title')
+
+    tr = find('td', text: workflow_execution.run_id).ancestor('tr')
+
+    within tr do
+      assert_selector 'td:nth-child(4)', text: workflow_execution.state
+      assert_selector 'input[type="submit"][value="Delete"]', count: 1
+      find('input[type="submit"][value="Delete"]').click
+    end
+
+    assert_text 'Confirmation required'
+    click_button 'Confirm'
+
+    assert_no_text workflow_execution.run_id
+  end
+
+  test 'should not delete a canceling workflow' do
+    workflow_execution = workflow_executions(:irida_next_example_canceling)
+
+    visit workflow_executions_path
+
+    assert_selector 'h1', text: I18n.t(:'workflow_executions.index.title')
+
+    tr = find('td', text: workflow_execution.run_id).ancestor('tr')
+
+    within tr do
+      assert_selector 'td:nth-child(4)', text: workflow_execution.state
+      assert_no_selector 'input[type="submit"][value="Delete"]'
+    end
   end
 end
