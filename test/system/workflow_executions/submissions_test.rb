b630d0294144398ad44e288856e655d53cb52f54
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-03-01 10:16:08 -0600

    chore: Added testing to ensure the samplesheet was added to the parameters

--- test/system/workflow_executions/submissions_test.rb
+++ test/system/workflow_executions/submissions_test.rb
@@ -21,7 +21,18 @@ class SubmissionsTest < ApplicationSystemTestCase
     click_on I18n.t(:'projects.samples.index.workflows.button_sr')
     assert_selector 'dialog' do
       assert_selector '.dialog--header', text: I18n.t(:'workflow_executions.submissions.pipeline_selection.title')
-      assert_selector 'button', text: 'phac-nml/iridanextexample'
+      assert_selector 'button', text: 'phac-nml/iridanextexample', count: 3
+      first('button', text: 'phac-nml/iridanextexample').click
+    end
+
+    assert_selector 'dialog.dialog--size-xl' do
+      assert_selector 'div.sample-sheet' do
+        assert_selector 'table' do
+          assert_selector 'tr[data-controller="nextflow--samplesheet"]', count: 2
+          assert_selector 'tr[data-controller="nextflow--samplesheet"]', text: @sample1.name, count: 1
+          assert_selector 'tr[data-controller="nextflow--samplesheet"]', text: @sample2.name, count: 1
+        end
+      end
     end
   end
 end
