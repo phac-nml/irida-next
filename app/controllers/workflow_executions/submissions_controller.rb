b3c58309f50feb97ecbc5e05b91f89e1a23e4c6a
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-03-08 12:23:56 -0600

    Workflow Executions: Update Workflow Executions Submission to use POST instead of GET (#458)
    * feat: update workflow executions submissions form to use post and create action instead of get and new action
    
    * chore: speedup submission modal by prelaoding attachments and files

--- app/controllers/workflow_executions/submissions_controller.rb
+++ app/controllers/workflow_executions/submissions_controller.rb
@@ -5,14 +5,14 @@ module WorkflowExecutions
   class SubmissionsController < ApplicationController
     respond_to :turbo_stream
     before_action :workflows
-    before_action :samples, only: %i[new]
-    before_action :workflow, only: %i[new]
+    before_action :samples, only: %i[create]
+    before_action :workflow, only: %i[create]
 
     def pipeline_selection
       render status: :ok
     end
 
-    def new
+    def create
       render status: :ok
     end
 
@@ -31,7 +31,7 @@ module WorkflowExecutions
 
     def samples
       sample_ids = params[:sample_ids]
-      @samples = Sample.where(id: sample_ids)
+      @samples = Sample.where(id: sample_ids).includes(attachments: :file_attachment)
     end
   end
 end
