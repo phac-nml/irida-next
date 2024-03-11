8ebc7c943f10512f82c56db4df2f4c61b8f13596
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-03-11 15:22:50 -0500

    chore: update cuprite timeout to hopefully alleviate flaky test failures

--- test/test_helpers/cuprite_setup.rb
+++ test/test_helpers/cuprite_setup.rb
@@ -11,7 +11,9 @@ Capybara.register_driver(:irida_next_cuprite) do |app|
     # See additional options for Dockerized environment in the respective section of this article
     browser_options: {},
     # Increase Chrome startup wait time (required for stable CI builds)
-    process_timeout: 30,
+    process_timeout: 45,
+    # Page load timeout, default is 5
+    timeout: 10,
     # Enable debugging capabilities
     inspector: true,
     # Allow running Chrome in a headful mode by setting HEADLESS env
