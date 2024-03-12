2e126b024e46ebd497e09be6d1ada8ac58a81e2c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-03-12 18:24:06 -0500

    chore: Fixed broken test

---
+++ test/components/list_filter_component_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'view_component_test_case'
+
+class ListFilterComponentTest < ViewComponentTestCase
+  test 'should create a tag from input after waiting 1 second' do
+  end
+end
