cc06d2bf34865469f5390b1e9b49cefd3394aa06
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-03-14 10:39:18 -0500

    adding missing module to submissions_test

--- test/system/workflow_executions/submissions_test.rb
+++ test/system/workflow_executions/submissions_test.rb
@@ -2,70 +2,72 @@
 
 require 'application_system_test_case'
 
-class SubmissionsTest < ApplicationSystemTestCase
-  setup do
-    @user = users(:john_doe)
-    login_as @user
-    @sample1 = samples(:sample1)
-    @sample2 = samples(:sample2)
-    @project = projects(:project1)
-    @namespace = groups(:group_one)
-  end
-
-  test 'should display a pipeline selection modal for project samples' do
-    visit namespace_project_samples_url(namespace_id: @namespace.path, project_id: @project.path)
-
-    find("input[type='checkbox'][value='#{@sample1.id}']").click
-    find("input[type='checkbox'][value='#{@sample2.id}']").click
-
-    click_on I18n.t(:'projects.samples.index.workflows.button_sr')
-    within 'dialog[open]' do
-      assert_selector '.dialog--header', text: I18n.t(:'workflow_executions.submissions.pipeline_selection.title')
-      assert_button text: 'phac-nml/iridanextexample', count: 3
-      first('button', text: 'phac-nml/iridanextexample').click
+module WorkflowExecutions
+  class SubmissionsTest < ApplicationSystemTestCase
+    setup do
+      @user = users(:john_doe)
+      login_as @user
+      @sample1 = samples(:sample1)
+      @sample2 = samples(:sample2)
+      @project = projects(:project1)
+      @namespace = groups(:group_one)
     end
 
-    within 'dialog[open].dialog--size-xl' do
-      within 'div.sample-sheet' do
-        within 'table' do
-          assert_selector 'tr[data-controller="nextflow--samplesheet"]', count: 2
-          assert_selector 'tr[data-controller="nextflow--samplesheet"]', text: @sample1.puid, count: 1
-          assert_selector 'tr[data-controller="nextflow--samplesheet"]', text: @sample2.puid, count: 1
+    test 'should display a pipeline selection modal for project samples' do
+      visit namespace_project_samples_url(namespace_id: @namespace.path, project_id: @project.path)
+
+      find("input[type='checkbox'][value='#{@sample1.id}']").click
+      find("input[type='checkbox'][value='#{@sample2.id}']").click
+
+      click_on I18n.t(:'projects.samples.index.workflows.button_sr')
+      within 'dialog[open]' do
+        assert_selector '.dialog--header', text: I18n.t(:'workflow_executions.submissions.pipeline_selection.title')
+        assert_button text: 'phac-nml/iridanextexample', count: 3
+        first('button', text: 'phac-nml/iridanextexample').click
+      end
+
+      within 'dialog[open].dialog--size-xl' do
+        within 'div.sample-sheet' do
+          within 'table' do
+            assert_selector 'tr[data-controller="nextflow--samplesheet"]', count: 2
+            assert_selector 'tr[data-controller="nextflow--samplesheet"]', text: @sample1.puid, count: 1
+            assert_selector 'tr[data-controller="nextflow--samplesheet"]', text: @sample2.puid, count: 1
+          end
         end
       end
     end
-  end
 
-  test 'should display a pipeline selection modal for group samples' do
-    visit group_samples_url(@namespace)
+    test 'should display a pipeline selection modal for group samples' do
+      visit group_samples_url(@namespace)
 
-    find("input[type='checkbox'][value='#{@sample1.id}']").click
-    find("input[type='checkbox'][value='#{@sample2.id}']").click
+      find("input[type='checkbox'][value='#{@sample1.id}']").click
+      find("input[type='checkbox'][value='#{@sample2.id}']").click
 
-    click_on I18n.t(:'groups.samples.index.workflows.button_sr')
-    within 'dialog[open]' do
-      assert_selector '.dialog--header', text: I18n.t(:'workflow_executions.submissions.pipeline_selection.title')
-      assert_button text: 'phac-nml/iridanextexample', count: 3
-      first('button', text: 'phac-nml/iridanextexample').click
-    end
+      click_on I18n.t(:'groups.samples.index.workflows.button_sr')
+      within 'dialog[open]' do
+        assert_selector '.dialog--header', text: I18n.t(:'workflow_executions.submissions.pipeline_selection.title')
+        assert_button text: 'phac-nml/iridanextexample', count: 3
+        first('button', text: 'phac-nml/iridanextexample').click
+      end
 
-    within 'dialog[open].dialog--size-xl' do
-      within 'div.sample-sheet' do
-        within 'table' do
-          assert_selector 'tr[data-controller="nextflow--samplesheet"]', count: 2
-          assert_selector 'tr[data-controller="nextflow--samplesheet"]', text: @sample1.puid, count: 1
-          assert_selector 'tr[data-controller="nextflow--samplesheet"]', text: @sample2.puid, count: 1
+      within 'dialog[open].dialog--size-xl' do
+        within 'div.sample-sheet' do
+          within 'table' do
+            assert_selector 'tr[data-controller="nextflow--samplesheet"]', count: 2
+            assert_selector 'tr[data-controller="nextflow--samplesheet"]', text: @sample1.puid, count: 1
+            assert_selector 'tr[data-controller="nextflow--samplesheet"]', text: @sample2.puid, count: 1
+          end
         end
       end
     end
-  end
 
-  test 'should not display a launch pipeline button for group samples' do
-    user = users(:ryan_doe)
-    login_as user
+    test 'should not display a launch pipeline button for group samples' do
+      user = users(:ryan_doe)
+      login_as user
 
-    visit group_samples_url(@namespace)
+      visit group_samples_url(@namespace)
 
-    assert_no_text I18n.t(:'groups.samples.index.workflows.button_sr')
+      assert_no_text I18n.t(:'groups.samples.index.workflows.button_sr')
+    end
   end
 end
