790f8d4a10290603b5a997d1a57459de07588e13
Chris Huynh <chris.huynh333@gmail.com>
2024-03-18 15:27:49 -0500

    Remove mailer call

--- app/jobs/data_exports/create_job.rb
+++ app/jobs/data_exports/create_job.rb
@@ -16,8 +16,6 @@ module DataExports
       data_export.expires_at = set_expiry
       data_export.status = 'ready'
       data_export.save
-
-      DataExportMailer.export_ready(data_export).deliver_later if data_export.email_notification
     end
 
     def initialize_manifest(export_type)
