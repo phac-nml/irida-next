86d3b3441cd43472b984386fab619a5b5791cb21
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-03-18 12:08:50 -0500

    chore: Fixed broken test

--- test/system/projects/samples_test.rb
+++ test/system/projects/samples_test.rb
@@ -1889,7 +1889,7 @@ module Projects
         assert_selector 'tr td', text: @sample2.puid
       end
       find("button[aria-label='#{I18n.t(:'components.list_filter.title')}").click
-      within 'div[data-controller="filter-list"] dialog' do
+      within 'dialog' do
         assert_selector 'h1', text: I18n.t(:'components.list_filter.title')
         find("input[type='text']").send_keys "#{@sample1.puid}, #{@sample2.puid}"
         assert_selector 'span.label', count: 2
@@ -1901,7 +1901,7 @@ module Projects
         assert_selector 'tr', count: 2
       end
       find("button[aria-label='#{I18n.t(:'components.list_filter.title')}").click
-      within 'div[data-controller="filter-list"] dialog' do
+      within 'dialog' do
         assert_selector 'h1', text: I18n.t(:'components.list_filter.title')
         click_button I18n.t(:'components.list_filter.clear')
         click_button I18n.t(:'components.list_filter.apply')
