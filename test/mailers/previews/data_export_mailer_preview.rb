031728be8c90ad0fe87e56a54316ed215e45ea9f
Chris Huynh <chris.huynh333@gmail.com>
2024-03-19 12:49:08 -0500

    Seed exports, add tests, add additional preview

--- test/mailers/previews/data_export_mailer_preview.rb
+++ test/mailers/previews/data_export_mailer_preview.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class DataExportMailerPreview < ActionMailer::Preview
-  def export_ready
-    data_export = DataExport.last
+  def export_ready_with_name
+    data_export = DataExport.find_by(name: 'Seeded export 3')
+    DataExportMailer.export_ready(data_export)
+  end
+
+  def export_ready_without_name
+    data_export = DataExport.first
     DataExportMailer.export_ready(data_export)
   end
 end
