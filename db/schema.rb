b3d0b5911c74dc6345ac63e3fe262eee0231d5d6
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-03-19 19:48:47 -0500

    schema fixing

--- db/schema.rb
+++ db/schema.rb
@@ -246,8 +246,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_142828) do
     t.datetime "updated_at", null: false
     t.uuid "submitter_id", null: false
     t.datetime "attachments_updated_at"
-    t.index ["created_at"], name: "index_workflow_executions_on_created_at"
     t.string "blob_run_directory"
+    t.index ["created_at"], name: "index_workflow_executions_on_created_at"
     t.index ["submitter_id"], name: "index_workflow_executions_on_submitter_id"
   end
 
