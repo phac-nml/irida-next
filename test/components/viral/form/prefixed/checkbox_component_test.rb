e927a35c98892467989d9c5ca8e662d9fd482e42
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-03-20 10:28:48 -0500

    chore: Added tests for prefixed checkbox

--- test/components/viral/form/prefixed/checkbox_component_test.rb
+++ test/components/viral/form/prefixed/checkbox_component_test.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'view_component_test_case'
 
-class Viral::Form::Prefixed::CheckboxComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(Viral::Form::Prefixed::CheckboxComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+module Viral
+  module Form
+    class PrefixedCheckboxComponentTest < ViewComponentTestCase
+      test 'default' do
+        render_preview(:default)
+        assert_selector 'span.font-mono', text: '--prefix', count: 1
+        assert_selector 'input[type="checkbox"]', count: 1
+        assert_selector 'label', text: 'Checkbox Label', count: 1
+      end
+
+      test 'with_icon' do
+        render_preview(:with_icon)
+        assert_selector 'span.Viral-Icon.Viral-Icon--colorWarning', count: 1
+        assert_selector 'input[type="checkbox"]', count: 1
+        assert_selector 'label', text: 'Checkbox Label', count: 1
+      end
+    end
   end
 end
