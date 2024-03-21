15f4d4bb8ce7617b86f149937add34d77ae7917a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-03-21 08:50:43 -0500

    chore: Updated testing for prefixed text component

---
+++ test/components/previews/viral_form_prefixed_text_input_component_preview.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class ViralFormPrefixedTextInputComponentPreview < ViewComponent::Preview
+  def default; end
+
+  def with_icon; end
+end
--- test/components/viral/form/prefixed/select_component_test.rb
+++ test/components/viral/form/prefixed/select_component_test.rb
@@ -5,19 +5,21 @@ require 'view_component_test_case'
 module Viral
   module Form
     module Prefixed
-      class CheckboxComponentTest < ViewComponentTestCase
+      class SelectComponentTest < ViewComponentTestCase
         test 'default' do
           render_preview(:default)
           assert_selector 'span.font-mono', text: '--prefix', count: 1
-          assert_selector 'input[type="checkbox"]', count: 1
-          assert_selector 'label', text: 'Checkbox Label', count: 1
+          assert_selector 'select', count: 1
+          assert_selector 'option', text: 'Lisbon', count: 1
+          assert_selector 'option', text: 'Madrid', count: 1
         end
 
         test 'with_icon' do
           render_preview(:with_icon)
           assert_selector 'span.Viral-Icon.Viral-Icon--colorWarning', count: 1
-          assert_selector 'input[type="checkbox"]', count: 1
-          assert_selector 'label', text: 'Checkbox Label', count: 1
+          assert_selector 'select', count: 1
+          assert_selector 'option', text: 'Lisbon', count: 1
+          assert_selector 'option', text: 'Madrid', count: 1
         end
       end
     end
