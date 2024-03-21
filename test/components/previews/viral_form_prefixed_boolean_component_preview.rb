1d3df85fc763a9fea5282a60e5cf4759f5bb91ee
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-03-21 15:20:54 -0500

    chore: Updated testing for boolean component

--- test/components/previews/viral_form_prefixed_boolean_component_preview.rb
+++ test/components/previews/viral_form_prefixed_boolean_component_preview.rb
@@ -3,4 +3,6 @@
 class ViralFormPrefixedBooleanComponentPreview < ViewComponent::Preview
   def default; end
   def with_icon; end
+
+  def with_false_value; end
 end
---
+++ test/components/viral/form/prefixed/boolean_component_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'view_component_test_case'
+
+module Viral
+  module Form
+    module Prefixed
+      class BooleanComponentTest < ViewComponentTestCase
+        test 'default' do
+          render_preview(:default)
+          assert_selector 'span.font-mono', text: '--prefix', count: 1
+          assert_selector 'input[type="radio"]', count: 2
+          assert_selector 'input[type="radio"][value="true"][checked="checked"]', count: 1
+          assert_no_selector 'input[type="radio"][value="false"][checked="checked"]'
+        end
+
+        test 'with_icon' do
+          render_preview(:with_icon)
+          assert_selector 'span.Viral-Icon.Viral-Icon--colorWarning', count: 1
+          assert_selector 'input[type="radio"]', count: 2
+          assert_selector 'input[type="radio"][value="true"][checked="checked"]', count: 1
+          assert_no_selector 'input[type="radio"][value="false"][checked="checked"]'
+        end
+
+        test 'with_false_value' do
+          render_preview(:with_false_value)
+          assert_selector 'span.font-mono', text: '--prefix', count: 1
+          assert_selector 'input[type="radio"]', count: 2
+          assert_no_selector 'input[type="radio"][value="true"][checked="checked"]'
+          assert_selector 'input[type="radio"][value="false"][checked="checked"]', count: 1
+        end
+      end
+    end
+  end
+end
