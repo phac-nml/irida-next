3d61e83a567925612383fe1dee3457783baa99cf
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-03-21 18:39:52 -0500

    sample attachments attaching to samples_workflow_attachments

--- app/services/workflow_executions/completion_service.rb
+++ app/services/workflow_executions/completion_service.rb
@@ -26,25 +26,26 @@ module WorkflowExecutions
       output_global_file_paths&.each do |blob_file_path|
         global_file_blob_list.append(download_and_make_new_blob(blob_file_path:))
       end
-      attachable_blobs_tuple_list.append({ attachable: @workflow_execution, blob_id_list: global_file_blob_list })
+      attachable_blobs_tuple_list.append({ attachable: @workflow_execution,
+                                           blob_id_list: global_file_blob_list })
 
-      # # per sample output files
-      # output_samples_file_paths = get_output_samples_file_paths(run_output_data:)
-      # output_samples_file_paths&.each do |sample_file_paths_tupple|
-      #   # todo replace this with a better way of keeping track of files
-      #   @file_blob_list = []
-      #   sample_file_paths_tupple[:file_paths]&.each do |file_path|
-      #     download_and_make_blob(file_path:)
-      #   end
-      #   #todo solve for this line
-      #   swe = get_samples_workflow_executions_somehow_todo(sample_file_paths_tupple[:sample_name, @workflow_execution])
-      #   unless @file_blob_list.empty?
-      #     #todo shouldnt have independant attachment creation for
-      #     Attachments::CreateService.new(
-      #       current_user, swe, { files: @file_blob_list }
-      #     ).execute
-      #   end
-      # end
+      # per sample output files
+      output_samples_file_paths = get_output_samples_file_paths(run_output_data:)
+      output_samples_file_paths&.each do |sample_file_paths_tuple| # :sample_puid, :data_paths
+        sample_file_blob_list = []
+        sample_file_paths_tuple[:data_paths]&.each do |blob_file_path|
+          sample_file_blob_list.append(download_and_make_new_blob(blob_file_path:))
+        end
+
+        samples_workflow_execution = \
+          @workflow_execution.samples_workflow_executions \
+                             .joins(:sample) \
+                             .where(sample: { puid: sample_file_paths_tuple[:sample_puid] }) \
+                             .first
+
+        attachable_blobs_tuple_list.append({ attachable: samples_workflow_execution,
+                                             blob_id_list: sample_file_blob_list })
+      end
 
       # attach blob lists to attachables
       unless attachable_blobs_tuple_list.empty?
@@ -67,20 +68,23 @@ module WorkflowExecutions
     def get_output_global_file_paths(run_output_data:)
       return nil unless run_output_data['files']['global']
 
-      #todo put data path mapping in it's own method
-      run_output_data['files']['global'].map { |entry| @output_base_path + entry['path'] }
+      get_path_mapping(run_output_data['files']['global'])
     end
 
     def get_output_samples_file_paths(run_output_data:)
       return nil unless run_output_data['files']['samples']
 
       samples_paths = []
-      run_output_data['files']['samples'].each do |sample_name, sample_data_paths|
-        data_paths = sample_data_paths.map { |entry| @output_base_path + entry['path'] }
+      run_output_data['files']['samples'].each do |sample_puid, sample_data_paths|
+        data_paths = get_path_mapping(sample_data_paths)
 
-        samples_paths.append({ sample_name:, data_paths: })
+        samples_paths.append({ sample_puid:, data_paths: })
       end
       samples_paths
+    end
+
+    def get_path_mapping(data_paths)
+      data_paths.map { |entry| @output_base_path + entry['path'] }
     end
   end
 end
