a5932734d46ba0d413d59335646602c13606e20f
Chris Huynh <chris.huynh333@gmail.com>
2024-03-21 13:04:18 -0500

    Start individual data export page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     resources :groups, only: %i[index new create]
     resources :projects, only: %i[index new create]
     resources :workflow_executions, only: %i[index create]
-    resources :data_exports, only: %i[index new create destroy] do
+    resources :data_exports, only: %i[index new create destroy show] do
       member do
         get :download
       end
