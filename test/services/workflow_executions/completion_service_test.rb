5abf96965dd255513b1f4d921cbf6f27939a04d2
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-03-22 13:03:30 -0500

    completion service test refactor

--- test/services/workflow_executions/completion_service_test.rb
+++ test/services/workflow_executions/completion_service_test.rb
@@ -4,13 +4,62 @@ require 'active_storage_test_case'
 
 module WorkflowExecutions
   class CompletionServiceTest < ActiveStorageTestCase
-    def setup
+    def setup # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+      # normal/
+      # get a new secure token for each workflow execution
       @workflow_execution_completed = workflow_executions(:irida_next_example_completed)
-      @workflow_execution_not_completed = workflow_executions(:irida_next_example)
-      @workflow_execution_with_samples = workflow_executions(:irida_next_example_completed_with_samples)
+      blob_run_directory_a = ActiveStorage::Blob.generate_unique_secure_token
+      @workflow_execution_completed.blob_run_directory = blob_run_directory_a
 
-      # get a new secure token for each test
-      @blob_run_directory = ActiveStorage::Blob.generate_unique_secure_token
+      # create file blobs
+      @normal_output_json_file_blob = make_and_upload_blob(
+        filepath: 'test/fixtures/files/blob_outputs/normal/iridanext.output.json.gz',
+        blob_run_directory: blob_run_directory_a
+      )
+      @normal_output_summary_file_blob = make_and_upload_blob(
+        filepath: 'test/fixtures/files/blob_outputs/normal/summary.txt',
+        blob_run_directory: blob_run_directory_a
+      )
+
+      # no_files/
+      # get a new secure token for each workflow execution
+      @workflow_execution_no_files = workflow_executions(:irida_next_example_completed2)
+      blob_run_directory_b = ActiveStorage::Blob.generate_unique_secure_token
+      @workflow_execution_no_files.blob_run_directory = blob_run_directory_b
+
+      # create file blobs
+      @no_files_output_json_file_blob = make_and_upload_blob(
+        filepath: 'test/fixtures/files/blob_outputs/no_files/iridanext.output.json.gz',
+        blob_run_directory: blob_run_directory_b
+      )
+
+      # normal2/
+      # get a new secure token for each workflow execution
+      @workflow_execution_with_samples = workflow_executions(:irida_next_example_completed_with_samples)
+      blob_run_directory_c = ActiveStorage::Blob.generate_unique_secure_token
+      @workflow_execution_with_samples.blob_run_directory = blob_run_directory_c
+
+      # create file blobs
+      @normal2_output_json_file_blob = make_and_upload_blob(
+        filepath: 'test/fixtures/files/blob_outputs/normal2/iridanext.output.json.gz',
+        blob_run_directory: blob_run_directory_c
+      )
+      @normal2_output_summary_file_blob = make_and_upload_blob(
+        filepath: 'test/fixtures/files/blob_outputs/normal2/summary.txt',
+        blob_run_directory: blob_run_directory_c
+      )
+      @normal2_output_analysis1_file_blob = make_and_upload_blob(
+        filepath: 'test/fixtures/files/blob_outputs/normal2/analysis1.txt',
+        blob_run_directory: blob_run_directory_c
+      )
+      @normal2_output_analysis2_file_blob = make_and_upload_blob(
+        filepath: 'test/fixtures/files/blob_outputs/normal2/analysis2.txt',
+        blob_run_directory: blob_run_directory_c
+      )
+      @normal2_output_analysis3_file_blob = make_and_upload_blob(
+        filepath: 'test/fixtures/files/blob_outputs/normal2/analysis3.txt',
+        blob_run_directory: blob_run_directory_c
+      )
 
       # override puid's so we can use the ones in the prepared iridanext.output.json.gz
       @sample1 = samples(:sample1)
@@ -23,17 +72,7 @@ module WorkflowExecutions
 
     test 'finalize completed workflow_execution' do
       workflow_execution = @workflow_execution_completed
-      blob_run_directory = @blob_run_directory
-      # Test prep
-      output_json_file_path = 'test/fixtures/files/blob_outputs/normal/iridanext.output.json.gz'
-      output_summary_file_path = 'test/fixtures/files/blob_outputs/normal/summary.txt'
 
-      make_and_upload_blob(filepath: output_json_file_path, blob_run_directory:)
-      output_summary_file_blob = make_and_upload_blob(filepath: output_summary_file_path, blob_run_directory:)
-
-      workflow_execution.blob_run_directory = blob_run_directory
-
-      # Test start
       assert 'completed', workflow_execution.state
 
       assert WorkflowExecutions::CompletionService.new(workflow_execution, {}).execute
@@ -42,15 +81,15 @@ module WorkflowExecutions
       assert_equal 1, workflow_execution.outputs.count
       # original file blob should not be the same as the output file blob, but contain the same file
       output_summary_file = workflow_execution.outputs[0]
-      assert_not_equal output_summary_file_blob.id, output_summary_file.id
-      assert_equal output_summary_file_blob.filename, output_summary_file.filename
-      assert_equal output_summary_file_blob.checksum, output_summary_file.file.checksum
+      assert_not_equal @normal_output_summary_file_blob.id, output_summary_file.id
+      assert_equal @normal_output_summary_file_blob.filename, output_summary_file.filename
+      assert_equal @normal_output_summary_file_blob.checksum, output_summary_file.file.checksum
 
       assert_equal 'finalized', workflow_execution.state
     end
 
     test 'finalize non complete workflow_execution' do
-      workflow_execution = @workflow_execution_not_completed
+      workflow_execution = workflow_executions(:irida_next_example)
 
       assert_not_equal 'completed', workflow_execution.state
 
@@ -61,16 +100,8 @@ module WorkflowExecutions
     end
 
     test 'finalize completed workflow_execution with no files' do
-      workflow_execution = @workflow_execution_completed
-      blob_run_directory = @blob_run_directory
-      # Test prep
-      output_json_file_path = 'test/fixtures/files/blob_outputs/no_files/iridanext.output.json.gz'
+      workflow_execution = @workflow_execution_no_files
 
-      make_and_upload_blob(filepath: output_json_file_path, blob_run_directory:)
-
-      workflow_execution.blob_run_directory = blob_run_directory
-
-      # Test start
       assert 'completed', workflow_execution.state
 
       assert WorkflowExecutions::CompletionService.new(workflow_execution, {}).execute
@@ -84,23 +115,7 @@ module WorkflowExecutions
 
     test 'sample outputs on samples_workflow_executions' do
       workflow_execution = @workflow_execution_with_samples
-      blob_run_directory = @blob_run_directory
-      # Test prep
-      output_json_file_path = 'test/fixtures/files/blob_outputs/normal2/iridanext.output.json.gz'
-      output_summary_file_path = 'test/fixtures/files/blob_outputs/normal2/summary.txt'
-      output_analysis1_file_path = 'test/fixtures/files/blob_outputs/normal2/analysis1.txt'
-      output_analysis2_file_path = 'test/fixtures/files/blob_outputs/normal2/analysis2.txt'
-      output_analysis3_file_path = 'test/fixtures/files/blob_outputs/normal2/analysis3.txt'
 
-      make_and_upload_blob(filepath: output_json_file_path, blob_run_directory:)
-      make_and_upload_blob(filepath: output_summary_file_path, blob_run_directory:)
-      output_analysis1_file_blob = make_and_upload_blob(filepath: output_analysis1_file_path, blob_run_directory:)
-      output_analysis2_file_blob = make_and_upload_blob(filepath: output_analysis2_file_path, blob_run_directory:)
-      output_analysis3_file_blob = make_and_upload_blob(filepath: output_analysis3_file_path, blob_run_directory:)
-
-      workflow_execution.blob_run_directory = blob_run_directory
-
-      # Test start
       assert 'completed', workflow_execution.state
 
       assert WorkflowExecutions::CompletionService.new(workflow_execution, {}).execute
@@ -114,41 +129,26 @@ module WorkflowExecutions
       assert_equal 2, workflow_execution.samples_workflow_executions[0].outputs.count
       # original file blob should not be the same as the output file blob, but contain the same file
       output1 = workflow_execution.samples_workflow_executions[0].outputs[0]
-      assert_not_equal output_analysis1_file_blob.id, output1.id
-      assert_equal output_analysis1_file_blob.filename, output1.filename
-      assert_equal output_analysis1_file_blob.checksum, output1.file.checksum
+      assert_not_equal @normal2_output_analysis1_file_blob.id, output1.id
+      assert_equal @normal2_output_analysis1_file_blob.filename, output1.filename
+      assert_equal @normal2_output_analysis1_file_blob.checksum, output1.file.checksum
       output2 = workflow_execution.samples_workflow_executions[0].outputs[1]
-      assert_not_equal output_analysis2_file_blob.id, output2.id
-      assert_equal output_analysis2_file_blob.filename, output2.filename
-      assert_equal output_analysis2_file_blob.checksum, output2.file.checksum
+      assert_not_equal @normal2_output_analysis2_file_blob.id, output2.id
+      assert_equal @normal2_output_analysis2_file_blob.filename, output2.filename
+      assert_equal @normal2_output_analysis2_file_blob.checksum, output2.file.checksum
 
       assert_equal 1, workflow_execution.samples_workflow_executions[1].outputs.count
       output3 = workflow_execution.samples_workflow_executions[1].outputs[0]
       # original file blob should not be the same as the output file blob, but contain the same file
-      assert_not_equal output_analysis3_file_blob.id, output3.id
-      assert_equal output_analysis3_file_blob.filename, output3.filename
-      assert_equal output_analysis3_file_blob.checksum, output3.file.checksum
+      assert_not_equal @normal2_output_analysis3_file_blob.id, output3.id
+      assert_equal @normal2_output_analysis3_file_blob.filename, output3.filename
+      assert_equal @normal2_output_analysis3_file_blob.checksum, output3.file.checksum
 
       assert_equal 'finalized', workflow_execution.state
     end
 
     test 'sample metadata on samples_workflow_executions' do
       workflow_execution = @workflow_execution_with_samples
-      blob_run_directory = @blob_run_directory
-      # Test prep
-      output_json_file_path = 'test/fixtures/files/blob_outputs/normal2/iridanext.output.json.gz'
-      output_summary_file_path = 'test/fixtures/files/blob_outputs/normal2/summary.txt'
-      output_analysis1_file_path = 'test/fixtures/files/blob_outputs/normal2/analysis1.txt'
-      output_analysis2_file_path = 'test/fixtures/files/blob_outputs/normal2/analysis2.txt'
-      output_analysis3_file_path = 'test/fixtures/files/blob_outputs/normal2/analysis3.txt'
-
-      make_and_upload_blob(filepath: output_json_file_path, blob_run_directory:)
-      make_and_upload_blob(filepath: output_summary_file_path, blob_run_directory:)
-      make_and_upload_blob(filepath: output_analysis1_file_path, blob_run_directory:)
-      make_and_upload_blob(filepath: output_analysis2_file_path, blob_run_directory:)
-      make_and_upload_blob(filepath: output_analysis3_file_path, blob_run_directory:)
-
-      workflow_execution.blob_run_directory = blob_run_directory
 
       # Test start
       assert 'completed', workflow_execution.state
