634ad59305b13dc38a24d22585788747c4fb7839
Chris Huynh <chris.huynh333@gmail.com>
2024-03-22 11:43:15 -0500

    Add manifest to data_export

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_28_145629) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_22_161334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
