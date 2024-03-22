012b66d132cd601025ee8a2db0b78f42b9b769fa
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-03-22 11:07:15 -0500

    refactor into smaller functions

--- app/services/workflow_executions/completion_service.rb
+++ app/services/workflow_executions/completion_service.rb
@@ -10,7 +10,7 @@ module WorkflowExecutions
 
       @workflow_execution = workflow_execution
       @storage_service = ActiveStorage::Blob.service
-      @file_blob_list = []
+      @attachable_blobs_tuple_list = []
       @output_base_path = "#{@workflow_execution.blob_run_directory}/output/"
     end
 
@@ -18,43 +18,17 @@ module WorkflowExecutions
       return false unless @workflow_execution.completed?
 
       run_output_data = download_decompress_parse_gziped_json("#{@output_base_path}iridanext.output.json.gz")
-      attachable_blobs_tuple_list = []
 
       # global run output files
       output_global_file_paths = get_output_global_file_paths(run_output_data:)
-      global_file_blob_list = []
-      output_global_file_paths&.each do |blob_file_path|
-        global_file_blob_list.append(download_and_make_new_blob(blob_file_path:))
-      end
-      attachable_blobs_tuple_list.append({ attachable: @workflow_execution,
-                                           blob_id_list: global_file_blob_list })
+      process_global_file_paths(output_global_file_paths:)
 
       # per sample output files
       output_samples_file_paths = get_output_samples_file_paths(run_output_data:)
-      output_samples_file_paths&.each do |sample_file_paths_tuple| # :sample_puid, :data_paths
-        sample_file_blob_list = []
-        sample_file_paths_tuple[:data_paths]&.each do |blob_file_path|
-          sample_file_blob_list.append(download_and_make_new_blob(blob_file_path:))
-        end
-
-        samples_workflow_execution = \
-          @workflow_execution.samples_workflow_executions \
-                             .joins(:sample) \
-                             .where(sample: { puid: sample_file_paths_tuple[:sample_puid] }) \
-                             .first
-
-        attachable_blobs_tuple_list.append({ attachable: samples_workflow_execution,
-                                             blob_id_list: sample_file_blob_list })
-      end
+      process_sample_file_paths(output_samples_file_paths:)
 
       # attach blob lists to attachables
-      unless attachable_blobs_tuple_list.empty?
-        attachable_blobs_tuple_list&.each do |tuple|
-          Attachments::CreateService.new(
-            current_user, tuple[:attachable], { files: tuple[:blob_id_list] }
-          ).execute
-        end
-      end
+      attach_blobs_to_attachables
 
       @workflow_execution.state = 'finalized'
 
@@ -85,6 +59,45 @@ module WorkflowExecutions
 
     def get_path_mapping(data_paths)
       data_paths.map { |entry| @output_base_path + entry['path'] }
+    end
+
+    def process_global_file_paths(output_global_file_paths:)
+      # Handle ouput files for workflow execution
+      global_file_blob_list = []
+      output_global_file_paths&.each do |blob_file_path|
+        global_file_blob_list.append(download_and_make_new_blob(blob_file_path:))
+      end
+      @attachable_blobs_tuple_list.append({ attachable: @workflow_execution,
+                                            blob_id_list: global_file_blob_list })
+    end
+
+    def process_sample_file_paths(output_samples_file_paths:)
+      # Handle output files for samples workflow execution
+      output_samples_file_paths&.each do |sample_file_paths_tuple| # :sample_puid, :data_paths
+        sample_file_blob_list = []
+        sample_file_paths_tuple[:data_paths]&.each do |blob_file_path|
+          sample_file_blob_list.append(download_and_make_new_blob(blob_file_path:))
+        end
+
+        samples_workflow_execution = \
+          @workflow_execution.samples_workflow_executions \
+                             .joins(:sample) \
+                             .where(sample: { puid: sample_file_paths_tuple[:sample_puid] }) \
+                             .first
+
+        @attachable_blobs_tuple_list.append({ attachable: samples_workflow_execution,
+                                              blob_id_list: sample_file_blob_list })
+      end
+    end
+
+    def attach_blobs_to_attachables
+      return if @attachable_blobs_tuple_list.empty?
+
+      @attachable_blobs_tuple_list&.each do |tuple| # :attachable, :blob_id_list
+        Attachments::CreateService.new(
+          current_user, tuple[:attachable], { files: tuple[:blob_id_list] }
+        ).execute
+      end
     end
   end
 end
