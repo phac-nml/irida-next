64ebbe505844986c02d38391242c21e36259792c
Deep Sidhu <deepsidhu85@gmail.com>
2024-03-22 13:03:40 -0500

    Updated bots create service to validate params, and updated controller unprocessable entity responses to contain the error messages from the services

--- app/services/bots/create_service.rb
+++ app/services/bots/create_service.rb
@@ -4,33 +4,62 @@ module Bots
   # Service used to Create Bot Accounts
   class CreateService < BaseService
     BotAccountCreateError = Class.new(StandardError)
-    attr_accessor :auth_object
+    attr_accessor :auth_object, :bot_user_account
 
     def initialize(user = nil, auth_object = nil, params = {})
       super(user, params)
       @auth_object = auth_object
+
+      user_params = {
+        email: "#{params[:bot_username]}@iridanext.com",
+        user_type: User.user_types[:project_bot],
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+
+      @bot_user_account = User.new(user_params)
     end
 
     def execute
       authorize! auth_object, to: :create_bot_accounts?
 
+      validate_params
+
       bot_user_account = create_bot_account
 
       create_bot_user_account_pat(bot_user_account)
+
+      bot_user_account
     rescue Bots::CreateService::BotAccountCreateError => e
-      auth_object.errors.add(:base, e.message)
-      false
+      @bot_user_account.errors.add(:base, e.message)
+      @bot_user_account
+    end
+
+    def validate_params # rubocop:disable Metrics/MethodLength
+      if params[:bot_username].blank?
+        raise BotAccountCreateError,
+              'Unable to create bot account as the bot username is required'
+      end
+
+      if params[:first_name].blank?
+        raise BotAccountCreateError,
+              'Unable to create bot account as the bot first name is required'
+      end
+
+      if params[:last_name].blank?
+        raise BotAccountCreateError,
+              'Unable to create bot account as the bot last name is required'
+      end
+
+      if params[:scopes].blank?
+        raise BotAccountCreateError,
+              'Unable to create bot account as the bot API scope must be selected'
+      end
+
+      true
     end
 
     def create_bot_account
-      user_params = {
-        email: "#{params[:bot_username]}@iridanext.com",
-        user_type: User.user_types[:project_bot],
-        first_name: 'Project',
-        last_name: 'Bot'
-      }
-
-      bot_user_account = User.new(user_params)
       bot_user_account.skip_password_validation = true
       bot_user_account.save!
 
