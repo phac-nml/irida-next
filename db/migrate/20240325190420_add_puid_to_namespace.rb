1867f42c227d0025d3652cf641d6f7dd83b7b1f3
Deep Sidhu <deepsidhu85@gmail.com>
2024-03-26 15:35:50 -0500

    Moved sql statement to update namespaces with existing project puids to outside loop

--- db/migrate/20240325190420_add_puid_to_namespace.rb
+++ db/migrate/20240325190420_add_puid_to_namespace.rb
@@ -7,22 +7,23 @@ class AddPuidToNamespace < ActiveRecord::Migration[7.1]
 
     reversible do |dir|
       dir.up do
-        Namespace.with_deleted.each do |namespace|
-          if namespace.type == Namespaces::ProjectNamespace.sti_name
-            execute <<~SQL.squish
-              UPDATE namespaces
-              set puid = projects.puid
-              FROM projects
-              WHERE projects.namespace_id = namespaces.id
-            SQL
-          else
-            puid = Irida::PersistentUniqueId.generate(namespace, time: namespace.created_at)
+        Namespace.with_deleted.where.not(type: 'Project').each do |namespace|
+          next unless namespace.type != Namespaces::ProjectNamespace.sti_name
 
-            execute <<-SQL.squish
+          puid = Irida::PersistentUniqueId.generate(namespace, time: namespace.created_at)
+
+          execute <<-SQL.squish
               UPDATE namespaces SET puid = '#{puid}' WHERE id = '#{namespace.id}'
-            SQL
-          end
+          SQL
         end
+
+        execute <<~SQL.squish
+          UPDATE namespaces
+          set puid = projects.puid
+          FROM projects
+          WHERE projects.namespace_id = namespaces.id
+        SQL
+
         change_column :namespaces, :puid, :string, null: false
       end
     end
