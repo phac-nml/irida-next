eb25f46d38e24cf9c7fe6f6167a4cfef95bf43bd
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-03-26 08:35:14 -0500

    chore: Added tests to make sure alternative files are added

--- test/components/nextflow_samplesheet_component_test.rb
+++ test/components/nextflow_samplesheet_component_test.rb
@@ -11,11 +11,30 @@ class NextflowSamplesheetComponentTest < ViewComponentTestCase
 
     assert_selector 'table' do
       assert_selector 'thead th', count: 4
+      assert_selector 'thead th:last', text: 'strandedness'
       assert_selector 'tbody tr', count: 3
       assert_selector 'tbody tr:first td:first', text: samples(:sample1).puid
       assert_selector 'tbody tr:first td:nth-child(3) select', count: 1
       assert_selector 'tbody tr:first td:nth-child(4) select', count: 1
       assert_selector 'tbody tr:first td:nth-child(5) select', count: 1
+    end
+  end
+
+  test 'with reference files' do
+    render_inline Nextflow::SamplesheetComponent.new(
+      samples: [samples(:sample1), samples(:sample2), samples(:sample3)],
+      schema: JSON.parse(File.read('test/fixtures/files/nextflow/samplesheet_schema_snvphyl.json'))
+    )
+
+    assert_selector 'table' do
+      assert_selector 'thead th', count: 4
+      assert_selector 'thead th:last', text: 'reference_assembly'
+      assert_selector 'tbody tr', count: 3
+      assert_selector 'tbody tr:first td:first', text: samples(:sample1).puid
+      assert_selector 'tbody tr:first td:nth-child(3) select', count: 1
+      assert_selector 'tbody tr:first td:nth-child(4) select', count: 1
+      assert_selector 'tbody tr:first td:nth-child(5) select', count: 1
+      assert_selector 'tbody tr:first td:last select option', count: 2
     end
   end
 end
