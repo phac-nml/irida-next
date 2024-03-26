c3477f69c2b1b19d201182270b12f0acacaf5d10
Deep Sidhu <deepsidhu85@gmail.com>
2024-03-26 15:14:15 -0500

    Updated migration to get existing project puids using sql

--- db/migrate/20240325190420_add_puid_to_namespace.rb
+++ db/migrate/20240325190420_add_puid_to_namespace.rb
@@ -9,10 +9,18 @@ class AddPuidToNamespace < ActiveRecord::Migration[7.1]
       dir.up do
         Namespace.with_deleted.each do |namespace|
           if namespace.type == Namespaces::ProjectNamespace.sti_name
-            project = Project.with_deleted.find_by(namespace:)
-            namespace.update!(puid: project.puid)
+            execute <<~SQL.squish
+              UPDATE namespaces
+              set puid = projects.puid
+              FROM projects
+              WHERE projects.namespace_id = namespaces.id
+            SQL
           else
-            namespace.update!(puid: Irida::PersistentUniqueId.generate(namespace, time: namespace.created_at))
+            puid = Irida::PersistentUniqueId.generate(namespace, time: namespace.created_at)
+
+            execute <<-SQL.squish
+              UPDATE namespaces SET puid = '#{puid}' WHERE id = '#{namespace.id}'
+            SQL
           end
         end
         change_column :namespaces, :puid, :string, null: false
