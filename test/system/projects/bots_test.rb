c3b3fe0d9aca7a80ac27ac4e3e15ad1eb525cb46
Deep Sidhu <deepsidhu85@gmail.com>
2024-04-02 17:25:13 -0500

    Added tests for empty state and creating new bot account

--- test/system/projects/bots_test.rb
+++ test/system/projects/bots_test.rb
@@ -13,9 +13,9 @@ module Projects
       @project = projects(:project1)
     end
 
-    test 'can see a list of project bot accounts' do
+    test 'can see a table listing of project bot accounts' do
       visit namespace_project_bots_path(@namespace, @project)
-      pause
+
       assert_selector 'h1', text: I18n.t(:'projects.bots.index.title')
       assert_selector 'p', text: I18n.t(:'projects.bots.index.subtitle')
 
@@ -34,7 +34,56 @@ module Projects
       assert_selector 'tr', count: 20 + header_row_count
     end
 
+    test 'can see an empty state for table listing of project bot accounts' do
+      project = projects(:project2)
+      visit namespace_project_bots_path(@namespace, project)
+
+      assert_selector 'h1', text: I18n.t(:'projects.bots.index.title')
+      assert_selector 'p', text: I18n.t(:'projects.bots.index.subtitle')
+
+      assert_selector 'tr', count: 0
+
+      assert_no_selector 'a', text: /\A#{I18n.t(:'components.pagination.next')}\Z/
+      assert_no_selector 'a', text: I18n.t(:'components.pagination.previous')
+
+      within('div.empty_state_message') do
+        assert_text I18n.t(:'projects.bots.index.bot_listing.empty_state.title')
+        assert_text I18n.t(:'projects.bots.index.bot_listing.empty_state.description')
+      end
+    end
+
     test 'can create a new project bot account' do
+      project = projects(:project2)
+      visit namespace_project_bots_path(@namespace, project)
+
+      assert_selector 'h1', text: I18n.t(:'projects.bots.index.title')
+      assert_selector 'p', text: I18n.t(:'projects.bots.index.subtitle')
+
+      assert_selector 'a', text: I18n.t(:'projects.bots.index.add_new_bot'), count: 1
+
+      assert_selector 'tr', count: 0
+
+      within('div.empty_state_message') do
+        assert_text I18n.t(:'projects.bots.index.bot_listing.empty_state.title')
+        assert_text I18n.t(:'projects.bots.index.bot_listing.empty_state.description')
+      end
+
+      click_link I18n.t(:'projects.bots.index.add_new_bot')
+
+      within('dialog') do
+        assert_selector 'h1', text: I18n.t(:'projects.bots.index.bot_listing.new_bot_modal.title')
+        assert_selector 'p', text: I18n.t(:'projects.bots.index.bot_listing.new_bot_modal.description')
+
+        fill_in 'Token Name', with: 'Uploader'
+        find('#bot_access_level').find('option',
+                                       text: I18n.t('activerecord.models.member.access_level.analyst')).select_option
+
+        all('input[type=checkbox]').each(&:click)
+
+        click_button I18n.t(:'projects.bots.index.bot_listing.new_bot_modal.submit')
+      end
+
+      assert_selector 'tr', count: 1 + header_row_count
     end
 
     test 'can delete a project bot account' do
