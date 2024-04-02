d1801af88671dd22ad41d689ae3ea90f4276f567
Chris Huynh <chris.huynh333@gmail.com>
2024-04-02 15:28:34 -0500

    Add additional assertions for test

--- test/jobs/data_exports/cleanup_job_test.rb
+++ test/jobs/data_exports/cleanup_job_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 module DataExports
   class CleanupJobTest < ActiveJob::TestCase
+    def setup
+      @data_export = data_exports(:data_export_one)
+    end
+
     test 'cleanup expired export' do
+      assert @data_export.valid?
+      assert @data_export.file.valid?
       travel 3.days
       assert_no_difference -> { ActiveStorage::Attachment.count },
                            -> { DataExport.count } do
@@ -16,6 +22,8 @@ module DataExports
                         -> { DataExport.count } => -1 do
         DataExports::CleanupJob.perform_now
       end
+
+      assert_raises(ActiveRecord::RecordNotFound) { @data_export.reload }
     end
   end
 end
