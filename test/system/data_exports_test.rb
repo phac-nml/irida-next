3b53f61bef4f7a7f3407a08352ffc7508fd33823
Chris Huynh <chris.huynh333@gmail.com>
2024-04-03 11:19:33 -0500

    Add tests

--- test/system/data_exports_test.rb
+++ test/system/data_exports_test.rb
@@ -180,4 +180,268 @@ class DataExportsTest < ApplicationSystemTestCase
       assert_no_text @data_export2.id
     end
   end
+
+  test 'member with access level >= analyst can see create export button' do
+    login_as users(:john_doe)
+
+    # project samples page
+    visit namespace_project_samples_url(@namespace, @project)
+    assert_selector 'a', text: I18n.t('projects.samples.index.create_export_button'), count: 1
+
+    # group samples page
+    visit group_samples_url(@namespace)
+    assert_selector 'a', text: I18n.t('projects.samples.index.create_export_button'), count: 1
+  end
+
+  test 'user with access level == guest cannot see create export button' do
+    login_as users(:ryan_doe)
+
+    # project samples page
+    visit namespace_project_samples_url(@namespace, @project)
+    assert_no_selector 'a', text: I18n.t('projects.samples.index.create_export_button')
+
+    # group samples page
+    visit group_samples_url(@namespace)
+    assert_no_selector 'a', text: I18n.t('projects.samples.index.create_export_button'), count: 1
+  end
+
+  test 'create export from project samples page' do
+    login_as users(:john_doe)
+
+    visit data_exports_path
+    within %(#data-exports-table-body) do
+      assert_selector 'tr', count: 2
+      assert_no_text 'test data export'
+    end
+    # project samples page
+    visit namespace_project_samples_url(@namespace, @project)
+    assert_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                    text: I18n.t('projects.samples.index.create_export_button')
+
+    within %(#samples-table) do
+      find("input[type='checkbox'][value='#{@sample1.id}']").click
+    end
+
+    assert_no_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                       text: I18n.t('projects.samples.index.create_export_button')
+
+    click_link I18n.t('projects.samples.index.create_export_button'), match: :first
+    within 'dialog[open].dialog--size-lg' do
+      assert_text I18n.t('data_exports.new_export_modal.name_label')
+      assert_text I18n.t('data_exports.new_export_modal.email_label')
+      assert_text "#{I18n.t('data_exports.new_export_modal.sample_count')} 1"
+
+      find('input#data_export_name').fill_in with: 'test data export'
+      find("input[type='checkbox'][id='data_export_email_notification']").click
+      click_button I18n.t('data_exports.new_export_modal.submit_button')
+    end
+
+    within %(#data-exports-table-body) do
+      assert_selector 'tr', count: 3
+      assert_text 'test data export'
+    end
+  end
+
+  test 'create export from group samples page' do
+    login_as users(:john_doe)
+
+    visit data_exports_path
+    within %(#data-exports-table-body) do
+      assert_selector 'tr', count: 2
+      assert_no_text 'test data export'
+    end
+    # project samples page
+    visit group_samples_url(@namespace)
+    assert_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                    text: I18n.t('projects.samples.index.create_export_button')
+
+    within %(#samples-table) do
+      find("input[type='checkbox'][value='#{@sample1.id}']").click
+    end
+
+    assert_no_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                       text: I18n.t('projects.samples.index.create_export_button')
+
+    click_link I18n.t('projects.samples.index.create_export_button'), match: :first
+    within 'dialog[open].dialog--size-lg' do
+      assert_text I18n.t('data_exports.new_export_modal.name_label')
+      assert_text I18n.t('data_exports.new_export_modal.email_label')
+      assert_text "#{I18n.t('data_exports.new_export_modal.sample_count')} 1"
+
+      find('input#data_export_name').fill_in with: 'test data export'
+      find("input[type='checkbox'][id='data_export_email_notification']").click
+      click_button I18n.t('data_exports.new_export_modal.submit_button')
+    end
+
+    within %(#data-exports-table-body) do
+      assert_selector 'tr', count: 3
+      assert_text 'test data export'
+    end
+  end
+
+  test 'checking samples on different page does not affect current page\'s export samples' do
+    login_as users(:john_doe)
+    subgroup12a = groups(:subgroup_twelve_a)
+    project29 = projects(:project29)
+    sample32 = samples(:sample32)
+
+    visit namespace_project_samples_url(subgroup12a, project29)
+    assert_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                    text: I18n.t('projects.samples.index.create_export_button')
+
+    within %(#samples-table) do
+      find("input[type='checkbox'][value='#{sample32.id}']").click
+    end
+
+    assert_no_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                       text: I18n.t('projects.samples.index.create_export_button')
+
+    visit namespace_project_samples_url(@namespace, @project)
+    assert_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                    text: I18n.t('projects.samples.index.create_export_button')
+
+    within %(#samples-table) do
+      find("input[type='checkbox'][value='#{@sample1.id}']").click
+    end
+
+    assert_no_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                       text: I18n.t('projects.samples.index.create_export_button')
+
+    click_link I18n.t('projects.samples.index.create_export_button'), match: :first
+    within 'dialog[open].dialog--size-lg' do
+      assert_text "#{I18n.t('data_exports.new_export_modal.sample_count')} 1"
+    end
+  end
+
+  test 'member with access level >= analyst can see create export button' do
+    login_as users(:john_doe)
+
+    # project samples page
+    visit namespace_project_samples_url(@namespace, @project)
+    assert_selector 'a', text: I18n.t('projects.samples.index.create_export_button'), count: 1
+
+    # group samples page
+    visit group_samples_url(@namespace)
+    assert_selector 'a', text: I18n.t('projects.samples.index.create_export_button'), count: 1
+  end
+
+  test 'user with access level == guest cannot see create export button' do
+    login_as users(:ryan_doe)
+
+    # project samples page
+    visit namespace_project_samples_url(@namespace, @project)
+    assert_no_selector 'a', text: I18n.t('projects.samples.index.create_export_button')
+
+    # group samples page
+    visit group_samples_url(@namespace)
+    assert_no_selector 'a', text: I18n.t('projects.samples.index.create_export_button'), count: 1
+  end
+
+  test 'create export from project samples page' do
+    login_as users(:john_doe)
+
+    visit data_exports_path
+    within %(#data-exports-table-body) do
+      assert_selector 'tr', count: 2
+      assert_no_text 'test data export'
+    end
+    # project samples page
+    visit namespace_project_samples_url(@namespace, @project)
+    assert_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                    text: I18n.t('projects.samples.index.create_export_button')
+
+    within %(#samples-table) do
+      find("input[type='checkbox'][value='#{@sample1.id}']").click
+    end
+
+    assert_no_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                       text: I18n.t('projects.samples.index.create_export_button')
+
+    click_link I18n.t('projects.samples.index.create_export_button'), match: :first
+    within 'dialog[open].dialog--size-lg' do
+      assert_text I18n.t('data_exports.new_export_modal.name_label')
+      assert_text I18n.t('data_exports.new_export_modal.email_label')
+      assert_text "#{I18n.t('data_exports.new_export_modal.sample_count')} 1"
+
+      find('input#data_export_name').fill_in with: 'test data export'
+      find("input[type='checkbox'][id='data_export_email_notification']").click
+      click_button I18n.t('data_exports.new_export_modal.submit_button')
+    end
+
+    within %(#data-exports-table-body) do
+      assert_selector 'tr', count: 3
+      assert_text 'test data export'
+    end
+  end
+
+  test 'create export from group samples page' do
+    login_as users(:john_doe)
+
+    visit data_exports_path
+    within %(#data-exports-table-body) do
+      assert_selector 'tr', count: 2
+      assert_no_text 'test data export'
+    end
+    # project samples page
+    visit group_samples_url(@namespace)
+    assert_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                    text: I18n.t('projects.samples.index.create_export_button')
+
+    within %(#samples-table) do
+      find("input[type='checkbox'][value='#{@sample1.id}']").click
+    end
+
+    assert_no_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                       text: I18n.t('projects.samples.index.create_export_button')
+
+    click_link I18n.t('projects.samples.index.create_export_button'), match: :first
+    within 'dialog[open].dialog--size-lg' do
+      assert_text I18n.t('data_exports.new_export_modal.name_label')
+      assert_text I18n.t('data_exports.new_export_modal.email_label')
+      assert_text "#{I18n.t('data_exports.new_export_modal.sample_count')} 1"
+
+      find('input#data_export_name').fill_in with: 'test data export'
+      find("input[type='checkbox'][id='data_export_email_notification']").click
+      click_button I18n.t('data_exports.new_export_modal.submit_button')
+    end
+
+    within %(#data-exports-table-body) do
+      assert_selector 'tr', count: 3
+      assert_text 'test data export'
+    end
+  end
+
+  test 'checking samples on different page does not affect current page\'s export samples' do
+    login_as users(:john_doe)
+    subgroup12a = groups(:subgroup_twelve_a)
+    project29 = projects(:project29)
+    sample32 = samples(:sample32)
+
+    visit namespace_project_samples_url(subgroup12a, project29)
+    assert_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                    text: I18n.t('projects.samples.index.create_export_button')
+
+    within %(#samples-table) do
+      find("input[type='checkbox'][value='#{sample32.id}']").click
+    end
+
+    assert_no_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                       text: I18n.t('projects.samples.index.create_export_button')
+
+    visit namespace_project_samples_url(@namespace, @project)
+    assert_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                    text: I18n.t('projects.samples.index.create_export_button')
+
+    within %(#samples-table) do
+      find("input[type='checkbox'][value='#{@sample1.id}']").click
+    end
+
+    assert_no_selector 'a.pointer-events-none.cursor-not-allowed.bg-slate-100.text-slate-600',
+                       text: I18n.t('projects.samples.index.create_export_button')
+
+    click_link I18n.t('projects.samples.index.create_export_button'), match: :first
+    within 'dialog[open].dialog--size-lg' do
+      assert_text "#{I18n.t('data_exports.new_export_modal.sample_count')} 1"
+    end
+  end
 end
