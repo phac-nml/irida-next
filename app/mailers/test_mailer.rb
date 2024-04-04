72053bcc0e8bfe65de56d78f003406c1425b67a0
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-04-04 09:47:29 -0500

    removing test mailer

--- app/mailers/test_mailer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Test Mailer
-class TestMailer < ApplicationMailer
-  def test_email
-    @user = params[:user]
-    mail(to: @user.email, subject: 'TEST')
-  end
-end
--- test/mailers/previews/test_mailer_preview.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Preview all emails at http://localhost:3000/rails/mailers/test_mailer
-class TestMailerPreview < ActionMailer::Preview
-  def test_email
-    TestMailer.with(user: User.first).test_email
-  end
-end
