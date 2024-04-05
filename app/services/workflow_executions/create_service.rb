862459f5d36ec1c1fa1da3d703d2049d9c219053
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-04-05 15:18:21 -0500

    chore: refactor code to reduce rubocop disables

--- app/services/workflow_executions/create_service.rb
+++ app/services/workflow_executions/create_service.rb
@@ -12,11 +12,11 @@ module WorkflowExecutions
     def execute
       return false if params.empty?
 
-      sanitize_workflow_params
-
       @workflow_execution = WorkflowExecution.new(params)
       @workflow_execution.submitter = current_user
       @workflow_execution.state = 'new'
+
+      @workflow_execution.workflow_params = sanitized_workflow_params if @workflow_execution.valid?
 
       if @workflow_execution.save
         WorkflowExecutionPreparationJob.set(wait_until: 30.seconds.from_now).perform_later(@workflow_execution)
@@ -25,30 +25,35 @@ module WorkflowExecutions
       @workflow_execution
     end
 
-    def sanitize_workflow_params # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Metrics/CyclomaticComplexity
-      return unless params.key?(:metadata) &&
-                    params[:metadata].key?(:workflow_name) &&
-                    params[:metadata].key?(:workflow_version)
-
+    def sanitized_workflow_params # rubocop:disable Metrics/AbcSize
       workflow = Irida::Pipelines.find_pipeline_by(params[:metadata][:workflow_name],
                                                    params[:metadata][:workflow_version])
       workflow_schema = JSON.parse(workflow.schema_loc.read)
 
       # remove blank values
-      params[:workflow_params].compact_blank!
+      sanitized_params = params[:workflow_params].compact_blank
 
       workflow_schema['definitions'].each do |_item, definition|
         definition['properties'].each do |name, property|
           formatted_name = format_name_as_arg(name)
-          next unless params[:workflow_params].key?(formatted_name.to_sym)
-
-          case property['type']
-          when 'integer'
-            params[:workflow_params][formatted_name.to_sym] = params[:workflow_params][formatted_name.to_sym].to_i
-          when 'number'
-            params[:workflow_params][formatted_name.to_sym] = params[:workflow_params][formatted_name.to_sym].to_f
+          if sanitized_params.key?(formatted_name.to_sym)
+            sanitized_params[formatted_name.to_sym] = sanitize_workflow_param(property,
+                                                                              sanitized_params[formatted_name.to_sym])
           end
         end
+      end
+
+      sanitized_params
+    end
+
+    def sanitize_workflow_param(property, value)
+      case property['type']
+      when 'integer'
+        value.to_i
+      when 'number'
+        value.to_f
+      else
+        value
       end
     end
   end
