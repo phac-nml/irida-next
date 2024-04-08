7ca6f769d148e835d1fabaad084cbfa372061142
Jeff Thiessen <JeffreyThiessen@users.noreply.github.com>
2024-04-08 07:41:36 -0500

    add custom text to signin success/failure popup (#517)
    * add custom text to signin success/failure popup
    
    * handle edge cases

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,7 +11,7 @@ module Users
 
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: OmniAuth::Utils.camelize(action_name))
+        set_flash_message(:notice, :success, kind: action_kind)
       else
         # Removing extra and credentials as it can overflow some session stores
         session['devise.omniauth_data'] = request.env['omniauth.auth'].except(:extra, :credentials)
@@ -25,10 +25,15 @@ module Users
 
     def failure
       if @user.respond_to?(:errors) && @user.errors.present? && @user.errors.full_messages.present?
-        set_flash_message :alert, :failure, kind: OmniAuth::Utils.camelize(action_name),
-                                            reason: @user.errors.full_messages.to_sentence
+        set_flash_message :alert, :failure, kind: action_kind, reason: @user.errors.full_messages.to_sentence
       end
       redirect_to new_user_session_path
+    end
+
+    private
+
+    def action_kind
+      Rails.configuration.auth_config["#{action_name}_text"] || OmniAuth::Utils.camelize(action_name)
     end
   end
 end
