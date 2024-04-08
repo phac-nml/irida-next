34f4b2ca528c80a94cf0fb203a9456777f08ae16
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-04-08 13:44:26 -0500

    chore: added workflow_params method to Pipeline and use that when building nextflow component ui

--- lib/irida/pipeline.rb
+++ lib/irida/pipeline.rb
@@ -7,6 +7,8 @@ module Irida
                   :engine, :engine_version, :url, :execute_loc,
                   :version, :schema_loc, :schema_input_loc
 
+    IGNORED_PARAMS = %w[outdir email].freeze
+
     def initialize(entry, version, schema_loc, schema_input_loc)
       @name = entry['name']
       @description = entry['description']
@@ -20,6 +22,37 @@ module Irida
       @version = version['name']
       @schema_loc = schema_loc
       @schema_input_loc = schema_input_loc
+    end
+
+    def workflow_params # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+      nextflow_schema = JSON.parse(schema_loc.read)
+      workflow_params = {}
+
+      nextflow_schema['definitions'].each do |key, definition|
+        next unless show_section?(definition['properties'])
+
+        workflow_params[key] = { title: definition['title'], description: definition['description'], properties: {} }
+
+        definition['properties'].each do |name, property|
+          next unless !property['hidden'] && IGNORED_PARAMS.exclude?(name)
+
+          workflow_params[key][:properties][name] = property.clone
+          workflow_params[key][:properties][name]['required'] =
+            definition['required'].present? && definition['required'].include?(name)
+
+          next unless key == 'input_output_options' && name == 'input'
+
+          workflow_params[key][:properties][name]['schema'] = schema_input_loc
+        end
+      end
+
+      workflow_params
+    end
+
+    private
+
+    def show_section?(properties)
+      properties.values.any? { |property| !property.key?('hidden') }
     end
   end
 end
