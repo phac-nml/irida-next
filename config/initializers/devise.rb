7781d73919a5d4e7b2e7df0e5be2a1c19ef2f55b
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-04-09 09:32:38 -0500

    feat: enabled specifying mail config in rails credentials file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -30,7 +30,11 @@ Devise.setup do |config| # rubocop:disable Metrics/BlockLength
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  if !Rails.application.credentials.action_mailer.nil? && !Rails.application.credentials.action_mailer.default_from.nil?
+    config.mailer_sender = Rails.application.credentials.action_mailer.default_from
+  else
+    config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  end
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
