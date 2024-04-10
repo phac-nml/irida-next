28a525995f7c4a8685a6de72d5d65870490e989a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-04-10 10:07:42 -0500

    chore: Fixed autos election of fastq_2 (#528)

--- app/javascript/controllers/nextflow/samplesheet_controller.js
+++ app/javascript/controllers/nextflow/samplesheet_controller.js
@@ -3,19 +3,33 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["select"];
 
+  connect() {
+    if (this.hasSelectTarget) {
+      // Find the first select selectedIndex
+      const selectedOption =
+        this.selectTargets[0].options[this.selectTargets[0].selectedIndex];
+      this.#updateMatchingSelect(
+        this.selectTargets[1],
+        selectedOption.dataset.puid,
+      );
+    }
+  }
+
   file_selected(event) {
     // find the selected option from the event target select
     const selectedOption = event.target.options[event.target.selectedIndex];
-
-    if (selectedOption.value) return;
 
     const updateSelect =
       event.target === this.selectTargets[0]
         ? this.selectTargets[1]
         : this.selectTargets[0];
 
+    this.#updateMatchingSelect(updateSelect, selectedOption.dataset.puid);
+  }
+
+  #updateMatchingSelect(updateSelect, puid) {
     const index = [...updateSelect.options].findIndex(
-      (options) => options.dataset.puid === selectedOption.dataset.puid,
+      (options) => options.dataset.puid === puid,
     );
 
     if (index > -1) {
