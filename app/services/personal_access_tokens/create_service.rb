a011fcb541e2cd73c4a6156f737ddbcddde26d19
Deep Sidhu <deepsidhu85@gmail.com>
2024-04-10 13:56:34 -0500

    Added error handling to personal access tokens create service, added tests

--- app/services/personal_access_tokens/create_service.rb
+++ app/services/personal_access_tokens/create_service.rb
@@ -4,23 +4,40 @@ module PersonalAccessTokens
   # Service used to create personal access tokens
   class CreateService < BaseService
     PersonalAccessTokenCreateError = Class.new(StandardError)
-    attr_accessor :namespace, :bot_user
+    attr_accessor :namespace, :bot_user, :personal_access_token
 
     def initialize(user, params, namespace = nil, bot_user = nil)
       super(user, params)
 
       @bot_user = bot_user
       @namespace = namespace
+      @personal_access_token = PersonalAccessToken.new(params.merge(user: bot_user.nil? ? current_user : bot_user))
     end
 
     def execute
       authorize! current_user, to: :generate_bot_personal_access_token? if bot_user.nil?
       authorize! namespace, to: :generate_bot_personal_access_token? unless bot_user.nil?
 
-      personal_access_token = PersonalAccessToken.new(params.merge(user: bot_user.nil? ? current_user : bot_user))
+      validate_params
+
       personal_access_token.save
 
       personal_access_token
+    rescue PersonalAccessTokens::CreateService::PersonalAccessTokenCreateError => e
+      personal_access_token.errors.add(:base, e.message)
+      personal_access_token
+    end
+
+    def validate_params
+      if params[:name].blank?
+        raise PersonalAccessTokenCreateError,
+              I18n.t('services.personal_access_tokens.create.required.token_name')
+      end
+
+      return if params[:scopes].present?
+
+      raise PersonalAccessTokenCreateError,
+            I18n.t('services.personal_access_tokens.create.required.scopes')
     end
   end
 end
---
+++ test/services/personal_access_tokens/create_service_test.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module PersonalAccessTokens
+  class CreateServiceTest < ActiveSupport::TestCase
+    def setup
+      @user = users(:john_doe)
+      @project = projects(:project1)
+    end
+
+    test 'create new personal access token for bot account' do
+      valid_params = {
+        name: 'Uploader',
+        scopes: %w[read_api api]
+      }
+
+      namespace_bot = namespace_bots(:project1_bot)
+
+      assert_difference -> { PersonalAccessToken.count } => 1 do
+        PersonalAccessTokens::CreateService.new(@user, valid_params, @project.namespace, namespace_bot.user).execute
+      end
+    end
+
+    test 'create new personal access token for bot account with missing token name' do
+      valid_params = {
+        scopes: %w[read_api api]
+      }
+
+      namespace_bot = namespace_bots(:project1_bot)
+
+      assert_difference -> { PersonalAccessToken.count } => 0 do
+        PersonalAccessTokens::CreateService.new(@user, valid_params, @project.namespace, namespace_bot.user).execute
+      end
+    end
+
+    test 'create new personal access token for bot account with missing scopes' do
+      valid_params = {
+        name: 'Uploader'
+      }
+
+      namespace_bot = namespace_bots(:project1_bot)
+
+      assert_difference -> { PersonalAccessToken.count } => 0 do
+        PersonalAccessTokens::CreateService.new(@user, valid_params, @project.namespace, namespace_bot.user).execute
+      end
+    end
+
+    test 'create new personal access token for user' do
+      valid_params = {
+        name: 'Uploader',
+        scopes: %w[read_api api]
+      }
+
+      assert_difference -> { PersonalAccessToken.count } => 1 do
+        PersonalAccessTokens::CreateService.new(@user, valid_params).execute
+      end
+    end
+
+    test 'create new personal access token for user with missing token name' do
+      valid_params = {
+        scopes: %w[read_api api]
+      }
+
+      assert_difference -> { PersonalAccessToken.count } => 0 do
+        PersonalAccessTokens::CreateService.new(@user, valid_params).execute
+      end
+    end
+
+    test 'create new personal access token for user with missing scopes' do
+      valid_params = {
+        name: 'Uploader'
+      }
+
+      assert_difference -> { PersonalAccessToken.count } => 0 do
+        PersonalAccessTokens::CreateService.new(@user, valid_params).execute
+      end
+    end
+  end
+end
