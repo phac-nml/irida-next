fd380a026eeae3a365a740c3972309b20798a7b6
Deep Sidhu <deepsidhu85@gmail.com>
2024-04-10 14:16:37 -0500

    Added lookbook preview

---
+++ test/components/previews/multi_level_menu_component_preview.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class MultiLevelMenuComponentPreview < ViewComponent::Preview
+  # Default Multi level menu
+  # ---------------------------
+  #
+  def default; end
+end
