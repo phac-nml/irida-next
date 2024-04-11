e7931617c8b932c1ab25d83832de0fc3d6bd1933
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-04-11 14:20:40 -0500

    chore: Added a performance to adding samples to hidden fields

--- app/javascript/controllers/sessionstorage_amend_form_controller.js
+++ app/javascript/controllers/sessionstorage_amend_form_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
+import _ from "lodash";
 
 export default class extends Controller {
   static targets = ["field"];
@@ -13,22 +14,35 @@ export default class extends Controller {
 
   connect() {
     const storageValues = JSON.parse(
-      sessionStorage.getItem(this.storageKeyValue)
+      sessionStorage.getItem(this.storageKeyValue),
     );
 
     if (storageValues) {
-      for (const storageValue of storageValues) {
-        const element = document.createElement("input");
-        element.type = "hidden";
-        element.id = this.fieldNameValue;
-        element.name = this.fieldNameValue;
-        element.value = storageValue;
-        this.fieldTarget.appendChild(element);
-      }
+      const chunked = _.chunk(storageValues, 100);
+      const div = document.createElement("div");
+
+      // iterate over each chunk and add the id's as hidden inputs to div. After each chunk, use a timeout to allow other code to run
+      chunked.forEach((chunk) => {
+        setTimeout(() => {
+          chunk.forEach((value) => {
+            div.appendChild(this.#createHiddenInput(value));
+          });
+        }, 100);
+      });
+      this.fieldTarget.appendChild(div);
     }
   }
 
   clear() {
     sessionStorage.removeItem(this.storageKeyValue);
+  }
+
+  #createHiddenInput(value) {
+    const element = document.createElement("input");
+    element.type = "hidden";
+    element.id = this.fieldNameValue;
+    element.name = this.fieldNameValue;
+    element.value = value;
+    return element;
   }
 }
