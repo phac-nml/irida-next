c008f463bae57d9f6bfb4e0889d750505b157123
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-04-12 08:13:04 -0500

    chore: Cleaned up rubocop warnings

--- app/controllers/workflow_executions/metadata_controller.rb
+++ app/controllers/workflow_executions/metadata_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module WorkflowExecutions
+  # Controller for metadata actions within a workflow execution
   class MetadataController < ApplicationController
     respond_to :turbo_stream
 
