e5a33dfaeecacfd6d13b7dbd40ac05c3863ff9c8
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-04-12 09:17:09 -0500

    chore: refactor samplesheet_component to identify cell_type outside of row_component to save processing time

--- app/components/nextflow/samplesheet_component.rb
+++ app/components/nextflow/samplesheet_component.rb
@@ -11,11 +11,35 @@ module Nextflow
       extract_properties(schema)
     end
 
+    private
+
     def extract_properties(schema)
       @properties = schema['items']['properties']
-      @properties.each_key do |property|
+      @required_file_inputs = []
+      @properties.each do |property, entry|
         @properties[property]['required'] = schema['items']['required'].include?(property)
+        @properties[property]['cell_type'] = identify_cell_type(property, entry)
       end
+    end
+
+    def identify_cell_type(property, entry)
+      return 'sample_cell' if property == 'sample'
+
+      return 'fastq_cell' if property.match(/fastq_\d+/)
+
+      return 'file_cell' if check_for_file(entry)
+
+      return 'metadata_cell' if entry['meta'].present?
+
+      return 'dropdown_cell' if entry['enum'].present?
+
+      'input_cell'
+    end
+
+    def check_for_file(entry)
+      entry['format'] == 'file-path' || (entry.key?('anyOf') && entry['anyOf'].any? do |e|
+        e['format'] == 'file-path'
+      end)
     end
   end
 end
