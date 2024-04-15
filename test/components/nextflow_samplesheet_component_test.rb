db44e73063ca39c5081c98b999da5cc190e6c3f4
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-04-15 13:16:59 -0500

    chore: More test work

--- test/components/nextflow_samplesheet_component_test.rb
+++ test/components/nextflow_samplesheet_component_test.rb
@@ -19,39 +19,30 @@ class NextflowSamplesheetComponentTest < ApplicationSystemTestCase
   end
 
   test 'with reference files' do
-    render_inline Nextflow::SamplesheetComponent.new(
-      samples: [samples(:sample1), samples(:sample2), samples(:sample3)],
-      schema: JSON.parse(File.read('test/fixtures/files/nextflow/samplesheet_schema_snvphyl.json')),
-      fields: %w[metadata_1 metadata_2 metadata_3],
-      namespace_id: 'KDSLFJSDLKFJLKDSJF'
-    )
+    visit('/rails/view_components/nextflow_samplesheet_component/default?schema_file=samplesheet_schema_snvphyl.json')
 
     assert_selector '.sample-sheet table' do
       assert_selector 'thead th', count: 4
-      assert_selector 'thead th:last', text: 'reference_assembly'
-      assert_selector 'tbody tr', count: 3
-      assert_selector 'tbody tr:first td:first', text: samples(:sample1).puid
-      assert_selector 'tbody tr:first td > select', count: 3
+      assert_selector 'thead th:last-of-type', text: 'REFERENCE_ASSEMBLY'
+      assert_selector 'tbody tr', count: 2
+      assert_selector 'tbody tr:first-of-type td:first-of-type', text: Sample.first.puid
+      assert_selector 'tbody tr:first-of-type td > select', count: 3
+      assert_selector 'tbody tr:first-of-type td:last-of-type > select option', count: 1
     end
   end
 
   test 'with metadata' do
-    render_inline Nextflow::SamplesheetComponent.new(
-      samples: [samples(:sample43), samples(:sample44)],
-      schema: JSON.parse(File.read('test/fixtures/files/nextflow/samplesheet_schema_meta.json')),
-      fields: %w[insdc_accession],
-      namespace_id: 'KDSLFJSDLKFJLKDSJF'
-    )
+    visit('/rails/view_components/nextflow_samplesheet_component/default?schema_file=samplesheet_schema_meta.json')
 
     assert_selector '.sample-sheet table' do |table|
       table.assert_selector 'thead th', count: 3
-      table.assert_selector 'thead th:last select', count: 1
-      table.assert_selector 'thead th:last select', text: 'insdc_accession'
+      table.assert_selector 'thead th:last-of-type select', count: 1
+      table.assert_selector 'thead th:last-of-type select', text: 'insdc_accession'
       table.assert_selector 'tbody tr', count: 2
-      table.assert_selector 'tbody tr:first td:first', text: samples(:sample43).puid
-      table.assert_selector 'tbody tr:first', text: 'ERR86724108'
-      table.assert_selector 'tbody tr:last td:first', text: samples(:sample44).puid
-      table.assert_selector 'tbody tr:last', text: 'ERR31551163'
+      table.assert_selector 'tbody tr:first-of-type td:first-of-type', text: Sample.first.puid
+      table.assert_selector 'tbody tr:first-of-type', text: 'ERR86724108'
+      table.assert_selector 'tbody tr:last-of-type td:first-of-type', text: Sample.second.puid
+      table.assert_selector 'tbody tr:last-of-type', text: 'ERR31551163'
 
       assert_selector 'thead th:last' do |_td|
         click_on 'insdc_accession'
