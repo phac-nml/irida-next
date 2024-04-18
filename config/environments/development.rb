65ee6fb89e5a648ad55bc7f3e07b8731a392958f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-04-18 13:22:54 -0500

    chore: Reverted unnecessary changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,8 +78,6 @@ Rails.application.configure do
   # Raises error for missing translations.
   config.i18n.raise_on_missing_translations = true
 
-  config.active_storage.service = :local
-
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
