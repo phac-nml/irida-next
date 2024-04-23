793de21bd49da43f6a45f03b031f2de17cd111f1
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-04-23 09:54:57 -0500

    fixing tests

--- app/services/projects/create_service.rb
+++ app/services/projects/create_service.rb
@@ -37,7 +37,7 @@ module Projects
                     Member.user_has_namespace_maintainer_access?(current_user,
                                                                  namespace)
 
-      Members::CreateService.new(current_user, project.namespace, false, {
+      Members::CreateService.new(current_user, project.namespace, {
                                    user: current_user,
                                    access_level: Member::AccessLevel::OWNER
                                  }).execute
