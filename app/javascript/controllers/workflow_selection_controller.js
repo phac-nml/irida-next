7609edf441dbbfd2d0224490e4db0b82fba73d2b
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-04-24 09:58:16 -0500

    Workflow Submission: Clean up dialog to use 1 form (#561)
    * chore: Working cleaning up workflow selection modal
    
    * chore: Cleaned up loading style
    
    * chore: Prevent dialog from closing once the submission type is selected
    
    * chore: Add back the escape ability
    
    * chore: Moved class comment directly above class
    
    * chore: Reverted file
    
    * chore: Used rails helpers to create hidden fields
    
    * chore: Cleaned up loading message

---
+++ app/javascript/controllers/workflow_selection_controller.js
@@ -0,0 +1,62 @@
+import { Controller } from "@hotwired/stimulus";
+
+function preventEscapeListener(event) {
+  if (event.key === "Escape") {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+}
+
+// Connects to data-controller="workflow-selection"
+export default class extends Controller {
+  static targets = ["workflow", "workflowName", "workflowVersion", "form"];
+  static outlets = ["selection"];
+
+  connect() {
+    document.addEventListener("turbo:submit-end", preventEscapeListener);
+  }
+
+  disconnect() {
+    this.removeEscapeListener();
+  }
+
+  removeEscapeListener() {
+    document.removeEventListener("keydown", preventEscapeListener, true);
+  }
+
+  preventClosingDialog() {
+    document.querySelector(".dialog--close").classList.add("hidden");
+    document.addEventListener("keydown", preventEscapeListener, true);
+  }
+
+  selectWorkflow({ params }) {
+    this.preventClosingDialog();
+
+    for (const workflow of this.workflowTargets) {
+      if (
+        params.workflowname !==
+          workflow.dataset.workflowSelectionWorkflownameParam ||
+        params.workflowversion !==
+          workflow.dataset.workflowSelectionWorkflowversionParam
+      ) {
+        workflow.classList.add("hidden");
+      } else {
+        workflow.disabled = true;
+        workflow.querySelector(".ws-default").classList.add("hidden");
+        workflow.querySelector(".ws-loading").classList.remove("hidden");
+
+        this.workflowNameTarget.value = params.workflowname;
+        this.workflowVersionTarget.value = params.workflowversion;
+
+        // Update the text inside ws_loading
+        const wsLoading = workflow.querySelector(".ws-loading-text");
+        wsLoading.textContent = wsLoading.textContent.replace(
+          "COUNT_PLACEHOLDER",
+          this.selectionOutlet.getTotal(),
+        );
+      }
+    }
+
+    this.formTarget.requestSubmit();
+  }
+}
