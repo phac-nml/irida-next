2c65a911462cf0c9c4bfe403ef6d53fba8c54cdc
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-04-24 13:19:00 -0500

    test cases for status job

--- test/jobs/workflow_execution_status_job_test.rb
+++ test/jobs/workflow_execution_status_job_test.rb
@@ -48,7 +48,7 @@ class WorkflowExecutionStatusJobTest < ActiveJobTestCase
       @stubs.get(endpoint) { |_env| raise Faraday::ConnectionFailed }
       @stubs.get(endpoint) { |_env| raise Faraday::ConnectionFailed }
       @stubs.get(endpoint) { |_env| raise Faraday::ConnectionFailed }
-      # a success after 5 attempts
+      # a success after 5 failed attempts
       @stubs.get(endpoint) do |_env|
         [
           200,
@@ -65,6 +65,62 @@ class WorkflowExecutionStatusJobTest < ActiveJobTestCase
     end
 
     assert_performed_jobs 6
+    assert @workflow_execution.reload.completing?
+  end
+
+  test 'repeated api exception errors' do
+    mock_client = connection_builder(stubs: @stubs, connection_count: 3)
+
+    Integrations::Ga4ghWesApi::V1::ApiConnection.stub :new, mock_client do
+      endpoint = "/runs/#{@workflow_execution.run_id}/status"
+      @stubs.get(endpoint) { |_env| raise Faraday::BadRequestError }
+      @stubs.get(endpoint) { |_env| raise Faraday::BadRequestError }
+      @stubs.get(endpoint) { |_env| raise Faraday::BadRequestError }
+      # a success (never reached) after 3 failed attempts
+      @stubs.get(endpoint) do |_env|
+        [
+          200,
+          { 'Content-Type': 'application/json' },
+          {
+            run_id: @workflow_execution.run_id,
+            state: 'COMPLETE'
+          }
+        ]
+      end
+
+      WorkflowExecutionStatusJob.perform_later(@workflow_execution)
+      perform_enqueued_jobs_one_at_a_time(only_class: WorkflowExecutionStatusJob)
+    end
+
+    assert_performed_jobs 3
+    @workflow_execution.reload
+    assert @workflow_execution.error?
+    assert @workflow_execution.error_code == 400
+  end
+
+  test 'api exception error then a success' do
+    mock_client = connection_builder(stubs: @stubs, connection_count: 3)
+
+    Integrations::Ga4ghWesApi::V1::ApiConnection.stub :new, mock_client do
+      endpoint = "/runs/#{@workflow_execution.run_id}/status"
+      @stubs.get(endpoint) { |_env| raise Faraday::BadRequestError }
+      # a success after 1 failed attempt
+      @stubs.get(endpoint) do |_env|
+        [
+          200,
+          { 'Content-Type': 'application/json' },
+          {
+            run_id: @workflow_execution.run_id,
+            state: 'COMPLETE'
+          }
+        ]
+      end
+
+      WorkflowExecutionStatusJob.perform_later(@workflow_execution)
+      perform_enqueued_jobs_one_at_a_time(only_class: WorkflowExecutionStatusJob)
+    end
+
+    assert_performed_jobs 2
     assert @workflow_execution.reload.completing?
   end
 end
