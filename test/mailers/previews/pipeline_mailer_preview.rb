91f6c32410d773d32d4e744b648684ebb2069b28
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-04-25 14:27:26 -0500

    making drop-down locale work within action mailer previews

--- test/mailers/previews/pipeline_mailer_preview.rb
+++ test/mailers/previews/pipeline_mailer_preview.rb
@@ -4,23 +4,13 @@
 class PipelineMailerPreview < ActionMailer::Preview
   def complete_email
     workflow_execution = WorkflowExecution.first
-    PipelineMailer.complete_email(workflow_execution)
-  end
-
-  def complete_email_in_french
-    workflow_execution = WorkflowExecution.first
-    workflow_execution.submitter.locale = :fr
+    workflow_execution.submitter.locale = params[:locale]
     PipelineMailer.complete_email(workflow_execution)
   end
 
   def error_email
     workflow_execution = WorkflowExecution.first
-    PipelineMailer.error_email(workflow_execution)
-  end
-
-  def error_email_in_french
-    workflow_execution = WorkflowExecution.first
-    workflow_execution.submitter.locale = :fr
+    workflow_execution.submitter.locale = params[:locale]
     PipelineMailer.error_email(workflow_execution)
   end
 end
