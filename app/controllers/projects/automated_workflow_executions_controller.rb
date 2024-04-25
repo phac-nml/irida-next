ff2ca70eef91db1b93840a582875a96e5885a399
Deep Sidhu <deepsidhu85@gmail.com>
2024-04-25 16:05:34 -0500

    Base setup of controller and routes for automated workflow executions

---
+++ app/controllers/projects/automated_workflow_executions_controller.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+module Projects
+  # Controller actions for Automated Workflow Executions
+  class AutomatedWorkflowExecutionsController < Projects::ApplicationController
+    include BreadcrumbNavigation
+
+    before_action :current_page
+    before_action :automated_workflows, only: %i[new edit]
+    before_action :automated_workflow, only: %i[edit update destroy show]
+
+    def index; end
+
+    def show; end
+
+    def new; end
+
+    def edit; end
+
+    def create
+      automated_workflow_execution_params.merge(namespace: @namespace)
+      @automated_workflow_execution = AutomatedWorkflowExecutions::CreateService.new(
+        current_user, automated_workflow_execution_params
+      ).execute
+
+      respond_to do |format|
+        format.turbo_stream do
+          if @automated_workflow_execution.persisted?
+            render status: :ok
+          else
+            render status: :unprocessable_entity
+          end
+        end
+      end
+    end
+
+    def update
+      automated_workflow_updated = AutomatedWorkflowExecutions::UpdateService.new(@automated_workflow,
+                                                                                  current_user).execute
+
+      respond_to do |format|
+        format.turbo_stream do
+          if automated_workflow_updated
+            render status: :ok
+          else
+            render status: :unprocessable_entity
+          end
+        end
+      end
+    end
+
+    def destroy
+      AutomatedWorkflowExecutions::DestroyService.new(@automated_workflow, current_user).execute
+
+      respond_to do |format|
+        format.turbo_stream do
+          if @automated_workflow.deleted?
+            render status: :ok
+          else
+            render status: :unprocessable_entity
+          end
+        end
+      end
+    end
+
+    private
+
+    def automated_workflow_execution_params
+      params.require(:automated_workflow_execution).permit(
+        :workflow_params, :email_notification, :updated_samples, metadata: { workflow_name: '', workflow_version: '' }
+      )
+    end
+
+    def automated_workflow
+      @automated_workflow = AutomatedWorkflowExecution.find_by(id: params[:id]) || not_found
+    end
+
+    def automated_workflows
+      @automated_workflows = Irida::Pipelines.automatable_pipelines
+    end
+
+    protected
+
+    def namespace
+      path = [params[:namespace_id], params[:project_id]].join('/')
+      @project ||= Namespaces::ProjectNamespace.find_by_full_path(path).project # rubocop:disable Rails/DynamicFindBy
+      @namespace = @project.namespace
+    end
+
+    def context_crumbs
+      super
+      case action_name
+      when 'index'
+        @context_crumbs += [{
+          name: 'Automated workflows',
+          path: '#'
+        }]
+      end
+    end
+
+    def current_page
+      @current_page = 'automated workflows'
+    end
+  end
+end
