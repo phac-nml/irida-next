8c3066c0aaf54f24d1d52815ee6ab44df9bd9109
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-04-25 08:57:29 -0500

    ensuring only humans are emailed

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -5,7 +5,7 @@ module MailerHelper
   def user_emails(namespace)
     user_memberships = Member.for_namespace_and_ancestors(namespace).not_expired
                              .where.not(access_level: Member::AccessLevel.manageable)
-    users = User.where(id: user_memberships.select(:user_id)).distinct
+    users = User.human_users.where(id: user_memberships.select(:user_id)).distinct
     users.pluck(:email)
   end
 
@@ -13,9 +13,9 @@ module MailerHelper
     manager_memberships = Member.for_namespace_and_ancestors(namespace).not_expired
                                 .where(access_level: Member::AccessLevel.manageable)
     managers = if member
-                 User.where(id: manager_memberships.select(:user_id)).and(User.where.not(id: member.user.id)).distinct
+                 User.human_users.where(id: manager_memberships.select(:user_id)).and(User.where.not(id: member.user.id)).distinct
                else
-                 User.where(id: manager_memberships.select(:user_id)).distinct
+                 User.human_users.where(id: manager_memberships.select(:user_id)).distinct
                end
     managers.pluck(:email)
   end
