b4b6e5c13d1bbac6a5a1209ba437d68d707f9405
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-04-25 11:58:12 -0500

    chore: remove method accidentally copied

--- app/models/automated_workflow_execution.rb
+++ app/models/automated_workflow_execution.rb
@@ -23,13 +23,4 @@ class AutomatedWorkflowExecution < ApplicationRecord
 
     errors.add(namespace.type, 'namespace cannot have automated workflow executions')
   end
-
-  def higher_access_level_than_group
-    return unless highest_group_member && highest_group_member.access_level > access_level
-
-    errors.add(:access_level, I18n.t('activerecord.errors.models.member.attributes.access_level.invalid',
-                                     user: user.email,
-                                     access_level: AccessLevel.human_access(highest_group_member.access_level),
-                                     group_name: highest_group_member.group.name))
-  end
 end
