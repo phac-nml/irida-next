146f28b0fbc4a57ae56af8e110e3ddaa15b4a857
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-04-25 08:59:12 -0500

    fixing rubocop error

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -13,7 +13,8 @@ module MailerHelper
     manager_memberships = Member.for_namespace_and_ancestors(namespace).not_expired
                                 .where(access_level: Member::AccessLevel.manageable)
     managers = if member
-                 User.human_users.where(id: manager_memberships.select(:user_id)).and(User.where.not(id: member.user.id)).distinct
+                 User.human_users.where(id: manager_memberships.select(:user_id))
+                     .and(User.where.not(id: member.user.id)).distinct
                else
                  User.human_users.where(id: manager_memberships.select(:user_id)).distinct
                end
