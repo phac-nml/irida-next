073868e19e59bc4afebf05bcf839a30f10688350
Deep Sidhu <deepsidhu85@gmail.com>
2024-04-26 15:57:49 -0500

    Updated workflow execution policy to add methods

--- app/policies/workflow_execution_policy.rb
+++ app/policies/workflow_execution_policy.rb
@@ -8,4 +8,80 @@ class WorkflowExecutionPolicy < ApplicationPolicy
     details[:id] = record.id
     false
   end
+
+  def index?
+    return true if record.submitter.automation_bot? == true
+    return true if Member.can_view?(user, record.namespace) == true
+
+    details[:id] = record.id
+    false
+  end
+
+  def destroy?
+    return true if record.submitter.id == user.id
+
+    details[:id] = record.id
+    false
+  end
+
+  def new?
+    return true if record.submitter.automation_bot? == true
+    return true if Member.can_submit_workflow?(user, record.namespace)
+
+    details[:id] = record.id
+    false
+  end
+
+  def show?
+    return true if record.submitter.id == user.id
+    return true if record.submitter.automation_bot? == true
+    return true if Member.can_view?(user, record.namespace) == true
+
+    details[:id] = record.id
+    false
+  end
+
+  def create?
+    return true if record.submitter.automation_bot? == true
+    return true if Member.can_submit_workflow?(user, record.namespace)
+
+    details[:id] = record.id
+    false
+  end
+
+  def cancel?
+    return true if record.submitter.id == user.id
+    return true if record.submitter.automation_bot? == true
+
+    details[:id] = record.id
+    false
+  end
+
+  def create_automated_workflow_executions?
+    return true if Member.can_modify?(user, record.namespace) == true
+
+    details[:id] = record.id
+    false
+  end
+
+  def destroy_automated_workflow_executions?
+    return true if Member.can_modify?(user, record.namespace) == true
+
+    details[:id] = record.id
+    false
+  end
+
+  def updated_automated_workflow_executions?
+    return true if Member.can_modify?(user, record.namespace) == true
+
+    details[:id] = record.id
+    false
+  end
+
+  def view_automated_workflow_executions?
+    return true if Member.can_view?(user, record.namespace) == true
+
+    details[:id] = record.id
+    false
+  end
 end
