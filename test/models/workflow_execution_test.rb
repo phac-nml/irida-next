15e7d71a462f17e53baecca209a92f4397a3efec
Chris Huynh <chris.huynh333@gmail.com>
2024-04-26 11:06:39 -0500

    Add tests for workflow execution model

--- test/models/workflow_execution_test.rb
+++ test/models/workflow_execution_test.rb
@@ -94,4 +94,152 @@ class WorkflowExecutionTest < ActiveSupport::TestCase
     assert_not @workflow_execution_valid.canceling?
     assert @workflow_execution_valid.canceled?
   end
+
+  test 'cancellable' do
+    @workflow_execution_valid.state = :initial
+    assert @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :submitted
+    assert @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :running
+    assert @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :prepared
+    assert @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :completing
+    assert_not @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :completed
+    assert_not @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :error
+    assert_not @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :canceling
+    assert_not @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :canceled
+    assert_not @workflow_execution_valid.cancellable?
+  end
+
+  test 'deletable' do
+    @workflow_execution_valid.state = :completed
+    assert @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :error
+    assert @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :canceled
+    assert @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :initial
+    assert_not @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :submitted
+    assert_not @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :running
+    assert_not @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :prepared
+    assert_not @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :completing
+    assert_not @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :canceling
+    assert_not @workflow_execution_valid.deletable?
+  end
+
+  test 'sent_to_ga4gh' do
+    @workflow_execution_valid.state = :submitted
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :running
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :completing
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :completed
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :error
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :canceling
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :canceled
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :prepared
+    assert_not @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :initial
+    assert_not @workflow_execution_valid.sent_to_ga4gh?
+  end
+
+  test 'send_email' do
+    workflow_execution_completed = workflow_executions(:irida_next_example_completed)
+    workflow_execution_error = workflow_executions(:irida_next_example_error)
+    workflow_execution_example = workflow_executions(:irida_next_example)
+
+    workflow_execution_completed.send_email do
+      assert_enqueued_emails 1
+      assert_enqueued_email_with PipelineMailer, :complete_email, args: [workflow_execution_completed]
+    end
+
+    workflow_execution_error.send_email do
+      assert_enqueued_emails 1
+      assert_enqueued_email_with PipelineMailer, :error_email, args: [workflow_execution_error]
+    end
+
+    workflow_execution_example.state = :submitted
+    workflow_execution_example.send_email do
+      assert_enqueued_emails 0
+    end
+
+    workflow_execution_example.state = :running
+    workflow_execution_example.send_email do
+      assert_enqueued_emails 0
+    end
+
+    workflow_execution_example.state = :completing
+    workflow_execution_example.send_email do
+      assert_enqueued_emails 0
+    end
+
+    workflow_execution_example.state = :canceling
+    workflow_execution_example.send_email do
+      assert_enqueued_emails 0
+    end
+
+    workflow_execution_example.state = :canceled
+    workflow_execution_example.send_email do
+      assert_enqueued_emails 0
+    end
+
+    workflow_execution_example.state = :prepared
+    workflow_execution_example.send_email do
+      assert_enqueued_emails 0
+    end
+
+    workflow_execution_example.state = :initial
+    workflow_execution_example.send_email do
+      assert_enqueued_emails 0
+    end
+  end
+
+  test 'as_wes_params to_json params' do
+    @workflow_execution_valid['tags']['test_key'] = 'test_value'
+    as_wes_params = @workflow_execution_valid.as_wes_params
+    assert_equal @workflow_execution_valid['workflow_params'].to_json, as_wes_params[:workflow_params]
+    assert_equal @workflow_execution_valid['workflow_engine_parameters'].to_json,
+                 as_wes_params[:workflow_engine_parameters]
+    assert_equal @workflow_execution_valid['tags'].to_json,
+                 as_wes_params[:tags]
+  end
 end
