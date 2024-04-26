505dc1cfe4cd40e290eb10ed7e53211bd8d61a0c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-04-26 11:40:47 -0500

    chore: Update file comment

--- db/migrate/20240426160617_add_namespace_ref_to_workflow_execution.rb
+++ db/migrate/20240426160617_add_namespace_ref_to_workflow_execution.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# migration to add namespace reference to workflow execution
 class AddNamespaceRefToWorkflowExecution < ActiveRecord::Migration[7.1]
   def change
     add_reference :workflow_executions, :namespace, type: :uuid, foreign_key: true
