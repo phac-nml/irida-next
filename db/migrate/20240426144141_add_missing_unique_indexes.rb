6678f15b0e436641dc23836def3a0d746849f0ca
Deep Sidhu <deepsidhu85@gmail.com>
2024-04-26 11:05:34 -0500

    Added in unique indexes that were previously added but missing the name attribute causing the indexes not to be applied in the schema

---
+++ db/migrate/20240426144141_add_missing_unique_indexes.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# migration to add missing unique indexes
+class AddMissingUniqueIndexes < ActiveRecord::Migration[7.1]
+  def up
+    add_index :members, %i[user_id namespace_id],
+              unique: true,
+              name: 'index_member_user_with_namespace'
+
+    add_index :namespace_group_links, %i[group_id namespace_id],
+              unique: true,
+              name: 'index_namespace_group_link_user_with_namespace'
+
+    add_index :samples, %i[name project_id],
+              unique: true,
+              name: 'index_sample_name_with_project'
+  end
+
+  def down; end
+end
