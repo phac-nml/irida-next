b41c14633e0c651bcb62c72d652e370b17edfb02
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-04-26 13:46:47 -0500

    chore: Updated internationalizations

--- db/migrate/20240426160117_add_name_to_workflow_execution.rb
+++ db/migrate/20240426160117_add_name_to_workflow_execution.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# migration to add name to workflow execution
 class AddNameToWorkflowExecution < ActiveRecord::Migration[7.1]
   def change
     add_column :workflow_executions, :name, :string
