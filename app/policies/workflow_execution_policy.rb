290a69b6789c4e420f14148803884746cbc5dc98
Deep Sidhu <deepsidhu85@gmail.com>
2024-04-29 11:55:00 -0500

    Added translations and updated policy methods to set details required for authorization errors

--- app/policies/workflow_execution_policy.rb
+++ app/policies/workflow_execution_policy.rb
@@ -12,7 +12,8 @@ class WorkflowExecutionPolicy < ApplicationPolicy
   def index?
     return true if Member.can_view?(user, record.namespace) == true
 
-    details[:id] = record.id
+    details[:name] = record.namespace.name
+    details[:namespace_type] = record.namespace.type
     false
   end
 
@@ -20,19 +21,12 @@ class WorkflowExecutionPolicy < ApplicationPolicy
     return true if record.submitter.id == user.id
     return true if Member.can_modify?(user, record.namespace) == true
 
-    details[:id] = record.id
+    details[:name] = record.namespace.name
+    details[:namespace_type] = record.namespace.type
     false
   end
 
-  def new?
-    return true if record.submitter.automation_bot? == true
-    return true if Member.can_submit_workflow?(user, record.namespace)
-
-    details[:id] = record.id
-    false
-  end
-
-  def show?
+  def read?
     return true if record.submitter.id == user.id
     return true if Member.can_view?(user, record.namespace) == true
 
@@ -44,44 +38,50 @@ class WorkflowExecutionPolicy < ApplicationPolicy
     return true if record.submitter.automation_bot? == true
     return true if Member.can_submit_workflow?(user, record.namespace)
 
-    details[:id] = record.id
+    details[:name] = record.namespace.name
+    details[:namespace_type] = record.namespace.type
     false
   end
 
-  def cancel?
+  def cancel? # rubocop:disable Metrics/AbcSize
     return true if record.submitter.id == user.id
     return true if record.submitter.automation_bot? == true
     return true if Member.can_modify?(user, record.namespace) == true
 
-    details[:id] = record.id
+    details[:name] = record.namespace.name
+    details[:namespace_type] = record.namespace.type
     false
   end
 
   def create_automated_workflow_executions?
     return true if Member.can_modify?(user, record.namespace) == true
 
-    details[:id] = record.id
+    details[:name] = record.namespace.name
+    details[:namespace_type] = record.namespace.type
     false
   end
 
   def destroy_automated_workflow_executions?
     return true if Member.can_modify?(user, record.namespace) == true
 
-    details[:id] = record.id
+    details[:name] = record.namespace.name
+    details[:namespace_type] = record.namespace.type
     false
   end
 
   def update_automated_workflow_executions?
     return true if Member.can_modify?(user, record.namespace) == true
 
-    details[:id] = record.id
+    details[:name] = record.namespace.name
+    details[:namespace_type] = record.namespace.type
     false
   end
 
   def view_automated_workflow_executions?
     return true if Member.can_view?(user, record.namespace) == true
 
-    details[:id] = record.id
+    details[:name] = record.namespace.name
+    details[:namespace_type] = record.namespace.type
     false
   end
 end
