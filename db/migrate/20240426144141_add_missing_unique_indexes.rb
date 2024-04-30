0da255b397517bbbd18a0a6637729728621953c9
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-04-30 07:54:38 -0500

    fix: add deleted_at is NULL to missing unique constraints so that they work with soft deleting

--- db/migrate/20240426144141_add_missing_unique_indexes.rb
+++ db/migrate/20240426144141_add_missing_unique_indexes.rb
@@ -5,14 +5,17 @@ class AddMissingUniqueIndexes < ActiveRecord::Migration[7.1]
   def change
     add_index :members, %i[user_id namespace_id],
               unique: true,
+              where: '(deleted_at IS NULL)',
               name: 'index_member_user_with_namespace'
 
     add_index :namespace_group_links, %i[group_id namespace_id],
               unique: true,
+              where: '(deleted_at IS NULL)',
               name: 'index_namespace_group_link_user_with_namespace'
 
     add_index :samples, %i[name project_id],
               unique: true,
+              where: '(deleted_at IS NULL)',
               name: 'index_sample_name_with_project'
   end
 end
