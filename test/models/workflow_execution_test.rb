e9b120e57db616274d565e567c485ea9b6b28da4
Chris Huynh <82407232+ChrisHuynh333@users.noreply.github.com>
2024-04-30 09:46:19 -0500

    Workflow Executions: Update Model Tests (#577)
    * Add tests for workflow execution model
    
    * Add assertions for send_email
    
    * Change test as wes params to test all params
    
    * Fix send_email asserts
    
    * remove assert_enqueued_email_with from send_email tests that don't send emails

--- test/models/workflow_execution_test.rb
+++ test/models/workflow_execution_test.rb
@@ -94,4 +94,161 @@ class WorkflowExecutionTest < ActiveSupport::TestCase
     assert_not @workflow_execution_valid.canceling?
     assert @workflow_execution_valid.canceled?
   end
+
+  test 'cancellable' do
+    @workflow_execution_valid.state = :initial
+    assert @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :submitted
+    assert @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :running
+    assert @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :prepared
+    assert @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :completing
+    assert_not @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :completed
+    assert_not @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :error
+    assert_not @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :canceling
+    assert_not @workflow_execution_valid.cancellable?
+
+    @workflow_execution_valid.state = :canceled
+    assert_not @workflow_execution_valid.cancellable?
+  end
+
+  test 'deletable' do
+    @workflow_execution_valid.state = :completed
+    assert @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :error
+    assert @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :canceled
+    assert @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :initial
+    assert_not @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :submitted
+    assert_not @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :running
+    assert_not @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :prepared
+    assert_not @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :completing
+    assert_not @workflow_execution_valid.deletable?
+
+    @workflow_execution_valid.state = :canceling
+    assert_not @workflow_execution_valid.deletable?
+  end
+
+  test 'sent_to_ga4gh' do
+    @workflow_execution_valid.state = :submitted
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :running
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :completing
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :completed
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :error
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :canceling
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :canceled
+    assert @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :prepared
+    assert_not @workflow_execution_valid.sent_to_ga4gh?
+
+    @workflow_execution_valid.state = :initial
+    assert_not @workflow_execution_valid.sent_to_ga4gh?
+  end
+
+  test 'send_email' do
+    workflow_execution_example = workflow_executions(:irida_next_example)
+
+    workflow_execution_example.state = :completed
+    assert_enqueued_emails 1 do
+      workflow_execution_example.send_email
+      assert_enqueued_email_with PipelineMailer, :complete_email, args: [workflow_execution_example]
+    end
+
+    workflow_execution_example.state = :error
+    assert_enqueued_emails 1 do
+      workflow_execution_example.send_email
+      assert_enqueued_email_with PipelineMailer, :complete_email, args: [workflow_execution_example]
+    end
+
+    workflow_execution_example.state = :submitted
+    assert_enqueued_emails 0 do
+      workflow_execution_example.send_email
+    end
+
+    workflow_execution_example.state = :running
+    assert_enqueued_emails 0 do
+      workflow_execution_example.send_email
+    end
+
+    workflow_execution_example.state = :completing
+    assert_enqueued_emails 0 do
+      workflow_execution_example.send_email
+    end
+
+    workflow_execution_example.state = :canceling
+    assert_enqueued_emails 0 do
+      workflow_execution_example.send_email
+    end
+
+    workflow_execution_example.state = :canceled
+    assert_enqueued_emails 0 do
+      workflow_execution_example.send_email
+    end
+
+    workflow_execution_example.state = :prepared
+    assert_enqueued_emails 0 do
+      workflow_execution_example.send_email
+    end
+
+    workflow_execution_example.state = :initial
+    assert_enqueued_emails 0 do
+      workflow_execution_example.send_email
+    end
+
+    assert_enqueued_emails 0 do
+      @workflow_execution_valid.send_email
+    end
+  end
+
+  test 'as_wes_params' do
+    @workflow_execution_valid['tags']['test_key'] = 'test_value'
+    as_wes_params = @workflow_execution_valid.as_wes_params
+    assert_equal @workflow_execution_valid['workflow_params'].to_json, as_wes_params[:workflow_params]
+    assert_equal @workflow_execution_valid['workflow_engine_parameters'].to_json,
+                 as_wes_params[:workflow_engine_parameters]
+    assert_equal @workflow_execution_valid['tags'].to_json,
+                 as_wes_params[:tags]
+    assert_equal @workflow_execution_valid['workflow_type'], as_wes_params[:workflow_type]
+    assert_equal @workflow_execution_valid['workflow_type_version'], as_wes_params[:workflow_type_version]
+    assert_equal @workflow_execution_valid['workflow_engine'], as_wes_params[:workflow_engine]
+    assert_equal @workflow_execution_valid['workflow_engine_version'], as_wes_params[:workflow_engine_version]
+    assert_equal @workflow_execution_valid['workflow_url'], as_wes_params[:workflow_url]
+  end
 end
