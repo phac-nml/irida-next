4a5e860552ff700f67d899847aca53e06300c7d8
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-04-30 14:30:16 -0500

    add destroy service tests

--- test/services/workflow_executions/destroy_service_test.rb
+++ test/services/workflow_executions/destroy_service_test.rb
@@ -52,6 +52,7 @@ module WorkflowExecutions
     test 'should destroy a completed workflow execution' do
       workflow_execution = workflow_executions(:irida_next_example_completed)
       assert workflow_execution.completed?
+      assert workflow_execution.cleaned?
 
       assert_difference -> { WorkflowExecution.count } => -1,
                         -> { SamplesWorkflowExecution.count } => -1,
@@ -60,13 +61,36 @@ module WorkflowExecutions
       end
     end
 
+    test 'should not destroy an uncleaned completed workflow execution' do
+      workflow_execution = workflow_executions(:irida_next_example_completed_unclean)
+      assert workflow_execution.completed?
+      assert_not workflow_execution.cleaned?
+
+      assert_no_difference -> { WorkflowExecution.count },
+                           -> { SamplesWorkflowExecution.count } do
+        WorkflowExecutions::DestroyService.new(workflow_execution, @user).execute
+      end
+    end
+
     test 'should destroy an errored workflow execution' do
       workflow_execution = workflow_executions(:irida_next_example_error)
       assert workflow_execution.error?
+      assert workflow_execution.cleaned?
 
       assert_difference -> { WorkflowExecution.count } => -1,
                         -> { SamplesWorkflowExecution.count } => -1,
                         -> { Sample.count } => 0 do
+        WorkflowExecutions::DestroyService.new(workflow_execution, @user).execute
+      end
+    end
+
+    test 'should not destroy an uncleaned error workflow execution' do
+      workflow_execution = workflow_executions(:irida_next_example_error_unclean)
+      assert workflow_execution.error?
+      assert_not workflow_execution.cleaned?
+
+      assert_no_difference -> { WorkflowExecution.count },
+                           -> { SamplesWorkflowExecution.count } do
         WorkflowExecutions::DestroyService.new(workflow_execution, @user).execute
       end
     end
@@ -84,10 +108,22 @@ module WorkflowExecutions
     test 'should destroy a canceled workflow execution' do
       workflow_execution = workflow_executions(:irida_next_example_canceled)
       assert workflow_execution.canceled?
+      assert workflow_execution.cleaned?
 
       assert_difference -> { WorkflowExecution.count } => -1,
                         -> { SamplesWorkflowExecution.count } => -1,
                         -> { Sample.count } => 0 do
+        WorkflowExecutions::DestroyService.new(workflow_execution, @user).execute
+      end
+    end
+
+    test 'should not destroy an uncleaned canceled workflow execution' do
+      workflow_execution = workflow_executions(:irida_next_example_canceled_unclean)
+      assert workflow_execution.canceled?
+      assert_not workflow_execution.cleaned?
+
+      assert_no_difference -> { WorkflowExecution.count },
+                           -> { SamplesWorkflowExecution.count } do
         WorkflowExecutions::DestroyService.new(workflow_execution, @user).execute
       end
     end
