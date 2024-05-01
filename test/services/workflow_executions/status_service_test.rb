1ab503029fe54fceb63a8125fbfb0bc147d62e21
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-05-01 10:51:51 -0500

    adding a status_service_test test

--- test/services/workflow_executions/status_service_test.rb
+++ test/services/workflow_executions/status_service_test.rb
@@ -174,5 +174,56 @@ module WorkflowExecutions
       assert_enqueued_emails 1
       assert_enqueued_email_with PipelineMailer, :error_user_email, args: [@workflow_execution]
     end
+
+    test 'get status of automated workflow execution which has errored' do
+      run_id = 'status_test_3'
+      stubs = Faraday::Adapter::Test::Stubs.new
+      stubs.post('/runs') do
+        [
+          200,
+          { 'Content-Type': 'application/json' },
+          { run_id: }
+        ]
+      end
+
+      conn = Faraday.new do |builder|
+        builder.adapter :test, stubs
+      end
+
+      @automation_bot = users(:projectA_automation_bot)
+      @workflow_execution.submitter = @automation_bot
+      assert WorkflowExecutions::SubmissionService.new(@workflow_execution, conn, @user, {}).execute
+
+      assert_equal run_id, @workflow_execution.run_id
+
+      assert_equal 'submitted', @workflow_execution.state
+
+      stubs = Faraday::Adapter::Test::Stubs.new
+      stubs.get("/runs/#{run_id}/status") do
+        [
+          200,
+          { 'Content-Type': 'application/json' },
+          { run_id:, state: 'SYSTEM_ERROR' }
+        ]
+      end
+
+      conn = Faraday.new do |builder|
+        builder.adapter :test, stubs
+      end
+
+      @workflow_execution = WorkflowExecutions::StatusService.new(@workflow_execution, conn, @user, {}).execute
+
+      assert_equal 'error', @workflow_execution.state
+
+      assert @workflow_execution.email_notification
+      assert_enqueued_emails 2
+      I18n.available_locales.each do |locale|
+        manager_emails = Member.manager_emails(@workflow_execution.namespace, locale)
+        next if manager_emails.empty?
+
+        assert_enqueued_email_with PipelineMailer, :error_manager_email,
+                                   args: [@workflow_execution, manager_emails, locale]
+      end
+    end
   end
 end
