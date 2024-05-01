6d3cc378db8bf45bf95f1e2f55977f0d1fb78080
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-05-01 14:52:18 -0500

    chore: removed dropdowns from data exports table

--- test/system/data_exports_test.rb
+++ test/system/data_exports_test.rb
@@ -56,19 +56,12 @@ class DataExportsTest < ApplicationSystemTestCase
 
     within %(#data-exports-table-body) do
       within %(tr:nth-child(2) td:last-child) do
-        first('button.Viral-Dropdown--icon').click
-        within('div[data-viral--dropdown-target="menu"] ul') do
-          assert_no_text I18n.t('data_exports.index.actions.download')
-          assert_text I18n.t('data_exports.index.actions.delete')
-        end
+        assert_text I18n.t('data_exports.index.actions.delete')
       end
 
       within %(tr:first-child td:last-child) do
-        first('button.Viral-Dropdown--icon').click
-        within('div[data-viral--dropdown-target="menu"] ul') do
-          assert_text I18n.t('data_exports.index.actions.download')
-          assert_text I18n.t('data_exports.index.actions.delete')
-        end
+        assert_text I18n.t('data_exports.index.actions.download')
+        assert_text I18n.t('data_exports.index.actions.delete')
       end
     end
   end
@@ -78,10 +71,7 @@ class DataExportsTest < ApplicationSystemTestCase
 
     within %(#data-exports-table-body) do
       assert_selector 'tr', count: 4
-      first('button.Viral-Dropdown--icon').click
-      within('div[data-viral--dropdown-target="menu"] ul') do
-        click_link I18n.t('data_exports.index.actions.delete'), match: :first
-      end
+      click_link I18n.t('data_exports.index.actions.delete'), match: :first
     end
     within('#turbo-confirm[open]') do
       click_button I18n.t(:'components.confirmation.confirm')
@@ -89,10 +79,7 @@ class DataExportsTest < ApplicationSystemTestCase
 
     within %(#data-exports-table-body) do
       assert_selector 'tr', count: 3
-      first('button.Viral-Dropdown--icon').click
-      within('div[data-viral--dropdown-target="menu"] ul') do
-        click_link I18n.t('data_exports.index.actions.delete'), match: :first
-      end
+      click_link I18n.t('data_exports.index.actions.delete'), match: :first
     end
     within('#turbo-confirm[open]') do
       click_button I18n.t(:'components.confirmation.confirm')
@@ -100,10 +87,7 @@ class DataExportsTest < ApplicationSystemTestCase
 
     within %(#data-exports-table-body) do
       assert_selector 'tr', count: 2
-      first('button.Viral-Dropdown--icon').click
-      within('div[data-viral--dropdown-target="menu"] ul') do
-        click_link I18n.t('data_exports.index.actions.delete'), match: :first
-      end
+      click_link I18n.t('data_exports.index.actions.delete'), match: :first
     end
     within('#turbo-confirm[open]') do
       click_button I18n.t(:'components.confirmation.confirm')
@@ -111,10 +95,7 @@ class DataExportsTest < ApplicationSystemTestCase
 
     within %(#data-exports-table-body) do
       assert_selector 'tr', count: 1
-      first('button.Viral-Dropdown--icon').click
-      within('div[data-viral--dropdown-target="menu"] ul') do
-        click_link I18n.t('data_exports.index.actions.delete'), match: :first
-      end
+      click_link I18n.t('data_exports.index.actions.delete'), match: :first
     end
     within('#turbo-confirm[open]') do
       click_button I18n.t(:'components.confirmation.confirm')
