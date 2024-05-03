6661cd4a51d7bc9cbbe1165bde0eab49247c91b6
Deep Sidhu <deepsidhu85@gmail.com>
2024-05-03 11:30:00 -0500

    Updated to show correct badge color on show pages for workflow executions

--- app/components/viral/page_header_component.rb
+++ app/components/viral/page_header_component.rb
@@ -3,15 +3,16 @@
 module Viral
   # Component for displaying a page header with title, subtitle and buttons
   class PageHeaderComponent < Component
-    attr_reader :title, :subtitle
+    attr_reader :title, :subtitle, :id, :id_color
 
     renders_one :icon
     renders_one :buttons
 
-    def initialize(title:, id: nil, subtitle: nil)
+    def initialize(title:, id: nil, subtitle: nil, id_color: :primary)
       @title = title
       @id = id
       @subtitle = subtitle
+      @id_color = id_color
     end
   end
 end
