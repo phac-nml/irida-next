b3f645bd349a744253289fa5ab282222d9c2d333
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-05-08 14:34:19 -0500

    fix migration

--- db/migrate/20240426181959_add_cleaned_boolean_workflow_execution.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Migration to add cleaned column to workflow_executions table
-class AddCleanedBooleanWorkflowExecution < ActiveRecord::Migration[7.1]
-  def change
-    add_column :workflow_executions, :cleaned, :boolean, default: false, null: false
-  end
-end
