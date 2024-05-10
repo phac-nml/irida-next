1caaa88aa19b45ef318680aef21ee60854ec625a
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-05-10 15:18:32 -0500

    update tests

--- test/graphql/workflow_executions_query_test.rb
+++ test/graphql/workflow_executions_query_test.rb
@@ -8,6 +8,14 @@ class WorkflowExecutionsQueryTest < ActiveSupport::TestCase
       workflowExecutions(first: $first) {
         nodes {
           id
+          runId
+          samples {
+            edges {
+              node {
+                id
+              }
+            }
+          }
         }
       }
     }
@@ -18,9 +26,16 @@ class WorkflowExecutionsQueryTest < ActiveSupport::TestCase
       node(id: $workflow_execution_id) {
         ... on WorkflowExecution {
           id
+          blobRunDirectory
+          httpErrorCode
           metadata
           runId
           state
+          submitter {
+            id
+            email
+          }
+          submitterId
           tags
           workflowEngine
           workflowEngineParameters
@@ -30,12 +45,12 @@ class WorkflowExecutionsQueryTest < ActiveSupport::TestCase
           workflowTypeVersion
           workflowUrl
           samples {
-            nodes {
-              id
+            edges {
+              node {
+                id
+              }
             }
           }
-          submitter { id }
-          submitterId
         }
       }
     }
@@ -58,11 +73,14 @@ class WorkflowExecutionsQueryTest < ActiveSupport::TestCase
   end
 
   test 'workflow executions nodes query should work' do
-    workflow_execution_id = IridaSchema.execute(
+    prelim_query = IridaSchema.execute(
       WORKFLOW_EXECUTIONS_QUERY,
       context: { current_user: @user },
       variables: { first: 1 }
-    )['data']['workflowExecutions']['nodes'][0]['id']
+    )['data']
+
+    workflow_execution_id = prelim_query['workflowExecutions']['nodes'][0]['id']
+    workflow_execution_run_id = prelim_query['workflowExecutions']['nodes'][0]['runId']
 
     result = IridaSchema.execute(WORKFLOW_EXECUTIONS_NODE_QUERY, context: { current_user: @user },
                                                                  variables: { workflow_execution_id: })
@@ -71,22 +89,25 @@ class WorkflowExecutionsQueryTest < ActiveSupport::TestCase
 
     data = result['data']['node']
     assert_not_empty data, 'workflow execution type should work'
-    assert_equal 'my_run_id_1', data['runId'], 'workflow execution run id should match'
+    assert_equal workflow_execution_run_id, data['runId'], 'workflow execution run id should match'
   end
 
   test 'workflow executions nodes query for samples should work' do
-    workflow_execution_id = IridaSchema.execute(
+    prelim_query = IridaSchema.execute(
       WORKFLOW_EXECUTIONS_QUERY,
       context: { current_user: @user },
       variables: { first: 1 }
-    )['data']['workflowExecutions']['nodes'][0]['id']
+    )['data']
+
+    workflow_execution_id = prelim_query['workflowExecutions']['nodes'][0]['id']
+    sample_id = prelim_query['workflowExecutions']['nodes'][0]['samples']['edges'][0]['node']['id']
 
     result = IridaSchema.execute(WORKFLOW_EXECUTIONS_NODE_QUERY, context: { current_user: @user },
                                                                  variables: { workflow_execution_id: })
 
     data = result['data']['node']
 
-    assert_not_empty data['samples']['nodes'], 'workflow execution samples resolver should work'
-    assert_equal 'gid://irida/Sample/21002189', data['samples']['nodes'][0]['id'], 'sample id should match'
+    assert_not_empty data['samples']['edges'], 'workflow execution samples resolver should work'
+    assert_equal sample_id, data['samples']['edges'][0]['node']['id'], 'sample id should match'
   end
 end
