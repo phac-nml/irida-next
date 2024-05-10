439da3e191c3a870b03463df10cb729d663fb034
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-05-10 10:28:20 -0500

    style: Updated header styles

--- app/components/viral/dialog/header_component.rb
+++ app/components/viral/dialog/header_component.rb
@@ -9,10 +9,6 @@ module Viral
       def initialize(title:, closable: true)
         @title = title
         @closable = closable
-        @system_arguments = {}
-        @system_arguments[:classes] =
-          class_names(@system_arguments[:classes],
-                      'flex items-start justify-between p-5 border-b rounded-t dark:border-slate-600')
       end
     end
   end
