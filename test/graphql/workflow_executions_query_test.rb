0b27e7211e8e247bc06cfd1bd17ebcf100cb7a92
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-05-10 15:25:37 -0500

    add new tests for new resolver

--- test/graphql/workflow_executions_query_test.rb
+++ test/graphql/workflow_executions_query_test.rb
@@ -192,4 +192,40 @@ class WorkflowExecutionsQueryTest < ActiveSupport::TestCase
     assert_not_empty data['samples']['edges'], 'workflow execution samples resolver should work'
     assert_equal sample_id, data['samples']['edges'][0]['node']['id'], 'sample id should match'
   end
+
+  test 'workflow executions nodes query for metadata though resolver should work' do
+    prelim_query = IridaSchema.execute(
+      WORKFLOW_EXECUTIONS_QUERY,
+      context: { current_user: @user },
+      variables: { first: 1 }
+    )['data']
+
+    workflow_execution_id = prelim_query['workflowExecutions']['nodes'][0]['id']
+
+    result = IridaSchema.execute(WORKFLOW_EXECUTIONS_NODE_QUERY, context: { current_user: @user },
+                                                                 variables: { workflow_execution_id: })
+
+    data = result['data']['node']
+
+    exp_metadata = { 'workflow_name' => 'wn1', 'workflow_version' => 'wv1' }
+    assert_equal exp_metadata, data['metadata']
+  end
+
+  test 'workflow executions nodes query for state should translate from enum' do
+    prelim_query = IridaSchema.execute(
+      WORKFLOW_EXECUTIONS_QUERY,
+      context: { current_user: @user },
+      variables: { first: 1 }
+    )['data']
+
+    workflow_execution_id = prelim_query['workflowExecutions']['nodes'][0]['id']
+
+    result = IridaSchema.execute(WORKFLOW_EXECUTIONS_NODE_QUERY, context: { current_user: @user },
+                                                                 variables: { workflow_execution_id: })
+
+    data = result['data']['node']
+
+    # We should have a string translated from the enum, not an integer
+    assert_equal 'initial', data['state']
+  end
 end
