8e1810e1bac94075200ad681e0bb8440c2aec7b9
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-05-16 11:34:27 -0500

    test: Added tests for pipeline overrides

--- test/lib/irida/pipelines_overrides_test.rb
+++ test/lib/irida/pipelines_overrides_test.rb
@@ -3,30 +3,47 @@
 require 'test_helper'
 require 'webmock/minitest'
 
-class PipelinesTest < ActiveSupport::TestCase
+module Irida
+  module Pipelines
+    module_function
+
+    def pipeline_config_dir=(new_value)
+      @pipeline_config_dir = new_value
+    end
+
+    def pipeline_schema_file_dir=(new_value)
+      @pipeline_schema_file_dir = new_value
+    end
+  end
+end
+
+class PipelinesOverrides < ActiveSupport::TestCase
   setup do
     @pipeline_schema_file_dir = 'tmp/storage/pipelines'
+
+    # Read in schema file to json
+    json_content = Rails.root.join('test/fixtures/files/nextflow/nextflow_schema.json')
 
     Irida::Pipelines.pipeline_config_dir = 'test/config/pipelines_with_overrides'
     Irida::Pipelines.pipeline_schema_file_dir = @pipeline_schema_file_dir
 
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.2/nextflow_schema.json')
-      .to_return(status: 200, body: '', headers: { etag: '[W/"a1Ab"]' })
+    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/2.0.2/nextflow_schema.json')
+      .to_return(status: 200, body: json_content, headers: { etag: '[W/"a1Ab"]' })
 
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.2/assets/schema_input.json')
-      .to_return(status: 200, body: '', headers: { etag: '[W/"b1Bc"]' })
+    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/2.0.2/assets/schema_input.json')
+      .to_return(status: 200, body: json_content, headers: { etag: '[W/"b1Bc"]' })
 
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.1/nextflow_schema.json')
-      .to_return(status: 200, body: '', headers: { etag: '[W/"c1Cd"]' })
+    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/2.0.1/nextflow_schema.json')
+      .to_return(status: 200, body: json_content, headers: { etag: '[W/"c1Cd"]' })
 
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.1/assets/schema_input.json')
-      .to_return(status: 200, body: '', headers: { etag: '[W/"d1De"]' })
+    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/2.0.1/assets/schema_input.json')
+      .to_return(status: 200, body: json_content, headers: { etag: '[W/"d1De"]' })
 
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.0/nextflow_schema.json')
-      .to_return(status: 200, body: '', headers: { etag: '[W/"e1Ef"]' })
+    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/2.0.0/nextflow_schema.json')
+      .to_return(status: 200, body: json_content, headers: { etag: '[W/"e1Ef"]' })
 
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.0/assets/schema_input.json')
-      .to_return(status: 200, body: '', headers: { etag: '[W/"f1Fg"]' })
+    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/2.0.0/assets/schema_input.json')
+      .to_return(status: 200, body: json_content, headers: { etag: '[W/"f1Fg"]' })
   end
 
   teardown do
@@ -34,11 +51,13 @@ class PipelinesTest < ActiveSupport::TestCase
   end
 
   test 'pipelines with overrides' do
-    workflow1 = Irida::Pipelines.find_pipeline_by('phac-nml/iridanextexample', '1.0.2')
+    Irida::Pipelines.register_pipelines
+
+    workflow1 = Irida::Pipelines.find_pipeline_by('phac-nml/iridanextexample', '2.0.2')
     assert_equal 'DEFAULT PROJECT NAME',
                  workflow1.workflow_params[:input_output_options][:properties][:project_name][:default]
 
-    workflow2 = Irida::Pipelines.find_pipeline_by('phac-nml/iridanextexample', '1.0.1')
+    workflow2 = Irida::Pipelines.find_pipeline_by('phac-nml/iridanextexample', '2.0.1')
     assert_equal 'UNIQUE PROJECT NAME',
                  workflow2.workflow_params[:input_output_options][:properties][:project_name][:default]
   end
