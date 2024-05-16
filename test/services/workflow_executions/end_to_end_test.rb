f7bcf114379aa071a045b052e3f11b8ccbaceaa2
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-05-16 16:35:06 -0500

    set up fixtures/blobs

--- test/services/workflow_executions/end_to_end_test.rb
+++ test/services/workflow_executions/end_to_end_test.rb
@@ -12,40 +12,15 @@ module WorkflowExecutions
     end
 
     test 'test create new workflow execution' do
-      workflow_params1 = {
-        metadata:
-          { workflow_name: 'phac-nml/iridanextexample', workflow_version: '1.0.2' },
-        workflow_params:
-        {
-          input: '/blah/samplesheet.csv',
-          outdir: '/blah/output'
-        },
-        workflow_type: 'NFL',
-        workflow_type_version: 'DSL2',
-        workflow_engine: 'nextflow',
-        workflow_engine_version: '23.10.0',
-        workflow_engine_parameters: { '-r': 'dev' },
-        workflow_url: 'https://github.com/phac-nml/iridanextexamplenew',
-        submitter_id: @user.id,
-        namespace_id: @project.namespace.id
-      }
-
-      # stub_request(:post, 'http://www.example.com/ga4gh/wes/v1/runs')
-      #   .to_return(body: '{ "run_id": "create_run_1" }',
-      #              headers: { content_type:
-      #                       'application/json' })
-
-      # stub_request(:get, 'http://www.example.com/ga4gh/wes/v1/runs/create_run_1/status')
-      #   .to_return(body: '{ "run_id": "create_run_1", "state": "COMPLETE" }',
-      #              headers: { content_type:
-      #                       'application/json' })
-
-      # do not perform completion job as this tests scope does not contain blob storage files
-      assert_performed_jobs 0, except: WorkflowExecutionPreparationJob do
-        @workflow_execution = WorkflowExecutions::CreateService.new(@user, workflow_params1).execute
-      end
+      @workflow_execution = workflow_executions(:irida_next_example_end_to_end)
 
       assert_equal 'initial', @workflow_execution.reload.state
+
+      assert_performed_jobs 1, except: WorkflowExecutionSubmissionJob do
+        WorkflowExecutionPreparationJob.perform_later(@workflow_execution)
+      end
+
+      assert_equal 'prepared', @workflow_execution.reload.state
     end
   end
 end
