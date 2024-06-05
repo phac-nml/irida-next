c17b6c90c7510dac95cc6dd4a91ecd7f8bc5b319
Deep Sidhu <deepsidhu85@gmail.com>
2024-06-05 14:56:03 -0500

    Removed setting of token in base_service as it's set in the irida schema:

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -7,18 +7,10 @@ class BaseService
 
   authorize :user, through: :current_user
 
-  authorize :token, through: :current_token
-
   attr_accessor :current_user, :params
 
   def initialize(user = nil, params = {})
     @current_user = user
     @params = params.dup
-  end
-
-  private
-
-  def current_token
-    Current.token
   end
 end
