2f1d6d3d657563211315d75d103b2db92542d9f2
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-06-11 14:03:53 -0500

    playing with spinner for long transfers

---
+++ app/javascript/controllers/spinner_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus";
+
+function preventEscapeListener(event) {
+  if (event.key === "Escape") {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+}
+
+export default class extends Controller {
+  static targets = ['submit'];
+
+  submitStart() {
+    document.addEventListener("keydown", preventEscapeListener, true);
+    document.querySelector(".dialog--header").classList.add("opacity-20");
+    document.querySelector(".dialog--section > *:not(:last-child)").classList.add("opacity-20");
+    document.querySelector(".dialog--close").classList.add("hidden");
+    document.querySelector("#spinner").classList.remove("hidden");
+  }
+
+  submitEnd() {
+    document.addEventListener("turbo:submit-end", preventEscapeListener);
+    document.querySelector(".dialog--header").classList.remove("opacity-20");
+    document.querySelector(".dialog--section > *:not(:last-child)").classList.remove("opacity-20");
+    document.querySelector(".dialog--close").classList.remove("hidden");
+    document.querySelector("#spinner").classList.add("hidden");
+  }
+}
