8a2f557dec3c0b23dc4a7e9325a528d087041b8f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-06-11 14:56:41 -0500

    chore: Disable workflor routes on dashboard if none available

--- config/routes/dashboard.rb
+++ config/routes/dashboard.rb
@@ -4,7 +4,7 @@ resource :dashboard, controller: 'dashboard', only: [] do
   scope module: :dashboard do
     resources :projects, only: [:index]
     resources :groups, only: [:index]
-    resources :workflow_executions, only: [:index]
+    resources :workflow_executions, only: [:index] if Irida::Pipelines.instance.available_pipelines.any?
   end
 
   root to: 'dashboard/projects#index'
