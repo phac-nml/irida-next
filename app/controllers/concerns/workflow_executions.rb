5989c53f957b63df9df0ed84199503327e4a51b4
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-06-11 14:01:04 -0500

    chore: Added file comment

--- app/controllers/concerns/workflow_executions.rb
+++ app/controllers/concerns/workflow_executions.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Module to check if there are workflow executions
 module WorkflowExecutions
   def has_workflow_executions?
     @has_workflow_executions = !File.zero?('config/pipelines/pipelines.json')
