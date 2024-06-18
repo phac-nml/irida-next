51bde16dc65399e6a4ac897ff7a8de0ea8d01c5c
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-06-18 10:23:39 -0500

    trying to fix a test

--- test/system/projects/samples_test.rb
+++ test/system/projects/samples_test.rb
@@ -272,18 +272,18 @@ module Projects
 
     test 'should not transfer samples' do
       project26 = projects(:project26)
-      sample31 = samples(:sample31)
+      sample30 = samples(:sample30)
       visit namespace_project_samples_url(@namespace, @project)
       assert_text 'Displaying 3 items'
       assert_selector '#samples-table table tbody tr', count: 3
-      check sample31.name
+      check sample30.name
       click_link I18n.t('projects.samples.index.transfer_button'), match: :first
       within('span[data-controller-connected="true"] dialog') do
         within %(turbo-frame[id="list_select_samples"]) do
-          assert_text sample31.name
+          assert_text sample30.name
+          assert_text sample30.puid
         end
         select project26.full_path, from: I18n.t('projects.samples.transfers.dialog.new_project_id')
-        assert_button I18n.t('projects.samples.transfers.dialog.submit_button')
         click_on I18n.t('projects.samples.transfers.dialog.submit_button')
       end
       within %(turbo-frame[id="samples_dialog"]) do
