48da52863dad5b8fd842d9526edec99e6671eca8
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-06-20 14:10:24 -0500

    chore: Working on updating the file list

--- app/javascript/controllers/file_upload_controller.js
+++ app/javascript/controllers/file_upload_controller.js
@@ -1,12 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["error"];
   static values = {
     ignore: [],
   };
 
   connect() {
     console.log("Ignored", this.ignoreValue);
+    this.ignoreRegex = new RegExp(
+      this.ignoreValue.map((a) => a.replace(".", "")).join("$|") + "$",
+    );
+    console.log(this.ignoreRegex);
     this.element.addEventListener("change", this.handleFileChange.bind(this));
   }
 
@@ -18,14 +23,25 @@ export default class extends Controller {
   }
 
   handleFileChange(event) {
-    const files = Array.from(event.target.files).filter(
-      (file) => !file.name.match(this.ignoreValue),
+    const files = Array.from(event.target.files);
+    // Split the files into ignore and non-ignore files
+    const ignoreFiles = files.filter((file) =>
+      file.name.match(this.ignoreRegex),
     );
-    this.element.dispatchEvent(
-      new CustomEvent("filechange", {
-        detail: { files },
-        bubbles: true,
-      }),
+    const notIgnoredFiles = files.filter(
+      (file) => !file.name.match(this.ignoreRegex),
     );
+
+    // if (ignoreFiles.length > 0) {
+    //   this.errorTarget.classList.remove("hidden");
+    // }
+    const fileList = new FileList();
+    notIgnoredFiles.forEach((file) => {
+      fileList.push(file);
+    });
+    this.element.files = fileList;
+
+    console.log("Ignored files", ignoreFiles);
+    console.log("Not ignored files", notIgnoredFiles);
   }
 }
