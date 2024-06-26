dcf18fdbcd2c1eae7948933ddab40b8ffcb72f5c
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-06-26 09:35:58 -0500

    adding rows to members table component

--- app/components/members/table_component.rb
+++ app/components/members/table_component.rb
@@ -4,10 +4,15 @@ module Members
   # Component for rendering a table of members
   class TableComponent < Component
     include Ransack::Helpers::FormHelper
+    include MembersHelper
 
     # rubocop:disable Naming/MethodParameterName
-    def initialize(q)
+    def initialize(namespace, members, q, current_user, abilities)
+      @namespace = namespace
+      @members = members
       @q = q
+      @current_user = current_user
+      @abilities = abilities
       @columns = columns
     end
 
@@ -17,6 +22,13 @@ module Members
         classes: class_names('relative overflow-x-auto'),
         data: { turbo: :temporary }
       }
+    end
+
+    def row_arguments(member)
+      { tag: 'tr' }.tap do |args|
+        args[:classes] = class_names('bg-white', 'border-b', 'dark:bg-slate-800', 'dark:border-slate-700')
+        args[:id] = dom_id(member)
+      end
     end
 
     def render_cell(**arguments, &)
