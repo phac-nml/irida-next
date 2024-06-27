8ee769258c40d65ed8637d4ffad500a93608af7f
Deep Sidhu <deepsidhu85@gmail.com>
2024-06-27 11:01:48 -0500

    Removed importing of utilities/form in application.js as it's not required there

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "utilities/form";
+//import "utilities/form";
 import * as ActiveStorage from "@rails/activestorage";
 ActiveStorage.start();
 
