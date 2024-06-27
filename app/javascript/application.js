6ccc74403e105c88dc71dae7edee640ff25d33a1
Deep Sidhu <deepsidhu85@gmail.com>
2024-06-27 11:20:07 -0500

    Removed commented out code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-//import "utilities/form";
 import * as ActiveStorage from "@rails/activestorage";
 ActiveStorage.start();
 
