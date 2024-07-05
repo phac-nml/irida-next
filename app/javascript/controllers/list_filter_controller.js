df2c11d0d6da589685059ab06c7fe0c6620c40f3
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-07-05 11:19:33 -0500

    chore: Fixed bug with select all

--- app/javascript/controllers/list_filter_controller.js
+++ app/javascript/controllers/list_filter_controller.js
@@ -5,106 +5,108 @@ const SPACE = 32;
 const COMMA = 188;
 
 export default class extends Controller {
-  static targets = ["tags", "template", "input", "count"];
-  static outlets = ["selection"];
+	static targets = ["tags", "template", "input", "count"];
+	static outlets = ["selection"];
 
-  handleInput(event) {
-    const value = event.target.value.trim();
-    if (event.keyCode === BACKSPACE && value.length === 0) {
-      // Handle backspace event when input is empty, otherwise just let
-      this.#handleBackspace(event);
-    } else if (
-      value.length === 0 &&
-      (event.keyCode === COMMA || event.keyCode === SPACE)
-    ) {
-      // Handle when a `,` is entered alone, that is do nothing
-      event.preventDefault();
-    } else if (event.keyCode === COMMA) {
-      // If string ends with a coma, directly add the tag without debounce
-      event.preventDefault();
-      this.#clearAndFocus();
-      this.tagsTarget.insertBefore(this.#formatTag(value), this.inputTarget);
-    }
-  }
+	handleInput(event) {
+		const value = event.target.value.trim();
+		if (event.keyCode === BACKSPACE && value.length === 0) {
+			// Handle backspace event when input is empty, otherwise just let
+			this.#handleBackspace(event);
+		} else if (
+			value.length === 0 &&
+			(event.keyCode === COMMA || event.keyCode === SPACE)
+		) {
+			// Handle when a `,` is entered alone, that is do nothing
+			event.preventDefault();
+		} else if (event.keyCode === COMMA) {
+			// If string ends with a coma, directly add the tag without debounce
+			event.preventDefault();
+			this.#clearAndFocus();
+			this.tagsTarget.insertBefore(this.#formatTag(value), this.inputTarget);
+		}
+	}
 
-  handlePaste(event) {
-    event.preventDefault();
-    const data = (event.clipboardData || window.clipboardData).getData("text");
-    const items = this.#getNamesAndPUID(data);
-    items.forEach((item) =>
-      this.tagsTarget.insertBefore(this.#formatTag(item), event.target),
-    );
-    this.#clearAndFocus();
-  }
+	handlePaste(event) {
+		event.preventDefault();
+		const data = (event.clipboardData || window.clipboardData).getData("text");
+		const items = this.#getNamesAndPUID(data);
+		for (const item of items) {
+			this.tagsTarget.insertBefore(this.#formatTag(item), event.target);
+		}
+		this.#clearAndFocus();
+	}
 
-  remove({ target }) {
-    const item = target.closest("span.filter-item");
-    item.parentNode.removeChild(item);
-  }
+	remove({ target }) {
+		const item = target.closest("span.filter-item");
+		item.parentNode.removeChild(item);
+	}
 
-  clear() {
-    const tags = this.tagsTarget.querySelectorAll(".search-tag");
-    [...tags].forEach((tag) => this.tagsTarget.removeChild(tag));
-    this.inputTarget.value = "";
-  }
+	clear() {
+		const tags = this.tagsTarget.querySelectorAll(".search-tag");
+		for (const tag of tags) {
+			this.tagsTarget.removeChild(tag);
+		}
+		this.inputTarget.value = "";
+	}
 
-  focus() {
-    this.inputTarget.focus();
-  }
+	focus() {
+		this.inputTarget.focus();
+	}
 
-  afterSubmit() {
-    if (this.hasSelectionOutlet) {
-      this.selectionOutlet.clear();
-    }
-    // check to see if there is any text in the input
-    if (this.inputTarget.value.length > 0) {
-      this.tagsTarget.insertBefore(
-        this.#formatTag(this.inputTarget.value),
-        this.inputTarget,
-      );
-      this.inputTarget.value = "";
-    }
-    this.#updateCount();
-  }
+	afterSubmit() {
+		if (this.hasSelectionOutlet) {
+			this.selectionOutlet.clear();
+		}
+		// check to see if there is any text in the input
+		if (this.inputTarget.value.length > 0) {
+			this.tagsTarget.insertBefore(
+				this.#formatTag(this.inputTarget.value),
+				this.inputTarget,
+			);
+			this.inputTarget.value = "";
+		}
+		this.#updateCount();
+	}
 
-  #handleBackspace(event) {
-    const tags = this.tagsTarget.querySelectorAll("span.search-tag");
-    if (tags.length === 0) return;
-    const last = tags[tags.length - 1];
-    const text = last.querySelector(".label").innerText;
-    this.tagsTarget.removeChild(last);
-    this.inputTarget.value = text;
-  }
+	#handleBackspace(event) {
+		const tags = this.tagsTarget.querySelectorAll("span.search-tag");
+		if (tags.length === 0) return;
+		const last = tags[tags.length - 1];
+		const text = last.querySelector(".label").innerText;
+		this.tagsTarget.removeChild(last);
+		this.inputTarget.value = text;
+	}
 
-  #getNamesAndPUID(value) {
-    return value
-      .split(/\r?\n|,/)
-      .map((t) => t.trim())
-      .filter(Boolean);
-  }
+	#getNamesAndPUID(value) {
+		return value
+			.split(/\r?\n|,/)
+			.map((t) => t.trim())
+			.filter(Boolean);
+	}
 
-  #clearAndFocus() {
-    this.inputTarget.value = "";
-    this.inputTarget.focus();
-  }
+	#clearAndFocus() {
+		this.inputTarget.value = "";
+		this.inputTarget.focus();
+	}
 
-  #formatTag(item) {
-    const clone = this.templateTarget.content.cloneNode(true);
-    clone.querySelector(".label").innerText = item;
-    clone.querySelector("input").value = item;
-    clone.querySelector("input").id = Date.now().toString(36);
-    return clone;
-  }
+	#formatTag(item) {
+		const clone = this.templateTarget.content.cloneNode(true);
+		clone.querySelector(".label").innerText = item;
+		clone.querySelector("input").value = item;
+		clone.querySelector("input").id = Date.now().toString(36);
+		return clone;
+	}
 
-  #updateCount() {
-    const count = this.tagsTarget.querySelectorAll(".search-tag").length;
-    if (count > 0) {
-      this.countTarget.innerText = count;
-      this.countTarget.classList.remove("hidden");
-      this.countTarget.classList.add("inline-flex");
-    } else {
-      this.countTarget.classList.remove("inline-flex");
-      this.countTarget.classList.add("hidden");
-    }
-  }
+	#updateCount() {
+		const count = this.tagsTarget.querySelectorAll(".search-tag").length;
+		if (count > 0) {
+			this.countTarget.innerText = count;
+			this.countTarget.classList.remove("hidden");
+			this.countTarget.classList.add("inline-flex");
+		} else {
+			this.countTarget.classList.remove("inline-flex");
+			this.countTarget.classList.add("hidden");
+		}
+	}
 }
--- app/javascript/controllers/selection_controller.js
+++ app/javascript/controllers/selection_controller.js
@@ -1,117 +1,127 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  // # indicates private attribute or method
-  // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_properties
-  #storageKey = null;
-  #numSelected = 0;
+	// # indicates private attribute or method
+	// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_properties
+	#storageKey = null;
+	#numSelected = 0;
 
-  static targets = ["rowSelection", "selectAll", "selected"];
-  static outlets = ["action-link"];
+	static targets = ["rowSelection", "selectAll", "selected"];
+	static outlets = ["action-link"];
 
-  static values = {
-    storageKey: {
-      type: String,
-    },
-    total: Number,
-  };
+	static values = {
+		storageKey: {
+			type: String,
+		},
+		total: Number,
+		url: String,
+	};
 
-  connect() {
-    this.#storageKey =
-      this.storageKeyValue ||
-      `${location.protocol}//${location.host}${location.pathname}${location.search}`;
+	connect() {
+		this.#storageKey =
+			this.storageKeyValue ||
+			`${location.protocol}//${location.host}${location.pathname}${location.search}`;
 
-    this.element.setAttribute("data-controller-connected", "true");
+		this.element.setAttribute("data-controller-connected", "true");
 
-    const storageValue = this.getStoredSamples();
+		const storageValue = this.getStoredSamples();
 
-    if (storageValue) {
-      this.#updateUI(storageValue);
-      this.#numSelected = storageValue.length;
-    } else {
-      this.save([]);
-    }
+		if (storageValue) {
+			this.#updateUI(storageValue);
+			this.#numSelected = storageValue.length;
+		} else {
+			this.save([]);
+		}
 
-    this.#updateCounts(storageValue.length);
-  }
+		this.#updateCounts(storageValue.length);
+	}
 
-  actionLinkOutletConnected(outlet) {
-    const storageValue = this.getStoredSamples();
-    outlet.setDisabled(storageValue.length);
-  }
+	toggleSelectAll(event) {
+		const form = document.querySelector("form#sample_search").cloneNode(true);
+		form.action = event.target.dataset.url;
+		form.classList.add("hidden");
+		document.body.appendChild(form);
+		form.requestSubmit();
+		document.body.removeChild(form);
+	}
 
-  toggle(event) {
-    this.#addOrRemove(event.target.checked, event.target.value);
-  }
+	actionLinkOutletConnected(outlet) {
+		const storageValue = this.getStoredSamples();
+		outlet.setDisabled(storageValue.length);
+	}
 
-  remove({ params: { id } }) {
-    this.#addOrRemove(false, id);
-  }
+	toggle(event) {
+		this.#addOrRemove(event.target.checked, event.target.value);
+	}
 
-  clear() {
-    sessionStorage.removeItem(this.#storageKey);
-  }
+	remove({ params: { id } }) {
+		this.#addOrRemove(false, id);
+	}
 
-  save(storageValue) {
-    sessionStorage.setItem(this.#storageKey, JSON.stringify([...storageValue]));
-    this.#numSelected = storageValue.length;
-  }
+	clear() {
+		sessionStorage.removeItem(this.#storageKey);
+	}
 
-  update(ids) {
-    this.save(ids);
-    this.#updateUI(ids);
-  }
+	save(storageValue) {
+		sessionStorage.setItem(this.#storageKey, JSON.stringify([...storageValue]));
+		this.#numSelected = storageValue.length;
+	}
 
-  getNumSelected() {
-    return this.#numSelected;
-  }
+	update(ids) {
+		this.save(ids);
+		this.#updateUI(ids);
+	}
 
-  getStoredSamples() {
-    return JSON.parse(sessionStorage.getItem(this.#storageKey)) || [];
-  }
+	getNumSelected() {
+		return this.#numSelected;
+	}
 
-  #addOrRemove(add, storageValue) {
-    const newStorageValue = this.getStoredSamples();
+	getStoredSamples() {
+		return JSON.parse(sessionStorage.getItem(this.#storageKey)) || [];
+	}
 
-    if (add) {
-      newStorageValue.push(storageValue);
-    } else {
-      const index = newStorageValue.indexOf(storageValue);
-      if (index > -1) {
-        newStorageValue.splice(index, 1);
-      }
-    }
+	#addOrRemove(add, storageValue) {
+		const newStorageValue = this.getStoredSamples();
 
-    this.save(newStorageValue);
-    this.#updateActionLinks(newStorageValue.length);
-    this.#setSelectAllCheckboxValue(newStorageValue.length);
-    this.#updateCounts(newStorageValue.length);
-  }
+		if (add) {
+			newStorageValue.push(storageValue);
+		} else {
+			const index = newStorageValue.indexOf(storageValue);
+			if (index > -1) {
+				newStorageValue.splice(index, 1);
+			}
+		}
 
-  #updateUI(ids) {
-    this.rowSelectionTargets.map((row) => {
-      row.checked = ids.indexOf(row.value) > -1;
-    });
-    this.#updateActionLinks(ids.length);
-    this.#setSelectAllCheckboxValue(ids.length);
-    this.#updateCounts(ids.length);
-  }
+		this.save(newStorageValue);
+		this.#updateActionLinks(newStorageValue.length);
+		this.#setSelectAllCheckboxValue(newStorageValue.length);
+		this.#updateCounts(newStorageValue.length);
+	}
 
-  #updateActionLinks(count) {
-    this.actionLinkOutlets.forEach((outlet) => {
-      outlet.setDisabled(count);
-    });
-  }
+	#updateUI(ids) {
+		this.rowSelectionTargets.map((row) => {
+			row.checked = ids.indexOf(row.value) > -1;
+		});
+		this.#updateActionLinks(ids.length);
+		this.#setSelectAllCheckboxValue(ids.length);
+		this.#updateCounts(ids.length);
+	}
 
-  #setSelectAllCheckboxValue(numSelected) {
-    if (this.hasSelectAllTarget && this.totalValue > 0) {
-      this.selectAllTarget.checked = this.totalValue === numSelected;
-    }
-  }
+	#updateActionLinks(count) {
+		this.actionLinkOutlets.forEach((outlet) => {
+			outlet.setDisabled(count);
+		});
+	}
 
-  #updateCounts(selected) {
-    if (this.hasSelectedTarget) {
-      this.selectedTarget.innerText = selected;
-    }
-  }
+	#setSelectAllCheckboxValue(numSelected) {
+		if (this.hasSelectAllTarget && this.totalValue > 0) {
+			this.selectAllTarget.checked = this.totalValue === numSelected;
+		}
+	}
+
+	#updateCounts(selected) {
+		if (this.hasSelectedTarget) {
+			this.selectedTarget.innerText = selected;
+		}
+	}
 }
