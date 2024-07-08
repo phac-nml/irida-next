1e989c64f261d15b6d98958a692cb61aa47206d2
Chris Huynh <chris.huynh333@gmail.com>
2024-07-08 11:00:09 -0500

    Convert to using class_names for conditional css classes

--- app/components/layout/sidebar/selected_component.rb
+++ app/components/layout/sidebar/selected_component.rb
@@ -4,10 +4,20 @@ module Layout
   module Sidebar
     # Sidebar selected component
     class SelectedComponent < Component
-      attr_reader :selected
+      erb_template <<-ERB
+        <div aria-hidden="true" class="<%= @selected_classes %>"></div>
+      ERB
 
       def initialize(selected: nil)
         @selected = selected
+
+        @selected_classes = class_names(
+          'w-1 h-6 my-0 mr-1 rounded-lg bg-primary-600',
+          {
+            'rounded-lg bg-primary-600': selected,
+            'bg-transparent': !selected
+          }
+        )
       end
     end
   end
