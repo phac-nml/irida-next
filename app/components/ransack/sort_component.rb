ea6255f9ee9c2f75ad0be444fbbb0ec046b19374
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-07-08 10:30:21 -0500

    chore: Fixed sorting

--- app/components/ransack/sort_component.rb
+++ app/components/ransack/sort_component.rb
@@ -3,14 +3,14 @@
 module Ransack
   # Component for ransack sort in table headers
   class SortComponent < Component
-    attr_reader :ransack_obj, :label, :field, :url, :value
+    attr_reader :ransack_obj, :label, :url, :value
 
     def initialize(ransack_obj:, label:, url:, field:)
       @ransack_obj = ransack_obj
       @label = label
       @url = url
       @field = field
-      @value = @ransack_obj.sorts[0].dir == 'asc' ? 'desc' : 'asc'
+      @value = "#{field} #{@ransack_obj.sorts[0].dir == 'asc' ? 'desc' : 'asc'}"
     end
 
     def icon
