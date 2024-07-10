253c71f7f165e4eca6ef736375dd1792f237647f
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-07-10 14:11:09 -0500

    fixing tests

--- test/components/members/table_component_test.rb
+++ test/components/members/table_component_test.rb
@@ -5,11 +5,10 @@ require 'test_helper'
 module Members
   class TableComponentTest < ViewComponent::TestCase
     test 'Should render a table of group members' do
-      namespace = groups(:group_one)
-      user = users(:john_doe)
-      members = Member.for_namespace_and_ancestors(namespace).not_expired
-
       with_request_url '/-/groups/group-1/-/members' do
+        namespace = groups(:group_one)
+        user = users(:john_doe)
+        members = Member.for_namespace_and_ancestors(namespace).not_expired
         q = Member.ransack({ s: 'user_email asc', user_email_cont: 'admin' })
 
         render_inline TableComponent.new(
@@ -25,16 +24,21 @@ module Members
         )
 
         assert_selector 'table', count: 1
+        assert_selector 'table thead th', count: 6
+        assert_selector 'table tbody tr', count: members.count
+        members.each do |member|
+          assert_selector 'table tbody tr td:nth-child(1)', text: member.user.email
+          assert_selector 'table tbody tr td:nth-child(2)', text: Member::AccessLevel.human_access(member.access_level)
+        end
       end
     end
 
     test 'Should render a table of project members' do
-      project =  projects(:project1)
-      namespace = project.namespace
-      user = users(:john_doe)
-      members = Member.for_namespace_and_ancestors(namespace).not_expired
-
-      with_request_url '/-/projects/project-1/-/members' do
+      with_request_url '/group-1/project-1/-/members' do
+        project = projects(:project1)
+        namespace = project.namespace
+        user = users(:john_doe)
+        members = Member.for_namespace_and_ancestors(namespace).not_expired
         q = Member.ransack({ s: 'user_email asc', user_email_cont: 'admin' })
 
         render_inline TableComponent.new(
@@ -50,6 +54,12 @@ module Members
         )
 
         assert_selector 'table', count: 1
+        assert_selector 'table thead th', count: 6
+        assert_selector 'table tbody tr', count: members.count
+        members.each do |member|
+          assert_selector 'table tbody tr td:nth-child(1)', text: member.user.email
+          assert_selector 'table tbody tr td:nth-child(2)', text: Member::AccessLevel.human_access(member.access_level)
+        end
       end
     end
   end
