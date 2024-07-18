6ab6b8f901f6868bdc0226f7739a66e7e25be775
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-07-18 10:23:47 -0500

    fix: Added missing top-level comment and frozen string literal

--- db/migrate/20240711160907_add_sessions_table.rb
+++ db/migrate/20240711160907_add_sessions_table.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# migration to add sessions table for storing session data
 class AddSessionsTable < ActiveRecord::Migration[7.1]
   def change
     create_table :sessions do |t|
