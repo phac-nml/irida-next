18d3e533c03404395fd485500107f3af8776a0d3
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-07-18 11:14:19 -0500

    chore: Fixed migration

--- db/schema.rb
+++ db/schema.rb
@@ -241,7 +241,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_11_160907) do
 
   create_table "sessions", force: :cascade do |t|
     t.string "session_id", null: false
-    t.text "data"
+    t.jsonb "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
