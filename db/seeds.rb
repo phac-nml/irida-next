cf02a6c78ec48b8993544c247a18b76345146cb8
Chris Huynh <chris.huynh333@gmail.com>
2024-07-22 13:37:12 -0500

    Fix export seeds to use namespace_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -196,55 +196,63 @@ def seed_workflow_executions # rubocop:disable Metrics/MethodLength, Metrics/Abc
 end
 
 def seed_exports # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-  users_and_sample_ids = [
+  export_params = [
     {
       user: User.find_by(email: 'admin@email.com'),
-      sample_id: Sample.find_by(name: 'Bacillus anthracis/Outbreak 2022 Sample 10').id
+      sample_id: Sample.find_by(name: 'Bacillus anthracis/Outbreak 2022 Sample 10').id,
+      namespace_id: Sample.find_by(name: 'Bacillus anthracis/Outbreak 2022 Sample 10').project.namespace.id
     },
     {
       user: User.find_by(email: 'user1@email.com'),
-      sample_id: Sample.find_by(name: 'Bartonella henselae/Outbreak 2022 Sample 10').id
+      sample_id: Sample.find_by(name: 'Bartonella henselae/Outbreak 2022 Sample 10').id,
+      namespace_id: Namespace.find_by(name: 'Bartonella').id
     },
     {
       user: User.find_by(email: 'user2@email.com'),
-      sample_id: Sample.find_by(name: 'Bordetella pertussis/Outbreak 2022 Sample 10').id
+      sample_id: Sample.find_by(name: 'Bordetella pertussis/Outbreak 2022 Sample 10').id,
+      namespace_id: Sample.find_by(name: 'Bordetella pertussis/Outbreak 2022 Sample 10').project.namespace.id
     },
     {
       user: User.find_by(email: 'user3@email.com'),
-      sample_id: Sample.find_by(name: 'Borrelia burgdorferi/Outbreak 2022 Sample 10').id
+      sample_id: Sample.find_by(name: 'Borrelia burgdorferi/Outbreak 2022 Sample 10').id,
+      namespace_id: Namespace.find_by(name: 'Borrelia').id
     },
     {
       user: User.find_by(email: 'user4@email.com'),
-      sample_id: Sample.find_by(name: 'Brucella abortus/Outbreak 2022 Sample 10').id
+      sample_id: Sample.find_by(name: 'Brucella abortus/Outbreak 2022 Sample 10').id,
+      namespace_id: Sample.find_by(name: 'Brucella abortus/Outbreak 2022 Sample 10').project.namespace.id
     },
     {
       user: User.find_by(email: 'user5@email.com'),
-      sample_id: Sample.find_by(name: 'Campylobacter jejuni/Outbreak 2022 Sample 10').id
+      sample_id: Sample.find_by(name: 'Campylobacter jejuni/Outbreak 2022 Sample 10').id,
+      namespace_id: Namespace.find_by(name: 'Campylobacter').id
     },
     {
       user: User.find_by(email: 'user6@email.com'),
-      sample_id: Sample.find_by(name: 'Yersinia pestis/Outbreak 2022 Sample 10').id
+      sample_id: Sample.find_by(name: 'Yersinia pestis/Outbreak 2022 Sample 10').id,
+      namespace_id: Sample.find_by(name: 'Yersinia pestis/Outbreak 2022 Sample 10').project.namespace.id
     },
     {
       user: User.find_by(email: 'user7@email.com'),
-      sample_id: Sample.find_by(name: 'Vibrio cholerae/Outbreak 2023 Sample 10').id
+      sample_id: Sample.find_by(name: 'Vibrio cholerae/Outbreak 2023 Sample 10').id,
+      namespace_id: Namespace.find_by(name: 'Vibrio').id
     }
   ]
-  users_and_sample_ids.each_with_index do |user_and_sample_id, index|
+  export_params.each_with_index do |export_param, index|
     # export with status=processing and no attachment
     DataExport.create(
-      user: user_and_sample_id[:user],
+      user: export_param[:user],
       name: index.even? ? "Seeded export #{index + 1}" : nil,
-      export_parameters: { ids: [user_and_sample_id[:sample_id]] },
+      export_parameters: { ids: [export_param[:sample_id]], namespace_id: export_param[:namespace_id] },
       status: 'processing',
       export_type: 'sample',
       email_notification: index.even? && true
     )
     # export with status=ready with zip attachment
     data_export = DataExport.create(
-      user: user_and_sample_id[:user],
+      user: export_param[:user],
       name: index.even? ?  nil : "Seeded export #{index + 1}",
-      export_parameters: { ids: [user_and_sample_id[:sample_id]] },
+      export_parameters: { ids: [export_param[:sample_id]], namespace_id: export_param[:namespace_id] },
       status: 'processing',
       export_type: 'sample',
       email_notification: index.even? || true
