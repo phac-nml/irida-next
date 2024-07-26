f135a5ab693e5ba6fd645346b6e6441e9872a19c
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-07-26 15:47:12 -0500

    fix and add tests for update_sample_metadata

--- test/graphql/update_sample_metadata_test.rb
+++ test/graphql/update_sample_metadata_test.rb
@@ -13,7 +13,10 @@ class UpdateSampleMetadataMutationTest < ActiveSupport::TestCase
           metadata
         },
         status,
-        errors
+        errors {
+          path
+          message
+        }
       }
     }
   GRAPHQL
@@ -28,7 +31,10 @@ class UpdateSampleMetadataMutationTest < ActiveSupport::TestCase
           metadata
         },
         status,
-        errors
+        errors {
+          path
+          message
+        }
       }
     }
   GRAPHQL
@@ -66,6 +72,27 @@ class UpdateSampleMetadataMutationTest < ActiveSupport::TestCase
                                  context: { current_user: @user, token: @api_scope_token },
                                  variables: { samplePuid: @sample.puid,
                                               metadata: { key1: 'value1' } })
+
+    assert_nil result['errors'], 'should work and have no errors.'
+
+    data = result['data']['updateSampleMetadata']
+
+    assert_not_empty data, 'updateSampleMetadata should be populated when no authorization errors'
+    assert_empty data['errors']
+    assert_not_empty data['status']
+    assert_not_empty data['status'][:added]
+    assert_equal 'key1', data['status'][:added].first
+    assert_not_empty data['sample']
+    assert_not_empty data['sample']['metadata']
+    assert_not_empty data['sample']['metadata']['key1']
+    assert_equal 'value1', data['sample']['metadata']['key1']
+  end
+
+  test 'updateSampleMetadata mutation should work with valid JSON String params, global id, and api scope token' do
+    result = IridaSchema.execute(UPDATE_SAMPLE_METADATA_BY_SAMPLE_ID_MUTATION,
+                                 context: { current_user: @user, token: @api_scope_token },
+                                 variables: { sampleId: @sample.to_global_id.to_s,
+                                              metadata: "{ \"key1\": \"value1\" }" }) # rubocop:disable Style/StringLiterals
 
     assert_nil result['errors'], 'should work and have no errors.'
 
@@ -122,7 +149,11 @@ class UpdateSampleMetadataMutationTest < ActiveSupport::TestCase
     assert_empty data['status'][:not_updated],
                  'status not_updated changes should be empty as the sample was not updated.'
     assert_not_empty data['errors']
-    assert_equal I18n.t('services.samples.metadata.empty_metadata', sample_name: @sample.name), data['errors'][0]
+    expected_error = {
+      'path' => %w[sample base],
+      'message' => I18n.t('services.samples.metadata.empty_metadata', sample_name: @sample.name)
+    }
+    assert_equal expected_error, data['errors'][0]
   end
 
   test 'updateSampleMetadata mutation should not work with valid params and read api scope token' do
@@ -168,5 +199,85 @@ class UpdateSampleMetadataMutationTest < ActiveSupport::TestCase
     error_message = result['errors'][0]['message']
 
     assert_equal 'You are not authorized to update samples for project Project 1 on this server.', error_message
+  end
+
+  test 'updateSampleMetadata mutation should not work with invalid sample id' do
+    result = IridaSchema.execute(UPDATE_SAMPLE_METADATA_BY_SAMPLE_ID_MUTATION,
+                                 context: { current_user: @user, token: @api_scope_token },
+                                 variables: { sampleId: 'not a sample id',
+                                              metadata: { key1: 'value1' } })
+
+    assert_nil result['errors'], 'should work and have no errors.'
+
+    data = result['data']['updateSampleMetadata']
+
+    assert_not_empty data, 'updateSampleMetadata should be populated when no authorization errors'
+    assert_not_empty data['errors']
+
+    expected_error = [
+      'path' => ['sample'],
+      'message' => 'not a sample id is not a valid IRIDA Next ID.'
+    ]
+    assert_equal expected_error, data['errors']
+  end
+
+  test 'updateSampleMetadata mutation should not work with invalid sample puid' do
+    result = IridaSchema.execute(UPDATE_SAMPLE_METADATA_BY_SAMPLE_PUID_MUTATION,
+                                 context: { current_user: @user, token: @api_scope_token },
+                                 variables: { samplePuid: 'not a sample puid',
+                                              metadata: { key1: 'value1' } })
+
+    assert_nil result['errors'], 'should work and have no errors.'
+
+    data = result['data']['updateSampleMetadata']
+
+    assert_not_empty data, 'updateSampleMetadata should be populated when no authorization errors'
+    assert_not_empty data['errors']
+
+    expected_error = [
+      'path' => ['sample'],
+      'message' => 'not found by provided ID or PUID'
+    ]
+    assert_equal expected_error, data['errors']
+  end
+
+  test 'updateSampleMetadata mutation should not work with invalid JSON formatting' do
+    result = IridaSchema.execute(UPDATE_SAMPLE_METADATA_BY_SAMPLE_ID_MUTATION,
+                                 context: { current_user: @user, token: @api_scope_token },
+                                 variables: { sampleId: @sample.to_global_id.to_s,
+                                              metadata: "bad formatting" }) # rubocop:disable Style/StringLiterals,Lint/RedundantCopDisableDirective
+
+    assert_nil result['errors'], 'should work and have no errors.'
+
+    data = result['data']['updateSampleMetadata']
+
+    assert_not_empty data
+    assert_not_empty data['errors']
+
+    expected_error = [
+      'path' => ['metadata'],
+      'message' => "JSON data is not formatted correctly. unexpected token at 'bad formatting'"
+    ]
+    assert_equal expected_error, data['errors']
+  end
+
+  test 'updateSampleMetadata mutation should not work with non JSON data' do
+    result = IridaSchema.execute(UPDATE_SAMPLE_METADATA_BY_SAMPLE_ID_MUTATION,
+                                 context: { current_user: @user, token: @api_scope_token },
+                                 variables: { sampleId: @sample.to_global_id.to_s,
+                                              metadata: 1234 })
+
+    assert_nil result['errors'], 'should work and have no errors.'
+
+    data = result['data']['updateSampleMetadata']
+
+    assert_not_empty data
+    assert_not_empty data['errors']
+
+    expected_error = [
+      'path' => ['metadata'],
+      'message' => 'is not JSON data'
+    ]
+    assert_equal expected_error, data['errors']
   end
 end
