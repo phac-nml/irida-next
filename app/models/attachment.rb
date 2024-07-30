95d4898dcf7cab1da14e4702f44f181a55e14506
Meghan Chua <17057809+malchua@users.noreply.github.com>
2024-07-30 11:47:35 -0500

    Simplify the assign_metadata method by adding a hash

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -44,40 +44,29 @@ class Attachment < ApplicationRecord
 
   private
 
-  def assign_metadata # rubocop:disable Metrics/AbcSize
+  FORMAT_REGEX = {
+    fasta: /^\S+\.fn?a(sta)?(\.gz)?$/,
+    fastq: /^\S+\.f(ast)?q(\.gz)?$/,
+    text: /^\S+\.(txt|rtf)?(\.gz)?$/,
+    csv: /^\S+\.(csv)?(\.gz)?$/,
+    tsv: /^\S+\.(tsv)?(\.gz)?$/,
+    spreadsheet: /^\S+\.(xls|xlsx)?$/,
+    json: /^\S+\.(json)?(\.gz)?$/,
+    genbank: /^\S+\.(gbk|gbf|genbank)?(\.gz)?$/
+  }.freeze
+
+  def assign_metadata
     return if metadata.key? 'format'
 
-    case filename.to_s
-    # Assigns fasta to metadata format and assembly to type for following file types:
-    # .fasta, .fasta.gz, .fna, .fna.gz, .fa, .fa.gz
-    when /^\S+\.fn?a(sta)?(\.gz)?$/
-      metadata['format'] = 'fasta'
-      metadata['type'] = 'assembly'
-    # Assigns fastq to metadata format for following file types: .fastq, .fastq.gz, .fq, .fq.gz
-    when /^\S+\.f(ast)?q(\.gz)?$/
-      metadata['format'] = 'fastq'
-    # Assigns text to metadata format for following file types: .txt, .txt.gz, .rtf, .rtf.gz
-    when /^\S+\.(txt|rtf)?(\.gz)?$/
-      metadata['format'] = 'text'
-    # Assigns text to metadata format for following file types: .csv, .csv.gz
-    when /^\S+\.(csv)?(\.gz)?$/
-      metadata['format'] = 'csv'
-    # Assigns text to metadata format for following file types: .tsv, .tsv.gz
-    when /^\S+\.(tsv)?(\.gz)?$/
-      metadata['format'] = 'tsv'
-    # Assigns spreadsheet to metadata format for following file types: .xls, .xlsx
-    when /^\S+\.(xls|xlsx)?$/
-      metadata['format'] = 'spreadsheet'
-    # Assigns text to metadata format for following file types: .json, json.gz
-    when /^\S+\.(json)?(\.gz)?$/
-      metadata['format'] = 'json'
-    # Assigns text to metadata format for following file types: .gbk, .gbk.gz, .gbf, .gbf.gz, .gb, .gb.gz
-    when /^\S+\.(gbk|gbf|genbank)?(\.gz)?$/
-      metadata['format'] = 'genbank'
-    # Else assigns unknown to metadata format
-    else
+    found_format = FORMAT_REGEX.find { |key, value| filename.to_s =~ value }
+
+    if found_format.nil?
       metadata['format'] = 'unknown'
+    else
+      metadata['format'] = found_format[0].to_s
+      metadata['type'] = 'assembly' if metadata['format'] == 'fasta'
     end
+
     metadata['compression'] = filename.to_s.match?(/^\S+(.gz)+$/) ? 'gzip' : 'none'
   end
 end
