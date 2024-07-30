0394b9159956f28db8b0d53b0f5632c785d5e116
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-07-30 11:24:32 -0500

    feat: make sample id and sample name column sticky in samples table, fix pagination links for samples table and morphing.

--- app/components/pagination_component.rb
+++ app/components/pagination_component.rb
@@ -4,9 +4,21 @@
 class PaginationComponent < ViewComponent::Base
   attr_reader :prev_url, :next_url, :info
 
-  def initialize(info:, prev_url: nil, next_url: nil)
+  def initialize(info:, prev_url: nil, next_url: nil, **link_arguments)
     @info = info
     @prev_url = prev_url
     @next_url = next_url
+    @link_arguments = link_arguments
+  end
+
+  def link_arguments
+    if @link_arguments.empty?
+      { data: {
+        turbo_stream: 'true'
+      } }
+    else
+      puts @link_arguments
+      @link_arguments
+    end
   end
 end
