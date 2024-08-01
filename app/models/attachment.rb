d90a2b7b744ce4d12dcb67683871a974e83b4037
Meghan Chua <17057809+malchua@users.noreply.github.com>
2024-08-01 12:50:15 -0500

    Make FORMAT_REGEX public to make it accessible

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,10 +2,21 @@
 
 # entity class for Attachment
 class Attachment < ApplicationRecord
+  include HasPuid
+
+  FORMAT_REGEX = {
+    fasta: /^\S+\.fn?a(sta)?(\.gz)?$/,
+    fastq: /^\S+\.f(ast)?q(\.gz)?$/,
+    text: /^\S+\.(txt|rtf)?(\.gz)?$/,
+    csv: /^\S+\.(csv)?(\.gz)?$/,
+    tsv: /^\S+\.(tsv)?(\.gz)?$/,
+    spreadsheet: /^\S+\.(xls|xlsx)?$/,
+    json: /^\S+\.(json)?(\.gz)?$/,
+    genbank: /^\S+\.(gbk|gbf|genbank)?(\.gz)?$/
+  }.freeze
+
   has_logidze
   acts_as_paranoid
-
-  include HasPuid
 
   belongs_to :attachable, touch: :attachments_updated_at, polymorphic: true
 
@@ -44,17 +55,7 @@ class Attachment < ApplicationRecord
 
   private
 
-  FORMAT_REGEX = {
-    fasta: /^\S+\.fn?a(sta)?(\.gz)?$/,
-    fastq: /^\S+\.f(ast)?q(\.gz)?$/,
-    text: /^\S+\.(txt|rtf)?(\.gz)?$/,
-    csv: /^\S+\.(csv)?(\.gz)?$/,
-    tsv: /^\S+\.(tsv)?(\.gz)?$/,
-    spreadsheet: /^\S+\.(xls|xlsx)?$/,
-    json: /^\S+\.(json)?(\.gz)?$/,
-    genbank: /^\S+\.(gbk|gbf|genbank)?(\.gz)?$/
-  }.freeze
-
+  # rubocop:disable Metrics/AbcSize
   def assign_metadata
     return if metadata.key? 'format'
 
@@ -69,4 +70,6 @@ class Attachment < ApplicationRecord
 
     metadata['compression'] = filename.to_s.match?(/^\S+(.gz)+$/) ? 'gzip' : 'none'
   end
+
+  # rubocop:enable Metrics/AbcSize
 end
