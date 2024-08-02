2da883e4708bb3fd954629828efe82ff81b81aaa
Chris Huynh <chris.huynh333@gmail.com>
2024-08-02 16:24:51 -0500

    try using only tbody as selector for flaky tests

--- test/system/projects/group_links_test.rb
+++ test/system/projects/group_links_test.rb
@@ -311,7 +311,7 @@ module Projects
       assert_text 'Displaying 4 items'
       assert_selector '#project-members table tbody tr', count: 4
       assert_selector '#project-members table thead th:first-child svg.icon-arrow_up'
-      within first('#project-members table tbody') do
+      within first('tbody') do
         assert_selector 'tr:first-child td:first-child', text: @group_link14.group.name
         assert_selector 'tr:first-child td:nth-child(4)',
                         text: Member::AccessLevel.human_access(@group_link14.group_access_level)
@@ -328,7 +328,7 @@ module Projects
 
       click_on 'Group'
       assert_selector '#project-members table thead th:first-child svg.icon-arrow_down'
-      within first('#project-members table tbody') do
+      within first('tbody') do
         assert_selector 'tr:first-child td:first-child', text: @group_link6.group.name
         assert_selector 'tr:first-child td:nth-child(4)',
                         text: Member::AccessLevel.human_access(@group_link6.group_access_level)
@@ -345,7 +345,7 @@ module Projects
 
       click_on 'Source'
       assert_selector '#project-members table thead th:nth-child(2) svg.icon-arrow_up'
-      within first('#project-members table tbody') do
+      within first('tbody') do
         assert_selector 'tr:first-child td:first-child', text: @group_link5.group.name
         assert_selector 'tr:first-child td:nth-child(4)',
                         text: Member::AccessLevel.human_access(@group_link5.group_access_level)
@@ -362,7 +362,7 @@ module Projects
 
       click_on 'Source'
       assert_selector '#project-members table thead th:nth-child(2) svg.icon-arrow_down'
-      within first('#project-members table tbody') do
+      within first('tbody') do
         assert_selector 'tr:first-child td:first-child', text: @group_link2.group.name
         assert_selector 'tr:first-child td:nth-child(4)',
                         text: Member::AccessLevel.human_access(@group_link2.group_access_level)
@@ -379,7 +379,7 @@ module Projects
 
       click_on 'Access Level'
       assert_selector '#project-members table thead th:nth-child(4) svg.icon-arrow_up'
-      within first('#project-members table tbody') do
+      within first('tbody') do
         assert_selector 'tr:first-child td:first-child', text: @group_link5.group.name
         assert_selector 'tr:first-child td:nth-child(4)',
                         text: Member::AccessLevel.human_access(@group_link5.group_access_level)
@@ -396,7 +396,7 @@ module Projects
 
       click_on 'Access Level'
       assert_selector '#project-members table thead th:nth-child(4) svg.icon-arrow_down'
-      within first('#project-members table tbody') do
+      within first('tbody') do
         assert_selector 'tr:first-child td:first-child', text: @group_link6.group.name
         assert_selector 'tr:first-child td:nth-child(4)',
                         text: Member::AccessLevel.human_access(@group_link6.group_access_level)
@@ -413,7 +413,7 @@ module Projects
 
       click_on 'Expiration'
       assert_selector '#project-members table thead th:nth-child(5) svg.icon-arrow_up'
-      within first('#project-members table tbody') do
+      within first('tbody') do
         assert_selector 'tr:first-child td:first-child', text: @group_link2.group.name
         assert_selector 'tr:first-child td:nth-child(5)',
                         text: Member::AccessLevel.human_access(@group_link2.expires_at)
