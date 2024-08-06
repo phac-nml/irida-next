7a3492420a3ba4c09c167795654160c694de3ba9
Chris Huynh <chris.huynh333@gmail.com>
2024-08-06 12:19:31 -0500

    use local time in website

--- app/components/viral/time_ago_component.rb
+++ app/components/viral/time_ago_component.rb
@@ -3,11 +3,9 @@
 module Viral
   # Card component for rendering sections of pages.
   class TimeAgoComponent < Viral::Component
-    attr_reader :time_ago, :original_time
+    attr_reader :original_time
 
-    def initialize(original_time:, current_time: Time.zone.now)
-      original_time = original_time.to_datetime if original_time.is_a?(String)
-      @time_ago = distance_of_time_in_words(current_time, original_time)
+    def initialize(original_time:)
       @original_time = original_time
     end
   end
