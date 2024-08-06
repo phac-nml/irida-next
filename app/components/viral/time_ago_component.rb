1e31f8588e45f6cc9fdf22c6bfb9600e6385f809
Chris Huynh <chris.huynh333@gmail.com>
2024-08-06 12:50:01 -0500

    use imported localtimehelper

--- app/components/viral/time_ago_component.rb
+++ app/components/viral/time_ago_component.rb
@@ -3,10 +3,12 @@
 module Viral
   # Card component for rendering sections of pages.
   class TimeAgoComponent < Viral::Component
+    include LocalTimeHelper
     attr_reader :original_time
 
     def initialize(original_time:)
-      @original_time = original_time
+      @time_ago = distance_of_time_in_words(local_time(Time.current), local_time(original_time))
+      @formatted_tooltip_time = local_time(original_time, :long)
     end
   end
 end
