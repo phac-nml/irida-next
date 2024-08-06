3d67a58208439e87c4757b728623af5afa8217e9
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-08-06 14:46:55 -0500

    feat: update node(s) queries to support User and Attachment type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,9 +3,19 @@
 module Types
   # User Type
   class UserType < Types::BaseType
+    implements GraphQL::Types::Relay::Node
     description 'A user'
 
     field :email, String, null: false, description: 'User email.'
     field :id, ID, null: false, description: 'ID of the user.'
+
+    def self.authorized?(object, context)
+      super &&
+        allowed_to?(
+          :read?,
+          object,
+          context: { user: context[:current_user], token: context[:token] }
+        )
+    end
   end
 end
