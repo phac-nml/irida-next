2cdc09f6a3c5329117eb7e148b8e54b34f8b4132
Chris Huynh <chris.huynh333@gmail.com>
2024-08-07 10:18:25 -0500

    change remaining timestamps to use local_time gem

--- app/components/updated_component.rb
+++ app/components/updated_component.rb
@@ -2,9 +2,11 @@
 
 # A component for displaying when a record was updated.
 class UpdatedComponent < ViewComponent::Base
+  include LocalTimeHelper
   attr_reader :updated_at
 
   def initialize(updated_at:)
-    @updated_at = distance_of_time_in_words(Time.zone.now, updated_at, scope: 'datetime.distance_in_words.updated')
+    @updated_at = distance_of_time_in_words(local_time(Time.zone.now), local_time(updated_at),
+                                            scope: 'datetime.distance_in_words.updated')
   end
 end
