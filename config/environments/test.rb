24c0da24382f4b90afba258045ee544d7a4ddb7d
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-08-14 15:41:03 -0500

    chore: remove raising exception on missing callback actions

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Raise error when a before_action's only/except options reference missing actions.
-  config.action_controller.raise_on_missing_callback_actions = true
+  config.action_controller.raise_on_missing_callback_actions = false
 
   config.middleware.insert_before 0, Capybara::Lockstep::Middleware
 
