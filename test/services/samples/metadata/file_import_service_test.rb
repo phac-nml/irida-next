48eb396e80910cef3801dd9cfa5246d613436fb1
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-08-15 10:48:04 -0500

    adding policy translations

--- test/services/samples/metadata/file_import_service_test.rb
+++ test/services/samples/metadata/file_import_service_test.rb
@@ -34,17 +34,27 @@ module Samples
       end
 
       test 'import sample metadata without permission for project namespace' do
-        assert_raises(ActionPolicy::Unauthorized) do
+        exception = assert_raises(ActionPolicy::Unauthorized) do
           params = { file: @csv, sample_id_column: 'sample_name' }
           Samples::Metadata::FileImportService.new(@project.namespace, @jane_doe, params).execute
         end
+        assert_equal Namespaces::ProjectNamespacePolicy, exception.policy
+        assert_equal :update_sample_metadata?, exception.rule
+        assert exception.result.reasons.is_a?(::ActionPolicy::Policy::FailureReasons)
+        assert_equal I18n.t(:'action_policy.policy.namespaces/project_namespace.update_sample_metadata?',
+                            name: @project.name), exception.result.message
       end
 
       test 'import sample metadata without permission for group' do
-        assert_raises(ActionPolicy::Unauthorized) do
+        exception = assert_raises(ActionPolicy::Unauthorized) do
           params = { file: @csv, sample_id_column: 'sample_puid' }
           Samples::Metadata::FileImportService.new(@group, @jane_doe, params).execute
         end
+        assert_equal GroupPolicy, exception.policy
+        assert_equal :update_sample_metadata?, exception.rule
+        assert exception.result.reasons.is_a?(::ActionPolicy::Policy::FailureReasons)
+        assert_equal I18n.t(:'action_policy.policy.group.update_sample_metadata?',
+                            name: @group.name), exception.result.message
       end
 
       test 'import sample metadata with empty params' do
