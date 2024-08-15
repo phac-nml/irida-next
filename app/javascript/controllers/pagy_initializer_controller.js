0e4b6b95898370f855aac704c2f3e830f276a36c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-08-15 15:12:36 -0500

    chore: Removed unused stimulus controller

--- app/javascript/controllers/pagy_initializer_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-import Pagy from "pagy";  // if using sprockets, you can remove above line, but make sure you have the appropriate directive if your manifest.js file.
-
-export default class extends Controller {
-  connect() {
-    Pagy.init(this.element);
-  }
-}
