c3ffa53ef9c75355520701f7648e246deca8b09b
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-08-28 07:52:17 -0500

    chore: Fixed styling and added placeholder

--- app/components/viral/select2_component.rb
+++ app/components/viral/select2_component.rb
@@ -3,14 +3,15 @@
 module Viral
   # Search component for rendering a searchable dropdown
   class Select2Component < Viral::Component
-    attr_reader :form, :name, :required
+    attr_reader :form, :name, :placeholder, :required
 
     renders_many :options, Viral::Select2OptionComponent
     renders_one  :empty_state
 
-    def initialize(form:, name:, required: true)
+    def initialize(form:, name:, placeholder: '', required: true)
       @form = form
       @name = name
+      @placeholder = placeholder
       @required = required
     end
   end
