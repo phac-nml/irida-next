b7d7a3a30142ce6666d985da8a820ae71c2082a5
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-08-30 09:05:08 -0500

    chore: update graphql policy to disallow expired tokens

--- test/graphql/projects_query_test.rb
+++ test/graphql/projects_query_test.rb
@@ -78,16 +78,16 @@ class ProjectsQueryTest < ActiveSupport::TestCase
     assert_equal projects_count, data['totalCount']
   end
 
-  test 'projects query should work but with errors due to expired token for uploader access level' do
+  test 'projects query should throw authorization error due to expired token for uploader access level' do
     user = users(:user_bot_account0)
     token = personal_access_tokens(:user_bot_account0_expired_pat)
 
     result = IridaSchema.execute(PROJECTS_QUERY, context: { current_user: user, token: },
                                                  variables: { first: 20 })
 
-    assert_not_nil result['errors'], 'should work and have errors.'
+    assert_not_nil result['errors'], 'should not work and have authorization errors.'
     error_message = result['errors'][0]['message']
-    assert_equal 'An object of type Project was hidden due to permissions',
+    assert_equal 'You are not authorized to perform this action',
                  error_message
   end
 
