aa59e7e9f562ee8a9b0397e8cf66789c0755dd29
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-09-06 11:56:41 -0500

    fix filters that expect arrays

--- app/graphql/types/sample_filter_type.rb
+++ app/graphql/types/sample_filter_type.rb
@@ -4,9 +4,9 @@ module Types
   # Sample Filter Type
   class SampleFilterType < BaseInputObject # rubocop:disable GraphQL/ObjectDescription
     Sample.ransackable_attributes.each do |attr|
-      Ransack.predicates.keys.map do |predicate, value|
-        value_type = value&.wants_array ? [String] : String
-        argument :"#{attr}_#{predicate}".to_sym,
+      Ransack.predicates.keys.map do |key|
+        value_type = Ransack.predicates[key].wants_array ? [String] : String
+        argument :"#{attr}_#{key}".to_sym,
                  value_type,
                  required: false,
                  camelize: false
