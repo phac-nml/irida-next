2dbb8a0d05aac28de55004575b38fdf9a597d751
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-09-09 09:37:42 -0500

    chore: add in tests for filtering and ordering project samples, and fixed scope_items to preserve ordering

--- test/graphql/project_samples_query_test.rb
+++ test/graphql/project_samples_query_test.rb
@@ -4,12 +4,12 @@ require 'test_helper'
 
 class ProjectSamplesQueryTest < ActiveSupport::TestCase
   PROJECT_QUERY = <<~GRAPHQL
-    query($projectPath: ID!) {
+    query($projectPath: ID!, $sampleFilter: SampleFilter, $sampleOrderBy: SampleOrder) {
       project(fullPath: $projectPath) {
         name
         path
         id
-        samples(first:10) {
+        samples(first:10, filter: $sampleFilter, orderBy: $sampleOrderBy) {
           nodes {
             id
             name
@@ -44,9 +44,9 @@ class ProjectSamplesQueryTest < ActiveSupport::TestCase
 
     # verify fetched sample data matches data on project
     project.samples.each_with_index do |sample, index|
-      assert_equal data['samples']['nodes'][index]['id'], sample.to_global_id.to_s
-      assert_equal data['samples']['nodes'][index]['name'], sample.name
-      assert_equal data['samples']['nodes'][index]['project']['id'], project.to_global_id.to_s
+      assert_equal sample.name, data['samples']['nodes'][index]['name']
+      assert_equal sample.to_global_id.to_s, data['samples']['nodes'][index]['id']
+      assert_equal project.to_global_id.to_s, data['samples']['nodes'][index]['project']['id']
     end
   end
 
@@ -70,9 +70,9 @@ class ProjectSamplesQueryTest < ActiveSupport::TestCase
 
     # verify fetched sample data matches data on project
     project.samples.each_with_index do |sample, index|
-      assert_equal data['samples']['nodes'][index]['id'], sample.to_global_id.to_s
-      assert_equal data['samples']['nodes'][index]['name'], sample.name
-      assert_equal data['samples']['nodes'][index]['project']['id'], project.to_global_id.to_s
+      assert_equal sample.name, data['samples']['nodes'][index]['name']
+      assert_equal sample.to_global_id.to_s, data['samples']['nodes'][index]['id']
+      assert_equal project.to_global_id.to_s, data['samples']['nodes'][index]['project']['id']
     end
   end
 
@@ -91,5 +91,55 @@ class ProjectSamplesQueryTest < ActiveSupport::TestCase
     error_message = result['errors'][0]['message']
 
     assert_equal I18n.t('action_policy.policy.project.read?', name: project.name), error_message
+  end
+
+  test 'project with sample query should work with filter' do
+    project = projects(:project1)
+
+    result = IridaSchema.execute(PROJECT_QUERY, context: { current_user: @user },
+                                                variables: { projectPath: project.full_path,
+                                                             sampleFilter: { name_cont: "Sample 2"} })
+
+    assert_nil result['errors'], 'should work and have no errors.'
+
+    data = result['data']['project']
+
+    assert_not_empty data, 'project type should work'
+    assert_equal project.name, data['name']
+
+    assert_equal project.to_global_id.to_s, data['id'], 'id should be GlobalID'
+    assert_equal 2, data['samples']['nodes'].count
+
+    # verify fetched sample data only includes ones with `Sample 2` in the name
+    project.samples.where('name ILIKE \'%Sample 2%\'').each_with_index do |sample, index|
+      assert_equal sample.name, data['samples']['nodes'][index]['name']
+      assert_equal sample.to_global_id.to_s, data['samples']['nodes'][index]['id']
+      assert_equal project.to_global_id.to_s, data['samples']['nodes'][index]['project']['id']
+    end
+  end
+
+  test 'project with sample query should work with order by' do
+    project = projects(:project1)
+
+    result = IridaSchema.execute(PROJECT_QUERY, context: { current_user: @user },
+                                                variables: { projectPath: project.full_path,
+                                                             sampleOrderBy: { field: 'created_at', direction: 'asc'} })
+
+    assert_nil result['errors'], 'should work and have no errors.'
+
+    data = result['data']['project']
+
+    assert_not_empty data, 'project type should work'
+    assert_equal project.name, data['name']
+
+    assert_equal project.to_global_id.to_s, data['id'], 'id should be GlobalID'
+    assert_equal project.samples.count, data['samples']['nodes'].count
+
+    # verify fetched sample data matches data on project
+    project.samples.order(created_at: :asc).each_with_index do |sample, index|
+      assert_equal sample.name, data['samples']['nodes'][index]['name']
+      assert_equal sample.to_global_id.to_s, data['samples']['nodes'][index]['id']
+      assert_equal project.to_global_id.to_s, data['samples']['nodes'][index]['project']['id']
+    end
   end
 end
