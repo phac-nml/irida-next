9f0a2a04147cf3e257c975fceaa6355918d06ab4
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-09-10 10:59:44 -0500

    first cut

--- app/models/project.rb
+++ app/models/project.rb
@@ -33,7 +33,7 @@ class Project < ApplicationRecord
   end
 
   def self.ransackable_attributes(_auth_object = nil)
-    %w[id created_at updated_at]
+    %w[id puid name created_at updated_at]
   end
 
   def self.ransackable_associations(_auth_object = nil)
