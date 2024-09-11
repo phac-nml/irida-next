6a47e048399f7821455682eb0156e895003c9eaf
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-09-11 15:08:09 -0500

    making the limit component easier to read

--- app/components/viral/pagy/limit_component.rb
+++ app/components/viral/pagy/limit_component.rb
@@ -4,17 +4,9 @@ module Viral
   module Pagy
     # Pagy component for the limit
     class LimitComponent < Viral::Component
-      def initialize(pagy, item:, **system_arguments)
+      def initialize(pagy, item:)
         @pagy = pagy
         @item = item
-        @system_arguments = system_arguments
-      end
-
-      def system_arguments
-        { tag: 'div' }.deep_merge(@system_arguments).tap do |args|
-          args[:id] = 'limit-component'
-          args[:classes] = class_names('inline-flex items-center mt-4 space-x-2 text-slate-500 dark:text-slate-400')
-        end
       end
     end
   end
