1b06cd311a623ef4e09486877d70b3bcb394f4bd
Chris Huynh <chris.huynh333@gmail.com>
2024-09-16 14:59:46 -0500

    change limit component to reconstruct url

--- app/components/viral/pagy/limit_component.rb
+++ app/components/viral/pagy/limit_component.rb
@@ -8,6 +8,22 @@ module Viral
         @pagy = pagy
         @item = item
       end
+
+      def current_url_with_limit(limit)
+        current_url = request.original_url
+        if current_url.include? '?'
+          split_url = current_url.split('?')
+          base_url = split_url[0]
+          get_params = split_url[1].split('&')
+          reconstructed_params = "?limit=#{limit}"
+          get_params.each do |param|
+            reconstructed_params << "&#{param}" unless param.include? 'limit='
+          end
+          "#{base_url}#{reconstructed_params}"
+        else
+          "#{current_url}?limit=#{limit}"
+        end
+      end
     end
   end
 end
