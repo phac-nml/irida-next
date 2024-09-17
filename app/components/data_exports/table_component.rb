c0b8653dce165d9ae67126350f96f19e8bfee1c3
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-09-17 13:37:07 -0500

    starting to create the data export table view component

---
+++ app/components/data_exports/table_component.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'ransack/helpers/form_helper'
+
+module DataExports
+  # Component for rendering a table of Data Exports
+  class TableComponent < Component
+    include Ransack::Helpers::FormHelper
+
+    # rubocop:disable Naming/MethodParameterName
+    def initialize(
+      data_exports,
+      pagy,
+      q,
+      row_actions: {},
+      **system_arguments
+    )
+      @data_exports = data_exports
+      @pagy = pagy
+      @q = q
+      @row_actions = row_actions
+      @system_arguments = system_arguments
+
+      @columns = columns
+    end
+    # rubocop:enable Naming/MethodParameterName
+
+    def system_arguments
+      { tag: 'div' }.deep_merge(@system_arguments).tap do |args|
+        args[:id] = 'data-exports-table'
+        # args[:classes] = class_names(args[:classes], 'overflow-auto scrollbar')
+      end
+    end
+
+    def wrapper_arguments
+      {
+        tag: 'div',
+        classes: class_names('table-container relative overflow-x-auto" data-turbo-temporary')
+        # classes: class_names('table-container flex flex-col shrink min-h-0')
+      }
+    end
+
+    def row_arguments(data_export)
+      { tag: 'tr' }.tap do |args|
+        args[:classes] = class_names('text-sm', 'font-normal', 'text-slate-500', 'dark:text-slate-400')
+        # args[:classes] = class_names('bg-white', 'border-b', 'dark:bg-slate-800', 'dark:border-slate-700')
+        args[:id] = dom_id(data_export)
+        # args[:id] = data_export.id
+      end
+    end
+
+    def render_cell(**arguments, &)
+      render(Viral::BaseComponent.new(**arguments), &)
+    end
+
+    private
+
+    def columns
+      %i[id name export_type status created_at expires_at]
+    end
+  end
+end
