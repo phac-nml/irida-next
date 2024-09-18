f4e24933f976d4c55909b4e1b709ea8a0d171953
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-09-18 08:14:51 -0500

    chore: Oops wrong value

--- test/components/puid_component_test.rb
+++ test/components/puid_component_test.rb
@@ -5,7 +5,7 @@ require 'application_system_test_case'
 class PuidComponentTest < ApplicationSystemTestCase
   def test_default
     visit('/rails/view_components/puid_component/default')
-    assert_text 'Value to copy'
+    assert_text '1234567890'
     find('button[data-clipboard-target="button"]').click
     assert_text I18n.t('components.clipboard.copied')
   end
