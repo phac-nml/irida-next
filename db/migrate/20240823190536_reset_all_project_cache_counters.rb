5db5feb7c94236370573748d640b88e7b9f44ce3
Meghan Chua <17057809+malchua@users.noreply.github.com>
2024-09-19 17:11:25 -0500

    Clarify a comment on a migration

--- db/migrate/20240823190536_reset_all_project_cache_counters.rb
+++ db/migrate/20240823190536_reset_all_project_cache_counters.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Migration to reset samples_count in all projects
+# Migration to reset samples_count to the actual count of project samples, for each project
 class ResetAllProjectCacheCounters < ActiveRecord::Migration[7.2]
   def up
     Project.all.each do |project|
