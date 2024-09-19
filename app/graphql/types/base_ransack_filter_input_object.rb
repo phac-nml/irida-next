47052b6ec3a64305ff1230e07d9480f018049f82
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-09-19 13:31:31 -0500

    chore: exclude jcont_all, jcont_any, jcont_key_all, jcont_key_any

--- app/graphql/types/base_ransack_filter_input_object.rb
+++ app/graphql/types/base_ransack_filter_input_object.rb
@@ -4,7 +4,7 @@ module Types
   # Bsse Ransack Filter Input Object
   class BaseRansackFilterInputObject < BaseInputObject
     DEFAULT_EXCLUDED_ATTRIBUTES = %w[id metadata deleted_at].freeze
-    JSONB_PREDICATE_KEYS = %w[jcont jcont_key].freeze
+    JSONB_PREDICATE_KEYS = %w[jcont jcont_all jcont_any jcont_key jcont_key_all jcont_key_any].freeze
 
     def self.default_predicate_keys
       Ransack.predicates.keys.excluding(JSONB_PREDICATE_KEYS)
