ff4d571482a48ba68f0d6a852b8de7ea5354f3ce
Meghan Chua <17057809+malchua@users.noreply.github.com>
2024-09-19 13:46:33 -0500

    Remove group sample counts

--- app/components/namespace_tree/row/without_children_component.rb
+++ app/components/namespace_tree/row/without_children_component.rb
@@ -8,18 +8,7 @@ module NamespaceTree
         @namespace = namespace
         @path = path
         @path_args = path_args
-        @sample_count = total_samples
-      end
-
-      def total_samples
-        case @namespace.type
-        when 'Group'
-          @namespace.project_namespaces.sum { |project_namespace| project_namespace.project.samples.size }
-        when 'Project'
-          @namespace.project.samples.size
-        when 'User'
-          0
-        end
+        @sample_count = @namespace.type == 'Project' ? @namespace.project.samples.size : 0
       end
     end
   end
