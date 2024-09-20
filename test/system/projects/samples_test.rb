1e554c2e1e03facf96ac10da1cacd198f78387b7
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-09-20 11:08:31 -0500

    chore: Fixed broken test

--- test/system/projects/samples_test.rb
+++ test/system/projects/samples_test.rb
@@ -497,6 +497,7 @@ module Projects
       sign_in users(:jean_doe)
       namespace = namespaces_user_namespaces(:john_doe_namespace)
       project = projects(:john_doe_project2)
+      Project.reset_counters(project.id, :samples_count)
       visit namespace_project_samples_url(namespace, project)
       within '#samples-table table tbody' do
         all('input[type=checkbox]').each { |checkbox| checkbox.click unless checkbox.checked? }
@@ -2432,12 +2433,9 @@ module Projects
       end
       assert_text I18n.t('projects.samples.deletions.destroy_multiple.success')
 
-      within '.table-container' do
-        assert_no_selector 'tr'
-        assert_no_text @sample1.name
-        assert_no_text @sample2.name
-        assert_no_text @sample3.name
+      within 'div[role="alert"]' do
         assert_text I18n.t('projects.samples.index.no_samples')
+        assert_text I18n.t('projects.samples.index.no_associated_samples')
       end
     end
 
