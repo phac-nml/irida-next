9fcb2dd5d05918360b66f81e08b784db304506c2
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-09-20 07:27:45 -0500

    chore: Added previews and testing

---
+++ test/components/viral/pagy_full_component_preview_test.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class PagyFullComponentPreviewTest < ApplicationSystemTestCase
+  test 'renders default' do
+    visit('/rails/view_components/viral_pagy_full_component/default')
+
+    assert_selector 'nav.pagy.nav'
+    assert_selector 'li button[disabled]', text: 'Previous'
+    assert_selector 'li > a', text: 'Next'
+    assert_selector 'li button[disabled]', text: '1'
+    assert_selector 'li > a:not([data-aria-disabled="true"])', count: '5'
+  end
+
+  test 'renders empty state' do
+    visit('/rails/view_components/viral_pagy_full_component/empty_state')
+
+    assert_selector 'h1', text: I18n.t('components.viral.pagy.empty_state.title')
+    assert_selector 'p', text: I18n.t('components.viral.pagy.empty_state.description')
+    assert_no_selector 'nav.pagy.nav'
+  end
+end
