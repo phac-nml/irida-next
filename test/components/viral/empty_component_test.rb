5750bcb6c237938031985f14d2c9aa2e25341409
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-09-20 10:20:33 -0500

    chore: Fixed empty component test

--- test/components/viral/empty_component_test.rb
+++ test/components/viral/empty_component_test.rb
@@ -7,7 +7,7 @@ module Viral
     test 'default' do
       render_preview(:default)
       assert_selector 'span.Viral-Icon svg'
-      assert_selector 'p', text: I18n.t(:'groups.show.shared_namespaces.no_shared.title')
+      assert_selector 'h1', text: I18n.t(:'groups.show.shared_namespaces.no_shared.title')
       assert_selector 'p', text: I18n.t(:'groups.show.shared_namespaces.no_shared.description')
     end
   end
