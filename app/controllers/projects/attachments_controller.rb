e48c34fb844b5eee4e9a6959ce68da0b122b0de5
Chris Huynh <82407232+ChrisHuynh333@users.noreply.github.com>
2024-09-20 07:37:50 -0500

    Project Attachments: Destroy (#762)
    * begin upload functionality
    
    * chore: fix sortlink for ransack_alias
    
    * start search bar for files
    
    * fix table, fix filter
    
    * add fr translations
    
    * remove search for this iteration
    
    * cleanup table component of unnecessary params
    
    * revert ransack cahnges, add separate sorting for filename and bytesize
    
    * run normalize
    
    * cleanup
    
    * change back file name
    
    * remove accidental code
    
    * start project attachments destroy
    
    * add paired end table listing
    
    * add tests, add fr translations, add z index for flash msgs
    
    * add rubocop disable
    
    * fix new destroy params
    
    * remove duplicate authorize call
    
    * run normalize
    
    * remove duplicate authorization
    
    * replace premade class with actual tailwind classes
    
    * add different description
    
    * finish deletion modal change
    
    ---------
    
    Co-authored-by: Eric Enns <492127+ericenns@users.noreply.github.com>

--- app/controllers/projects/attachments_controller.rb
+++ app/controllers/projects/attachments_controller.rb
@@ -5,8 +5,12 @@ module Projects
   class AttachmentsController < Projects::ApplicationController
     include Metadata
     before_action :current_page
+    before_action :new_destroy_params, only: %i[new_destroy]
+    before_action :attachment, only: %i[destroy]
 
     def index
+      authorize! @project, to: :view_attachments?
+
       @q = @project.namespace.attachments.ransack(params[:q])
       set_default_sort
       @pagy, @attachments = pagy_with_metadata_sort(@q.result)
@@ -24,8 +28,6 @@ module Projects
     end
 
     def create
-      authorize! @project, to: :update_sample?
-
       @attachments = ::Attachments::CreateService.new(current_user, @project.namespace, attachment_params).execute
 
       status = if !@attachments.count.positive?
@@ -44,6 +46,36 @@ module Projects
       end
     end
 
+    def new_destroy
+      authorize! @project, to: :destroy_attachment?
+      render turbo_stream: turbo_stream.update('attachment_modal',
+                                               partial: 'delete_attachment_modal',
+                                               locals: {
+                                                 open: true
+                                               }), status: :ok
+    end
+
+    def destroy # rubocop:disable Metrics/MethodLength
+      @destroyed_attachments = ::Attachments::DestroyService.new(@project.namespace, @attachment, current_user).execute
+
+      respond_to do |format|
+        if @destroyed_attachments.count.positive?
+          status = destroy_status(@attachment, @destroyed_attachments.length)
+          format.turbo_stream do
+            render status:, locals: { destroyed_attachments: @destroyed_attachments }
+          end
+        else
+          format.turbo_stream do
+            render status: :unprocessable_entity,
+                   locals: { message: t('.error',
+                                        filename: @attachment.file.filename,
+                                        errors: @attachment.errors.full_messages.first),
+                             destroyed_attachments: nil }
+          end
+        end
+      end
+    end
+
     private
 
     def current_page
@@ -57,6 +89,20 @@ module Projects
           name: t(:'projects.sidebar.files'),
           path: namespace_project_attachments_path(@project.parent, @project)
         }]
+    end
+
+    def new_destroy_params
+      @attachment = Attachment.find_by(id: params[:attachment_id])
+    end
+
+    def attachment
+      @attachment = Attachment.find_by(id: params[:id])
+    end
+
+    def destroy_status(attachment, count)
+      return count == 2 ? :ok : :multi_status if attachment.associated_attachment
+
+      count == 1 ? :ok : :unprocessable_entity
     end
 
     def layout_fixed
