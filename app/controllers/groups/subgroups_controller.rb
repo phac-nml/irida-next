4cd92e9c4bd30e6bbd57b7b38ae678149a425dde
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-09-25 14:29:03 -0500

    chore: Working on auto-submit

--- app/controllers/groups/subgroups_controller.rb
+++ app/controllers/groups/subgroups_controller.rb
@@ -7,13 +7,8 @@ module Groups
 
     def index
       authorize! @group, to: :read?
-
-      @q = namespace_children.ransack(params[:q])
-      set_default_sort
-      @pagy, @namespaces = pagy(@q.result.include_route)
-
       respond_to do |format|
-        format.html do
+        format.html { redirect_to group_path(@group) }
         format.turbo_stream do
           if params.key? :parent_id
             render_subgroup
@@ -25,10 +20,6 @@ module Groups
     end
 
     private
-
-    def set_default_sort
-      @q.sorts = 'created_at desc' if @q.sorts.empty?
-    end
 
     def render_subgroup
       @group = Group.find(params[:parent_id])
