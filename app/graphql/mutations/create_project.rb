af69452b089d22a349d28e03d91e20e5476216d7
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-09-26 16:17:16 -0500

    first cut, name, desc, path working on user namespace

---
+++ app/graphql/mutations/create_project.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+module Mutations
+  # Base Mutation
+  class CreateProject < BaseMutation
+    null true
+    description 'Create a new project..'
+    argument :description, String, required: false, description: 'The description to give the project.'
+    argument :group_id, ID,
+             required: false,
+             description: 'The Node ID of a group to create the project in. For example, `gid://irida/Group/a84cd757-dedb-4c64-8b01-097020163077`.' # rubocop:disable Layout/LineLength
+    argument :group_puid, ID, # rubocop:disable GraphQL/ExtractInputType
+             required: false,
+             description: 'Persistent Unique Identifier of a group to create the project in. For example, `INXT_GRP_AAAAAAAAAA`.' # rubocop:disable Layout/LineLength
+    argument :name, String, required: true, description: 'The name to give the project.' # rubocop:disable GraphQL/ExtractInputType
+    argument :path, String, required: false, description: 'A custom path for the project.' # rubocop:disable GraphQL/ExtractInputType
+
+    field :errors, [Types::UserErrorType], null: false, description: 'A list of errors that prevented the mutation.'
+    field :project, Types::ProjectType, description: 'The newly created project.'
+
+    def resolve(args)
+      namespace = get_namespace(args)
+      args[:path] = if args[:path]
+                      args[:path].to_s.parameterize(separator: '_')
+                    else
+                      args[:name].to_s.parameterize(separator: '_')
+                    end
+      create_project(namespace, args)
+    end
+
+    def ready?(**_args)
+      authorize!(to: :mutate?, with: GraphqlPolicy, context: { user: context[:current_user], token: context[:token] })
+    end
+
+    private
+
+    def get_namespace(args)
+      # Only search for a group if an id/puid was provided
+      if args[:group_id] || args[:group_puid]
+        group = get_group_from_id_or_puid_args(args)
+
+        if group.nil? || !group.persisted?
+          user_errors = [{ path: ['group'], message: 'Group not found by provided ID or PUID' }]
+          return { project: nil, errors: user_errors }
+        end
+
+        group.namespace
+      else
+        current_user.namespace
+      end
+    end
+
+    def create_project(namespace, args) # rubocop:disable Metrics/MethodLength
+      namespace_attributes = {
+        name: args[:name],
+        path: args[:path],
+        description: args[:description],
+        parent_id: namespace.id
+      }
+
+      project = Projects::CreateService.new(
+        current_user, { namespace_attributes: }
+      ).execute
+
+      if project.persisted?
+        {
+          project:,
+          errors: []
+        }
+      else
+        user_errors = project.errors.map do |error|
+          {
+            path: ['project', error.attribute.to_s.camelize(:lower)],
+            message: error.message
+          }
+        end
+        {
+          project: nil,
+          errors: user_errors
+        }
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -9,6 +9,7 @@ module Types
     field :attach_files_to_project, mutation: Mutations::AttachFilesToProject # rubocop:disable GraphQL/FieldDescription
     field :attach_files_to_sample, mutation: Mutations::AttachFilesToSample # rubocop:disable GraphQL/FieldDescription, GraphQL/ExtractType
     field :create_direct_upload, mutation: Mutations::CreateDirectUpload # rubocop:disable GraphQL/FieldDescription
+    field :create_project, mutation: Mutations::CreateProject # rubocop:disable GraphQL/FieldDescription
     field :create_sample, mutation: Mutations::CreateSample # rubocop:disable GraphQL/FieldDescription,GraphQL/ExtractType
     field :update_sample_metadata, mutation: Mutations::UpdateSampleMetadata # rubocop:disable GraphQL/FieldDescription
   end
