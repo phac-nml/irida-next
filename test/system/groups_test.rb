b01d5691d85b00b7df90bcf32aee49d27af15de9
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-09-27 09:44:05 -0500

    chore: Fixed test

--- test/system/groups_test.rb
+++ test/system/groups_test.rb
@@ -132,11 +132,9 @@ class GroupsTest < ApplicationSystemTestCase
     click_on I18n.t('groups.sidebar.settings')
     click_link I18n.t('groups.sidebar.general')
 
-    within 'form[action="/group-1"]' do
-      fill_in I18n.t('activerecord.attributes.group.name'), with: group_name
-      fill_in I18n.t('activerecord.attributes.group.description'), with: group_description
-      click_on I18n.t('groups.edit.details.submit')
-    end
+    fill_in I18n.t('activerecord.attributes.group.name'), with: group_name
+    fill_in I18n.t('activerecord.attributes.group.description'), with: group_description
+    click_on I18n.t('groups.edit.details.submit')
 
     assert_text I18n.t('groups.update.success', group_name:)
 
