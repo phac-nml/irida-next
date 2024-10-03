b4fc6123b138bb3542792a54f3cbe77bac5800ac
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-10-02 20:34:20 -0500

    chore: Projects dashboard page search

--- app/javascript/controllers/filters_controller.js
+++ app/javascript/controllers/filters_controller.js
@@ -5,16 +5,7 @@ import _ from "lodash";
 export default class extends Controller {
   static outlets = ["selection"];
 
-  initialize() {
-    this.submit = this.submit.bind(this);
-  }
-
-  connect() {
-    this.submit = _.debounce(this.submit, 500);
-  }
-
   submit() {
-    this.element.requestSubmit();
     if (this.hasSelectionOutlet) {
       this.selectionOutlet.clear();
     }
