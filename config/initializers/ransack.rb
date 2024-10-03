3aef64c1406b96bac881dfec7b003d52ea07d1b1
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-10-03 09:01:37 -0500

    feat: update metadata index on attachments and samples to be on lowercase so that we can do case-insensitive searching

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -5,10 +5,18 @@ module Arel
     def contains_key(other)
       Arel::Nodes::InfixOperation.new(:'?', self, Arel::Nodes::Quoted.new(other))
     end
+
+    # contains but case insensitive
+    def contains_ci(other)
+      Arel::Nodes::Contains.new(
+        Arel::Nodes::SqlLiteral.new("lower(\"#{relation.name}\".\"#{name}\"::text)::jsonb"),
+        Arel::Nodes.build_quoted(other, self)
+      )
+    end
   end
 end
 
 Ransack.configure do |config|
-  config.add_predicate 'jcont', arel_predicate: 'contains', formatter: proc { |v| JSON.parse(v.to_s) }
-  config.add_predicate 'jcont_key', arel_predicate: 'contains_key'
+  config.add_predicate 'jcont', arel_predicate: 'contains_ci', formatter: proc { |v| JSON.parse(v.to_s.downcase) }
+  config.add_predicate 'jcont_key', arel_predicate: 'contains_key', formatter: proc { |v| v.to_s.downcase }
 end
---
+++ db/migrate/20241003125314_update_metadata_indexes.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Update metadata index for attachments and samples to be on lower case
+class UpdateMetadataIndexes < ActiveRecord::Migration[7.2]
+  def change # rubocop:disable Metrics/MethodLength
+    reversible do |dir|
+      dir.up do
+        execute <<~SQL.squish
+          DROP INDEX index_attachments_on_metadata;
+          CREATE INDEX index_attachments_on_metadata ON attachments USING GIN ((LOWER(metadata::text)::jsonb));
+
+          DROP INDEX index_samples_on_metadata;
+          CREATE INDEX index_samples_on_metadata ON samples USING GIN ((LOWER(metadata::text)::jsonb));
+        SQL
+      end
+
+      dir.down do
+        execute <<~SQL.squish
+          DROP INDEX index_attachments_on_metadata;
+          DROP INDEX index_samples_on_metadata;
+        SQL
+        add_index :attachments, :metadata, using: :gin
+        add_index :samples, :metadata, using: :gin
+      end
+    end
+  end
+end
