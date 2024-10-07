e141d97af75ee3e803c6c3944bca2acc44a5cf32
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-10-07 14:47:10 -0500

    refactor: Conditional rendering for ransack sort hidden fields
    This was causing issues with the lookbook preview for the search component.

--- app/components/ransack/hidden_sort_field_component.rb
+++ app/components/ransack/hidden_sort_field_component.rb
@@ -8,5 +8,9 @@ module Ransack
     def initialize(ransack_obj)
       @ransack_obj = ransack_obj
     end
+
+    def render?
+      ransack_obj.present? && !ransack_obj.sorts.empty?
+    end
   end
 end
