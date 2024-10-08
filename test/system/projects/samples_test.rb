957531a2bdff7efaf7312d7cfc54013d0417b55a
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-10-08 11:26:25 -0500

    attempting to fix flaky test

--- test/system/projects/samples_test.rb
+++ test/system/projects/samples_test.rb
@@ -520,6 +520,19 @@ module Projects
       visit namespace_project_samples_url(namespace1, project2)
       assert_text strip_tags(I18n.t(:'viral.pagy.limit_component.summary', from: 1, to: 20, count: 220,
                                                                            locale: @user.locale))
+<<<<<<< HEAD
+=======
+
+      click_button I18n.t(:'projects.samples.index.select_all_button')
+
+      within 'tbody' do
+        assert_selector 'input[name="sample_ids[]"]:checked', count: 20, wait: 1
+      end
+      within 'tfoot' do
+        assert_text 'Samples: 220'
+        assert_selector 'strong[data-selection-target="selected"]', text: '220'
+      end
+>>>>>>> a8eb74e2a (attempting to fix flaky test)
     end
 
     test 'empty state of transfer sample project selection' do
