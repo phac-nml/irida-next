01dd4efbc1152fa4e03bfdd04a9340d0fd83da09
Chris Huynh <chris.huynh333@gmail.com>
2024-10-15 14:40:04 -0500

    remove unused target

--- app/javascript/controllers/new_analysis_export_controller.js
+++ app/javascript/controllers/new_analysis_export_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["completedList", "error", "submitButton"];
+  static targets = ["error", "submitButton"];
 
   errorTargetConnected() {
     this.disableSubmit()
