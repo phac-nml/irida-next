0c008439a8f80505530048ca3b9185c8bb465fc4
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-10-18 15:16:47 -0500

    trying to fix tests

--- app/models/automated_workflow_execution.rb
+++ app/models/automated_workflow_execution.rb
@@ -2,6 +2,8 @@
 
 # entity class for Automated Workflow Executions
 class AutomatedWorkflowExecution < ApplicationRecord
+  attr_accessor :disabled
+
   METADATA_JSON_SCHEMA = Rails.root.join('config/schemas/workflow_execution_metadata.json')
 
   has_logidze
