1463cd6ab680f70f0ad3b52cb10ffda147f826ff
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-10-18 13:48:41 -0500

    attempting to enable/disable automated workflows after pipeline initialization

--- config/initializers/pipelines.rb
+++ config/initializers/pipelines.rb
@@ -5,3 +5,18 @@ require 'irida/pipelines'
 Rails.application.config.to_prepare do
   Irida::Pipelines.instance = Irida::Pipelines.new if Irida::Pipelines.instance.nil?
 end
+
+Rails.application.config.after_initialize do
+  Irida::Pipelines.instance.available_pipelines.each_value do |pipeline|
+    next unless pipeline.automatable
+
+    automated_workflow = AutomatedWorkflowExecution.find_by(
+      "metadata ->> 'workflow_name' = ? and metadata ->> 'workflow_version' = ?", pipeline.name, pipeline.version
+    )
+
+    next unless automated_workflow
+
+    automated_workflow.disabled = pipeline.executable ? false : true
+    automated_workflow.save
+  end
+end
