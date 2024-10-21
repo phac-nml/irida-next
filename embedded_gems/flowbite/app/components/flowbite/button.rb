45d89d2701b2478da8ab29d1e179ec14c7ca5cc1
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-10-21 13:55:34 -0500

    style: :lipstick: Ability to change button size, with preivews

--- embedded_gems/flowbite/app/components/flowbite/button.rb
+++ embedded_gems/flowbite/app/components/flowbite/button.rb
@@ -4,21 +4,20 @@ module Flowbite
   class Button < Flowbite::Component
     DEFAULT_SCHEME = :default
     SCHEME_MAPPINGS = {
-      DEFAULT_SCHEME => '',
-      :primary => 'Button--primary',
-      :secondary => 'Button--secondary',
-      :default => 'text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700',
-      :danger => 'Button--danger',
-      :invisible => 'Button--invisible',
-      :link => 'Button--link'
+      primary: 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800',
+      secondary: 'Button--secondary',
+      default: 'text-gray-900 focus:outline-none bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700',
+      danger: 'Button--danger',
+      invisible: 'Button--invisible',
+      link: 'Button--link'
     }.freeze
     SCHEME_OPTIONS = SCHEME_MAPPINGS.keys
 
-    DEFAULT_SIZE = :base
+    DEFAULT_SIZE = :default
     SIZE_MAPPINGS = {
       extra_small: 'px-3 py-2 text-xs',
       small: 'px-3 py-2 text-sm',
-      base: 'px-5 py-2.5 text-sm',
+      default: 'px-5 py-2.5 text-sm',
       large: 'px-5 py-3 text-base',
       extra_large: 'px-6 py-3.5 text-base'
     }.freeze
@@ -47,7 +46,8 @@ module Flowbite
       @system_arguments[:classes] = class_names(
         system_arguments[:classes],
         SCHEME_MAPPINGS[fetch_or_fallback(SCHEME_OPTIONS, scheme, DEFAULT_SCHEME)],
-        SIZE_MAPPINGS[fetch_or_fallback(SIZE_OPTIONS, size, DEFAULT_SIZE)]
+        SIZE_MAPPINGS[fetch_or_fallback(SIZE_OPTIONS, size, DEFAULT_SIZE)],
+        'rounded-lg font-medium'
       )
     end
 
---
+++ test/components/previews/flowbite_button_preview.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class FlowbiteButtonPreview < ViewComponent::Preview
+  def default; end
+  def sizes; end
+end
