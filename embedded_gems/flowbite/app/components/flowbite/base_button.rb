fbc0b6f9d7e84734e5e4f927225efbb174efce23
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-10-22 07:50:33 -0500

    refactor: :recycle: Work on cleaning up button code and comments

--- embedded_gems/flowbite/app/components/flowbite/base_button.rb
+++ embedded_gems/flowbite/app/components/flowbite/base_button.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 module Flowbite
+  # File: base_button.rb
+  # Purpose: Defines the BaseButton class for the Flowbite component library
+  #
+  # This file contains the implementation of the BaseButton class, which serves as
+  # a foundation for creating button components in the Flowbite framework. It
+  # provides functionality for customizing button attributes such as tag, type,
+  # appearance, and disabled state.
+  #
+  # The BaseButton class is part of the Flowbite module and inherits from
+  # Flowbite::Component, allowing it to integrate seamlessly with other Flowbite
+  # components.
   class BaseButton < Flowbite::Component
+    # Define constants for default and allowed values
     DEFAULT_TAG = :button
     TAG_OPTIONS = [DEFAULT_TAG, :a].freeze
 
@@ -11,29 +23,32 @@ module Flowbite
     attr_reader :disabled
     alias disabled? disabled
 
-    def initialize(tag: DEFAULT_TAG, type: DEFAULT_TYPE, block: false, disabled: false, inactive: false,
+    def initialize(tag: DEFAULT_TAG, type: DEFAULT_TYPE, disabled: false, inactive: false,
                    **system_arguments)
       @system_arguments = system_arguments
-      @system_arguments[:tag] = fetch_or_fallback(TAG_OPTIONS, tag, DEFAULT_TAG)
 
+      # Set the tag and type
+      @system_arguments[:tag] = fetch_or_fallback(TAG_OPTIONS, tag, DEFAULT_TAG)
       if @system_arguments[:tag] == :button
         @system_arguments[:type] =
           fetch_or_fallback(TYPE_OPTIONS, type, DEFAULT_TYPE)
       end
 
+      # Add CSS classes
       @system_arguments[:classes] = class_names(
         system_arguments[:classes],
-        'btn-block' => block,
-        'Button--inactive' => inactive
+        'cursor-not-allowed' => disabled
       )
 
+      # Handle disabled state
       @disabled = disabled
       return unless @disabled
 
       @system_arguments[:tag] = :button
-      @system_arguments[:disabled] = ''
+      @system_arguments[:disabled] = true
     end
 
+    # Render the button using the BaseComponent
     def call
       render(Flowbite::BaseComponent.new(**@system_arguments)) { content }
     end
