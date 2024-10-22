c7cd604ca96d62fddd075b8a06d8c85db45c937b
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-10-22 09:20:03 -0500

    docs: :bulb: Updated file comments

--- embedded_gems/flowbite/app/components/flowbite/base_button.rb
+++ embedded_gems/flowbite/app/components/flowbite/base_button.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
 module Flowbite
-  # File: base_button.rb
-  # Purpose: Defines the BaseButton class for the Flowbite component library
+  # This file defines the BaseButton class within the Flowbite module.
+  # BaseButton is a foundational component for creating buttons in the Flowbite design system.
   #
-  # This file contains the implementation of the BaseButton class, which serves as
-  # a foundation for creating button components in the Flowbite framework. It
-  # provides functionality for customizing button attributes such as tag, type,
-  # appearance, and disabled state.
-  #
-  # The BaseButton class is part of the Flowbite module and inherits from
-  # Flowbite::Component, allowing it to integrate seamlessly with other Flowbite
-  # components.
   class BaseButton < Flowbite::Component
     # Define constants for default and allowed values
     DEFAULT_TAG = :button
@@ -23,6 +15,11 @@ module Flowbite
     attr_reader :disabled
     alias disabled? disabled
 
+    # Initialize a new BaseButton instance
+    # @param tag [Symbol] The HTML tag to use for the button (:button or :a)
+    # @param type [Symbol] The type attribute for button tags (:button, :submit, or :reset)
+    # @param disabled [Boolean] Whether the button should be disabled
+    # @param system_arguments [Hash] Additional arguments to be passed to the component
     def initialize(tag: DEFAULT_TAG, type: DEFAULT_TYPE, disabled: false, **system_arguments)
       @system_arguments = system_arguments
 
@@ -43,6 +40,7 @@ module Flowbite
     end
 
     # Render the button using the BaseComponent
+    # @return [Flowbite::BaseComponent] The rendered button component
     def call
       render(Flowbite::BaseComponent.new(**@system_arguments)) { content }
     end
