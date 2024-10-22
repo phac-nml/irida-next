b81bec9dcf23059f0814c03c2d0a07f6a0b9555b
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-10-22 11:11:16 -0500

    trying to fix ci tests

--- config/initializers/pipelines.rb
+++ config/initializers/pipelines.rb
@@ -4,17 +4,17 @@ require 'irida/pipelines'
 
 Rails.application.config.to_prepare do
   Irida::Pipelines.instance = Irida::Pipelines.new if Irida::Pipelines.instance.nil?
-end
 
-Rails.configuration.after_initialize do
-  Irida::Pipelines.instance.available_pipelines.each_value do |pipeline|
-    automated_workflows = AutomatedWorkflowExecution.where(
-      "metadata ->> 'workflow_name' = ? and metadata ->> 'workflow_version' = ?", pipeline.name, pipeline.version
-    )
+  Rails.application.config.after_initialize do
+    Irida::Pipelines.instance.available_pipelines.each_value do |pipeline|
+      automated_workflows = AutomatedWorkflowExecution.where(
+        "metadata ->> 'workflow_name' = ? and metadata ->> 'workflow_version' = ?", pipeline.name, pipeline.version
+      )
 
-    automated_workflows.each do |automated_workflow|
-      automated_workflow.disabled = pipeline.executable && pipeline.automatable ? false : true
-      automated_workflow.save
+      automated_workflows.each do |automated_workflow|
+        automated_workflow.disabled = pipeline.executable && pipeline.automatable ? false : true
+        automated_workflow.save
+      end
     end
   end
 end
