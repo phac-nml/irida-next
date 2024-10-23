b9004e959616f1ed94aad1d7a70c1c0b4553de65
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-10-23 13:22:58 -0500

    refactor: :recycle: Made scheme mapping private

--- embedded_gems/flowbite/app/components/flowbite/button.rb
+++ embedded_gems/flowbite/app/components/flowbite/button.rb
@@ -49,20 +49,6 @@ module Flowbite
       )
     end
 
-    def generate_scheme_mapping(scheme)
-      enabled_prefix = enableable? ? 'enabled:' : ''
-      # rubocop:disable Layout/LineLength
-      case scheme
-      when :primary
-        "bg-primary-700 text-white #{enabled_prefix}hover:bg-primary-800 focus:ring-primary-300 dark:focus:ring-primary-600"
-      when :default
-        "text-slate-900 bg-white border border-slate-200 #{enabled_prefix}hover:bg-slate-100 #{enabled_prefix}hover:text-primary-700 focus:ring-4 focus:ring-slate-100 dark:focus:ring-slate-700 dark:bg-slate-800 dark:text-slate-400 dark:border-slate-600 #{enabled_prefix}dark:hover:text-white #{enabled_prefix}dark:hover:bg-slate-700"
-      when :danger
-        "border border-red-100 bg-slate-50 text-red-500 #{enabled_prefix}hover:text-red-50 #{enabled_prefix}dark:hover:text-red-50 #{enabled_prefix}hover:bg-red-800 focus:ring-red-300 dark:border-red-800 dark:bg-slate-900 dark:text-red-500 dark:focus:ring-red-900"
-      end
-      # rubocop:enable Layout/LineLength
-    end
-
     private
 
     # Trims the content by removing leading and trailing whitespace.
@@ -79,6 +65,24 @@ module Flowbite
 
       # strip unsets `html_safe`, so we have to set it back again to guarantee that HTML blocks won't break
       trimmed_content.html_safe # rubocop:disable Rails/OutputSafety
+    end
+
+    # Generates the appropriate CSS classes for the button's color scheme and tag type.
+    #
+    # @param scheme [Symbol] The color scheme of the button (:primary, :default, or :danger).
+    # @return [String] A string of CSS classes corresponding to the specified scheme.
+    def generate_scheme_mapping(scheme)
+      enabled_prefix = enableable? ? 'enabled:' : ''
+      # rubocop:disable Layout/LineLength
+      case scheme
+      when :primary
+        "bg-primary-700 text-white #{enabled_prefix}hover:bg-primary-800 focus:ring-primary-300 dark:focus:ring-primary-600"
+      when :default
+        "text-slate-900 bg-white border border-slate-200 #{enabled_prefix}hover:bg-slate-100 #{enabled_prefix}hover:text-primary-700 focus:ring-4 focus:ring-slate-100 dark:focus:ring-slate-700 dark:bg-slate-800 dark:text-slate-400 dark:border-slate-600 #{enabled_prefix}dark:hover:text-white #{enabled_prefix}dark:hover:bg-slate-700"
+      when :danger
+        "border border-red-100 bg-slate-50 text-red-500 #{enabled_prefix}hover:text-red-50 #{enabled_prefix}dark:hover:text-red-50 #{enabled_prefix}hover:bg-red-800 focus:ring-red-300 dark:border-red-800 dark:bg-slate-900 dark:text-red-500 dark:focus:ring-red-900"
+      end
+      # rubocop:enable Layout/LineLength
     end
 
     def enableable?
