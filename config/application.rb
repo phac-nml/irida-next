2d47f44a4eba1dae4db7cf2d8e00ab8e1932d30d
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-10-23 11:21:58 -0500

    trying to fix ci tests

--- config/application.rb
+++ config/application.rb
@@ -15,19 +15,6 @@ module Irida
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    initializer 'update_automated_pipeline_status', after: :finisher_hook do
-      Irida::Pipelines.instance.available_pipelines.each_value do |pipeline|
-        automated_workflows = AutomatedWorkflowExecution.where(
-          "metadata ->> 'workflow_name' = ? and metadata ->> 'workflow_version' = ?", pipeline.name, pipeline.version
-        )
-
-        automated_workflows.each do |automated_workflow|
-          automated_workflow.disabled = pipeline.executable && pipeline.automatable ? false : true
-          automated_workflow.save
-        end
-      end
-    end
-
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
