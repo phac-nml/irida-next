449ce1ceaf599fb533adc6dcbe9a29eb9e74ead0
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-10-23 09:03:43 -0500

    chore: Reducing number of sizes and cleaning up previews

--- embedded_gems/flowbite/app/lib/flowbite/button_sizes.rb
+++ embedded_gems/flowbite/app/lib/flowbite/button_sizes.rb
@@ -4,15 +4,12 @@ module Flowbite
   # This module contains methods for generating and managing button sizes.
   module ButtonSizes
     # Default size for buttons
-    DEFAULT_SIZE = :default
+    DEFAULT_SIZE = :medium
 
     # A hash of predefined button size mappings
     SIZE_MAPPINGS = {
-      extra_small: 'px-3 py-2 text-xs',
-      small: 'px-3 py-2 text-sm',
-      default: 'px-5 py-2.5 text-sm',
-      large: 'px-5 py-3 text-base',
-      extra_large: 'px-6 py-3.5 text-base'
+      small: 'px-3 py-2 text-xs',
+      medium: 'px-3 py-2 text-sm'
     }.freeze
     SIZE_OPTIONS = SIZE_MAPPINGS.keys
   end
--- test/components/previews/flowbite_button_preview.rb
+++ test/components/previews/flowbite_button_preview.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
 class FlowbiteButtonPreview < ViewComponent::Preview
-  def default_button; end
+  # @param scheme select { choices: [default,primary, blue, alternative, dark, light, green, red, purple, yellow] } "The color scheme of the button."
+  # @param size select { choices: [small, medium] } "The size of the button."
+  # @param disabled toggle "The Boolean disabled attribute, when present, makes the element not mutable, focusable, or even submitted with the form. The user can neither edit nor focus on the control, nor its form control descendants."
+  # @param block toggle "If true, the button will take up the full width of its container."
+  def playground(scheme: :default, size: :medium, disabled: false, block: false)
+    render Flowbite::Button.new(scheme:, size:, disabled:, block:) do
+      'Button'
+    end
+  end
+
+  # @param disabled toggle "The Boolean disabled attribute, when present, makes the element not mutable, focusable, or even submitted with the form. The user can neither edit nor focus on the control, nor its form control descendants."
+  def default(disabled: false)
+    render Flowbite::Button.new(disabled:) do
+      'Button'
+    end
+  end
+
   def button_sizes; end
   def button_sizes_with_icon; end
   def buttons_with_icons; end
