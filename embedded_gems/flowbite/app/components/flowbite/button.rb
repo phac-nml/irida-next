5aec232a504dd3b0bc4093bcf022eb538c018ac5
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-10-23 11:34:30 -0500

    style: :lipstick: Fixed link as a button state

--- embedded_gems/flowbite/app/components/flowbite/button.rb
+++ embedded_gems/flowbite/app/components/flowbite/button.rb
@@ -7,16 +7,20 @@ module Flowbite
   # styling across the application.
   #
   class Button < Flowbite::Component
-    include Flowbite::ButtonSchemes
     include Flowbite::ButtonVisuals
     include Flowbite::ButtonSizes
 
+    SCHEME_OPTIONS = %i[primary default danger].freeze
+    DEFAULT_SCHEME = :default
+    # rubocop:disable Layout/LineLength
+    DEFAULT_CLASSES = 'rounded-lg font-medium focus:outline-none focus:ring-4 focus:z-10 disabled:opacity-70 disabled:cursor-not-allowed'
+    # rubocop:enable Layout/LineLength
+
     # rubocop:disable Metrics/ParameterLists
     def initialize(base_button_class: Flowbite::BaseButton, scheme: DEFAULT_SCHEME, size: DEFAULT_SIZE, block: false,
-                   disabled: false, label_wrap: false, **system_arguments)
+                   disabled: false, **system_arguments)
       @base_button_class = base_button_class
       @scheme = scheme
-      @label_wrap = label_wrap
       @size = size
       @block = block
 
@@ -27,7 +31,7 @@ module Flowbite
 
       @system_arguments[:classes] = class_names(
         system_arguments[:classes],
-        SCHEME_MAPPINGS[fetch_or_fallback(SCHEME_OPTIONS, scheme, DEFAULT_SCHEME)],
+        generate_scheme_mapping(fetch_or_fallback(SCHEME_OPTIONS, scheme, DEFAULT_SCHEME)),
         SIZE_MAPPINGS[fetch_or_fallback(SIZE_OPTIONS, size, DEFAULT_SIZE)],
         DEFAULT_CLASSES,
         'block w-full' => block
@@ -43,6 +47,20 @@ module Flowbite
         @system_arguments[:classes],
         'text-center inline-flex items-center'
       )
+    end
+
+    def generate_scheme_mapping(scheme)
+      enabled_prefix = enableable? ? 'enabled:' : ''
+      # rubocop:disable Layout/LineLength
+      case scheme
+      when :primary
+        "bg-primary-700 text-white #{enabled_prefix}hover:bg-primary-800 focus:ring-primary-300 dark:focus:ring-primary-600"
+      when :default
+        "text-slate-900 bg-white border border-slate-200 #{enabled_prefix}hover:bg-slate-100 #{enabled_prefix}hover:text-primary-700 focus:ring-4 focus:ring-slate-100 dark:focus:ring-slate-700 dark:bg-slate-800 dark:text-slate-400 dark:border-slate-600 #{enabled_prefix}dark:hover:text-white #{enabled_prefix}dark:hover:bg-slate-700"
+      when :danger
+        "border border-red-100 bg-slate-50 text-red-500 #{enabled_prefix}hover:text-red-50 #{enabled_prefix}dark:hover:text-red-50 #{enabled_prefix}hover:bg-red-800 focus:ring-red-300 dark:border-red-800 dark:bg-slate-900 dark:text-red-500 dark:focus:ring-red-900"
+      end
+      # rubocop:enable Layout/LineLength
     end
 
     private
@@ -61,6 +79,10 @@ module Flowbite
 
       # strip unsets `html_safe`, so we have to set it back again to guarantee that HTML blocks won't break
       trimmed_content.html_safe # rubocop:disable Rails/OutputSafety
+    end
+
+    def enableable?
+      @system_arguments[:tag].present? && @system_arguments[:tag] == 'a'
     end
   end
 end
