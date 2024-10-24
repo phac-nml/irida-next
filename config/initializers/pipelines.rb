43c3653b12a294bab573103a16cda83d0fbcedc6
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-10-24 14:00:04 -0500

    trying to fix ci tests

--- config/initializers/pipelines.rb
+++ config/initializers/pipelines.rb
@@ -9,16 +9,14 @@ end
 Rails.application.config.after_initialize do
   if defined?(Rails::Server)
     Irida::Pipelines.instance.available_pipelines.each_value do |pipeline|
-      next unless pipeline.automatable
-
-      automated_workflow = AutomatedWorkflowExecution.find_by(
+      automated_workflows = AutomatedWorkflowExecution.where(
         "metadata ->> 'workflow_name' = ? and metadata ->> 'workflow_version' = ?", pipeline.name, pipeline.version
       )
 
-      next unless automated_workflow
-
-      automated_workflow.disabled = pipeline.executable ? false : true
-      automated_workflow.save
+      automated_workflows.each do |automated_workflow|
+        automated_workflow.disabled = pipeline.executable && pipeline.automatable ? false : true
+        automated_workflow.save
+      end
     end
   end
 end
