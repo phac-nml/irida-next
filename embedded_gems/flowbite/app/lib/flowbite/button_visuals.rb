daa0253a1c3e86bff24a869cc6adf496433c18a0
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-10-24 08:48:12 -0500

    test: :white_check_mark: Added Flowbite Button tests

--- embedded_gems/flowbite/app/lib/flowbite/button_visuals.rb
+++ embedded_gems/flowbite/app/lib/flowbite/button_visuals.rb
@@ -10,26 +10,26 @@ module Flowbite
     }.freeze
 
     def self.included(base)
-      base.renders_one :leading_visual, types: visual_types(:mr)
-      base.renders_one :trailing_visual, types: visual_types(:ml)
+      base.renders_one :leading_visual, types: visual_types(margin_direction: :mr, name: :leading_visual)
+      base.renders_one :trailing_visual, types: visual_types(margin_direction: :ml, name: :trailing_visual)
     end
 
-    def self.visual_types(margin_direction)
+    def self.visual_types(margin_direction:, name:)
       {
-        icon: ->(**args) { icon_visual(args, margin_direction) },
-        svg: ->(**args) { svg_visual(args, margin_direction) }
+        icon: ->(**args) { icon_visual(args, margin_direction, name) },
+        svg: ->(**args) { svg_visual(args, margin_direction, name) }
       }
     end
 
-    def icon_visual(args, margin_direction)
-      args[:class] = class_names(args[:class], icon_classes(margin_direction))
+    def icon_visual(args, margin_direction, name)
+      args[:class] = class_names(args[:class], icon_classes(margin_direction), "#{name}_icon")
       Flowbite::Icon.new(**args)
     end
 
-    def svg_visual(args, margin_direction)
+    def svg_visual(args, margin_direction, name)
       Flowbite::BaseComponent.new(
         tag: :span,
-        classes: class_names(icon_classes(margin_direction)),
+        classes: class_names(icon_classes(margin_direction), "#{name}_svg"),
         **args
       )
     end
