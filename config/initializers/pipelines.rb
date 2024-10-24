05613b0895c2b2b1369fe0527dda1c968f97cf7e
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-10-24 16:16:30 -0500

    adding a test

--- config/initializers/pipelines.rb
+++ config/initializers/pipelines.rb
@@ -7,7 +7,7 @@ Rails.application.config.to_prepare do
 end
 
 Rails.application.config.after_initialize do
-  if defined?(Rails::Server)
+  if defined?(Rails::Server) || Rails.env.test?
     Irida::Pipelines.instance.available_pipelines.each_value do |pipeline|
       automated_workflows = AutomatedWorkflowExecution.where(
         "metadata ->> 'workflow_name' = ? and metadata ->> 'workflow_version' = ?", pipeline.name, pipeline.version
