16b9e072294557094b4c2034bbb6c83f98baa691
Meghan Chua <17057809+malchua@users.noreply.github.com>
2024-11-01 15:28:48 -0500

    Add dashboard projects samples_counts tests

--- test/system/dashboard/projects_test.rb
+++ test/system/dashboard/projects_test.rb
@@ -8,6 +8,9 @@ module Dashboard
       @user = users(:john_doe)
       login_as @user
       @project = projects(:project1)
+      @project2 = projects(:project2)
+      @group1 = groups(:group_one)
+      @sample1 = samples(:sample1)
     end
 
     test 'can see the list of projects' do
@@ -214,6 +217,179 @@ module Dashboard
       assert_text 'Displaying 1 item'
       assert_selector 'tr', count: 1
       assert_text projects(:john_doe_project2).human_name
+    end
+
+    test 'should update samples count after a sample deletion' do
+      visit dashboard_projects_url
+
+      assert_selector 'h1', text: I18n.t(:'dashboard.projects.index.title')
+
+      assert_equal 3, @project.samples.size
+
+      within("#project_#{@project.id}-samples-count") do
+        assert_text @project.samples.size
+      end
+
+      visit namespace_project_sample_url(@group1, @project, @sample1)
+      click_link I18n.t('projects.samples.show.remove_button')
+
+      within('#turbo-confirm[open]') do
+        click_button I18n.t(:'components.confirmation.confirm')
+      end
+
+      visit dashboard_projects_url
+
+      assert_selector 'h1', text: I18n.t(:'dashboard.projects.index.title')
+
+      click_on I18n.t(:'dashboard.projects.index.sorting.updated_at_desc')
+      click_on I18n.t(:'dashboard.projects.index.sorting.namespace_name_asc')
+
+      assert_text @project.namespace.name
+      assert_equal 2, @project.reload.samples.size
+
+      within("#project_#{@project.id}-samples-count") do
+        assert_text @project.samples.size
+      end
+    end
+
+    test 'should update samples count after a sample creation' do
+      visit dashboard_projects_url
+
+      assert_selector 'h1', text: I18n.t(:'dashboard.projects.index.title')
+
+      assert_equal 3, @project.samples.size
+
+      within("#project_#{@project.id}-samples-count") do
+        assert_text @project.samples.size
+      end
+
+      visit namespace_project_samples_url(@group1, @project)
+
+      click_link I18n.t('projects.samples.index.new_button')
+
+      find('input#sample_name').fill_in with: 'Test Sample'
+      click_button I18n.t('projects.samples.new.submit_button')
+
+      visit dashboard_projects_url
+
+      assert_selector 'h1', text: I18n.t(:'dashboard.projects.index.title')
+
+      fill_in I18n.t(:'dashboard.projects.index.search.placeholder'), with: @project.namespace.name
+      find('input.t-search-component').native.send_keys(:return)
+
+      assert_text @project.namespace.name
+      assert_equal 4, @project.reload.samples.size
+
+      within("#project_#{@project.id}-samples-count") do
+        assert_text @project.samples.size
+      end
+    end
+
+    test 'should update samples count after a sample transfer' do
+      visit dashboard_projects_url
+
+      assert_selector 'h1', text: I18n.t(:'dashboard.projects.index.title')
+
+      assert_equal 3, @project.samples.size
+
+      within("#project_#{@project.id}-samples-count") do
+        assert_text @project.samples.size
+      end
+
+      visit namespace_project_samples_url(@group1, @project)
+
+      find("input[type='checkbox'][id='sample_#{@sample1.id}']").click
+      click_link I18n.t('projects.samples.index.transfer_button')
+
+      within('span[data-controller-connected="true"] dialog') do
+        assert_text I18n.t('projects.samples.transfers.dialog.description.singular')
+        within %(turbo-frame[id="list_selections"]) do
+          assert_text @sample1.name
+        end
+        find('input#select2-input').click
+        find("button[data-viral--select2-primary-param='#{@project2.full_path}']").click
+        click_on I18n.t('projects.samples.transfers.dialog.submit_button')
+      end
+
+      visit dashboard_projects_url
+
+      assert_selector 'h1', text: I18n.t(:'dashboard.projects.index.title')
+
+      fill_in I18n.t(:'dashboard.projects.index.search.placeholder'), with: @project.namespace.name
+      find('input.t-search-component').native.send_keys(:return)
+
+      assert_text @project.namespace.name
+      assert_no_text @project2.namespace.name
+      assert_equal 2, @project.reload.samples.size
+
+      within("#project_#{@project.id}-samples-count") do
+        assert_text @project.samples.size
+      end
+
+      fill_in I18n.t(:'dashboard.projects.index.search.placeholder'), with: @project2.namespace.name
+      find('input.t-search-component').native.send_keys(:return)
+
+      assert_text @project2.namespace.name
+      assert_no_text @project.namespace.name
+      assert_equal 21, @project2.reload.samples.size
+
+      within("#project_#{@project2.id}-samples-count") do
+        assert_text @project2.samples.size
+      end
+    end
+
+    test 'should update samples count after a sample clone' do
+      visit dashboard_projects_url
+
+      assert_selector 'h1', text: I18n.t(:'dashboard.projects.index.title')
+
+      assert_equal 3, @project.samples.size
+
+      within("#project_#{@project.id}-samples-count") do
+        assert_text @project.samples.size
+      end
+
+      visit namespace_project_samples_url(@group1, @project)
+
+      find("input[type='checkbox'][id='sample_#{@sample1.id}']").click
+      click_link I18n.t('projects.samples.index.clone_button')
+
+      within('span[data-controller-connected="true"] dialog') do
+        assert_text I18n.t('projects.samples.clones.dialog.description.singular')
+        within %(turbo-frame[id="list_selections"]) do
+          assert_text @sample1.name
+        end
+        find('input#select2-input').click
+        find("button[data-viral--select2-primary-param='#{@project2.full_path}']").click
+        click_on I18n.t('projects.samples.clones.dialog.submit_button')
+      end
+      assert_text I18n.t('projects.samples.clones.create.success')
+
+      visit dashboard_projects_url
+
+      assert_selector 'h1', text: I18n.t(:'dashboard.projects.index.title')
+
+      fill_in I18n.t(:'dashboard.projects.index.search.placeholder'), with: @project.namespace.name
+      find('input.t-search-component').native.send_keys(:return)
+
+      assert_text @project.namespace.name
+      assert_no_text @project2.namespace.name
+      assert_equal 3, @project.reload.samples.size
+
+      within("#project_#{@project.id}-samples-count") do
+        assert_text @project.samples.size
+      end
+
+      fill_in I18n.t(:'dashboard.projects.index.search.placeholder'), with: @project2.namespace.name
+      find('input.t-search-component').native.send_keys(:return)
+
+      assert_text @project2.namespace.name
+      assert_no_text @project.namespace.name
+      assert_equal 21, @project2.reload.samples.size
+
+      within("#project_#{@project2.id}-samples-count") do
+        assert_text @project2.samples.size
+      end
     end
   end
 end
