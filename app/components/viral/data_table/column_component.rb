1f8f20e26ef6a1ed994d53ea3421df5f072f5ddc
Chris Huynh <chris.huynh333@gmail.com>
2024-11-05 13:10:21 -0600

    start new data-table-component

---
+++ app/components/viral/data_table/column_component.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Viral
+  module DataTable
+    # Component responsible for specific column attributes of DataTableComponent
+    class ColumnComponent < Viral::Component
+      attr_reader :title
+
+      def initialize(
+        title,
+        **system_arguments,
+        &block
+      )
+        @title = title
+        @system_arguments = system_arguments
+        @block = block
+      end
+
+      def system_arguments
+        {
+          classes: class_names('px-3 py-3')
+        }
+      end
+
+      def call(row)
+        puts 'hihihi'
+        puts row
+        puts 'byby'
+        puts @block
+        @block.call(row)
+      end
+    end
+  end
+end
