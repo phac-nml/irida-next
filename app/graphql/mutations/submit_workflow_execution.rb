2bb1cee49af92d89eeba616e9d176a871fed5cb1
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-11-06 13:22:31 -0600

    working logic

--- app/graphql/mutations/submit_workflow_execution.rb
+++ app/graphql/mutations/submit_workflow_execution.rb
@@ -9,19 +9,20 @@ module Mutations
     argument :name, String
     argument :workflow_name, String
     argument :workflow_version, String
-    argument :namespace_id, ID
+    argument :namespace_id, ID # TODO: replace with an either-or group or project, and extract namespace id from whichever is provided
     argument :workflow_params, GraphQL::Types::JSON
     argument :workflow_type, String
     argument :workflow_type_version, String
     argument :workflow_engine, String
     argument :workflow_engine_version, String
-    argument :workflow_engine_parameters, GraphQL::Types::JSON
+    argument :workflow_engine_parameters, [GraphQL::Types::JSON] # TODO: is there a better way to handle keys that start with "-"
     argument :workflow_url, String
 
-    argument :update_samples, String
-    argument :email_notification, String
+    argument :update_samples, String # TODO: make this a bool and turn into "1" or "0"
+    argument :email_notification, String # TODO: make this a bool and turn into "1" or "0"
 
-    argument :samples_workflow_executions_attributes, GraphQL::Types::JSON
+    # argument :samples_workflow_executions_attributes, GraphQL::Types::JSON
+    argument :sample_info_list, [GraphQL::Types::JSON] # TODO: give this a better name
 
     field :errors, [Types::UserErrorType], null: false, description: 'A list of errors that prevented the mutation.'
     field :workflow_execution, Types::WorkflowExecutionType, description: 'The newly created workflow execution.'
@@ -38,6 +39,7 @@ module Mutations
 
     def create_workflow_execution(args) # rubocop:disable Metrics/MethodLength
       workflow_execution_params = {
+        name: args[:name],
         metadata: { workflow_name: args[:workflow_name],
                     workflow_version: args[:workflow_version] },
         namespace_id: args[:namespace_id],
@@ -46,13 +48,13 @@ module Mutations
         workflow_type_version: args[:workflow_type_version],
         workflow_engine: args[:workflow_engine],
         workflow_engine_version: args[:workflow_engine_version],
-        # TODO: fix this
+        # TODO: fix this issue with key starting with "-r"
         # workflow_engine_parameters: args[:workflow_engine_parameters],
-        workflow_engine_parameters: { '-r' => '1.0.3' },
+        workflow_engine_parameters: workflow_engine_parameters(args[:workflow_engine_parameters]),
         workflow_url: args[:workflow_url],
         update_samples: args[:update_samples],
         email_notification: args[:email_notification],
-        samples_workflow_executions_attributes: args[:samples_workflow_executions_attributes]
+        samples_workflow_executions_attributes: samples_workflow_executions_attributes(args[:sample_info_list])
       }
 
       workflow_execution = WorkflowExecutions::CreateService.new(
@@ -78,6 +80,46 @@ module Mutations
           errors: nil
         }
       end
+    end
+
+    def workflow_engine_parameters(parameter_list)
+      result = {}
+      parameter_list.each do |params|
+        result[params['key']] = params['value']
+      end
+      result
+    end
+
+    def samples_workflow_executions_attributes(sample_info_list)
+      result = {}
+      sample_info_list.each_with_index do |sample_info, index|
+        key = index.to_s
+        # TODO: endable this error handling for sample_id's
+        # begin
+          sample = IridaSchema.object_from_id(sample_info['sample_id'], { expected_type: Sample })
+        # rescue GraphQL::CoercionError => e
+        #   user_errors.append(
+        #     {
+        #       path: ['copySamples'],
+        #       message: e.message
+        #     }
+        #   )
+        #   next
+        # end
+
+        samplesheet_params = { 'sample' => sample.puid }
+        sample_info['files'].each do |file_info|
+          # TODO: file id error checking should probably be done as a validate step on the WorkflowExecution object
+          samplesheet_params[file_info['file_type']] = file_info['file_id']
+        end
+
+        result[key] = {
+          'sample_id' => sample.id,
+          'samplesheet_params' => samplesheet_params
+        }
+      end
+
+      result
     end
   end
 end
