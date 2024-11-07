956e8d8b3d2d2ca90efbe72a138092cf0bb0a88f
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2024-11-07 17:30:04 -0600

    Restructure code flow. no error handling yet.

--- app/graphql/mutations/submit_workflow_execution.rb
+++ app/graphql/mutations/submit_workflow_execution.rb
@@ -6,23 +6,30 @@ module Mutations
     null true
     description 'Create a new workflow execution..'
 
-    argument :name, String
-    argument :workflow_name, String
-    argument :workflow_version, String
-    argument :namespace_id, ID # TODO: replace with an either-or group or project, and extract namespace id from whichever is provided
-    argument :workflow_params, GraphQL::Types::JSON
-    argument :workflow_type, String
-    argument :workflow_type_version, String
-    argument :workflow_engine, String
-    argument :workflow_engine_version, String
-    argument :workflow_engine_parameters, [GraphQL::Types::JSON] # TODO: is there a better way to handle keys that start with "-"
-    argument :workflow_url, String
+    argument :email_notification, Boolean,
+             description: 'Set to true to enable email notifications from this workflow execution'
+    argument :name, String, description: 'Name for the new workflow.'
+    # TODO: figure out if this is workflow schema dependant
+    argument :samples_files_info_list, [GraphQL::Types::JSON], description: 'List of hashes containing a `sample_id` (String), and `files` (Hash) containing pairs of `file_type` (String) and `file_id` (String).' # rubocop:disable GraphQL/ExtractInputType,Layout/LineLength
+    argument :update_samples, Boolean, description: 'Set true for samples to be updated from this workflow execution' # rubocop:disable GraphQL/ExtractInputType
+    argument :workflow_engine, String, description: '' # rubocop:disable GraphQL/ExtractInputType
+    argument :workflow_engine_parameters, [GraphQL::Types::JSON], description: 'List of Hashes containing `key` and `value` to be passed to the workflow engine.' # rubocop:disable GraphQL/ExtractInputType,Layout/LineLength
+    argument :workflow_engine_version, String, description: 'Workflow Engine Version' # rubocop:disable GraphQL/ExtractInputType
+    argument :workflow_name, String, description: 'Name of the pipeline to be run on this workflow execution' # rubocop:disable GraphQL/ExtractInputType
+    argument :workflow_params, GraphQL::Types::JSON, description: 'Parameters to be passed to the pipeline.' # rubocop:disable GraphQL/ExtractInputType
+    argument :workflow_type, String, description: 'Type of pipelines workflow.' # rubocop:disable GraphQL/ExtractInputType
+    argument :workflow_type_version, String, description: 'Version of the pipelines workflow type.' # rubocop:disable GraphQL/ExtractInputType
+    argument :workflow_url, String, description: 'Url for the pipeline.' # rubocop:disable GraphQL/ExtractInputType
+    argument :workflow_version, String, description: 'Version of the pipeline to be run on this workflow execution' # rubocop:disable GraphQL/ExtractInputType
 
-    argument :update_samples, String # TODO: make this a bool and turn into "1" or "0"
-    argument :email_notification, String # TODO: make this a bool and turn into "1" or "0"
-
-    # argument :samples_workflow_executions_attributes, GraphQL::Types::JSON
-    argument :sample_info_list, [GraphQL::Types::JSON] # TODO: give this a better name
+    # one of project/group, to use as the namespace
+    argument :project_id, ID, # rubocop:disable GraphQL/ExtractInputType
+             required: false,
+             description: 'The Node ID of the project to run workflow in. For example, `gid://irida/Project/a84cd757-dedb-4c64-8b01-097020163077`.' # rubocop:disable Layout/LineLength
+    argument :group_id, ID, # rubocop:disable GraphQL/OrderedArguments,GraphQL/ExtractInputType
+             required: false,
+             description: 'The Node ID of the group to run workflow in. For example, `gid://irida/Group/a84cd757-dedb-4c64-8b01-097020163077`.' # rubocop:disable Layout/LineLength
+    validates required: { one_of: %i[project_id group_id] }
 
     field :errors, [Types::UserErrorType], null: false, description: 'A list of errors that prevented the mutation.'
     field :workflow_execution, Types::WorkflowExecutionType, description: 'The newly created workflow execution.'
@@ -37,24 +44,37 @@ module Mutations
 
     private
 
-    def create_workflow_execution(args) # rubocop:disable Metrics/MethodLength
+    def create_workflow_execution(args) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
+      workflow_engine_parameters = build_workflow_engine_parameters(args[:workflow_engine_parameters])
+
+      update_samples = args[:update_samples] ? '1' : '0'
+      email_notification = args[:email_notification] ? '1' : '0'
+
+      # TODO: handle these errors
+      namespace_id = namespace(args[:project_id], args[:group_id])
+
+      samples_workflow_executions_attributes = build_samples_workflow_executions_attributes(args[:samples_files_info_list]) # rubocop:disable Layout/LineLength
+      if samples_workflow_executions_attributes[:result].nil?
+        # TODO: handle these errors
+      else
+        samples_workflow_executions_attributes = samples_workflow_executions_attributes[:result]
+      end
+
       workflow_execution_params = {
         name: args[:name],
         metadata: { workflow_name: args[:workflow_name],
                     workflow_version: args[:workflow_version] },
-        namespace_id: args[:namespace_id],
+        namespace_id:,
         workflow_params: args[:workflow_params],
         workflow_type: args[:workflow_type],
         workflow_type_version: args[:workflow_type_version],
         workflow_engine: args[:workflow_engine],
         workflow_engine_version: args[:workflow_engine_version],
-        # TODO: fix this issue with key starting with "-r"
-        # workflow_engine_parameters: args[:workflow_engine_parameters],
-        workflow_engine_parameters: workflow_engine_parameters(args[:workflow_engine_parameters]),
+        workflow_engine_parameters:,
         workflow_url: args[:workflow_url],
-        update_samples: args[:update_samples],
-        email_notification: args[:email_notification],
-        samples_workflow_executions_attributes: samples_workflow_executions_attributes(args[:sample_info_list])
+        update_samples:,
+        email_notification:,
+        samples_workflow_executions_attributes:
       }
 
       workflow_execution = WorkflowExecutions::CreateService.new(
@@ -82,7 +102,17 @@ module Mutations
       end
     end
 
-    def workflow_engine_parameters(parameter_list)
+    def namespace(project_id, group_id)
+      if project_id
+        IridaSchema.object_from_id(project_id, { expected_type: Project }).namespace.id
+      else # group_id
+        IridaSchema.object_from_id(group_id, { expected_type: Group }).id
+      end
+    end
+
+    # workflow engine parameters can have keys that start with `-` which is not allowed in graphql,
+    # so we parse a list of key value pairs into a hash that can be used.
+    def build_workflow_engine_parameters(parameter_list)
       result = {}
       parameter_list.each do |params|
         result[params['key']] = params['value']
@@ -90,36 +120,46 @@ module Mutations
       result
     end
 
-    def samples_workflow_executions_attributes(sample_info_list)
+    def build_samples_workflow_executions_attributes(sample_info_list) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
       result = {}
+      errors = []
       sample_info_list.each_with_index do |sample_info, index|
-        key = index.to_s
-        # TODO: endable this error handling for sample_id's
-        # begin
+        # Check that the sample is the actual sample
+        begin
           sample = IridaSchema.object_from_id(sample_info['sample_id'], { expected_type: Sample })
-        # rescue GraphQL::CoercionError => e
-        #   user_errors.append(
-        #     {
-        #       path: ['copySamples'],
-        #       message: e.message
-        #     }
-        #   )
-        #   next
-        # end
+        rescue GraphQL::CoercionError => e
+          errors.append(
+            {
+              path: %w[submitWorkflowExecution sample],
+              message: e.message
+            }
+          )
+          next
+        end
 
         samplesheet_params = { 'sample' => sample.puid }
         sample_info['files'].each do |file_info|
-          # TODO: file id error checking should probably be done as a validate step on the WorkflowExecution object
+          # TODO: verify that these are validated by the WorkflowExecution, otherwise verify them here
           samplesheet_params[file_info['file_type']] = file_info['file_id']
         end
 
-        result[key] = {
+        # We expect results to have numbered entries as strings starting with "0", so we use the index
+        result[index.to_s] = {
           'sample_id' => sample.id,
           'samplesheet_params' => samplesheet_params
         }
       end
 
-      result
+      if errors.count.positive?
+        result = nil
+      else
+        errors = nil
+      end
+
+      {
+        result:,
+        errors:
+      }
     end
   end
 end
