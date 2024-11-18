1fa9f54d668617bc6ec312abdd033b1e5c03313c
Chris Huynh <chris.huynh333@gmail.com>
2024-11-18 11:56:22 -0600

    refactor sticky logic

--- app/components/viral/data_table/column_component.rb
+++ app/components/viral/data_table/column_component.rb
@@ -6,6 +6,25 @@ module Viral
     class ColumnComponent < Viral::Component
       attr_reader :title
 
+      STICKY_CLASSES = {
+        left: {
+          header: 'sticky left-0 bg-slate-50 dark:bg-slate-700 z-10 ',
+          body: 'sticky left-0 bg-slate-50 dark:bg-slate-800 z-10'
+        },
+        sample_left_id: {
+          header: 'sticky left-0 min-w-56 max-w-56 bg-slate-50 dark:bg-slate-700 z-10',
+          body: 'sticky left-0 min-w-56 max-w-56 bg-slate-50 dark:bg-slate-800'
+        },
+        sample_left_name: {
+          header: 'sticky left-56 bg-slate-50 dark:bg-slate-700 z-10 z-10',
+          body: 'sticky left-56 bg-slate-50 dark:bg-slate-800'
+        },
+        right: {
+          header: 'sticky right-0 bg-slate-50 dark:bg-slate-700 z-10',
+          body: 'sticky right-0 bg-white dark:bg-slate-800 space-x-2 z-10'
+        }
+      }.freeze
+
       def initialize(
         title,
         **system_arguments,
@@ -16,17 +35,12 @@ module Viral
         @block = block
       end
 
-      def system_arguments(column_title, is_header)
-        additional_classes = if column_title == I18n.t('viral.data_table_component.header.action')
-                               if is_header
-                                 'bg-slate-50 dark:bg-slate-700 sticky right-0'
-                               else
-                                 'sticky right-0 bg-white dark:bg-slate-800 z-10 space-x-2'
-                               end
-                             end
+      def system_arguments(is_header: nil)
+        cell_type_key = is_header ? :header : :body
         {
           classes:
-          class_names('px-3 py-3', additional_classes)
+          class_names('px-3 py-3',
+                      @system_arguments[:sticky_key] && STICKY_CLASSES[@system_arguments[:sticky_key]][cell_type_key])
         }
       end
 
