0649b44462e869e33c13837999f735c227ba0ffb
Chris Huynh <chris.huynh333@gmail.com>
2024-11-19 11:44:52 -0600

    further implement tabs, cleanup

--- embedded_gems/pathogen/app/components/pathogen/tabs_panel.rb
+++ embedded_gems/pathogen/app/components/pathogen/tabs_panel.rb
@@ -2,32 +2,38 @@
 
 module Pathogen
   class TabsPanel < Pathogen::Component
-    BODY_TAG_DEFAULT = :ul
-
     TAG_DEFAULT = :nav
     TAG_OPTIONS = [TAG_DEFAULT, :div].freeze
+
+    WRAPPER_DEFAULT_CLASSES = '
+      flex text-sm font-medium text-center border-b text-slate-500 border-slate-200
+      dark:text-slate-400 dark:border-slate-700
+    '
+
+    BODY_TAG_DEFAULT = :ul
+    BODY_DEFAULT_CLASSES = 'flex flex-wrap -mb-px'
 
     renders_many :tabs, lambda { |selected: false, **system_arguments|
       # system_arguments[:classes] = tab_nav_tab_classes(system_arguments[:classes])
       Pathogen::Tab.new(
-        list: true,
         selected: selected,
+        controls: @id,
         **system_arguments
       )
     }
 
-    def initialize(label:, tag: TAG_DEFAULT, body_arguments: {}, **system_arguments)
-      # @align = EXTRA_ALIGN_DEFAULT
+    def initialize(id:, label: '', tag: TAG_DEFAULT, body_arguments: {}, **system_arguments)
+      @id = id
       @system_arguments = system_arguments
       @body_arguments = body_arguments
 
       @system_arguments[:tag] = fetch_or_fallback(TAG_OPTIONS, tag, TAG_DEFAULT)
-      # @system_arguments[:classes] = tab_nav_classes(system_arguments[:classes])
-
+      @system_arguments[:classes] = class_names(WRAPPER_DEFAULT_CLASSES)
+      @system_arguments[:id] = @id
       @body_arguments[:tag] = BODY_TAG_DEFAULT
-      # @body_arguments[:classes] = tab_nav_body_classes(body_arguments[:classes])
-
-      # aria_label_for_page_nav(label)
+      @body_arguments[:classes] = class_names(BODY_DEFAULT_CLASSES)
+      @body_arguments[:role] = 'tablist'
+      @body_arguments[:'aria-label'] = label
     end
 
     # def before_render
