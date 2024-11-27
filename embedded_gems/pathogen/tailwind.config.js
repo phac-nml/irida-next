2d23fb97e4c68a97015e3e8aa81a616ed84a0f48
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-11-27 16:10:23 -0600

    style: :lipstick: Updated button styles for light and dark and added semantic tailwind colour names

--- embedded_gems/pathogen/tailwind.config.js
+++ embedded_gems/pathogen/tailwind.config.js
@@ -1,5 +1,10 @@
 const colors = require("tailwindcss/colors");
 
+/*
+Colors naming convention: [light|dark]-[role]-[primary|secondary]-[interaction]
+e.g. hover:dark-brand-primary-hover
+*/
+
 const globalColors = {
   brand: colors.emerald,
   neutral: colors.slate,
@@ -22,60 +27,75 @@ module.exports = {
       colors: globalColors,
       backgroundColor: {
         // Light mode
-        "brand-primary": globalColors.brand[600],
-        "brand-primary-hover": globalColors.brand[500],
-        "brand-secondary": globalColors.brand[100],
-        "brand-secondary-hover": globalColors.brand[200],
-        "neutral-primary": globalColors.neutral[100],
-        "neutral-primary-hover": globalColors.neutral[200],
-        "neutral-secondary": globalColors.neutral[50],
-        "danger-primary": globalColors.danger[600],
-        "danger-primary-hover": globalColors.danger[800],
-        "danger-secondary": globalColors.danger[100],
-        "danger-secondary-hover": globalColors.danger[200],
+        "light-brand-primary": globalColors.brand[600],
+        "light-brand-primary-hover": globalColors.brand[500],
+        "light-brand-secondary": globalColors.brand[100],
+        "light-brand-secondary-hover": globalColors.brand[200],
+        "light-neutral-primary": globalColors.neutral[100],
+        "light-neutral-primary-hover": globalColors.neutral[200],
+        "light-neutral-secondary": globalColors.neutral[50],
+        "light-danger-primary": globalColors.danger[600],
+        "light-danger-primary-hover": globalColors.danger[800],
+        "light-danger-secondary": globalColors.danger[100],
+        "light-danger-secondary-hover": globalColors.danger[200],
+
         // Dark mode
-        "dark:brand-primary": globalColors.brand[500],
-        "dark:brand-primary-hover": globalColors.brand[400],
-        "dark:brand-secondary": globalColors.brand[900],
-        "dark:brand-secondary-hover": globalColors.brand[800],
-        "dark:neutral-primary": globalColors.neutral[800],
-        "dark:neutral-primary-hover": globalColors.neutral[700],
-        "dark:neutral-secondary": globalColors.neutral[900],
-        "dark:danger-primary": globalColors.danger[500],
-        "dark:danger-primary-hover": globalColors.danger[400],
-        "dark:danger-secondary": globalColors.danger[900],
-        "dark:danger-secondary-hover": globalColors.danger[800],
+        "dark-brand-primary": globalColors.brand[700],
+        "dark-brand-primary-hover": globalColors.brand[800],
+        "dark-brand-secondary": globalColors.brand[900],
+        "dark-brand-secondary-hover": globalColors.brand[800],
+        "dark-neutral-primary": globalColors.neutral[800],
+        "dark-neutral-primary-hover": globalColors.neutral[700],
+        "dark-neutral-secondary": globalColors.neutral[900],
+        "dark-danger-primary": globalColors.danger[600],
+        "dark-danger-primary-hover": globalColors.danger[700],
+        "dark-danger-secondary": globalColors.danger[900],
+        "dark-danger-secondary-hover": globalColors.danger[800],
       },
       borderColor: {
         // Light mode
-        "brand-primary": globalColors.brand[400],
-        "neutral-primary": globalColors.neutral[300],
-        "danger-primary": globalColors.danger[400],
-        "danger-secondary": globalColors.danger[300],
+        "light-brand-primary": globalColors.brand[600],
+        "light-neutral-primary": globalColors.neutral[400],
+        "light-danger-primary": globalColors.danger[400],
+        "light-danger-secondary": globalColors.danger[300],
+
         // Dark mode
-        "dark:brand-primary": globalColors.brand[300],
-        "dark:neutral-primary": globalColors.brand[200],
-        "dark:danger-primary": globalColors.danger[300],
-        "dark:danger-secondary": globalColors.danger[200],
+        "dark-brand-primary": globalColors.brand[500],
+        "dark-neutral-primary": globalColors.neutral[600],
+        "dark-danger-primary": globalColors.danger[500],
+        "dark-danger-secondary": globalColors.danger[600],
       },
       textColor: {
         // Light mode
-        "brand-onprimary": colors.white,
-        "brand-onsecondary": globalColors.brand[800],
-        "neutral-primary": globalColors.neutral[800],
-        "neutral-secondary": globalColors.neutral[500],
-        "neutral-emphasis": globalColors.neutral[900],
-        "danger-onprimary": globalColors.danger[800],
-        "danger-onprimary-hover": globalColors.neutral[50],
-        "danger-onsecondary": globalColors.danger[800],
+        "light-brand-onprimary": colors.white,
+        "light-brand-onsecondary": globalColors.brand[800],
+        "light-neutral-primary": globalColors.neutral[800],
+        "light-neutral-secondary": globalColors.neutral[500],
+        "light-neutral-emphasis": globalColors.neutral[900],
+        "light-danger-onprimary": colors.white,
+        "light-danger-onprimary-hover": globalColors.neutral[50],
+        "light-danger-onsecondary": globalColors.danger[800],
+
         // Dark mode
-        "dark:brand-onprimary": colors.white,
-        "dark:brand-onsecondary": globalColors.brand[200],
-        "dark:neutral-primary": globalColors.neutral[200],
-        "dark:neutral-secondary": globalColors.neutral[400],
-        "dark:neutral-emphasis": globalColors.neutral[100],
-        "dark:danger-onprimary": colors.white,
-        "dark:danger-onsecondary": globalColors.danger[200],
+        "dark-brand-onprimary": colors.white,
+        "dark-brand-onsecondary": globalColors.brand[200],
+        "dark-neutral-primary": globalColors.neutral[300],
+        "dark-neutral-secondary": globalColors.neutral[400],
+        "dark-neutral-emphasis": globalColors.neutral[100],
+        "dark-danger-onprimary": colors.white,
+        "dark-danger-onprimary-hover": globalColors.neutral[900],
+        "dark-danger-onsecondary": globalColors.danger[200],
+      },
+      ringColor: {
+        // Light mode
+        "light-brand-primary": globalColors.brand[100],
+        "light-neutral-primary": globalColors.neutral[100],
+        "light-danger-primary": globalColors.danger[100],
+
+        // Dark mode
+        "dark-brand-primary": globalColors.brand[800],
+        "dark-neutral-primary": globalColors.neutral[600],
+        "dark-danger-primary": globalColors.danger[600],
       },
     },
   },
