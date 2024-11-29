57b4e5e86262e13fa1c9bd8f8f71f70c1dbd6fec
Chris Huynh <chris.huynh333@gmail.com>
2024-11-29 11:46:04 -0600

    refactor PAT translations

--- test/system/profile_test.rb
+++ test/system/profile_test.rb
@@ -104,7 +104,7 @@ class ProfileTest < ApplicationSystemTestCase
 
     assert_text I18n.t(:'profiles.personal_access_tokens.index.active_personal_access_tokens',
                        count: @active_token_count)
-    within('#personal-access-tokens-table') do
+    within('#access-tokens-table') do
       assert_text token_to_revoke.name
     end
     within %(tr[id=#{token_to_revoke.id}]) do
@@ -114,7 +114,7 @@ class ProfileTest < ApplicationSystemTestCase
     within('#turbo-confirm[open]') do
       click_button I18n.t(:'components.confirmation.confirm')
     end
-    within('#personal-access-tokens-table') do
+    within('#access-tokens-table') do
       assert_no_text token_to_revoke.name
     end
     assert_text I18n.t(:'profiles.personal_access_tokens.index.active_personal_access_tokens',
