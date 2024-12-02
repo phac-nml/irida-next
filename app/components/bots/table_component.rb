448c1ca2d8df467edeb605760bc79b1b63feea1d
Chris Huynh <chris.huynh333@gmail.com>
2024-12-02 10:40:38 -0600

    cleanup, add rubocop disable

--- app/components/bots/table_component.rb
+++ app/components/bots/table_component.rb
@@ -7,6 +7,7 @@ module Bots
   class TableComponent < Component
     include Ransack::Helpers::FormHelper
 
+    # rubocop: disable Metrics/ParameterLists
     def initialize(
       bot_accounts,
       namespace,
@@ -22,6 +23,7 @@ module Bots
       @empty = empty
       @system_arguments = system_arguments
     end
+    # rubocop: enable Metrics/ParameterLists
 
     private
 
@@ -46,22 +48,6 @@ module Bots
         group_bot_path(id: bot.id)
       elsif @namespace.is_a?(Namespaces::ProjectNamespace)
         namespace_project_bot_path(id: bot.id)
-      end
-    end
-
-    def revoke_path(token)
-      if @namespace.is_a?(Group)
-        revoke_group_bot_personal_access_token_path(
-          bot_id: @bot_account.id,
-          id: token.id
-        )
-      elsif @namespace.is_a?(Namespaces::ProjectNamespace)
-        revoke_namespace_project_bot_personal_access_token_path(
-          bot_id: @bot_account.id,
-          id: token.id
-        )
-      else
-        revoke_profile_personal_access_token_path(id: token.id)
       end
     end
   end
