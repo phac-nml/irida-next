219339b5894c80298fb5136522be6650c89dacac
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-12-02 14:37:42 -0600

    fix: Fixing path issues

--- embedded_gems/pathogen/demo/config/tailwind.config.js
+++ embedded_gems/pathogen/demo/config/tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
     "./app/views/**/*.{erb,haml,html,slim}",
-    "./test/components/previews/**/*{rb,erb,haml,html,slim}",
   ],
   plugins: [require("@tailwindcss/typography")],
 };
