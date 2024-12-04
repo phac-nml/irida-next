20b576d17fcf66f535392295ff223329993faa60
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-12-04 11:04:45 -0600

    refactor: Add frozen string literal comment and standardize string quotes in initializer files for consistency

--- embedded_gems/pathogen/demo/config/initializers/pathogen.rb
+++ embedded_gems/pathogen/demo/config/initializers/pathogen.rb
@@ -1,6 +1,8 @@
-require "pathogen/view_components"
+# frozen_string_literal: true
+
+require 'pathogen/view_components'
 
 # Use concat to create new arrays instead of modifying the existing ones
-components_path = Rails.root.join("../app/components")
-Rails.application.config.autoload_paths = Rails.application.config.autoload_paths.dup.concat([ components_path ])
-Rails.application.config.eager_load_paths = Rails.application.config.eager_load_paths.dup.concat([ components_path ])
+components_path = Rails.root.join('../app/components')
+Rails.application.config.autoload_paths = Rails.application.config.autoload_paths.dup.concat([components_path])
+Rails.application.config.eager_load_paths = Rails.application.config.eager_load_paths.dup.concat([components_path])
