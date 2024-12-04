2357922922ebaed944cd8ac3b5fe6cb3503507f4
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-12-04 10:43:32 -0600

    refactor: Enhance test_helper.rb with comments for clarity and parallel test setup

--- embedded_gems/pathogen/demo/test/test_helper.rb
+++ embedded_gems/pathogen/demo/test/test_helper.rb
@@ -1,7 +1,19 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
+# frozen_string_literal: true
 
+# This file sets up the test environment for the Rails application.
+# It ensures that the Rails environment is set to 'test' and requires
+# the necessary files for running tests. It also configures the test
+# suite to run tests in parallel and loads all fixtures for use in tests.
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+
+# This module provides support for parallelizing test cases and setting up fixtures.
+#
+# ActiveSupport::TestCase:
+# - parallelize(workers: :number_of_processors): Runs tests in parallel using the number of processors available.
+# - fixtures :all: Loads all fixtures in the test/fixtures/*.yml directory for all tests in alphabetical order.
+# - Additional helper methods can be added to be used by all tests.
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
