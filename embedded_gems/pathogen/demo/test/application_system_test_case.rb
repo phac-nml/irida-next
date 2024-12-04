22ba2b90ec56b92593a82a8360eb003795dacb48
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-12-04 10:54:49 -0600

    refactor: Enhance ApplicationSystemTestCase with comments for clarity and improve formatting

--- embedded_gems/pathogen/demo/test/application_system_test_case.rb
+++ embedded_gems/pathogen/demo/test/application_system_test_case.rb
@@ -1,5 +1,10 @@
-require "test_helper"
+# frozen_string_literal: true
 
+require 'test_helper'
+
+# This class serves as the base class for all system test cases in the application.
+# It inherits from ActionDispatch::SystemTestCase and configures the test driver to use
+# Selenium with headless Chrome and a screen size of 1400x1400.
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end
