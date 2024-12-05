e9ae9f89199940d9b1463ce5a8c044cd11f5bd54
Meghan Chua <17057809+malchua@users.noreply.github.com>
2024-12-05 12:22:28 -0600

    Add tests for new component

---
+++ test/components/viral/form/prefixed/select2_component_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Viral
+  module Form
+    module Prefixed
+      class Select2ComponentTest < ViewComponent::TestCase
+        def test_component_renders_something_useful
+          # assert_equal(
+          #   %(<span>Hello, components!</span>),
+          #   render_inline(Viral::Form::Prefixed::Select2Component.new(message: "Hello, components!")).css("span").to_html
+          # )
+        end
+      end
+    end
+  end
+end
