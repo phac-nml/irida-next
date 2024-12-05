bccc454c1205125728bfb687cce4ecf3a276363d
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-12-05 11:11:40 -0600

    refactor: Updating sidebar to label what user is in

--- app/components/layout/sidebar_component.rb
+++ app/components/layout/sidebar_component.rb
@@ -3,16 +3,15 @@
 module Layout
   # Sidebar component to for navigation
   class SidebarComponent < Component
-    attr_reader :label, :icon_name, :tooltip
+    attr_reader :label, :tooltip
 
     renders_one :header, Sidebar::HeaderComponent
     renders_many :sections, Sidebar::SectionComponent
     renders_many :items, Sidebar::ItemComponent
 
-    def initialize(label:, icon_name:, pipelines_enabled: true, **system_arguments)
+    def initialize(label:, pipelines_enabled: true, **system_arguments)
       @label = label
       @tooltip = label
-      @icon_name = icon_name
       @pipelines_enabled = pipelines_enabled
       @system_arguments = system_arguments
     end
