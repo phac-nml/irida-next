abfff45ebb93339c30af4c108948f9d1b2209492
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-12-10 19:25:53 -0600

    feat: :sparkles: Initial work on editing line list

--- app/controllers/projects/samples_controller.rb
+++ app/controllers/projects/samples_controller.rb
@@ -89,6 +89,23 @@ module Projects
       end
     end
 
+    def check_editable
+      @sample = Sample.find(params[:id])
+      @field = params[:field]
+
+      if @sample.field_editable?(@field)
+        render turbo_stream: turbo_stream.replace(
+          helpers.dom_id(@sample, @field),
+          partial: 'samples/edit_field_form',
+          locals: { sample: @sample, field: @field }
+        )
+      else
+        render turbo_stream: turbo_stream.append('flash',
+                                                 partial: 'shared/alert',
+                                                 locals: { message: 'This field is not editable' })
+      end
+    end
+
     private
 
     def sample
