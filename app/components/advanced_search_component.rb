bb73aa994fddf525f7a3ea33ac9214259de56a4a
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-12-16 11:45:47 -0600

    creating slots for buttons

--- app/components/advanced_search_component.rb
+++ app/components/advanced_search_component.rb
@@ -4,6 +4,10 @@
 class AdvancedSearchComponent < Component
   attr_reader :form, :filters
 
+  renders_one :add_condition_button
+  renders_one :add_group_button
+  renders_one :remove_group_button
+
   def initialize(form:, filters:, metadata_fields: [], operations: [])
     @form = form
     @filters = filters
