a7a0da7ac7719a7bc2a8d80df371c2d3a06b4def
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-12-16 13:46:00 -0600

    passing a list of fields & operations and creating a new advanced search stimulus controller

--- app/components/advanced_search_component.rb
+++ app/components/advanced_search_component.rb
@@ -2,16 +2,12 @@
 
 # View component for advanced search component
 class AdvancedSearchComponent < Component
-  attr_reader :form, :filters
-
   renders_one :add_condition_button
   renders_one :add_group_button
   renders_one :remove_group_button
 
-  def initialize(form:, filters:, metadata_fields: [], operations: [])
-    @form = form
-    @filters = filters
-    @metadata_fields = metadata_fields
+  def initialize(fields: [], operations: [])
+    @fields = fields
     @operations = operations
   end
 end
