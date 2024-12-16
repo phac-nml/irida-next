155726cc6d96118f0596e3d3ac11e9aad6e50e36
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-12-16 11:45:47 -0600

    creating slots for buttons

--- test/components/previews/advanced_search_component_preview.rb
+++ test/components/previews/advanced_search_component_preview.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class AdvancedSearchComponentPreview < ViewComponent::Preview
-  def default; end
+  def with_group_condition_buttons; end
+
+  def with_or_and_buttons; end
+
+  def with_no_remove_or_button; end
 end
