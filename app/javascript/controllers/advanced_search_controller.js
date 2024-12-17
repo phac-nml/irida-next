2bf2d6b4554ce1f887c9459f60fe2085ee553b33
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2024-12-17 10:06:21 -0600

    starting to add group/condition functionality

--- app/javascript/controllers/advanced_search_controller.js
+++ app/javascript/controllers/advanced_search_controller.js
@@ -1,11 +1,52 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  connect() {}
+  static targets = [
+    "conditionsContainer", //many
+    "conditionTemplate",
+    "groupsContainer",
+    "groupTemplate",
+  ];
+
+  connect() {
+    this.addGroup();
+    this.addCondition();
+  }
 
   idempotentConnect() {}
 
   submit() {
     this.element.requestSubmit();
+  }
+
+  addCondition(event) {
+    console.log("add condition");
+    // console.log(this.conditionTemplateTargets.length);
+    if (event) {
+      console.log(event.currentTarget.parentElement);
+      console.log(
+        event.currentTarget.parentElement.parentElement.closest("template"),
+      );
+      event.currentTarget.parentElement.previousElementSibling.insertAdjacentHTML(
+        "beforeend",
+        this.conditionTemplateTarget.innerHTML,
+      );
+    } else {
+      this.conditionsContainerTarget.insertAdjacentHTML(
+        "beforeend",
+        this.conditionTemplateTarget.innerHTML,
+      );
+    }
+  }
+
+  removeCondition(event) {
+    event.currentTarget.parentElement.remove();
+  }
+
+  addGroup() {
+    this.groupsContainerTarget.insertAdjacentHTML(
+      "beforeend",
+      this.groupTemplateTarget.innerHTML,
+    );
   }
 }
