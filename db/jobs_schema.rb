45e0fa2847216ca58c2bbc2cce98eec028eee4f0
Eric Enns <492127+ericenns@users.noreply.github.com>
2024-12-17 10:12:20 -0600

    chore: update to rails 7.2.2.1 and update good_jobs to 4.6.0

--- db/jobs_schema.rb
+++ db/jobs_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_14_150155) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_17_161002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_14_150155) do
     t.datetime "enqueued_at"
     t.datetime "discarded_at"
     t.datetime "finished_at"
+    t.datetime "jobs_finished_at"
   end
 
   create_table "good_job_executions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
