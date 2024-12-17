7ff68d803e4ee3d315ba5f4da4d5285c1db145b8
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-12-16 21:54:08 -0600

    test: Add test for updating metadata value not from an analysis

--- test/system/groups/samples_test.rb
+++ test/system/groups/samples_test.rb
@@ -767,5 +767,28 @@ module Groups
         click_on I18n.t('shared.samples.metadata.file_imports.errors.ok_button')
       end
     end
+
+    test 'can update metadata value that is not from an analysis' do
+      visit group_samples_url(@group)
+      assert_selector 'table thead tr th', count: 6
+
+      click_on 'Last Updated'
+      assert_selector 'table thead th:nth-child(5) svg.icon-arrow_up'
+
+      assert_selector 'label', text: I18n.t('projects.samples.shared.metadata_toggle.label'), count: 1
+      find('label', text: I18n.t('projects.samples.shared.metadata_toggle.label')).click
+
+      within 'div.overflow-auto.scrollbar' do |div|
+        div.scroll_to div.find('table thead th:nth-child(7)')
+      end
+
+      assert_selector 'table thead tr th', count: 9
+      within('table tbody tr:first-child') do
+        assert_text @sample30.name
+        assert_selector 'td:nth-child(7) input[type="submit"][value="value1"]'
+        click_button 'input[type="submit"][value="value1"]'
+        assert_selector 'input[type="text"][value="value1"]'
+      end
+    end
   end
 end
