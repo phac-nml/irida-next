2245596e9a2fd3c30596b7e2612cd212c7f6312f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-12-18 11:28:43 -0600

    feat: Implement editable cell component with button interactions and update tests for button selectors

--- app/components/samples/editable_cell.rb
+++ app/components/samples/editable_cell.rb
@@ -5,35 +5,6 @@ module Samples
   class EditableCell < Component
     with_collection_parameter :field
 
-    erb_template <<~ERB
-      <td id="<%= dom_id(@sample, @field) %>"
-          class="relative"
-          role="gridcell">
-        <%= form_with(
-              url: editable_namespace_project_sample_metadata_field_path(
-                @sample.project.namespace.parent,
-                @sample.project,
-                @sample
-              ),
-              method: :get,
-              class: "w-full"
-            ) do |form| %>
-
-          <%= form.hidden_field :field, value: @field %>
-          <%= form.hidden_field :format, value: "turbo_stream" %>
-
-          <button type="submit"
-                  class="w-full p-4 text-left cursor-pointer hover:bg-slate-50 focus:ring-4 focus:outline-none focus:ring-primary-200 dark:hover:bg-slate-600 dark:focus:ring-primary-700"
-                  aria-label="<%= t(:".aria_label", value: @sample.metadata[@field]) %> value"
-                  <%= "autofocus" if @autofocus %>>
-            <span class="block truncate">
-              <%= @sample.metadata[@field] || "" %>
-            </span>
-          </button>
-        <% end %>
-      </td>
-    ERB
-
     def initialize(field:, sample:, autofocus: false)
       @sample = sample
       @field = field
