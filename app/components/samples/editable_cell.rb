c8bdf21dbc4847ef16e2b42e902540aa95a79bb7
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-12-17 21:10:11 -0600

    feat: Add autofocus option to editable cell for improved user experience

--- app/components/samples/editable_cell.rb
+++ app/components/samples/editable_cell.rb
@@ -10,14 +10,15 @@ module Samples
         <%= form_with(url: editable_namespace_project_sample_metadata_field_path(@sample.project.namespace.parent, @sample.project, @sample), method: :get) do |form| %>
           <%= form.hidden_field :field, value: @field %>
           <%= form.hidden_field :format, value: "turbo_stream" %>
-          <%= form.submit @sample.metadata[@field]  || "", class: "cursor-pointer p-4 hover:bg-slate-50 dark:hover:bg-slate-600 w-full text-left" %>
+          <%= form.submit @sample.metadata[@field]  || "", class: "cursor-pointer p-4 hover:bg-slate-50 dark:hover:bg-slate-600 focus:ring-4 focus:outline-none focus:ring-primary-200 dark:focus:ring-primary-700 w-full text-left", autofocus: @autofocus %>
         <% end %>
       </td>
     ERB
 
-    def initialize(field:, sample:)
+    def initialize(field:, sample:, autofocus: false)
       @sample = sample
       @field = field
+      @autofocus = autofocus
     end
   end
 end
