a2f618a5cff52f4fc85faa927e94d1865b36043e
Meghan Chua <17057809+malchua@users.noreply.github.com>
2024-12-19 18:36:42 -0600

    Add tests to prefixed select2 component

--- test/components/viral/form/prefixed/select2_component_test.rb
+++ test/components/viral/form/prefixed/select2_component_test.rb
@@ -1,16 +1,32 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require 'application_system_test_case'
 
 module Viral
   module Form
     module Prefixed
-      class Select2ComponentTest < ViewComponent::TestCase
-        def test_component_renders_something_useful
-          # assert_equal(
-          #   %(<span>Hello, components!</span>),
-          #   render_inline(Viral::Form::Prefixed::Select2Component.new(message: "Hello, components!")).css("span").to_html
-          # )
+      class Select2ComponentTest < ApplicationSystemTestCase
+        test 'default' do
+          visit '/rails/view_components/viral_form_prefixed_select2_component/default'
+          assert_selector 'input[type="hidden"][name="user"]', visible: :hidden, count: 1
+          assert_no_selector 'input[type="submit"][disabled="disabled"]'
+
+          find('input#select2-input[type="text"]').click
+          assert_selector 'div[data-viral--select2-target="dropdown"]', visible: :visible
+          assert_selector 'ul[data-viral--select2-target="scroller"] li', count: 11
+
+          find('li button[data-viral--select2-primary-param="User 1"]').click
+          assert_selector 'input[type="hidden"][name="user"][value="1"]', visible: :hidden, count: 1
+          assert_no_selector 'input[type="submit"][disabled="disabled"]'
+          assert_selector 'input[type="submit"]', count: 1
+
+          find('input#select2-input[type="text"]').send_keys :backspace
+          assert_selector 'input[type="submit"][disabled="disabled"]', count: 1
+          find('input#select2-input[type="text"]').send_keys '22'
+          find('input#select2-input[type="text"]').send_keys :enter
+          assert_selector 'input[type="hidden"][name="user"][value="22"]', visible: :hidden, count: 1
+          assert_no_selector 'input[type="submit"][disabled="disabled"]'
+          assert_selector 'input[type="submit"]', count: 1
         end
       end
     end
