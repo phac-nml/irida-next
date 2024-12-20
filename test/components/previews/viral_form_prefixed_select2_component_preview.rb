3c81ab16b19dc97955f23d75e3a6e891be3e5b98
Meghan Chua <17057809+malchua@users.noreply.github.com>
2024-12-19 18:35:46 -0600

    Fix component preview

--- test/components/previews/viral_form_prefixed_select2_component_preview.rb
+++ test/components/previews/viral_form_prefixed_select2_component_preview.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class ViralFormPrefixedSelectComponentPreview < ViewComponent::Preview
+class ViralFormPrefixedSelect2ComponentPreview < ViewComponent::Preview
   def default; end
 end
