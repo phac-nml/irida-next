9aa4e6adaece6730f346ad22d0036da323211a34
Meghan Chua <17057809+malchua@users.noreply.github.com>
2024-12-31 17:08:12 -0600

    Fix component tests

--- test/components/viral/form/prefixed/select2_component_test.rb
+++ test/components/viral/form/prefixed/select2_component_test.rb
@@ -9,7 +9,6 @@ module Viral
         test 'default' do
           visit '/rails/view_components/viral_form_prefixed_select2_component/default'
           assert_selector 'input[type="hidden"][name="user"]', visible: :hidden, count: 1
-          assert_no_selector 'input[type="submit"][disabled="disabled"]'
 
           find('input#select2-input[type="text"]').click
           assert_selector 'div[data-viral--select2-target="dropdown"]', visible: :visible
@@ -17,16 +16,11 @@ module Viral
 
           find('li button[data-viral--select2-primary-param="User 1"]').click
           assert_selector 'input[type="hidden"][name="user"][value="1"]', visible: :hidden, count: 1
-          assert_no_selector 'input[type="submit"][disabled="disabled"]'
-          assert_selector 'input[type="submit"]', count: 1
 
           find('input#select2-input[type="text"]').send_keys :backspace
-          assert_selector 'input[type="submit"][disabled="disabled"]', count: 1
           find('input#select2-input[type="text"]').send_keys '22'
           find('input#select2-input[type="text"]').send_keys :enter
           assert_selector 'input[type="hidden"][name="user"][value="22"]', visible: :hidden, count: 1
-          assert_no_selector 'input[type="submit"][disabled="disabled"]'
-          assert_selector 'input[type="submit"]', count: 1
         end
       end
     end
