817a289e1fe928c81630a548bf9d3e73f6c1a09c
Chris Huynh <chris.huynh333@gmail.com>
2025-01-02 15:53:14 -0600

    move filtering to sample rather than samplesheet

--- app/controllers/file_selector_controller.rb
+++ app/controllers/file_selector_controller.rb
@@ -23,11 +23,16 @@ class FileSelectorController < ApplicationController
   private
 
   def file_selector_params
-    params.require(:file_selector).permit(:attachable, :index, :name, :selected_id, :required, files: [:filename, :global_id, :id])
+    params.require(:file_selector).permit(:attachable, :index, :name, :selected_id, :required, file_filter_params: [:pattern, :direction, :pe_only])
   end
 
   def listing_attachments
-    @listing_attachments = Attachment.where(id: file_selector_params[:files].map{|i| i[:id]})
+    filter_params = file_selector_params[:file_filter_params]
+    pattern = filter_params[:pattern] == "" ? nil : filter_params[:pattern]
+    direction = filter_params[:direction].to_sym
+    pe_only = filter_params[:pe_only] == "true"
+    files = @attachable.fastq_files(pattern, direction, pe_only)
+    @listing_attachments = Attachment.where(id: files.map{|i| i[:id]})
   end
 
   def attachable
@@ -41,7 +46,16 @@ class FileSelectorController < ApplicationController
     @attachment_params = {filename: attachment.filename.to_s, global_id: attachment.to_global_id, id: attachment.id}
     return unless attachment.associated_attachment && (file_selector_params[:name] == 'fastq_1' || file_selector_params[:name] == 'fastq_2')
 
+    @associated_attachment_params = {}
     associated_attachment = attachment.associated_attachment
-    @associated_attachment_params = {filename: associated_attachment.filename.to_s, global_id: associated_attachment.to_global_id, id: associated_attachment.id}
+
+    @associated_attachment_params[:file] = {filename: associated_attachment.filename.to_s, global_id: associated_attachment.to_global_id, id: associated_attachment.id}
+    @associated_attachment_params[:name] = file_selector_params["name"] == "fastq_1" ? "fastq_2" : "fastq_1"
+    @associated_attachment_params[:file_filter_params] = {
+      pattern: file_selector_params[:file_filter_params][:pattern],
+    direction:  file_selector_params[:file_filter_params][:direction] == "pe_forward" ? "pe_reverse" : "pe_forward",
+    pe_only: file_selector_params[:file_filter_params][:pe_only]
+    }
+
   end
 end
