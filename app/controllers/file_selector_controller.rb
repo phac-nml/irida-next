6f1a302c7285f48b67108460a8fc7aab924d27f6
Chris Huynh <chris.huynh333@gmail.com>
2025-01-02 13:40:44 -0600

    fix regex filtering, more cleanup

--- app/controllers/file_selector_controller.rb
+++ app/controllers/file_selector_controller.rb
@@ -2,9 +2,9 @@
 
 # Controller actions for Projects
 class FileSelectorController < ApplicationController
-  before_action :attachable, :index, :name, only: %i[new create]
-  before_action :selected, only: %i[new]
+  before_action :attachable, only: %i[new create]
   before_action :attachments, only: %i[create]
+  before_action :listing_attachments, only: %i[new create]
 
   def new
     render turbo_stream: turbo_stream.update('file_selector_dialog',
@@ -23,25 +23,15 @@ class FileSelectorController < ApplicationController
   private
 
   def file_selector_params
-    params.require(:file_selector).permit(:attachable, :index, :name, :selected_id, :required)
+    params.require(:file_selector).permit(:attachable, :index, :name, :selected_id, :required, files: [:filename, :global_id, :id])
+  end
+
+  def listing_attachments
+    @listing_attachments = Attachment.where(id: file_selector_params[:files].map{|i| i[:id]})
   end
 
   def attachable
     @attachable = Sample.find(file_selector_params[:attachable])
-  end
-
-  def index
-    @index = file_selector_params[:index]
-  end
-
-  def name
-    @name = file_selector_params[:name]
-  end
-
-  def selected
-    return if file_selector_params[:selected_id].nil?
-
-    @selected_id = file_selector_params[:selected_id]
   end
 
   def attachments
@@ -49,8 +39,8 @@ class FileSelectorController < ApplicationController
     return if params[:attachment_id] == 'no_attachment'
     attachment = Attachment.find(params[:attachment_id])
     @attachment_params = {filename: attachment.filename.to_s, global_id: attachment.to_global_id, id: attachment.id}
+    return unless attachment.associated_attachment && (file_selector_params[:name] == 'fastq_1' || file_selector_params[:name] == 'fastq_2')
 
-    return unless attachment.associated_attachment && (file_selector_params[:name] == 'fastq_1' || file_selector_params[:name == 'fastq_2'])
     associated_attachment = attachment.associated_attachment
     @associated_attachment_params = {filename: associated_attachment.filename.to_s, global_id: associated_attachment.to_global_id, id: associated_attachment.id}
   end
