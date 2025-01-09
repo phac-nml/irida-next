d8b4b3f4e93bb8d398edd41e066e08187c0d8ac5
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-01-09 09:20:53 -0600

    removing all previews for the advanced serch component except default

--- app/components/advanced_search_component.rb
+++ app/components/advanced_search_component.rb
@@ -2,10 +2,6 @@
 
 # View component for advanced search component
 class AdvancedSearchComponent < Component
-  renders_one :add_condition_button
-  renders_one :add_group_button
-  renders_one :remove_group_button
-
   def initialize(form:, search:, fields: [], operations: [], open: false, status: true) # rubocop:disable Metrics/ParameterLists
     @form = form
     @search = search
