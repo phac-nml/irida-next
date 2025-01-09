7bfd75acfbbd333553951abde52887cd0c04414c
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-01-09 16:08:36 -0600

    fix broken test

--- test/system/projects/samples_test.rb
+++ test/system/projects/samples_test.rb
@@ -2299,11 +2299,12 @@ module Projects
       visit namespace_project_samples_url(@namespace, @project)
       assert_selector 'table thead tr th', count: 6
 
-      find('label', text: I18n.t('projects.samples.shared.metadata_toggle.label')).click
-      assert_selector 'table thead tr th', count: 8
-
       fill_in placeholder: I18n.t(:'projects.samples.table_filter.search.placeholder'), with: @sample1.name
       find('input.t-search-component').native.send_keys(:return)
+
+      assert_selector 'label', text: I18n.t('projects.samples.shared.metadata_toggle.label'), count: 1
+      find('label', text: I18n.t('projects.samples.shared.metadata_toggle.label')).click
+      assert_selector 'table thead tr th', count: 8
 
       within 'div.overflow-auto.scrollbar' do |div|
         div.scroll_to div.find('table thead th:nth-child(7)')
@@ -2314,7 +2315,6 @@ module Projects
 
       within('table tbody tr:first-child td:nth-child(7)') do
         ### ACTIONS START ###
-        # check within the from with method get that the value is 'value1':
         within('form[method="get"]') do
           find('button').click
         end
