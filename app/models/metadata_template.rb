8d83e3cde3ad1f4554d8142c860e8640aa010f27
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-01-09 11:45:35 -0600

    feat(metadata): Add JSON schema validation for MetadataTemplate fields and create schema file

--- app/models/metadata_template.rb
+++ app/models/metadata_template.rb
@@ -3,6 +3,7 @@
 # MetadataTemplate represents a template for structured metadata fields
 class MetadataTemplate < ApplicationRecord
   include TrackActivity
+  METADATA_TEMPLATE_JSON_SCHEMA = Rails.root.join('config/schemas/metadata_template_metadata.json')
 
   has_logidze
   acts_as_paranoid
@@ -19,6 +20,7 @@ class MetadataTemplate < ApplicationRecord
   belongs_to :group, optional: true, foreign_key: :namespace_id # rubocop:disable Rails/InverseOf
   belongs_to :project_namespace, optional: true, foreign_key: :namespace_id, class_name: 'Namespaces::ProjectNamespace' # rubocop:disable Rails/InverseOf
 
+  validates :fields, presence: true, json: { message: ->(errors) { errors }, schema: METADATA_TEMPLATE_JSON_SCHEMA }
   validate :validate_namespace
 
   private
