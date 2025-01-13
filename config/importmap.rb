53a1edfec3cd1f158b3db22fa2d3413e1c8ad31d
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2025-01-13 13:43:03 -0600

    revert changes to validator and popperjs importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,14 +7,14 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin 'flowbite', to: 'https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.turbo.min.js'
-pin '@popperjs/core', to: '@popperjs--core.js' # @2.11.7
+pin '@popperjs/core', to: 'https://ga.jspm.io/npm:@popperjs/core@2.11.7/dist/esm/index.js'
 pin '@sindresorhus/slugify', to: '@sindresorhus--slugify.js' # @2.2.1
 pin '@sindresorhus/transliterate', to: '@sindresorhus--transliterate.js' # @1.6.0
 pin 'escape-string-regexp' # @5.0.0
 pin '@rails/activestorage', to: '@rails--activestorage.js' # @7.2.201
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'xlsx' # @0.18.5
-pin 'validator' # @13.12.0
+pin 'validator', to: 'https://ga.jspm.io/npm:validator@13.11.0/index.js'
 pin_all_from 'app/javascript/utilities', under: 'utilities'
 pin 'sortablejs' # @1.15.2
 pin 'local-time' # @3.0.2
