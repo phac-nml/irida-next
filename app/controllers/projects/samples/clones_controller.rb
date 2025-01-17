de84a7a6d3ec243f329e3a6c8dc0f55c4f1e1e68
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-01-17 14:01:28 -0600

    chore: remove unused  method

--- app/controllers/projects/samples/clones_controller.rb
+++ app/controllers/projects/samples/clones_controller.rb
@@ -28,17 +28,6 @@ module Projects
         params.require(:clone).permit(:new_project_id, sample_ids: [])
       end
 
-      def render_sample_errors
-        errors = @project.errors.messages_for(:sample)
-        if @cloned_sample_ids.count.positive?
-          render status: :partial_content,
-                 locals: { type: :alert, message: t('projects.samples.clones.create.error'), errors: }
-        else
-          render status: :unprocessable_entity,
-                 locals: { type: :alert, message: t('projects.samples.clones.create.error'), errors: }
-        end
-      end
-
       def projects
         @projects = authorized_scope(Project, type: :relation,
                                               as: :manageable).where.not(namespace_id: @project.namespace_id)
