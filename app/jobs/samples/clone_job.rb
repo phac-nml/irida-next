0f7598bd9ee1fecad2056fecf04803194d2a8a47
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-01-17 12:56:43 -0600

    chore: update samples cloning to be performed in the background and use turbo streams to refresh the ui once clone is complete

---
+++ app/jobs/samples/clone_job.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module Samples
+  # Job used to clone samples
+  class CloneJob < ApplicationJob
+    queue_as :default
+
+    def perform(project, current_user, new_project_id, sample_ids, broadcast_target)
+      @cloned_sample_ids = ::Samples::CloneService.new(project, current_user).execute(new_project_id, sample_ids)
+
+      if project.errors.empty?
+        Turbo::StreamsChannel.broadcast_replace_to broadcast_target,
+                                                   target: "clone_samples_dialog_content",
+                                                   partial: "projects/samples/shared/success",
+                                                   locals: {
+                                                      type: :success,
+                                                      message: I18n.t('projects.samples.clones.create.success'),
+                                                    }
+      elsif project.errors.include?(:sample)
+        errors = project.errors.messages_for(:sample)
+        Turbo::StreamsChannel.broadcast_replace_to broadcast_target,
+                                                   target: "clone_samples_dialog_content",
+                                                   partial: "projects/samples/shared/errors",
+                                                   locals: {
+                                                      type: :alert,
+                                                      message: I18n.t('projects.samples.clones.create.error'),
+                                                      errors: errors,
+                                                    }
+      else
+        errors = project.errors.full_messages_for(:base)
+        Turbo::StreamsChannel.broadcast_replace_to broadcast_target,
+                                                   target: "clone_samples_dialog_content",
+                                                   partial: "projects/samples/shared/errors",
+                                                   locals: {
+                                                      type: :alert,
+                                                      message: I18n.t('projects.samples.clones.create.no_samples_cloned_error'),
+                                                      errors: errors,
+                                                    }
+      end
+    end
+  end
+end
