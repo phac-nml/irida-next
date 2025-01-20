90416870fd29d4875fa6c242b400c3c74c703a32
Meghan Chua <17057809+malchua@users.noreply.github.com>
2025-01-20 07:59:16 -0600

    Add test for calling new workflow execution page

--- test/controllers/groups/workflow_executions_controller_test.rb
+++ test/controllers/groups/workflow_executions_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
-class Groups::WorkflowExecutionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+module Groups
+  class WorkflowExecutionsControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      sign_in users(:john_doe)
+      @sample1 = samples(:sample1)
+      @attachment1 = attachments(:attachment1)
+      @workflow_execution = workflow_executions(:automated_example_completed)
+      @namespace = groups(:group_one)
+      @project = projects(:project1)
+      @group = groups(:group_one)
+    end
+
+    test 'should show a listing of workflow executions for the project' do
+      get group_workflow_executions_path(@group)
+
+      assert_response :success
+    end
+  end
 end
