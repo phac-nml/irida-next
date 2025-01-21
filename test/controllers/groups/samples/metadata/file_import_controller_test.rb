e7d63e98af7b3a2acc4347963e8c73947928a921
Deep Sidhu <deepsidhu85@gmail.com>
2025-01-21 14:58:45 -0600

    Updated controller tests

--- test/controllers/groups/samples/metadata/file_import_controller_test.rb
+++ test/controllers/groups/samples/metadata/file_import_controller_test.rb
@@ -12,100 +12,16 @@ module Groups
           @csv = fixture_file_upload('test/fixtures/files/metadata/valid.csv')
         end
 
-        test 'import sample metadata with permission' do
-          post group_samples_file_import_path(@group, format: :turbo_stream),
-               params: {
-                 file_import: {
-                   file: @csv,
-                   sample_id_column: 'sample_name'
+        test 'should enqueue a Samples::MetadataImportJob' do
+          assert_enqueued_jobs 1, only: ::Samples::MetadataImportJob do
+            post group_samples_file_import_path(@group, format: :turbo_stream),
+                 params: {
+                   file_import: {
+                     file: @csv,
+                     sample_id_column: 'sample_name'
+                   }
                  }
-               }
-
-          assert_response :success
-        end
-
-        test 'import sample metadata without permission' do
-          login_as users(:micha_doe)
-
-          post group_samples_file_import_path(@group, format: :turbo_stream),
-               params: {
-                 file_import: {
-                   file: @csv,
-                   sample_id_column: 'sample_name'
-                 }
-               }
-
-          assert_response :success
-        end
-
-        # test 'import sample metadata with no file' do
-        #   post group_samples_file_import_path(@group, format: :turbo_stream),
-        #        params: {
-        #          file_import: { sample_id_column: 'sample_name' }
-        #        }
-        #   assert_response :unprocessable_entity
-        # end
-
-        test 'import sample metadata with no sample_id_column' do
-          csv = fixture_file_upload('test/fixtures/files/metadata/missing_sample_id_column.csv')
-          post group_samples_file_import_path(@group, format: :turbo_stream),
-               params: {
-                 file_import: { file: csv, sample_id_column: 'sample_name' }
-               }
-          assert_response :success
-        end
-
-        test 'import sample metadata with duplicate column names' do
-          csv = fixture_file_upload('test/fixtures/files/metadata/duplicate_headers.csv')
-          post group_samples_file_import_path(@group, format: :turbo_stream),
-               params: {
-                 file_import: { file: csv, sample_id_column: 'sample_name' }
-               }
-          assert_response :success
-        end
-
-        test 'import sample metadata with no metadata columns' do
-          csv = fixture_file_upload('test/fixtures/files/metadata/missing_metadata_columns.csv')
-          post group_samples_file_import_path(@group, format: :turbo_stream),
-               params: {
-                 file_import: { file: csv, sample_id_column: 'sample_name' }
-               }
-          assert_response :success
-        end
-
-        test 'import sample metadata with no metadata rows' do
-          csv = fixture_file_upload('test/fixtures/files/metadata/missing_metadata_rows.csv')
-          post group_samples_file_import_path(@group, format: :turbo_stream),
-               params: {
-                 file_import: { file: csv, sample_id_column: 'sample_name' }
-               }
-          assert_response :success
-        end
-
-        test 'import sample metadata with invalid file' do
-          other = fixture_file_upload('test/fixtures/files/metadata/invalid.txt')
-          post group_samples_file_import_path(@group, format: :turbo_stream),
-               params: {
-                 file_import: {
-                   file: other,
-                   sample_id_column: 'sample_name'
-                 }
-               }
-
-          assert_response :success
-        end
-
-        test 'import sample metadata with a sample that does not belong to group' do
-          csv = fixture_file_upload('test/fixtures/files/metadata/mixed_project_samples.csv')
-          post group_samples_file_import_path(@group, format: :turbo_stream),
-               params: {
-                 file_import: {
-                   file: csv,
-                   sample_id_column: 'sample_name'
-                 }
-               }
-
-          assert_response :success
+          end
         end
       end
     end
--- test/controllers/projects/samples/metadata/file_import_controller_test.rb
+++ test/controllers/projects/samples/metadata/file_import_controller_test.rb
@@ -13,100 +13,16 @@ module Projects
           @csv = fixture_file_upload('test/fixtures/files/metadata/valid.csv')
         end
 
-        test 'import sample metadata with permission' do
-          post namespace_project_samples_file_import_path(@namespace, @project, format: :turbo_stream),
-               params: {
-                 file_import: {
-                   file: @csv,
-                   sample_id_column: 'sample_name'
+        test 'should enqueue a Samples::MetadataImportJob' do
+          assert_enqueued_jobs 1, only: ::Samples::MetadataImportJob do
+            post namespace_project_samples_file_import_path(@namespace, @project, format: :turbo_stream),
+                 params: {
+                   file_import: {
+                     file: @csv,
+                     sample_id_column: 'sample_name'
+                   }
                  }
-               }
-
-          assert_response :success
-        end
-
-        test 'import sample metadata without permission' do
-          login_as users(:micha_doe)
-
-          post namespace_project_samples_file_import_path(@namespace, @project, format: :turbo_stream),
-               params: {
-                 file_import: {
-                   file: @csv,
-                   sample_id_column: 'sample_name'
-                 }
-               }
-
-          assert_response :success
-        end
-
-        # test 'import sample metadata with no file' do
-        #   post namespace_project_samples_file_import_path(@namespace, @project, format: :turbo_stream),
-        #        params: {
-        #          file_import: { sample_id_column: 'sample_name' }
-        #        }
-        #   assert_response :unprocessable_entity
-        # end
-
-        test 'import sample metadata with no sample_id_column' do
-          csv = fixture_file_upload('test/fixtures/files/metadata/missing_sample_id_column.csv')
-          post namespace_project_samples_file_import_path(@namespace, @project, format: :turbo_stream),
-               params: {
-                 file_import: { file: csv, sample_id_column: 'sample_name' }
-               }
-          assert_response :success
-        end
-
-        test 'import sample metadata with duplicate column names' do
-          csv = fixture_file_upload('test/fixtures/files/metadata/duplicate_headers.csv')
-          post namespace_project_samples_file_import_path(@namespace, @project, format: :turbo_stream),
-               params: {
-                 file_import: { file: csv, sample_id_column: 'sample_name' }
-               }
-          assert_response :success
-        end
-
-        test 'import sample metadata with no metadata columns' do
-          csv = fixture_file_upload('test/fixtures/files/metadata/missing_metadata_columns.csv')
-          post namespace_project_samples_file_import_path(@namespace, @project, format: :turbo_stream),
-               params: {
-                 file_import: { file: csv, sample_id_column: 'sample_name' }
-               }
-          assert_response :success
-        end
-
-        test 'import sample metadata with no metadata rows' do
-          csv = fixture_file_upload('test/fixtures/files/metadata/missing_metadata_rows.csv')
-          post namespace_project_samples_file_import_path(@namespace, @project, format: :turbo_stream),
-               params: {
-                 file_import: { file: csv, sample_id_column: 'sample_name' }
-               }
-          assert_response :success
-        end
-
-        test 'import sample metadata with invalid file' do
-          other = fixture_file_upload('test/fixtures/files/metadata/invalid.txt')
-          post namespace_project_samples_file_import_path(@namespace, @project, format: :turbo_stream),
-               params: {
-                 file_import: {
-                   file: other,
-                   sample_id_column: 'sample_name'
-                 }
-               }
-
-          assert_response :success
-        end
-
-        test 'import sample metadata with a sample that does not belong to project' do
-          csv = fixture_file_upload('test/fixtures/files/metadata/mixed_project_samples.csv')
-          post namespace_project_samples_file_import_path(@namespace, @project, format: :turbo_stream),
-               params: {
-                 file_import: {
-                   file: csv,
-                   sample_id_column: 'sample_name'
-                 }
-               }
-
-          assert_response :success
+          end
         end
       end
     end
