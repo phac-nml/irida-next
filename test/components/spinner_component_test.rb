d3d3ecb72e3b18689c629f40b2a1d5a6cf6cb986
Deep Sidhu <deepsidhu85@gmail.com>
2025-01-22 14:54:17 -0600

    Added spinner view component test

---
+++ test/components/spinner_component_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'view_component_test_case'
+
+class SpinnerComponentTest < ViewComponentTestCase
+  test 'default' do
+    render_inline SpinnerComponent.new(message: 'Still loading..please wait...')
+    assert_selector 'div[id="spinner"]', count: 1
+    assert_text 'Still loading..please wait...'
+  end
+end
