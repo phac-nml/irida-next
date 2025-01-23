63c186b3fba39cbf2614d0f570182efa1c616af9
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-01-23 12:18:36 -0600

    feat: Add template_name to activity components and refine metadata template action types
    - Introduced `template_name` to both group and project activity components for enhanced context.
    - Refined `metadata_template_link` method in both components to exclude the `metadata_template_destroy` action, simplifying the action types handled.
    - Removed the `template_id` parameter from the destroy service to streamline the metadata template destruction process.

--- app/components/activities/group_activity_component.rb
+++ app/components/activities/group_activity_component.rb
@@ -21,7 +21,7 @@ module Activities
     end
 
     def metadata_template_link
-      metadata_template_action_types = %w[metadata_template_create metadata_template_update metadata_template_destroy]
+      metadata_template_action_types = %w[metadata_template_create metadata_template_update]
       metadata_template_action_types.include?(@activity[:action])
     end
   end
