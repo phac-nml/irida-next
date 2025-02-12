e3421be1e553e53782fb48e9f392c74866a9cad3
Chris Huynh <chris.huynh333@gmail.com>
2025-02-12 14:09:58 -0600

    add fix to nextflow metadata fields, associated test

--- test/controllers/workflow_executions/submission_controller_test.rb
+++ test/controllers/workflow_executions/submission_controller_test.rb
@@ -12,5 +12,16 @@ module WorkflowExecutions
       get pipeline_selection_workflow_executions_submissions_path(format: :turbo_stream)
       assert_response :ok
     end
+
+    test 'create submission' do
+      sign_in users(:john_doe)
+      project1 = projects(:project1)
+      sample1 = samples(:sample1)
+      post workflow_executions_submissions_path(namespace_id: project1.namespace.id,
+                                                workflow_name: 'phac-nml/iridanextexample',
+                                                workflow_version: '1.0.3',
+                                                samples: [sample1.id], format: :turbo_stream)
+      assert_response :ok
+    end
   end
 end
