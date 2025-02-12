75756c4d66573f185c2001dcde6cd371f179a3df
Meghan Chua <17057809+malchua@users.noreply.github.com>
2025-02-12 13:52:42 -0600

    Add groups_workflow_executions_path to routes

--- config/routes/group.rb
+++ config/routes/group.rb
@@ -27,6 +27,17 @@ constraints(::Constraints::GroupUrlConstrainer.new) do
       end
     end
 
+    if Irida::Pipelines.instance.available_pipelines.any?
+      resources :workflow_executions, only: %i[index] do
+        member do
+          put :cancel
+        end
+        collection do
+          get :select
+        end
+      end
+    end
+
     resources :attachments, only: %i[create destroy index new] do
       get :new_destroy
     end
