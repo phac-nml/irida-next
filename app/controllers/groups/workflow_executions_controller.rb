55f1727d7491ef7391764d62b0e24b3984a8d5f0
Meghan Chua <17057809+malchua@users.noreply.github.com>
2025-02-12 13:49:24 -0600

    Create groups workflow executions page

---
+++ app/controllers/groups/workflow_executions_controller.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module Groups
+  # Workflow executions controller for groups
+  class WorkflowExecutionsController < Groups::ApplicationController
+    include BreadcrumbNavigation
+    include Metadata
+    include WorkflowExecutionActions
+
+    before_action :group
+    before_action :namespace
+
+    private
+
+    def group
+      @group ||= Group.find_by_full_path(request.params[:group_id] || request.params[:id]) # rubocop:disable Rails/DynamicFindBy
+    end
+
+    def namespace
+      @namespace = @group
+    end
+
+    def load_workflows
+      authorized_scope(WorkflowExecution, type: :relation, as: :group_shared,
+                                          scope_options: { group: @group })
+    end
+
+    def current_page
+      @current_page = t(:'general.default_sidebar.workflows')
+    end
+
+    def context_crumbs
+      super
+      @context_crumbs +=
+        [{
+          name: I18n.t('groups.workflow_executions.index.title'),
+          path: group_workflow_executions_path
+        }]
+      return unless action_name == 'show' && !@workflow_execution.nil?
+
+      @context_crumbs +=
+        [{
+          name: @workflow_execution.id,
+          path: group_workflow_executions_path(@workflow_execution)
+        }]
+    end
+
+    protected
+
+    def layout_fixed
+      @fixed = false
+    end
+
+    def redirect_path
+      group_workflow_executions_path
+    end
+  end
+end
