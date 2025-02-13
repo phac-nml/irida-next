5c7825f3016dde926315e0ce1d02c3a2adfdfec3
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-02-13 10:22:27 -0600

    Advanced Search Close better Dirty Check (#945)
    * chore: update close in advanced search to check if the form is dirty e.g. modified before confirming close
    
    * chore: change dirty method to be private and add in a test case where just the field was changed

--- test/components/advanced_search_component_test.rb
+++ test/components/advanced_search_component_test.rb
@@ -125,6 +125,49 @@ class AdvancedSearchComponentTest < ApplicationSystemTestCase
         assert_includes text, I18n.t(:'advanced_search_component.confirm_close_text')
         assert_no_selector 'h1', text: I18n.t(:'advanced_search_component.title')
       end
+
+      click_button I18n.t(:'advanced_search_component.title')
+      within 'dialog' do
+        # verify accessibility
+        assert_accessible
+
+        # verify the form is pre-populated
+        assert_selector 'h1', text: I18n.t(:'advanced_search_component.title')
+        assert_selector "div[data-advanced-search-target='groupsContainer']", count: 1
+        within all("div[data-advanced-search-target='groupsContainer']")[0] do
+          assert_selector "div[data-advanced-search-target='conditionsContainer']", count: 1
+        end
+
+        # verify the dialog has a close button
+        assert_selector ".dialog--header button[aria-label='#{I18n.t('components.dialog.close')}']"
+
+        # select a value for field
+        within all("div[data-advanced-search-target='groupsContainer']")[0] do
+          within all("div[data-advanced-search-target='conditionsContainer']")[0] do
+            find("select[name$='[field]']").find("option[value='metadata.age']").select_option
+          end
+        end
+
+        # verify that the dialog close action prompts a confirm dialog if unapplied filters
+        text = dismiss_confirm do
+          click_button I18n.t('components.dialog.close')
+        end
+        assert_includes text, I18n.t(:'advanced_search_component.confirm_close_text')
+
+        # verify that dismissing the confirm keeps the unapplied filters and dialog open
+        within all("div[data-advanced-search-target='groupsContainer']")[0] do
+          within all("div[data-advanced-search-target='conditionsContainer']")[0] do
+            assert_equal 'metadata.age', find("select[name$='[field]']").find("option[value='metadata.age']").value
+          end
+        end
+
+        # verify that accepting the confirm discards the unapplied filters and closes the dialog
+        text = accept_confirm do
+          click_button I18n.t('components.dialog.close')
+        end
+        assert_includes text, I18n.t(:'advanced_search_component.confirm_close_text')
+        assert_no_selector 'h1', text: I18n.t(:'advanced_search_component.title')
+      end
     end
   end
 end
