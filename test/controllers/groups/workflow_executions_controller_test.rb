8890dd48f592b84d1f585cf64de65a862a46b31e
Meghan Chua <17057809+malchua@users.noreply.github.com>
2025-02-13 22:16:32 -0600

    Add groups workflow execution show page tests

--- test/controllers/groups/workflow_executions_controller_test.rb
+++ test/controllers/groups/workflow_executions_controller_test.rb
@@ -7,6 +7,7 @@ module Groups
     setup do
       sign_in users(:john_doe)
       @group = groups(:group_one)
+      @workflow_execution = workflow_executions(:workflow_execution_group_shared1)
     end
 
     test 'should show a listing of workflow executions for the group' do
@@ -27,6 +28,44 @@ module Groups
       sign_in users(:ryan_doe)
 
       get group_workflow_executions_path(@group)
+
+      assert_response :unauthorized
+    end
+
+    test 'should show workflow execution that was shared to group by the user' do
+      get group_workflow_execution_path(@group, @workflow_execution)
+
+      assert_response :success
+    end
+
+    test 'should show workflow execution that shared to group but not by the user' do
+      workflow_execution = workflow_executions(:workflow_execution_group_shared2)
+
+      get group_workflow_execution_path(@group, workflow_execution)
+
+      assert_response :success
+    end
+
+    test 'should not show shared workflow execution for user with incorrect permissions' do
+      sign_in users(:micha_doe)
+
+      get group_workflow_execution_path(@group, @workflow_execution)
+
+      assert_response :unauthorized
+    end
+
+    test 'should not show workflow execution that is not shared' do
+      workflow_execution = workflow_executions(:workflow_execution_valid)
+
+      get group_workflow_execution_path(@group, workflow_execution)
+
+      assert_response :not_found
+    end
+
+    test 'should not show project workflow execution for guests' do
+      sign_in users(:ryan_doe)
+
+      get group_workflow_execution_path(@group, @workflow_execution)
 
       assert_response :unauthorized
     end
