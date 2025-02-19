1f75c3c18d52e3ed8babad2e3f2e528825a2b6ed
Chris Huynh <chris.huynh333@gmail.com>
2025-02-19 10:33:53 -0600

    some cleanup

--- app/javascript/controllers/progress_bar_controller.js
+++ app/javascript/controllers/progress_bar_controller.js
@@ -1,6 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="progress-bar"
+// A progress bar will accept itemsToComplete to calculate the ongoing percentage of completed tasks (ie: samples to copy)
+// Each time a task is completed (one sample is copied), an empty div will be appended and observed via the MutationObserver,
+// and completedCount will be incremented by 1. This then updates the progress bar and percentage text.
+// eg: if there are 100 samples to copy, itemsToComplete = 100, and we'll observe 100 empty divs appended, each increasing
+// completedCount by 1 and increasing the progress vars.
 export default class extends Controller {
   static values = {
     itemsToComplete: 0,
@@ -26,6 +30,7 @@ export default class extends Controller {
 
   updateProgress() {
     let progress = (this.completedCountValue / this.itemsToCompleteValue) * 100;
+    // avoids rounding errors
     if (progress > 100) {
       progress = 100;
     }
