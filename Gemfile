c571c3632ce0fc0f209f6edbafb860d00b7dd27a
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-02-20 07:35:36 -0600

    chore: upgrade rails to 8.0.1

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.3.7'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem 'rails', '~> 7.2.0'
+gem 'rails', '~> 8.0.1'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem 'sprockets-rails'
--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Irida
   # main application class
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
