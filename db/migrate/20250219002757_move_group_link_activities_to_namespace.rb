da46c92acf3cca7b4da25800bc5f5df5e39050da
Deep Sidhu <deepsidhu85@gmail.com>
2025-02-20 18:07:19 -0600

    Updated migration to add group level activity for namespace group links

--- db/migrate/20250219002757_move_group_link_activities_to_namespace.rb
+++ db/migrate/20250219002757_move_group_link_activities_to_namespace.rb
@@ -6,7 +6,7 @@ class MoveGroupLinkActivitiesToNamespace < ActiveRecord::Migration[7.2]
     activity_keys = %w[namespace_group_link.create namespace_group_link.destroy namespace_group_link.update]
     activities = PublicActivity::Activity.where(key: activity_keys)
 
-    activities.each do |activity|
+    activities.each do |activity| # rubocop:disable Metrics/BlockLength
       group_link = NamespaceGroupLink.with_deleted.find_by(id: activity.trackable_id)
 
       next if group_link.nil?
@@ -41,6 +41,34 @@ class MoveGroupLinkActivitiesToNamespace < ActiveRecord::Migration[7.2]
 
       activity.parameters[:action] = action
       activity.save!
+
+      activity_key = if activity.parameters[:action] == 'group_link_create'
+                       'group.namespace_group_link.created'
+                     elsif activity.parameters[:action] == 'group_link_destroy'
+                       'group.namespace_group_link.destroyed'
+                     else
+                       'group.namespace_group_link.updated'
+                     end
+
+      action_type = if activity.parameters[:action] == 'group_link_create'
+                      'group_link_created'
+                    elsif activity.parameters[:action] == 'group_link_destroyed'
+                      'group_link_destroyed'
+                    else
+                      'group_link_updated'
+                    end
+
+      # Create group link activity in the group activities
+      PublicActivity::Activity.create!(trackable_type: 'Namespace', trackable_id: group_link.group.id,
+                                       owner_type: 'User', owner_id: activity.owner_id, key: activity_key,
+                                       parameters: {
+                                         group_name: activity.parameters[:group_name],
+                                         group_puid: activity.parameters[:group_puid],
+                                         namespace_name: activity.parameters[:namespace_name],
+                                         namespace_puid: activity.parameters[:namespace_puid],
+                                         namespace_type: activity.parameters[:namespace_type],
+                                         action: action_type
+                                       })
     end
   end
 end
