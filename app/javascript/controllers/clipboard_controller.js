ff68eb51f9f21fe544a82c264b5c8edebdf06d6a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-02-24 13:44:17 -0600

    revert: Should not have made changes to this file

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,24 +1,25 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["source", "buttonLabel", "successIcon"];
-  static classes = ["copied"];
+  static targets = ["button", "content"];
+  #tooltip;
 
-  async copy() {
-    try {
-      const content = this.sourceTarget.textContent.trim();
-      await navigator.clipboard.writeText(content);
+  connect() {
+    this.#tooltip = new Tooltip(this.contentTarget, this.buttonTarget, {
+      placement: "top",
+      triggerType: "none",
+    });
+  }
 
-      // Visual feedback
-      this.successIconTarget.classList.remove("hidden");
-      this.buttonLabelTarget.classList.add("sr-only");
-      // Reset after 2 seconds
-      setTimeout(() => {
-        this.successIconTarget.classList.add("hidden");
-        this.buttonLabelTarget.classList.remove("sr-only");
-      }, 2000);
-    } catch (err) {
-      console.error("Failed to copy text:", err);
-    }
+  copy(e) {
+    e.stopImmediatePropagation();
+    navigator.clipboard.writeText(e.target.value).then(this.#notify.bind(this));
+  }
+
+  #notify() {
+    this.#tooltip.show();
+    setTimeout(() => {
+      this.#tooltip.hide();
+    }, 1000);
   }
 }
