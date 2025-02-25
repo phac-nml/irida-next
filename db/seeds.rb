fad2574a8ab537eab6e875367ce2091b5c82c212
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-02-25 14:25:29 -0600

    refactor: Optimize file preview rendering by caching parsed contents for CSV, JSON, text, and TSV formats

--- db/seeds.rb
+++ db/seeds.rb
@@ -230,6 +230,7 @@ def seed_workflow_executions # rubocop:disable Metrics/MethodLength, Metrics/Abc
   attachment = workflow_execution_completed.outputs.build
   attachment.file.attach(io: Rails.root.join('test/fixtures/files/blob_outputs/normal', csv_file_name).open,
                          filename: csv_file_name)
+  attachment.save!
 
   tsv_file_name = 'testTSV.tsv'
   attachment = workflow_execution_completed.outputs.build
