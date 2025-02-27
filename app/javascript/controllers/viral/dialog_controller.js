18c5ce3fd330a0d66bffc917d8aa5dd11018e133
Chris Huynh <chris.huynh333@gmail.com>
2025-02-26 18:01:34 -0600

    remove missed code thats no longer needed

--- app/javascript/controllers/viral/dialog_controller.js
+++ app/javascript/controllers/viral/dialog_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["dialog", "close"];
+  static targets = ["dialog"];
 
   static values = { open: Boolean };
 
