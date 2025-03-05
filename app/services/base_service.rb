3d4dd8d36c4152e65656612ff5be10109112feb7
Chris Huynh <chris.huynh333@gmail.com>
2025-03-05 10:34:27 -0600

    change partial to accept percentage; change broadcast to pass partial

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -16,23 +16,12 @@ class BaseService
 
   private
 
-  def update_progress_bar(percentage, broadcast_target) # rubocop:disable Metrics/MethodLength
+  def update_progress_bar(percentage, broadcast_target)
     return unless broadcast_target
 
-    Turbo::StreamsChannel.broadcast_action_to(
-      broadcast_target,
-      action: 'replace',
-      target: 'progress-bar',
-      content: "<div id='progress-bar'>" \
-               "<div class='flex justify-between mb-1'>" \
-               "<span class='text-base font-medium text-slate-900 dark:text-white'>" \
-               "#{I18n.t('shared.progress_bar.in_progress')}</span>" \
-               "<span class='text-sm font-medium text-slate-900 dark:text-white'>#{percentage.ceil}%</span>" \
-               '</div>' \
-               "<div class='w-full bg-slate-200 rounded-full h-2.5 dark:bg-slate-700'>" \
-               "<div class='bg-primary-600 h-2.5 rounded-full' style='width: #{percentage}%' role='progressbar'></div>" \
-               '</div>' \
-               '</div>'
-    )
+    Turbo::StreamsChannel.broadcast_replace_to broadcast_target,
+                                               partial: 'shared/progress_bar',
+                                               locals: { percentage: },
+                                               target: 'progress-bar'
   end
 end
