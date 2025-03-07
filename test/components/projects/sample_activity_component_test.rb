19ffcb480c2f86fac7dc9682a6677dcd31738d6c
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2025-03-07 16:03:01 -0600

    add tests

--- test/components/projects/sample_activity_component_test.rb
+++ test/components/projects/sample_activity_component_test.rb
@@ -250,5 +250,28 @@ module Projects
       assert_selector 'a[disabled="disabled"]',
                       text: sample.puid
     end
+
+    test 'batch sample import actvity' do
+      project_namespace = namespaces_project_namespaces(:project1_namespace)
+      activities = project_namespace.human_readable_activity(project_namespace.retrieve_project_activity).reverse
+
+      assert_equal(1, activities.count do |activity|
+        activity[:key].include?('project_namespace.import_samples.create')
+      end)
+
+      activity_to_render = activities.find do |a|
+        a[:key] == 'activity.namespaces_project_namespace.import_samples.create_html'
+      end
+
+      render_inline Activities::Projects::SampleActivityComponent.new(activity: activity_to_render)
+
+      assert_text strip_tags(
+        I18n.t('activity.namespaces_project_namespace.import_samples.create_html',
+               user: 'System',
+               href: 2)
+      )
+      assert_selector 'a',
+                      text: 2
+    end
   end
 end
