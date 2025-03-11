ddfa0b65d70aed3f6b50ef801363bea5e6f44183
Chris Huynh <chris.huynh333@gmail.com>
2025-03-11 13:43:45 -0500

    start dialog and listing

--- app/controllers/concerns/list_actions.rb
+++ app/controllers/concerns/list_actions.rb
@@ -5,11 +5,17 @@ module ListActions
   extend ActiveSupport::Concern
 
   def list
+    puts params
+    puts 'hihihihiihi'
     @page = params[:page].to_i
     if params[:list_class] == 'sample'
       @samples = Sample.where(id: params[:sample_ids])
     elsif params[:list_class] == 'workflow_execution'
+      if params[:list_action] == 'destroy'
+        set_workflows_for_destroy
+      else
       @workflow_executions = WorkflowExecution.where(id: params[:workflow_execution_ids])
+      end
     end
 
     respond_to do |format|
@@ -18,4 +24,20 @@ module ListActions
       end
     end
   end
+
+  private
+
+  def set_workflows_for_destroy
+    @deletable_workflow_executions = []
+    @non_deletable_workflow_executions = []
+    workflow_executions = WorkflowExecution.where(id: params[:workflow_execution_ids])
+    workflow_executions.each do |workflow_execution|
+      if workflow_execution.deletable?
+        @deletable_workflow_executions << workflow_execution
+      else
+        @non_deletable_workflow_executions << workflow_execution
+      end
+    end
+  end
+
 end
