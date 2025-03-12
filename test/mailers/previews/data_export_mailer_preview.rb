43b770acf03cd16df1f7a50ebc79343b805a3f29
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-03-12 13:03:20 -0500

    chore: refactored and formatted email templates and layouts to pass erb-formatter formatting

--- test/mailers/previews/data_export_mailer_preview.rb
+++ test/mailers/previews/data_export_mailer_preview.rb
@@ -2,7 +2,7 @@
 
 class DataExportMailerPreview < ActionMailer::Preview
   def export_ready_with_name
-    data_export = DataExport.find_by(name: 'Seeded export 4')
+    data_export = DataExport.find_by(name: 'Seeded sample export 4')
     DataExportMailer.export_ready(data_export)
   end
 
