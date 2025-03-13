7d94f49480eecd5bcbe4bb3cd76805822d92cec3
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-03-13 10:35:38 -0500

    Cleanup: Email templates and layouts (#982)
    * chore: refactored and formatted email templates and layouts to pass erb-formatter formatting
    
    * chore: fix pipeline complete manager email
    
    * chore: switch to span tags instead of p tags so that rendering is consistent between email clients, and re add padding
    
    * chore: fix db/seeds so that data_export_mailer/export_ready_without_name preview renders correctly
    
    * chore: update export_ready_without_name preview, so use an export that does not have a name
    
    * chore: adjust spacing of header and footer
    
    * chore: remove nbsp character to fix rendering issue when opening email in edge from outlook

--- test/mailers/previews/data_export_mailer_preview.rb
+++ test/mailers/previews/data_export_mailer_preview.rb
@@ -2,12 +2,12 @@
 
 class DataExportMailerPreview < ActionMailer::Preview
   def export_ready_with_name
-    data_export = DataExport.find_by(name: 'Seeded export 4')
+    data_export = DataExport.find_by(name: 'Seeded sample export 4')
     DataExportMailer.export_ready(data_export)
   end
 
   def export_ready_without_name
-    data_export = DataExport.second
+    data_export = DataExport.where(status: 'ready', name: nil).first
     DataExportMailer.export_ready(data_export)
   end
 end
