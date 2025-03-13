e02904c623bffd6fbdb2675a5a4791fc0313cb49
Chris Huynh <chris.huynh333@gmail.com>
2025-03-13 18:00:25 -0500

    add more tests

--- test/system/workflow_executions_test.rb
+++ test/system/workflow_executions_test.rb
@@ -586,6 +586,7 @@ class WorkflowExecutionsTest < ApplicationSystemTestCase
   end
 
   test 'cannot delete non-deletable workflows' do
+    # attempt to destroy deletable and non-deletable workflows
     workflow_execution1 = workflow_executions(:irida_next_example_completed_unclean)
     visit workflow_executions_path
 
