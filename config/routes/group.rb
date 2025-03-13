8a2fd11592120ceffebfd10fa362f8599e786654
Meghan Chua <17057809+malchua@users.noreply.github.com>
2025-03-13 10:10:11 -0500

    Add destroy functionality

--- config/routes/group.rb
+++ config/routes/group.rb
@@ -28,7 +28,7 @@ constraints(::Constraints::GroupUrlConstrainer.new) do
     end
 
     if Irida::Pipelines.instance.available_pipelines.any?
-      resources :workflow_executions, only: %i[index show edit update] do
+      resources :workflow_executions, only: %i[index destroy show edit update] do
         member do
           put :cancel
         end
