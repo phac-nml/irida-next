bf369807bcb66dbad33178a58b7b46704f93e4bd
Chris Huynh <chris.huynh333@gmail.com>
2025-03-13 12:22:11 -0500

    complete logic flow for multi workflow deletion; add multi workflow deletion to projects

--- app/services/workflow_executions/destroy_service.rb
+++ app/services/workflow_executions/destroy_service.rb
@@ -3,17 +3,33 @@
 module WorkflowExecutions
   # Service used to delete a WorkflowExecution
   class DestroyService < BaseService
-    def initialize(workflow_execution, user = nil, params = {})
+    def initialize(user = nil, params = {})
       super(user, params)
-      @workflow_execution = workflow_execution
+      @workflow_execution = params[:workflow_execution] if params[:workflow_execution]
+      @workflow_execution_ids = params[:workflow_execution_ids] if params[:workflow_execution_ids]
     end
 
     def execute
+      @workflow_execution.nil? ? destroy_multiple : destroy_single
+    end
+
+    private
+
+    def destroy_single
       return unless @workflow_execution.deletable?
 
       authorize! @workflow_execution, to: :destroy?
 
       @workflow_execution.destroy
+    end
+
+    def destroy_multiple
+      workflows = WorkflowExecution.where(id: @workflow_execution_ids, state: ['completed', 'canceled', 'error'], cleaned: true)
+      workflows_to_delete_count = workflows.count
+
+      workflows.destroy_all
+
+      workflows_to_delete_count
     end
   end
 end
