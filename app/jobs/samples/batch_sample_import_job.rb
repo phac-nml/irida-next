56dc91724ec75854de56f0c9b9906439346b28c5
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2025-03-14 17:51:44 -0500

    happy path working with turbo streams

--- app/jobs/samples/batch_sample_import_job.rb
+++ app/jobs/samples/batch_sample_import_job.rb
@@ -5,47 +5,46 @@ module Samples
   class BatchSampleImportJob < ApplicationJob
     queue_as :default
 
-    def perform(namespace, current_user, broadcast_target, blob_id, params) # rubocop:disable Lint/UnusedMethodArgument
+    def perform(namespace, current_user, broadcast_target, blob_id, params) # rubocop:disable Metrics/MethodLength
       ::Samples::BatchFileImportService.new(namespace, current_user, blob_id, params).execute
 
-      # TODO: change all these from metadata to batch sample
-      # if namespace.errors.empty?
-      #   Turbo::StreamsChannel.broadcast_replace_to(
-      #     broadcast_target,
-      #     target: 'import_metadata_dialog_content',
-      #     partial: 'shared/samples/metadata/file_imports/success',
-      #     locals: {
-      #       type: :success,
-      #       message: I18n.t('shared.samples.metadata.file_imports.success.description')
-      #     }
-      #   )
+      if namespace.errors.empty?
+        Turbo::StreamsChannel.broadcast_replace_to(
+          broadcast_target,
+          target: 'import_spreadsheet_dialog_content',
+          partial: 'shared/samples/spreadsheet_imports/success',
+          locals: {
+            type: :success,
+            message: I18n.t('shared.samples.spreadsheet_imports.success.description')
+          }
+        )
 
-      # elsif namespace.errors.include?(:sample)
-      #   errors = namespace.errors.messages_for(:sample)
+      elsif namespace.errors.include?(:sample)
+        errors = namespace.errors.messages_for(:sample)
 
-      #   Turbo::StreamsChannel.broadcast_replace_to(
-      #     broadcast_target,
-      #     target: 'import_metadata_dialog_content',
-      #     partial: 'shared/samples/metadata/file_imports/errors',
-      #     locals: {
-      #       type: :alert,
-      #       message: I18n.t('shared.samples.metadata.file_imports.errors.description'),
-      #       errors: errors
-      #     }
-      #   )
-      # else
-      #   errors = namespace.errors.full_messages_for(:base)
+        Turbo::StreamsChannel.broadcast_replace_to(
+          broadcast_target,
+          target: 'import_spreadsheet_dialog_content',
+          partial: 'shared/samples/spreadsheet_imports/errors',
+          locals: {
+            type: :alert,
+            message: I18n.t('shared.samples.spreadsheet_imports.errors.description'),
+            errors: errors
+          }
+        )
+      else
+        errors = namespace.errors.full_messages_for(:base)
 
-      #   Turbo::StreamsChannel.broadcast_replace_to(
-      #     broadcast_target,
-      #     target: 'import_metadata_dialog_content',
-      #     partial: 'shared/samples/metadata/file_imports/errors',
-      #     locals: {
-      #       type: :alert,
-      #       errors: errors
-      #     }
-      #   )
-      # end
+        Turbo::StreamsChannel.broadcast_replace_to(
+          broadcast_target,
+          target: 'import_spreadsheet_dialog_content',
+          partial: 'shared/samples/spreadsheet_imports/errors',
+          locals: {
+            type: :alert,
+            errors: errors
+          }
+        )
+      end
     end
   end
 end
