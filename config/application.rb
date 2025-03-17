84b2480d6d41e69ee89aefd67667ba762822d61c
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-03-17 14:18:38 -0500

    chore: move i18n_csv_tasks.rb file to non loaded lib folder

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Irida
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[assets tasks i18n_tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
