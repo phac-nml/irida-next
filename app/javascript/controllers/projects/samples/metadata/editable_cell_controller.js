84508fba51ffc2fb00abdd49db5cbb1e1608a769
Chris Huynh <chris.huynh333@gmail.com>
2025-03-20 12:00:05 -0500

    rework logic in editable cell js

--- app/javascript/controllers/projects/samples/metadata/editable_cell_controller.js
+++ app/javascript/controllers/projects/samples/metadata/editable_cell_controller.js
@@ -4,10 +4,11 @@ export default class extends Controller {
   static targets = ["formTemplate"];
 
   submit(event) {
+    let tableCell = event.target.parentNode;
     let form = this.formTemplateTarget.innerHTML
       .replace(/URL_PLACEHOLDER/g, event.target.dataset.url)
       .replace(/FIELD_PLACEHOLDER/g, event.target.dataset.field);
-    event.target.parentNode.insertAdjacentHTML("beforeend", form);
-    this.element.getElementsByTagName("form")[0].requestSubmit();
+    tableCell.insertAdjacentHTML("beforeend", form);
+    tableCell.getElementsByTagName("form")[0].requestSubmit();
   }
 }
