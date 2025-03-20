8678fe6fa708fc939b7feb1cbb834ae027defbc7
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-03-20 14:04:11 -0500

    chore: fix w3v html markup errors on groups dashboard

---
+++ test/controllers/dashboard/groups_controller_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Dashboard
+  class GroupsControllerTest < ActionDispatch::IntegrationTest
+    include Devise::Test::IntegrationHelpers
+
+    test 'should get index' do
+      sign_in users(:john_doe)
+
+      get dashboard_groups_path
+      assert_response :success
+
+      w3c_validate "Groups Dashboard"
+    end
+  end
+end
