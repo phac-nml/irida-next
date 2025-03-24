46cc15cec5a141bc097ce70f5065472194ea3ff9
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-03-24 15:34:44 -0500

    Sidebar accessibility improvements (#996)
    * chore: Updated Layout and Sidebar to reflow based on viewport size and make all elements with focus contain focus class
    
    * chore: update layout component to only remove max-xl:collapsed if viewport width is smalller than 80rem
    
    * Chore: Update dropdown javascript controller to hide on focusout
    
    * chore: update event listeners in layout controller
    
    * chore: Update layout component to blur on smaller sizes

--- app/javascript/controllers/layout_controller.js
+++ app/javascript/controllers/layout_controller.js
@@ -1,24 +1,96 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["layoutContainer", "expandButton", "link"];
+  static targets = [
+    "layoutContainer",
+    "expandButton",
+    "link",
+    "content",
+    "logo",
+    "sidebarOverlay",
+  ];
 
-  initialize() {
+  connect() {
     // Need to determine the previous state
-    localStorage.getItem("layout") === "collapsed"
-      ? this.collapse()
-      : this.expand();
+    if (localStorage.getItem("layout") === "collapsed") {
+      this.collapse();
+    }
+
+    this.boundHandleSidebarOverlayClick =
+      this.handleSidebarOverlayClick.bind(this);
+    this.boundHandleContentFocus = this.handleContentFocus.bind(this);
+
+    this.sidebarOverlayTarget.addEventListener(
+      "click",
+      this.boundHandleSidebarOverlayClick,
+    );
+    this.expandButtonTarget.addEventListener(
+      "focus",
+      this.boundHandleContentFocus,
+    );
+  }
+
+  disconnect() {
+    this.contentTarget.removeEventListener(
+      "click",
+      this.boundHandleSidebarOverlayClick,
+    );
+    this.expandButtonTarget.removeEventListener(
+      "focus",
+      this.boundHandleContentFocus,
+    );
   }
 
   collapse() {
-    this.layoutContainerTarget.classList.add("collapsed");
-    this.expandButtonTarget.classList.remove("hidden");
+    this.layoutContainerTarget.classList.add("max-xl:collapsed", "collapsed");
+    this.expandButtonTarget.classList.remove("xl:hidden");
     localStorage.setItem("layout", "collapsed");
   }
 
-  expand() {
+  expand(event) {
     this.layoutContainerTarget.classList.remove("collapsed");
-    this.expandButtonTarget.classList.add("hidden");
+    if (window.innerWidth < this.#convertRemToPixels(80)) {
+      this.layoutContainerTarget.classList.remove("max-xl:collapsed");
+    }
+    this.expandButtonTarget.classList.add("xl:hidden");
+    if (typeof event !== "undefined") {
+      this.logoTarget.focus();
+    }
     localStorage.setItem("layout", "expanded");
+  }
+
+  handleSidebarOverlayClick() {
+    if (window.innerWidth < this.#convertRemToPixels(80)) {
+      if (
+        !this.layoutContainerTarget.classList.contains(
+          "max-xl:collapsed",
+          "collapsed",
+        )
+      ) {
+        this.collapse();
+      }
+    }
+  }
+
+  handleContentFocus(event) {
+    if (
+      this.expandButtonTarget.contains(event.target) &&
+      window.innerWidth < this.#convertRemToPixels(80)
+    ) {
+      if (
+        !this.layoutContainerTarget.classList.contains(
+          "max-xl:collapsed",
+          "collapsed",
+        )
+      ) {
+        this.collapse();
+      }
+    }
+  }
+
+  #convertRemToPixels(rem) {
+    return (
+      rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
+    );
   }
 }
