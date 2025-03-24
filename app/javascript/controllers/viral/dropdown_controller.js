15536fd1f4ae9208d4e4a8194c1b0361c8a463e4
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-03-24 14:36:04 -0500

    Chore: Update dropdown javascript controller to hide on focusout

--- app/javascript/controllers/viral/dropdown_controller.js
+++ app/javascript/controllers/viral/dropdown_controller.js
@@ -10,12 +10,32 @@ export default class extends Controller {
   };
 
   connect() {
-    new Dropdown(this.menuTarget, this.triggerTarget, {
+    this.dropdown = new Dropdown(this.menuTarget, this.triggerTarget, {
       triggerType: this.triggerValue,
       offsetSkidding: this.skiddingValue,
       offsetDistance: this.distanceValue,
     });
 
+    this.boundHandleTriggerFocusOut = this.handleTriggerFocusOut.bind(this);
+
     this.element.setAttribute("data-controller-connected", "true");
+
+    this.menuTarget.addEventListener(
+      "focusout",
+      this.boundHandleTriggerFocusOut,
+    );
+  }
+
+  disconnect() {
+    this.menuTarget.removeEventListener(
+      "focusout",
+      this.boundHandleTriggerFocusOut,
+    );
+  }
+
+  handleTriggerFocusOut(event) {
+    if (!this.menuTarget.contains(event.relatedTarget)) {
+      this.dropdown.hide();
+    }
   }
 }
