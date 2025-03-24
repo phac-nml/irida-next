89a4a20abf3dc165e759be5c58a3fc05d6ea855a
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-03-24 13:52:11 -0500

    chore: Updated Layout and Sidebar to reflow based on viewport size and make all elements with focus contain focus class

--- app/javascript/controllers/layout_controller.js
+++ app/javascript/controllers/layout_controller.js
@@ -1,24 +1,93 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["layoutContainer", "expandButton", "link"];
+  static targets = [
+    "layoutContainer",
+    "expandButton",
+    "link",
+    "content",
+    "logo",
+  ];
 
-  initialize() {
+  connect() {
     // Need to determine the previous state
-    localStorage.getItem("layout") === "collapsed"
-      ? this.collapse()
-      : this.expand();
+    if (localStorage.getItem("layout") === "collapsed") {
+      this.collapse();
+    }
+
+    this.contentTarget.addEventListener(
+      "click",
+      this.handleContentClick.bind(this),
+    );
+    this.expandButtonTarget.addEventListener(
+      "focus",
+      this.handleContentFocus.bind(this),
+    );
+  }
+
+  disconnect() {
+    this.contentTarget.removeEventListener(
+      "click",
+      this.handleContentClick.bind(this),
+    );
+    this.expandButtonTarget.removeEventListener(
+      "focus",
+      this.handleContentFocus.bind(this),
+    );
   }
 
   collapse() {
-    this.layoutContainerTarget.classList.add("collapsed");
-    this.expandButtonTarget.classList.remove("hidden");
+    this.layoutContainerTarget.classList.add("max-xl:collapsed", "collapsed");
+    this.expandButtonTarget.classList.remove("xl:hidden");
     localStorage.setItem("layout", "collapsed");
   }
 
-  expand() {
-    this.layoutContainerTarget.classList.remove("collapsed");
-    this.expandButtonTarget.classList.add("hidden");
+  expand(event) {
+    this.layoutContainerTarget.classList.remove(
+      "max-xl:collapsed",
+      "collapsed",
+    );
+    this.expandButtonTarget.classList.add("xl:hidden");
+    if (typeof event !== "undefined") {
+      this.logoTarget.focus();
+    }
     localStorage.setItem("layout", "expanded");
+  }
+
+  handleContentClick(event) {
+    if (this.expandButtonTarget.contains(event.target)) {
+      return;
+    } else if (window.innerWidth < this.#convertRemToPixels(80)) {
+      if (
+        !this.layoutContainerTarget.classList.contains(
+          "max-xl:collapsed",
+          "collapsed",
+        )
+      ) {
+        this.collapse();
+      }
+    }
+  }
+
+  handleContentFocus(event) {
+    if (
+      this.expandButtonTarget.contains(event.target) &&
+      window.innerWidth < this.#convertRemToPixels(80)
+    ) {
+      if (
+        !this.layoutContainerTarget.classList.contains(
+          "max-xl:collapsed",
+          "collapsed",
+        )
+      ) {
+        this.collapse();
+      }
+    }
+  }
+
+  #convertRemToPixels(rem) {
+    return (
+      rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
+    );
   }
 }
