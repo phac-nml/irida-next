fd5d9b9bc5c601a02d44ceda7df8f9eefcdbda5d
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-03-26 14:47:27 -0500

    refactor: :recycle: Changed to using html label for select2

--- app/components/viral/form/prefixed/select2_component.rb
+++ app/components/viral/form/prefixed/select2_component.rb
@@ -5,14 +5,15 @@ module Viral
     module Prefixed
       # Select2 component with a prefix
       class Select2Component < ViewComponent::Base
-        attr_reader :form, :name, :selected_value, :placeholder, :required
+        attr_reader :form, :id, :name, :selected_value, :placeholder, :required
 
         renders_many :options, Viral::Select2OptionComponent
         renders_one :empty_state
 
-        def initialize(form:, name:, selected_value: false, placeholder: '', required: true)
+        def initialize(form:, id:, name:, selected_value: false, placeholder: '', required: true)
           @form = form
           @name = name
+          @id = id
           @selected_value = selected_value
           @placeholder = placeholder
           @required = required
