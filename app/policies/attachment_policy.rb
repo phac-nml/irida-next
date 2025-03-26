eabb2445830be4fe4c65bcdb06bb83632225e6e0
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-03-26 12:10:35 -0500

    Add test cases and refactor AttachmentPolicy read? method

--- app/policies/attachment_policy.rb
+++ app/policies/attachment_policy.rb
@@ -3,10 +3,15 @@
 # Policy for attachments authorization
 class AttachmentPolicy < ApplicationPolicy
   def read?
-    if record.attachable.instance_of?(Sample) || record.attachable.instance_of?(Namespaces::ProjectNamespace)
-      allowed_to?(:read?, record.attachable.project)
-    elsif record.attachable.instance_of?(Group)
+    case record.attachable
+    when SamplesWorkflowExecution
+      allowed_to?(:read?, record.attachable.workflow_execution)
+    when WorkflowExecution, Group
       allowed_to?(:read?, record.attachable)
+    when Namespaces::ProjectNamespace
+      allowed_to?(:read?, record.attachable.project)
+    else
+      false
     end
   end
 end
