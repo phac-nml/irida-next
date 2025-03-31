0ad21a89f33f3ca649738b0972adb27bb428cfed
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-03-31 09:25:06 -0500

    test: update select2 component test to assert correct item count and improve input handling 🎉🔧

--- test/components/viral/form/prefixed/select2_component_test.rb
+++ test/components/viral/form/prefixed/select2_component_test.rb
@@ -12,14 +12,15 @@ module Viral
 
           find('input.select2-input[type="text"]').click
           assert_selector 'div[data-viral--select2-target="dropdown"]', visible: :visible
-          assert_selector 'ul[data-viral--select2-target="scroller"] li', count: 11
+          assert_selector 'ul[data-viral--select2-target="scroller"] li', count: 50
 
           find('li button[data-viral--select2-primary-param="User 1"]').click
           assert_selector 'input[type="hidden"][name="user"][value="1"]', visible: :hidden, count: 1
 
-          find('input.select2-input[type="text"]').send_keys :backspace
-          find('input.select2-input[type="text"]').send_keys '22'
-          find('input.select2-input[type="text"]').send_keys :enter
+          find('input.select2-input[type="text"]').send_keys([:ctrl, 'a'], '22')
+          assert_selector 'div[data-viral--select2-target="dropdown"]', visible: :visible
+          find('ul[data-viral--select2-target="scroller"] > li button').click
+
           assert_selector 'input[type="hidden"][name="user"][value="22"]', visible: :hidden, count: 1
         end
       end
