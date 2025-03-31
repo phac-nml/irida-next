ca8e3c7be1f2f2ffd30743e35e05a8458087ea71
Chris Huynh <chris.huynh333@gmail.com>
2025-03-31 10:32:23 -0500

    refactor scope queries in destroy service multi destroy

--- app/services/workflow_executions/destroy_service.rb
+++ app/services/workflow_executions/destroy_service.rb
@@ -27,19 +27,20 @@ module WorkflowExecutions
     def destroy_multiple
       authorize! @namespace, to: :destroy_workflow_executions? unless @namespace.nil?
 
-      workflow_executions = if @namespace
-                              authorized_scope(WorkflowExecution, type: :relation, as: :automated,
-                                                                  scope_options: { project: @namespace.project })
-                                .where(id: @workflow_execution_ids, state: %w[completed canceled error], cleaned: true)
-                            else
-                              authorized_scope(WorkflowExecution, type: :relation, as: :user,
-                                                                  scope_options: { user: current_user })
-                                .where(id: @workflow_execution_ids, state: %w[completed canceled error], cleaned: true)
-                            end
+      workflow_executions_scope = if @namespace
+                                    authorized_scope(WorkflowExecution, type: :relation, as: :automated,
+                                                                        scope_options: { project: @namespace.project })
+                                  else
+                                    authorized_scope(WorkflowExecution, type: :relation, as: :user,
+                                                                        scope_options: { user: current_user })
+                                  end
+      deletable_workflow_executions = workflow_executions_scope.where(
+        id: @workflow_execution_ids,
+        state: %w[completed canceled error], cleaned: true
+      )
+      workflows_to_delete_count = deletable_workflow_executions.count
 
-      workflows_to_delete_count = workflow_executions.count
-
-      workflow_executions.destroy_all
+      deletable_workflow_executions.destroy_all
 
       workflows_to_delete_count
     end
