d6f0d956ddcd5db959ef34e21de83734ae377eeb
Deep Sidhu <deepsidhu85@gmail.com>
2025-04-03 16:28:36 -0500

    Added sections to dropdown component, added/removed translations

--- app/components/viral/dropdown/item_component.rb
+++ app/components/viral/dropdown/item_component.rb
@@ -4,12 +4,13 @@ module Viral
   module Dropdown
     # Item component for dropdown
     class ItemComponent < Viral::Component
-      attr_reader :label, :icon, :url
+      attr_reader :label, :icon, :url, :section_header
 
-      def initialize(label:, url: nil, icon_name: nil, **system_arguments)
+      def initialize(label:, url: nil, icon_name: nil, section_header: false, **system_arguments)
         @label = label
         @icon = icon_name
         @url = url
+        @section_header = section_header
         @system_arguments = system_arguments
       end
     end
