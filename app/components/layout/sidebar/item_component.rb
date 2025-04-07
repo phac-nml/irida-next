0f2638d7c615406e6fcd6d090640ef596b6d9fb9
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-04-07 11:02:07 -0500

    refactor: update sidebar item component to use item_icon method and replace icons for consistency 🎨✨

--- app/components/layout/sidebar/item_component.rb
+++ app/components/layout/sidebar/item_component.rb
@@ -4,13 +4,17 @@ module Layout
   module Sidebar
     # Sidebar item component
     class ItemComponent < Component
-      attr_reader :url, :label, :icon, :selected
+      attr_reader :url, :label, :icon_name, :selected
 
       def initialize(url:, label:, icon: nil, selected: false)
         @url = url
         @label = label
-        @icon = icon
+        @icon_name = icon
         @selected = selected
+      end
+
+      def item_icon
+        icon @icon_name, class: 'size-4', style: 'width: 27px;'
       end
     end
   end
