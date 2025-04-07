91f0294c70c799b1a9622d8cb12e1cff429adb78
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-04-07 10:30:46 -0500

    feat: add rails_icons gem and initializer for icon configuration 🎨✨
    - Removed `heroicon-rails` dependency from Gemfile for better icon management.
    - Added `rails_icons` gem (version ~> 1.2) to Gemfile for enhanced icon support.
    - Created `rails_icons.rb` initializer to configure the default icon library to "phosphor" and set up options for future customization. 🛠️

---
+++ config/initializers/rails_icons.rb
@@ -0,0 +1,25 @@
+RailsIcons.configure do |config|
+  config.default_library = "phosphor"
+  # config.default_variant = "" # Set a default variant for all libraries
+
+  # Override Phosphor defaults
+  # config.libraries.phosphor.exclude_variants = [:duotone, :thin] # Exclude specific variants
+
+  # config.libraries.phosphor.bold.default.css = "size-6"
+  # config.libraries.phosphor.bold.default.data = {}
+
+  # config.libraries.phosphor.duotone.default.css = "size-6"
+  # config.libraries.phosphor.duotone.default.data = {}
+
+  # config.libraries.phosphor.fill.default.css = "size-6"
+  # config.libraries.phosphor.fill.default.data = {}
+
+  # config.libraries.phosphor.light.default.css = "size-6"
+  # config.libraries.phosphor.light.default.data = {}
+
+  # config.libraries.phosphor.regular.default.css = "size-6"
+  # config.libraries.phosphor.regular.default.data = {}
+
+  # config.libraries.phosphor.thin.default.css = "size-6"
+  # config.libraries.phosphor.thin.default.data = {}
+end
