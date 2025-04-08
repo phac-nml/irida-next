6448f095516ecb28e7874753c05a06af80020f6a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-04-08 13:50:13 -0500

    refactor: update item_component to use item_icon method for improved clarity and consistency 🎨✨ refactor: change icon_name for 'Inbox' item in dropdown preview to 'mailbox' for accuracy 🔄

--- app/components/viral/dropdown/item_component.rb
+++ app/components/viral/dropdown/item_component.rb
@@ -4,13 +4,19 @@ module Viral
   module Dropdown
     # Item component for dropdown
     class ItemComponent < Viral::Component
-      attr_reader :label, :icon, :url
+      attr_reader :label, :url
 
       def initialize(label:, url: nil, icon_name: nil, **system_arguments)
         @label = label
-        @icon = icon_name
+        @icon_name = icon_name
         @url = url
         @system_arguments = system_arguments
+      end
+
+      def item_icon
+        return if @icon_name.blank?
+
+        icon @icon_name, class: 'size-5'
       end
     end
   end
