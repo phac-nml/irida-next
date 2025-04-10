4f7c9a645ddce1e4fa924d831c7eeaf05b5d0e1a
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-04-10 15:12:23 -0500

    starting to add translations for operations

--- app/components/advanced_search_component.rb
+++ app/components/advanced_search_component.rb
@@ -2,12 +2,28 @@
 
 # View component for advanced search component
 class AdvancedSearchComponent < Component
-  def initialize(form:, search:, fields: [], operations: [], open: false, status: true) # rubocop:disable Metrics/ParameterLists
+  def initialize(form:, search:, fields: [], open: false, status: true)
     @form = form
     @search = search
     @fields = fields
     @operations = operations
     @open = open
     @status = status
+  end
+
+  private
+
+  def operations
+    {
+      I18n.t('advanced_search_component.operation.equals') => '=',
+      I18n.t('advanced_search_component.operation.not_equals') => '!=',
+      I18n.t('advanced_search_component.operation.less_than') => '<=',
+      I18n.t('advanced_search_component.operation.greater_than') => '>=',
+      I18n.t('advanced_search_component.operation.contains') => 'contains',
+      I18n.t('advanced_search_component.operation.exists') => 'exists',
+      I18n.t('advanced_search_component.operation.not_exists') => 'not_exists',
+      I18n.t('advanced_search_component.operation.in') => 'in',
+      I18n.t('advanced_search_component.operation.not_in') => 'not_in'
+    }
   end
 end
