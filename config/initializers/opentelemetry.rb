f4bfbcab5c4030d72e8f0b2eb5d646ec93214f8e
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2025-04-10 14:22:26 -0500

    fix imports

--- config/initializers/opentelemetry.rb
+++ config/initializers/opentelemetry.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'opentelemetry/sdk'
-require 'opentelemetry/instrumentation/active_job'
 require 'opentelemetry-exporter-otlp'
 
 require 'opentelemetry-metrics-sdk'
