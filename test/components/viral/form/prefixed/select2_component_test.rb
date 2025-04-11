a8f64a1cbe7bbb007179c87315e71357d6903fcb
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-04-11 15:09:12 -0500

    feat: update viral select2 component to improve user option display and test accuracy 🎉✨
    - Refactored the option rendering in `default.html.erb` to ensure proper labeling of user options.
    - Updated the test in `select2_component_test.rb` to match the new button data-label for user selection, enhancing test reliability. 🔍

--- test/components/viral/form/prefixed/select2_component_test.rb
+++ test/components/viral/form/prefixed/select2_component_test.rb
@@ -14,7 +14,7 @@ module Viral
           assert_selector 'div[data-viral--select2-target="dropdown"]', visible: :visible
           assert_selector 'ul[data-viral--select2-target="scroller"] li', count: 50
 
-          find('li button[data-viral--select2-primary-param="User 1"]').click
+          find('li button[data-label="User 1"]').click
           assert_selector 'input[type="hidden"][name="user"][value="1"]', visible: :hidden, count: 1
 
           find('input.select2-input[type="text"]').send_keys([:ctrl, 'a'], '22')
