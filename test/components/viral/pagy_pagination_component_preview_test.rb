18581f1c0c429a12bdefd2b551b9d408e2c57a90
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-04-11 08:29:16 -0500

    refactor: update pagination component to improve accessibility and visual consistency 🎨✨
    - Changed the rendering of disabled pagination links from `<a>` to `<span>` with appropriate classes for better accessibility.
    - Updated tests to reflect the new structure of pagination links, ensuring they check for the correct elements and text using I18n for localization.
    - Enhanced the overall user experience by ensuring that disabled links are visually distinct and semantically correct.

--- test/components/viral/pagy_pagination_component_preview_test.rb
+++ test/components/viral/pagy_pagination_component_preview_test.rb
@@ -8,19 +8,19 @@ module Viral
       render_preview(:default)
 
       assert_selector 'nav.pagy.nav'
-      assert_selector 'li a[aria-disabled="true"]', text: 'Previous'
-      assert_selector 'li > a', text: 'Next'
-      assert_selector 'li a[aria-disabled="true"]', text: '1'
-      assert_selector 'li > a:not([aria-disabled="true"])', count: '5'
+      assert_selector 'li span.cursor-not-allowed', text: I18n.t('viral.pagy.pagination_component.previous')
+      assert_selector 'li > a', text: I18n.t('viral.pagy.pagination_component.next')
+      assert_selector 'li span.cursor-not-allowed', text: '1', count: 1
+      assert_selector 'li > a:not([aria-disabled="true"])', count: 5
     end
 
     test 'does not render when only one page' do
       render_preview(:only_one_page)
 
       assert_no_selector 'nav.pagy.nav'
-      assert_no_selector 'li a[aria-disabled="true"]', text: 'Previous'
-      assert_no_selector 'li a[aria-disabled="true"]', text: 'Next'
-      assert_no_selector 'li a[aria-disabled="true"]', text: '1'
+      assert_no_selector 'li span.cursor-not-allowed', text: I18n.t('viral.pagy.pagination_component.previous')
+      assert_no_selector 'li span.cursor-not-allowed', text: I18n.t('viral.pagy.pagination_component.next')
+      assert_no_selector 'li span.cursor-not-allowed', text: '1'
       assert_no_selector 'li > a:not([aria-disabled="true"])'
     end
 
@@ -28,11 +28,11 @@ module Viral
       render_preview(:many_pages)
 
       assert_selector 'nav.pagy.nav'
-      assert_selector 'li > a', text: 'Previous'
-      assert_selector 'li > a', text: 'Next'
-      assert_selector 'li a[aria-disabled="true"]', text: '5'
-      assert_selector 'li > a:not([aria-disabled="true"])', count: '6'
-      assert_selector 'li a[aria-disabled="true"]', text: '...', count: '2'
+      assert_selector 'li > a', text: I18n.t('viral.pagy.pagination_component.previous')
+      assert_selector 'li > a', text: I18n.t('viral.pagy.pagination_component.next')
+      assert_selector 'li span.cursor-not-allowed', text: '5', count: 1
+      assert_selector 'li > a:not([aria-disabled="true"])', count: 6
+      assert_selector 'li span.cursor-not-allowed', text: '...', count: 2
     end
   end
 end
