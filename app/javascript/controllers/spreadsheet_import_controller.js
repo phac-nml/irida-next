91a53377e440b00df522829cd68c27f3b9af1b78
Chris Huynh <chris.huynh333@gmail.com>
2025-04-14 15:20:55 -0500

    fix some JS conditionals for group vs proj, fix some var names

--- app/javascript/controllers/spreadsheet_import_controller.js
+++ app/javascript/controllers/spreadsheet_import_controller.js
@@ -173,6 +173,9 @@ export default class extends Controller {
       this.#selectedHeaders["spreadsheet_import_sample_description_column"],
       unselectedHeaders,
     );
+    if (this.hasStaticProjectTarget) {
+      this.#enableTarget(this.staticProjectTarget);
+    }
     this.checkFormInputsReadyForSubmit();
   }
 
