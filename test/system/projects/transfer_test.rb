6b4add34b7d13c8f462fa39a2e515ecec3eb0864
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-04-16 08:08:19 -0500

    fix: update viral_select2 options to use namespace data for accurate selection 🌟🔧

--- test/system/projects/transfer_test.rb
+++ test/system/projects/transfer_test.rb
@@ -16,7 +16,7 @@ module Projects
     test 'should transfer project' do
       visit namespace_project_edit_path(@project.namespace.parent, @project)
       find('input.select2-input').click
-      find("button[data-label='#{@namespace.full_path}']").click
+      find("button[data-value='#{@namespace.id}']").click
       assert_selector 'input.select2-input', count: 1
       click_on I18n.t(:'projects.edit.advanced.transfer.submit')
 
