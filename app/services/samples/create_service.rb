d903ad50c89cd13dd45b7d6654889c409b0ca68b
Chris Huynh <chris.huynh333@gmail.com>
2025-04-16 13:49:06 -0500

    fix create service for activity conditional

--- app/services/samples/create_service.rb
+++ app/services/samples/create_service.rb
@@ -15,17 +15,20 @@ module Samples
     def execute
       authorize! @project, to: :create_sample? unless @project.nil?
 
-      if sample.save && @include_activity
-        @project.namespace.create_activity key: 'namespaces_project_namespace.samples.create',
-                                           owner: current_user,
-                                           parameters:
-                                             {
-                                               sample_id: sample.id,
-                                               sample_puid: sample.puid,
-                                               action: 'sample_create'
-                                             }
+      if sample.save
 
-        update_samples_count if @project.parent.type == 'Group'
+        update_samples_count if @project.parent.type == Group.sti_name
+
+        if @include_activity
+          @project.namespace.create_activity key: 'namespaces_project_namespace.samples.create',
+                                             owner: current_user,
+                                             parameters:
+                                               {
+                                                 sample_id: sample.id,
+                                                 sample_puid: sample.puid,
+                                                 action: 'sample_create'
+                                               }
+        end
       end
 
       sample
--- test/services/samples/create_service_test.rb
+++ test/services/samples/create_service_test.rb
@@ -142,5 +142,18 @@ module Samples
         Samples::CreateService.new(@user, project1, short_name_params).execute
       end
     end
+
+    test 'sample count increases when activity is not written' do
+      project1 = projects(:project1)
+      group1 = groups(:group_one)
+      valid_params = { name: 'a new sample', description: 'sample with already existing name',
+                       include_activity: false }
+
+      assert_difference -> { Sample.count } => 1,
+                        -> { project1.reload.samples_count } => 1,
+                        -> { group1.reload.aggregated_samples_count } => 1 do
+        Samples::CreateService.new(@user, project1, valid_params).execute
+      end
+    end
   end
 end
