36b4a5a0288dfbebc4edaed0953d0e1e3e99b67c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-04-17 07:22:39 -0500

    fix: update Select2OptionComponent styles for improved accessibility and consistency 🎨✨

--- app/components/viral/select2_option_component.rb
+++ app/components/viral/select2_option_component.rb
@@ -5,8 +5,9 @@ module Viral
   class Select2OptionComponent < Viral::Component
     erb_template <<~ERB
       <li
-        class="w-full flex-col w-full border-2 border-transparent text-left p-2.5 bg-slate-50 hover:bg-slate-100
-              dark:bg-slate-700 dark:hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        class="flex flex-col text-left p-2.5 rounded-md bg-slate-50 hover:bg-slate-100 dark:bg-slate-700 dark:hover:bg-slate-600
+               cursor-pointer border border-transparent transition-colors duration-150 mx-0.5 my-0.5"
+               "
         role="option"
         aria-selected="false"
         id="select2-option-<%= @value %>"
