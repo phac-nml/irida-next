01801d7c2889a05c5ff8c3817f7f42a0359c37e1
Deep Sidhu <deepsidhu85@gmail.com>
2025-04-22 12:25:18 -0500

    Added parameters serialization for activities

--- config/initializers/public_activity.rb
+++ config/initializers/public_activity.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 PublicActivity::Activity.class_eval do
+  serialize :parameters, coder: YAML, type: Hash unless %i[json jsonb hstore].include?(columns_hash['parameters'].type)
+
   has_one :activity_extended_detail
   has_one :extended_details, through: :activity_extended_detail, source: :extended_detail
 end
