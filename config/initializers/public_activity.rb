60da04c746617011d89e5b1e70ae4fa25d8c3670
Deep Sidhu <deepsidhu85@gmail.com>
2025-04-22 12:30:57 -0500

    Attempting to fix serialization errors

--- config/initializers/public_activity.rb
+++ config/initializers/public_activity.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 PublicActivity::Activity.class_eval do
-  serialize :parameters, coder: YAML, type: Hash unless %i[json jsonb hstore].include?(columns_hash['parameters'].type)
+  serialize :parameters, coder: YAML, type: Hash
 
   has_one :activity_extended_detail
   has_one :extended_details, through: :activity_extended_detail, source: :extended_detail
