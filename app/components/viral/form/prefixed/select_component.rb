071de42112850bea16d5695cfae227d2c7bb01e7
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-04-23 13:40:03 -0500

    reverting the viral_prefixed_select_component changes

--- app/components/viral/form/prefixed/select_component.rb
+++ app/components/viral/form/prefixed/select_component.rb
@@ -9,18 +9,11 @@ module Viral
 
         renders_one :prefix
 
-        def initialize(form:, name:, options: [], selected_value: false, **system_arguments)
+        def initialize(form:, name:, options: [], selected_value: false)
           @form = form
           @name = name
           @options = options
           @selected_value = selected_value
-
-          @system_arguments = system_arguments
-          @system_arguments[:class] = class_names(
-            @system_arguments[:class],
-            'rounded-none rounded-e-lg bg-slate-50 border text-slate-900 block flex-1 min-w-0 w-full text-sm',
-            'border-slate-300 p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white'
-          )
         end
       end
     end
