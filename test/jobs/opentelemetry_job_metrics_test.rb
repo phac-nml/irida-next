059caf09ecd4c4272940516020e7a0f7efa0d812
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2025-04-24 18:31:37 -0500

    smarter metric reporting and more tests

--- test/jobs/opentelemetry_job_metrics_test.rb
+++ test/jobs/opentelemetry_job_metrics_test.rb
@@ -81,4 +81,70 @@ class OpenTelemetryJobMetricsTest < ActiveJob::TestCase
     mock_queue_latency_instrument.verify
     @mock_meter.verify
   end
+
+  test 'update queue count to 0 when queue is empty' do
+    # mock instrument on meter that records values
+    mock_queue_count_instrument = Minitest::Mock.new
+    mock_queue_count_instrument.expect(:record, nil, [3])
+    mock_queue_count_instrument.expect(:record, nil, [0])
+    # using 'default' as our queue name
+    @mock_meter.expect(:create_gauge, mock_queue_count_instrument, ['default_queue_count'], description: String)
+
+    # queue some jobs
+    DummyJob.set(wait: 2.minutes).perform_later(1)
+    DummyJob.set(wait: 2.minutes).perform_later(1)
+    DummyJob.set(wait: 2.minutes).perform_later(1)
+    travel_to(3.minutes.from_now) do
+      # run metrics while job is queued
+      @job_queue_metrics.update_queue_counts
+      # run metrics while job is queued AGAIN to verify repeat data points don't get resent
+      @job_queue_metrics.update_queue_counts
+      # don't let job procs hang
+      GoodJob.perform_inline
+    end
+
+    # run metrics again after jobs have finished
+    @job_queue_metrics.update_queue_counts
+    # run metrics again after jobs have finished AGAIN to verify repeat data points don't get resent
+    @job_queue_metrics.update_queue_counts
+
+    # verify objects called as expected
+    mock_queue_count_instrument.verify
+    @mock_meter.verify
+  end
+
+  test 'update queue latency to 0 when queue is empty' do
+    # mock instrument on meter that records values
+    mock_queue_latency_instrument = Minitest::Mock.new
+    mock_queue_latency_instrument.expect(:record, nil) do |latency|
+      latency > 55 && latency < 65 # Expect latency to be about a minute
+    end
+    mock_queue_latency_instrument.expect(:record, nil, [0]) # should be 0 when jobs are cleared
+    # using 'default' as our queue name
+    @mock_meter.expect(
+      :create_gauge, mock_queue_latency_instrument, ['default_queue_min_wait_time'], unit: String, description: String
+    )
+
+    # queue some jobs
+    DummyJob.set(wait: 2.minutes).perform_later(1)
+    DummyJob.set(wait: 2.minutes).perform_later(1)
+    DummyJob.set(wait: 2.minutes).perform_later(1)
+    travel_to(3.minutes.from_now) do
+      # run metrics while job is queued
+      @job_queue_metrics.update_minimum_queue_times
+      # run metrics while job is queued AGAIN to verify repeat data points don't get resent
+      @job_queue_metrics.update_minimum_queue_times
+      # don't let job procs hang
+      GoodJob.perform_inline
+    end
+
+    # run metrics again after jobs have finished
+    @job_queue_metrics.update_minimum_queue_times
+    # run metrics again after jobs have finished AGAIN to verify repeat data points don't get resent
+    @job_queue_metrics.update_minimum_queue_times
+
+    # verify objects called as expected
+    mock_queue_latency_instrument.verify
+    @mock_meter.verify
+  end
 end
