e2f78dce705e276eb39651ebab7113f7a9314ca5
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-05-08 11:44:34 -0500

    working around i18n-tasks unused error

--- test/components/samples/table_component_test.rb
+++ test/components/samples/table_component_test.rb
@@ -7,7 +7,7 @@ module Samples
     test 'Should render a table of samples for a group that are sorted by the project column' do
       with_request_url '/-/groups/group-1/-/samples' do
         namespace = groups(:group_one)
-        project_ids = namespace.project_namespaces.pluck(:id)
+        project_ids = Project.where(namespace_id: namespace.project_namespace_ids).pluck(:id)
         query = Sample::Query.new({ sort: 'namespaces.puid asc', project_ids: project_ids })
         pagy, samples = query.results(limit: 50, page: 1)
         samples = samples.includes(project: { namespace: :parent })
@@ -31,8 +31,16 @@ module Samples
 
         assert_selector 'table', count: 1
         assert_selector 'table thead th', count: 6
-        assert_selector 'table tbody tr', count: samples.count
+        assert_selector 'table thead th:first-child', text: I18n.t('samples.table_component.puid')
+        assert_selector 'table thead th:nth-child(2)', text: I18n.t('samples.table_component.name')
+        assert_selector 'table thead th:nth-child(3)', text: I18n.t('samples.table_component.namespaces.puid')
         assert_selector 'table thead th:nth-child(3) svg.icon-arrow_up'
+        assert_selector 'table thead th:nth-child(4)', text: I18n.t('samples.table_component.created_at')
+        assert_selector 'table thead th:nth-child(5)', text: I18n.t('samples.table_component.updated_at')
+        assert_selector 'table thead th:nth-child(6)',
+                        text: I18n.t('samples.table_component.attachments_updated_at')
+        assert_selector 'table tbody tr', count: samples.count
+
         previous_project_puid = ''
         samples.each do |sample|
           assert_selector 'table tbody tr td:nth-child(2)', text: sample.name
