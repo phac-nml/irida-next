c195f5bb43c29aa6616ce875cf5f6e01e3a582b3
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-05-09 14:51:01 -0500

    chore: fix rubocop warnings/issues

--- app/controllers/projects/samples_controller.rb
+++ app/controllers/projects/samples_controller.rb
@@ -97,7 +97,7 @@ module Projects
       respond_to do |format|
         format.turbo_stream do
           if params[:select].present?
-            @sample_ids = @query.results.reorder(nil).where(updated_at: ..params[:timestamp].to_datetime).select(:id).pluck(:id)
+            @sample_ids = @query.results.reorder(nil).where(updated_at: ..params[:timestamp].to_datetime).pluck(:id)
           end
         end
       end
