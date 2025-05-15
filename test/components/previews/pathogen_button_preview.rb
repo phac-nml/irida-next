66246bb7b63aa13452d256e6a70c46950a56e527
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-05-15 15:52:31 -0500

    refactor: consolidate button preview templates into comprehensive visual examples

--- test/components/previews/pathogen_button_preview.rb
+++ test/components/previews/pathogen_button_preview.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class PathogenButtonPreview < ViewComponent::Preview
-  # @param scheme select { choices: [default,primary, danger] } "The color scheme of the button."
+  # @param scheme select { choices: [default, primary, danger] } "The color scheme of the button."
   # @param size select { choices: [small, medium] } "The size of the button."
-  # @param disabled toggle "The Boolean disabled attribute, when present, makes the element not mutable, focusable, or even submitted with the form. The user can neither edit nor focus on the control, nor its form control descendants."
+  # @param disabled toggle "The Boolean disabled attribute, when present, makes the element not mutable, focusable, or even submitted with the form."
   # @param block toggle "If true, the button will take up the full width of its container."
   def playground(scheme: :default, size: :medium, disabled: false, block: false)
     render Pathogen::Button.new(scheme:, size:, disabled:, block:, test_selector: 'playground') do
@@ -11,79 +11,15 @@ class PathogenButtonPreview < ViewComponent::Preview
     end
   end
 
-  # @param disabled toggle "The Boolean disabled attribute, when present, makes the element not mutable, focusable, or even submitted with the form. The user can neither edit nor focus on the control, nor its form control descendants."
-  def default(disabled: false)
-    render Pathogen::Button.new(disabled:, test_selector: 'default') do
-      'Button'
-    end
-  end
-
-  # @param disabled toggle "The Boolean disabled attribute, when present, makes the element not mutable, focusable, or even submitted with the form. The user can neither edit nor focus on the control, nor its form control descendants."
-  # @param block toggle "If true, the button will take up the full width of its container."
-  def primary(disabled: false, block: false)
-    render Pathogen::Button.new(scheme: :primary, disabled:, block:, test_selector: 'primary') do
-      'Button'
-    end
-  end
-
-  # @param disabled toggle "The Boolean disabled attribute, when present, makes the element not mutable, focusable, or even submitted with the form. The user can neither edit nor focus on the control, nor its form control descendants."
-  # @param block toggle "If true, the button will take up the full width of its container."
-  def danger(disabled: false, block: false)
-    render Pathogen::Button.new(scheme: :danger, disabled:, block:, test_selector: 'danger') do
-      'Button'
-    end
-  end
-
-  def all_schemes; end
-
-  def full_width
-    render Pathogen::Button.new(block: true, test_selector: 'full-width') do
-      'Button'
-    end
-  end
-
-  # @param scheme select { choices: [default,primary, danger] } "The color scheme of the button."
-  # @param href text "The URL to link to."
-  # @param disabled toggle "The Boolean disabled attribute, when present, makes the element not mutable, focusable, or even submitted with the form. The user can neither edit nor focus on the control, nor its form control descendants."
-  def link_as_a_button(scheme: :default, href: '#', disabled: false)
-    render Pathogen::Button.new(scheme:, href:, tag: :a, disabled:, test_selector: 'link-as-a-button') do
-      'Button'
-    end
-  end
-
-  # @param scheme select { choices: [default,primary, danger] } "The color scheme of the button."
+  # Shows all available button states and variants in a single view
   # @param size select { choices: [small, medium] } "The size of the button."
-  def leading_visual(scheme: :primary, size: :medium)
-    render_with_template(locals: {
-                           scheme:,
-                           size:
-                         })
+  def all_states_and_variants(size: :medium)
+    render_with_template(locals: { size: })
   end
 
-  # @param scheme select { choices: [default,primary, danger] } "The color scheme of the button."
+  # Shows all visual elements (icons, SVGs) in different configurations
   # @param size select { choices: [small, medium] } "The size of the button."
-  def leading_visual_svg(scheme: :primary, size: :medium)
-    render_with_template(locals: {
-                           scheme:,
-                           size:
-                         })
-  end
-
-  # @param scheme select { choices: [default,primary, danger] } "The color scheme of the button."
-  # @param size select { choices: [small, medium] } "The size of the button."
-  def trailing_visual(scheme: :primary, size: :medium)
-    render_with_template(locals: {
-                           scheme:,
-                           size:
-                         })
-  end
-
-  # @param scheme select { choices: [default,primary, danger] } "The color scheme of the button."
-  # @param size select { choices: [small, medium] } "The size of the button."
-  def trailing_visual_svg(scheme: :primary, size: :medium)
-    render_with_template(locals: {
-                           scheme:,
-                           size:
-                         })
+  def visual_elements(size: :medium)
+    render_with_template(locals: { size: })
   end
 end
