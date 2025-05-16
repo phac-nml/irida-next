22d4bdebbf18f3eecd461d6b71c449f16619315a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-05-16 06:38:46 -0500

    refactor: remove size parameter from Pathogen::Button component

--- test/components/previews/pathogen_button_preview.rb
+++ test/components/previews/pathogen_button_preview.rb
@@ -2,24 +2,19 @@
 
 class PathogenButtonPreview < ViewComponent::Preview
   # @param scheme select { choices: [default, primary, danger, ghost] } "The color scheme of the button."
-  # @param size select { choices: [small, medium] } "The size of the button."
-  # @param disabled toggle "The Boolean disabled attribute, when present, makes the element not mutable, focusable, or even submitted with the form."
-  # @param block toggle "If true, the button will take up the full width of its container."
-  def playground(scheme: :default, size: :medium, disabled: false, block: false)
-    render Pathogen::Button.new(scheme:, size:, disabled:, block:, test_selector: 'playground') do
+  def playground(scheme: :default)
+    render Pathogen::Button.new(scheme:, test_selector: 'playground') do
       'Button'
     end
   end
 
   # Shows all available button states and variants in a single view
-  # @param size select { choices: [small, medium] } "The size of the button."
-  def all_states_and_variants(size: :medium)
-    render_with_template(locals: { size: })
+  def all_states_and_variants
+    render_with_template
   end
 
   # Shows all visual elements (icons, SVGs) in different configurations
-  # @param size select { choices: [small, medium] } "The size of the button."
-  def visual_elements(size: :medium)
-    render_with_template(locals: { size: })
+  def visual_elements
+    render_with_template
   end
 end
