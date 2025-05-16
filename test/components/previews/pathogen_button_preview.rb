981f112a0f0802ab636a49e58e3755c5881b00d9
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-05-16 06:51:46 -0500

    feat: add unstyled button scheme with custom class support and base style override

--- test/components/previews/pathogen_button_preview.rb
+++ test/components/previews/pathogen_button_preview.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PathogenButtonPreview < ViewComponent::Preview
-  # @param scheme select { choices: [default, primary, danger, ghost] } "The color scheme of the button."
+  # @param scheme select { choices: [default, primary, danger, ghost, unstyled] } "The color scheme of the button."
   def playground(scheme: :default)
     render Pathogen::Button.new(scheme:, test_selector: 'playground') do
       'Button'
@@ -15,6 +15,11 @@ class PathogenButtonPreview < ViewComponent::Preview
 
   # Shows all visual elements (icons, SVGs) in different configurations
   def visual_elements
+    render_with_template
+  end
+
+  # Shows all available button schemes
+  def all_schemes
     render_with_template
   end
 end
