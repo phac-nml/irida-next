5b3d9b29619f8afc313a526846b7da899c0b82aa
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-05-16 09:37:10 -0500

    test: update BaseButtonTest to utilize BaseButton initialization for improved clarity and consistency 🎉✨

--- test/components/pathogen/base_button_test.rb
+++ test/components/pathogen/base_button_test.rb
@@ -6,7 +6,7 @@ module Pathogen
   class BaseButtonTest < ViewComponent::TestCase
     test 'renders button with default type and classes' do
       render_inline(BaseButton.new) { 'Click me' }
-      
+
       assert_selector "button[type='button']", text: 'Click me'
       assert_selector '.inline-flex.items-center.justify-center', count: 1
       assert_selector '.px-5.py-2\.5.text-sm.font-semibold', count: 1
@@ -16,7 +16,7 @@ module Pathogen
 
     test 'renders anchor tag when specified' do
       render_inline(BaseButton.new(tag: :a, href: '#')) { 'Link' }
-      
+
       assert_selector 'a[href="#"]', text: 'Link'
       refute_selector 'a[type]' # Type should not be set on anchor tags
     end
@@ -24,14 +24,14 @@ module Pathogen
     test 'renders submit and reset button types' do
       render_inline(BaseButton.new(type: :submit)) { 'Submit' }
       assert_selector "button[type='submit']", text: 'Submit'
-      
+
       render_inline(BaseButton.new(type: :reset)) { 'Reset' }
       assert_selector "button[type='reset']", text: 'Reset'
     end
 
     test 'applies disabled state with proper attributes' do
       render_inline(BaseButton.new(disabled: true)) { 'Disabled' }
-      
+
       assert_selector 'button[disabled]', text: 'Disabled'
       assert_selector '[aria_disabled="true"]', text: 'Disabled'
       assert_selector '.disabled\:cursor-not-allowed', text: 'Disabled'
@@ -40,7 +40,7 @@ module Pathogen
 
     test 'converts disabled anchor to button with proper attributes' do
       render_inline(BaseButton.new(tag: :a, disabled: true)) { 'Disabled Link' }
-      
+
       assert_selector 'button[disabled]', text: 'Disabled Link'
       assert_selector '[aria_disabled="true"]', text: 'Disabled Link'
       refute_selector 'a' # Should be converted to button
@@ -49,19 +49,19 @@ module Pathogen
 
     test 'preserves custom classes' do
       render_inline(BaseButton.new(classes: 'custom-class another-class')) { 'Custom' }
-      
+
       assert_selector 'button.custom-class.another-class', text: 'Custom'
     end
 
     test 'includes cursor-pointer class by default' do
       render_inline(BaseButton.new) { 'Clickable' }
-      
+
       assert_selector 'button.cursor-pointer', text: 'Clickable'
     end
 
     test 'sets tabindex to -1 when disabled' do
       render_inline(BaseButton.new(disabled: true)) { 'Disabled' }
-      
+
       assert_selector 'button[tabindex="-1"]', text: 'Disabled'
     end
 
