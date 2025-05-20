d6844b744025d0de80f1058d1f02aab9e5088b06
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-05-20 06:42:59 -0500

    feat: add custom button styles to DropdownComponent 🎨✨
    - Updated `DropdownComponent` to accept `button_styles` as an attribute, allowing for customizable button appearances.
    - Enhanced `system_arguments_for_button` method to apply custom styles when provided, improving flexibility in design.
    - Added a preview method `with_custom_button_styles` in `ViralDropdownComponentPreview` to demonstrate the new feature.
    - Created a system test `DropdownComponentTest` to verify that custom button styles are applied correctly. ✅

--- test/components/previews/viral_dropdown_component_preview.rb
+++ test/components/previews/viral_dropdown_component_preview.rb
@@ -8,4 +8,17 @@ class ViralDropdownComponentPreview < ViewComponent::Preview
   def with_icon; end
 
   def with_item_icon; end
+
+  # Preview for custom button_styles
+  def with_custom_button_styles
+    render(Viral::DropdownComponent.new(
+             label: 'Custom',
+             button_styles: 'bg-emerald-800 text-white px-4 py-2 flex gap-2 rounded-full cursor-pointer',
+             caret: true
+           )) do |dropdown|
+      dropdown.with_item(label: 'Item 1', url: '#')
+      dropdown.with_item(label: 'Item 2', url: '#')
+      dropdown.with_item(label: 'Item 3', url: '#')
+    end
+  end
 end
