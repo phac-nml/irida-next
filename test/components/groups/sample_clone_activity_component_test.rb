7555fae419b4a2aa68df5ac9694a3aa93fbb52c6
Chris Huynh <chris.huynh333@gmail.com>
2025-05-21 16:20:28 -0500

    add activities tests

---
+++ test/components/groups/sample_clone_activity_component_test.rb
@@ -0,0 +1,169 @@
+# frozen_string_literal: true
+
+require 'view_component_test_case'
+
+module Groups
+  class SampleCloneActivityComponentTest < ViewComponentTestCase
+    include ActionView::Helpers::SanitizeHelper
+
+    setup do
+      @user = users(:john_doe)
+    end
+
+    test 'group sample clone source and target activities' do
+      group = groups(:group_one)
+      project_namespace = namespaces_project_namespaces(:project2_namespace)
+      sample1 = samples(:sample1)
+      sample2 = samples(:sample2)
+      Groups::Samples::CloneService.new(group, @user)
+                                   .execute(
+                                     project_namespace.project.id,
+                                     [sample1.id, sample2.id],
+                                     nil
+                                   )
+
+      activities = group.human_readable_activity(group.retrieve_group_activity).reverse
+
+      assert_equal(1, activities.count do |activity|
+        activity[:key].include?('group.samples.clone')
+      end)
+
+      activity_to_render = activities.find do |a|
+        a[:key] == 'activity.group.samples.clone_html'
+      end
+
+      render_inline Activities::Groups::SampleCloneActivityComponent.new(activity: activity_to_render)
+
+      assert_text strip_tags(
+        I18n.t('activity.group.samples.clone_html', user: @user.email,
+                                                    cloned_samples_count: activity_to_render[:cloned_samples_count],
+                                                    href: activity_to_render[:target_project_puid])
+      )
+      assert_selector 'span', text: 2
+
+      assert_selector 'a', text: I18n.t(:'components.activity.more_details')
+
+      activities = project_namespace.human_readable_activity(project_namespace.retrieve_project_activity).reverse
+
+      assert_equal(2, activities.count do |activity|
+        activity[:key].include?('project_namespace.samples.cloned_from')
+      end)
+
+      activity_to_render = activities.find do |a|
+        a[:key] == 'activity.namespaces_project_namespace.samples.cloned_from_html'
+      end
+
+      render_inline Activities::Projects::SampleCloneActivityComponent.new(activity: activity_to_render)
+
+      assert_text strip_tags(
+        I18n.t(
+          'activity.namespaces_project_namespace.samples.cloned_from_html',
+          user: @user.email,
+          href: activity_to_render[:source_project_puid],
+          cloned_samples_count: activity_to_render[:cloned_samples_count]
+        )
+      )
+
+      assert_selector 'a',
+                      text: activity_to_render[:source_project_puid]
+
+      assert_selector 'a', text: I18n.t(:'components.activity.more_details')
+    end
+
+    test 'sample clone target project actvity' do
+      project_namespace = namespaces_project_namespaces(:project2_namespace)
+
+      activities = project_namespace.human_readable_activity(project_namespace.retrieve_project_activity).reverse
+
+      assert_equal(1, activities.count do |activity|
+        activity[:key].include?('project_namespace.samples.cloned_from')
+      end)
+
+      activity_to_render = activities.find do |a|
+        a[:key] == 'activity.namespaces_project_namespace.samples.cloned_from_html'
+      end
+
+      render_inline Activities::Projects::SampleCloneActivityComponent.new(activity: activity_to_render)
+
+      assert_text strip_tags(
+        I18n.t(
+          'activity.namespaces_project_namespace.samples.cloned_from_html',
+          user: 'System',
+          href: activity_to_render[:source_project_puid],
+          cloned_samples_count: activity_to_render[:cloned_samples_count]
+        )
+      )
+
+      assert_selector 'a',
+                      text: activity_to_render[:source_project_puid]
+
+      assert_selector 'a', text: I18n.t(:'components.activity.more_details')
+    end
+
+    test 'sample clone source project deleted actvity' do
+      project_namespace = namespaces_project_namespaces(:project2_namespace)
+      project1 = projects(:project1)
+
+      project1.really_destroy!
+
+      activities = project_namespace.human_readable_activity(project_namespace.retrieve_project_activity).reverse
+
+      assert_equal(1, activities.count do |activity|
+        activity[:key].include?('project_namespace.samples.cloned_from')
+      end)
+
+      activity_to_render = activities.find do |a|
+        a[:key] == 'activity.namespaces_project_namespace.samples.cloned_from_html'
+      end
+
+      render_inline Activities::Projects::SampleCloneActivityComponent.new(activity: activity_to_render)
+
+      assert_text strip_tags(
+        I18n.t(
+          'activity.namespaces_project_namespace.samples.cloned_from_html',
+          user: 'System',
+          href: activity_to_render[:source_project_puid],
+          cloned_samples_count: activity_to_render[:cloned_samples_count]
+        )
+      )
+
+      assert_selector 'span',
+                      text: activity_to_render[:source_project_puid]
+
+      assert_selector 'a', text: I18n.t(:'components.activity.more_details')
+    end
+
+    test 'sample clone target project deleted actvity' do
+      project_namespace = namespaces_project_namespaces(:project1_namespace)
+      project2 = projects(:project2)
+
+      project2.really_destroy!
+
+      activities = project_namespace.human_readable_activity(project_namespace.retrieve_project_activity).reverse
+
+      assert_equal(1, activities.count do |activity|
+        activity[:key].include?('project_namespace.samples.clone')
+      end)
+
+      activity_to_render = activities.find do |a|
+        a[:key] == 'activity.namespaces_project_namespace.samples.clone_html'
+      end
+
+      render_inline Activities::Projects::SampleCloneActivityComponent.new(activity: activity_to_render)
+
+      assert_text strip_tags(
+        I18n.t(
+          'activity.namespaces_project_namespace.samples.clone_html',
+          user: 'System',
+          href: activity_to_render[:target_project_puid],
+          cloned_samples_count: activity_to_render[:cloned_samples_count]
+        )
+      )
+
+      assert_selector 'span',
+                      text: activity_to_render[:target_project_puid]
+
+      assert_selector 'a', text: I18n.t(:'components.activity.more_details')
+    end
+  end
+end
