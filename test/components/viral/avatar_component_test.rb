6a5a2c19fd0a025ac9ebdf402b7522c14c2a5aaf
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-05-21 15:02:50 -0500

    refactor: change avatar component base element from div to span for semantic HTML

--- test/components/viral/avatar_component_test.rb
+++ test/components/viral/avatar_component_test.rb
@@ -6,22 +6,22 @@ module Viral
   class AvatarComponentTest < ViewComponentTestCase
     test 'default avatar' do
       render_inline(Viral::AvatarComponent.new(name: 'J'))
-      assert_selector('div.avatar')
-      assert_selector('div.w-12.h-12')
+      assert_selector('span.avatar')
+      assert_selector('span.w-12.h-12')
       assert_text('J')
     end
 
     test 'small avatar' do
       render_inline(Viral::AvatarComponent.new(name: 'J', size: :small))
-      assert_selector('div.avatar')
-      assert_selector('div.w-8.h-8')
+      assert_selector('span.avatar')
+      assert_selector('span.w-8.h-8')
       assert_text('J')
     end
 
     test 'large avatar' do
       render_inline(Viral::AvatarComponent.new(name: 'J', size: :large))
-      assert_selector('div.avatar')
-      assert_selector('div.w-16.h-16')
+      assert_selector('span.avatar')
+      assert_selector('span.w-16.h-16')
       assert_text('J')
     end
 
