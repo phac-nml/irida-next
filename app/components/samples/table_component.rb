42a4109fecc6127e3d745f00d899568e2bacc1ba
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-05-22 11:47:40 -0500

    chore: fix sample table component to reconnect editable cell controller on morph so that cells are still editable

--- app/components/samples/table_component.rb
+++ app/components/samples/table_component.rb
@@ -54,7 +54,8 @@ module Samples
       {
         tag: 'div',
         classes: class_names('table-container @2xl:flex @2xl:flex-col @3xl:shrink @3xl:min-h-0'),
-        'data-controller' => 'editable-cell'
+        'data-controller' => 'editable-cell',
+        'data-action' => 'turbo:morph-element->editable-cell#idempotentConnect'
       }
     end
 
