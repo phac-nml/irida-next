9a36b110554a644aff28335418a476cf66332b0c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-05-30 11:05:32 -0500

    feat(clipboard): update clipboard component to support dynamic titles and improve localization
    - Enhanced ClipboardComponent to accept a dynamic title for improved accessibility and user experience.
    - Updated HTML to utilize the new title attribute for ARIA labels and tooltips.
    - Modified localization files to include dynamic title translations for both English and French.
    - Adjusted component previews to reflect the new title functionality.

--- app/components/clipboard_component.rb
+++ app/components/clipboard_component.rb
@@ -2,13 +2,14 @@
 
 # Clipboard component for copying to clipboard
 class ClipboardComponent < Viral::Component
-  def initialize(value:, button_classes: '', button_name: nil)
+  def initialize(value:, title:, button_classes: '', button_name: nil)
     @value = value
+    @title = title
     @button_classes = class_names(button_classes, 'cursor-pointer ml-1')
     @button_name = button_name || "clipboard-button-#{SecureRandom.hex(4)}"
   end
 
   private
 
-  attr_reader :value, :button_classes, :button_name
+  attr_reader :value, :button_classes, :button_name, :title
 end
