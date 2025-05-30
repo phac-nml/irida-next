bf1dc1f267a29271ac1a2a0a7aab224333e29126
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-05-30 10:05:45 -0500

    feat(clipboard): enhance clipboard component with dynamic button naming and improved accessibility
    - Added dynamic button naming to the ClipboardComponent, allowing for unique identifiers.
    - Updated HTML structure to include ARIA attributes for better accessibility, including aria-describedby for the button and aria-live for the tooltip.
    - Improved semantic markup by assigning IDs to content and tooltip elements for enhanced user experience.

--- app/components/clipboard_component.rb
+++ app/components/clipboard_component.rb
@@ -2,12 +2,13 @@
 
 # Clipboard component for copying to clipboard
 class ClipboardComponent < Viral::Component
-  def initialize(value:, button_classes: '')
+  def initialize(value:, button_classes: '', button_name: nil)
     @value = value
     @button_classes = class_names(button_classes, 'cursor-pointer ml-1')
+    @button_name = button_name || "clipboard-button-#{SecureRandom.hex(4)}"
   end
 
   private
 
-  attr_reader :value, :button_classes
+  attr_reader :value, :button_classes, :button_name
 end
