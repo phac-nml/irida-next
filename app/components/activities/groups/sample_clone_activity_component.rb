fe2f0c69041000d516b986796c189cddd1e054a9
Chris Huynh <chris.huynh333@gmail.com>
2025-06-02 15:39:17 -0500

    fix group activity logic now that we aren't passing project

--- app/components/activities/groups/sample_clone_activity_component.rb
+++ app/components/activities/groups/sample_clone_activity_component.rb
@@ -7,19 +7,6 @@ module Activities
       def initialize(activity: nil)
         @activity = activity
       end
-
-      def namespace_puid(namespace)
-        return namespace.puid unless namespace.nil?
-        return @activity[:target_project_puid] if @activity[:target_project_puid]
-
-        @activity[:source_project_puid]
-      end
-
-      def project_exists(namespace)
-        return false if namespace.nil?
-
-        !namespace.deleted? && !namespace.project.deleted?
-      end
     end
   end
 end
