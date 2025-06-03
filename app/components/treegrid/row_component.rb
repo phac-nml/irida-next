8607cb406d3bcd5db430332b06cee25a0a3ec0c7
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-06-03 14:30:41 -0500

    chore: updated treegrid row component to pad based on level, and update it to accept required aria attributes

--- app/components/treegrid/row_component.rb
+++ app/components/treegrid/row_component.rb
@@ -4,17 +4,25 @@ module Treegrid
   # Treegrid Row component
   class RowComponent < Component
     erb_template <<-ERB
-      <div role="row" <%= @expandable ? 'aria-expanded="#{@expanded}"' : '' %> tabindex="<%= @tabindex %>" data-treegrid-target="row">
+      <%= tag.div(class: "treegrid-row", role: "row", aria: @aria, data: @data, style: @style, tabindex: @tabindex) do %>
         <div role="gridcell" aria-colindex="1">
           <%= content %>
         </div>
-      </div>
+      <% end %>
     ERB
 
-    def initialize(expanded: false, expandable: false, tabindex: '-1')
-      @expandable = expandable
-      @expanded = expanded
+    def initialize(expanded: false, expandable: false, tabindex: '-1', level: '1', posinset: '1', setsize: '1') # rubocop:disable Metrics/ParameterLists
       @tabindex = tabindex
+      @level = level
+
+      @aria = {
+        level: level,
+        posinset: posinset,
+        setsize: setsize
+      }
+      @aria[:expanded] = expanded if expandable
+      @data = { 'treegrid-target': 'row' }
+      @style = "--treegrid-level: #{level};"
     end
   end
 end
