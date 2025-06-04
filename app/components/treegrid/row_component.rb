8e789a6ce81d131a4771fbdf4e278743fece2077
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-06-04 11:17:05 -0500

    cleaned up treegrid row component and added the ability to pass system arguments for the row

--- app/components/treegrid/row_component.rb
+++ app/components/treegrid/row_component.rb
@@ -4,7 +4,7 @@ module Treegrid
   # Treegrid Row component
   class RowComponent < Component
     erb_template <<-ERB
-      <%= tag.div(class: "treegrid-row", role: "row", aria: @aria, data: @data, style: @style, tabindex: @tabindex) do %>
+      <%= tag.div(**@system_arguments) do %>
         <div role="gridcell" aria-colindex="1">
           <button class="treegrid-row-toggle" type="button" tabindex="-1" data-action="click->treegrid#toggleRow">
             <%= viral_icon(name: "chevron_right", classes: "size-4") %>
@@ -14,18 +14,30 @@ module Treegrid
       <% end %>
     ERB
 
-    def initialize(expanded: false, expandable: false, tabindex: '-1', level: '1', posinset: '1', setsize: '1') # rubocop:disable Metrics/ParameterLists
-      @tabindex = tabindex
-      @level = level
+    def initialize(
+      expanded: false,
+      expandable: false,
+      tabindex: -1,
+      level: 1,
+      posinset: 1,
+      setsize: 1,
+      **system_arguments
+    )
+      @system_arguments = system_arguments
 
-      @aria = {
-        level: level,
-        posinset: posinset,
-        setsize: setsize
-      }
-      @aria[:expanded] = expanded if expandable
-      @data = { 'treegrid-target': 'row' }
-      @style = "--treegrid-level: #{level};"
+      @system_arguments[:aria] = (@system_arguments[:aria] || {}).deep_merge({
+                                                                               level: level,
+                                                                               posinset: posinset,
+                                                                               setsize: setsize
+                                                                             })
+      @system_arguments[:aria][:expanded] = expanded if expandable
+      @system_arguments[:data] = {} unless @system_arguments.key?(:data)
+      @system_arguments[:data]['treegrid-target'] = 'row'
+      @system_arguments[:style] = "--treegrid-level: #{level};"
+      @system_arguments[:class] = class_names(@system_arguments[:classes], 'treegrid-row')
+      @system_arguments.delete(:classes)
+      @system_arguments[:role] = 'row'
+      @system_arguments[:tabindex] = tabindex
     end
   end
 end
