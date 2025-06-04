2478585b92cb13e0e7ba17e865df13cc4972da10
Chris Huynh <chris.huynh333@gmail.com>
2025-06-04 11:43:03 -0500

    move creating group activity into its own function to cleanup

--- app/services/groups/samples/destroy_service.rb
+++ app/services/groups/samples/destroy_service.rb
@@ -26,7 +26,7 @@ module Groups
         @deleted_samples_data[:group_data].count
       end
 
-      def create_activities_and_update_samples_count # rubocop:disable Metrics/MethodLength
+      def create_activities_and_update_samples_count
         total_deleted_samples_count = 0
 
         @deleted_samples_data[:project_data].each do |project_puid, sample_data|
@@ -40,6 +40,10 @@ module Groups
           total_deleted_samples_count += samples_deleted_count
         end
 
+        create_group_activity(total_deleted_samples_count)
+      end
+
+      def create_group_activity(total_deleted_samples_count)
         group_ext_details = ExtendedDetail.create!(
           details: {
             deleted_samples_data: @deleted_samples_data[:group_data],
