7427eaf8b94d13259a09272118801300cc30d25e
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-06-04 15:27:49 -0500

    chore: converted dashboard groups index to use treegrid component

--- app/components/treegrid/row_component.rb
+++ app/components/treegrid/row_component.rb
@@ -5,25 +5,33 @@ module Treegrid
   class RowComponent < Component
     erb_template <<-ERB
       <%= tag.div(**@system_arguments) do %>
-        <div role="gridcell" aria-colindex="1">
-          <button class="treegrid-row-toggle" type="button" tabindex="-1" data-action="click->treegrid#toggleRow">
+        <div class="flex items-baseline py-2 px-2" role="gridcell" aria-colindex="1">
+          <%= tag.button(**@button_arguments) do %>
             <%= viral_icon(name: "chevron_right", classes: "size-4") %>
-          </button>
+          <% end %>
           <%= content %>
         </div>
       <% end %>
     ERB
 
-    def initialize(
+    def initialize( # rubocop:disable Metrics/ParameterLists
       expanded: false,
       expandable: false,
       tabindex: -1,
       level: 1,
       posinset: 1,
       setsize: 1,
+      button_arguments: {},
       **system_arguments
     )
+      @button_arguments = button_arguments
       @system_arguments = system_arguments
+
+      @button_arguments[:data] ||= {}
+      @button_arguments[:data][:action] = 'click->treegrid#toggleRow'
+      @button_arguments[:type] = 'button'
+      @button_arguments[:class] = 'treegrid-row-toggle h-8 w-8 cursor-pointer hover:bg-slate-100 rounded-lg'
+      @button_arguments[:tabindex] = '-1'
 
       @system_arguments[:aria] = (@system_arguments[:aria] || {}).deep_merge({
                                                                                level: level,
@@ -34,7 +42,7 @@ module Treegrid
       @system_arguments[:data] = {} unless @system_arguments.key?(:data)
       @system_arguments[:data]['treegrid-target'] = 'row'
       @system_arguments[:style] = "--treegrid-level: #{level};"
-      @system_arguments[:class] = class_names(@system_arguments[:classes], 'treegrid-row')
+      @system_arguments[:class] = class_names(@system_arguments[:classes], 'treegrid-row rounded-lg')
       @system_arguments.delete(:classes)
       @system_arguments[:role] = 'row'
       @system_arguments[:tabindex] = tabindex
