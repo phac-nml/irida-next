a30deee9ca39e2c879603bca96b6e2b31e1a8114
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-06-06 10:08:31 -0500

    refactor: update page header component tests for improved selector accuracy
    - Replaced text assertions with more specific selector assertions for title and subtitle elements.
    - Ensured button and avatar assertions are aligned with the updated component structure.
    - Enhanced test clarity and maintainability by using unique identifiers for elements.

--- test/components/viral/page_header_component_test.rb
+++ test/components/viral/page_header_component_test.rb
@@ -6,22 +6,22 @@ class PageHeaderComponentTest < ApplicationSystemTestCase
   test 'renders header' do
     visit('/rails/view_components/viral_page_header_component/with_icon')
 
-    assert_text 'Page header'
-    assert_text 'This is a page header'
+    assert_selector '#page-title', text: 'Page header'
+    assert_selector '#page-subtitle', text: 'This is a page header'
+    assert_selector 'section svg.icon-users', count: 1
   end
 
   test 'renders header with buttons' do
     visit('/rails/view_components/viral_page_header_component/with_buttons')
-    assert_selector 'div.page-header .button', count: 1
-  end
-
-  test 'renders header with icon' do
-    visit('/rails/view_components/viral_page_header_component/with_icon')
-    assert_selector 'div.page-header svg'
+    assert_selector '#page-title', text: 'Page header'
+    assert_selector '#page-subtitle', text: 'This is a page header'
+    assert_selector 'section .button', count: 1
   end
 
   test 'renders header with avatar' do
     visit('/rails/view_components/viral_page_header_component/with_avatar')
-    assert_selector 'div.page-header span.avatar'
+    assert_selector '#page-title', text: 'Page header'
+    assert_selector '#page-subtitle', text: 'This is a page header'
+    assert_selector 'section span.avatar'
   end
 end
