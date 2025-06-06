62a2e47c667d0aaa4776c8df72db9b07852ff02b
Chris Huynh <chris.huynh333@gmail.com>
2025-06-06 11:46:55 -0500

    start refactor

---
+++ app/controllers/samples/clones_controller.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Samples
+  # controller for sample cloning
+  class ClonesController < ApplicationController
+    respond_to :turbo_stream
+    before_action :projects, :ensure_enabled
+
+    def new
+      authorize! (@namespace.group_namespace? ? @namespace : @namespace.project), to: :clone_sample?
+      @broadcast_target = "samples_clone_#{SecureRandom.uuid}"
+    end
+
+    def create
+      @broadcast_target = params[:broadcast_target]
+      new_project_id = clone_params[:new_project_id]
+      sample_ids = clone_params[:sample_ids]
+
+      Samples::CloneJob.set(wait_until: 1.second.from_now).perform_later(
+        @namespace, current_user, new_project_id, sample_ids, @broadcast_target
+      )
+
+      render status: :ok
+    end
+
+    private
+
+    def clone_params
+      params.expect(clone: [:new_project_id, { sample_ids: [] }])
+    end
+
+    def projects
+      @projects = authorized_scope(Project, type: :relation, as: :manageable)
+
+      return unless @namespace.project_namespace?
+
+      @projects = @projects.where.not(namespace_id: @project.namespace_id)
+    end
+
+    def ensure_enabled
+      not_found unless Flipper.enabled?(:group_samples_clone)
+    end
+  end
+end
---
+++ app/jobs/samples/clone_job.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module Samples
+  # Job used to clone samples
+  class CloneJob < ApplicationJob
+    queue_as :default
+    queue_with_priority 15
+
+    def perform(namespace, current_user, new_project_id, sample_ids, broadcast_target) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+      service = if namespace.group_namespace?
+                  Groups::Samples::CloneService.new(namespace, current_user)
+                else
+                  Projects::Samples::CloneService.new(namespace, current_user)
+                end
+      @cloned_sample_ids = service.execute(
+        new_project_id,
+        sample_ids,
+        Flipper.enabled?(:progress_bars) ? broadcast_target : nil
+      )
+
+      if namespace.errors.empty?
+        Turbo::StreamsChannel.broadcast_replace_to(
+          broadcast_target,
+          target: 'clone_samples_dialog_content',
+          partial: 'shared/samples/success',
+          locals: {
+            type: :success,
+            message: I18n.t('shared.samples.clones.create.success')
+          }
+        )
+      elsif namespace.errors.include?(:samples)
+        errors = namespace.errors.messages_for(:samples)
+        Turbo::StreamsChannel.broadcast_replace_to(
+          broadcast_target,
+          target: 'clone_samples_dialog_content',
+          partial: 'shared/samples/errors',
+          locals: {
+            type: :alert,
+            message: I18n.t('shared.samples.clones.create.error'),
+            errors: errors
+          }
+        )
+      else
+        errors = namespace.errors.full_messages_for(:base)
+        Turbo::StreamsChannel.broadcast_replace_to(
+          broadcast_target,
+          target: 'clone_samples_dialog_content',
+          partial: 'shared/samples/errors',
+          locals: {
+            type: :alert,
+            message: I18n.t('shared.samples.clones.create.no_samples_cloned_error'),
+            errors: errors
+          }
+        )
+      end
+    end
+  end
+end
