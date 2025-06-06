053120af6d98cf2dba12b4d67650ecca6e422cf7
Deep Sidhu <deepsidhu85@gmail.com>
2025-06-06 17:48:19 -0500

    Added tests and updated logic for initial setup controller

---
+++ test/controllers/admin/initial_setup_controller_test.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Admin
+  class CheckInitialSetupControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      @user = users(:john_doe)
+      sign_in @user
+    end
+
+    test 'update only user to an admin if in initial setup state' do
+      users = User.all - [@user]
+      users.each(&:destroy)
+
+      get admin_initial_setup_url(id: @user.id, initial_setup: true)
+
+      assert @user.reload.admin?
+
+      # redirect to sign in page after initial account setup
+      assert_response :redirect, to: new_user_session_path
+    end
+
+    test 'if an account already exists a new one should now be setup as an admin' do
+      user = User.last
+
+      get admin_initial_setup_url(id: user.id, initial_setup: true)
+
+      assert_not @user.reload.admin?
+
+      # redirect to dashboard
+      assert_response :redirect, to: root_path
+    end
+
+    test 'No initial setup completed' do
+      users = User.all
+      users.each(&:destroy)
+
+      get admin_initial_setup_url(id: '123', initial_setup: true)
+
+      # redirect to dashboard
+      assert_response :redirect, to: new_user_registration_path
+    end
+  end
+end
---
+++ test/services/users/update_service_test.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Users
+  class UpdateServiceTest < ActiveSupport::TestCase
+    def setup
+      @user = users(:john_doe)
+    end
+
+    test 'update user params' do
+      params = { email: 'newemail@email.com', first_name: 'new_first_name', last_name: 'new_last_name' }
+
+      assert_changes lambda {
+        [@user.email, @user.first_name, @user.last_name]
+      }, to: %w[newemail@email.com new_first_name new_last_name] do
+        Users::UpdateService.new(@user, @user, params).execute
+      end
+    end
+
+    test 'update user to admin during initial setup' do
+      users = User.all - [@user]
+      users.each(&:destroy)
+
+      assert_not @user.admin?
+
+      params = { admin: true, initial_setup: true }
+
+      assert_changes lambda {
+        @user.admin
+      }, to: true do
+        Users::UpdateService.new(nil, @user, params).execute
+      end
+    end
+
+    test 'admin can update users' do
+      user = User.all.last
+      params = { email: 'newemail@email.com', first_name: 'new_first_name', last_name: 'new_last_name' }
+
+      assert_changes lambda {
+        [user.email, user.first_name, user.last_name]
+      }, to: %w[newemail@email.com new_first_name new_last_name] do
+        Users::UpdateService.new(@user, user, params).execute
+      end
+    end
+  end
+end
