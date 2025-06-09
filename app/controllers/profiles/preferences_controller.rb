8fc2500a6a020b169217b219859e3a2c6a259e99
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-06-09 14:52:16 -0500

    fixing tests

--- app/controllers/profiles/preferences_controller.rb
+++ app/controllers/profiles/preferences_controller.rb
@@ -14,7 +14,7 @@ module Profiles
       @current_page = t(:'profiles.sidebar.preferences')
     end
 
-    def update
+    def update # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
       authorize! @user
       respond_to do |format|
         # Locale is called now rather than from the around_action in app_controller because we need the locale
@@ -22,16 +22,22 @@ module Profiles
         updated = @user.update(update_params)
         if updated
           I18n.with_locale(current_user.locale) do
+            flash[:success] = t('.success')
             format.turbo_stream do
-              render status: :ok, locals: { type: 'success', message: t('.success') }
+              render status: :ok
             end
             format.html do
-              flash[:success] = t('.success')
               redirect_back_or_to profile_preferences_path
             end
           end
         else
-          format.html { render :show, status: :unprocessable_entity, locals: { user: @user } }
+          flash[:error] = t('.error')
+          format.turbo_stream do
+            render status: :unprocessable_entity
+          end
+          format.html do
+            render :show, status: :unprocessable_entity, locals: { user: @user }
+          end
         end
       end
     end
