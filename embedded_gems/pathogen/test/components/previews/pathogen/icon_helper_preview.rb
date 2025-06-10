c34a026bad8b89f06994585c23bd85fdd5158c90
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-06-10 15:50:44 -0500

    refactor(icon_helper_preview): Enhance IconHelperPreview with additional icon configurations and types

--- embedded_gems/pathogen/test/components/previews/pathogen/icon_helper_preview.rb
+++ embedded_gems/pathogen/test/components/previews/pathogen/icon_helper_preview.rb
@@ -1,45 +1,48 @@
 # frozen_string_literal: true
 
-class Pathogen::IconHelperPreview < ViewComponent::Preview
-  # @!group Basic Usage
-  def default
-    render_with_template(locals: { icon: Pathogen::ICON::CLIPBOARD })
-  end
+module Pathogen
+  # Preview class for demonstrating the IconHelper functionality with various icon types and configurations.
+  class IconHelperPreview < ViewComponent::Preview
+    # @!group Basic Usage
+    def default
+      render_with_template(locals: { icon: Pathogen::ICON::CLIPBOARD })
+    end
 
-  def with_custom_class
-    render_with_template(locals: { icon: Pathogen::ICON::CLIPBOARD, class: 'h-8 w-8 text-primary-600' })
-  end
+    def with_custom_class
+      render_with_template(locals: { icon: Pathogen::ICON::CLIPBOARD, class: 'h-8 w-8 text-primary-600' })
+    end
 
-  def with_data_attributes
-    render_with_template(locals: { icon: Pathogen::ICON::CLIPBOARD, data: { 'test-selector': 'custom-icon' } })
-  end
-  # @!endgroup
+    def with_data_attributes
+      render_with_template(locals: { icon: Pathogen::ICON::CLIPBOARD, data: { 'test-selector': 'custom-icon' } })
+    end
+    # @!endgroup
 
-  # @!group Icon Types
-  def phosphor_icon
-    render_with_template(locals: { icon: Pathogen::ICON::CLIPBOARD })
-  end
+    # @!group Icon Types
+    def phosphor_icon
+      render_with_template(locals: { icon: Pathogen::ICON::CLIPBOARD })
+    end
 
-  def heroicon
-    render_with_template(locals: { icon: Pathogen::ICON::BEAKER })
-  end
+    def heroicon
+      render_with_template(locals: { icon: Pathogen::ICON::BEAKER })
+    end
 
-  def named_icon
-    render_with_template(locals: { icon: Pathogen::ICON::IRIDA_LOGO })
-  end
-  # @!endgroup
+    def named_icon
+      render_with_template(locals: { icon: Pathogen::ICON::IRIDA_LOGO })
+    end
+    # @!endgroup
 
-  # @!group Common Icons
-  def clipboard
-    render_with_template(locals: { icon: Pathogen::ICON::CLIPBOARD })
-  end
+    # @!group Common Icons
+    def clipboard
+      render_with_template(locals: { icon: Pathogen::ICON::CLIPBOARD })
+    end
 
-  def user_circle
-    render_with_template(locals: { icon: Pathogen::ICON::USER_CIRCLE })
-  end
+    def user_circle
+      render_with_template(locals: { icon: Pathogen::ICON::USER_CIRCLE })
+    end
 
-  def gear_six
-    render_with_template(locals: { icon: Pathogen::ICON::GEAR_SIX })
+    def gear_six
+      render_with_template(locals: { icon: Pathogen::ICON::GEAR_SIX })
+    end
+    # @!endgroup
   end
-  # @!endgroup
 end
