09965b3b4c333d64c55c856425a875b3f32b3527
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-06-11 13:27:56 -0500

    setup

--- app/javascript/controllers/viral/dialog_controller.js
+++ app/javascript/controllers/viral/dialog_controller.js
@@ -1,9 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
+import { createFocusTrap } from "focus-trap";
 
 export default class extends Controller {
   static targets = ["dialog"];
 
   static values = { open: Boolean };
+
+  #focusTrap = null;
+
+  initialize() {
+    console.log("initialize");
+
+    // const container = document.getElementById("dialog");
+    const container = this.dialogTarget;
+
+    this.#focusTrap = createFocusTrap(container, {
+      onActivate: () => container.classList.add("is-active"),
+      onDeactivate: () => container.classList.remove("is-active"),
+    });
+  }
 
   connect() {
     if (this.openValue) this.open();
@@ -17,11 +32,13 @@ export default class extends Controller {
   open() {
     this.openValue = true;
     this.dialogTarget.showModal();
+    this.#focusTrap.activate();
   }
 
   close() {
     this.openValue = false;
     this.dialogTarget.close();
+    this.#focusTrap.deactivate();
   }
 
   handleEsc(event) {
