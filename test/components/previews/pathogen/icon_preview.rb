af64f106b052adccba9fa623d89edb38a8283c80
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-06-13 14:24:47 -0500

    refactor(icon_preview): Organize Pathogen::IconPreview class structure for improved readability and maintainability

--- test/components/previews/pathogen/icon_preview.rb
+++ test/components/previews/pathogen/icon_preview.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-class Pathogen::IconPreview < ViewComponent::Preview
-  # @!group Icons
-  # @label Phosphor Icons
-  # @description Display all available Phosphor icons in a grid layout
-  def phosphor_icons
-  end
+module Pathogen
+  class IconPreview < ViewComponent::Preview
+    # @!group Icons
+    # @label Phosphor Icons
+    # @description Display all available Phosphor icons in a grid layout
+    def phosphor_icons; end
 
-  # @label Heroicons
-  # @description Display all available Heroicons in a grid layout
-  def heroicons
-  end
+    # @label Heroicons
+    # @description Display all available Heroicons in a grid layout
+    def heroicons; end
 
-  # @!endgroup
+    # @!endgroup
+  end
 end
