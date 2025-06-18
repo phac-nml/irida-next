97eb1ee35b3d91f866655b1d29f1b0aea227eb64
Chris Huynh <chris.huynh333@gmail.com>
2025-06-18 17:11:23 -0500

    start custom datepicker

---
+++ app/components/viral/form/test_datepicker_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Viral
+  module Form
+    # Form text input component (numbers, email, text, etc.)
+    class TestDatepickerComponent < Viral::Component
+      attr_reader :container, :name, :value, :required, :pattern, :placeholder
+
+      # def initialize(container:, name:, value: nil, required: nil, pattern: nil, placeholder: nil)
+      def initialize
+        # @container = container
+        # @name = name
+        # @value = value
+        # @required = required
+        # @pattern = pattern
+        # @placeholder = placeholder
+      end
+    end
+  end
+end
