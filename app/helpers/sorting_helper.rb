babbf7608fbaaebcec0e498247b10077caecaf07
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-06-19 15:05:33 -0500

    retaining focus on sort dropdown of project dashboard page

--- app/helpers/sorting_helper.rb
+++ app/helpers/sorting_helper.rb
@@ -16,7 +16,8 @@ module SortingHelper
                        icon_name: active_sort(ransack_obj, field, dir) ? 'check' : 'blank',
                        prefix:,
                        data: {
-                         turbo_stream: true
+                         turbo_stream: true,
+                         turbo_action: 'replace'
                        },
                        **add_aria_current(ransack_obj, field, dir))
   end
