30e5285790403e03a213df7b966127c3d09e81d7
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-06-20 11:10:26 -0500

    feat(view_helper): Add ViewHelper module for component rendering support refactor(icon_preview): Remove descriptions from icon preview methods for clarity

--- test/components/previews/pathogen/icon_preview.rb
+++ test/components/previews/pathogen/icon_preview.rb
@@ -4,19 +4,15 @@ module Pathogen
   class IconPreview < ViewComponent::Preview
     # @!group Icons
     # @label Phosphor Icons
-    # @description Display all available Phosphor icons in a grid layout
     def phosphor_icons; end
 
     # @label Heroicons
-    # @description Display all available Heroicons in a grid layout
     def heroicons; end
 
     # @label Named Icons
-    # @description Display all available Named icons in a grid layout
     def named_icons; end
 
     # @label Special Icons
-    # @description Display all special/semantic icons in a grid layout
     def special_icons; end
 
     # @!endgroup
