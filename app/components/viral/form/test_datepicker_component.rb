fe227b733021f6ff765691d86af45779493f6122
Chris Huynh <chris.huynh333@gmail.com>
2025-06-23 13:59:02 -0500

    add styling for date types; add logic for minDate

--- app/components/viral/form/test_datepicker_component.rb
+++ app/components/viral/form/test_datepicker_component.rb
@@ -4,13 +4,28 @@ module Viral
   module Form
     # Form text input component (numbers, email, text, etc.)
     class TestDatepickerComponent < Viral::Component
-      attr_reader :id, :min_date, :selected_date
+      attr_reader :id, :min_date, :selected_date, :months, :min_year
 
       # def initialize(container:, name:, value: nil, required: nil, pattern: nil, placeholder: nil)
       def initialize(id: 'datepicker', min_date: nil, selected_date: nil)
         @id = id
         @min_date = min_date
         @selected_date = selected_date
+        @months = %w[
+          January
+          February
+          March
+          April
+          May
+          June
+          July
+          August
+          September
+          October
+          November
+          December
+        ]
+        @min_year = min_date.nil? ? '1' : min_date.split('-')[0]
       end
     end
   end
