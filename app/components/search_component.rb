cf0d82900396b41073fb36c14eb607d1deb40687
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-06-25 11:38:58 -0500

    adding search role to search forms

--- app/components/search_component.rb
+++ app/components/search_component.rb
@@ -7,13 +7,14 @@ class SearchComponent < Component
   include Ransack::Helpers::FormHelper
 
   # rubocop:disable Metrics/ParameterLists
-  def initialize(query:, url:, search_attribute:, placeholder:, value: nil, **kwargs)
+  def initialize(query:, url:, search_attribute:, placeholder:, value: nil, **system_arguments)
     @query = query
     @url = url
     @value = value
     @search_attribute = search_attribute
     @placeholder = placeholder
-    @kwargs = kwargs
+    @system_arguments = system_arguments
+    @system_arguments[:html] = (@system_arguments[:html] || {}).merge({ role: :search })
   end
   # rubocop:enable Metrics/ParameterLists
 end
