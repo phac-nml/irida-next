1c36958dde2607c60527d5180131605599b115fd
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-07-02 22:43:41 -0500

    fixing test & adding translations

--- test/components/members/search_component_test.rb
+++ test/components/members/search_component_test.rb
@@ -7,8 +7,9 @@ class SearchComponentTest < ViewComponent::TestCase
     url = '/-/groups/group-1/-/members'
     search_attribute = :user_email_cont
     placeholder = 'a placeholder'
+    total_count = 0
 
-    render_inline SearchComponent.new(query: Member.ransack, url:, search_attribute:, placeholder:)
+    render_inline SearchComponent.new(query: Member.ransack, url:, search_attribute:, placeholder:, total_count:)
 
     assert_selector "form[action='#{url}']", count: 1
     assert_selector "label[for='q_#{search_attribute}']", count: 1
@@ -17,13 +18,18 @@ class SearchComponentTest < ViewComponent::TestCase
   end
 
   test 'accessibility' do
-    url = '/-/groups/group-1/-/members'
-    search_attribute = :user_email_cont
-    placeholder = 'a placeholder'
+    with_request_url '/-/groups/group-1/-/members?q%5Bs%5D=user_email+asc&q%5Buser_email_cont%5D=user&tab=&format=turbo_stream' do # rubocop:disable Layout/LineLength
+      url = '/-/groups/group-1/-/members?q%5Bs%5D=user_email+asc&q%5Buser_email_cont%5D=user&tab=&format=turbo_stream'
+      search_attribute = :user_email_cont
+      placeholder = 'a placeholder'
+      total_count = 0
+      search_term = 'user'
 
-    render_inline SearchComponent.new(query: Member.ransack, url:, search_attribute:, placeholder:)
+      render_inline SearchComponent.new(query: Member.ransack, url:, search_attribute:, placeholder:, total_count:)
 
-    assert_selector 'form[role^="search"]', count: 1
-    assert_selector 'input[type^="search"]', count: 1
+      assert_selector 'div[role^="status"]', count: total_count,
+                                             text: I18n.t(:'components.search.results_message.zero',
+                                                          search_term:)
+    end
   end
 end
