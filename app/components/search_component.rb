ff1997856acecb0a029626f035550acd37137a6a
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-07-02 21:33:46 -0500

    moving the screen reader announcement to the search component

--- app/components/search_component.rb
+++ app/components/search_component.rb
@@ -7,13 +7,24 @@ class SearchComponent < Component
   include Ransack::Helpers::FormHelper
 
   # rubocop:disable Metrics/ParameterLists
-  def initialize(query:, url:, search_attribute:, placeholder:, value: nil, **kwargs)
+  def initialize(query:, url:, search_attribute:, placeholder:, total_count:, value: nil, **kwargs)
     @query = query
     @url = url
-    @value = value
     @search_attribute = search_attribute
     @placeholder = placeholder
+    @total_count = total_count
+    @value = value
     @kwargs = kwargs
   end
   # rubocop:enable Metrics/ParameterLists
+
+  def results_message
+    if @total_count.zero?
+      "No results found for '#{params[:q][@search_attribute]}'"
+    elsif @total_count == 1
+      "1 result found for '#{params[:q][@search_attribute]}'"
+    else
+      "#{@total_count} results found for '#{params[:q][@search_attribute]}'"
+    end
+  end
 end
