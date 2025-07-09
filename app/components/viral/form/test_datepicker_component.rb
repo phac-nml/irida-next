89c619a088b6f2832776850670cd520095324769
Chris Huynh <chris.huynh333@gmail.com>
2025-07-09 13:50:07 -0500

    finish initial keyboard navigation logic, start tying in component to actual uses in app

--- app/components/viral/form/test_datepicker_component.rb
+++ app/components/viral/form/test_datepicker_component.rb
@@ -4,13 +4,16 @@ module Viral
   module Form
     # Form text input component (numbers, email, text, etc.)
     class TestDatepickerComponent < Viral::Component
-      attr_reader :id, :min_date, :selected_date, :months, :min_year
+      attr_reader :input_id, :input_name, :id, :min_date, :selected_date, :months, :min_year, :autosubmit
 
-      # def initialize(container:, name:, value: nil, required: nil, pattern: nil, placeholder: nil)
-      def initialize(id: 'datepicker', min_date: nil, selected_date: nil)
+      def initialize(input_id: nil, input_name: '', id: 'datepicker', min_date: nil, selected_date: nil,
+                     autosubmit: false)
+        @input_id = input_id
+        @input_name = input_name
         @id = id
         @min_date = min_date
         @selected_date = selected_date
+        @autosubmit = autosubmit
         @months = %w[
           January
           February
@@ -25,7 +28,8 @@ module Viral
           November
           December
         ]
-        @min_year = min_date.nil? ? '1' : min_date.split('-')[0]
+
+        @min_year = min_date.nil? ? '1' : min_date.to_s.split('-')[0]
       end
     end
   end
