a59a1906ee78274a5c20c241d84f7e6b7ae821f1
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2025-07-16 15:11:56 +0000

    fix bug where sign in redirect failed to send token

--- app/javascript/controllers/integration_access_token_controller.js
+++ app/javascript/controllers/integration_access_token_controller.js
@@ -3,16 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static values = {
     token: String,
-    urls: Array
+    urls: Array,
+    target: String,
   }
 
   connect() {
-    const referrer = document.referrer
-    if (this.urlsValue.includes(referrer)) {
-      setTimeout(() => {
-        window.opener.postMessage(this.tokenValue, referrer)
-        window.close()
-      }, 3000)
-    }
+    setTimeout(() => {
+      window.opener.postMessage(this.tokenValue, this.targetValue)
+      window.close()
+    }, 3000)
   }
 }
