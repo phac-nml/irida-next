bd6ca2f7c87660a7e7f3b97369734c047a480932
Chris Huynh <chris.huynh333@gmail.com>
2025-07-16 14:34:24 -0500

    add lookbook preview

---
+++ test/components/previews/viral_form_test_datepicker_component_preview.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class ViralFormTestDatepickerComponentPreview < ViewComponent::Preview
+  def default; end
+  def with_min_date; end
+  def with_selected_date; end
+  def with_min_and_selected_dates; end
+end
