a6e3a4fdbb965ffd7027db11ce4fe36d41bf20d2
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-07-18 11:40:25 -0500

    adding pathogen condition wrapper component tests

--- test/components/pathogen/system/link_test.rb
+++ test/components/pathogen/system/link_test.rb
@@ -5,17 +5,22 @@ require 'application_system_test_case'
 module Pathogen
   module System
     class TooltipComponentTest < ApplicationSystemTestCase
+      setup do
+        @link_text = 'This is a link with tooltip'
+        @tooltip_text = 'Tooltip text'
+      end
+
       test 'tooltip appears on hover & disappears on escape' do
         visit('/rails/view_components/pathogen_link/tooltip')
         within('.Viral-Preview > [data-controller-connected="true"]') do
           # Invoke tooltip on hover
           assert_selector '[data-viral--tooltip-target="target"]', visible: false
-          find('a', text: 'This is a link with tooltip').hover
-          assert_text 'Tooltip text'
+          find('a', text: @link_text).hover
+          assert_text @tooltip_text
           assert_selector '[data-viral--tooltip-target="target"]', visible: true
-          # Dismiss tooltip by 'Escape' key
-          find('a', text: 'This is a link with tooltip').native.send_keys(:escape)
-          assert_no_text 'Tooltip text'
+          # Dismiss tooltip on escape
+          find('a', text: @link_text).native.send_keys(:escape)
+          assert_no_text  @tooltip_text
           assert_selector '[data-viral--tooltip-target="target"]', visible: false
         end
       end
@@ -25,12 +30,12 @@ module Pathogen
         within('.Viral-Preview > [data-controller-connected="true"]') do
           # Invoke tooltip on focus
           assert_selector '[data-viral--tooltip-target="target"]', visible: false
-          find('a', text: 'This is a link with tooltip').trigger('focus')
-          assert_text 'Tooltip text'
+          find('a', text: @link_text).trigger('focus')
+          assert_text @tooltip_text
           assert_selector '[data-viral--tooltip-target="target"]', visible: true
           # Dismiss tooltip on blur
-          find('a', text: 'This is a link with tooltip').native.send_keys(:tab)
-          assert_no_text 'Tooltip text'
+          find('a', text: @link_text).native.send_keys(:tab)
+          assert_no_text  @tooltip_text
           assert_selector '[data-viral--tooltip-target="target"]', visible: false
         end
       end
