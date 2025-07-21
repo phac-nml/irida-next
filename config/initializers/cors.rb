707ebcbd8764f927972cfd8470970674e71bd095
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2025-07-21 21:06:10 +0000

    cleanup

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# return unless Flipper.enabled?(:integration_access_token_generation)
-
 # https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
