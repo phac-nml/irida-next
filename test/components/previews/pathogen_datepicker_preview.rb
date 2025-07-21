853adb413f732b27978f1d7907820494bd0e228e
Chris Huynh <chris.huynh333@gmail.com>
2025-07-21 20:46:49 +0000

    add pathogen datepicker to lookbook, remove test_datepicker

--- test/components/previews/pathogen_datepicker_preview.rb
+++ test/components/previews/pathogen_datepicker_preview.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ViralFormTestDatepickerComponentPreview < ViewComponent::Preview
+class PathogenDatepickerPreview < ViewComponent::Preview
   def default; end
   def with_min_date; end
   def with_selected_date; end
