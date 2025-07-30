d8a3dbb873df280909ecd839142c3857f3f83166
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2025-07-30 18:02:20 +0000

    clean job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+# Parent class for jobs
 class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
+  def raise_error(error_message)
+    Rails.logger.error error_message
+    raise error_message
+  end
 end
