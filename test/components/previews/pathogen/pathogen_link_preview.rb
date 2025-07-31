bba0c93388d8c9583fe3f5aac7f4a486a2ccd7b5
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-07-31 10:25:29 -0500

    moving the link preview under pathogen

---
+++ test/components/previews/pathogen/pathogen_link_preview.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Pathogen
+  class PathogenLinkPreview < ViewComponent::Preview
+    include Pathogen::ViewHelper
+
+    def default
+      pathogen_link(href: '#') do
+        'This is a link'
+      end
+    end
+
+    # @label With Tooltip
+    def tooltip
+      pathogen_link(href: '#') do |component|
+        component.with_tooltip(text: 'Tooltip text')
+        'This is a link with tooltip'
+      end
+    end
+  end
+end
