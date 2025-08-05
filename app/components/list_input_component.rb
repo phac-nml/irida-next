8397f33fd010f366fb1605635fc690c2d86fc6c2
Katherine Thiessen <ksierks@users.noreply.github.com>
2025-08-05 07:26:42 -0500

    Advanced search accessibility (#1141)
    * conditionally applying autofocus to advanced search button or filter searchbox
    
    * resizing operator dropdown
    
    * adding aria error labels
    
    * fixing focus when clearing filter & adding tests
    
    * fixing focus when clearing filter & adding tests for group samples
    
    * adding aria error labels to ListInputComponent
    
    * fixing autofocus syntax
    
    * fixing W3CValidator stray end tag error
    
    * adding a trigger to dialog to focus on
    
    * disabling the blank dropdown options
    
    * Revert "disabling the blank dropdown options"
    
    This reverts commit 0786f91b9df0c969d89ba87e4bcc2fcf88972c58.
    
    * creating a seperate blank error for dropdown
    
    * focusing after add/remove group/condition
    
    * focusing after add/remove group/condition
    
    * replacing class w-auto with w-full

--- app/components/list_input_component.rb
+++ app/components/list_input_component.rb
@@ -4,8 +4,27 @@
 class ListInputComponent < Component
   attr_reader :list_input_form_name, :show_description
 
-  def initialize(list_input_form_name:, show_description: true)
+  def initialize(list_input_form_name:, show_description: true, **input_arguments)
     @list_input_form_name = list_input_form_name
     @show_description = show_description
+    @input_arguments = input_arguments
+  end
+
+  def input_arguments
+    { tag: 'input' }.deep_merge(@input_arguments).tap do |args|
+      args[:type] = 'text'
+      args[:name] = @list_input_form_name
+      args[:autofocus] = true
+      args[:classes] = class_names(args[:classes], 'bg-transparent border-none grow')
+      args[:aria] ||= {}
+      args[:aria][:label] = t(:'components.list_input.description')
+      args[:data] ||= {}
+      args[:data][:action] = '
+        keydown->list-filter#handleInput
+        paste->list-filter#handlePaste
+        turbo:morph-element->list-filter#idempotentConnect
+      '
+      args[:data][:'list-filter-target'] = 'input'
+    end
   end
 end
