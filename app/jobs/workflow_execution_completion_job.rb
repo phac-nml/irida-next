32f53da422b04a9a66e215711b52b851f14ed82d
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2025-08-06 16:12:53 +0000

    completion job/service notes

--- app/jobs/workflow_execution_completion_job.rb
+++ app/jobs/workflow_execution_completion_job.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 # Queues the workflow execution completion job
-class WorkflowExecutionCompletionJob < ApplicationJob
+class WorkflowExecutionCompletionJob < WorkflowExecutionJob
   queue_as :default
   queue_with_priority 10
 
   def perform(workflow_execution)
+    # validate workflow_execution object is fit to run jobs on
+    unless validate_initial_state(workflow_execution, [:completing], validate_run_id: false)
+      return handle_error_state_and_clean(workflow_execution)
+    end
+
     WorkflowExecutions::CompletionService.new(workflow_execution).execute
+
+    # TODO: this job doesn't have any tests.
+    # They should be added when job queuing from CompletionService is refactored into this job.
   end
 end
