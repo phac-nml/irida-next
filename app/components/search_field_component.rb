da6900c4bc0ccb0d8b95e9a43d31d371cae0ee92
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-08-07 15:29:09 -0500

    Enhance SearchFieldComponent with clear button functionality and Stimulus controller integration. Update HTML and Ruby component to support dynamic button actions based on search results. Modify locale files to include clear button labels in English and French. Refactor table filters in project and group samples to utilize the new component.

--- app/components/search_field_component.rb
+++ app/components/search_field_component.rb
@@ -2,9 +2,14 @@
 
 # Component for rendering a search field with a submit button
 class SearchFieldComponent < Component
-  def initialize(label:, form:, field_name:)
+  def initialize(label:, form:, field_name:, has_search_results: false)
     @label = label
     @form = form
     @field_name = field_name
+    @has_search_results = has_search_results
+  end
+
+  def clear_button?
+    @has_search_results
   end
 end
