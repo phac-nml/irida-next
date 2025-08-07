aaac99e526b6a63dc80fdba80c8d7ec5ed1a539c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-08-07 09:42:48 -0500

    Refactor Pagy limit component to improve URL handling and query parameter management
    - Updated form action to use current_url_without_limit for better clarity and functionality.
    - Introduced current_url_without_limit method to streamline query parameter handling by removing the 'limit' parameter.
    - Enhanced overall maintainability of the component's code structure.

--- app/components/viral/pagy/limit_component.rb
+++ app/components/viral/pagy/limit_component.rb
@@ -8,6 +8,19 @@ module Viral
         @pagy = pagy
         @item = item
       end
+
+      def current_url_without_limit
+        current_uri = URI.parse(request.original_url)
+
+        if current_uri.query
+          query_params = URI.decode_www_form(current_uri.query).to_h
+          query_params.delete('limit')
+          new_query_string = URI.encode_www_form(query_params)
+          current_uri.query = new_query_string
+        end
+
+        current_uri.to_s
+      end
     end
   end
 end
