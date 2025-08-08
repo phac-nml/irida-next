cda551910b2c45d22408d060c74c809eece05a55
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-08-08 19:47:15 +0000

    Refactor SearchFieldComponent to improve label and placeholder handling. Update locale files for consistent search field labels and placeholders in English and French. Adjust table filter integration to utilize new label and placeholder structure.

--- app/components/search_field_component.rb
+++ app/components/search_field_component.rb
@@ -2,8 +2,9 @@
 
 # Component for rendering a search field with a submit button
 class SearchFieldComponent < Component
-  def initialize(label:, form:, field_name:, has_search_results: false)
+  def initialize(label:, placeholder:, form:, field_name:, has_search_results: false)
     @label = label
+    @placeholder = placeholder
     @form = form
     @field_name = field_name
     @has_search_results = has_search_results
