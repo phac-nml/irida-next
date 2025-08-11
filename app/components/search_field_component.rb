d5f3c355c13c4cf484d94d067cfbd7c2185d37cc
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-08-11 20:15:03 +0000

    Refactor SearchFieldComponent to use value from initialization and update search field value in projects index view for improved functionality.

--- app/components/search_field_component.rb
+++ app/components/search_field_component.rb
@@ -2,14 +2,15 @@
 
 # Component for rendering a search field with a submit button
 class SearchFieldComponent < Component
-  def initialize(label:, placeholder:, form:, field_name:)
+  def initialize(label:, placeholder:, form:, field_name:, value: nil)
     @label = label
     @placeholder = placeholder
     @form = form
     @field_name = field_name
+    @value = value
   end
 
   def clear_button?
-    params[:q][@field_name].present?
+    @value.present?
   end
 end
