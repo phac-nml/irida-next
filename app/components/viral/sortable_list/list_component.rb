ad574e534d2dc696e7cfd69f68b1ce431c07a10f
deepsidhu85 <deepsidhu85@gmail.com>
2025-08-11 16:56:31 -0500

    Added visual cues for required fields

--- app/components/viral/sortable_list/list_component.rb
+++ app/components/viral/sortable_list/list_component.rb
@@ -4,14 +4,15 @@ module Viral
   module SortableList
     # This component creates the individual lists for the sortable_lists_component.
     class ListComponent < Viral::Component
-      attr_reader :id, :group, :title, :list_items
+      attr_reader :id, :group, :title, :list_items, :required
 
       # If creating multiple lists to utilize the same list values, assign them the same group
-      def initialize(id: nil, group: nil, title: nil, list_items: [], **system_arguments)
+      def initialize(id: nil, group: nil, title: nil, list_items: [], required: false, **system_arguments)
         @id = id
         @group = group
         @title = title
         @list_items = list_items
+        @required = required
         @system_arguments = system_arguments
         @system_arguments[:list_classes] =
           class_names(system_arguments[:list_classes],
--- app/components/viral/sortable_lists_component.rb
+++ app/components/viral/sortable_lists_component.rb
@@ -3,15 +3,19 @@
 module Viral
   # This component creates the sortable_lists.
   class SortableListsComponent < Viral::Component
-    attr_reader :title, :description, :templates, :template_label
+    attr_reader :title, :description, :templates, :template_label,
+                :required
 
-    renders_many :lists, Viral::SortableList::ListComponent
+    renders_many :lists, lambda { |id:, group:, title:, **system_arguments|
+      Viral::SortableList::ListComponent.new(id:, group:, title:, required: @required, **system_arguments)
+    }
 
-    def initialize(title: nil, description: nil, templates: [], template_label: nil)
+    def initialize(title: nil, description: nil, templates: [], template_label: nil, required: false)
       @title = title
       @description = description
       @templates = templates
       @template_label = template_label
+      @required = required
     end
   end
 end
