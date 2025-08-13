9ab184d7bebd75b0d5a528b63a80dd571ba63ca9
Chris Huynh <chris.huynh333@gmail.com>
2025-08-13 16:36:48 +0000

    move pathogen previews; fix rubocop issues

--- test/components/previews/pathogen_datepicker_preview.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class PathogenDatepickerPreview < ViewComponent::Preview
-  def default; end
-  def with_no_min_date; end
-  def with_selected_date; end
-  def with_selected_date_and_no_min_date; end
-end
