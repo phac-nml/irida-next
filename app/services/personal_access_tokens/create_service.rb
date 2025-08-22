576969d7114bc6c082c61da457e2571a995ce0e2
deepsidhu85 <deepsidhu85@gmail.com>
2025-08-22 13:51:16 -0500

    Updated service to add errors to object rather than raise error and rescue which was only returning one error even though multiple attributes were left blank in the UI

--- app/services/personal_access_tokens/create_service.rb
+++ app/services/personal_access_tokens/create_service.rb
@@ -3,8 +3,6 @@
 module PersonalAccessTokens
   # Service used to create personal access tokens
   class CreateService < BaseService
-    PersonalAccessTokenNameCreateError = Class.new(StandardError)
-    PersonalAccessTokenScopeCreateError = Class.new(StandardError)
     attr_accessor :namespace, :bot_user, :personal_access_token
 
     def initialize(user, params, namespace = nil, bot_user = nil)
@@ -19,29 +17,24 @@ module PersonalAccessTokens
       authorize! current_user, to: :generate_bot_personal_access_token? if bot_user.nil?
       authorize! namespace, to: :generate_bot_personal_access_token? unless bot_user.nil?
 
-      validate_params
+      return @personal_access_token unless validate_params
 
       personal_access_token.save
 
-      personal_access_token
-    rescue PersonalAccessTokens::CreateService::PersonalAccessTokenNameCreateError => e
-      personal_access_token.errors.add(:name, e.message)
-      personal_access_token
-    rescue PersonalAccessTokens::CreateService::PersonalAccessTokenScopeCreateError => e
-      personal_access_token.errors.add(:scopes, e.message)
       personal_access_token
     end
 
     def validate_params
       if params[:name].blank?
-        raise PersonalAccessTokenNameCreateError,
-              I18n.t('services.personal_access_tokens.create.required.token_name')
+        @personal_access_token.errors.add :name, I18n.t('services.personal_access_tokens.create.required.token_name')
       end
 
-      return if params[:scopes].present?
+      if params[:scopes].blank?
+        @personal_access_token.errors.add :scopes,
+                                          I18n.t('services.personal_access_tokens.create.required.scopes')
+      end
 
-      raise PersonalAccessTokenScopeCreateError,
-            I18n.t('services.personal_access_tokens.create.required.scopes')
+      @personal_access_token.errors.none?
     end
   end
 end
