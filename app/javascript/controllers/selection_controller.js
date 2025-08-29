e84f86c42bbe3e257f151510ae6a401bce92f5aa
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-08-29 12:33:18 +0000

    Fix indeterminate state logic for select all checkbox in selection controller

--- app/javascript/controllers/selection_controller.js
+++ app/javascript/controllers/selection_controller.js
@@ -126,8 +126,7 @@ export default class extends Controller {
       this.selectPageTarget.checked = uncheckedBoxes.length === 0;
       this.selectPageTarget.indeterminate = !(
         uncheckedBoxes.length === 0 ||
-        uncheckedBoxes.length === this.totalValue ||
-        this.rowSelectionTargets.length - uncheckedBoxes.length === 0
+        uncheckedBoxes.length === this.rowSelectionTargets.length
       );
     }
   }
