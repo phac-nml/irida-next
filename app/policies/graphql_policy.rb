8edbfe0295c50c496f5a7be571f31703ee1eeafd
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-08-29 20:44:05 +0000

    Refactor: Inline token read permission check in query? method for clarity

--- app/policies/graphql_policy.rb
+++ app/policies/graphql_policy.rb
@@ -5,20 +5,11 @@ class GraphqlPolicy < ApplicationPolicy
   # nil is allowed as a user accessing the api through graphiql while logged into the website would not have a token
   authorize :token, allow_nil: true
 
-  def query?
-    return true if token_allows_read?
+  def query? # rubocop:disable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+    return true if token&.active? && token&.scopes&.to_set&.intersect?(%w[api read_api].to_set) # rubocop:disable Style/SafeNavigationChainLength
     return true if token.nil? && !user.nil? # allow users with a session to query the api
 
     false
-  end
-
-  private
-
-  def token_allows_read?
-    return false unless token&.active?
-
-    scopes = token.scopes
-    scopes&.to_set&.intersect?(%w[api read_api].to_set)
   end
 
   def mutate? # rubocop:disable Metrics/CyclomaticComplexity
