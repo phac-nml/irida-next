7ab821976ebaae96ed483cd19f532bd19bef4ea6
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-08-29 11:08:05 -0500

    Refactor: Simplify query authorization logic in GraphqlPolicy by extracting token validation into a separate method

--- app/policies/graphql_policy.rb
+++ app/policies/graphql_policy.rb
@@ -5,11 +5,20 @@ class GraphqlPolicy < ApplicationPolicy
   # nil is allowed as a user accessing the api through graphiql while logged into the website would not have a token
   authorize :token, allow_nil: true
 
-  def query? # rubocop:disable Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
-    return true if token&.active? && token&.scopes&.to_set&.intersect?(%w[api read_api].to_set)
+  def query?
+    return true if token_allows_read?
     return true if token.nil? && !user.nil? # allow users with a session to query the api
 
     false
+  end
+
+  private
+
+  def token_allows_read?
+    return false unless token&.active?
+
+    scopes = token.scopes
+    scopes&.to_set&.intersect?(%w[api read_api].to_set)
   end
 
   def mutate? # rubocop:disable Metrics/CyclomaticComplexity
