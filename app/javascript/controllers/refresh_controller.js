41b85eab1166a7d1f288036d770d474c3d9b2cc4
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-09-10 16:00:04 +0000

    feat: add in refresh controller that causes refresh notice to be displayed when using turbo_stream_from

---
+++ app/javascript/controllers/refresh_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["notice", "source"];
+
+  initialize() {
+    this.boundMessageHandler = this.messageHandler.bind(this);
+  }
+
+  sourceTargetConnected(element) {
+    element.addEventListener("message", this.boundMessageHandler, true);
+  }
+
+  sourceTargetDisconnected(element) {
+    element.removeEventListener("message", this.boundMessageHandler, true);
+  }
+
+  messageHandler(event) {
+    if (
+      typeof event.data === "string" &&
+      event.data === '<turbo-stream action="refresh"></turbo-stream>'
+    ) {
+      this.noticeTarget.classList.remove("hidden");
+      event.stopImmediatePropagation();
+    }
+  }
+}
