9bf8d802fb93b47a25c0fab2983529fe7dbde065
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-09-19 11:10:35 -0500

    Improves search field component styling
    Refactors the search field component to improve its styling and responsiveness.
    
    - Adjusts width for medium-sized screens and larger.
    - Makes the label background transparent.
    - Removes the placeholder argument from the component.

--- app/components/search_field_component.rb
+++ app/components/search_field_component.rb
@@ -2,17 +2,13 @@
 
 # Component for rendering a search field with a submit button
 class SearchFieldComponent < Component
-  def initialize(label:, placeholder:, form:, field_name:, value: nil, **system_arguments) # rubocop:disable Metrics/ParameterLists
+  def initialize(label:, form:, field_name:, value: nil, **system_arguments)
     @label = label
-    @placeholder = placeholder
     @form = form
     @field_name = field_name
     @value = value
     @system_arguments = system_arguments
-    @system_arguments[:classes] = class_names(
-      @system_arguments[:classes],
-      'flex flex-col'
-    )
+
     @system_arguments[:data] ||= {}
     @system_arguments[:data][:action] = 'focusin->search-field#onFocusin focusout->search-field#onFocusout'
     @system_arguments[:data][:controller] = 'search-field'
