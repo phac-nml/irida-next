3caf7ec907e36041d8fbafb86384cc05730ab068
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-09-22 08:06:27 -0500

    Guards against tooltip destruction on disconnect
    Ensures that the tooltip is only destroyed if it exists,
    preventing potential errors during component disconnection.

--- app/javascript/controllers/pathogen/tooltip_controller.js
+++ app/javascript/controllers/pathogen/tooltip_controller.js
@@ -3,15 +3,18 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["trigger", "target"];
 
+  #tooltip;
+
   connect() {
     this.element.setAttribute("data-controller-connected", "true");
   }
 
   disconnect() {
-    this.#tooltip.destroy();
-    this.#tooltip = null;
+    if (this.#tooltip) {
+      this.#tooltip.destroy();
+      this.#tooltip = null;
+    }
   }
-
 
   targetTargetConnected () {
     this.#tooltip = new Tooltip(this.targetTarget, this.triggerTarget, {
