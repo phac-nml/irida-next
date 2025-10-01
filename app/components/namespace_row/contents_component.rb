1b388913db89c2c5d20d85ed421d9184ccbe9ab9
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-10-01 12:49:54 -0500

    Refactors pathogen icon usage to use symbols
    Updates all instances of pathogen_icon helper calls to use symbols instead of strings for the icon name.
    
    This improves code readability and maintainability by adhering to a consistent style for referencing icons.

--- app/components/namespace_row/contents_component.rb
+++ app/components/namespace_row/contents_component.rb
@@ -32,7 +32,7 @@ module NamespaceRow
 
     def avatar_icon
       if @namespace.group_namespace?
-        pathogen_icon('squares-four', size: :md, color: :subdued)
+        pathogen_icon(:squares_four, size: :md, color: :subdued)
       elsif @namespace.project_namespace?
         pathogen_icon(:stack, size: :md, color: :subdued)
       end
