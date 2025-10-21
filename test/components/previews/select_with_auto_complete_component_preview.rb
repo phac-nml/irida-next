6d376cbc05816974a4fc826811384a78f491405a
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-10-21 12:10:47 -0500

    trying out tom-select

---
+++ test/components/previews/select_with_auto_complete_component_preview.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class SelectWithAutoCompleteComponentPreview < ViewComponent::Preview
+  def default
+    render_with_template(locals: {
+                           fields: %w[name puid created_at updated_at attachments_updated_at]
+                         })
+  end
+
+  def with_groups
+    sample_fields = %w[name puid created_at updated_at attachments_updated_at]
+    metadata_fields = { 'Metadata fields': [['age', 'metadata.age'],
+                                            ['country', 'metadata.country'],
+                                            ['earliest_date', 'metadata.earliest_date'],
+                                            ['food', 'metadata.food'],
+                                            ['gender', 'metadata.gender'],
+                                            ['insdc_accession', 'metadata.insdc_accession'],
+                                            ['ncbi_accession', 'metadata.ncbi_accession'],
+                                            ['onset', 'metadata.onset'],
+                                            ['patient_age', 'metadata.patient_age'],
+                                            ['patient_sex', 'metadata.patient_sex'],
+                                            ['wgs_id', 'metadata.wgs_id']]}
+
+    render_with_template(locals: {
+                           fields: metadata_fields.merge({ 'Sample fields': sample_fields })
+                         })
+  end
+end
