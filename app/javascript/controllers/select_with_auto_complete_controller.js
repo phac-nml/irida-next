cd878beb183acd9e8f852b9a430e9a5504341393
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-10-22 09:26:48 -0500

    adding roles to groups for screenreader

--- app/javascript/controllers/select_with_auto_complete_controller.js
+++ app/javascript/controllers/select_with_auto_complete_controller.js
@@ -4,7 +4,27 @@ import TomSelect from "tom-select";
 export default class SelectWithAutoCompleteController extends Controller {
   connect() {
     console.debug("SelectWithAutoCompleteController: Connected");
-    new TomSelect(this.element);
+    new TomSelect(this.element, {
+      render: {
+        optgroup: function (data, escape) {
+          var optgroup = document.createElement("div");
+          optgroup.className = "optgroup";
+          optgroup.role = "group";
+          optgroup.setAttribute("aria-labelledby", data.group.value);
+          var optgroup_header = document.createElement("div");
+          optgroup_header.className = "optgroup-header";
+          optgroup_header.role = "presentation";
+          optgroup_header.innerText = data.group.label;
+          optgroup_header.id = data.group.value;
+          optgroup.appendChild(optgroup_header);
+          optgroup.appendChild(data.options);
+          return optgroup;
+        },
+        optgroup_header: function (data, escape) {
+          return "";
+        },
+      },
+    });
   }
 
   disconnect() {
