caf054990f1f57e5c5b0f32758455c07dfd6aab8
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-10-29 13:51:00 -0500

    chore: Replace AddPipelineIdToWorkflowExecutionMetadata to set pipeline_id to the value present in pipelines.json

---
+++ db/migrate/20251029175823_add_pipeline_id_to_workflow_execution_metadata.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Updates the `pipeline_id` to match the entry in the pipelines config
+class AddPipelineIdToWorkflowExecutionMetadata < ActiveRecord::Migration[8.0]
+  def change # rubocop:disable Metrics/AbcSize
+    pipeline_name_to_id = Irida::Pipelines.instance.available_pipelines.map do |pipeline_id, pipeline_config|
+      [pipeline_config.name, pipeline_config.pipeline_id]
+    end.to_h
+
+    workflow_executions = WorkflowExecution.all
+    workflow_executions.each do |workflow_execution|
+      workflow_execution.metadata['pipeline_id'] = pipeline_name_to_id[workflow_execution.metadata['workflow_name']]
+      workflow_execution.save
+    end
+
+    automated_executions = AutomatedWorkflowExecution.all
+    automated_executions.each do |automated_execution|
+      automated_execution.metadata['pipeline_id'] = pipeline_name_to_id[automated_execution.metadata['workflow_name']]
+      automated_execution.save
+    end
+  end
+end
