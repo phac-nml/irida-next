436c59b3bec5658b9880e1dda29cddedfe98c6c2
Sukhdeep Sidhu <ssidhu@phac5029663.cscscience.ca>
2025-10-31 15:57:53 -0500

    Fixed line length linter warnings

--- test/lib/irida/pipelines_overrides_test.rb
+++ test/lib/irida/pipelines_overrides_test.rb
@@ -10,7 +10,9 @@ class PipelinesOverrides < ActiveSupport::TestCase
     # Read in schema file to json
     body = Rails.root.join('test/fixtures/files/nextflow/nextflow_schema.json')
     nextflow_fastmatch_body = Rails.root.join('test/fixtures/files/nextflow/nextflow_schema_fastmatch.json')
-    nextflow_samplesheet_fastmatch_body = Rails.root.join('test/fixtures/files/nextflow/samplesheet_schema_fastmatch.json')
+    nextflow_samplesheet_fastmatch_body = Rails.root.join(
+      'test/fixtures/files/nextflow/samplesheet_schema_fastmatch.json'
+    )
 
     stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/2.0.2/nextflow_schema.json')
       .to_return(status: 200, body:, headers: { etag: '[W/"a1Ab"]' })
@@ -68,17 +70,21 @@ class PipelinesOverrides < ActiveSupport::TestCase
     workflow2 = pipelines.find_pipeline_by('PNC Fast Match', '0.4.0')
 
     assert_equal 'new_isolates_date',
-                 workflow1.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']['metadata_1']['x-irida-next-selected']
+                 workflow1.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']
+    ['metadata_1']['x-irida-next-selected']
 
     assert_equal 'prediceted_primary_identification_name',
-                 workflow1.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']['metadata_2']['x-irida-next-selected']
+                 workflow1.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']
+    ['metadata_2']['x-irida-next-selected']
 
     assert_equal 'new_isolates_date',
-                 workflow2.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']['metadata_1']['x-irida-next-selected']
+                 workflow2.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']
+    ['metadata_1']['x-irida-next-selected']
 
     # Workflow2 has an entry level override which overrides ["metadata_2"]["x-irida-next-selected"]
     assert_not_equal 'prediceted_primary_identification_name',
-                     workflow2.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']['metadata_2']['x-irida-next-selected']
+                     workflow2.workflow_params[:input_output_options][:properties][:input][:schema]['items']
+    ['properties']['metadata_2']['x-irida-next-selected']
   end
 
   test 'pipelines with samplesheet overrides at version level' do
@@ -89,15 +95,19 @@ class PipelinesOverrides < ActiveSupport::TestCase
     workflow2 = pipelines.find_pipeline_by('PNC Fast Match', '0.4.0')
 
     assert_equal 'new_isolates_date',
-                 workflow1.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']['metadata_1']['x-irida-next-selected']
+                 workflow1.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']
+    ['metadata_1']['x-irida-next-selected']
 
     assert_equal 'prediceted_primary_identification_name',
-                 workflow1.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']['metadata_2']['x-irida-next-selected']
+                 workflow1.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']
+    ['metadata_2']['x-irida-next-selected']
 
     assert_equal 'new_isolates_date',
-                 workflow2.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']['metadata_1']['x-irida-next-selected']
+                 workflow2.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']
+    ['metadata_1']['x-irida-next-selected']
 
     assert_equal 'overridden_metadata_field',
-                 workflow2.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']['metadata_2']['x-irida-next-selected']
+                 workflow2.workflow_params[:input_output_options][:properties][:input][:schema]['items']['properties']
+    ['metadata_2']['x-irida-next-selected']
   end
 end
