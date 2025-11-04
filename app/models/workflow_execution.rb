57fc244a32ba9fc4b46d207f92937efc2f8659bd
Eric Enns <492127+ericenns@users.noreply.github.com>
2025-11-04 14:17:59 -0600

    chore: fix bad rebase

--- app/models/workflow_execution.rb
+++ app/models/workflow_execution.rb
@@ -110,11 +110,6 @@ class WorkflowExecution < ApplicationRecord # rubocop:disable Metrics/ClassLengt
                       workflow_version: metadata['workflow_version']))
   end
 
-  def workflow
-    Irida::Pipelines.instance.find_pipeline_by(metadata['pipeline_id'], metadata['workflow_version'],
-                                               'available')
-  end
-
   private
 
   def send_user_emails
