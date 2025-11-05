398aa6a87488173fd8f4d3f7d1c8fb329a78b900
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-11-05 10:36:46 -0600

    Refactors sample selection with Turbo Frames
    Moves sample selection functionality to use Turbo Frames for enhanced responsiveness and a smoother user experience.
    
    This change streamlines the selection process, eliminating the need for turbo_stream format in the controller and replacing it with turbo_frame_tag in the view.

--- app/controllers/projects/samples_controller.rb
+++ app/controllers/projects/samples_controller.rb
@@ -100,13 +100,9 @@ module Projects
       authorize! @project, to: :sample_listing?
       @sample_ids = []
 
-      respond_to do |format|
-        format.turbo_stream do
-          if params[:select].present?
-            @sample_ids = @query.results.reorder(nil).where(updated_at: ..params[:timestamp].to_datetime).pluck(:id)
-          end
-        end
-      end
+      return if params[:select].blank?
+
+      @sample_ids = @query.results.reorder(nil).where(updated_at: ..params[:timestamp].to_datetime).pluck(:id)
     end
 
     private
