b5ef5c1a876a6caa23071485bc6bd0fc871f4d34
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-11-13 14:34:31 -0600

    Fixes locale selection and base component
    Updates the locale selection to properly reflect the user's locale or the default.
    
    Refactors Viral components to inherit from a central `Component` class for consistency and to align with ViewComponent v4.

--- app/components/layout/language_selection_component.rb
+++ app/components/layout/language_selection_component.rb
@@ -3,8 +3,11 @@
 module Layout
   # Component for rendering language selection
   class LanguageSelectionComponent < Component
+    attr_reader :locale
+
     def initialize(user: Current.user)
       @user = user
+      @locale = @user&.locale || I18n.default_locale
       @locale_options = I18n.available_locales.map { |locale| [I18n.t(:"locales.#{locale}", locale: locale), locale] }
     end
   end
