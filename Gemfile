0bec38a21151bb87f19731aba5ba326e37171207
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-11-18 16:03:48 -0600

    Configures reactionview gem
    Configures the reactionview gem to intercept .html.erb templates and enable debug mode in development.
    
    Moves the gem declaration out of the development group, so it's available in all environments, and creates an initializer file to manage the gem's configuration.

--- Gemfile
+++ Gemfile
@@ -139,6 +139,9 @@ gem 'caxlsx'
 # renders client's local time zone
 gem 'local_time', '~> 3.0', '>= 3.0.2'
 
+# reactionview
+gem 'reactionview'
+
 # OpenTelemetry
 gem 'opentelemetry-api'
 gem 'opentelemetry-common'
@@ -179,7 +182,6 @@ group :development do
   gem 'faker'
 
   gem 'herb'
-  gem 'reactionview'
 
   # LookBook
   gem 'actioncable'
--- config/initializers/reactionview.rb
+++ config/initializers/reactionview.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
-if defined?(ReActionView)
-  ReActionView.configure do |config|
-    # Intercept .html.erb templates and process them with `Herb::Engine` for enhanced features
-    config.intercept_erb = true
+ReActionView.configure do |config|
+  # Intercept .html.erb templates and process them with `Herb::Engine` for enhanced features
+  config.intercept_erb = true
 
-    # Enable debug mode in development (adds debug attributes to HTML)
-    config.debug_mode = Rails.env.development?
+  # Enable debug mode in development (adds debug attributes to HTML)
+  config.debug_mode = Rails.env.development?
 
-    # Add custom transform visitors to process templates before compilation
-    # config.transform_visitors = [
-    #   Herb::Visitor::new
-    # ]
-  end
+  # Add custom transform visitors to process templates before compilation
+  # config.transform_visitors = [
+  #   Herb::Visitor::new
+  # ]
 end
