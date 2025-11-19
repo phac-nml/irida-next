ec9d8d6a7ba7f45a3135b0dc59febe0c532c1d51
Sukhdeep Sidhu <ssidhu@phac5029663.cscscience.ca>
2025-11-19 08:23:57 -0600

    Refactored for better code organization, updated logic to launch jobs depending on state/settings

---
+++ app/jobs/workflow_execution_minimum_runtime_job.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# Job to check if minimum time has elapsed before queueing a status check job for a workflow execution
+class WorkflowExecutionMinimumRuntimeJob < WorkflowExecutionJob
+  queue_as :default
+  queue_with_priority 10
+
+  def perform(workflow_execution) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+    return if workflow_execution.canceling? || workflow_execution.canceled? || workflow_execution.nil?
+
+    min_run_time = minimum_run_time(workflow_execution)
+    running_state = false
+
+    running_state = workflow_execution_state(workflow_execution) unless min_run_time.nil?
+
+    time_elapsed = Time.now.to_i - workflow_execution.created_at.to_i
+
+    # Check if the condition is met
+    if !min_run_time.nil? && running_state && (time_elapsed >= min_run_time)
+      # Condition met: process the object
+      WorkflowExecutionStatusJob.set(
+        wait_until: status_check_interval(workflow_execution).seconds.from_now
+      ).perform_later(workflow_execution)
+    else
+      # Condition not met: re-enqueue the job with a delay
+      delay = min_run_time - time_elapsed
+      WorkflowExecutionMinimumRuntimeJob.set(wait_until: delay.seconds.from_now).perform_later(workflow_execution)
+    end
+  end
+
+  def workflow_execution_state(workflow_execution)
+    running_state = false
+    wes_connection = Integrations::Ga4ghWesApi::V1::ApiConnection.new.conn
+    wes_client = Integrations::Ga4ghWesApi::V1::Client.new(conn: wes_connection)
+    status = wes_client.get_run_status(workflow_execution.run_id)
+    running_state = true if status[:state] == 'RUNNING'
+
+    running_state
+  end
+end
