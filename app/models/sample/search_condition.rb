dd434f1b5c5e6bd875e1c18b60a7f9ca4eaeea74
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-11-20 10:23:45 -0600

    Reverted sample query

--- app/models/sample/search_condition.rb
+++ app/models/sample/search_condition.rb
@@ -10,14 +10,6 @@ class Sample::SearchCondition # rubocop:disable Style/ClassAndModuleChildren
   attribute :value
 
   def empty?
-    field.to_s.empty? && operator.to_s.empty? && value_empty?
-  end
-
-  private
-
-  def value_empty?
-    return value.compact.empty? if value.is_a?(Array)
-
-    value.to_s.empty?
+    field.empty? && operator.empty? && ((value.is_a?(Array) && value.compact!.nil?) || value.empty?)
   end
 end
