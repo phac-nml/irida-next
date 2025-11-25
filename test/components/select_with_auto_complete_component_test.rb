b80ed4b9a8f9cb94fa5df0f51df25572e186fdd0
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-11-25 08:30:57 -0600

    testing keyboard interactions

--- test/components/select_with_auto_complete_component_test.rb
+++ test/components/select_with_auto_complete_component_test.rb
@@ -3,7 +3,7 @@
 require 'application_system_test_case'
 
 class SelectWithAutoCompleteComponentTest < ApplicationSystemTestCase
-  def test_default # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+  def test_default # rubocop:disable Metrics/MethodLength
     visit('/rails/view_components/select_with_auto_complete_component/default')
     within "div[data-controller='select-with-auto-complete']" do
       combobox = find("input[role='combobox']")
@@ -16,12 +16,70 @@ class SelectWithAutoCompleteComponentTest < ApplicationSystemTestCase
         group = find("div[role='group']")
         within group do
           assert_selector "div[role='presentation']", text: 'Metadata fields'
+          assert_selector "div[role='option'][data-value='metadata.age']"
           assert_selector "div[role='option'][data-value='metadata.patient_age']"
-          option = find("div[role='option'][data-value='metadata.age']")
-          combobox.send_keys(:down)
-          assert_equal option[:id], combobox['aria-activedescendant']
         end
       end
+    end
+  end
+
+  def test_enter_key
+    visit('/rails/view_components/select_with_auto_complete_component/default')
+    within "div[data-controller='select-with-auto-complete']" do
+      combobox = find("input[role='combobox']")
+      combobox.send_keys(:down, :down, :enter)
+      assert_selector "div[role='listbox']", visible: false
+      assert_equal 'patient_age', combobox.value
+    end
+  end
+
+  def test_down_and_up_keys
+    visit('/rails/view_components/select_with_auto_complete_component/default')
+    within "div[data-controller='select-with-auto-complete']" do
+      combobox = find("input[role='combobox']")
+      combobox.send_keys(:down, :down, :down)
+      first_option = find("div[role='option'][data-value='metadata.age']")
+      assert_equal first_option[:id], combobox['aria-activedescendant']
+      combobox.send_keys(:up)
+      last_option = find("div[role='option'][data-value='metadata.patient_age']")
+      assert_equal last_option[:id], combobox['aria-activedescendant']
+    end
+  end
+
+  def test_right_and_left_keys
+    visit('/rails/view_components/select_with_auto_complete_component/default')
+    within "div[data-controller='select-with-auto-complete']" do
+      combobox = find("input[role='combobox']")
+      combobox.send_keys(:left, :left, :right)
+      cursor_position = page.evaluate_script("document.getElementById('field').selectionStart")
+      assert_equal 2, cursor_position
+    end
+  end
+
+  def test_home_and_end_keys
+    visit('/rails/view_components/select_with_auto_complete_component/default')
+    within "div[data-controller='select-with-auto-complete']" do
+      combobox = find("input[role='combobox']")
+      combobox.send_keys(:home)
+      cursor_position = page.evaluate_script("document.getElementById('field').selectionStart")
+      assert_equal 0, cursor_position
+      combobox.send_keys(:end)
+      cursor_position = page.evaluate_script("document.getElementById('field').selectionStart")
+      assert_equal 3, cursor_position
+    end
+  end
+
+  def test_escape_key
+    visit('/rails/view_components/select_with_auto_complete_component/default')
+    within "div[data-controller='select-with-auto-complete']" do
+      combobox = find("input[role='combobox']")
+      combobox.click
+      listbox = find("div[role='listbox']")
+      assert_matches_style(listbox, 'display' => 'block')
+      combobox.send_keys(:escape)
+      assert_matches_style(listbox, 'display' => 'none')
+      combobox.send_keys(:escape)
+      assert_empty combobox.value
     end
   end
 end
