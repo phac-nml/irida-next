d09a6b74e57124e9ef2d13ac94aa5bef8ee9b041
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-12-03 11:03:50 -0600

    conditionally display the 'Run from...' section within workflow exection summary

--- app/helpers/namespace_path_helper.rb
+++ app/helpers/namespace_path_helper.rb
@@ -3,9 +3,9 @@
 # Helper for namespace paths
 module NamespacePathHelper
   def namespace_path(namespace)
-    if namespace.group_namespace?
+    if namespace&.group_namespace?
       group_path(namespace)
-    elsif namespace.project_namespace?
+    elsif namespace&.project_namespace?
       namespace_project_path(namespace.parent, namespace.project)
     end
   end
