b30bc4caa86fefa654ccf2e480756af349e5fd01
Chris Huynh <chris.huynh333@gmail.com>
2025-12-03 11:50:11 -0600

    parse out spinners

--- app/javascript/controllers/nextflow/samplesheet_controller.js
+++ app/javascript/controllers/nextflow/samplesheet_controller.js
@@ -11,7 +11,9 @@ export default class extends Controller {
     "form",
     "formFieldError",
     "formFieldErrorMessage",
-    "spinner",
+    "toastMessageContainer",
+    "processingSpinner",
+    "samplesheetSpinner",
     "workflowAttributes",
     "samplesheetProperties",
     "trTemplate",
@@ -34,6 +36,7 @@ export default class extends Controller {
     "filterClearButton",
     "filterSearchButton",
     "samplesheetSamplesForm",
+    "samplesheetContainer",
   ];
 
   static values = {
@@ -108,18 +111,10 @@ export default class extends Controller {
   #filterEnabled = false;
 
   connect() {
-    console.log("hi");
-    // if (this.hasWorkflowAttributesTarget) {
-    //   this.#setSamplesheetParametersAndData();
-    //   this.#updateMetadataColumnHeaderNames();
-    //   this.#disableProcessingState();
-    // }
     this.element.setAttribute("data-controller-connected", "true");
   }
 
   processTest({ detail: { content } }) {
-    console.log("process test");
-    console.log("in if");
     this.#setSamplesheetParametersAndData();
     this.#updateMetadataColumnHeaderNames();
     this.#disableProcessingState();
@@ -280,14 +275,17 @@ export default class extends Controller {
   }
 
   #enableProcessingState(message) {
-    document.getElementById("nextflow-spinner-message").innerHTML = message;
+    document.getElementById("nextflow-processing-spinner-message").innerHTML =
+      message;
     this.submitTarget.disabled = true;
-    this.spinnerTarget.classList.remove("hidden");
+    this.samplesheetSpinnerTarget.classList.remove("hidden");
+    this.samplesheetContainerTarget.classList.add("hidden");
   }
 
   #disableProcessingState() {
+    this.samplesheetContainerTarget.classList.remove("hidden");
     this.submitTarget.disabled = false;
-    this.spinnerTarget.classList.add("hidden");
+    this.samplesheetSpinnerTarget.classList.add("hidden");
   }
 
   #disableErrorState() {
@@ -311,6 +309,7 @@ export default class extends Controller {
       behavior: "smooth",
       block: "start",
     });
+    this.toastMessageContainerTarget.innerHTML = "";
   }
 
   #disableFormFieldErrorState() {
