56a588c006fe6baa9b52752a575f33b68c4e2409
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-12-09 10:33:19 -0600

    Removes hidden attribute from language selection component
    Removes the `hidden` attribute from the language selection component in the view.
    This allows the component to be rendered by default.
    
    Removes a test that checked the fallback to the default locale as the behavior is not needed.

--- test/components/layout/language_selection_component_test.rb
+++ test/components/layout/language_selection_component_test.rb
@@ -1,26 +1,15 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require 'view_component_test_case'
 
 module Layout
   class LanguageSelectionComponentTest < ViewComponentTestCase
     test 'renders the current locale name' do
       user = users(:john_doe)
 
-      render_inline(Layout::LanguageSelectionComponent.new(user:))
+      render_inline(Layout::LanguageSelectionComponent.new(user: user))
 
       expected_text = I18n.t(:"locales.#{user.locale}", locale: user.locale)
-      assert_selector '#language-selection-dd-label', text: expected_text
-    end
-
-    test 'falls back to default locale when user locale is invalid' do
-      user = users(:john_doe)
-      user.locale = 'not_a_locale'
-
-      render_inline(Layout::LanguageSelectionComponent.new(user:))
-
-      default_locale = I18n.default_locale
-      expected_text = I18n.t(:"locales.#{default_locale}", locale: default_locale)
       assert_selector '#language-selection-dd-label', text: expected_text
     end
   end
