b9d363301dc8f9de7a52011704811bdad21964f3
Sukhdeep Sidhu <ssidhu@phac5029663.cscscience.ca>
2025-12-10 07:46:58 -0600

    Reverted change for checking count which was suggested by vscode

--- app/controllers/groups/samples_controller.rb
+++ app/controllers/groups/samples_controller.rb
@@ -121,7 +121,7 @@ module Groups
     def results_message_for_advanced_search
       if @pagy&.count&.zero?
         I18n.t(:'components.search.advanced.results_message.zero')
-      elsif @pagy&.one?
+      elsif @pagy&.count == 1 # rubocop:disable Style/CollectionQuerying
         I18n.t(:'components.search.advanced.results_message.singular')
       else
         I18n.t(:'components.search.advanced.results_message.plural', total_count: @pagy&.count)
@@ -131,7 +131,7 @@ module Groups
     def results_message_for_quick_search
       if @pagy&.count&.zero?
         I18n.t(:'components.search.results_message.zero', search_term: @query.name_or_puid_cont)
-      elsif @pagy&.one?
+      elsif @pagy&.count == 1 # rubocop:disable Style/CollectionQuerying
         I18n.t(:'components.search.results_message.singular', search_term: @query.name_or_puid_cont)
       else
         I18n.t(:'components.search.results_message.plural', total_count: @pagy&.count,
