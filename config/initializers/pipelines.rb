078d629eb9b9e2427d79bbc128c6ba95458dff48
Sukhdeep Sidhu <ssidhu@phac5029663.cscscience.ca>
2025-12-12 16:34:57 -0600

    Reverted logic for setting irida pipelines instance to empty hash if using validator

--- config/initializers/pipelines.rb
+++ config/initializers/pipelines.rb
@@ -3,14 +3,16 @@
 require 'irida/pipelines'
 
 Rails.application.config.to_prepare do
-  next if defined?(Rake::Task) && Rake::Task.task_defined?('pipeline_json_validator:validate')
-
-  pipeline_config_file = if Rails.root.join("config/pipelines/#{Rails.env}.json").exist?
-                           "config/pipelines/#{Rails.env}.json"
-                         else
-                           'config/pipelines/pipelines.json'
-                         end
-  Irida::Pipelines.instance = Irida::Pipelines.new(pipeline_config_file:) if Irida::Pipelines.instance.nil?
+  if defined?(Rake::Task) && Rake::Task.task_defined?('pipeline_json_validator:validate')
+    Irida::Pipelines.instance = {}
+  else
+    pipeline_config_file = if Rails.root.join("config/pipelines/#{Rails.env}.json").exist?
+                             "config/pipelines/#{Rails.env}.json"
+                           else
+                             'config/pipelines/pipelines.json'
+                           end
+    Irida::Pipelines.instance = Irida::Pipelines.new(pipeline_config_file:) if Irida::Pipelines.instance.nil?
+  end
 end
 
 Rails.application.config.after_initialize do
