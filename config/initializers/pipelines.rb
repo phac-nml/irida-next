4e8fcfe4e92d3ddbc3b7a176e925c3d56f3aec5a
Sukhdeep Sidhu <ssidhu@phac5029663.cscscience.ca>
2025-12-12 16:22:46 -0600

    Reverted change to routes file, updated pipelines initializer to continue if using pipeline json validator

--- config/initializers/pipelines.rb
+++ config/initializers/pipelines.rb
@@ -3,14 +3,14 @@
 require 'irida/pipelines'
 
 Rails.application.config.to_prepare do
-  unless defined?(Rake::Task) && Rake::Task.task_defined?('pipeline_json_validator:validate')
-    pipeline_config_file = if Rails.root.join("config/pipelines/#{Rails.env}.json").exist?
-                             "config/pipelines/#{Rails.env}.json"
-                           else
-                             'config/pipelines/pipelines.json'
-                           end
-    Irida::Pipelines.instance = Irida::Pipelines.new(pipeline_config_file:) if Irida::Pipelines.instance.nil?
-  end
+  next if defined?(Rake::Task) && Rake::Task.task_defined?('pipeline_json_validator:validate')
+
+  pipeline_config_file = if Rails.root.join("config/pipelines/#{Rails.env}.json").exist?
+                           "config/pipelines/#{Rails.env}.json"
+                         else
+                           'config/pipelines/pipelines.json'
+                         end
+  Irida::Pipelines.instance = Irida::Pipelines.new(pipeline_config_file:) if Irida::Pipelines.instance.nil?
 end
 
 Rails.application.config.after_initialize do
