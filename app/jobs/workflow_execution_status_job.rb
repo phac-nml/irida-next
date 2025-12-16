cabed6addd9da9ab3af87ff3ae2d1c9cf993f2f0
Sukhdeep Sidhu <ssidhu@phac5029663.cscscience.ca>
2025-12-16 14:13:16 -0600

    Refactored to move delay status check and workflow execution cancel on max run time exceeded into service from job to maintain idempotency

--- app/jobs/workflow_execution_status_job.rb
+++ app/jobs/workflow_execution_status_job.rb
@@ -20,7 +20,7 @@ class WorkflowExecutionStatusJob < WorkflowExecutionJob
     workflow_execution
   end
 
-  def perform(workflow_execution, min_run_time = nil)
+  def perform(workflow_execution)
     # User signaled to cancel
     return if workflow_execution.canceling? || workflow_execution.canceled?
 
@@ -30,59 +30,27 @@ class WorkflowExecutionStatusJob < WorkflowExecutionJob
     end
 
     wes_connection = Integrations::Ga4ghWesApi::V1::ApiConnection.new.conn
-
-    return if min_run_time && delay_status_check?(wes_connection, workflow_execution, min_run_time)
-
     result = WorkflowExecutions::StatusService.new(workflow_execution, wes_connection).execute
 
-    if result
-      queue_next_job(workflow_execution.reload, min_run_time)
+    if result.nil?
+      nil
+    elsif result
+      queue_next_job(workflow_execution.reload)
     else
       handle_unable_to_process_job(workflow_execution, self.class.name)
     end
   end
 
-  # Delay the status check if the workflow is in running state and minimum run time has not been reached
-  def delay_status_check?(wes_connection, workflow_execution, min_run_time)
-    return false if workflow_execution.reload.state == 'running'
-
-    wes_client = Integrations::Ga4ghWesApi::V1::Client.new(conn: wes_connection)
-    status = wes_client.get_run_status(workflow_execution.run_id)
-
-    return false unless status[:state] == 'RUNNING'
-
-    workflow_execution.state = :running
-    workflow_execution.save
-
-    WorkflowExecutionStatusJob.set(wait_until: min_run_time.seconds.from_now).perform_later(workflow_execution,
-                                                                                            min_run_time)
-    true
-  end
-
-  def queue_next_job(workflow_execution, min_run_time = nil)
+  def queue_next_job(workflow_execution)
     case workflow_execution.state.to_sym
     when :canceled, :error
       WorkflowExecutionCleanupJob.perform_later(workflow_execution)
     when :completing
       WorkflowExecutionCompletionJob.perform_later(workflow_execution)
     else
-      requeue_or_cancel(workflow_execution, min_run_time)
-    end
-  end
-
-  def requeue_or_cancel(workflow_execution, min_run_time = nil)
-    run_time = workflow_execution.workflow.state_time_calculation(workflow_execution, :running)
-    maximum_run_time = workflow_execution.workflow.maximum_run_time(workflow_execution.samples.count)
-
-    # Max runtime for pipeline (in running state) has exceeded so a cancellation request to WES is made
-    if workflow_execution.cancellable? && maximum_run_time && run_time && (run_time > maximum_run_time)
-      workflow_execution.state = :canceling
-      workflow_execution.save
-      WorkflowExecutionCancelationJob.perform_later(workflow_execution, workflow_execution.submitter)
-    else
       WorkflowExecutionStatusJob.set(
         wait_until: workflow_execution.workflow.status_check_interval.seconds.from_now
-      ).perform_later(workflow_execution, min_run_time)
+      ).perform_later(workflow_execution)
     end
   end
 end
