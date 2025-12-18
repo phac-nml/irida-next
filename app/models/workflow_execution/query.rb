43a6b580c73496a77689f40e1b1beabebd73fc29
Josh Adam <josh.adam@phac-aspc.gc.ca>
2025-12-18 12:30:34 -0600

    Refactors Sample and WorkflowExecution query classes to inherit from AdvancedSearchQueryForm, consolidating common functionality and improving code maintainability.

--- app/models/workflow_execution/query.rb
+++ app/models/workflow_execution/query.rb
@@ -3,22 +3,11 @@
 # Model to represent workflow execution search form
 # Provides advanced search capabilities for filtering workflow executions
 # Supports groups, conditions, operators, basic search, and pagination
-class WorkflowExecution::Query # rubocop:disable Style/ClassAndModuleChildren
-  include ActiveModel::Model
-  include ActiveModel::Attributes
-  include Pagy::Backend
-  include AdvancedSearchable
-  include AdvancedSearchConditions
-  include AdvancedSearchConditionDispatcher
-  prepend SortableQuery
-  include AdvancedSearchQuery
-
+class WorkflowExecution::Query < AdvancedSearchQueryForm # rubocop:disable Style/ClassAndModuleChildren
   ResultTypeError = Class.new(StandardError)
 
   ALLOWED_SORT_COLUMNS = %w[id name run_id state created_at updated_at].freeze
 
-  attribute :column, :string
-  attribute :direction, :string
   attribute :name_or_id_cont, :string
   attribute :name_or_id_in, default: -> { [] }
   attribute :namespace_ids, default: -> { [] }
@@ -27,40 +16,14 @@ class WorkflowExecution::Query # rubocop:disable Style/ClassAndModuleChildren
       conditions: [WorkflowExecution::SearchCondition.new(field: '', operator: '', value: '')]
     )]
   }
-  attribute :sort, :string, default: 'updated_at desc'
-  attribute :advanced_query, :boolean, default: false
 
-  validates :direction, inclusion: { in: %w[asc desc] }
   validates :namespace_ids, length: { minimum: 1 }
-  validates :column, inclusion: {
-    in: lambda { |record|
-      ALLOWED_SORT_COLUMNS + [record.column].select { |c| c&.start_with?('metadata.') }
-    }
-  }
   validates_with WorkflowExecutionAdvancedSearchGroupValidator
-
-  def initialize(attributes = {}, scope: WorkflowExecution, **kwargs)
-    attributes = if attributes.present?
-                   attributes.merge(kwargs)
-                 else
-                   kwargs
-                 end
-
-    super(attributes)
-    @scope = scope
-    self.sort = sort
-    self.advanced_query = advanced_query?
-    self.groups = groups
-  end
 
   private
 
   def model_class
     WorkflowExecution
-  end
-
-  def search_scope
-    @scope
   end
 
   def filter_column
