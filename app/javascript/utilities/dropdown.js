f655ce473ebe3d7fdd7bf35c00582127f3143c85
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-12-18 14:02:58 -0600

    starting to remove flowbite dropdown

---
+++ app/javascript/utilities/dropdown.js
@@ -0,0 +1,9 @@
+//  Our own custom dropdown.js that replaces flowbite dropdown.js e.g.
+// - handles opening/closing
+// - checking state
+// - event listeners on open/close etc.
+// - styling to position the dropdown above/below based on available space in the viewport
+
+export function testMe() {
+  console.log("test me");
+}
