fe5cd2a337ff01ed3693754c141ad53037ebc58a
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2025-12-29 13:20:15 -0600

    removing unused controller

--- app/javascript/utilities/dropdown.js
+++
@@ -1,9 +0,0 @@
-//  Our own custom dropdown.js that replaces flowbite dropdown.js e.g.
-// - handles opening/closing
-// - checking state
-// - event listeners on open/close etc.
-// - styling to position the dropdown above/below based on available space in the viewport
-
-export function testMe() {
-  console.log("test me");
-}
