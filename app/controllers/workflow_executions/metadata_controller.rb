5ade322d304fcd631b01f69670ddf542d901632c
Chris Huynh <chris.huynh333@gmail.com>
2025-12-30 15:03:57 -0600

    change metadata query

--- app/controllers/workflow_executions/metadata_controller.rb
+++ app/controllers/workflow_executions/metadata_controller.rb
@@ -47,16 +47,34 @@ module WorkflowExecutions
     end
 
     def fetch_metadata
-      metadata = {}
-      @samples.each do |sample|
-        metadata[sample.id] = {
-          sample_id: sample.id,
-          samplesheet_params: {
-            "#{@header}": sample.metadata.fetch(@field, '')
-          }
-        }
+      node = Arel::Nodes::InfixOperation.new('->>', Sample.arel_table[:metadata], Arel::Nodes::Quoted.new(@field))
+      query = Sample.where(id: params[:sample_ids]).pluck(:id, node).map do |results|
+        { "#{results[0]}": { sample_id: results[0], samplesheet_params: { "#{@header}": results[1] } } }
       end
-      metadata
+
+      # query is an array of hashes, and we'll merge them into an empty has to create a nested hash that can be merged
+      # in samplesheet_controller.js
+      {}.merge(*query)
+
+      # TODO: potential logic for when this controller can receive multiple headers in PR1338
+      # fields_to_query = [:id]
+
+      # @metadata_fields.each_value do |metadata_field|
+      #   fields_to_query.append(Arel::Nodes::InfixOperation.new('->>', Sample.arel_table[:metadata], Arel::Nodes::Quoted.new(metadata_field)))
+      # end
+
+      # Sample.where(id: params[:sample_ids]).pluck(fields_to_query).map do |results|
+      #   { "#{results[0]}": { sample_id: results[0], samplesheet_params: retrieve_metadata(results) } }
+      # end
+      #
     end
+
+    # TODO: potential logic for when this controller can receive multiple headers in PR1338
+    # def retrieve_metadata(pluck_results)
+    #   metadata_values = {}
+    #   @metadata_fields.each.with_index(1) do |(header, _metadata_field), index|
+    #     metadata_values[header] = pluck_results[index]
+    #   end
+    # end
   end
 end
