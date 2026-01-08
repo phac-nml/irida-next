27c43049c75b1ff909db11d4c052a6a662d40899
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-01-08 10:46:25 -0600

    playing with stimulus controller inheritance

--- test/components/viral/form/prefixed/select2_component_test.rb
+++ test/components/viral/form/prefixed/select2_component_test.rb
@@ -11,14 +11,14 @@ module Viral
           assert_selector 'input[type="hidden"][name="user"]', visible: :hidden, count: 1
 
           find('input.select2-input[type="text"]').click
-          assert_selector 'div[data-viral--select2-target="dropdown"]', visible: :visible
+          assert_selector 'div[data-viral--select2-target="menu"]', visible: :visible
           assert_selector 'ul[data-viral--select2-target="scroller"] li', count: 50
 
           find('li[data-label="User 1"]').click
           assert_selector 'input[type="hidden"][name="user"][value="1"]', visible: :hidden, count: 1
 
           find('input.select2-input[type="text"]').send_keys([:ctrl, 'a'], '22')
-          assert_selector 'div[data-viral--select2-target="dropdown"]', visible: :visible
+          assert_selector 'div[data-viral--select2-target="menu"]', visible: :visible
           find('ul[data-viral--select2-target="scroller"] > li').click
 
           assert_selector 'input[type="hidden"][name="user"][value="22"]', visible: :hidden, count: 1
