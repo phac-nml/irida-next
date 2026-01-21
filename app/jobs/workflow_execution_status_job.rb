64b90452a93e01aa84f13b167263fab8bb2ef826
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2026-01-21 14:28:50 -0600

    workflow ex status jobs working with continuable

--- app/jobs/workflow_execution_status_job.rb
+++ app/jobs/workflow_execution_status_job.rb
@@ -2,6 +2,8 @@
 
 # Queues the workflow execution status job
 class WorkflowExecutionStatusJob < WorkflowExecutionJob
+  include ActiveJob::Continuable
+
   queue_as :default
   queue_with_priority 5
 
@@ -21,28 +23,55 @@ class WorkflowExecutionStatusJob < WorkflowExecutionJob
   end
 
   def perform(workflow_execution)
-    # User signaled to cancel
-    return if workflow_execution.canceling? || workflow_execution.canceled?
+    @workflow_execution = workflow_execution
 
-    # validate workflow_execution object is fit to run jobs on
-    unless validate_initial_state(workflow_execution, nil, validate_run_id: true)
-      return handle_error_state_and_clean(workflow_execution)
-    end
-
-    wes_connection = Integrations::Ga4ghWesApi::V1::ApiConnection.new.conn
-    result = WorkflowExecutions::StatusService.new(workflow_execution, wes_connection).execute
-
-    if result
-      queue_next_job(workflow_execution.reload)
-    else
-      handle_unable_to_process_job(workflow_execution, self.class.name)
-    end
+    step :verify_initial_state
+    step :query_state
+    step :verify_new_state
+    step :save_state
+    step :queue_next_job
   end
 
-  def queue_next_job(workflow_execution)
-    case workflow_execution.state.to_sym
+  def verify_initial_state
+    # User signaled to cancel
+    @invalid_initial_state = @workflow_execution.canceling? || @workflow_execution.canceled?
+
+    return if @invalid_initial_state
+
+    @invalid_initial_state = !validate_initial_state(@workflow_execution, nil, validate_run_id: true)
+
+    handle_error_state_and_clean(@workflow_execution) if @invalid_initial_state
+  end
+
+  def query_state
+    return if @invalid_initial_state
+
+    @state = WorkflowExecutions::StatusService.new(@workflow_execution).execute
+  end
+
+  def verify_new_state
+    return if @invalid_initial_state
+    return unless @state.nil?
+
+    handle_unable_to_process_job(@workflow_execution, self.class.name)
+  end
+
+  def save_state
+    return if @invalid_initial_state
+    return if @state.nil?
+
+    @workflow_execution.state = @state
+    @workflow_execution.save
+  end
+
+  def queue_next_job
+    return if @invalid_initial_state
+    return if @state.nil?
+
+    @workflow_execution.reload
+    case @workflow_execution.state.to_sym
     when :canceled, :error
-      WorkflowExecutionCleanupJob.perform_later(workflow_execution)
+      WorkflowExecutionCleanupJob.perform_later(@workflow_execution)
     when :completing
       WorkflowExecutionCompletionJob.perform_later(workflow_execution)
     when :canceling # max run time exceeded
--- app/services/workflow_executions/status_service.rb
+++ app/services/workflow_executions/status_service.rb
@@ -3,42 +3,41 @@
 module WorkflowExecutions
   # Service used to update the status of a WorkflowExecution
   class StatusService < BaseService
-    def initialize(workflow_execution, wes_connection, user = nil, params = {})
+    def initialize(workflow_execution, user = nil, params = {})
       super(user, params)
 
       @workflow_execution = workflow_execution
+      wes_connection = Integrations::Ga4ghWesApi::V1::ApiConnection.new.conn
       @wes_client = Integrations::Ga4ghWesApi::V1::Client.new(conn: wes_connection)
     end
 
-    def execute # rubocop:disable Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
+    def execute
       return false if @workflow_execution.run_id.nil?
 
       run_status = @wes_client.get_run_status(@workflow_execution.run_id)
-
       state = run_status[:state]
 
-      new_state = if state == 'RUNNING'
-                    if max_run_time_exceeded_cancel?(@workflow_execution.workflow)
-                      :canceling
-                    else
-                      :running
-                    end
-                  elsif state == 'COMPLETE'
-                    :completing
-                  elsif Integrations::Ga4ghWesApi::V1::States::CANCELATION_STATES.include?(state)
-                    :canceled
-                  elsif Integrations::Ga4ghWesApi::V1::States::ERROR_STATES.include?(state)
-                    :error
-                  end
-
-      @workflow_execution.state = new_state unless new_state.nil?
-
-      @workflow_execution.save
+      if state == 'RUNNING'
+        # Send cancellation request to WES if pipeline has exceeded maximum runtime, if set
+        if max_run_time_exceeded_cancel?(@workflow_execution.workflow)
+          :canceling
+        else
+          :running
+        end
+      elsif state == 'COMPLETE'
+        :completing
+      elsif Integrations::Ga4ghWesApi::V1::States::CANCELATION_STATES.include?(state)
+        :canceled
+      elsif Integrations::Ga4ghWesApi::V1::States::ERROR_STATES.include?(state)
+        :error
+      else
+        Rails.logger.error("Could not process state '#{state}' returned by WES client. Full response:  #{run_status}")
+        nil
+      end
     end
 
     private
 
-    # Send cancellation request to WES if pipeline has exceeded maximum runtime, if set
     def max_run_time_exceeded_cancel?(pipeline)
       run_time = pipeline.state_time_calculation(@workflow_execution, :running)
       maximum_run_time = pipeline.maximum_run_time(@workflow_execution.samples.count)
