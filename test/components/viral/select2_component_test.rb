72e105a05a0fbbb56eae1d9c62c2c8add4285aa1
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-01-26 12:03:50 -0600

    adding feature flag to select2_controller.js

--- test/components/viral/select2_component_test.rb
+++ test/components/viral/select2_component_test.rb
@@ -3,6 +3,9 @@
 require 'application_system_test_case'
 
 class Select2ComponentTest < ApplicationSystemTestCase
+  def setup
+    Flipper.enable(:flowbite_replacement)
+  end
   test 'default' do
     visit '/rails/view_components/viral_select2_component/default'
     assert_selector 'input[type="hidden"][name="user"]', visible: :hidden, count: 1
