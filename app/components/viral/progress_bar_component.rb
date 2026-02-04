f3c1d639199049d46811c4808b8504a958f2b469
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-02-04 14:54:00 -0600

    adding preview for the progress bar component

--- app/components/viral/progress_bar_component.rb
+++ app/components/viral/progress_bar_component.rb
@@ -3,18 +3,23 @@
 module Viral
   # Viral component for displaying a progress bar
   class ProgressBarComponent < Viral::Component
-    attr_reader :text
+    attr_reader :text, :percentage
 
     def initialize(
       text: nil,
+      percentage: 0,
       **system_arguments
     )
       @text = text
+      @percentage = percentage
       @system_arguments = system_arguments
       @system_arguments[:classes] = class_names(
         'fixed bottom-4 right-4 z-50 w-64 p-4 bg-white rounded-lg shadow-md border border-gray-200',
         @system_arguments[:classes]
       )
+      @system_arguments[:data] ||= {}
+      @system_arguments[:data][:controller] = 'progress-bar'
+      @system_arguments[:data]['progress-bar-percentage-value'] = @percentage
     end
   end
 end
