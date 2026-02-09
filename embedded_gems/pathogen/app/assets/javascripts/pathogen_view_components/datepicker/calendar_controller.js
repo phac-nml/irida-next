ed5994884c5ea157a52c73d4806041a3bf9c9db6
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-02-09 14:58:47 -0600

    moving datepicker changes to another PR

--- embedded_gems/pathogen/app/assets/javascripts/pathogen_view_components/datepicker/calendar_controller.js
+++ embedded_gems/pathogen/app/assets/javascripts/pathogen_view_components/datepicker/calendar_controller.js
@@ -498,7 +498,7 @@ export default class extends Controller {
         this.backButtonTarget.disabled)
     ) {
       event.preventDefault();
-      this.pathogenDatepickerInputOutlet.focusTrigger();
+      this.pathogenDatepickerInputOutlet.focusDatepickerInput();
       this.pathogenDatepickerInputOutlet.hideCalendar();
     }
   }
