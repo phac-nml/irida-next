c9b6a105d164a6ec9170220727f7c3e6ffc01367
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-02-12 16:28:36 -0600

    ♻️ refactor: use .send to access private methods in operator tests
    Replace public method declarations with .send(:method_name) to access
    private methods in test classes, following Ruby best practices.
    
    Affected test files:
    - comparison_operators_test.rb
    - equality_operators_test.rb
    - pattern_operators_test.rb
    - set_operators_test.rb

--- test/models/concerns/advanced_search/operators/comparison_operators_test.rb
+++ test/models/concerns/advanced_search/operators/comparison_operators_test.rb
@@ -8,10 +8,6 @@ module AdvancedSearch
       # Test class that includes the ComparisonOperators module
       class TestClass
         include AdvancedSearch::Operators::ComparisonOperators
-
-        # Make private methods accessible for testing
-        public :condition_less_than_or_equal, :condition_greater_than_or_equal,
-               :condition_date_comparison, :condition_numeric_comparison
       end
 
       def setup
@@ -23,10 +19,9 @@ module AdvancedSearch
 
       # condition_less_than_or_equal tests
       test 'condition_less_than_or_equal uses lteq for regular fields' do
-        result = @test_instance.condition_less_than_or_equal(
-          @scope, @created_at_node, '2024-01-01',
-          metadata_field: false, metadata_key: nil
-        )
+        result = @test_instance.send(:condition_less_than_or_equal,
+                                     @scope, @created_at_node, '2024-01-01',
+                                     metadata_field: false, metadata_key: nil)
         sql = result.to_sql
         assert_includes sql, '<='
         assert_not_includes sql, 'TO_DATE'
@@ -34,10 +29,9 @@ module AdvancedSearch
       end
 
       test 'condition_less_than_or_equal uses date comparison for date metadata fields' do
-        result = @test_instance.condition_less_than_or_equal(
-          @scope, @node, '2024-01-01',
-          metadata_field: true, metadata_key: 'created_date'
-        )
+        result = @test_instance.send(:condition_less_than_or_equal,
+                                     @scope, @node, '2024-01-01',
+                                     metadata_field: true, metadata_key: 'created_date')
         sql = result.to_sql
         assert_includes sql, 'TO_DATE'
         assert_includes sql, '<='
@@ -45,10 +39,9 @@ module AdvancedSearch
       end
 
       test 'condition_less_than_or_equal uses numeric comparison for numeric metadata fields' do
-        result = @test_instance.condition_less_than_or_equal(
-          @scope, @node, '100',
-          metadata_field: true, metadata_key: 'count'
-        )
+        result = @test_instance.send(:condition_less_than_or_equal,
+                                     @scope, @node, '100',
+                                     metadata_field: true, metadata_key: 'count')
         sql = result.to_sql
         assert_includes sql, 'CAST'
         assert_includes sql, 'DOUBLE PRECISION'
@@ -57,10 +50,9 @@ module AdvancedSearch
 
       # condition_greater_than_or_equal tests
       test 'condition_greater_than_or_equal uses gteq for regular fields' do
-        result = @test_instance.condition_greater_than_or_equal(
-          @scope, @created_at_node, '2024-01-01',
-          metadata_field: false, metadata_key: nil
-        )
+        result = @test_instance.send(:condition_greater_than_or_equal,
+                                     @scope, @created_at_node, '2024-01-01',
+                                     metadata_field: false, metadata_key: nil)
         sql = result.to_sql
         assert_includes sql, '>='
         assert_not_includes sql, 'TO_DATE'
@@ -68,10 +60,9 @@ module AdvancedSearch
       end
 
       test 'condition_greater_than_or_equal uses date comparison for date metadata fields' do
-        result = @test_instance.condition_greater_than_or_equal(
-          @scope, @node, '2024-01-01',
-          metadata_field: true, metadata_key: 'updated_date'
-        )
+        result = @test_instance.send(:condition_greater_than_or_equal,
+                                     @scope, @node, '2024-01-01',
+                                     metadata_field: true, metadata_key: 'updated_date')
         sql = result.to_sql
         assert_includes sql, 'TO_DATE'
         assert_includes sql, '>='
@@ -79,10 +70,9 @@ module AdvancedSearch
       end
 
       test 'condition_greater_than_or_equal uses numeric comparison for numeric metadata fields' do
-        result = @test_instance.condition_greater_than_or_equal(
-          @scope, @node, '50',
-          metadata_field: true, metadata_key: 'score'
-        )
+        result = @test_instance.send(:condition_greater_than_or_equal,
+                                     @scope, @node, '50',
+                                     metadata_field: true, metadata_key: 'score')
         sql = result.to_sql
         assert_includes sql, 'CAST'
         assert_includes sql, 'DOUBLE PRECISION'
@@ -91,7 +81,7 @@ module AdvancedSearch
 
       # condition_date_comparison tests
       test 'condition_date_comparison includes date regex validation' do
-        result = @test_instance.condition_date_comparison(@scope, @node, '2024-01-01', :lteq)
+        result = @test_instance.send(:condition_date_comparison, @scope, @node, '2024-01-01', :lteq)
         sql = result.to_sql
         # Regex matches YYYY, YYYY-MM, or YYYY-MM-DD formats
         assert_includes sql, '~'
@@ -100,12 +90,12 @@ module AdvancedSearch
       end
 
       test 'condition_date_comparison returns none for invalid date format' do
-        result = @test_instance.condition_date_comparison(@scope, @node, '2024-99-40', :lteq)
+        result = @test_instance.send(:condition_date_comparison, @scope, @node, '2024-99-40', :lteq)
         assert result.none?
       end
 
       test 'condition_date_comparison works with gteq' do
-        result = @test_instance.condition_date_comparison(@scope, @node, '2024-06-15', :gteq)
+        result = @test_instance.send(:condition_date_comparison, @scope, @node, '2024-06-15', :gteq)
         sql = result.to_sql
         assert_includes sql, '>='
         assert_includes sql, 'TO_DATE'
@@ -113,7 +103,7 @@ module AdvancedSearch
 
       # condition_numeric_comparison tests
       test 'condition_numeric_comparison includes numeric regex validation' do
-        result = @test_instance.condition_numeric_comparison(@scope, @node, '123', :lteq)
+        result = @test_instance.send(:condition_numeric_comparison, @scope, @node, '123', :lteq)
         sql = result.to_sql
         # Regex matches integers and decimals (including negatives)
         assert_includes sql, '~'
@@ -121,12 +111,12 @@ module AdvancedSearch
       end
 
       test 'condition_numeric_comparison returns none for invalid numeric format' do
-        result = @test_instance.condition_numeric_comparison(@scope, @node, '12a3', :lteq)
+        result = @test_instance.send(:condition_numeric_comparison, @scope, @node, '12a3', :lteq)
         assert result.none?
       end
 
       test 'condition_numeric_comparison works with gteq' do
-        result = @test_instance.condition_numeric_comparison(@scope, @node, '456.78', :gteq)
+        result = @test_instance.send(:condition_numeric_comparison, @scope, @node, '456.78', :gteq)
         sql = result.to_sql
         assert_includes sql, '>='
         assert_includes sql, 'CAST'
--- test/models/concerns/advanced_search/operators/pattern_operators_test.rb
+++ test/models/concerns/advanced_search/operators/pattern_operators_test.rb
@@ -8,10 +8,6 @@ module AdvancedSearch
       # Test class that includes the PatternOperators module
       class TestClass
         include AdvancedSearch::Operators::PatternOperators
-
-        # Make private methods accessible for testing
-        public :condition_contains, :condition_not_contains, :condition_exists, :condition_not_exists,
-               :cast_to_text_if_uuid, :uuid_column?, :escape_like_wildcards
       end
 
       def setup
@@ -23,67 +19,67 @@ module AdvancedSearch
 
       # escape_like_wildcards tests
       test 'escape_like_wildcards escapes percent sign' do
-        assert_equal '\\%test', @test_instance.escape_like_wildcards('%test')
+        assert_equal '\\%test', @test_instance.send(:escape_like_wildcards, '%test')
       end
 
       test 'escape_like_wildcards escapes underscore' do
-        assert_equal '\\_test', @test_instance.escape_like_wildcards('_test')
+        assert_equal '\\_test', @test_instance.send(:escape_like_wildcards, '_test')
       end
 
       test 'escape_like_wildcards escapes backslash' do
-        assert_equal '\\\\test', @test_instance.escape_like_wildcards('\\test')
+        assert_equal '\\\\test', @test_instance.send(:escape_like_wildcards, '\\test')
       end
 
       test 'escape_like_wildcards escapes multiple special characters' do
-        assert_equal '\\%\\_\\\\', @test_instance.escape_like_wildcards('%_\\')
+        assert_equal '\\%\\_\\\\', @test_instance.send(:escape_like_wildcards, '%_\\')
       end
 
       test 'escape_like_wildcards returns string unchanged if no special characters' do
         # NOTE: underscore IS a special character in SQL LIKE, so it gets escaped
-        assert_equal 'normalstring', @test_instance.escape_like_wildcards('normalstring')
+        assert_equal 'normalstring', @test_instance.send(:escape_like_wildcards, 'normalstring')
       end
 
       # uuid_column? tests
       test 'uuid_column? returns true for uuid column' do
-        assert @test_instance.uuid_column?(WorkflowExecution, 'id')
+        assert @test_instance.send(:uuid_column?, WorkflowExecution, 'id')
       end
 
       test 'uuid_column? returns false for non-uuid column' do
-        assert_not @test_instance.uuid_column?(WorkflowExecution, 'name')
+        assert_not @test_instance.send(:uuid_column?, WorkflowExecution, 'name')
       end
 
       test 'uuid_column? returns false for non-existent column' do
-        assert_not @test_instance.uuid_column?(WorkflowExecution, 'nonexistent')
+        assert_not @test_instance.send(:uuid_column?, WorkflowExecution, 'nonexistent')
       end
 
       # cast_to_text_if_uuid tests
       test 'cast_to_text_if_uuid returns original node for non-uuid column' do
-        result = @test_instance.cast_to_text_if_uuid(@node, WorkflowExecution, 'name')
+        result = @test_instance.send(:cast_to_text_if_uuid, @node, WorkflowExecution, 'name')
         assert_equal @node, result
       end
 
       test 'cast_to_text_if_uuid returns cast function for uuid column' do
-        result = @test_instance.cast_to_text_if_uuid(@uuid_node, WorkflowExecution, 'id')
+        result = @test_instance.send(:cast_to_text_if_uuid, @uuid_node, WorkflowExecution, 'id')
         assert_instance_of Arel::Nodes::NamedFunction, result
         assert_equal 'CAST', result.name
       end
 
       # condition_contains tests
       test 'condition_contains creates ILIKE query with wildcards' do
-        result = @test_instance.condition_contains(@scope, @node, 'test')
+        result = @test_instance.send(:condition_contains, @scope, @node, 'test')
         sql = result.to_sql
         assert_includes sql, "ILIKE '%test%'"
       end
 
       test 'condition_contains escapes special characters in value' do
-        result = @test_instance.condition_contains(@scope, @node, '%test%')
+        result = @test_instance.send(:condition_contains, @scope, @node, '%test%')
         sql = result.to_sql
         assert_includes sql, "ILIKE '%\\%test\\%%'"
       end
 
       test 'condition_contains casts uuid column to text' do
-        result = @test_instance.condition_contains(@scope, @uuid_node, 'abc', model_class: WorkflowExecution,
-                                                                              field_name: 'id')
+        result = @test_instance.send(:condition_contains, @scope, @uuid_node, 'abc',
+                                     model_class: WorkflowExecution, field_name: 'id')
         sql = result.to_sql
         assert_includes sql, 'CAST'
         assert_includes sql, 'TEXT'
@@ -91,28 +87,28 @@ module AdvancedSearch
 
       # condition_not_contains tests
       test 'condition_not_contains creates NOT ILIKE query with null check' do
-        result = @test_instance.condition_not_contains(@scope, @node, 'test')
+        result = @test_instance.send(:condition_not_contains, @scope, @node, 'test')
         sql = result.to_sql
         assert_includes sql, 'NOT ILIKE'
         assert_includes sql, 'IS NULL'
       end
 
       test 'condition_not_contains escapes special characters in value' do
-        result = @test_instance.condition_not_contains(@scope, @node, '%test%')
+        result = @test_instance.send(:condition_not_contains, @scope, @node, '%test%')
         sql = result.to_sql
         assert_includes sql, "NOT ILIKE '%\\%test\\%%'"
       end
 
       # condition_exists tests
       test 'condition_exists creates IS NOT NULL query' do
-        result = @test_instance.condition_exists(@scope, @node)
+        result = @test_instance.send(:condition_exists, @scope, @node)
         sql = result.to_sql
         assert_includes sql, 'IS NOT NULL'
       end
 
       # condition_not_exists tests
       test 'condition_not_exists creates IS NULL query' do
-        result = @test_instance.condition_not_exists(@scope, @node)
+        result = @test_instance.send(:condition_not_exists, @scope, @node)
         sql = result.to_sql
         assert_includes sql, 'IS NULL'
       end
