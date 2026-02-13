e978fea5ce8e6dcf4594bb6759a851af99b26265
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2026-02-13 12:31:17 -0600

    refactor and add tests

--- app/jobs/workflow_execution_cleanup_job.rb
+++ app/jobs/workflow_execution_cleanup_job.rb
@@ -2,19 +2,32 @@
 
 # cleans up workflow execution files no longer needed after completion
 class WorkflowExecutionCleanupJob < WorkflowExecutionJob
+  include ActiveJob::Continuable
+
   queue_as :default
   queue_with_priority 30
 
   def perform(workflow_execution)
-    # Don't run service if already cleaned
-    return if workflow_execution.nil? || workflow_execution.cleaned?
+    @workflow_execution = workflow_execution
 
     # Only run service on runs that can be cleaned
-    return unless workflow_execution.completed? ||
-                  workflow_execution.canceled? ||
-                  workflow_execution.error?
+    return unless validate_initial_state(@workflow_execution, %i[completed error canceled], validate_run_id: false)
+    # Don't run service if already cleaned
+    return if @workflow_execution.nil? || @workflow_execution.cleaned?
 
-    # Final stage of WorkflowExecution life cycle. No further work required.
-    WorkflowExecutions::CleanupService.new(workflow_execution).execute
+    step :clean_up_blob_run_directory
+    step :update_to_cleaned
+  end
+
+  def clean_up_blob_run_directory
+    # This check is for safety as passing nil or an empty string into deleted_prefixed will delete all blobs
+    if @workflow_execution.blob_run_directory.present? # rubocop:disable Style/GuardClause
+      ActiveStorage::Blob.service.delete_prefixed(@workflow_execution.blob_run_directory)
+    end
+  end
+
+  def update_to_cleaned
+    @workflow_execution.cleaned = true
+    @workflow_execution.save
   end
 end
