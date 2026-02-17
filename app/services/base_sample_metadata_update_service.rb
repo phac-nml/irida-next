997c9388e08af8f1dadcdc1a18d59cf00d01310b
Chris Huynh <chris.huynh333@gmail.com>
2026-02-17 13:52:33 -0600

    add starting bulk service, start refactor of metadata file import service

---
+++ app/services/base_sample_metadata_update_service.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+# Service used to Delete Samples
+class BaseSampleMetadataUpdateService < BaseService
+  attr_accessor :sample, :sample_ids, :namespace
+
+  def initialize(user = nil, params = {})
+    super
+  end
+
+  private
+
+  def perform_metadata_update(sample, metadata)
+    metadata_changes = { added: [], updated: [], deleted: [], not_updated: [], unchanged: [] }
+    metadata.each do |key, value|
+      validate_metadata_value(key, value)
+
+      key = strip_whitespaces(key.to_s.downcase)
+      value = strip_whitespaces(value.to_s) # remove data types
+      status = get_metadata_change_status(sample, key, value)
+      next unless status
+
+      metadata_changes[status] << key
+      if %i[updated added].include?(status)
+        add_metadata_to_sample(sample, key, value)
+      elsif status == :deleted
+        remove_metadata_from_sample(sample, key)
+      end
+    end
+    metadata_changes
+  end
+
+  def remove_metadata_from_sample(sample, key)
+    sample.metadata.delete(key)
+    sample.metadata_provenance.delete(key)
+  end
+
+  def add_metadata_to_sample(sample, key, value)
+    sample.metadata_provenance[key] =
+      if @analysis_id.nil?
+        { source: 'user', id: current_user.id, updated_at: Time.current }
+      else
+        { source: 'analysis', id: '1', updated_at: Time.current }
+      end
+    sample.metadata[key] = value
+  end
+
+  def get_metadata_change_status(sample, key, value) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    if value.blank?
+      :deleted if sample.field?(key)
+    elsif sample.metadata_provenance.key?(key) && @analysis_id.nil? &&
+          sample.metadata_provenance[key]['source'] == 'analysis'
+      :not_updated
+    elsif sample.field?(key) && sample.metadata[key] == value
+      @force_update ? :updated : :unchanged
+    else
+      sample.field?(key) ? :updated : :added
+    end
+  end
+end
