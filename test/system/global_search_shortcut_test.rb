7e668d194f6e5070483330dd46a34b08c97efd7c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-02-20 20:01:38 -0600

    🚸 feat: open global search in shortcut dialog with filters

---
+++ test/system/global_search_shortcut_test.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class GlobalSearchShortcutTest < ApplicationSystemTestCase
+  setup do
+    login_as users(:john_doe)
+  end
+
+  test 'ctrl+k opens dialog on nested pages and escape clears and closes it' do
+    visit '/-/groups/group-1'
+
+    trigger_global_search_shortcut
+
+    dialog = find("dialog[data-global-search-shortcut-target='dialog'][open]", visible: :all)
+
+    within(dialog) do
+      find('summary', text: 'Filters').click
+      find("input[name='q']").set('Shigella')
+      uncheck('Group')
+    end
+
+    find('body').send_keys(:escape)
+    assert_no_selector "dialog[data-global-search-shortcut-target='dialog'][open]", visible: :all
+
+    trigger_global_search_shortcut
+    dialog = find("dialog[data-global-search-shortcut-target='dialog'][open]", visible: :all)
+
+    within(dialog) do
+      assert_equal '', find("input[name='q']").value
+      assert find("input[name='types[]'][value='groups']", visible: :all).checked?
+    end
+  end
+
+  test 'enter submits dialog form to global search route' do
+    visit '/-/projects/project-1'
+
+    trigger_global_search_shortcut
+
+    within("dialog[data-global-search-shortcut-target='dialog'][open]") do
+      find("input[name='q']").set('Project 1')
+      find("input[name='q']").send_keys(:enter)
+    end
+
+    assert_current_path(%r{\A/-/search\?})
+    assert_selector '[data-global-search-version="g1"]'
+    assert_field 'q', with: 'Project 1'
+  end
+
+  private
+
+  def trigger_global_search_shortcut
+    page.execute_script(<<~JS)
+      window.dispatchEvent(new KeyboardEvent("keydown", {
+        key: "k",
+        ctrlKey: true,
+        bubbles: true
+      }));
+    JS
+  end
+end
