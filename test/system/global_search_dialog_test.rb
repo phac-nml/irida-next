5fc88b67df2c424c73bbd53c9c6ed9b4ea9bcd15
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-02-23 15:02:01 -0600

    🐛 fix: harden global search dialog keyboard handling

--- test/system/global_search_dialog_test.rb
+++ test/system/global_search_dialog_test.rb
@@ -24,7 +24,26 @@ class GlobalSearchDialogTest < ApplicationSystemTestCase
     within(dialog) do
       heading = find('#global-search-dialog-title', visible: :all)
       assert heading.matches_css?('.sr-only', wait: 0)
+      assert_selector "label[for='global-search-dialog-query'].sr-only", visible: :all
     end
+  end
+
+  test 'slash opens dialog on nested pages' do
+    visit '/-/groups/group-1'
+
+    trigger_slash_shortcut
+
+    assert_selector "dialog[data-global-search-dialog-target='dialog'][open]", visible: :all
+  end
+
+  test 'global search shortcuts ignore repeated and composing key events' do
+    visit '/-/groups/group-1'
+
+    trigger_keydown(key: 'k', ctrl_key: true, repeat: true)
+    assert_no_selector "dialog[data-global-search-dialog-target='dialog'][open]", visible: :all
+
+    trigger_keydown(key: 'k', ctrl_key: true, composing: true)
+    assert_no_selector "dialog[data-global-search-dialog-target='dialog'][open]", visible: :all
   end
 
   test 'ctrl+k opens dialog on nested pages and escape clears and closes it' do
@@ -79,12 +98,40 @@ class GlobalSearchDialogTest < ApplicationSystemTestCase
   private
 
   def trigger_global_search_shortcut
-    page.execute_script(<<~JS)
-      window.dispatchEvent(new KeyboardEvent("keydown", {
-        key: "k",
-        ctrlKey: true,
+    trigger_keydown(key: 'k', ctrl_key: true)
+  end
+
+  def trigger_slash_shortcut
+    trigger_keydown(key: '/')
+  end
+
+  def trigger_keydown(options)
+    page.execute_script(keydown_dispatch_script, options)
+  end
+
+  def keydown_dispatch_script
+    <<~JS
+      const options = arguments[0];
+      const event = new KeyboardEvent("keydown", {
+        key: options.key,
+        code: options.code,
+        ctrlKey: Boolean(options.ctrl_key),
+        metaKey: Boolean(options.meta_key),
+        altKey: Boolean(options.alt_key),
+        shiftKey: Boolean(options.shift_key),
+        repeat: Boolean(options.repeat),
         bubbles: true
-      }));
+      });
+
+      if (options.composing) {
+        try {
+          Object.defineProperty(event, "isComposing", { value: true });
+        } catch (_error) {
+          // Ignore unsupported property override in browser driver.
+        }
+      }
+
+      window.dispatchEvent(event);
     JS
   end
 end
