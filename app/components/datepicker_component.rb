c7586d4e931332cedd238b58c51e358d97329290
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-02-26 16:32:33 -0600

    ♻️ refactor: simplify DatepickerComponent initialization by consolidating parameters

--- app/components/datepicker_component.rb
+++ app/components/datepicker_component.rb
@@ -8,31 +8,10 @@ class DatepickerComponent < Component
     v1: Datepicker::V1::Component
   }.freeze
 
-  # rubocop:disable Metrics/ParameterLists
-  def initialize(
-    id:,
-    input_name:,
-    version: nil,
-    label: nil,
-    input_aria_label: nil,
-    min_date: 1.day.from_now,
-    selected_date: nil,
-    autosubmit: false,
-    calendar_arguments: {},
-    **system_arguments
-  )
+  def initialize(version: nil, **args)
     @version = version
-    @id = id
-    @input_name = input_name
-    @label = label
-    @input_aria_label = input_aria_label
-    @min_date = min_date
-    @selected_date = selected_date
-    @autosubmit = autosubmit
-    @calendar_arguments = calendar_arguments
-    @system_arguments = system_arguments
+    @args = args
   end
-  # rubocop:enable Metrics/ParameterLists
 
   def call
     render resolved_component
@@ -41,17 +20,7 @@ class DatepickerComponent < Component
   private
 
   def resolved_component
-    implementation_class.new(
-      id: @id,
-      input_name: @input_name,
-      label: @label,
-      input_aria_label: @input_aria_label,
-      min_date: @min_date,
-      selected_date: @selected_date,
-      autosubmit: @autosubmit,
-      calendar_arguments: @calendar_arguments,
-      **@system_arguments
-    )
+    implementation_class.new(**@args)
   end
 
   def implementation_class
