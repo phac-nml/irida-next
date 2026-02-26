23f67b21198a08add6e32b68fb0e5ab5f99ed639
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-02-26 12:04:56 -0600

    ♻️ refactor: remove datepicker assets from embedded pathogen

--- config/initializers/pathogen_view_paths.rb
+++ config/initializers/pathogen_view_paths.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
 ActionController::Base.append_view_path Rails.root.join('embedded_gems/pathogen/app/components')
-ActionController::Base.append_view_path Rails.root.join('app/components')
--- embedded_gems/pathogen/config/importmap.rb
+++ embedded_gems/pathogen/config/importmap.rb
@@ -4,12 +4,6 @@
 # Note: pin_all_from doesn't work reliably with engine paths, so we pin explicitly
 pin 'pathogen_view_components/tabs_controller', to: 'pathogen_view_components/tabs_controller.js'
 pin 'pathogen_view_components/tooltip_controller', to: 'pathogen_view_components/tooltip_controller.js'
-pin 'pathogen_view_components/datepicker/input_controller',
-    to: 'pathogen_view_components/datepicker/input_controller.js'
-pin 'pathogen_view_components/datepicker/calendar_controller',
-    to: 'pathogen_view_components/datepicker/calendar_controller.js'
-pin 'pathogen_view_components/datepicker/utils', to: 'pathogen_view_components/datepicker/utils.js'
-pin 'pathogen_view_components/datepicker/constants', to: 'pathogen_view_components/datepicker/constants.js'
 
 # Pin main entry point
 pin 'pathogen_view_components', to: 'pathogen_view_components.js'
