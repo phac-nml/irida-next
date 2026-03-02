810e88e9dde6f0c7d74edcc023bff634434f30ce
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-03-02 11:16:04 -0600

    adding comments to the menu_controller.js

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,3 +1,23 @@
+// Stimulus controller that handles an accessible floating menu. It uses
+// Floating UI (formerly Popper) to position the menu relative to a trigger
+// element and manages show/hide logic including outside click detection.
+//
+// The controller is shared between multiple menu implementations
+// (e.g. dropdown, select2, combobox, and datepicker)
+// and is designed to be flexible and reusable. It exposes a `share()` method
+// for passing in optional callbacks that run on show/hide, and it supports
+// both click-triggered and non-click-triggered menus.
+//
+// Usage example (dropdown):
+//
+// <button data-controller="menu" data-menu-trigger-type="click">
+//   Toggle menu
+// </button>
+//
+// <div data-menu-target="menu" hidden>
+//   <!-- menu content -->
+// </div>
+
 import { Controller } from "@hotwired/stimulus";
 import {
   autoUpdate,
@@ -73,6 +93,9 @@ export default class MenuController extends Controller {
       this.#onShow();
     }
 
+    // Floating UI will call `this.update()` whenever it detects that the
+    // trigger/menu geometry changes (scrolling, resize, etc.). The returned
+    // cleanup function is stored so we can call it on hide/disconnect.
     this.#cleanup = autoUpdate(
       this.triggerTarget,
       this.menuTarget,
@@ -95,6 +118,8 @@ export default class MenuController extends Controller {
     this.#cleanup?.();
   }
 
+  // recompute the menu position using Floating UI; called initially on show
+  // and subsequently by `autoUpdate` whenever layout changes.
   update() {
     computePosition(this.triggerTarget, this.menuTarget, {
       placement: "bottom",
@@ -119,7 +144,7 @@ export default class MenuController extends Controller {
         });
       })
       .catch(() => {
-        console.log("catch");
+        // fallback for when Floating UI fails; we simply align below trigger
         const triggerRect = this.triggerTarget.getBoundingClientRect();
         Object.assign(this.menuTarget.style, {
           position: this.strategyValue,
