c4b5276cf6f33ea7d9163ef00438ee2d958dae69
Chris Huynh <chris.huynh333@gmail.com>
2026-03-04 13:45:02 -0600

    fix incorrect comment

--- app/components/nextflow_component.rb
+++ app/components/nextflow_component.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Stable entrypoint for rendering a drop down across UI versions.
+# Stable entrypoint for rendering Nextflow component across UI versions.
 class NextflowComponent < Versioning::VersionedComponent
   IMPLEMENTATIONS = {
     v1: Nextflow::V1::Component,
