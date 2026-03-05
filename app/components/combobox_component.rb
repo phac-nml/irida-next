c9a3bbd3dcd0fd65a794c15c7dc7a3ea8d6332b7
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-03-05 15:09:46 -0600

    adding v2 combobox component which uses floating ui

--- app/components/combobox_component.rb
+++ app/components/combobox_component.rb
@@ -3,8 +3,11 @@
 # Stable entrypoint for rendering a combobox across UI versions.
 class ComboboxComponent < Versioning::VersionedComponent
   IMPLEMENTATIONS = {
-    v1: Combobox::V1::Component
+    v1: Combobox::V1::Component,
+    v2: Combobox::V2::Component
   }.freeze
 
-  VERSION_RESOLVER = -> {}
+  VERSION_RESOLVER = lambda {
+    Flipper.enabled?(:v2_combobox) ? :v2 : :v1
+  }
 end
