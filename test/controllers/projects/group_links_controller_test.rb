d67d754a803c30f39d7da72adbe6a6551f666eb4
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-03-05 20:35:22 -0600

    test(group-links): migrate sorting assertions to controller tests

--- test/controllers/projects/group_links_controller_test.rb
+++ test/controllers/projects/group_links_controller_test.rb
@@ -6,6 +6,42 @@ module Projects
   class GroupLinksControllerTest < ActionDispatch::IntegrationTest
     include Devise::Test::IntegrationHelpers
 
+    test 'should apply default sort and support sorting project group links' do
+      sign_in users(:john_doe)
+
+      project_namespace = namespaces_project_namespaces(:project25_namespace)
+      project = projects(:project25)
+      group_link2 = namespace_group_links(:namespace_group_link2)
+      group_link5 = namespace_group_links(:namespace_group_link5)
+      group_link14 = namespace_group_links(:namespace_group_link14)
+
+      get namespace_project_group_links_path(project_namespace.parent, project, format: :turbo_stream)
+      assert_response :success
+      assert_sort_state(1, 'ascending')
+      assert_first_rows_include(group_link2.group.name, group_link5.group.name)
+
+      get namespace_project_group_links_path(project_namespace.parent, project,
+                                             format: :turbo_stream, group_links_q: { s: 'group_name desc' })
+      assert_response :success
+      assert_sort_state(1, 'descending')
+      group_names = Nokogiri::HTML(response.body).css('table tbody tr td:first-child').map { |node| node.text.squish }
+      assert_equal group_link14.group.name, group_names.first
+      assert_equal group_link2.group.name, group_names.last
+
+      get namespace_project_group_links_path(project_namespace.parent, project,
+                                             format: :turbo_stream, group_links_q: { s: 'group_access_level asc' })
+      assert_response :success
+      assert_sort_state(4, 'ascending')
+      group_names = Nokogiri::HTML(response.body).css('table tbody tr td:first-child').map { |node| node.text.squish }
+      assert_equal group_link5.group.name, group_names.first
+      assert_equal group_link14.group.name, group_names.last
+
+      get namespace_project_group_links_path(project_namespace.parent, project,
+                                             format: :turbo_stream, group_links_q: { s: 'expires_at asc' })
+      assert_response :success
+      assert_sort_state(5, 'ascending')
+    end
+
     test 'should share project namespace with group' do
       sign_in users(:john_doe)
       group = groups(:group_one)
