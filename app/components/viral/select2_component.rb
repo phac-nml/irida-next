7f44714f4a89ac755ce138e9bb7d114e0627424b
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-03-09 11:16:49 -0500

    starting to create the v2 select2 component

--- app/components/viral/select2_component.rb
+++ app/components/viral/select2_component.rb
@@ -4,9 +4,12 @@ module Viral
   # Stable entrypoint for rendering a searchable drop down across UI versions.
   class Select2Component < Versioning::VersionedComponent
     IMPLEMENTATIONS = {
-      v1: Viral::Select2::V1::Component
+      v1: Viral::Select2::V1::Component,
+      v2: Viral::Select2::V2::Component
     }.freeze
 
-    VERSION_RESOLVER = -> {}
+    VERSION_RESOLVER = lambda {
+      Flipper.enabled?(:v2_select2) ? :v2 : :v1
+    }
   end
 end
