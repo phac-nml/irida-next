77a0a64eeff8b2b0cd016b96fc25fd36c06576cd
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-03-27 16:50:27 -0500

    🐛 fix: move icon previews to app-owned namespace

---
+++ test/components/previews/icon_component_preview.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class IconComponentPreview < ViewComponent::Preview
+  # @!group App Icons
+
+  # @label Basic Usage & Getting Started
+  def basic_usage; end
+
+  # @label Colors & Sizes
+  def colors_and_sizes; end
+
+  # @label Factory Methods & Enhanced Features
+  def factory_methods; end
+
+  # @label Icon Library & Catalog
+  def icon_library; end
+
+  # @!endgroup
+end
