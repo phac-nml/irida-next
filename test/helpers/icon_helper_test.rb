2672eb7c810b815207b0c052dade2c3c9a28d137
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-03-28 20:18:38 -0500

    🐛 fix: stabilize icon helper tests with view context

--- test/helpers/icon_helper_test.rb
+++ test/helpers/icon_helper_test.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require 'view_component_test_case'
 
-class IconHelperTest < ActionView::TestCase
-  include IconHelper
+class IconHelperTest < ViewComponentTestCase
+  delegate :view_context, to: :vc_test_controller
 
   test 'rails_icon renders native rails_icons output' do
-    fragment = Nokogiri::HTML::DocumentFragment.parse(rails_icon(:check).to_s)
+    fragment = Nokogiri::HTML::DocumentFragment.parse(view_context.rails_icon(:check).to_s)
 
     assert_equal 1, fragment.css('svg').count
   end
 
   test 'icon renders app icon component output' do
-    fragment = Nokogiri::HTML::DocumentFragment.parse(icon(:check, size: :sm, color: :primary).to_s)
+    fragment = Nokogiri::HTML::DocumentFragment.parse(view_context.icon(:check, size: :sm, color: :primary).to_s)
 
     assert_equal 1, fragment.css('svg.check-icon.size-4').count
     assert_includes fragment.to_html, 'text-primary-600'
   end
 
   test 'icon helper accepts a block' do
-    fragment = Nokogiri::HTML::DocumentFragment.parse(icon(:check, size: :sm) { 'ignored' }.to_s)
+    fragment = Nokogiri::HTML::DocumentFragment.parse(view_context.icon(:check, size: :sm) { 'ignored' }.to_s)
 
     assert_equal 1, fragment.css('svg.check-icon.size-4').count
   end
