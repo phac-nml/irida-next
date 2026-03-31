74b2a3c50be5d7d108258dfd52b6a61c0f077f60
Eric Enns <492127+ericenns@users.noreply.github.com>
2026-03-31 07:37:00 -0500

    chore: reduce flakiness of project history test

--- test/system/projects/history_test.rb
+++ test/system/projects/history_test.rb
@@ -30,32 +30,31 @@ module Projects
 
     test 'can see modal with changes to project from previous version' do
       visit namespace_project_history_path(@namespace, @project)
-      click_on 'Version 1'
+      assert_button I18n.t(:'components.history.link_text', version: 1)
+      click_button I18n.t(:'components.history.link_text', version: 1)
 
-      within('#history_modal') do
-        assert_selector 'h1', text: I18n.t(:'components.history.link_text', version: 1)
-        assert_selector 'p', text: I18n.t(:'projects.history.project_history_modal_description.project_created',
-                                          user: 'System')
+      assert_selector 'dialog h1', text: I18n.t(:'components.history.link_text', version: 1)
+      assert_selector 'dialog p', text: I18n.t(:'projects.history.project_history_modal_description.project_created',
+                                               user: 'System')
 
-        assert_no_text I18n.t(:'components.history_version.previous_version')
-        assert_text I18n.t(:'components.history_version.current_version', version: 1)
+      assert_no_text I18n.t(:'components.history_version.previous_version')
+      assert_text I18n.t(:'components.history_version.current_version', version: 1)
 
-        assert_selector 'span[data-test-item-selector="key"]', text: 'name'
-        assert_selector 'span[data-test-item-selector="key"]', text: 'path'
-        assert_selector 'span[data-test-item-selector="key"]', text: 'type'
-        assert_selector 'span[data-test-item-selector="key"]', text: 'owner_id'
-        assert_selector 'span[data-test-item-selector="key"]', text: 'parent_id'
-        assert_selector 'span[data-test-item-selector="key"]', text: 'description'
-        assert_selector 'span[data-test-item-selector="key"]', text: 'puid'
+      assert_selector 'span[data-test-item-selector="key"]', text: 'name'
+      assert_selector 'span[data-test-item-selector="key"]', text: 'path'
+      assert_selector 'span[data-test-item-selector="key"]', text: 'type'
+      assert_selector 'span[data-test-item-selector="key"]', text: 'owner_id'
+      assert_selector 'span[data-test-item-selector="key"]', text: 'parent_id'
+      assert_selector 'span[data-test-item-selector="key"]', text: 'description'
+      assert_selector 'span[data-test-item-selector="key"]', text: 'puid'
 
-        assert_selector 'span[data-test-item-selector="value"]', text: 'Project 1'
-        assert_selector 'span[data-test-item-selector="value"]', text: 'project-1'
-        assert_selector 'span[data-test-item-selector="value"]', text: 'Project'
-        assert_selector 'span[data-test-item-selector="value"]', text: @project.namespace.owner.id
-        assert_selector 'span[data-test-item-selector="value"]', text: @project.namespace.parent.id
-        assert_selector 'span[data-test-item-selector="value"]', text: 'Project 1 description'
-        assert_selector 'span[data-test-item-selector="value"]', text: @project.puid
-      end
+      assert_selector 'span[data-test-item-selector="value"]', text: 'Project 1'
+      assert_selector 'span[data-test-item-selector="value"]', text: 'project-1'
+      assert_selector 'span[data-test-item-selector="value"]', text: 'Project'
+      assert_selector 'span[data-test-item-selector="value"]', text: @project.namespace.owner.id
+      assert_selector 'span[data-test-item-selector="value"]', text: @project.namespace.parent.id
+      assert_selector 'span[data-test-item-selector="value"]', text: 'Project 1 description'
+      assert_selector 'span[data-test-item-selector="value"]', text: @project.puid
     end
   end
 end
