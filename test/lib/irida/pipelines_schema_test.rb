efcfea06c0910af20398efd87e1cdb163437c1a1
Eric Enns <492127+ericenns@users.noreply.github.com>
2026-04-13 10:32:21 -0500

    chore: fix tests that used stubbing of github user content to instead mock clone_repo

--- test/lib/irida/pipelines_schema_test.rb
+++ test/lib/irida/pipelines_schema_test.rb
@@ -7,26 +7,21 @@ class PipelinesSchemaTest < ActiveSupport::TestCase
   setup do
     @pipeline_schema_file_dir = "#{ActiveStorage::Blob.service.root}/pipelines"
 
-    # Read in schema file to json
-    body = Rails.root.join('test/fixtures/files/nextflow/nextflow_schema.json')
+    @test_schema_body = Rails.root.join('test/fixtures/files/nextflow/nextflow_schema.json').read
 
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.2/nextflow_schema.json')
-      .to_return(status: 200, body:, headers: { etag: '[W/"a1Ab"]' })
+    # Use a closure to capture @test_schema_body for the clone_repo implementation
+    schema_body = @test_schema_body
+    clone_repo_impl = lambda do |_uri, _sha, clone_dir|
+      FileUtils.mkdir_p(clone_dir)
+      File.write(File.join(clone_dir, 'nextflow_schema.json'), schema_body)
 
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.2/assets/schema_input.json')
-      .to_return(status: 200, body:, headers: { etag: '[W/"b1Bc"]' })
+      # Create assets/schema_input.json
+      FileUtils.mkdir_p(File.join(clone_dir, 'assets'))
+      File.write(File.join(clone_dir, 'assets', 'schema_input.json'), schema_body)
+      nil
+    end
 
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.1/nextflow_schema.json')
-      .to_return(status: 200, body:, headers: { etag: '[W/"c1Cd"]' })
-
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.1/assets/schema_input.json')
-      .to_return(status: 200, body:, headers: { etag: '[W/"d1De"]' })
-
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.0/nextflow_schema.json')
-      .to_return(status: 200, body:, headers: { etag: '[W/"e1Ef"]' })
-
-    stub_request(:any, 'https://raw.githubusercontent.com/phac-nml/iridanextexample/1.0.0/assets/schema_input.json')
-      .to_return(status: 200, body:, headers: { etag: '[W/"f1Fg"]' })
+    Irida::PipelineRepository.singleton_class.send(:define_method, :clone_repo, clone_repo_impl)
   end
 
   teardown do
