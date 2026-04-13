4ec360f42701bfbf95ce62d0ea6131796fc04e0c
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-04-13 14:01:04 -0500

    moving component tests

--- test/components/prefixed/select2/v2/component_test.rb
+++ test/components/prefixed/select2/v2/component_test.rb
@@ -2,24 +2,28 @@
 
 require 'application_system_test_case'
 
-module Viral
-  module Form
-    module Prefixed
-      class Select2ComponentTest < ApplicationSystemTestCase
+module Prefixed
+  module Select2
+    module V2
+      class ComponentTest < ApplicationSystemTestCase
+        setup do
+          Flipper.enable(:v2_prefixed_select2)
+        end
+
         test 'default' do
-          visit '/rails/view_components/viral_form_prefixed_select2_component/default'
+          visit '/rails/view_components/prefixed_select2_component/default'
           assert_selector 'input[type="hidden"][name="user"]', visible: :hidden, count: 1
 
           find('input.select2-input[type="text"]').click
-          assert_selector 'div[data-select2--v1-target="dropdown"]', visible: :visible
-          assert_selector 'ul[data-select2--v1-target="scroller"] li', count: 50
+          assert_selector 'div[data-select2--v2-target="dropdown"]', visible: :visible
+          assert_selector 'ul[data-select2--v2-target="scroller"] li', count: 50
 
           find('li[data-label="User 1"]').click
           assert_selector 'input[type="hidden"][name="user"][value="1"]', visible: :hidden, count: 1
 
           find('input.select2-input[type="text"]').send_keys([:ctrl, 'a'], '22')
-          assert_selector 'div[data-select2--v1-target="dropdown"]', visible: :visible
-          find('ul[data-select2--v1-target="scroller"] > li').click
+          assert_selector 'div[data-select2--v2-target="dropdown"]', visible: :visible
+          find('ul[data-select2--v2-target="scroller"] > li').click
 
           assert_selector 'input[type="hidden"][name="user"][value="22"]', visible: :hidden, count: 1
         end
