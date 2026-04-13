56ecce2936f52b204d585fce79d26371dba1e9a8
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-04-13 15:16:23 -0500

    versioning the language selection component

---
+++ app/components/layout/language_selection/v1/component.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Layout
+  module LanguageSelection
+    module V1
+      # Component for rendering language selection
+      class Component < ::Component
+        def initialize(user: Current.user)
+          @user = user
+          @locale_options = I18n.available_locales.map do |locale|
+            [I18n.t(:"locales.#{locale}", locale: locale), locale]
+          end
+        end
+
+        private
+
+        def locale
+          @user.locale.to_sym
+        end
+      end
+    end
+  end
+end
--- app/components/layout/language_selection_component.rb
+++ app/components/layout/language_selection_component.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 module Layout
-  # Component for rendering language selection
-  class LanguageSelectionComponent < Component
-    def initialize(user: Current.user)
-      @user = user
-      @locale_options = I18n.available_locales.map { |locale| [I18n.t(:"locales.#{locale}", locale: locale), locale] }
-    end
+  # Stable entrypoint for rendering a language selection menu across UI versions.
+  class LanguageSelectionComponent < Versioning::VersionedComponent
+    IMPLEMENTATIONS = {
+      v1: Layout::LanguageSelection::V1::Component,
+      v2: Layout::LanguageSelection::V2::Component
+    }.freeze
 
-    private
-
-    def locale
-      @user.locale.to_sym
-    end
+    VERSION_RESOLVER = lambda {
+      Flipper.enabled?(:v2_language_selection, Current.user) ? :v2 : :v1
+    }
   end
 end
