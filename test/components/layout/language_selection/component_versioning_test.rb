ce3c9e37ee85d2dfb2b0b78a9ef6e525dbb2442e
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-04-14 13:56:32 -0500

    fixing modules within component version testing

--- test/components/layout/language_selection/component_versioning_test.rb
+++ test/components/layout/language_selection/component_versioning_test.rb
@@ -2,54 +2,56 @@
 
 require 'view_component_test_case'
 
-module Dropdown
-  class ComponentVersioningTest < ViewComponentTestCase
-    test 'renders v1 when feature flag is disabled' do
-      Flipper.disable(:v2_language_selection)
-      render_component
+module Layout
+  module LanguageSelection
+    class ComponentVersioningTest < ViewComponentTestCase
+      test 'renders v1 when feature flag is disabled' do
+        Flipper.disable(:v2_language_selection)
+        render_component
 
-      assert_selector '[data-controller="dropdown--v1"]'
-      assert_selector '[data-dropdown--v1-target="trigger"]'
-      assert_selector '[data-dropdown--v1-target="menu"]', visible: :hidden
-    end
-
-    test 'renders v2 when feature flag is enabled' do
-      Flipper.enable(:v2_language_selection)
-      render_component
-
-      assert_selector '[data-controller="dropdown--v2"]'
-      assert_selector '[data-dropdown--v2-target="trigger"]'
-      assert_selector '[data-dropdown--v2-target="menu"]', visible: :hidden
-    end
-
-    test 'renders v1 when version override is v1' do
-      render_component(version: :v1)
-
-      assert_selector '[data-controller="dropdown--v1"]'
-      assert_selector '[data-dropdown--v1-target="trigger"]'
-      assert_selector '[data-dropdown--v1-target="menu"]', visible: :hidden
-    end
-
-    test 'renders v2 when version override is v2' do
-      render_component(version: :v2)
-
-      assert_selector '[data-controller="dropdown--v2"]'
-      assert_selector '[data-dropdown--v2-target="trigger"]'
-      assert_selector '[data-dropdown--v2-target="menu"]', visible: :hidden
-    end
-
-    test 'raises when version override is invalid' do
-      assert_raises(ArgumentError) do
-        render_component(version: :v3)
+        assert_selector '[data-controller="dropdown--v1"]'
+        assert_selector '[data-dropdown--v1-target="trigger"]'
+        assert_selector '[data-dropdown--v1-target="menu"]', visible: :hidden
       end
-    end
 
-    private
+      test 'renders v2 when feature flag is enabled' do
+        Flipper.enable(:v2_language_selection)
+        render_component
 
-    def render_component(version: nil)
-      user = users(:john_doe)
+        assert_selector '[data-controller="dropdown--v2"]'
+        assert_selector '[data-dropdown--v2-target="trigger"]'
+        assert_selector '[data-dropdown--v2-target="menu"]', visible: :hidden
+      end
 
-      render_inline(Layout::LanguageSelectionComponent.new(version: version, user: user))
+      test 'renders v1 when version override is v1' do
+        render_component(version: :v1)
+
+        assert_selector '[data-controller="dropdown--v1"]'
+        assert_selector '[data-dropdown--v1-target="trigger"]'
+        assert_selector '[data-dropdown--v1-target="menu"]', visible: :hidden
+      end
+
+      test 'renders v2 when version override is v2' do
+        render_component(version: :v2)
+
+        assert_selector '[data-controller="dropdown--v2"]'
+        assert_selector '[data-dropdown--v2-target="trigger"]'
+        assert_selector '[data-dropdown--v2-target="menu"]', visible: :hidden
+      end
+
+      test 'raises when version override is invalid' do
+        assert_raises(ArgumentError) do
+          render_component(version: :v3)
+        end
+      end
+
+      private
+
+      def render_component(version: nil)
+        user = users(:john_doe)
+
+        render_inline(Layout::LanguageSelectionComponent.new(version: version, user: user))
+      end
     end
   end
 end
