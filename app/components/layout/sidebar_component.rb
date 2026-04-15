453ca6651e97ac3854065d5e405bbfc611ca3f28
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-04-15 13:44:01 -0500

    applying the dropdown component to the sidebar component

--- app/components/layout/sidebar_component.rb
+++ app/components/layout/sidebar_component.rb
@@ -4,12 +4,9 @@ module Layout
   # Stable entrypoint for rendering a language selection menu across UI versions.
   class SidebarComponent < Versioning::VersionedComponent
     IMPLEMENTATIONS = {
-      v1: Layout::Sidebar::V1::Component,
-      v2: Layout::Sidebar::V2::Component
+      v1: Layout::Sidebar::V1::Component
     }.freeze
 
-    VERSION_RESOLVER = lambda {
-      Flipper.enabled?(:v2_sidebar, Current.user) ? :v2 : :v1
-    }
+    VERSION_RESOLVER = -> { :v1 }
   end
 end
