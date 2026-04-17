16eedae4e34e5437db8f913a15a41e9088bf29a6
Sukhdeep Sidhu <ssidhu@phac5029663.cscscience.ca>
2026-04-17 08:55:59 -0500

    Added test for personal access token mailer

--- app/graphql/concerns/whitelist_ip_concern.rb
+++ app/graphql/concerns/whitelist_ip_concern.rb
@@ -4,14 +4,19 @@
 module WhitelistIpConcern # rubocop:disable GraphQL/ObjectDescription
   include ActiveSupport::Concern
 
+  # Adds the given IP address to the list of whitelisted IP addresses for the given personal access token,
+  # if it is not already present. If it is a new IP address, an email notification will be sent to the user
+  # associated with the personal access token.
   def whitelist_ip(personal_access_token, ip_address)
     return if personal_access_token.nil? || ip_address.nil?
 
-    if personal_access_token.ip_addresses.empty?
+    existing_ip_addresses = personal_access_token.ip_addresses || []
+
+    if existing_ip_addresses.empty?
       personal_access_token.update(ip_addresses: [ip_address])
     else
-      unless personal_access_token.ip_addresses.include?(ip_address)
-        personal_access_token.update(ip_addresses: personal_access_token.ip_addresses + [ip_address])
+      unless existing_ip_addresses.include?(ip_address)
+        personal_access_token.update(ip_addresses: existing_ip_addresses + [ip_address])
         # send email to user about new IP address being added to their token and that
         # they should either revoke or rotate the token if they do not recognize the IP address
         PersonalAccessTokenMailer.new_ip_address_for_token(personal_access_token, ip_address).deliver_later
