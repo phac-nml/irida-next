1a4101e127ba12552a52e396fc5b4ff2fa584c0c
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-04-20 14:41:35 -0500

    fixing a comment

--- app/components/layout/sidebar_component.rb
+++ app/components/layout/sidebar_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Layout
-  # Stable entrypoint for rendering a language selection menu across UI versions.
+  # Stable entrypoint for rendering a sidebar across UI versions.
   class SidebarComponent < Versioning::VersionedComponent
     IMPLEMENTATIONS = {
       v1: Layout::Sidebar::V1::Component
