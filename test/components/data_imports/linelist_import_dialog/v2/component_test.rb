412746ef9c9f808f9f02a9191652fc84fe6f8cdb
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-04-23 12:57:32 -0500

    moving form fields into a partial

--- test/components/data_imports/linelist_import_dialog/v2/component_test.rb
+++ test/components/data_imports/linelist_import_dialog/v2/component_test.rb
@@ -12,8 +12,7 @@ module DataImports
           render_inline(
             Component.new(
               broadcast_target: broadcast_target,
-              open: true,
-              closable: true
+              open: true
             )
           )
 
@@ -23,7 +22,7 @@ module DataImports
             assert_selector "input[type='file'][name='file_import[file]']"
             assert_selector "select[name='file_import[sample_id_column]']"
             assert_selector "input[type='checkbox'][name='file_import[ignore_empty_values]']"
-            assert_selector "input[type='submit'][disabled][value='#{I18n.t('shared.samples.metadata.file_imports.dialog.submit_button')}']" # rubocop:disable Layout/LineLength
+            assert_selector "input[type='submit'][disabled][value='#{I18n.t('shared.samples.metadata.file_imports.form_fields.submit_button')}']" # rubocop:disable Layout/LineLength
           end
         end
       end
