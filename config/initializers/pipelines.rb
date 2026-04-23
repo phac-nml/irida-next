5783ee6e61c5bdbcb048c40987645d0167019bc2
Eric Enns <492127+ericenns@users.noreply.github.com>
2026-04-23 07:06:36 -0500

    chore: add in option to skip pipeline initialization which is helpful for faster seeding of data

--- config/initializers/pipelines.rb
+++ config/initializers/pipelines.rb
@@ -3,17 +3,19 @@
 require 'irida/pipelines'
 
 Rails.application.config.to_prepare do
-  pipeline_config_file = if Rails.root.join("config/pipelines/#{Rails.env}.json").exist?
-                           "config/pipelines/#{Rails.env}.json"
-                         else
-                           'config/pipelines/pipelines.json'
-                         end
-  Irida::Pipelines.instance = Irida::Pipelines.new(pipeline_config_file:) if Irida::Pipelines.instance.nil?
+  unless ENV['SKIP_PIPELINE_INITIALIZATION']
+    pipeline_config_file = if Rails.root.join("config/pipelines/#{Rails.env}.json").exist?
+                             "config/pipelines/#{Rails.env}.json"
+                           else
+                             'config/pipelines/pipelines.json'
+                           end
+    Irida::Pipelines.instance = Irida::Pipelines.new(pipeline_config_file:) if Irida::Pipelines.instance.nil?
+  end
 end
 
 Rails.application.config.after_initialize do
-  if defined?(Rails::Server) ||
-     (Rails.env.test? && ActiveRecord::Base.connection.table_exists?('automated_workflow_executions'))
+  if !ENV.fetch('SKIP_PIPELINE_INITIALIZATION', nil) && (defined?(Rails::Server) ||
+       (Rails.env.test? && ActiveRecord::Base.connection.table_exists?('automated_workflow_executions')))
 
     AutomatedWorkflowExecution.find_each do |automated_workflow_execution|
       pipeline = automated_workflow_execution.workflow
