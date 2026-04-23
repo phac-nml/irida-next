1e28ba30045e7db8051667728280c3857d81ceb7
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-04-23 10:10:12 -0500

    adding component tests

---
+++ test/components/data_imports/linelist_import_dialog/v1/component_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'view_component_test_case'
+
+module DataImports
+  module LinelistImportDialog
+    module V1
+      class ComponentTest < ViewComponentTestCase
+        test 'default' do
+          broadcast_target = "metadata_import_#{SecureRandom.uuid}"
+          group = groups(:group_one)
+          url = Rails.application.routes.url_helpers.group_samples_file_import_path(group_id: group.id)
+
+          render_inline(
+            Component.new(
+              broadcast_target: broadcast_target,
+              open: true,
+              url: url,
+              closable: true
+            )
+          )
+
+          assert_selector 'h1', text: I18n.t('shared.samples.metadata.file_imports.dialog.title')
+          assert_selector "form[action='#{url}'][method='post']" do
+            assert_selector "input[type='hidden'][name='broadcast_target'][value='#{broadcast_target}']", visible: false
+            assert_selector "input[type='file'][name='file_import[file]']"
+            assert_selector "select[name='file_import[sample_id_column]']"
+            assert_selector "input[type='checkbox'][name='file_import[ignore_empty_values]']"
+            assert_selector "input[type='submit'][disabled][value='#{I18n.t('shared.samples.metadata.file_imports.dialog.submit_button')}']" # rubocop:disable Layout/LineLength
+          end
+        end
+      end
+    end
+  end
+end
