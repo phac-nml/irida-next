6a93562b784429f89c6e70a56cb700824cb69d6a
Chris Huynh <chris.huynh333@gmail.com>
2026-05-01 15:43:04 -0500

    add previews and component tests

---
+++ test/components/combobox_datepicker/v1/component_test.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module ComboboxDatepicker
+  module V1
+    class ComponentTest < ViewComponent::TestCase
+      test 'basic datepicker' do
+        datepicker = ComboboxDatepicker::V1::Component.new(id: 'test_id', input_name: 'test_input_name')
+
+        render_inline(datepicker)
+        assert_selector 'div#test_id-datepicker.relative'
+        assert_no_selector 'label'
+        assert_selector 'input[type="text"]', count: 1
+        assert_selector 'svg.calendar-dots-icon', count: 1
+        assert_selector 'svg.caret-down-icon', count: 1
+        assert_match(/id="test_id-calendar"/, rendered_content)
+        assert_selector('template[data-combobox-datepicker--v1--input-target="calendarTemplate"]', visible: :all)
+        assert_match(/data-controller="combobox-datepicker--v1--input"/, rendered_content)
+        assert_match(/data-controller="combobox-datepicker--v1--calendar"/, rendered_content)
+      end
+
+      test 'datepicker with label' do
+        datepicker = ComboboxDatepicker::V1::Component.new(
+          id: 'test_id',
+          input_name: 'test_input_name',
+          label: 'this is a label'
+        )
+        render_inline(datepicker)
+        assert_selector 'label', text: 'this is a label'
+        assert_selector 'input[type="text"]', count: 1
+        assert_selector 'svg.calendar-dots-icon', count: 1
+        assert_selector 'svg.caret-down-icon', count: 1
+      end
+
+      test 'datepicker with required' do
+        datepicker = ComboboxDatepicker::V1::Component.new(
+          id: 'test_id',
+          input_name: 'test_input_name',
+          label: 'this is a label',
+          required: true
+        )
+        render_inline(datepicker)
+        assert_selector 'abbr', text: '*'
+      end
+    end
+  end
+end
