427aa4fb2418e406c0b39ea9d56c6db160d1d79e
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-05-06 11:55:35 -0500

    create a web worker and call the graphql BulkUpdateSampleMetadata mutation

--- app/components/data_imports/linelist_import_dialog/v2/component.rb
+++ app/components/data_imports/linelist_import_dialog/v2/component.rb
@@ -5,8 +5,14 @@ module DataImports
     module V2
       # Client-side linelist import dialog implementation.
       class Component < ::Component
-        def initialize(broadcast_target:, **_system_arguments)
+        def initialize(broadcast_target:, group: nil, project: nil, **_system_arguments)
           @broadcast_target = broadcast_target
+          @group = group
+          @project = project
+        end
+
+        def graphql_url
+          helpers.api_graphql_path
         end
       end
     end
