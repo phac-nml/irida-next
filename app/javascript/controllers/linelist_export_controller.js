99682382df050352e683bd76042f3777e7ef7e54
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-05-08 13:48:30 -0500

    playing with shims to load & reuse chunk within worker

--- app/javascript/controllers/linelist_export_controller.js
+++ app/javascript/controllers/linelist_export_controller.js
@@ -127,6 +127,10 @@ export default class extends Controller {
         this.t(this.preparingRowsMessageValue, { count: totalCount }),
       );
 
+      const importMap = document.querySelector(
+        'script[type="importmap"]',
+      ).textContent;
+
       this.workerClient.start({
         sample_ids: sampleIds,
         metadata_fields: metadataFields,
@@ -137,6 +141,7 @@ export default class extends Controller {
         format,
         filename,
         total_count: totalCount,
+        map: importMap,
       });
     } catch (error) {
       this.updateProgress(
