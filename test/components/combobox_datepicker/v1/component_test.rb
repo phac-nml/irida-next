d95cd7d8fffebf217dc05f8a7f2a433ca833c75c
Chris Huynh <chris.huynh333@gmail.com>
2026-05-08 10:49:38 -0500

    start component tests

--- test/components/combobox_datepicker/v1/component_test.rb
+++ test/components/combobox_datepicker/v1/component_test.rb
@@ -1,47 +1,277 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require 'application_system_test_case'
 
 module ComboboxDatepicker
   module V1
-    class ComponentTest < ViewComponent::TestCase
-      test 'basic datepicker' do
-        datepicker = ComboboxDatepicker::V1::Component.new(id: 'test_id', input_name: 'test_input_name')
+    class ComponentTest < ApplicationSystemTestCase
+      def test_default_datepicker # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+        test_date = DateTime.new(2026, 5, 7, 0, 0, 0, '-06:00')
+        Timecop.travel(test_date) do
+          visit('/rails/view_components/combobox_datepicker_component/default')
 
-        render_inline(datepicker)
-        assert_selector 'div#test_id-datepicker.relative'
-        assert_no_selector 'label'
-        assert_selector 'input[type="text"]', count: 1
-        assert_selector 'svg.calendar-dots-icon', count: 1
-        assert_selector 'svg.caret-down-icon', count: 1
-        assert_match(/id="test_id-calendar"/, rendered_content)
-        assert_selector('template[data-combobox-datepicker--v1--input-target="calendarTemplate"]', visible: :all)
-        assert_match(/data-controller="combobox-datepicker--v1--input"/, rendered_content)
-        assert_match(/data-controller="combobox-datepicker--v1--calendar"/, rendered_content)
+          # open by clicking arrow so date node is focused
+          find('div[data-combobox-datepicker--v1--input-target="inputArrow"]').click
+
+          # verify May is selected and only 8 months exist (Jan-Apr options are not appended to to minDate)
+          assert_selector 'select option', count: 8
+          assert_selector 'select', text: 'May'
+
+          # all grid rows and cells are 'filled out'
+          assert_selector 'table tbody tr', count: 6
+          assert_selector 'table tbody tr td', count: 42
+          # Apr 26-30 are filled out and disabled in first week
+          (26..30).each do |i|
+            assert_selector 'table tbody tr:first-child td[aria-disabled="true"][data-date-within-month-position="outOfMonth"]', # rubocop:disable Layout/LineLength
+                            text: i.to_s
+          end
+          # May 1-2 are disabled first week
+          (1..2).each do |i|
+            assert_selector 'table tbody tr:first-child td[aria-disabled="true"][data-date-within-month-position="inMonth"]', # rubocop:disable Layout/LineLength
+                            text: i.to_s
+          end
+          # May 3-7 are disabled in second week
+          (3..7).each do |i|
+            assert_selector 'table tbody tr:nth-child(2) td[aria-disabled="true"][data-date-within-month-position="inMonth"]', # rubocop:disable Layout/LineLength
+                            text: i.to_s
+          end
+          # 8-9 to 30th are selectable dates and are 'inMonth'
+          (8..9).each do |i|
+            assert_selector 'table tbody tr:nth-child(2) td[data-date-within-month-position="inMonth"]', text: i.to_s
+          end
+
+          (10..16).each do |i|
+            assert_selector 'table tbody tr:nth-child(3) td[data-date-within-month-position="inMonth"]', text: i.to_s
+          end
+          (17..23).each do |i|
+            assert_selector 'table tbody tr:nth-child(4) td[data-date-within-month-position="inMonth"]',
+                            text: i.to_s
+          end
+
+          (24..30).each do |i|
+            assert_selector 'table tbody tr:nth-child(5) td[data-date-within-month-position="inMonth"]', text: i.to_s
+          end
+
+          assert_selector 'table tbody tr:last-child td[data-date-within-month-position="inMonth"]', text: '31'
+
+          # June 1-6 fill out last week, and are 'outOfMonth'
+          (1..6).each do |i|
+            assert_selector 'table tbody tr:last-child td[data-date-within-month-position="outOfMonth"]', text: i.to_s
+          end
+
+          # May 8th is focused upon opening
+          assert_selector 'td', text: '8', focused: true
+        end
       end
 
-      test 'datepicker with label' do
-        datepicker = ComboboxDatepicker::V1::Component.new(
-          id: 'test_id',
-          input_name: 'test_input_name',
-          label: 'this is a label'
-        )
-        render_inline(datepicker)
-        assert_selector 'label', text: 'this is a label'
-        assert_selector 'input[type="text"]', count: 1
-        assert_selector 'svg.calendar-dots-icon', count: 1
-        assert_selector 'svg.caret-down-icon', count: 1
+      def test_open_datepicker_by_clicking_input
+        test_date = DateTime.new(2026, 5, 6, 0, 0, 0, '-06:00')
+        Timecop.travel(test_date) do
+          visit('/rails/view_components/combobox_datepicker_component/default')
+
+          # by clicking input, focus does not transfer into datepicker and stays on input
+          find('#test_id-input').click
+
+          assert_no_selector 'td[data-date="2026-05-08"]', focused: true
+          assert_selector 'input#test_id-input', focused: true
+
+          assert_text 'ngpaewngpegaw'
+        end
       end
 
-      test 'datepicker with required' do
-        datepicker = ComboboxDatepicker::V1::Component.new(
-          id: 'test_id',
-          input_name: 'test_input_name',
-          label: 'this is a label',
-          required: true
-        )
-        render_inline(datepicker)
-        assert_selector 'abbr', text: '*'
+      def test_space_and_enter_on_datepicker
+        test_date = DateTime.new(2026, 5, 7, 0, 0, 0, '-06:00')
+        Timecop.travel(test_date) do
+          visit('/rails/view_components/combobox_datepicker_component/default')
+
+          # open by clicking arrow so date node is focused
+          find('div[data-combobox-datepicker--v1--input-target="inputArrow"]').click
+
+          # May 8th is focused upon opening
+          assert_selector 'td', text: '8', focused: true
+
+          # space to select 2026-05-08
+          find('#test_id-calendar').send_keys(:space)
+
+          # verify calendar still open and 2026-05-08 selected
+          assert_selector '#test_id-calendar'
+          assert_selector 'td[data-date="2026-05-08"][aria-selected="true"]'
+
+          # go to 2026-05-09 and select via enter
+          find('#test_id-calendar').send_keys(:right, :enter)
+
+          # calendar closes
+          assert_no_selector '#test_id-calendar'
+          # reopen calendar and verify 8th is no longer selected and 9th is selected
+          find('div[data-combobox-datepicker--v1--input-target="inputArrow"]').click
+          assert_no_selector 'td[data-date="2026-05-08"][aria-selected="true"]'
+          # May 8th is focused upon opening
+          assert_selector 'td', text: '9', focused: true
+          assert_selector 'td[data-date="2026-05-09"][aria-selected="true"]'
+        end
+      end
+
+      def test_home_and_end # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+        test_date = DateTime.new(2026, 5, 7, 0, 0, 0, '-06:00')
+        Timecop.travel(test_date) do
+          visit('/rails/view_components/combobox_datepicker_component/default')
+
+          # open by clicking arrow so date node is focused
+          find('div[data-combobox-datepicker--v1--input-target="inputArrow"]').click
+
+          # May 8th is focused upon opening
+          assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
+
+          # Focus May 9th (Sat)
+          find('#test_id-calendar').send_keys(:end)
+          assert_selector 'td[data-date="2026-05-09"]', focused: true
+
+          # Focus May 8th (Fri) as 7th and earlier is disabled
+          find('#test_id-calendar').send_keys(:home)
+          assert_selector 'td[data-date="2026-05-08"]', focused: true
+
+          # Go down to next week where full week is selectable/focusable
+          find('#test_id-calendar').send_keys(:down)
+          assert_selector 'td[data-date="2026-05-15"]', focused: true
+
+          # Focus May 16th (Sat)
+          find('#test_id-calendar').send_keys(:end)
+          assert_selector 'td[data-date="2026-05-16"]', focused: true
+
+          # Focus May 10th (Sun)
+          find('#test_id-calendar').send_keys(:home)
+          assert_selector 'td[data-date="2026-05-10"]', focused: true
+        end
+      end
+
+      def test_arrow_keys # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+        test_date = DateTime.new(2026, 5, 7, 0, 0, 0, '-06:00')
+        Timecop.travel(test_date) do
+          visit('/rails/view_components/combobox_datepicker_component/default')
+
+          # open by clicking arrow so date node is focused
+          find('div[data-combobox-datepicker--v1--input-target="inputArrow"]').click
+
+          # May 8th is focused upon opening
+          assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
+
+          # Can't go up as 1st is disabled
+          find('#test_id-calendar').send_keys(:up)
+          assert_selector 'td[data-date="2026-05-08"]', focused: true
+
+          # Can't go left as 7th is disabled
+          find('#test_id-calendar').send_keys(:left)
+          assert_selector 'td[data-date="2026-05-08"]', focused: true
+
+          find('#test_id-calendar').send_keys(:right)
+          assert_selector 'td[data-date="2026-05-09"]', focused: true
+
+          find('#test_id-calendar').send_keys(:down)
+          assert_selector 'td[data-date="2026-05-06"]', focused: true
+
+          find('#test_id-calendar').send_keys(:left)
+          assert_selector 'td[data-date="2026-05-15"]', focused: true
+
+          find('#test_id-calendar').send_keys(:up)
+          assert_selector 'td[data-date="2026-05-08"]', focused: true
+        end
+      end
+
+      def test_page_up_and_down # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+        test_date = DateTime.new(2026, 5, 7, 0, 0, 0, '-06:00')
+        Timecop.travel(test_date) do
+          visit('/rails/view_components/combobox_datepicker_component/default')
+
+          # open by clicking arrow so date node is focused
+          find('div[data-combobox-datepicker--v1--input-target="inputArrow"]').click
+
+          # May 8th is focused upon opening
+          assert_selector 'td[data-date="2026-05-08"]', focused: true
+
+          # June 8th focused after page_down
+          find('#test_id-calendar').send_keys(:page_down)
+          assert_selector 'td[data-date="2026-06-08"]', focused: true
+
+          # May 8th focused after page_up
+          find('#test_id-calendar').send_keys(:page_up)
+          assert_selector 'td[data-date="2026-05-08"]', focused: true
+
+          # navigate to May 31st
+          find('#test_id-calendar').send_keys(:page_down, :up, :left)
+          assert_selector 'td[data-date="2026-05-31"]', focused: true
+
+          # page_down into June changes focus to June 30th (rather than 31st which doesn't exist)
+          find('#test_id-calendar').send_keys(:page_down)
+          assert_selector 'td[data-date="2026-06-30"]', focused: true
+
+          # navigate to July 31st
+          find('#test_id-calendar').send_keys(:page_down, :right)
+          assert_selector 'td[data-date="2026-07-31"]', focused: true
+
+          # page_up focuses June 30th (rather than 31st again)
+          find('#test_id-calendar').send_keys(:page_up)
+          assert_selector 'td[data-date="2026-06-30"]', focused: true
+        end
+      end
+
+      def test_shift_page_up_and_down
+        test_date = DateTime.new(2026, 5, 7, 0, 0, 0, '-06:00')
+        Timecop.travel(test_date) do
+          visit('/rails/view_components/combobox_datepicker_component/default')
+
+          # open by clicking arrow so date node is focused
+          find('div[data-combobox-datepicker--v1--input-target="inputArrow"]').click
+
+          # May 8th, 2026 is focused upon opening
+          assert_selector 'td[data-date="2026-05-08"]', focused: true
+
+          # May 8th, 2027 focused after shift+page_down
+          find('#test_id-calendar').send_keys(%i[shift page_down])
+          assert_selector 'td[data-date="2027-05-08"]', focused: true
+
+          # May 9th, 2026 focused after navigating to May 9th, 2027 and shift+page_up
+          find('#test_id-calendar').send_keys(:right, %i[shift page_up])
+          assert_selector 'td[data-date="2026-05-09"]', focused: true
+
+          # May 1th, 2027 focused after shift+page_down
+          find('#test_id-calendar').send_keys(%i[shift page_down], :up)
+          assert_selector 'td[data-date="2027-05-02"]', focused: true
+
+          # May 8th, 2026 focused after navigating to May 2th, 2027 and shift+page_up, adjusted to min_date as
+          # May 2nd, 2026 is disabled
+          find('#test_id-calendar').send_keys(%i[shift page_up])
+          assert_selector 'td[data-date="2026-05-08"]', focused: true
+        end
+      end
+
+      def test_page_up_and_down_on_feb_leap_year
+        test_date = DateTime.new(2028, 2, 28, 0, 0, 0, '-06:00')
+        Timecop.travel(test_date) do
+          # visit('/rails/view_components/combobox_datepicker_component/default')
+
+          # # open by clicking arrow so date node is focused
+          # find('div[data-combobox-datepicker--v1--input-target="inputArrow"]').click
+
+          # # Feb 29 is focused upon opening
+          # assert_selector 'td[data-date="2028-02-29"]', focused: true
+
+          # # May 8th, 2027 focused after shift+page_down
+          # find('#test_id-calendar').send_keys(%i[shift page_down])
+          # assert_selector 'td[data-date="2027-05-08"]', focused: true
+
+          # # May 9th, 2026 focused after navigating to May 9th, 2027 and shift+page_up
+          # find('#test_id-calendar').send_keys(:right, %i[shift page_up])
+          # assert_selector 'td[data-date="2026-05-09"]', focused: true
+
+          # # May 1th, 2027 focused after shift+page_down
+          # find('#test_id-calendar').send_keys(%i[shift page_down], :up)
+          # assert_selector 'td[data-date="2027-05-02"]', focused: true
+
+          # # May 8th, 2026 focused after navigating to May 2th, 2027 and shift+page_up, adjusted to min_date as
+          # # May 2nd, 2026 is disabled
+          # find('#test_id-calendar').send_keys(%i[shift page_up])
+          # assert_selector 'td[data-date="2026-05-08"]', focused: true
+        end
       end
     end
   end
