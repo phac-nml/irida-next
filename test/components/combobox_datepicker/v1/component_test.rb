3d22e6345dd943c2946c32d087d4c6ce1b0e1711
Chris Huynh <chris.huynh333@gmail.com>
2026-05-14 10:34:49 -0500

    try and fix flakes

--- test/components/combobox_datepicker/v1/component_test.rb
+++ test/components/combobox_datepicker/v1/component_test.rb
@@ -144,23 +144,23 @@ module ComboboxDatepicker
 
             # Focus May 9th (Sat)
             find('#test_id-calendar').send_keys(:end)
-            assert_selector 'td[data-date="2026-05-09"]', focused: true
+            assert_selector 'td[data-date="2026-05-09"]', text: '9', focused: true
 
             # Focus May 8th (Fri) as 7th and earlier is disabled
             find('#test_id-calendar').send_keys(:home)
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
 
             # Go down to next week where full week is selectable/focusable
             find('#test_id-calendar').send_keys(:down)
-            assert_selector 'td[data-date="2026-05-15"]', focused: true
+            assert_selector 'td[data-date="2026-05-15"]', text: '15', focused: true
 
             # Focus May 16th (Sat)
             find('#test_id-calendar').send_keys(:end)
-            assert_selector 'td[data-date="2026-05-16"]', focused: true
+            assert_selector 'td[data-date="2026-05-16"]', text: '16', focused: true
 
             # Focus May 10th (Sun)
             find('#test_id-calendar').send_keys(:home)
-            assert_selector 'td[data-date="2026-05-10"]', focused: true
+            assert_selector 'td[data-date="2026-05-10"]', text: '10', focused: true
           end
         end
       end
@@ -182,15 +182,15 @@ module ComboboxDatepicker
 
             # Navigate to May 6, 2027 (max date)
             find('#test_id-calendar').send_keys(%i[shift page_down])
-            assert_selector 'td[data-date="2027-05-06"]', focused: true
+            assert_selector 'td[data-date="2027-05-06"]', text: '6', focused: true
 
             # Focus May 2 (Sun)
             find('#test_id-calendar').send_keys(:home)
-            assert_selector 'td[data-date="2027-05-02"]', focused: true
+            assert_selector 'td[data-date="2027-05-02"]', text: '2', focused: true
 
             # Focus May 6 (Thu)
             find('#test_id-calendar').send_keys(:end)
-            assert_selector 'td[data-date="2027-05-06"]', focused: true
+            assert_selector 'td[data-date="2027-05-06"]', text: '6', focused: true
           end
         end
       end
@@ -212,23 +212,23 @@ module ComboboxDatepicker
 
             # Can't go up as 1st is disabled
             find('#test_id-calendar').send_keys(:up)
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
 
             # Can't go left as 7th is disabled
             find('#test_id-calendar').send_keys(:left)
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
 
             find('#test_id-calendar').send_keys(:right)
-            assert_selector 'td[data-date="2026-05-09"]', focused: true
+            assert_selector 'td[data-date="2026-05-09"]', text: '9', focused: true
 
             find('#test_id-calendar').send_keys(:down)
-            assert_selector 'td[data-date="2026-05-16"]', focused: true
+            assert_selector 'td[data-date="2026-05-16"]', text: '16', focused: true
 
             find('#test_id-calendar').send_keys(:left)
-            assert_selector 'td[data-date="2026-05-15"]', focused: true
+            assert_selector 'td[data-date="2026-05-15"]', text: '15', focused: true
 
             find('#test_id-calendar').send_keys(:up)
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
           end
         end
       end
@@ -250,15 +250,15 @@ module ComboboxDatepicker
 
             # Navigate to max date
             find('#test_id-calendar').send_keys(%i[shift page_down])
-            assert_selector 'td[data-date="2027-05-06"]', focused: true
+            assert_selector 'td[data-date="2027-05-06"]', text: '6', focused: true
 
             # Can't go right as 7th is disabled
             find('#test_id-calendar').send_keys(:right)
-            assert_selector 'td[data-date="2027-05-06"]', focused: true
+            assert_selector 'td[data-date="2027-05-06"]', text: '6', focused: true
 
             # Can't go right as 13th is disabled
             find('#test_id-calendar').send_keys(:down)
-            assert_selector 'td[data-date="2027-05-06"]', focused: true
+            assert_selector 'td[data-date="2027-05-06"]', text: '6', focused: true
           end
         end
       end
@@ -276,41 +276,41 @@ module ComboboxDatepicker
             # May 8th is focused upon opening
             assert_field 'month-select', with: I18n.t('components.datepicker.months.may')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
 
             # June 8th focused after page_down
             find('#test_id-calendar').send_keys(:page_down)
             assert_field 'month-select', with: I18n.t('components.datepicker.months.june')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-06-08"]', focused: true
+            assert_selector 'td[data-date="2026-06-08"]', text: '8', focused: true
 
             # May 8th focused after page_up
             find('#test_id-calendar').send_keys(:page_up)
             assert_field 'month-select', with: I18n.t('components.datepicker.months.may')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
 
             # navigate to May 31st
             find('#test_id-calendar').send_keys(:page_down, :up, :left)
-            assert_selector 'td[data-date="2026-05-31"]', focused: true
+            assert_selector 'td[data-date="2026-05-31"]', text: '31', focused: true
 
             # page_down into June changes focus to June 30th (rather than 31st which doesn't exist)
             find('#test_id-calendar').send_keys(:page_down)
             assert_field 'month-select', with: I18n.t('components.datepicker.months.june')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-06-30"]', focused: true
+            assert_selector 'td[data-date="2026-06-30"]', text: '30', focused: true
 
             # navigate to July 31st
             find('#test_id-calendar').send_keys(:page_down, :right)
             assert_field 'month-select', with: I18n.t('components.datepicker.months.july')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-07-31"]', focused: true
+            assert_selector 'td[data-date="2026-07-31"]', text: '31', focused: true
 
             # page_up focuses June 30th (rather than 31st again)
             find('#test_id-calendar').send_keys(:page_up)
             assert_field 'month-select', with: I18n.t('components.datepicker.months.june')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-06-30"]', focused: true
+            assert_selector 'td[data-date="2026-06-30"]', text: '30', focused: true
           end
         end
       end
@@ -327,32 +327,32 @@ module ComboboxDatepicker
             # May 8th, 2026 is focused upon opening
             assert_field 'month-select', with: I18n.t('components.datepicker.months.may')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
 
             # May 8th, 2027 focused after shift+page_down
             find('#test_id-calendar').send_keys(%i[shift page_down])
             assert_field 'month-select', with: I18n.t('components.datepicker.months.may')
             assert_field 'year-select', with: '2027'
-            assert_selector 'td[data-date="2027-05-08"]', focused: true
+            assert_selector 'td[data-date="2027-05-08"]', text: '8', focused: true
 
             # May 9th, 2026 focused after navigating to May 9th, 2027 and shift+page_up
             find('#test_id-calendar').send_keys(:right, %i[shift page_up])
             assert_field 'month-select', with: I18n.t('components.datepicker.months.may')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-05-09"]', focused: true
+            assert_selector 'td[data-date="2026-05-09"]', text: '9', focused: true
 
             # May 1th, 2027 focused after shift+page_down
             find('#test_id-calendar').send_keys(%i[shift page_down], :up)
             assert_field 'month-select', with: I18n.t('components.datepicker.months.may')
             assert_field 'year-select', with: '2027'
-            assert_selector 'td[data-date="2027-05-02"]', focused: true
+            assert_selector 'td[data-date="2027-05-02"]', text: '2', focused: true
 
             # May 8th, 2026 focused after navigating to May 2th, 2027 and shift+page_up, adjusted to min_date as
             # May 2nd, 2026 is disabled
             find('#test_id-calendar').send_keys(%i[shift page_up])
             assert_field 'month-select', with: I18n.t('components.datepicker.months.may')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
           end
         end
       end
@@ -370,31 +370,31 @@ module ComboboxDatepicker
             # Start on Jan 29, 2027 is focused upon opening
             assert_field 'month-select', with: I18n.t('components.datepicker.months.january')
             assert_field 'year-select', with: '2027'
-            assert_selector 'td[data-date="2027-01-29"]', focused: true
+            assert_selector 'td[data-date="2027-01-29"]', text: '29', focused: true
 
             # Navigate to Feb 29, 2028
             find('#test_id-calendar').send_keys(%i[shift page_down], :page_down)
             assert_field 'month-select', with: I18n.t('components.datepicker.months.february')
             assert_field 'year-select', with: '2028'
-            assert_selector 'td[data-date="2028-02-29"]', focused: true
+            assert_selector 'td[data-date="2028-02-29"]', text: '29', focused: true
 
             # shift+page_up to verify we land on Feb 28, 2027 (rather than non-existent Feb 29)
             find('#test_id-calendar').send_keys(%i[shift page_up])
             assert_field 'month-select', with: I18n.t('components.datepicker.months.february')
             assert_field 'year-select', with: '2027'
-            assert_selector 'td[data-date="2027-02-28"]', focused: true
+            assert_selector 'td[data-date="2027-02-28"]', text: '28', focused: true
 
             # Navigate back to Feb 29, 2028
             find('#test_id-calendar').send_keys(%i[shift page_down], :right)
             assert_field 'month-select', with: I18n.t('components.datepicker.months.february')
             assert_field 'year-select', with: '2028'
-            assert_selector 'td[data-date="2028-02-29"]', focused: true
+            assert_selector 'td[data-date="2028-02-29"]', text: '29', focused: true
 
             # shift+page_up to verify we land on Feb 28, 2029 (rather than non-existent Feb 29)
             find('#test_id-calendar').send_keys(%i[shift page_down])
             assert_field 'month-select', with: I18n.t('components.datepicker.months.february')
             assert_field 'year-select', with: '2029'
-            assert_selector 'td[data-date="2029-02-28"]', focused: true
+            assert_selector 'td[data-date="2029-02-28"]', text: '28', focused: true
           end
         end
       end
@@ -412,19 +412,19 @@ module ComboboxDatepicker
             # Start on May 8, 2026
             assert_field 'month-select', with: I18n.t('components.datepicker.months.may')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
 
             # Navigate to June 1, 2026
             find('#test_id-calendar').send_keys(:page_down, :up)
             assert_field 'month-select', with: I18n.t('components.datepicker.months.june')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-06-01"]', focused: true
+            assert_selector 'td[data-date="2026-06-01"]', text: '1', focused: true
 
             # page_up and end up back on May 8, 2026 as May 1, 2026 is disabled
             find('#test_id-calendar').send_keys(:page_up)
             assert_field 'month-select', with: I18n.t('components.datepicker.months.may')
             assert_field 'year-select', with: '2026'
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
           end
         end
       end
@@ -440,22 +440,22 @@ module ComboboxDatepicker
             find('button[data-combobox-datepicker--v1--input-target="inputArrow"]').click
 
             # Start on May 8, 2026
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
             assert_no_selector 'button.next-btn[aria-disabled="true"]'
 
             # Navigate to May 6, 2027 (May 8 is disabled)
             find('#test_id-calendar').send_keys(%i[shift page_down])
-            assert_selector 'td[data-date="2027-05-06"]', focused: true
+            assert_selector 'td[data-date="2027-05-06"]', text: '6', focused: true
             assert_selector 'button.next-btn[aria-disabled="true"]'
 
             # Navigate back to April 13, 2027
             find('#test_id-calendar').send_keys(:page_up, :down)
-            assert_selector 'td[data-date="2027-04-13"]', focused: true
+            assert_selector 'td[data-date="2027-04-13"]', text: '13', focused: true
             assert_no_selector 'button.next-btn[aria-disabled="true"]'
 
             # page_down into disabled May 13th, focused forced to maxDate of May 6th
             find('#test_id-calendar').send_keys(:page_down)
-            assert_selector 'td[data-date="2027-05-06"]', focused: true
+            assert_selector 'td[data-date="2027-05-06"]', text: '6', focused: true
             assert_selector 'button.next-btn[aria-disabled="true"]'
           end
         end
@@ -472,15 +472,15 @@ module ComboboxDatepicker
             find('button[data-combobox-datepicker--v1--input-target="inputArrow"]').click
 
             # Start on May 8, 2026
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
 
             # Navigate to May 1, 2027
             find('#test_id-calendar').send_keys(%i[shift page_down], :up)
-            assert_selector 'td[data-date="2027-05-01"]', focused: true
+            assert_selector 'td[data-date="2027-05-01"]', text: '1', focused: true
 
             # shift+page_up and end up back on May 8, 2026 as May 1, 2026 is disabled
             find('#test_id-calendar').send_keys(%i[shift page_up])
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
           end
         end
       end
@@ -499,7 +499,7 @@ module ComboboxDatepicker
             # open by clicking arrow so date node is focused
             find('body').send_keys(:tab, :down)
             assert_selector '#test_id-calendar'
-            assert_selector 'td[data-date="2026-05-08"]', focused: true
+            assert_selector 'td[data-date="2026-05-08"]', text: '8', focused: true
             assert_selector 'svg.caret-down-icon.rotate-180'
 
             find('#test_id-calendar').send_keys(:escape)
