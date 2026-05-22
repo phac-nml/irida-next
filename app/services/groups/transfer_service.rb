8e25d4343ff3354238b3c8e47c22bb0f2df22a4b
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-05-22 11:01:19 -0500

    removing predicate from transfer service method

--- app/services/groups/transfer_service.rb
+++ app/services/groups/transfer_service.rb
@@ -8,7 +8,7 @@ module Groups
       @transfer_form = transfer_form
     end
 
-    def execute # rubocop:disable Naming/PredicateMethod, Metrics/AbcSize
+    def execute # rubocop:disable Metrics/AbcSize
       return false unless @transfer_form.valid?
 
       new_namespace ||= @transfer_form.new_parent
@@ -34,8 +34,6 @@ module Groups
       update_samples_count(old_namespace, new_namespace)
 
       new_namespace.update_metadata_summary_by_namespace_transfer(@group, old_namespace)
-
-      true
     end
 
     private
