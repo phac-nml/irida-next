6046ab59c0e614cccb4e4e8b7d37b563fbdea604
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-05-27 10:49:16 -0500

    refactor(token_component): enhance accessibility and functionality of mask button
    - Removed unused translation keys for mask button labels in the token component.
    - Updated the aria-pressed attribute for the mask button to reflect its visibility state.
    - Improved focus styles for better accessibility.
    - Added tests to verify the correct behavior of the mask button's aria-pressed state.

--- test/components/viral/system/token_component_test.rb
+++ test/components/viral/system/token_component_test.rb
@@ -7,6 +7,14 @@ module System
     test 'token component' do
       visit('/rails/view_components/token_component/default')
       within('.Viral-Preview > [data-controller-connected="true"]') do
+        assert_selector %(button[data-token-target="maskButton"][aria-pressed="false"])
+
+        find(%(button[data-token-target="maskButton"])).click
+        assert_selector %(button[data-token-target="maskButton"][aria-pressed="true"])
+
+        find(%(button[data-token-target="maskButton"])).click
+        assert_selector %(button[data-token-target="maskButton"][aria-pressed="false"])
+
         click_button 'Copy to clipboard'
         assert_no_text 'Copy to clipboard'
         assert_text 'Copied!'
