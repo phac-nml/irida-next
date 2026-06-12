c0b8116ac00db9efc42fc1d3a94ce934254a961a
Sukhdeep Sidhu <ssidhu@phac5029663.cscscience.ca>
2026-06-12 10:48:47 -0500

    Added UI to change visibility of parent group

--- app/services/groups/update_service.rb
+++ app/services/groups/update_service.rb
@@ -9,21 +9,24 @@ module Groups
       super
     end
 
-    def execute # rubocop:disable Metrics/AbcSize
-      authorize! @group, to: :update?
+    def execute # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+      authorize! @group, to: :update? unless params.key?(:public)
+      authorize! @group, to: :change_visibility? if (params.to_unsafe_h.size == 1) && params.key?(:public)
+
       updated = group.update(params)
 
       if updated
         if group.parent.nil?
-          if params.key?(:public) && params[:public] == true
+          if params.key?(:public) && params[:public] == 'true'
             update_descendants_to_public
-          elsif params.key?(:public) && params[:public] == false
+          elsif params.key?(:public) && params[:public] == 'false'
             update_descendants_to_private
           end
         end
 
         @group.create_activity key: 'group.update',
                                owner: current_user
+
       end
 
       updated
