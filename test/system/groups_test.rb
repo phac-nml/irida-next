d2ecea6e2972a5feb3c9dcc5d7b2ede24fa522ca
Sukhdeep Sidhu <ssidhu@phac5029663.cscscience.ca>
2026-06-19 09:05:53 -0500

    Updated test to set flipper flag and disable on teardown

--- test/system/groups_test.rb
+++ test/system/groups_test.rb
@@ -16,6 +16,11 @@ class GroupsTest < ApplicationSystemTestCase
     @project30 = projects(:project30)
     @sample34 = samples(:sample34)
     login_as @user
+    Flipper.enable(:global_groups)
+  end
+
+  def teardown
+    Flipper.disable(:global_groups)
   end
 
   test 'can create a group' do
