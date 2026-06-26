b575f3dfd043a991be66a9d05f74be217d096c9f
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2026-06-26 16:11:23 -0500

    simplify test structure

--- test/services/samples/transfer_service_test.rb
+++ test/services/samples/transfer_service_test.rb
@@ -770,5 +770,30 @@ module Samples
       missing_sample1.destroy
       missing_sample2.destroy
     end
+
+    test 'authorized results in a broadcasted success message and log data with correct responsible id' do
+      broadcast_target = SecureRandom.uuid
+      sample_ids = [@sample1.id, @sample2.id]
+
+      assert_difference -> { @new_project.reload.samples.count } => 2 do
+        Samples::TransferJob.perform_now(@current_project.namespace, @john_doe, @new_project.id, sample_ids,
+                                         broadcast_target)
+      end
+
+      turbo_streams = capture_turbo_stream_broadcasts broadcast_target
+
+      assert_equal @john_doe.id, @new_project.samples.find_by(name: @sample1.name).reload_log_data.responsible_id
+      assert_equal @john_doe.id, @new_project.samples.find_by(name: @sample2.name).reload_log_data.responsible_id
+      assert_equal 4, turbo_streams.size
+      # first 3 turbo streams are for progress bar updates
+      turbo_streams.take(3).each do |ts|
+        assert_equal 'replace', ts['action']
+        assert_equal "#{broadcast_target}-progress-bar", ts['target']
+      end
+      # last turbo stream is the success message
+      assert_equal 'replace', turbo_streams.last['action']
+      assert_equal 'transfer_samples_dialog_content', turbo_streams.last['target']
+      assert_includes turbo_streams.last.to_html, I18n.t('samples.transfers.create.success')
+    end
   end
 end
