5bf8f59b87db2fd5100d0fbca0be467fd1203ddf
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-07-02 14:18:50 -0500

    test(accessibility): cover detached button helper markup

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'nokogiri'
 
 # Test for ApplicationHelper
 class ApplicationHelperTest < ActionView::TestCase
@@ -33,5 +34,37 @@ class ApplicationHelperTest < ActionView::TestCase
       # For English locale, method explicitly returns '{}'
       assert_equal '{}', local_time_i18n_config
     end
+  end
+
+  test 'renders detached form and linked submit button' do
+    html = detached_button_to('Delete', '/projects/1', method: :delete, form_id: 'delete-project-1')
+    fragment = Nokogiri::HTML::DocumentFragment.parse(html)
+
+    form = fragment.at_css('form#delete-project-1')
+    button = fragment.at_css('button[form="delete-project-1"]')
+
+    assert_not_nil form
+    assert_not_nil button
+    assert_equal 'Delete', button.text.strip
+    assert_equal 'post', form['method']
+    assert_equal 'delete', form.at_css('input[name="_method"]')['value']
+    assert_equal '/projects/1', form['action']
+    assert_equal 'sr-only', form['class']
+  end
+
+  test 'accepts custom button attributes' do
+    html = detached_button_to(
+      'Archive',
+      '/projects/2/archive',
+      class: 'btn btn-danger',
+      data: { turbo_confirm: 'Are you sure?' }
+    )
+    fragment = Nokogiri::HTML::DocumentFragment.parse(html)
+    button = fragment.at_css('button')
+
+    assert_not_nil button
+    assert_equal 'btn btn-danger', button['class']
+    assert_equal 'Are you sure?', button['data-turbo-confirm']
+    assert button['form'].start_with?('detached_form_')
   end
 end
