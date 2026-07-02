eade8451e23c0529680ec76195cea1225fe34459
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-07-02 14:41:36 -0500

    refactor(helpers): route button_to through detached forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,18 +2,68 @@
 
 # Helper for Application
 module ApplicationHelper
-  # Builds a hidden form and a detached submit button linked through the HTML form attribute.
-  # This preserves Rails method spoofing and CSRF handling while allowing flexible layout.
+  # Renders detached button markup for all button_to-style calls.
+  # The hidden form carries method spoofing and hidden params while the button
+  # references the form with the HTML `form` attribute.
+  # @return [ActiveSupport::SafeBuffer]
+  def button_to(name = nil, options = nil, html_options = nil, &)
+    detached_button_to(name, options, html_options, &)
+  end
+
+  # Builds detached button markup and supports the same call patterns as button_to.
   # @return [ActiveSupport::SafeBuffer] Hidden form followed by linked button markup
-  def detached_button_to(label, url, method: :post, **options)
+  def detached_button_to(name = nil, options = nil, html_options = nil, &)
+    normalized_options = normalize_html_options(html_options)
+    form_class = requested_form_class(normalized_options)
+    markup = ActionView::Helpers::UrlHelper.instance_method(:button_to).bind_call(self, name, options,
+                                                                                  normalized_options, &)
+
+    detach_button_markup(markup, form_class:)
+  end
+
+  private
+
+  def requested_form_class(html_options)
+    return if html_options.blank?
+
+    options = html_options.to_h.with_indifferent_access
+    options[:form_class].presence || options.dig(:form, :class).presence
+  end
+
+  def normalize_html_options(html_options)
+    return html_options if html_options.blank?
+
+    options = html_options.to_h.deep_dup.with_indifferent_access
     form_id = options.delete(:form_id)
-    form_class = options.delete(:form_class) || 'sr-only'
-    generated_form_id = form_id || "detached_form_#{SecureRandom.hex(8)}"
+    return options if form_id.blank?
 
-    hidden_form = form_with(url:, method:, id: generated_form_id, class: form_class) { ''.html_safe }
-    detached_button = button_tag(label, { type: 'submit', form: generated_form_id }.merge(options))
+    options[:form] ||= {}
+    options[:form][:id] = form_id
+    options
+  end
 
-    safe_join([hidden_form, detached_button])
+  def detach_button_markup(markup, form_class: nil)
+    fragment = Nokogiri::HTML::DocumentFragment.parse(markup)
+    form = fragment.at_css('form')
+    control = form&.at_css('button, input[type="submit"]')
+    return markup if form.nil? || control.nil?
+
+    form_id = ensure_form_id!(form)
+    form['class'] = form_class || 'sr-only'
+    control['form'] = form_id
+    control.remove
+
+    # rubocop:disable Rails/OutputSafety
+    safe_join([form.to_html.html_safe, control.to_html.html_safe])
+    # rubocop:enable Rails/OutputSafety
+  end
+
+  def ensure_form_id!(form)
+    return form['id'] if form['id'].present?
+
+    generated_id = "detached_form_#{SecureRandom.hex(8)}"
+    form['id'] = generated_id
+    generated_id
   end
 
   # Returns the i18n configuration for LocalTime as a JSON string.
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -67,4 +67,45 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_equal 'Are you sure?', button['data-turbo-confirm']
     assert button['form'].start_with?('detached_form_')
   end
+
+  test 'supports params and method spoofing for button_to-compatible calls' do
+    html = detached_button_to(
+      'Remove',
+      '/projects/1',
+      method: :delete,
+      params: { sample_id: 12 }
+    )
+    fragment = Nokogiri::HTML::DocumentFragment.parse(html)
+    form = fragment.at_css('form')
+    button = fragment.at_css('button')
+
+    assert_not_nil form
+    assert_not_nil button
+    assert_not_nil form.at_css('input[name="_method"][value="delete"]')
+    assert_not_nil form.at_css('input[name="sample_id"][value="12"]')
+    assert_equal form['id'], button['form']
+  end
+
+  test 'supports block syntax compatible with button_to' do
+    html = detached_button_to('/projects/1', method: :get, class: 'button') do
+      '<span>Open</span>'.html_safe
+    end
+    fragment = Nokogiri::HTML::DocumentFragment.parse(html)
+    button = fragment.at_css('button')
+
+    assert_not_nil button
+    assert_equal 'button', button['class']
+    assert_includes button.to_html, '<span>Open</span>'
+  end
+
+  test 'overrides button_to with detached markup' do
+    html = button_to('Archive', '/projects/1', method: :post, class: 'button')
+    fragment = Nokogiri::HTML::DocumentFragment.parse(html)
+    form = fragment.at_css('form')
+    button = fragment.at_css('button')
+
+    assert_not_nil form
+    assert_not_nil button
+    assert_equal form['id'], button['form']
+  end
 end
