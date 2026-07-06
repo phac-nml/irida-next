1cdf10b752a32c9fb129921866ea51e5ce2e881d
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-07-06 14:05:15 -0500

    adding new cleanup service & job tests

--- test/jobs/workflow_executions/workflow_execution_cleanup_job_test.rb
+++ test/jobs/workflow_executions/workflow_execution_cleanup_job_test.rb
@@ -7,16 +7,54 @@ module WorkflowExecutions
   class WorkflowExecutionCleanupJobTest < ActiveJob::TestCase
     include FaradayTestHelpers
 
+    def setup
+      @stubs = faraday_test_adapter_stubs
+    end
+
+    def teardown
+      # reset connections after each test to clear cache
+      Faraday.default_connection = nil
+    end
+
     test 'successful job on completed workflow execution' do
       workflow_execution = workflow_executions(:irida_next_example_completed_unclean_DELETE)
 
       assert_not workflow_execution.cleaned?
 
-      perform_enqueued_jobs(only: WorkflowExecutionCleanupJob) do
-        WorkflowExecutionCleanupJob.perform_later(workflow_execution)
+      assert_difference -> { workflow_execution.outputs.count }, 2 do
+        with_cleanup_service_wes_stubs(workflow_execution, run_log_state: 'COMPLETE') do
+          perform_enqueued_jobs(only: WorkflowExecutionCleanupJob) do
+            WorkflowExecutionCleanupJob.perform_later(workflow_execution)
+          end
+        end
       end
 
       assert workflow_execution.reload.cleaned?
+      output_filenames = workflow_execution.outputs.map { |attachment| attachment.filename.to_s }
+      assert_includes output_filenames, 'run_log.json'
+      assert_includes output_filenames, 'run_stdout.json'
+
+      assert_performed_jobs(1, only: WorkflowExecutionCleanupJob)
+      assert_enqueued_jobs(0, except: Turbo::Streams::BroadcastStreamJob)
+    end
+
+    test 'successful job on completed workflow execution when stdout endpoint is unavailable' do
+      workflow_execution = workflow_executions(:irida_next_example_completed_unclean_DELETE)
+
+      assert_not workflow_execution.cleaned?
+
+      assert_difference -> { workflow_execution.outputs.count }, 1 do
+        with_cleanup_service_wes_stubs(workflow_execution, run_log_state: 'COMPLETE', stdout_not_found: true) do
+          perform_enqueued_jobs(only: WorkflowExecutionCleanupJob) do
+            WorkflowExecutionCleanupJob.perform_later(workflow_execution)
+          end
+        end
+      end
+
+      assert workflow_execution.reload.cleaned?
+      output_filenames = workflow_execution.outputs.map { |attachment| attachment.filename.to_s }
+      assert_includes output_filenames, 'run_log.json'
+      assert_not_includes output_filenames, 'run_stdout.json'
 
       assert_performed_jobs(1, only: WorkflowExecutionCleanupJob)
       assert_enqueued_jobs(0, except: Turbo::Streams::BroadcastStreamJob)
@@ -27,11 +65,18 @@ module WorkflowExecutions
 
       assert_not workflow_execution.cleaned?
 
-      perform_enqueued_jobs(only: WorkflowExecutionCleanupJob) do
-        WorkflowExecutionCleanupJob.perform_later(workflow_execution)
+      assert_no_difference -> { workflow_execution.outputs.count } do
+        with_cleanup_service_wes_stubs(workflow_execution, run_log_state: 'CANCELED') do
+          perform_enqueued_jobs(only: WorkflowExecutionCleanupJob) do
+            WorkflowExecutionCleanupJob.perform_later(workflow_execution)
+          end
+        end
       end
 
       assert workflow_execution.reload.cleaned?
+      output_filenames = workflow_execution.outputs.map { |attachment| attachment.filename.to_s }
+      assert_not_includes output_filenames, 'run_log.json'
+      assert_not_includes output_filenames, 'run_stdout.json'
 
       assert_performed_jobs(1, only: WorkflowExecutionCleanupJob)
       assert_enqueued_jobs(2, only: Turbo::Streams::BroadcastStreamJob)
@@ -43,14 +88,21 @@ module WorkflowExecutions
 
       assert_not workflow_execution.cleaned?
 
-      perform_enqueued_jobs(only: WorkflowExecutionCleanupJob) do
-        WorkflowExecutionCleanupJob.perform_later(workflow_execution)
+      assert_difference -> { workflow_execution.outputs.count }, 2 do
+        with_cleanup_service_wes_stubs(workflow_execution, run_log_state: 'SYSTEM_ERROR') do
+          perform_enqueued_jobs(only: WorkflowExecutionCleanupJob) do
+            WorkflowExecutionCleanupJob.perform_later(workflow_execution)
+          end
+        end
       end
 
       assert workflow_execution.reload.cleaned?
+      output_filenames = workflow_execution.outputs.map { |attachment| attachment.filename.to_s }
+      assert_includes output_filenames, 'run_log.json'
+      assert_includes output_filenames, 'run_stdout.json'
 
       assert_performed_jobs(1, only: WorkflowExecutionCleanupJob)
-      assert_enqueued_jobs(2, only: Turbo::Streams::BroadcastStreamJob)
+      assert_enqueued_jobs(12, only: Turbo::Streams::BroadcastStreamJob)
       assert_enqueued_jobs(0, except: Turbo::Streams::BroadcastStreamJob)
     end
 
@@ -66,14 +118,21 @@ module WorkflowExecutions
       assert_equal 'error', workflow_execution.state
       assert_not workflow_execution.cleaned?
 
-      perform_enqueued_jobs(only: WorkflowExecutionCleanupJob) do
-        WorkflowExecutionCleanupJob.perform_later(workflow_execution)
+      assert_difference -> { workflow_execution.outputs.count }, 2 do
+        with_cleanup_service_wes_stubs(workflow_execution, run_log_state: 'SYSTEM_ERROR') do
+          perform_enqueued_jobs(only: WorkflowExecutionCleanupJob) do
+            WorkflowExecutionCleanupJob.perform_later(workflow_execution)
+          end
+        end
       end
 
       assert workflow_execution.reload.cleaned?
+      output_filenames = workflow_execution.outputs.map { |attachment| attachment.filename.to_s }
+      assert_includes output_filenames, 'run_log.json'
+      assert_includes output_filenames, 'run_stdout.json'
 
       assert_performed_jobs(1, only: WorkflowExecutionCleanupJob)
-      assert_enqueued_jobs(4, only: Turbo::Streams::BroadcastStreamJob) # 2 got queued from setting the namespace to nil
+      assert_enqueued_jobs(14, only: Turbo::Streams::BroadcastStreamJob) # 2 queued from setting the namespace to nil
       assert_enqueued_jobs(0, except: Turbo::Streams::BroadcastStreamJob)
     end
 
@@ -87,6 +146,9 @@ module WorkflowExecutions
       end
 
       assert_not workflow_execution.reload.cleaned?
+      output_filenames = workflow_execution.outputs.map { |attachment| attachment.filename.to_s }
+      assert_not_includes output_filenames, 'run_log.json'
+      assert_not_includes output_filenames, 'run_stdout.json'
 
       assert_performed_jobs(1, only: WorkflowExecutionCleanupJob)
       assert_enqueued_jobs(0)
@@ -102,9 +164,53 @@ module WorkflowExecutions
       end
 
       assert workflow_execution.reload.cleaned?
+      output_filenames = workflow_execution.outputs.map { |attachment| attachment.filename.to_s }
+      assert_not_includes output_filenames, 'run_log.json'
+      assert_not_includes output_filenames, 'run_stdout.json'
 
       assert_performed_jobs(1, only: WorkflowExecutionCleanupJob)
       assert_enqueued_jobs(0)
+    end
+
+    private
+
+    def with_cleanup_service_wes_stubs(workflow_execution, run_log_state:, run_stdout: 'workflow stdout', # rubocop:disable Metrics/MethodLength
+                                       stdout_not_found: false)
+      mock_client = connection_builder(stubs: @stubs, connection_count: 1)
+
+      Integrations::Ga4ghWesApi::V1::ApiConnection.stub :new, mock_client do
+        @stubs.get("/runs/#{workflow_execution.run_id}") do |_env|
+          [
+            200,
+            { 'Content-Type': 'application/json' },
+            { run_id: workflow_execution.run_id, state: run_log_state }
+          ]
+        end
+
+        if stdout_not_found
+          @stubs.get("/runs/#{workflow_execution.run_id}/stdout") do |_env|
+            raise Faraday::ResourceNotFound.new({
+                                                  status: 'not found',
+                                                  headers: '',
+                                                  body: '',
+                                                  request: {
+                                                    method: 'get',
+                                                    url: "/runs/#{workflow_execution.run_id}/stdout"
+                                                  }
+                                                })
+          end
+        else
+          @stubs.get("/runs/#{workflow_execution.run_id}/stdout") do |_env|
+            [
+              200,
+              { 'Content-Type': 'text/plain' },
+              run_stdout
+            ]
+          end
+        end
+
+        yield
+      end
     end
   end
 end
