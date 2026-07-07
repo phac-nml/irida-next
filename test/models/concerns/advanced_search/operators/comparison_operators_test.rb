d27c82d0d8044aa6fcfdd9639aa36771cb424cbf
Chris Huynh <chris.huynh333@gmail.com>
2026-07-07 10:30:53 -0500

    fix tests from rebase

--- test/models/concerns/advanced_search/operators/comparison_operators_test.rb
+++ test/models/concerns/advanced_search/operators/comparison_operators_test.rb
@@ -29,15 +29,6 @@ module AdvancedSearch
       end
 
       test 'condition_less_than_or_equal uses lteq for regular fields with FF and metadata_field is false' do
-        result = @test_instance.send(:condition_less_than_or_equal,
-                                     @scope, @created_at_node, '2024-01-01', nil)
-        sql = result.to_sql
-        assert_includes sql, '<='
-        assert_not_includes sql, 'TO_DATE'
-        assert_not_includes sql, 'CAST'
-      end
-
-      test 'condition_less_than_or_equal uses lteq for regular fields with FF and metadata_field is false' do
         Flipper.enable(:advanced_search_metadata_operators)
         result = @test_instance.send(:condition_less_than_or_equal,
                                      @scope, @created_at_node, '2024-01-01', nil)
@@ -47,7 +38,7 @@ module AdvancedSearch
         assert_not_includes sql, 'CAST'
       end
 
-      test 'condition_less_than_or_equal uses lteq for regular fields with FF and metadata_field is false' do
+      test 'condition_less_than_or_equal uses lteq for regular fields without FF and metadata_field is false' do
         result = @test_instance.send(:condition_less_than_or_equal,
                                      @scope, @created_at_node, '2024-01-01', nil)
         sql = result.to_sql
@@ -83,15 +74,6 @@ module AdvancedSearch
       end
 
       test 'condition_greater_than_or_equal uses gteq for regular fields with FF and metadata_field is false' do
-        result = @test_instance.send(:condition_greater_than_or_equal,
-                                     @scope, @created_at_node, '2024-01-01',  nil)
-        sql = result.to_sql
-        assert_includes sql, '>='
-        assert_not_includes sql, 'TO_DATE'
-        assert_not_includes sql, 'CAST'
-      end
-
-      test 'condition_greater_than_or_equal uses gteq for regular fields with FF and metadata_field is false' do
         Flipper.enable(:advanced_search_metadata_operators)
         result = @test_instance.send(:condition_greater_than_or_equal,
                                      @scope, @created_at_node, '2024-01-01',  nil)
@@ -101,7 +83,7 @@ module AdvancedSearch
         assert_not_includes sql, 'CAST'
       end
 
-      test 'condition_greater_than_or_equal uses gteq for regular fields with FF and metadata_field is false' do
+      test 'condition_greater_than_or_equal uses gteq for regular fields without FF and metadata_field is false' do
         result = @test_instance.send(:condition_greater_than_or_equal,
                                      @scope, @created_at_node, '2024-01-01',  nil)
         sql = result.to_sql
