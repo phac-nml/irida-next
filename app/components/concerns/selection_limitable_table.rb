0335bcf9debc48568bcd0c3a454693aef93a51e8
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-07-09 14:04:00 -0500

    fix(selection-ui): handle storage quota alerts in controller

--- app/components/concerns/selection_limitable_table.rb
+++ app/components/concerns/selection_limitable_table.rb
@@ -6,17 +6,15 @@ module SelectionLimitableTable
 
   SELECTION_LIMIT_ALERT_ID = 'selection-limit-alert'
 
-  class_methods do
-    def selection_limit_exceeded?(count)
-      Irida::SelectionLimits.exceeded?(count)
-    end
-  end
-
   def selection_limit_data_attributes
     {
       'selection-max-selection-value': Irida::SelectionLimits::MAX_COUNT,
       'selection-limit-message-value': I18n.t(
         'components.selection.limit.selection_limit_reached',
+        max: Irida::SelectionLimits::MAX_COUNT
+      ),
+      'selection-storage-limit-message-value': I18n.t(
+        'components.selection.limit.storage_full',
         max: Irida::SelectionLimits::MAX_COUNT
       )
     }
@@ -37,6 +35,6 @@ module SelectionLimitableTable
   private
 
   def selection_limit_exceeded?(count)
-    self.class.selection_limit_exceeded?(count)
+    Irida::SelectionLimits.exceeded?(count)
   end
 end
