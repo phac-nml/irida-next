5124ac1fedade73491dbf68dc2914afdf38fb29b
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-07-10 09:55:12 -0500

    fix(system-feature-flags): rescue adapter errors in ChangeUserOptIn
    Address PR #2024 comment 3559649691

--- app/services/system_feature_flags/change_user_opt_in.rb
+++ app/services/system_feature_flags/change_user_opt_in.rb
@@ -16,12 +16,13 @@ module SystemFeatureFlags
       @user = user
     end
 
-    def execute # rubocop:disable Metrics/AbcSize
+    def execute # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
       return failure(:invalid_enabled, feature_key:) unless [true, false].include?(enabled)
 
       with_feature_lock(feature_key:, settings:) do
         abort_mutation!(:not_eligible) unless settings.opt_in_feature_eligible_for_user?(feature_key, user)
 
+        @feature_state_before_mutation = snapshot_feature_state(feature_key)
         if enabled
           Flipper.enable_actor(feature_key.to_sym, user)
         else
@@ -32,6 +33,12 @@ module SystemFeatureFlags
       success(change: nil, feature_key:)
     rescue AbortMutation => e
       failure(e.error, feature_key:)
+    rescue ActiveRecord::ActiveRecordError, Flipper::Error => e
+      # Defensive: transaction rollback restores DB state, but this ensures
+      # Flipper's in-memory cache is consistent with the database.
+      restore_feature_state!(feature_key, @feature_state_before_mutation) unless @feature_state_before_mutation.nil?
+      log_mutation_failure('Unable to change user experimental feature opt-in', e)
+      failure(:mutation_failed, feature_key:)
     end
 
     private
