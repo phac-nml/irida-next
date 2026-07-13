ce33cadd5f135bb44bb1e4ccc6c53fdecf91be56
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-07-13 14:05:58 -0500

    test(attachments-table): cover opt-in sort turbo replace

---
+++ test/components/attachments/table_component_test.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Attachments
+  class TableComponentTest < ViewComponent::TestCase
+    test 'adds turbo_action replace to sort links when sort_replaces_history is true' do
+      render_table_component(sort_replaces_history: true)
+
+      assert_selector 'table thead a[data-turbo-action="replace"]', count: 6
+    end
+
+    test 'does not add turbo_action replace to sort links when sort_replaces_history is false' do
+      render_table_component(sort_replaces_history: false)
+
+      assert_no_selector 'table thead a[data-turbo-action="replace"]'
+    end
+
+    private
+
+    def render_table_component(sort_replaces_history:)
+      with_request_url '/-/groups/group-1/-/attachments' do
+        attachments = Attachment.where(id: attachments(:attachment1).id)
+        pagy = pagy_for(attachments)
+        q = Attachment.ransack({ s: 'puid asc' })
+        render_inline Attachments::TableComponent.new(
+          attachments,
+          pagy,
+          q,
+          groups(:group_one),
+          true,
+          true,
+          sort_replaces_history:
+        )
+      end
+    end
+
+    def pagy_for(attachments)
+      Pagy::Offset.new(count: attachments.count, page: 1, limit: 20,
+                       request: Pagy::Request.new(
+                         request: { base_url: 'localhost:3000', path: '/', params: {} }
+                       ))
+    end
+  end
+end
