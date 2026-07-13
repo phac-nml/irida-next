8de73e3059ac13eeee7e8b911916a70ef47a0588
Chris Huynh <chris.huynh333@gmail.com>
2026-07-13 13:41:02 -0500

    add a couple floats to the previous changes

--- test/models/concerns/advanced_search/filtering_test.rb
+++ test/models/concerns/advanced_search/filtering_test.rb
@@ -50,17 +50,17 @@ module AdvancedSearch
 
     test 'operator numeric_less_than_equals' do
       condition = WorkflowExecution::SearchCondition.new(field: 'metadata.test_field',
-                                                         operator: 'numeric_less_than_equals', value: '20')
+                                                         operator: 'numeric_less_than_equals', value: '20.1')
       result = @test_instance.send(:add_condition,
                                    @test_instance.model_class, condition)
 
       sql = result.to_sql
-      assert_includes sql, "CAST(\"workflow_executions\".\"metadata\" ->> 'test_field' AS DOUBLE PRECISION) <= 20.0"
+      assert_includes sql, "CAST(\"workflow_executions\".\"metadata\" ->> 'test_field' AS DOUBLE PRECISION) <= 20.1"
     end
 
     test 'operator numeric_greater_than_equals' do
       condition = WorkflowExecution::SearchCondition.new(field: 'metadata.test_field',
-                                                         operator: 'numeric_greater_than_equals', value: '20')
+                                                         operator: 'numeric_greater_than_equals', value: '20.0')
       result = @test_instance.send(:add_condition,
                                    @test_instance.model_class, condition)
 
