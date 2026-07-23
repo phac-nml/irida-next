ce310abd6eba4c4f187661634166589ce9c4afcf
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-07-23 15:03:33 -0500

    removing selected_name

--- app/components/combobox/v1/option_component.rb
+++ app/components/combobox/v1/option_component.rb
@@ -4,6 +4,8 @@ module Combobox
   module V1
     # Option row used by combobox slots.
     class OptionComponent < ::Component
+      attr_reader :value, :label
+
       erb_template <<~ERB
         <%= tag.div(
           role: "option",
