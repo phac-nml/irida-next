aa845560c83c77f1e130a8e2c22e422bf0346d05
Chris Huynh <chris.huynh333@gmail.com>
2026-07-27 17:46:15 -0500

    start attachments listings refactor

--- app/controllers/projects/samples/attachments_controller.rb
+++ app/controllers/projects/samples/attachments_controller.rb
@@ -5,6 +5,7 @@ module Projects
     # Controller actions for Project Samples Attachments
     class AttachmentsController < Projects::Samples::ApplicationController # rubocop:disable Metrics/ClassLength
       include SampleAttachment
+      include ListActions
 
       before_action :attachment, only: %i[destroy]
       before_action :new_destroy_params, only: %i[new_destroy]
