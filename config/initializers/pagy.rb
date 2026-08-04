d2fd8261c1d88a1e4edcb1365d6d4bf2c12e4c0d
Sukhdeep Sidhu <ssidhu@phac5029663.cscscience.ca>
2026-08-04 10:57:28 -0500

    Updated pagy to latest version to fix vunerability

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -11,10 +11,10 @@ require 'pagy/toolbox/helpers/support/a_lambda'
 # Add your global options below. They will be applied globally.
 # For example:
 #
-Pagy.options[:limit] = 20               # Limit the items per page
-Pagy.options[:client_max_limit] = 100   # The client can request a limit up to 100
-# Pagy.options[:max_pages] = 200          # Allow only 200 pages
-# Pagy.options[:jsonapi] = true           # Use JSON:API compliant URLs
+Pagy::OPTIONS[:limit] = 20 # Limit the items per page
+Pagy::OPTIONS[:max_limit] = 100 # The client can request a limit up to 100
+# Pagy::OPTIONS[:max_pages] = 200          # Allow only 200 pages
+# Pagy::OPTIONS[:jsonapi] = true           # Use JSON:API compliant URLs
 
 ############ JavaScript ####################################################################
 # See https://ddnexus.github.io/pagy/resources/javascript/ for details.
