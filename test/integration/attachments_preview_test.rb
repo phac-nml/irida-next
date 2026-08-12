7b4040cee1d539b9cbe4af25616e027968cc2bd8
Chris Huynh <chris.huynh333@gmail.com>
2026-08-12 13:11:38 -0500

    remove unused translation, add a couple more tests

--- test/integration/attachments_preview_test.rb
+++ test/integration/attachments_preview_test.rb
@@ -231,4 +231,20 @@ class AttachmentsPreviewTest < ActionDispatch::IntegrationTest
     assert_select 'a', I18n.t('shared.workflow_executions.index.title')
     assert_select 'a', attachment.attachable.workflow_execution.name
   end
+
+  test 'should list attachments' do
+    project = projects(:projectA)
+    sample = samples(:sampleC)
+    namespace = namespaces_user_namespaces(:jeff_doe_namespace)
+
+    pe_fwd_attachment = attachments(:attachmentPEFWD4)
+    pe_rev_attachment = attachments(:attachmentPEREV4)
+    non_pe_attachment = attachments(:attachmentG)
+    post list_namespace_project_sample_attachments_path(namespace, project, sample, format: :turbo_stream), params: {
+      page: 1,
+      attachment_ids: ["[#{pe_fwd_attachment.id}, #{pe_rev_attachment.id}]", non_pe_attachment.id],
+      list_class: 'attachment'
+    }
+    assert_response :success
+  end
 end
