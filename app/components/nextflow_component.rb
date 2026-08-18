1d3c33004c7b69320e5d9a5b643630964e233d39
Chris Huynh <chris.huynh333@gmail.com>
2026-08-18 10:49:54 -0500

    add a default_version override to versioning nextflow component

--- app/components/nextflow_component.rb
+++ app/components/nextflow_component.rb
@@ -3,7 +3,10 @@
 # Stable entrypoint for rendering Nextflow component across UI versions.
 class NextflowComponent < Versioning::VersionedComponent
   IMPLEMENTATIONS = {
-    v1: Nextflow::V2::Component
+    v2: Nextflow::V2::Component
   }.freeze
-  VERSION_RESOLVER = -> { :v1 }
+
+  DEFAULT_VERSION = :v2
+
+  VERSION_RESOLVER = -> { :v2 }
 end
