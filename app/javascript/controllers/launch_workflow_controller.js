09d31ae662a43b641999392996644ba5a1b0c3bf
Chris Huynh <chris.huynh333@gmail.com>
2026-08-20 14:01:16 -0500

    refactor pipeline selection to receive sample count from backend

---
+++ app/javascript/controllers/launch_workflow_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus";
+import { createHiddenInput } from "utilities/form";
+
+export default class extends Controller {
+  static outlets = ["selection"];
+  static targets = ["form"];
+
+  appendSelectionLength(event) {
+    const form = document.getElementById("launch-workflow-form");
+    form.appendChild(
+      createHiddenInput(
+        "sample_count",
+        this.selectionOutlet.getOrCreateStoredItems().length,
+      ),
+    );
+  }
+}
