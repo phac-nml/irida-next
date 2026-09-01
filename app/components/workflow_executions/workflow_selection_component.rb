8be7461ceada36d5fcfaa2ca4230073b5c65844c
Chris Huynh <chris.huynh333@gmail.com>
2026-09-01 12:09:56 -0500

    cleanup

--- app/components/workflow_executions/workflow_selection_component.rb
+++ app/components/workflow_executions/workflow_selection_component.rb
@@ -1,39 +1,42 @@
 # frozen_string_literal: true
 
 module WorkflowExecutions
-  # Component for rendering and organizing the workflow selection buttons
+  # Component for rendering and organizing the workflow selection
   class WorkflowSelectionComponent < Component
     def initialize(workflows:, namespace_id:, sample_count:)
       @workflows = workflows
       @namespace_id = namespace_id
       @sample_count = sample_count.to_i
-      @organized_workflows = { available: [], unavailable: [] }
-      organize_workflows
+      @organized_workflows = organize_workflows
     end
 
     private
 
-    def organize_workflows # rubocop:disable Metrics/MethodLength
-      @workflows.each do |_, workflow| # rubocop:disable Style/HashEachMethods
-        min_samples_configured = workflow.min_samples_limit_configured?
-        max_samples_configured = workflow.max_samples_limit_configured?
-        message = ''
-
-        if min_samples_configured && @sample_count < minimum_samples(min_samples_configured, workflow)
-          message = I18n.t('shared.workflow_executions.sample_limits.min_samples_required',
-                           min_samples: workflow.minimum_samples)
-        end
-
-        if max_samples_configured && @sample_count > workflow.maximum_samples.to_i
-          message = I18n.t('shared.workflow_executions.sample_limits.max_samples_exceeded',
-                           max_samples: workflow.maximum_samples)
-        end
+    def organize_workflows
+      @workflows.each_with_object(available: [], unavailable: []) do |(_, workflow), organized|
+        message = sample_limit_message(workflow)
 
         if message.blank?
-          @organized_workflows[:available] << workflow
+          organized[:available] << workflow
         else
-          @organized_workflows[:unavailable] << { workflow:, message: }
+          organized[:unavailable] << { workflow:, message: }
         end
+      end
+    end
+
+    def sample_limit_message(workflow)
+      min_samples_configured = workflow.min_samples_limit_configured?
+      max_samples_configured = workflow.max_samples_limit_configured?
+
+      if min_samples_configured && @sample_count < minimum_samples(min_samples_configured, workflow)
+        I18n.t('shared.workflow_executions.sample_limits.min_samples_required',
+               min_samples: workflow.minimum_samples)
+
+      elsif max_samples_configured && @sample_count > workflow.maximum_samples.to_i
+        I18n.t('shared.workflow_executions.sample_limits.max_samples_exceeded',
+               max_samples: workflow.maximum_samples)
+      else
+        ''
       end
     end
 
