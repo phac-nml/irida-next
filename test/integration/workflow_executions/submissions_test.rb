9d6064535480cb8d88dc206e5d0a8b313bee48c9
Chris Huynh <chris.huynh333@gmail.com>
2026-09-01 11:32:03 -0500

    refactor a little

--- test/integration/workflow_executions/submissions_test.rb
+++ test/integration/workflow_executions/submissions_test.rb
@@ -33,9 +33,11 @@ module WorkflowExecutions
     end
 
     test 'pipeline selection request with minimum samples error' do
+      disabled_button_text = "phac-nml/iridanextexample 1.0.3 IRIDA Next Example Pipeline #{I18n.t(
+        'shared.workflow_executions.sample_limits.min_samples_required', min_samples: 2
+      )}"
       get pipeline_selection_workflow_executions_submissions_path(@group, sample_count: 1, format: :turbo_stream)
       assert_response :success
-
       assert_select 'h1', I18n.t('workflow_executions.submissions.pipeline_selection.title')
 
       # verify ordering
@@ -47,21 +49,18 @@ module WorkflowExecutions
         assert_equal 'phac-nml/iridanextexample 1.0.1 IRIDA Next Example Pipeline',
                      workflow_selection_button[2].text.squish
         assert_equal 'phac-nml/snvphylnfc 2.4.0 SNVPhyl nf-core pipeline', workflow_selection_button[3].text.squish
-        assert_equal "phac-nml/iridanextexample 1.0.3 IRIDA Next Example Pipeline #{I18n.t(
-          'shared.workflow_executions.sample_limits.min_samples_required', min_samples: 2
-        )}", workflow_selection_button[4].text.squish
+        assert_equal disabled_button_text, workflow_selection_button[4].text.squish
       end
-      assert_select 'button[aria-disabled="true"]', "phac-nml/iridanextexample 1.0.3 IRIDA Next Example Pipeline #{
-        I18n.t(
-          'shared.workflow_executions.sample_limits.min_samples_required', min_samples: 2
-        )}"
+      assert_select 'button[aria-disabled="true"]', disabled_button_text
       assert_select 'span', text: I18n.t('workflow_executions.submissions.pipeline_selection.unavailable'), count: 1
     end
 
     test 'pipeline selection request with maximum samples error' do
+      disabled_button_text = "phac-nml/iridanextexample 1.0.3 IRIDA Next Example Pipeline #{I18n.t(
+        'shared.workflow_executions.sample_limits.max_samples_exceeded', max_samples: 2
+      )}"
       get pipeline_selection_workflow_executions_submissions_path(@group, sample_count: 100, format: :turbo_stream)
       assert_response :success
-
       assert_select 'h1', I18n.t('workflow_executions.submissions.pipeline_selection.title')
 
       # verify ordering
@@ -73,14 +72,9 @@ module WorkflowExecutions
         assert_equal 'phac-nml/iridanextexample 1.0.1 IRIDA Next Example Pipeline',
                      workflow_selection_button[2].text.squish
         assert_equal 'phac-nml/snvphylnfc 2.4.0 SNVPhyl nf-core pipeline', workflow_selection_button[3].text.squish
-        assert_equal "phac-nml/iridanextexample 1.0.3 IRIDA Next Example Pipeline #{I18n.t(
-          'shared.workflow_executions.sample_limits.max_samples_exceeded', max_samples: 2
-        )}", workflow_selection_button[4].text.squish
+        assert_equal disabled_button_text, workflow_selection_button[4].text.squish
       end
-      assert_select 'button[aria-disabled="true"]', "phac-nml/iridanextexample 1.0.3 IRIDA Next Example Pipeline #{
-        I18n.t(
-          'shared.workflow_executions.sample_limits.max_samples_exceeded', max_samples: 2
-        )}"
+      assert_select 'button[aria-disabled="true"]', disabled_button_text
       assert_select 'span', text: I18n.t('workflow_executions.submissions.pipeline_selection.unavailable'), count: 1
     end
   end
