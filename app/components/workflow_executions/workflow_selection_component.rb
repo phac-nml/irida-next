2dcadacccf91f370efa9378d28644f8159282e32
Chris Huynh <chris.huynh333@gmail.com>
2026-09-01 10:25:33 -0500

    add organize workflow logic

--- app/components/workflow_executions/workflow_selection_component.rb
+++ app/components/workflow_executions/workflow_selection_component.rb
@@ -6,45 +6,45 @@ module WorkflowExecutions
     def initialize(workflows:, namespace_id:, sample_count:)
       @workflows = workflows
       @namespace_id = namespace_id
-      @sample_count = sample_count
-
+      @sample_count = sample_count.to_i
+      @organized_workflows = { available: [], unavailable: [] }
       organize_workflows
-      # @workflow = workflow
-      # @sample_count = sample_count.to_i
-      # @max_samples_configured = max_samples_configured?
-      # @min_samples_configured = min_samples_configured?
-      # @disabled_message = validate_workflow
-      # @disabled = @disabled_message.present?
+      puts @organized_workflows
     end
 
     private
 
-    def organize_workflows; end
+    def organize_workflows
+      @workflows.each do |_, workflow|
+        puts 'engpdfdfdeagaegdfdfeawg'
+        puts @workflows
+        puts workflow
+        min_samples_configured = workflow.min_samples_limit_configured?
+        max_samples_configured = workflow.max_samples_limit_configured?
+        next unless min_samples_configured || max_samples_configured
 
-    def min_samples_configured?
-      @workflow.min_samples_limit_configured?
-    end
+        message = ''
+        if min_samples_configured && @sample_count < minimum_samples(min_samples_configured, workflow)
+          message = I18n.t('shared.workflow_executions.sample_limits.min_samples_required',
+                           min_samples: workflow.minimum_samples)
+        end
 
-    def max_samples_configured?
-      @workflow.max_samples_limit_configured?
-    end
+        if max_samples_configured && @sample_count > workflow.maximum_samples.to_i
+          message = I18n.t('shared.workflow_executions.sample_limits.max_samples_exceeded',
+                           max_samples: @workflow.maximum_samples)
+        end
 
-    def validate_workflow
-      return '' unless @max_samples_configured || @min_samples_configured
-
-      if @min_samples_configured && @sample_count < minimum_samples
-        return I18n.t('shared.workflow_executions.sample_limits.min_samples_required',
-                      min_samples: @workflow.minimum_samples)
+        if message.blank?
+          @organized_workflows[:available] << workflow
+        else
+          @organized_workflows[:unavailable] << { workflow:, message: }
+        end
       end
-
-      return unless @max_samples_configured && @sample_count > @workflow.maximum_samples.to_i
-
-      I18n.t('shared.workflow_executions.sample_limits.max_samples_exceeded', max_samples: @workflow.maximum_samples)
     end
 
-    def minimum_samples
-      if @min_samples_configured
-        @workflow.minimum_samples.to_i
+    def minimum_samples(min_samples_configured, workflow)
+      if min_samples_configured
+        workflow.minimum_samples.to_i
       else
         0
       end
