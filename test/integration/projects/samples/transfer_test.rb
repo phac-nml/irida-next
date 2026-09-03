e90b958ffd977642d41a1015fb4e3ab2fb71338e
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2026-09-03 18:55:13 -0500

    doing groups side of the same tests

--- test/integration/projects/samples/transfer_test.rb
+++ test/integration/projects/samples/transfer_test.rb
@@ -17,37 +17,6 @@ module Projects
         @broadcast_target = "samples_transfer_test_#{SecureRandom.uuid}"
       end
 
-      test 'should get new for group if owner' do
-        get new_samples_transfer_path(namespace_id: @namespace.id, format: :turbo_stream)
-        assert_response :success
-      end
-
-      test 'should get new for group if maintainer' do
-        sign_in users(:joan_doe)
-
-        get new_samples_transfer_path(namespace_id: @namespace.id, format: :turbo_stream)
-        assert_response :success
-      end
-
-      test 'should not get new for group if access level less than a maintainer' do
-        sign_in users(:ryan_doe)
-
-        get new_samples_transfer_path(namespace_id: @namespace.id, format: :turbo_stream)
-        assert_response :unauthorized
-      end
-
-      [[::Samples::TransferJob, false], [::Samples::TransferJobV2, true]].each do |job_class, v2|
-        test "should enqueue a #{job_class.name} for group" do
-          Flipper.enable(:v2_sample_transfer) if v2
-
-          assert_enqueued_jobs 1, only: job_class do
-            post_transfer(sample_ids: [@sample1.id, @sample2.id], destination: @project2)
-          end
-
-          Flipper.disable(:v2_sample_transfer) if v2
-        end
-      end
-
       test 'should get new for project if owner' do
         get new_samples_transfer_path(namespace_id: @project.namespace.id, format: :turbo_stream)
         assert_response :success
@@ -74,7 +43,7 @@ module Projects
                    broadcast_target: 'a_broadcast_target'
                  }
           end
-
+        ensure
           Flipper.disable(:v2_sample_transfer) if v2
         end
       end
@@ -87,18 +56,20 @@ module Projects
           assert_transfer_dialog
         end
 
-        post list_samples_path(format: :turbo_stream),
-             params: { page: 1, sample_ids: @project.samples.ids, list_class: 'sample' },
-             as: :turbo_stream
-        assert_response :success
-        assert_select 'turbo-stream[action="append"][target="list_selections"]' do
-          assert_select 'template' do
-            @project.samples.each do |sample|
-              assert_select 'p', text: /#{Regexp.escape(sample.name)}/
-              assert_select 'p', text: /#{Regexp.escape(sample.puid)}/
-            end
-          end
-        end
+        post_list(@project.samples.ids)
+
+        # post list_samples_path(format: :turbo_stream),
+        #      params: { page: 1, sample_ids: @project.samples.ids, list_class: 'sample' },
+        #      as: :turbo_stream
+        # assert_response :success
+        # assert_select 'turbo-stream[action="append"][target="list_selections"]' do
+        #   assert_select 'template' do
+        #     @project.samples.each do |sample|
+        #       assert_select 'p', text: /#{Regexp.escape(sample.name)}/
+        #       assert_select 'p', text: /#{Regexp.escape(sample.puid)}/
+        #     end
+        #   end
+        # end
       end
 
       test 'transfer dialog with plural description' do
@@ -146,7 +117,7 @@ module Projects
           [@sample1, @sample2, @sample30].each do |sample|
             assert_select "tbody#samples-table-body tr##{dom_id(sample)}", count: 1
           end
-
+        ensure
           Flipper.disable(:v2_sample_transfer) if v2
         end
       end
@@ -172,16 +143,21 @@ module Projects
             assert_select '[role="progressbar"]'
             assert_select 'button.dialog--close', count: 0
           end
-
+        ensure
           Flipper.disable(:v2_sample_transfer) if v2_job
           Flipper.disable(:v2_select2) if v2_select
         end
       end
 
-      test 'should not transfer samples with session storage cleared' do
-        assert_samples_page(@project, 3)
-        assert_transfer_enqueued(::Samples::TransferJob, sample_ids: [])
-        assert_no_transfer_progress_or_selection(::Samples::TransferJob)
+      [[::Samples::TransferJob, false], [::Samples::TransferJobV2, true]].each do |job_class, v2|
+        test "should not transfer samples with session storage cleared class: #{job_class.name}" do
+          Flipper.enable(:v2_sample_transfer) if v2
+          assert_samples_page(@project, 3)
+          assert_transfer_enqueued(job_class, sample_ids: [])
+          assert_no_transfer_progress_or_selection(job_class)
+        end
+      ensure
+        Flipper.disable(:v2_sample_transfer) if v2
       end
 
       test 'transfer samples with and without same name in destination project' do
@@ -214,15 +190,6 @@ module Projects
         assert_not @project.samples.exists?(@sample1.id)
         assert_samples_page(@project2, 21)
         assert_select 'tbody#samples-table-body input[name="sample_ids[]"][checked]', count: 0
-      end
-
-      test 'should not transfer samples with session storage cleared v2' do
-        Flipper.enable(:v2_sample_transfer)
-        assert_samples_page(@project, 3)
-        assert_transfer_enqueued(::Samples::TransferJobV2, sample_ids: [])
-        assert_no_transfer_progress_or_selection(::Samples::TransferJobV2)
-      ensure
-        Flipper.disable(:v2_sample_transfer)
       end
 
       test 'transfer samples with and without same name in destination project v2' do
