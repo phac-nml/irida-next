65512d470532f6704d35dfca37e07274506453ce
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-09-03 08:11:16 -0500

    adding service tests for code coverage

--- test/services/projects/transfer_service_test.rb
+++ test/services/projects/transfer_service_test.rb
@@ -303,5 +303,36 @@ module Projects
 
       assert_not public_subgroup1_project1.namespace.public?
     end
+
+    test 'transfer private project to public group with flipper disabled' do
+      Flipper.disable(:global_groups)
+      public_group_namespace = groups(:public_group1)
+      transfer_form = ::Projects::TransferForm.new(
+        { new_namespace_id: public_group_namespace.id }.merge(project: @project)
+      )
+      assert_not @project.namespace.public?
+
+      assert_changes -> { @project.namespace.parent }, to: public_group_namespace do
+        Projects::TransferService.new(@project, @john_doe, transfer_form).execute
+      end
+
+      assert_not @project.namespace.public?
+    end
+
+    test 'transfer public project to private group with flipper disabled' do
+      Flipper.disable(:global_groups)
+      private_group_namespace = groups(:group_one)
+      public_subgroup1_project1 = projects(:subgroup1Project1)
+      transfer_form = ::Projects::TransferForm.new(
+        { new_namespace_id: private_group_namespace.id }.merge(project: public_subgroup1_project1)
+      )
+      assert public_subgroup1_project1.namespace.public?
+
+      assert_changes -> { public_subgroup1_project1.namespace.parent }, to: private_group_namespace do
+        Projects::TransferService.new(public_subgroup1_project1, @john_doe, transfer_form).execute
+      end
+
+      assert public_subgroup1_project1.namespace.public?
+    end
   end
 end
