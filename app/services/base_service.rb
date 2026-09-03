f04080f16eb1c096b813dfafeec8e45372a915a2
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-09-03 15:00:58 -0500

    add transfer guard to v2_sample_transfer

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -30,6 +30,25 @@ class BaseService
 
   private
 
+  # Validate that samples do not have active workflow executions.
+  #
+  # Prevents actions on samples that are currently being used in a workflow execution.
+  # Active workflow states are: initial, prepared, submitted, running.
+  #
+  # @param sample_ids [Array<Integer>] IDs of samples to check
+  # @param action_type [String] action i18n namespace (e.g., 'transfer', 'destroy')
+  # @param error_class [Class] exception class to raise on validation failure
+  # @raise [StandardError] if any samples have active workflow executions
+  def validate_no_active_workflow_executions_for_action(sample_ids, action_type:, error_class: BaseError)
+    active_workflow_sample_puids = active_workflow_execution_sample_puids(sample_ids)
+
+    return if active_workflow_sample_puids.empty?
+
+    raise error_class,
+          I18n.t("services.samples.#{action_type}.active_workflow_executions",
+                 sample_puids: active_workflow_sample_puids.join(', '))
+  end
+
   def strip_whitespaces(string)
     string.gsub(/\s+/, ' ').strip
   end
--- app/services/samples/transfer_service.rb
+++ app/services/samples/transfer_service.rb
@@ -41,7 +41,11 @@ module Samples
       validate(sample_ids, 'transfer', new_project_id)
 
       if Flipper.enabled?(:prevent_sample_deletions_and_transfers_with_active_workflows)
-        validate_no_active_workflow_executions(sample_ids)
+        validate_no_active_workflow_executions_for_action(
+          sample_ids,
+          action_type: 'transfer',
+          error_class: TransferError
+        )
       end
 
       @new_project = Project.find_by(id: new_project_id)
@@ -55,23 +59,6 @@ module Samples
     rescue BaseSampleService::BaseError, TransferService::TransferError => e
       @namespace.errors.add(:base, e.message)
       []
-    end
-
-    # Validate that samples do not have active workflow executions.
-    #
-    # Prevents transferring samples that are currently being used in a workflow execution.
-    # Active workflow states are: initial, prepared, submitted, running.
-    #
-    # @param sample_ids [Array<Integer>] IDs of samples to check
-    # @raise [TransferError] if any samples have active workflow executions
-    def validate_no_active_workflow_executions(sample_ids)
-      active_workflow_sample_puids = active_workflow_execution_sample_puids(sample_ids)
-
-      return if active_workflow_sample_puids.empty?
-
-      raise TransferError,
-            I18n.t('services.samples.transfer.active_workflow_executions',
-                   sample_puids: active_workflow_sample_puids.join(', '))
     end
 
     # Validate that a maintainer can transfer samples between projects.
