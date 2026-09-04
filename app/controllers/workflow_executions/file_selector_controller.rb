2bbd15da436db45bbbe9875ac568ab57c3c36c0a
Chris Huynh <chris.huynh333@gmail.com>
2026-09-04 09:33:58 -0500

    add fallback to def attachable

--- app/controllers/workflow_executions/file_selector_controller.rb
+++ app/controllers/workflow_executions/file_selector_controller.rb
@@ -68,13 +68,16 @@ module WorkflowExecutions
         @attachable = authorized_scope(Sample, type: :relation, as: :namespace_samples,
                                                scope_options: { namespace: @namespace })
                       .where(id: attachable_id).first
+
       when Namespaces::ProjectNamespace.sti_name, Group.sti_name
         @attachable = Namespace.where(id: authorized_namespace_ids)
                                .where(type: file_selector_params[:attachable_type])
                                .find(attachable_id)
-      else
-        raise ActiveRecord::RecordNotFound
       end
+
+      return unless @attachable.nil?
+
+      raise ActiveRecord::RecordNotFound
     end
 
     def attachments
--- test/controllers/workflow_executions/file_selector_controller_test.rb
+++ test/controllers/workflow_executions/file_selector_controller_test.rb
@@ -187,6 +187,34 @@ module WorkflowExecutions
       assert_response :unauthorized
     end
 
+    test 'new file selection with invalid attachable_id and attachable type Sample' do
+      get new_workflow_executions_file_selector_path(
+        file_selector: @expected_fastq_params.merge(attachable_id: 'invalid id'),
+        format: :turbo_stream
+      )
+
+      assert_response :not_found
+    end
+
+    test 'new file selection with invalid attachable_id and attachable type Namespace' do
+      sign_in users(:snvphyl_user)
+      get new_workflow_executions_file_selector_path(
+        file_selector: @project_ref_params.merge(attachable_id: 'invalid id'),
+        format: :turbo_stream
+      )
+
+      assert_response :not_found
+    end
+
+    test 'new file selection with invalid attachable_type' do
+      get new_workflow_executions_file_selector_path(
+        file_selector: @expected_fastq_params.merge(attachable_type: 'invalid type'),
+        format: :turbo_stream
+      )
+
+      assert_response :not_found
+    end
+
     private
 
     def parsed_files_payload
