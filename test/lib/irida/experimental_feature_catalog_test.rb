ade73192ec3ccde5fefdfb3ef2b5a98f101192dc
Josh Adam <josh.adam@phac-aspc.gc.ca>
2026-09-09 14:39:40 -0500

    test(feature-flags): include advanced_search_v2 in admin catalog order

--- test/lib/irida/experimental_feature_catalog_test.rb
+++ test/lib/irida/experimental_feature_catalog_test.rb
@@ -11,6 +11,7 @@ module Irida
         accessibility_statement
         advanced_search_disable_standard_operators_for_metadata_in_graphql
         advanced_search_metadata_operators
+        advanced_search_v2
         advanced_search_with_auto_complete
         automated_workflow_execution_subscriber
         client_linelist_exports_v1
