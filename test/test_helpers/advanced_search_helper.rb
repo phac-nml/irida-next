2ce5e548f6887b432b2b6a8c236672e841a1c6cf
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-09-10 10:02:54 -0500

    renaming advanced search helper params

--- test/test_helpers/advanced_search_helper.rb
+++ test/test_helpers/advanced_search_helper.rb
@@ -6,7 +6,7 @@ module AdvancedSearchHelper
   # submits. `groups` is an array of groups, where each group is an array of condition
   # hashes such as { field:, operator:, value: } (omit `value` for existence operators,
   # or pass an array for `in`/`not_in`).
-  def attachments_advanced_search_params(groups)
+  def advanced_search_params(groups)
     groups_attributes = groups.each_with_index.to_h do |conditions, group_index|
       conditions_attributes = conditions.each_with_index.to_h do |condition, condition_index|
         [condition_index.to_s, condition]
