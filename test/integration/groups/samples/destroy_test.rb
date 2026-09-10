1a66a2628d4c6d5dce9503cdaa9fefd22bef09a4
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2026-09-10 15:42:38 -0500

    more test coverage

--- test/integration/groups/samples/destroy_test.rb
+++ test/integration/groups/samples/destroy_test.rb
@@ -217,6 +217,25 @@ module Groups
         end
       end
 
+      test 'samples listing within delete samples dialog with reason' do
+        Flipper.enable(:sample_deletion_reason)
+        get new_samples_deletions_path,
+            params: {
+              namespace_id: @group1.id,
+              deletion_type: 'multiple',
+              deletion: {
+                reason: 'cleanup'
+              }
+            }, as: :turbo_stream
+
+        assert_response :success
+        assert_select 'turbo-stream[target="samples_dialog"]' do
+          assert_select 'turbo-frame#list_selections'
+        end
+      ensure
+        Flipper.disable(:sample_deletion_reason)
+      end
+
       test 'delete samples belonging to group' do
         assert_samples_page(@group1, 26)
         assert_difference('Sample.count', -2) do
