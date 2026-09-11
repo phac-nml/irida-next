286b26f8c2987e805e5416f533f18bae18b3d3fc
Sukhdeep Sidhu <ssidhu@phac5029663.cscscience.ca>
2026-09-11 12:09:31 -0500

    Added tests

--- test/integration/groups/groups_test.rb
+++ test/integration/groups/groups_test.rb
@@ -42,6 +42,7 @@ module Groups
       assert_select 'h1', text: 'New Group'
 
       get edit_group_path(created_group)
+      assert_response :success
 
       assert_select 'h1', text: I18n.t(:'groups.edit.details.title')
 
@@ -65,6 +66,42 @@ module Groups
 
       assert_select 'h2', text: I18n.t(:'groups.edit.advanced.change_visibility.title')
       assert_select 'p', text: I18n.t(:'groups.edit.advanced.change_visibility.description.private')
+    end
+
+    test 'should not display visibility options when global_groups is disabled' do
+      Flipper.disable(:global_groups)
+      sign_in @user
+      get new_group_path
+      assert_response :success
+
+      assert_select 'div', text: I18n.t(:'groups.new.visibility.title'), count: 0
+      assert_select 'label', text: I18n.t(:'groups.new.visibility.private.label'), count: 0
+      assert_select 'span', text: I18n.t(:'groups.new.visibility.private.description'), count: 0
+      assert_select 'label', text: I18n.t(:'groups.new.visibility.public.label'), count: 0
+      assert_select 'span', text: I18n.t(:'groups.new.visibility.public.description'), count: 0
+    end
+
+    test 'can create a group with global_groups is disabled' do
+      Flipper.disable(:global_groups)
+      sign_in @user
+
+      params = { group: { name: 'New Group 3', path: 'new-group-3', description: 'This is yet another new group' } }
+      created_group = nil
+
+      assert_difference('Group.count', 1) do
+        post groups_path, params: params
+        created_group = Group.last
+      end
+
+      follow_redirect!
+      assert_response :success
+
+      assert_select 'h1', text: 'New Group 3'
+
+      get edit_group_path(created_group)
+      assert_response :success
+
+      assert_select 'h2', text: I18n.t(:'groups.edit.advanced.change_visibility.title'), count: 0
     end
   end
 end
