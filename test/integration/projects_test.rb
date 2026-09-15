dbb023e557d7ad6ceec392df5b7b965967633638
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2026-09-15 13:12:34 -0500

    code review changes

--- test/integration/projects_test.rb
+++ test/integration/projects_test.rb
@@ -12,6 +12,23 @@ class ProjectsTest < ActionDispatch::IntegrationTest
     @old_namespace = groups(:group_one)
   end
 
+  test 'owner can see project transfer section in general settings' do
+    get namespace_project_edit_path(@project.namespace.parent, @project)
+
+    assert_response :success
+    assert_select 'h2', text: I18n.t('projects.edit.advanced.transfer.title')
+  end
+
+  test 'maintainer cannot see project transfer section in general settings' do
+    sign_in users(:joan_doe)
+    get namespace_project_edit_path(@project.namespace.parent, @project)
+
+    assert_response :success
+    assert_equal Member::AccessLevel::MAINTAINER,
+                 Member.find_by(user: users(:joan_doe), namespace: @project.namespace.parent).access_level
+    assert_select 'h2', text: I18n.t('projects.edit.advanced.transfer.title'), count: 0
+  end
+
   test 'should redirect with success flash when transfer succeeds' do
     assert_changes -> { @project.namespace.reload.parent }, from: @old_namespace, to: @namespace do
       post namespace_project_transfer_path(@old_namespace, @project),
@@ -32,6 +49,26 @@ class ProjectsTest < ActionDispatch::IntegrationTest
     end
 
     assert_response :unauthorized
+    assert_select "div[data-viral--flash-type-value='error']" do
+      assert_select 'div',
+                    "#{I18n.t('common.statuses.error')}: #{I18n.t(
+                      'action_policy.policy.project.transfer?',
+                      name: @project.name
+                    )}"
+    end
+  end
+
+  test 'maintainer should not transfer a project' do
+    sign_in users(:joan_doe)
+
+    assert_no_changes -> { @project.namespace.reload.parent } do
+      post namespace_project_transfer_path(@old_namespace, @project),
+           params: { projects_transfer_form: { new_namespace_id: @namespace.id } }, as: :turbo_stream
+    end
+
+    assert_response :unauthorized
+    assert_equal Member::AccessLevel::MAINTAINER,
+                 Member.find_by(user: users(:joan_doe), namespace: @project.namespace.parent).access_level
     assert_select "div[data-viral--flash-type-value='error']" do
       assert_select 'div',
                     "#{I18n.t('common.statuses.error')}: #{I18n.t(
